9a53d3e9cfbe8100f5088266915861a1434e354a
Jum <zheyangkaishi@gmail.com>
2016-12-02 13:09:04 +0800

    RangeSumQueryImmutable

---
+++ python/RangeSumQueryImmutable.py
@@ -0,0 +1,24 @@
+class NumArray(object):
+    def __init__(self, nums):
+        """
+        initialize your data structure here.
+        :type nums: List[int]
+        """
+        self.l=list(nums)
+        
+
+    def sumRange(self, i, j):
+        """
+        sum of elements nums[i..j], inclusive.
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        return sum(self.l[i:j+1])
+        
+
+
+# Your NumArray object will be instantiated and called as such:
+# numArray = NumArray(nums)
+# numArray.sumRange(0, 1)
+# numArray.sumRange(1, 2)
