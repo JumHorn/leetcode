5b4f1a1c89e56d1a87eaa409ea62aaaa4abc76d2
Jum <zheyangkaishi@gmail.com>
2016-12-05 17:48:55 +0800

    shell

---
+++ shell/WordFrequency.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+declare -A map=()
+while read line 
+do 
+for n in $line
+do 
+map["$n"]=expr'map["$n"]+1'
+done
+done < words.txt
+for n in ${!map[@]}
+do
+echo "$n ${map[$n]}"
+done
---
+++ shell/read.sh
@@ -0,0 +1,10 @@
+while read line;
+do 
+echo $line;
+for n in $line;
+do 
+echo $n;
+done
+done < words.txt
+
+
