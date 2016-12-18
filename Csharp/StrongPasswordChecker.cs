fdfaaab2067c8c27913678cb2e70a3e2c463f4b5
Jum <zheyangkaishi@gmail.com>
2016-12-19 00:22:08 +0800

    无法处理4个以上相同的删除

--- Csharp/StrongPasswordChecker.cs
+++ Csharp/StrongPasswordChecker.cs
@@ -8,7 +8,20 @@ public class StrongPasswordChecker
         len=s.Length;
         if(len<6)
         {
-            temp=6-len+repeat_check(pwd);
+            temp=6-len;
+            t=number_check(pwd)+uppercase_check(pwd)+lowercase_check(pwd);
+            if(temp>=t)
+            {
+                return temp;
+            }
+            else
+            {
+                return t;
+            }
+        }
+        if(len<21)
+        {
+            temp=repeat_check(pwd);
             t=number_check(pwd)+uppercase_check(pwd)+lowercase_check(pwd);
             if(temp>=t)
             {
@@ -21,7 +34,16 @@ public class StrongPasswordChecker
         }
         else
         {
-            
+            temp=repeat_check(pwd);
+            t=number_check(pwd)+uppercase_check(pwd)+lowercase_check(pwd)+len-20;
+            if(temp>=t)
+            {
+                return temp;
+            }
+            else
+            {
+                return t;
+            }
         }
         return 0;
     }
@@ -36,13 +58,15 @@ public class StrongPasswordChecker
     public int repeat_check(char[] s)
     {   
         int i=0,t=0;
-        for(i=0;i<s.Length-2;i++)
+        while(i<s.Length-2)
         {
             if(s[i]==s[i+1]&&s[i]==s[i+2])
             {
                 t++;
                 i+=3;
+                continue;
             }
+            i++;
         }
         return t;
     }
