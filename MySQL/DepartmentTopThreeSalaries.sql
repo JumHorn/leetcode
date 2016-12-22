7d0e7db8d8faf48ed2885b19bb205e5a82cd0ee1
Jum <zheyangkaishi@gmail.com>
2016-12-23 00:37:01 +0800

    topthreehighestsalary

--- MySQL/DepartmentTopThreeSalaries.sql
+++ MySQL/DepartmentTopThreeSalaries.sql
@@ -1,4 +1,11 @@
 # Write your MySQL query statement below
 select D1.Name as Department,E1.Name as Employee, E1.Salary as Salary
 from Employee E1 inner join Department D1 on E1.DepartmentId=D1.Id
-where E1.Salary >= (select min(Salary) from Employee E2 where E1.DepartmentId=E2.DepartmentId order by E2.Salary desc limit 0,3)
+where E1.Salary >= (select min(Salary) from Employee E2 where E2.DepartmentId=1 order by E2.Salary desc limit 0,3;)
+
+-- [1 "Joe" 60000 1]
+-- [2 "Ralph" 30000 1]
+-- [3 "Joel" 50000 1]
+-- [4 "Tracy" 55000 1]
+-- "Department": 
+-- [1 "IT"]
