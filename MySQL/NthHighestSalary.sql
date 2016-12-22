f28079f210212f454c14ca0ca0060b93dd364e4e
Jum <zheyangkaishi@gmail.com>
2016-12-22 15:09:59 +0800

    Nthhighestsalary

--- MySQL/NthHighestSalary.sql
+++ MySQL/NthHighestSalary.sql
@@ -1,10 +1,14 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-  RETURN (
-      # Write your MySQL query statement below.
-      select distinct Salary
+      declare a int;
+      declare m int;
+      set a=N-1;
+      if N=0 then set m=NULL;
+      else
+      select distinct Salary into m
       from Employee
       order by Salary desc
-      limit (N-1),N
-  );
+      limit a,1;
+      end if;
+  RETURN m;
 END
