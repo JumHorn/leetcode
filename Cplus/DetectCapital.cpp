c7422349d93acf3d3134299f7d15c3d2ebc50fcb
Jum <zheyangkaishi@gmail.com>
2017-08-13 11:34:23 +0800

    DetectCapital.cpp

---
+++ Cplus/DetectCapital.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<cctype>
+using namespace std;
+
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        if(word.size()<=1)
+            return true;
+        if(isupper(word[0]))
+        {
+            if(isupper(word[1]))
+            {
+                for(int i=2;i<word.length();i++)
+                {
+                    if(!isupper(word[i]))
+                    {
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                for(int i=2;i<word.length();i++)
+                {
+                    if(isupper(word[i]))
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        else
+        {
+            for(int i=1;i<word.length();i++)
+            {
+                if(isupper(word[i]))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+int main()
+{}
