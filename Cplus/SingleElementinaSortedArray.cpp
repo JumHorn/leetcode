12666ef6914582fa189cf323bee49c0e63863078
Jum <zheyangkaishi@gmail.com>
2018-03-22 23:02:56 +0800

    SingleElementinaSortedArray

---
+++ Cplus/SingleElementinaSortedArray.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int result=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            result^=nums[i];
+        }
+        return result;
+    }
+};
