6fc59b02b765c02515ad5de978d4eb8b790ad0dd
JumHorn <zheyangkaishi@gmail.com>
2018-06-10 19:57:50 +0800

    update another solution

--- Cplus/TopKFrequentElements.cpp
+++ Cplus/TopKFrequentElements.cpp
@@ -36,3 +36,34 @@ public:
 		return res;
 	}
 };
+
+//java solution
+//开辟一个大的数组能存储最大的value值的下标
+//将value作为下标，key作为值存储到数组中，再从数组中反向查找k个值
+
+// public List<Integer> topKFrequent(int[] nums, int k) {
+
+// 	List<Integer>[] bucket = new List[nums.length + 1];
+// 	Map<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();
+
+// 	for (int n : nums) {
+// 		frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
+// 	}
+
+// 	for (int key : frequencyMap.keySet()) {
+// 		int frequency = frequencyMap.get(key);
+// 		if (bucket[frequency] == null) {
+// 			bucket[frequency] = new ArrayList<>();
+// 		}
+// 		bucket[frequency].add(key);
+// 	}
+
+// 	List<Integer> res = new ArrayList<>();
+
+// 	for (int pos = bucket.length - 1; pos >= 0 && res.size() < k; pos--) {
+// 		if (bucket[pos] != null) {
+// 			res.addAll(bucket[pos]);
+// 		}
+// 	}
+// 	return res;
+// }
