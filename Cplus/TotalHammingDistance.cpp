65b73e80f80d4022e311639f7d0506e8094e72e5
JumHorn <zheyangkaishi@gmail.com>
2018-09-13 22:25:54 +0800

    Total Hamming Distance

--- Cplus/TotalHammingDistance.cpp
+++ Cplus/TotalHammingDistance.cpp
@@ -5,29 +5,47 @@ class Solution {
 public:
     int totalHammingDistance(vector<int>& nums) {
 		int res=0;
-		if(nums.empty())
+		for(int i=0;i<32;i++)
 		{
-			return 0;
-		}
-		for(int i=0;i<nums.size()-1;i++)//substract 1 cause great problem
-		{
-			for(int j=i+1;j<nums.size();j++)
+			int group=0;
+			for(int j=0;j<nums.size();j++)
 			{
-				res+=bitCount(nums[i]^nums[j]);
+				if((nums[j]>>i)&1==1)
+					group++;
 			}
+			res+=group*(nums.size()-group);
 		}
 		return res;
-    }
-
-	int bitCount(int x)
-	{
-		x-=((x>>1)&0x55555555);
-		x=(x&0x33333333)+((x>>2)&0x33333333);
-		x=(x+(x>>4))&0x0F0F0F0F;
-		x+=x>>8;
-		x+=x>>16;
-		return x&0x3F;
 	}
+};
+
+//class Solution {
+//public:
+//    int totalHammingDistance(vector<int>& nums) {
+//		int res=0;
+//		if(nums.empty())
+//		{
+//			return 0;
+//		}
+//		for(int i=0;i<nums.size()-1;i++)//substract 1 cause great problem
+//		{
+//			for(int j=i+1;j<nums.size();j++)
+//			{
+//				res+=bitCount(nums[i]^nums[j]);
+//			}
+//		}
+//		return res;
+//    }
+//
+//	int bitCount(int x)
+//	{
+//		x-=((x>>1)&0x55555555);
+//		x=(x&0x33333333)+((x>>2)&0x33333333);
+//		x=(x+(x>>4))&0x0F0F0F0F;
+//		x+=x>>8;
+//		x+=x>>16;
+//		return x&0x3F;
+//	}
 
 //	int Hamming(int x)
 //	{
@@ -39,4 +57,4 @@ public:
 //		}
 //		return bit;
 //	}
-};
+//};
