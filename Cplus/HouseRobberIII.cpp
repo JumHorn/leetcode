9a606e546b4cc168496990f7952c6aef5fd45712
JumHorn <JumHorn@gmail.com>
2018-10-31 23:48:23 +0800

    add a effecient solution

--- Cplus/HouseRobberIII.cpp
+++ Cplus/HouseRobberIII.cpp
@@ -30,3 +30,28 @@ public:
 		return max(tmp1,tmp2);
     }
 };
+
+//more effeciency solution
+//class Solution {
+//public:
+//    int rob(TreeNode* root) {
+//        vector<int> ans;
+//        ans = subrob(root);
+//        return max(ans[0], ans[1]);
+//    }
+//    vector<int> subrob(TreeNode* root){
+//        vector<int> res;
+//        if(!root){
+//            res.push_back(0);
+//            res.push_back(0);
+//            return res;
+//        }
+//        vector<int> left = subrob(root->left);
+//        vector<int> right = subrob(root->right);
+//        int not_rubbed = max(left[0], left[1]) + max(right[0], right[1]);
+//        int rubbed = root->val + left[0] + right[0];
+//        res.push_back(not_rubbed);
+//        res.push_back(rubbed);
+//        return res;
+//    }
+//};
