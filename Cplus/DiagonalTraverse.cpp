6246a2b9365491d515d8adb09dd293d0680dd375
JumHorn <JumHorn@gmail.com>
2018-11-05 21:09:24 +0800

    Diagonal Traverse

---
+++ Cplus/DiagonalTraverse.cpp
@@ -0,0 +1,52 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& matrix) {
+		if(matrix.size()==0)
+			return vector<int>();
+		vector<int> res(matrix.size()*matrix[0].size());
+		int i=0,j=0,index=0;
+		bool dir=true;
+		while(true)
+		{
+			res[index++]=matrix[i][j];
+			if(i==matrix.size()-1&&j==matrix[0].size()-1)
+				break;
+			if(dir)//up-right
+			{
+				if(i==0||j==matrix[0].size()-1)
+				{
+					dir=false;
+					if(j==matrix[0].size()-1)
+						i++;
+					else
+						j++;
+				}
+				else
+				{
+					i--;
+					j++;
+				}
+			}	
+			else//down-left
+			{
+				if(i==matrix.size()-1||j==0)
+				{
+					dir=true;
+					if(i==matrix.size()-1)
+						j++;
+					else
+						i++;
+				}
+				else
+				{
+					i++;
+					j--;
+				}
+			}
+		}
+		return res;
+    }
+};
