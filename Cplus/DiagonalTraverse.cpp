24423e1b5bbd912e7193a1f581d13c654a109d4b
JumHorn <JumHorn@gmail.com>
2018-11-05 21:24:57 +0800

    Diagonal Traverse no improve

--- Cplus/DiagonalTraverse.cpp
+++ Cplus/DiagonalTraverse.cpp
@@ -16,38 +16,46 @@ public:
 				break;
 			if(dir)//up-right
 			{
+				while(true)
 				if(j==matrix[0].size()-1)
 				{
 					dir=false;
 					i++;
+					break;
 				}
 				else if(i==0)
 				{
 					dir=false;
 					j++;
+					break;
 				}
 				else
 				{
 					i--;
 					j++;
+					res[index++]=matrix[i][j];
 				}
 			}	
 			else//down-left
 			{
+				while(true)
 				if(i==matrix.size()-1)
 				{
 					dir=true;
 					j++;
+					break;
 				}
 				else if(j==0)
 				{
 					dir=true;
 					i++;
+					break;
 				}
 				else
 				{
 					i++;
 					j--;
+					res[index++]=matrix[i][j];
 				}
 			}
 		}
