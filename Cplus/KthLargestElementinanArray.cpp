eb4e0bb72168e48f5e90e2f2346c383e861fb1cd
JumHorn <JumHorn@gmail.com>
2018-11-23 08:12:20 +0800

    Kth Largest Element in an Array

---
+++ Cplus/KthLargestElementinanArray.cpp
@@ -0,0 +1,13 @@
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+		make_heap(nums.begin(),nums.end());
+	    for(int i=0;i<k;i++)
+			pop_heap(nums.begin(),nums.end()-i);
+		return nums[nums.size()-k];	
+    }
+};
