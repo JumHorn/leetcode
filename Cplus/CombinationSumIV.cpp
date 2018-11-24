3f6498b3184e1bbb015dba14a41ccfafd436abd2
JumHorn <JumHorn@gmail.com>
2018-11-25 00:29:07 +0800

    Combination Sum IV beats 100%

--- Cplus/CombinationSumIV.cpp
+++ Cplus/CombinationSumIV.cpp
@@ -5,20 +5,30 @@ using namespace std;
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-		sort(nums.begin(),nums.end());        
-		return combinationSum(nums,target);
+		sort(nums.begin(),nums.end());
+		vector<int> mem(target+1,-1);
+		return combinationSum(mem,nums,target);
     }
 
-	int combinationSum(vector<int>& nums,int target)
+	int combinationSum(vector<int>& mem,vector<int>& nums,int target)
 	{
 		if(target==0)
 			return 1;
-		if(target<0)
-			return 0;
 		int sum=0;
 		for(int i=0;i<nums.size();i++)
 		{
-			sum+=combinationSum(nums,target-nums[i]);
+			if(target-nums[i]<0)
+				break;
+			if(mem[target-nums[i]]==-1)
+			{
+				int tmp=combinationSum(mem,nums,target-nums[i]);
+				mem[target-nums[i]]=tmp;
+				sum+=tmp;
+			}
+			else
+			{
+				sum+=mem[target-nums[i]];
+			}
 		}
 		return sum;
 	}
