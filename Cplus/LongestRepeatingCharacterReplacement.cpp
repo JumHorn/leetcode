410a98abce91dc2a267ec130ee5d5bb32cc445bc
JumHorn <JumHorn@gmail.com>
2018-11-26 22:41:53 +0800

    Longest Repeating Character Replacement

---
+++ Cplus/LongestRepeatingCharacterReplacement.cpp
@@ -0,0 +1,26 @@
+#include<string>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        if(s.length()<=k)
+            return s.length();
+		int window=k,maxcount=0;
+		vector<int> hash(26,0);
+		for(int i=0;i<k;i++)
+			hash[s[i]-'A']++;
+		for(int j=k;j<s.length();j++)
+		{
+			hash[s[j]-'A']++;
+			maxcount=*max_element(hash.begin(),hash.end());
+			if(window-maxcount<k)
+				window++;
+			else
+				hash[s[j-window]-'A']--;
+		}
+		return window;
+    }
+};
