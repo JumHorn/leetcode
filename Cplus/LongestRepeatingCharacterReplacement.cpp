b81022e757f112f2953b1fe4f81ed9938462de7f
JumHorn <JumHorn@gmail.com>
2018-11-26 23:13:22 +0800

    most simplified code

--- Cplus/LongestRepeatingCharacterReplacement.cpp
+++ Cplus/LongestRepeatingCharacterReplacement.cpp
@@ -6,20 +6,13 @@ using namespace std;
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        if(s.length()<=k)
-            return s.length();
-		int window=k,maxcount=0;
+		int window=0,maxcount=0;
 		vector<int> hash(26,0);
-		for(int i=0;i<k;i++)
-			hash[s[i]-'A']++;
-		for(int j=k;j<s.length();j++)
+		for(int j=0;j<s.length();j++)
 		{
 			hash[s[j]-'A']++;
 			maxcount=max(hash[s[j]-'A'],maxcount);//this trick makes the algorithm the best
-			if(window-maxcount<k)
-				window++;
-			else
-				hash[s[j-window]-'A']--;
+			window-maxcount<k?window++:hash[s[j-window]-'A']--;
 		}
 		return window;
     }
