3bb3971fd217d43e1fe2f800f0646bbbcfb9f6bb
JumHorn <JumHorn@gmail.com>
2018-11-28 22:51:46 +0800

    Contiguous Array

--- Cplus/ContiguousArray.cpp
+++ Cplus/ContiguousArray.cpp
@@ -1,19 +1,20 @@
+#include<map>
 #include<vector>
 using namespace std;
 
 class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
-		int res=0;
+		int res=0,total=0;
+		map<int,int> hash;
+        hash[0]=-1;
 		for(int i=0;i<nums.size();i++)
 		{
-			int one=0,zero=0;
-			for(int j=i;j<nums.size();j++)
-			{
-				nums[j]?one++:zero++;
-				if(one==zero&&j-i+1>res)
-					res=j-i+1;
-			}
+			nums[i]?total++:total--;
+			if(hash.count(total)!=0)
+				res=max(res,i-hash[total]);
+			else
+				hash[total]=i;
 		}
 		return res;
     }
