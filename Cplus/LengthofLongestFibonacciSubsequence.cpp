c21ff39d62274e2c9887853aa9f7709e4d3b54d4
JumHorn <JumHorn@gmail.com>
2018-12-06 00:09:14 +0800

    list only for insert and delete

--- Cplus/LengthofLongestFibonacciSubsequence.cpp
+++ Cplus/LengthofLongestFibonacciSubsequence.cpp
@@ -1,4 +1,5 @@
 #include<vector>
+#include<list>
 using namespace std;
 /*
 [1,2,3,4,5,6,7,8]
@@ -15,33 +16,32 @@ class Solution {
 public:
     int lenLongestFibSubseq(vector<int>& A) {
         int res=2;
-        vector<vector<int> > v;
-	 	vector<int> tmp(4);
+        list<vector<int> > v;
+	 	vector<int> tmp(3);
 		tmp[0]=A[0];
 		tmp[1]=A[1];
-		tmp[2]=tmp[0]+tmp[1];
-        tmp[3]=2;
+        tmp[2]=2;
 		v.push_back(tmp);
+		list<vector<int> >::iterator iter=v.begin();
+		iter++;
 		for(int i=2;i<A.size();i++)
 		{
-			for(int j=0;j<v.size();j++)
+			for(iter++;iter!=v.end();iter++)
 			{
-				if(A[i]==v[j][2])
+				if(A[i]==(*iter)[0]+(*iter)[1])
 				{
-					v[j][0]=v[j][1];
-					v[j][1]=v[j][2];
-					v[j][2]=v[j][1]+v[j][0];
-					v[j][3]++;
-                    if(v[j][3]>res)
-                        res=v[j][3];
+					(*iter)[0]=(*iter)[1];
+					(*iter)[1]=A[i];
+					(*iter)[2]++;
+                    if((*iter)[2]>res)
+                        res=(*iter)[2];
 				}
 			}
 			for(int j=0;j<i;j++)
 			{
 				tmp[0]=A[j];
 				tmp[1]=A[i];
-				tmp[2]=tmp[0]+tmp[1];
-				tmp[3]=2;
+				tmp[2]=2;
 				v.push_back(tmp);
 			}
 		}
