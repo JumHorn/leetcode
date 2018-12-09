e69f8fc3fbd31b661f14d19cf3f27bfdf2d15f89
JumHorn <JumHorn@gmail.com>
2018-12-09 22:33:10 +0800

    time limited exceeded

--- Cplus/ContainerWithMostWater.cpp
+++ Cplus/ContainerWithMostWater.cpp
@@ -9,12 +9,15 @@ dp[i][j]=max{ dp[i-1][j],dp[i][j-1],(j-i)*min{height[i],height[j]} }
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        vector<vector<int> > dp(height.size(),vector<int>(height.size()));
+        vector<vector<int> > dp(2,vector<int>(height.size()));
 		for(int j=1;j<height.size();j++)
 			dp[0][j]=max(dp[0][j-1],j*min(height[0],height[j]));
 		for(int i=1;i<height.size();i++)
 			for(int j=i+1;j<height.size();j++)
-				dp[i][j]=max(max(dp[i-1][j],dp[i][j-1]),(j-i)*min(height[i],height[j]));
-		return dp[height.size()-2].back();
+            {
+				dp[1][j]=max(max(dp[0][j],dp[1][j-1]),(j-i)*min(height[i],height[j]));
+                dp[0][j]=max(dp[0][j],dp[1][j]);
+            }
+		return dp[0].back();
     }
 };
