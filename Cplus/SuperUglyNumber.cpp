bac6f4b5a53b5b99d759b3ef77d2679e686f4964
JumHorn <JumHorn@gmail.com>
2018-12-16 21:47:13 +0800

    time limited exceeded

--- Cplus/SuperUglyNumber.cpp
+++ Cplus/SuperUglyNumber.cpp
@@ -1,21 +1,34 @@
 #include<vector>
+#include<unordered_set>
 using namespace std;
 
 class Solution {
 public:
     int nthSuperUglyNumber(int n, vector<int>& primes) {
+		unordered_set<int> visited(primes.begin(),primes.end());
 		int ugly=1;
 		for(int i=1;i<n;i++)
 		{
 			ugly++;
 			int j,tmp=ugly;
+            if(visited.find(tmp)!=visited.end())
+            {
+                visited.insert(ugly);
+                continue;
+            }
 			for(j=0;j<primes.size();j++)
 			{
 				if(tmp%primes[j]==0)
 				{
 					tmp=tmp/primes[j];
+					if(visited.find(tmp)!=visited.end())
+					{
+						visited.insert(ugly);
+						break;
+					}
                     if(tmp==1)
                     {
+						visited.insert(ugly);
                         break;
                     }
 					j=-1;
