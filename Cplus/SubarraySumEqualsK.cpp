6a4c9284d53e0517d21e767517263630add62b33
JumHorn <JumHorn@gmail.com>
2018-12-19 21:52:39 +0800

    add a better approach

--- Cplus/SubarraySumEqualsK.cpp
+++ Cplus/SubarraySumEqualsK.cpp
@@ -36,28 +36,17 @@ public:
 };
 
 
-
-//slide window failed
-//class Solution {
-//public:
-//    int subarraySum(vector<int>& nums, int k) {
-//		int start=0,end=0,sum=0,res=0;
-// 		while(start<nums.size())
-//		{
-//			sum+=nums[start];
-//			if(sum<k)
-//				start++;
-//			else if(sum>k)
-//			{
-//				sum-=nums[end++];
-//				sum-=nums[start];
-//			}
-//			else
-//			{
-//				start++;
-//				res++;
-//			}
-//		}		
-//		return res;
+//public class Solution {
+//    public int subarraySum(int[] nums, int k) {
+//        int count = 0, sum = 0;
+//        HashMap < Integer, Integer > map = new HashMap < > ();
+//        map.put(0, 1);
+//        for (int i = 0; i < nums.length; i++) {
+//            sum += nums[i];
+//            if (map.containsKey(sum - k))
+//                count += map.get(sum - k);
+//            map.put(sum, map.getOrDefault(sum, 0) + 1);
+//        }
+//        return count;
 //    }
-//};
+//}
