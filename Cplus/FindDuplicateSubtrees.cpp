7f8892fe9c0ab401d559a68aa36dc451c424916b
JumHorn <JumHorn@gmail.com>
2018-12-20 23:09:15 +0800

    Find Duplicate Subtrees

--- Cplus/FindDuplicateSubtrees.cpp
+++ Cplus/FindDuplicateSubtrees.cpp
@@ -12,47 +12,16 @@ struct TreeNode {
 class Solution {
 public:
     vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
-        vector<TreeNode*> res;
-		list<TreeNode*> nodes;
-		getAllNodes(nodes,root);
-		for(list<TreeNode*>::iterator iter=nodes.begin();iter!=nodes.end();iter++)
-		{
-			list<TreeNode*>::iterator it=iter,tmp=nodes.end();
-			for(it++;it!=nodes.end();)
-            {
-				if(isSame(*it,*iter))
-				{
-				    tmp=iter;
-					it=nodes.erase(it);
-				}
-                else
-                    it++;
-			}
-            if(tmp!=nodes.end())
-                res.push_back(*tmp);
-		}
-		return res;
+        unordered_map<string, int>m;
+        vector<TreeNode*>res;
+        DFS(root, m, res);
+        return res;
     }
 
-	bool isSame(TreeNode* n1,TreeNode* n2)
-	{
-		if(n1==NULL&&n2==NULL)
-			return true;
-		if(n1!=NULL&&n2!=NULL)
-		{
-			if(n1->val==n2->val)
-				return isSame(n1->left,n2->left)&&isSame(n1->right,n2->right);
-		}
-		return false;
-	}
-
-	//dfs get all nodes
-	void getAllNodes(list<TreeNode*>& nodes,TreeNode* root)
-	{
-		if(root==NULL)
-			return;
-		nodes.push_back(root);
-		getAllNodes(nodes,root->left);
-		getAllNodes(nodes,root->right);
-	}
+    string DFS(TreeNode* root, unordered_map<string, int>& m, vector<TreeNode*>& res){
+        if(!root) return "";
+        string s = to_string(root->val) +','+ DFS(root->left, m, res) +','+ DFS(root->right, m, res);
+        if(++m[s] == 2) res.push_back(root);
+        return s;
+    }
 };
