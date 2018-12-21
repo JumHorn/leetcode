3945e46151c50cbf6787c26a91ba91ee6726904e
JumHorn <JumHorn@gmail.com>
2018-12-21 21:22:17 +0800

    not finished yet

--- Cplus/KnightProbabilityinChessboard.cpp
+++ Cplus/KnightProbabilityinChessboard.cpp
@@ -1,4 +1,6 @@
 #include<cmath>
+#include<vector>
+#include<map>
 using namespace std;
 
 class Solution {
@@ -7,23 +9,32 @@ class Solution {
 public:
 	Solution():dr({2, 2, 1, 1, -1, -1, -2, -2}),dc({1, -1, 2, -2, 2, -2, 1, -1}){}
     double knightProbability(int N, int K, int r, int c) {
-        int inner=0;
-		probability(N,K,r,c,inner);
+        map<vector<int>,int> m;
+		int inner=probability(N,K,r,c,m);
 		return 1.0*inner/pow(8,K);
     }
 
-	void probability(int N,int K,int r,int c,int& inner)
+	int probability(int N,int K,int r,int c,map<vector<int>,int>& m)
 	{
 		if(r<0||r>=N||c<0||c>=N)
-			return;
+			return 0;
         if(K==0)
-        {
-            inner++;
-            return;
-        }
+            return 1;
+		int sum=0;
 		for(int i=0;i<8;i++)
 		{
-			probability(N,K-1,r+dr[i],c+dc[i],inner);
+			vector<int> tmp(3);
+			tmp[0]=r+dr[i];
+			tmp[1]=c+dc[i];
+			tmp[2]=K;
+			if(m.find(tmp)==m.end())
+			{
+				int res=probability(N,K-1,r+dr[i],c+dc[i],m);
+				m[tmp]=res;
+			}
+			else
+				sum+=m[tmp];
 		}
+		return sum;
 	}
 };
