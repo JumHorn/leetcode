0e74db15b0a931923689729f2f3263ff5a926e12
JumHorn <JumHorn@gmail.com>
2018-12-26 22:34:37 +0800

    time limited exceeded

---
+++ Cplus/NumberofSubarrayswithBoundedMaximum.cpp
@@ -0,0 +1,48 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int numSubarrayBoundedMax(vector<int>& A, int L, int R) {
+		int res=0;
+		int index=0;
+		for(int j=0;j<A.size();j++)
+		{
+			if(A[j]>A[index])
+				index=j;
+            int tmp=isValid(L,R,A[index]);
+			res+=tmp;
+            int partmax=index;
+			for(int i=1;i<=j;i++)
+			{
+				if(i-1==partmax)
+				{
+					partmax=maxIndex(A,i,j);
+                    tmp=isValid(L,R,A[partmax]);
+                }
+				res+=tmp;
+			}
+		}
+		return res;
+    }
+
+	int isValid(int L,int R,int val)
+	{
+		if(val<=R&&val>=L)
+			return 1;
+		else
+			return 0;
+	}
+
+	int maxIndex(vector<int>& A,int i,int j)
+	{
+		int res=i++;
+		while(i<=j)
+        {
+			if(A[i]>A[res])
+				res=i;
+            i++;
+        }
+		return res;
+	}
+};
