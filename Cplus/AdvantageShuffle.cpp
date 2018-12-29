a3a5b12c49b00a46dc3092376856355ce8a4051a
JumHorn <JumHorn@gmail.com>
2018-12-29 23:49:09 +0800

    Advantage Shuffle

--- Cplus/AdvantageShuffle.cpp
+++ Cplus/AdvantageShuffle.cpp
@@ -6,9 +6,10 @@ class Solution {
 public:
     vector<int> advantageCount(vector<int>& A, vector<int>& B) {
 		sort(A.begin(),A.end());
-		for(vector<int>::size_type i=0;i<B.size();i++)
+
+		for(int i=0;i<B.size();i++)
 		{
-            vector<int>::size_type j=0;
+            int j=0;
 			for(j=0;j<A.size();j++)
 				if(A[j]>B[i])
 				{
@@ -19,13 +20,13 @@ public:
             if(j==A.size())
                 B[i]=INT_MIN;
 		}
-		for(vector<int>::size_type i=0,j=0;i<B.size();i++)
+		for(int i=0,j=0;i<B.size();i++)
 		{
 			if(B[i]==INT_MIN)
 			{
 				while(A[j]==INT_MIN)
 					j++;
-				B[i]=A[j];
+				B[i]=A[j++];
 			}
 		}
 		return B;
