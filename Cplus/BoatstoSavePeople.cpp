a40c6acd795288e22ef27e123a18c918aa22b9ac
JumHorn <JumHorn@gmail.com>
2018-12-30 18:48:05 +0800

    beautifier code

--- Cplus/BoatstoSavePeople.cpp
+++ Cplus/BoatstoSavePeople.cpp
@@ -9,13 +9,9 @@ public:
 		int i=0,j=people.size()-1,res=0;
 		while(i<=j)
 		{
-			if(people[i]+people[j]>limit)
-				j--;
-			else
-			{
+			if(people[i]+people[j]<=limit)
 				i++;
-				j--;
-			}
+			j--;
 			res++;
 		}
 		return res;
