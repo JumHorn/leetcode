8a79a23322c95d6296b149b3597988e672999e0d
JumHorn <JumHorn@gmail.com>
2019-01-03 21:26:52 +0800

    much better

--- Cplus/PermutationsII.cpp
+++ Cplus/PermutationsII.cpp
@@ -6,20 +6,26 @@ class Solution {
 public:
     vector<vector<int> > permuteUnique(vector<int>& nums) {
 		vector<vector<int> > res;
+		sort(nums.begin(),nums.end());
 		permute(res,nums,0);
 		return res;
     }
 
-	void permute(vector<vector<int> >& res,vector<int>& nums,int start)
+	void permute(vector<vector<int> >& res,vector<int> nums,int start)
 	{
 		if(start==nums.size())
-			if(find(res.begin(),res.end(),nums)==res.end())
-				res.push_back(nums);
+        {
+			res.push_back(nums);
+            return;
+        }
 		for(int i=start;i<nums.size();i++)
 		{
+			if(i>start&&nums[i]==nums[start])
+				continue;
 			swap(nums[i],nums[start]);
 			permute(res,nums,start+1);
-			swap(nums[i],nums[start]);
 		}
 	}
 };
+
+
