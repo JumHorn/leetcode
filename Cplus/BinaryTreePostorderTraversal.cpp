cfb42bfd239bff646a7eaa75f127f9044c130228
JumHorn <JumHorn@gmail.com>
2019-01-28 23:36:42 +0800

    Binary Tree Postorder Traversal beats 100%

---
+++ Cplus/BinaryTreePostorderTraversal.cpp
@@ -0,0 +1,41 @@
+#include<vector>
+#include<stack>
+using namespace std;
+
+//Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;   
+		stack<TreeNode*> s;
+		if(root==NULL)
+			return res;
+		s.push(root);
+		TreeNode* pre,*curr=root;
+		while(!s.empty())
+		{
+			pre=curr;
+			curr=s.top();
+			if((curr->left==NULL&&curr->right==NULL)||(curr->left==pre||curr->right==pre))
+			{
+				res.push_back(curr->val);
+				s.pop();
+			}
+			else
+			{
+				if(curr->right!=NULL)
+					s.push(curr->right);
+				if(curr->left!=NULL)
+					s.push(curr->left);
+			}
+		}
+		return res;
+    }
+};
