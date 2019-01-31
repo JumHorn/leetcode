83d4b730cf85f68a2b8eb33fe62ceb18a080e62a
JumHorn <JumHorn@gmail.com>
2019-01-31 22:55:49 +0800

    time limited exceeded

--- Cplus/PerfectSquares.cpp
+++ Cplus/PerfectSquares.cpp
@@ -1,20 +1,37 @@
 #include<algorithm>
+#include<unordered_map>
 #include<iostream>
 using namespace std;
 
 class Solution {
 public:
     int numSquares(int n) {
+		unordered_map<int,int> calculated;
+		return numSquares(calculated,n);
+    }
+
+    int numSquares(unordered_map<int,int>& calculated,int n) {
+		if(calculated.find(n)!=calculated.end())
+			return calculated[n];
 		int i=1;
 		while(i*i<n)
 			i++;
 		if(i*i==n)
+		{
+			calculated[n]=1;
 			return 1;
+		}
 		int m=INT_MAX;
 		for(int j=i-1;j>=1;j--)
 		{
 			m=min(m,1+numSquares(n-j*j));
+            if(m==2)
+            {
+                calculated[n]=2;
+                return 2;
+            }
 		}
+		calculated[n]=m;
 		return m;
     }
 };
