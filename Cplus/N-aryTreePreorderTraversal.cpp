f5648301cd734f70c4bd1cafcbf7989a1ff4361f
JumHorn <JumHorn@gmail.com>
2019-02-02 22:39:55 +0800

    N-ary Tree Preorder Traversal

---
+++ Cplus/N-aryTreePreorderTraversal.cpp
@@ -0,0 +1,33 @@
+#include<vector>
+using namespace std;
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        vector<int> res;
+		preorder(res,root);
+		return res;
+    }
+
+	void preorder(vector<int>& res,Node* n)
+	{
+		if(n==NULL)
+			return;
+		res.push_back(n->val);
+		for(int i=0;i<(int)n->children.size();i++)
+			preorder(res,n->children[i]);
+	}
+};
