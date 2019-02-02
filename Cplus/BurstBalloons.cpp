a1d9c04b568cb00e7475aaf366f80e63e1b1550e
JumHorn <JumHorn@gmail.com>
2019-02-02 21:20:48 +0800

    time limited exceeded

--- Cplus/BurstBalloons.cpp
+++ Cplus/BurstBalloons.cpp
@@ -11,22 +11,28 @@ public:
 			return nums[0];
 		if(nums.size()==2)
 			return nums[0]*nums[1]+max(nums[0],nums[1]);		
-		int res=0;
-		vector<int>::iterator iter=min_element(nums.begin(),nums.end());
-		if(iter==nums.begin())
+		int res=0,result=0;
+		for(vector<int>::iterator iter=nums.begin();iter!=nums.end();++iter)
 		{
-			res+=*(iter+1)**iter;
+			res=0;
+			if(iter==nums.begin())
+			{
+				res+=*(iter+1)**iter;
+			}
+			else if(iter==nums.end()-1)
+			{
+				res+=*(iter-1)**iter;
+			}
+			else
+			{
+				res+=*(iter-1)*(*iter)**(iter+1);
+			}
+			vector<int> tmp=nums;
+			tmp.erase(iter-nums.begin()+tmp.begin());
+			res+=maxCoins(tmp);
+			if(res>result)
+				result=res;
 		}
-		else if(iter==nums.end()-1)
-		{
-			res+=*(iter-1)**iter;
-		}
-		else
-		{
-			res+=*(iter-1)*(*iter)**(iter+1);
-		}
-		nums.erase(iter);
-		res+=maxCoins(nums);
-		return res;
+		return result;
     }
 };
