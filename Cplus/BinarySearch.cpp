978e5aebd1bdb10254fd4e1a5695edc163108157
JumHorn <JumHorn@gmail.com>
2019-02-05 22:05:21 +0800

    clean code

--- Cplus/BinarySearch.cpp
+++ Cplus/BinarySearch.cpp
@@ -10,13 +10,14 @@ public:
     }
 
     int search(vector<int>& nums, int target,int left,int right) {
-		int mid=(left+right)/2;
+		//int mid=(left+right)/2;
+		int mid = left + (right-left)/2;//avoid overflow
+		if(left>right)
+			return -1;
 		if(nums[mid]==target)
 			return mid;
-		if(right<=left)
-			return -1;
 		if(nums[mid]<target)
 			return search(nums,target,mid+1,right);
-		return search(nums,target,left,mid);
+		return search(nums,target,left,mid-1);
     }
 };
