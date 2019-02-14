4b90e183d30238744bdcf1825e7991770d4b7c00
JumHorn <JumHorn@gmail.com>
2019-02-15 00:08:08 +0800

    no improvement

--- Cplus/LongestIncreasingSubsequence.cpp
+++ Cplus/LongestIncreasingSubsequence.cpp
@@ -9,12 +9,15 @@ public:
 			return 0;
 		vector<int> dp(nums.size(),1);
 		dp[0]=1;
+		int res=1;
 		for(int i=1;i<(int)nums.size();i++)
 		{
 			for(int j=0;j<i;j++)
 				if(nums[i]>nums[j])
 					dp[i]=max(dp[i],dp[j]+1);
+			if(dp[i]>res)
+				res=dp[i];
 		}		
-		return *max_element(dp.begin(),dp.end());
+		return res;
     }
 };
