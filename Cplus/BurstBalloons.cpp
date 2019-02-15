c4d67fa0325c849a88937105cd4458c3a83b8cd9
JumHorn <JumHorn@gmail.com>
2019-02-15 22:10:50 +0800

    add a simple dp solution

--- Cplus/BurstBalloons.cpp
+++ Cplus/BurstBalloons.cpp
@@ -5,34 +5,19 @@ using namespace std;
 class Solution {
 public:
     int maxCoins(vector<int>& nums) {
-		if(nums.empty())
-			return 0;
-		if(nums.size()==1)
-			return nums[0];
-		if(nums.size()==2)
-			return nums[0]*nums[1]+max(nums[0],nums[1]);		
-		int res=0,result=0;
-		for(vector<int>::iterator iter=nums.begin();iter!=nums.end();++iter)
-		{
-			res=0;
-			if(iter==nums.begin())
+		int len=nums.size();
+		nums.insert(nums.begin(),1);
+		nums.push_back(1);
+		vector<vector<int> > dp(nums.size(),vector<int>(nums.size()));
+		for(int n=1;n<=len;n++)
+			for(int i=1;i<=len-n+1;i++)
 			{
-				res+=*(iter+1)**iter;
+				int j=i+n-1;
+				for(int k=i;k<=j;k++)
+				{
+					dp[i][j]=max(dp[i][j],nums[i-1]*nums[k]*nums[j+1]+dp[i][k-1]+dp[k+1][j]);
+				}
 			}
-			else if(iter==nums.end()-1)
-			{
-				res+=*(iter-1)**iter;
-			}
-			else
-			{
-				res+=*(iter-1)*(*iter)**(iter+1);
-			}
-			vector<int> tmp=nums;
-			tmp.erase(iter-nums.begin()+tmp.begin());
-			res+=maxCoins(tmp);
-			if(res>result)
-				result=res;
-		}
-		return result;
+		return dp[1][len];
     }
 };
