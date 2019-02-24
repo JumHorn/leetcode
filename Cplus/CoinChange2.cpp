b18e443f75a7c99ed0b8504e0b4a7a84865b3f1e
JumHorn <JumHorn@gmail.com>
2019-02-24 13:45:30 +0800

    Coin Change 2

--- Cplus/CoinChange2.cpp
+++ Cplus/CoinChange2.cpp
@@ -1,23 +1,24 @@
 #include<vector>
 #include<algorithm>
+#include<unordered_map>
 using namespace std;
 
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-		sort(coins.begin(),coins.end());
-		return change(coins,0,amount);
+        if(amount==0)
+            return 1;
+		vector<vector<int> > dp(coins.size()+1,vector<int>(amount+1));
+		for(int j=1;j<=amount;j++)
+			for(int i=1;i<=(int)coins.size();i++)
+			{
+				if(j==coins[i-1])
+					dp[i][j]=1+dp[i-1][j];
+				else if(j<coins[i-1])
+					dp[i][j]=dp[i-1][j];
+				else
+					dp[i][j]=dp[i][j-coins[i-1]]+dp[i-1][j];
+			}
+		return dp.back().back();
     }
-
-	int change(vector<int>& coins,int start,int amount)
-	{
-		if(amount==0)
-			return 1;
-		if(amount<0)
-			return 0;
-		int sum=0;
-		for(int i=start;i<(int)coins.size();i++)
-			sum+=change(coins,i,amount-coins[i]);		
-		return sum;
-	}
 };
