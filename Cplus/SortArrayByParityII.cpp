391142d26bb90f9805400c5722340d807e71de5d
JumHorn <JumHorn@gmail.com>
2019-03-20 23:13:17 +0800

    Sort Array By Parity II

---
+++ Cplus/SortArrayByParityII.cpp
@@ -0,0 +1,22 @@
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& A) {
+		for(int odd=1,even=0;true;)
+		{
+			while(odd<(int)A.size()&&A[odd]%2==1)
+				odd+=2;
+			while(even<(int)A.size()&&A[even]%2==0)
+				even+=2;
+			if(odd>=(int)A.size()||even>=(int)A.size())
+				break;
+			swap(A[odd],A[even]);
+			odd+=2;
+			even+=2;
+		}
+		return A;		
+    }
+};
