a3e30fc630c14364c292f5ece40bf017a035c539
JumHorn <JumHorn@gmail.com>
2019-04-02 00:20:44 +0800

    time limited exceeded

--- Cplus/PartitionArrayintoDisjointIntervals.cpp
+++ Cplus/PartitionArrayintoDisjointIntervals.cpp
@@ -4,17 +4,36 @@ using namespace std;
 
 class Solution {
 public:
-    int partitionDisjoint(vector<int>& A) {
-		vector<int> B=A;        
-		sort(B.begin(),B.end());
-		unsigned long long sumA=0,sumB=0;
-		for(int i=0;i<(int)A.size();i++)
+	int partitionDisjoint(vector<int>& A) {
+		int i=0;
+		int right=getIndex(A,i);
+		int rightvalue=A[i];
+		if(right<=i)
 		{
-			sumA+=A[i];
-			sumB+=B[i];
-			if(sumA==sumB)
-				return i+1;
+			return i+1;
+		}
+		for(int j=i+1;j<=right;j++)
+		{
+			int tmp=getIndex(A,j);
+			if(tmp>right)
+			{
+				right=tmp;
+				rightvalue=A[j];
+			}
+			else if(A[j]==rightvalue)
+				right++;
+			if(right==(int)A.size())
+				return right+1;
 		}
 		return 0;
-    }
+	}
+
+	int getIndex(vector<int>& arr,int start)
+	{
+		int res=0;
+		for(int i=0;i<(int)arr.size();i++)
+			if(arr[start]>arr[i])
+				res++;
+		return res;
+	}
 };
