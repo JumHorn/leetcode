b3144b2fcdb94d082deb0b089e2e59afd30401ff
JumHorn <JumHorn@gmail.com>
2019-04-06 09:42:17 +0800

    Rectangle Area II beats 100%

--- Cplus/RectangleAreaII.cpp
+++ Cplus/RectangleAreaII.cpp
@@ -7,22 +7,36 @@ class Solution {
 public:
     int rectangleArea(vector<vector<int> >& rectangles) {
 		sort(rectangles.begin(),rectangles.end(),*this);
-		int h=0,y=-1,x=0;
+		unsigned long long h=0,y=0,x=0,ylen=0,xlen=0;
 		unsigned long long res=0;
 		for(int i=0;i<(int)rectangles.size();i++)
-			h=max(h,rectangles[i][3]);
-		while(++y<h)
+			h=max((int)h,rectangles[i][3]);
+		while(y<h)
 		{
 			x=0;
+			xlen=0;
+			ylen=INT_MAX;
+			bool flag=false;
 			for(int i=0;i<(int)rectangles.size();i++)
 			{
-				if(y>=rectangles[i][1]&&y<rectangles[i][3]&&rectangles[i][2]>x)
+				if(rectangles[i][1]>(int)y)
+					ylen=min((int)ylen,rectangles[i][1]);
+				if((int)y>=rectangles[i][1]&&(int)y<rectangles[i][3]&&rectangles[i][2]>(int)x)
 				{
-					x=max(x,rectangles[i][0]);
-					res+=rectangles[i][2]-x;
+					flag=true;
+					x=max((int)x,rectangles[i][0]);
+					ylen=min((int)ylen,rectangles[i][3]);
+					xlen+=rectangles[i][2]-x;
 					x=rectangles[i][2];
 				}
 			}
+			if(flag)
+			{
+				res+=xlen*(ylen-y);
+				y=ylen;
+			}
+			else
+				y=ylen;
 		}
 		return res%mode;
     }
