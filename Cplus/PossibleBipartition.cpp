2dfae2b9b9be448d7865c5c159cbfaa4a7e7ef26
JumHorn <JumHorn@gmail.com>
2019-04-09 23:06:06 +0800

    wrong answer because of create graph

--- Cplus/PossibleBipartition.cpp
+++ Cplus/PossibleBipartition.cpp
@@ -4,51 +4,24 @@ using namespace std;
 class Solution {
 public:
     bool possibleBipartition(int N, vector<vector<int> >& dislikes) {
-        if(N==1)
-            return true;
-    	vector<int> group1(N+1);
-    	vector<int> group2(N+1);
-		for(int i=1;i<N+1;i++)
-		{
-			group1[i]=i;
-			group2[i]=i;
-		}
-		int p1=dislikes[0][0],p2=dislikes[0][1];
-		for(int i=1;i<(int)dislikes.size();i++)
-		{
-			if(p1==dislikes[i][0]||Union(group1,p1,dislikes[i][0]))
-			{
-				if(dislikes[i][1]!=Find(group1,dislikes[i][1]))
-					return false;
-				if(!Union(group2,p2,dislikes[i][1]))
-					return false;
-			}
-			else if(p2==dislikes[i][0]||Union(group2,p2,dislikes[i][0]))
-			{
-				if(dislikes[i][1]!=Find(group2,dislikes[i][1]))
-					return false;
-				if(!Union(group1,p1,dislikes[i][1]))
-					return false;
-			}
-			else
+		vector<vector<int> > graph(N+1,vector<int>());
+		for(int i=0;i<(int)dislikes.size();i++)
+			graph[dislikes[i][0]].push_back(dislikes[i][1]);
+		vector<int> color(N+1);
+		for(int i=1;i<(int)graph.size();i++)
+			if(color[i]==0&&!possibleBipartion(graph,color,i,1))
 				return false;
-		}
 		return true;
     }
 
-	int Find(vector<int>& parent,int x)
+	bool possibleBipartion(vector<vector<int> >& graph,vector<int>& color,int n,int c)
 	{
-		if(parent[x]!=x)
-			parent[x]=Find(parent,parent[x]);
-		return parent[x];
-	}
-
-	bool Union(vector<int>& parent,int x,int y)
-	{
-		int xr=Find(parent,x),yr=Find(parent,y);
-		if(xr==yr)
-			return false;
-		parent[yr]=xr;
+		if(color[n]!=0)
+			return color[n]!=c;
+		color[n]=c;
+		for(int i=0;i<(int)graph[n].size();i++)
+			if(!possibleBipartion(graph,color,graph[n][i],-c))
+				return false;
 		return true;
 	}
 };
