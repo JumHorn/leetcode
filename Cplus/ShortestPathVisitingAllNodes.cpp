b4a093b3f1f7a639b50ee06784f6d36a98c3b68c
JumHorn <JumHorn@gmail.com>
2019-04-13 00:24:50 +0800

    time limited exceeded

--- Cplus/ShortestPathVisitingAllNodes.cpp
+++ Cplus/ShortestPathVisitingAllNodes.cpp
@@ -6,6 +6,8 @@ using namespace std;
 class Solution {
 public:
     int shortestPathLength(vector<vector<int> >& graph) {
+        if(graph.empty()||graph[0].empty())
+            return 0;
 		queue<pair<int,unordered_set<int> > > *q=new queue<pair<int,unordered_set<int> > >();
 		queue<pair<int,unordered_set<int> > > *p=new queue<pair<int,unordered_set<int> > >();
         for(int i=0;i<(int)graph.size();i++)
@@ -15,27 +17,35 @@ public:
 				s.insert(i);
 				q->push(pair<int,unordered_set<int> >(i,s));
 			}
+		if(q->empty())
+		{
+			unordered_set<int> s;
+			s.insert(0);
+			q->push(pair<int,unordered_set<int> >(0,s));
+		}
 		int res=0;
 		while(true)
 		{
-			res++;
 			while(!q->empty())
 			{
 				if(q->front().second.size()==graph.size())
 					return res;
-				for(int i=0;i<(int)graph[q->front().first].size();i++)
+				for(int i=graph[q->front().first].size()-1;i>=0;i--)
 				{
 					pair<int,unordered_set<int> > tmp=q->front();
 					tmp.second.insert(graph[q->front().first][i]);
-					tmp.first=i;
+					tmp.first=graph[q->front().first][i];
 					p->push(tmp);
 				}
 				q->pop();
 			}
+            res++;
 			queue<pair<int,unordered_set<int> > > *tmp=q;
 			q=p;
 			p=tmp;
 		}
+		delete p;
+		delete q;
 		return res;
 	}
 };
