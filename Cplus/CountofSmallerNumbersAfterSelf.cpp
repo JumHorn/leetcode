be6433938af0a3e006f7a82764949176d6d49978
JumHorn <JumHorn@gmail.com>
2019-04-20 10:32:34 +0800

    merge sort time limited exceeded

--- Cplus/CountofSmallerNumbersAfterSelf.cpp
+++ Cplus/CountofSmallerNumbersAfterSelf.cpp
@@ -5,19 +5,48 @@ class Solution {
 public:
     vector<int> countSmaller(vector<int>& nums) {
 		vector<int> res(nums.size());
-		for(int i=(int)nums.size()-2;i>=0;i--)
+		vector<pair<int,int> > pairs(nums.size());
+		for(int i=0;i<(int)nums.size();i++)
 		{
-			for(int j=i+1;j<(int)nums.size();j++)
-				if(nums[i]>nums[j])
-				{
-					if(res[j]==(int)nums.size()-j-1)
-					{
-						res[i]+=res[j]+1;
-						break;
-					}
-					++res[i];
-				}
+			pairs[i].first=nums[i];
+			pairs[i].second=i;
 		}
+		mergeSort(res,pairs,0,nums.size());
 		return res;		
     }
+
+	void mergeSort(vector<int>& res,vector<pair<int,int> >& pairs,int start,int end)
+	{
+		if(end-start<=1)
+			return;
+		int mid=(end-start)/2+start;
+		mergeSort(res,pairs,start,mid);
+		mergeSort(res,pairs,mid,end);
+		merge(res,pairs,start,mid,end);
+	}
+
+	void merge(vector<int>& res,vector<pair<int,int> >& pairs,int start,int mid,int end)
+	{
+		vector<pair<int,int> > tmp(res.size());
+		int s=start,m=mid,d=0,d1=0;
+		while(s!=mid&&m!=end)
+		{
+			if(pairs[s].first>pairs[m].first)
+			{
+				for(int i=s;i!=mid;i++)
+					res[pairs[i].second]++;
+				tmp[d++]=pairs[m++];
+			}
+			else
+			{
+				tmp[d++]=pairs[s++];
+			}
+		}
+		while(s!=mid)
+			tmp[d++]=pairs[s++];
+		while(m!=end)
+			tmp[d++]=pairs[m++];
+		while(start!=end)
+			pairs[start++]=tmp[d1++];
+	}
 };
