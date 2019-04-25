b801281e818599440cd85223b028689ac4cc165d
JumHorn <JumHorn@gmail.com>
2019-04-25 22:14:30 +0800

    3d vector much slower than C array

--- Cplus/RemoveBoxes.cpp
+++ Cplus/RemoveBoxes.cpp
@@ -6,16 +6,21 @@ class Solution {
 public:
 	int removeBoxes(vector<int>& boxes)
 	{
-		vector<vector<vector<int> > > visited(100,vector<vector<int> >(100,vector<int>(100)));
+		int visited[100][100][100]={0};
 		return removeBoxes(boxes,visited,0,boxes.size()-1,0);
 	}
 
-    int removeBoxes(vector<int>& boxes,vector<vector<vector<int> > >& visited,int l,int r,int k)
+    int removeBoxes(vector<int>& boxes,int (*visited)[100][100],int l,int r,int k)
 	{
 		if(l>r)
 			return 0;
 		if(visited[l][r][k]!=0)
 			return visited[l][r][k];
+        while(l<r&&boxes[l]==boxes[l+1])
+        {
+            l++;
+            k++;
+        }
 		int res=(k+1)*(k+1)+removeBoxes(boxes,visited,l+1,r,0);
 		for(int j=l+1;j<=r;j++)
 		{
