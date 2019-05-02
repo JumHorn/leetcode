b24e932c9cf915b2bb8c319645793a705d32585c
JumHorn <JumHorn@gmail.com>
2019-05-02 18:13:58 +0800

    Wiggle Subsequence beats 100%

--- Cplus/WiggleSubsequence.cpp
+++ Cplus/WiggleSubsequence.cpp
@@ -7,21 +7,27 @@ public:
     int wiggleMaxLength(vector<int>& nums) {
         if(nums.empty())
             return 0;
-		vector<vector<int> > dp(nums.size(),vector<int>(2,1));
+		int (*dp)[2]=new int[nums.size()][2];
+        dp[0][0]=dp[0][1]=1;
 		for(int i=1;i<(int)nums.size();i++)
-			for(int j=0;j<i;j++)
-			{
-				if(nums[i]-nums[j]==0)
-					continue;
-				if(nums[i]-nums[j]>0)
-				{
-					dp[i][0]=max(dp[i][0],dp[j][1]+1);
-				}
-				else
-				{
-					dp[i][1]=max(dp[i][1],dp[j][0]+1);
-				}
-			}
-		return max(dp.back()[0],dp.back()[1]);
+        {
+            if(nums[i]-nums[i-1]==0)
+            {
+                dp[i][0]=dp[i-1][0];
+                dp[i][1]=dp[i-1][1];
+                continue;
+            }
+            if(nums[i]-nums[i-1]>0)
+            {
+                dp[i][0]=max(dp[i-1][0],dp[i-1][1]+1);
+                dp[i][1]=dp[i-1][1];
+            }
+            else
+            {
+                dp[i][1]=max(dp[i][1],dp[i-1][0]+1);
+                dp[i][0]=dp[i-1][0];
+            }
+        }
+		return max(dp[nums.size()-1][0],dp[nums.size()-1][1]);
     }
 };
