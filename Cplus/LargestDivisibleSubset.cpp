b93038baab63221f5a01e9165e63f47f17f84555
JumHorn <JumHorn@gmail.com>
2019-05-02 14:45:19 +0800

    Largest Divisible Subset

---
+++ Cplus/LargestDivisibleSubset.cpp
@@ -0,0 +1,39 @@
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+		vector<int> res;
+		if(nums.empty())
+			return res;
+		sort(nums.begin(),nums.end());       
+		vector<vector<int> > dp(nums.size(),vector<int>(2,1));
+		int maxnum=1,maxindex=0;
+		for(int i=1;i<(int)nums.size();i++)
+		{
+			for(int j=0;j<i;j++)
+				if(nums[i]%nums[j]==0&&dp[i][0]<dp[j][0]+1)
+				{
+					dp[i][1]=j;
+				 	dp[i][0]=dp[j][0]+1;
+					if(dp[i][0]>maxnum)
+					{
+						maxindex=i;
+						maxnum=dp[i][0];
+					}
+				}
+		}
+        if(maxnum==1)
+            res.push_back(nums[maxindex]);  
+		while(maxnum!=1)
+		{
+			res.push_back(nums[maxindex]);
+			maxnum=dp[maxindex][0];
+			maxindex=dp[maxindex][1];
+		}
+		
+		return res;
+    }
+};
