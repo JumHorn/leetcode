75f2012414e603bccaf4ff37200681dfd89a38b9
JumHorn <JumHorn@gmail.com>
2019-05-03 21:59:33 +0800

    Find K Closest Elements

--- Cplus/FindKClosestElements.cpp
+++ Cplus/FindKClosestElements.cpp
@@ -3,19 +3,16 @@
 #include<algorithm>
 using namespace std;
 
+/*
+user defined comp must meets the requirement comp(a,b)=true then must comp(b,a)=false
+*/
+
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         for(int i=0;i<(int)arr.size();i++)
 			arr[i]-=x;
-		//sort(arr.begin(),arr.end(),*this);
-		for(int i=0;i<(int)arr.size()-1;i++)
-			for(int j=(int)arr.size()-1;j>i;j--)
-			{
-				if(!comp(arr[j-1],arr[j]))
-					swap(arr[j-1],arr[j]);
-			}
-
+		sort(arr.begin(),arr.end(),*this);
 		vector<int> res(arr.begin(),arr.begin()+k);
         for(int i=0;i<(int)res.size();i++)
             res[i]+=x;
@@ -23,12 +20,15 @@ public:
         return res;
     }
 
-	bool comp(int l,int r)
+	bool operator()(int l,int r)
 	{
-		if(abs(l)==abs(r))
-		{
-			return l<=0;
-		}
+        if(abs(l)==abs(r))
+        {
+            if(l<0&&r>0)
+                return true;
+            else
+                return false;
+        }
 		return abs(l)<abs(r);
 	}
 };
