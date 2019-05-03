8be2f5d07feef9ecaac6787b82a6b0dbec0a84f8
JumHorn <JumHorn@gmail.com>
2019-05-03 21:21:16 +0800

    bubble sort time limited exceeded

--- Cplus/FindKClosestElements.cpp
+++ Cplus/FindKClosestElements.cpp
@@ -1,5 +1,6 @@
 #include<vector>
 #include<cmath>
+#include<algorithm>
 using namespace std;
 
 class Solution {
@@ -7,7 +8,14 @@ public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         for(int i=0;i<(int)arr.size();i++)
 			arr[i]-=x;
-		sort(arr.begin(),arr.end(),*this);
+		//sort(arr.begin(),arr.end(),*this);
+		for(int i=0;i<(int)arr.size()-1;i++)
+			for(int j=(int)arr.size()-1;j>i;j--)
+			{
+				if(!comp(arr[j-1],arr[j]))
+					swap(arr[j-1],arr[j]);
+			}
+
 		vector<int> res(arr.begin(),arr.begin()+k);
         for(int i=0;i<(int)res.size();i++)
             res[i]+=x;
@@ -15,8 +23,12 @@ public:
         return res;
     }
 
-	bool operator()(int l,int r)
+	bool comp(int l,int r)
 	{
+		if(abs(l)==abs(r))
+		{
+			return l<=0;
+		}
 		return abs(l)<abs(r);
 	}
 };
