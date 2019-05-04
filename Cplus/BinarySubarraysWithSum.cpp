64d3dfa8964718cd6f018870a6727b5314e6d6a7
JumHorn <JumHorn@gmail.com>
2019-05-04 09:03:43 +0800

    slide window

--- Cplus/BinarySubarraysWithSum.cpp
+++ Cplus/BinarySubarraysWithSum.cpp
@@ -3,27 +3,57 @@
 using namespace std;
 
 /*
-if A contains the negetive number
-
-dp[i]=A[0]+A[1]+...A[i]
-
-S[i,j] = dp[j]-dp[i-1]
-0<i<=j<n
+slide window
+add all subarray which end with i,when i updated
 */
 
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& A, int S) {
-		int res=0,presum=0;
-		vector<int> dp(A.size()+1);
-		dp[0]=1;
-		for(int i=0;i<(int)A.size();i++)
+		if(A.empty())
+			return 0;
+		int res=0;
+		int i=-1,j=0,sum=0;
+		while(++i<(int)A.size())
 		{
-			presum+=A[i];
-			if(presum-S>=0)
-				res+=dp[presum-S];
-			dp[presum]++;
+			sum+=A[i];
+			if(sum==S)
+            {
+				res++;
+                for(int k=j;k<i;k++)
+                {
+                    if(A[k]==0)
+                        res++;
+                    else
+                        break;
+                }
+            }
+			else if(sum>S)
+            {
+                while(j<=i&&A[j]==0)
+                    j++;
+                j++;
+                sum--;
+                if(j<=i)
+                    res++;
+                for(int k=j;k<=i;k++)
+                {
+                    if(A[k]==0)
+                        res++;
+                    else
+                        break;
+                }
+            }
 		}
 		return res;
     }
 };
+
+int main()
+{
+    //int a[]={1,0,1,0,1};
+    vector<int> v(5);
+    v[2]=1;
+    Solution sol;
+    sol.numSubarraysWithSum(v,0);
+}
