5ff5a3fbabed6cbc582620316a7932228bf3de6f
JumHorn <JumHorn@gmail.com>
2019-05-04 21:09:20 +0800

    Freedom Trail

--- Cplus/FreedomTrail.cpp
+++ Cplus/FreedomTrail.cpp
@@ -1,33 +1,38 @@
 #include<climits>
 #include<string>
+#include<vector>
 using namespace std;
 
 class Solution {
 public:
     int findRotateSteps(string ring, string key) {
-		return findRotateSteps(ring,0,key,0);
+		vector<vector<int> > dp(key.length(),vector<int>(ring.length()));
+		return findRotateSteps(dp,ring,0,key,0);
     }
 
-	int findRotateSteps(const string& ring,int r,const string& key,int k)
+	int findRotateSteps(vector<vector<int> >& dp,const string& ring,int r,const string& key,int k)
 	{
 		if(k==(int)key.length())
 			return 0;
+		if(dp[k][r]!=0)
+			return dp[k][r];
 		int n=ring.size(),res=INT_MAX,tmp,radius=0;
 		if(ring[r]==key[k])
-			return findRotateSteps(ring,r,key,k+1)+1;
-		while(++radius<=ring.size()/2)
+			return findRotateSteps(dp,ring,r,key,k+1)+1;
+		while(++radius<=(int)ring.size()/2)
 		{
 			tmp=(r+radius)%n;
 			if(ring[tmp]==key[k])
 			{
-				res=min(res,findRotateSteps(ring,tmp,key,k+1)+radius+1);
+				res=min(res,findRotateSteps(dp,ring,tmp,key,k+1)+radius+1);
 			}
 			tmp=(r-radius+n)%n;
 			if(ring[tmp]==key[k])
 			{
-				res=min(res,findRotateSteps(ring,tmp,key,k+1)+radius+1);
+				res=min(res,findRotateSteps(dp,ring,tmp,key,k+1)+radius+1);
 			}
 		}
+		dp[k][r]=res;
 		return res;
 	}
 };
