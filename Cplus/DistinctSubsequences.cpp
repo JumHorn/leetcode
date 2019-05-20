1aab4ff4847f6be2f2a3e0a5cb205af9de5487ed
JumHorn <JumHorn@gmail.com>
2019-05-20 22:39:40 +0800

    Distinct Subsequences beats 6.49%

--- Cplus/DistinctSubsequences.cpp
+++ Cplus/DistinctSubsequences.cpp
@@ -1,21 +1,29 @@
 #include<string>
+#include<vector>
 using namespace std;
 
 class Solution {
 public:
     int numDistinct(string s, string t) {
-		return numDistinct(s,0,t,0);        
+		vector<vector<int> > dp(s.size()+1,vector<int>(t.size()+1,-1));
+		return numDistinct(s,0,t,0,dp);
     }
 
-	int numDistinct(const string& s,int i,const string& t,int j)
+	int numDistinct(const string& s,int i,const string& t,int j,vector<vector<int> >& dp)
 	{
 		if(j>=(int)t.size())
 			return 1;
+		if(i<(int)s.size()&&dp[i][j]!=-1)
+			return dp[i][j];
 		int res=0;
 		for(int k=i;k<(int)s.size();k++)
 		{
 			if(s[k]==t[j])
-				res+=numDistinct(s,k+1,t,j+1);
+			{
+				int tmp=numDistinct(s,k+1,t,j+1,dp);
+				dp[k+1][j+1]=tmp;
+				res+=tmp;
+			}
 		}
 		return res;
 	}
