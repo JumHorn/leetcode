79394bf3a5f8aa4ced368b4c34d4ba96d114ff5a
JumHorn <JumHorn@gmail.com>
2019-05-22 23:31:10 +0800

    Palindrome Partitioning II beats 13.12%

---
+++ Cplus/PalindromePartitioningII.cpp
@@ -0,0 +1,33 @@
+#include<string>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int minCut(string s) {
+        if(s.empty())
+            return 0;
+		int M=s.length();
+		vector<int> dp(M+1,INT_MAX);
+        dp[0]=0;
+		for(int i=0;i<M;i++)
+			for(int j=0;j<=i;j++)
+			{
+				if(isPalindrome(s,j,i))
+				{
+					dp[i+1]=min(dp[i+1],dp[j]+1);
+				}
+			}
+	    return dp.back()-1;
+    }
+
+	bool isPalindrome(const string& s,int j,int i)
+	{
+		while(j<i)
+		{
+			if(s[j++]!=s[i--])
+				return false;
+		}
+		return true;
+	}
+};
