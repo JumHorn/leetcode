10a125ade1ee858caefc4d54a68f9b69daf09718
JumHorn <JumHorn@gmail.com>
2019-05-24 20:51:12 +0800

    Word Ladder II beats 25.97%

--- Cplus/WordLadderII.cpp
+++ Cplus/WordLadderII.cpp
@@ -10,53 +10,52 @@ class Solution {
 public:
 	vector<vector<string> > findLadders(string beginWord, string endWord, vector<string>& wordList) {
 		map<string,unordered_set<string> > graph;
-		for(int i=0;i<(int)wordList.size();i++)
-			if(transform(beginWord,wordList[i])==1)
-				graph[beginWord].insert(wordList[i]);
-
-		for(int i=0;i<(int)wordList.size()-1;i++)
-			for(int j=i+1;j<(int)wordList.size();j++)
-				if(transform(wordList[i],wordList[j])==1)
-				{
-					graph[wordList[i]].insert(wordList[j]);
-					graph[wordList[j]].insert(wordList[i]);
-				}
-		if(graph.find(endWord)==graph.end())
-			return vector<vector<string> >();
 		vector<vector<string> >  res;
 		unordered_set<string> visited;
-
+		unordered_set<string> words(wordList.begin(),wordList.end());
 		queue<string> q;
 		int depth=1;
 		q.push(beginWord);
-		while(!q.empty())
+		bool flag=true;
+		while(!q.empty()&&flag)
 		{
 			int n=q.size();
 			depth+=1;
+			for(unordered_set<string>::iterator iter=visited.begin();iter!=visited.end();++iter)
+				words.erase(*iter);
+			visited.clear();
 			while(--n>=0)
 			{
-				if(graph[q.front()].find(endWord)!=graph[q.front()].end())
+				string t=q.front();
+				for(int i=0;i<(int)t.length();i++)
 				{
-					//BFS
-					visited.clear();
-					vector<string> tmp(depth);
-					tmp[0]=beginWord;
-					tmp[depth-1]=endWord;
-					findLadders(graph,res,tmp,visited,endWord,depth);
-					return res;
-				}
-				for(unordered_set<string>::iterator iter=graph[q.front()].begin();iter!=graph[q.front()].end();++iter)
-				{
-					if(visited.find(*iter)==visited.end())
+					for(char c='a';c<='z';c++)
 					{
-						visited.insert(*iter);
-						q.push(*iter);
+						t[i]=c;
+						if(words.find(t)!=words.end())
+						{
+							graph[q.front()].insert(t);
+							q.push(t);
+							visited.insert(t);
+							if(t==endWord)
+								flag=false;
+						}	
 					}
+					t=q.front();
 				}
 				q.pop();
 			}
 		}
-		return vector<vector<string> >();
+		if(flag)
+			return vector<vector<string> >();
+
+		//BFS
+		visited.clear();
+		vector<string> tmp(depth);
+		tmp[0]=beginWord;
+		tmp[depth-1]=endWord;
+		findLadders(graph,res,tmp,visited,endWord,depth);
+		return res;
 	}
 	//DFS
 	void findLadders(map<string,unordered_set<string> >& graph,vector<vector<string> >& res,vector<string>& tmp,unordered_set<string>& visited,const string& dst,int depth)
@@ -77,14 +76,5 @@ public:
 				visited.erase(visited.find(*iter));
 			}
 		}
-	}
-
-	int transform(const string& w1,const string& w2)
-	{
-		int res=0;
-		for(int i=0;i<(int)w1.size();i++)
-			if(w1[i]!=w2[i])
-				res++;
-		return res;
 	}
 };
