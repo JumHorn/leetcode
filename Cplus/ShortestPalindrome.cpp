4843259145c7b0553336987e7a963975371733a9
JumHorn <JumHorn@gmail.com>
2019-05-29 00:08:52 +0800

    Shortest Palindrome beats 86.51%

--- Cplus/ShortestPalindrome.cpp
+++ Cplus/ShortestPalindrome.cpp
@@ -1,28 +1,37 @@
 #include<string>
 #include<algorithm>
+#include<vector>
 using namespace std;
+
+//kmp next array
 
 class Solution {
 public:
     string shortestPalindrome(string s) {
-		int i=0,j=s.length();        
-		while(--j>=0)
-			if(isPalindrome(s,i,j))
+        if(s.empty())
+            return "";
+		string rev=s;
+		reverse(rev.begin(),rev.end());
+		string t=s+'#'+rev;
+		vector<int> next(t.length());
+		int k=0;
+		for(int i=1;i<(int)next.size();i++)
+		{
+			if(t[next[i-1]]==t[i])
+				next[i]=next[i-1]+1;
+			else
 			{
-				string tmp=s.substr(j+1);
-				reverse(tmp.begin(),tmp.end());
-				return tmp+s;
+                k=i;
+				k=next[k-1];
+				while(k>0&&t[k]!=t[i])
+				{
+					k=next[k-1];
+				}
+				if(t[k]==t[i])
+                    ++k;
+				next[i]=k;
 			}
-		return "";
+		}
+		return rev.substr(0,rev.length()-next.back())+s;
     }
-
-	bool isPalindrome(const string& s,int i,int j)
-	{
-		if(i>=j)
-			return true;
-		while(i<j)
-			if(s[i++]!=s[j--])
-				return false;
-		return true;
-	}
 };
