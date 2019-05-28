0cf0cb29eec509d0b2ffac38379c7ea07da0d009
JumHorn <JumHorn@gmail.com>
2019-05-29 00:12:47 +0800

    cleaner code

--- Cplus/ShortestPalindrome.cpp
+++ Cplus/ShortestPalindrome.cpp
@@ -17,20 +17,12 @@ public:
 		int k=0;
 		for(int i=1;i<(int)next.size();i++)
 		{
-			if(t[next[i-1]]==t[i])
-				next[i]=next[i-1]+1;
-			else
-			{
-                k=i;
-				k=next[k-1];
-				while(k>0&&t[k]!=t[i])
-				{
-					k=next[k-1];
-				}
-				if(t[k]==t[i])
-                    ++k;
-				next[i]=k;
-			}
+            k=next[i-1];
+            while(k>0&&t[k]!=t[i])
+                k=next[k-1];
+            if(t[k]==t[i])
+                ++k;
+            next[i]=k;
 		}
 		return rev.substr(0,rev.length()-next.back())+s;
     }
