fbb59f1cabfe62a604874aece399f45ea7a14366
JumHorn <JumHorn@gmail.com>
2019-06-01 23:19:39 +0800

    signed integer overflow

--- Cplus/ExpressionAddOperators.cpp
+++ Cplus/ExpressionAddOperators.cpp
@@ -8,15 +8,20 @@ public:
     vector<string> addOperators(string num, int target) {
 		vector<string> res;
 		for(int i=0;i<(int)num.size();i++)
-			addOperators(num,i+1,stoi(num.substr(0,i+1)),target,num.substr(0,i+1),res);
+		{
+			string str=num.substr(0,i+1);
+			int val=stoi(num.substr(0,i+1));
+			addOperators(num,i+1,val,val,target,str,res);
+		}
 		return res;
     }
 
-	void addOperators(const string& num,int i,int val,int target,string str,vector<string>& res)
+	//cv:current value pv:previous value
+	void addOperators(const string& num,int i,int cv,int pv,int target,string str,vector<string>& res)
 	{
 		if(i>=(int)num.size())
 		{
-			if(target==val)
+			if(target==cv)
 				res.push_back(str);
 			return;
 		}
@@ -25,38 +30,12 @@ public:
 		{
 			string tmp=num.substr(i,j-i);
 			int k=stoi(tmp);
-
-			string str1=str;
-			int val1=val;
-			str1+='+';
-			val1+=k;
-			str1+=tmp;
-			addOperators(num,j,val1,target,str1,res);
-
-			str1=str;
-			val1=val;
-			str1+='-';
-			val1-=k;
-			str1+=tmp;
-			addOperators(num,j,val1,target,str1,res);
-
-			str1=str;
-			val1=val;
-			str1+='*';
-			val1*=k;
-			str1+=tmp;
-			addOperators(num,j,val1,target,str1,res);
+			addOperators(num,j,cv+k,k,target,str+'+'+tmp,res);
+			addOperators(num,j,cv-k,-k,target,str+'-'+tmp,res);
+			addOperators(num,j,cv-pv+k*pv,k*pv,target,str+'*'+tmp,res);
 
             if(num[i]=='0')
                 break;
 		}
 	}
 };
-
-int main()
-{
-	Solution sol;
-	auto v=sol.addOperators(string("600"),6);
-	for(auto n:v)
-		cout<<n<<endl;
-}
