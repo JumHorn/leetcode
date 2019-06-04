2d4d14017af176348f7c02c55b61551a74d170b0
JumHorn <JumHorn@gmail.com>
2019-06-04 23:59:37 +0800

    The Skyline Problem beats 13.35%

--- Cplus/TheSkylineProblem.cpp
+++ Cplus/TheSkylineProblem.cpp
@@ -1,5 +1,7 @@
 #include<vector>
 #include<queue>
+#include<set>
+#include<unordered_map>
 using namespace std;
 
 class Solution {
@@ -9,17 +11,40 @@ public:
         if(buildings.empty())
             return res;
 		vector<int> v(buildings.size());
-		priority_queue<int> q;
+		unordered_map<int,int> mergemap;
 		for(int i=0;i<(int)buildings.size();i++)
 		{
-			q.push(buildings[i][0]);
-			q.push(buildings[i][1]);
+			if(mergemap.find(buildings[i][2])==mergemap.end())
+				mergemap[buildings[i][2]]=i;
+			else
+			{
+				int index=mergemap[buildings[i][2]];
+				if(buildings[index][1]>=buildings[i][0])
+				{
+					if(buildings[index][1]<buildings[i][1])
+						buildings[index][1]=buildings[i][1];
+					buildings[i][0]=-1;
+				}
+				else
+					mergemap[buildings[i][2]]=i;
+			}
 		}
-		int sweepline=q.top();
+		priority_queue<pair<int,int> > q;
+		for(int i=0;i<(int)buildings.size();i++)
+		{
+			if(buildings[i][0]!=-1)
+			{
+				q.push({-buildings[i][0],i});
+				q.push({-buildings[i][1],i});
+			}
+		}
 		while(!q.empty())
 		{
-			sweepline=q.top();
+			int sweepline=-q.top().first;
+            int index=q.top().second;
 			q.pop();
+            bool flag=true;
+			bool flag1=false;
 			int maxsky=0,secondsky=0;
 			for(int i=0;i<(int)v.size();i++)
 			{
@@ -32,16 +57,30 @@ public:
 					v[i]=-1;
 					continue;
 				}
+				if(buildings[i][2]>buildings[index][2])
+                {
+                    flag=false;
+					break;
+                }
 				if(buildings[i][2]>maxsky)
 				{
-					secondsky=maxsky;
+					if(flag1)
+                    	secondsky=maxsky;
 					maxsky=buildings[i][2];
+					flag1=(sweepline!=buildings[i][1]);
+				}
+                if(maxsky!=buildings[i][2]&&buildings[i][2]>secondsky&&sweepline!=buildings[i][1])
+				{
+					secondsky=buildings[i][2];
 				}
 			}
-            if(res.empty()||res.back()[1]!=maxsky)
-                res.push_back({sweepline,maxsky});
-			else
-				res.push_back({sweepline,secondsky});
+            if(flag)
+            {
+                if(res.empty()||res.back()[1]!=maxsky)
+                    res.push_back({sweepline,maxsky});
+                else
+                    res.push_back({sweepline,secondsky});
+            }
 		}
 		return res;
     }
