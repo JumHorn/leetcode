f85e5b22c21bd6d12394c721e122ac20e9b7421f
JumHorn <JumHorn@gmail.com>
2019-06-05 23:52:59 +0800

    Russian Doll Envelopes beats 18.69%

---
+++ Cplus/RussianDollEnvelopes.cpp
@@ -0,0 +1,20 @@
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int maxEnvelopes(vector<vector<int> >& envelopes) {
+		if(envelopes.empty())
+			return 0;
+		vector<int> dp(envelopes.size(),1);        
+		sort(envelopes.begin(),envelopes.end());
+		for(int i=1;i<(int)dp.size();i++)
+			for(int j=0;j<i;j++)
+			{
+				if(envelopes[i][0]>envelopes[j][0]&&envelopes[i][1]>envelopes[j][1])
+					dp[i]=max(dp[i],dp[j]+1);
+			}
+		return *max_element(dp.begin(),dp.end());
+    }
+};
