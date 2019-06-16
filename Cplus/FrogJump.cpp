b13519933a553c4ca6fbd2d2c50e40933e554ba4
JumHorn <JumHorn@gmail.com>
2019-06-16 08:30:06 +0800

    FrogJump beats 92.36%

--- Cplus/FrogJump.cpp
+++ Cplus/FrogJump.cpp
@@ -1,21 +1,32 @@
 #include<vector>
+#include<unordered_map>
 using namespace std;
 
 class Solution {
 public:
     bool canCross(vector<int>& stones) {
-		return canCross(stones,0,0);        
+		vector<unordered_map<int,bool> > dp(1100);
+		return canCross(stones,0,0,dp);  
     }
 
-	bool canCross(vector<int>& stones,int current,int skip)
+	bool canCross(vector<int>& stones,int current,int skip,vector<unordered_map<int,bool> >& dp)
 	{
 		if(current==(int)stones.size()-1)
+		{
+			dp[current][skip]=true;
 			return true;
+		}
+		if(dp[current].find(skip)!=dp[current].end())
+			return dp[current][skip];
 		int k=skip+1,i,j;
 		for(i=current+1;i<(int)stones.size();i++)
 		{
-			if(k+stones[current]==stones[i]&&canCross(stones,i,k))
-				return true;
+			if(k+stones[current]==stones[i])
+			{
+				if(canCross(stones,i,k,dp))
+					return true;
+				dp[i][k]=false;
+			}
 			if(stones[i]>k+stones[current])
 				break;
 		}
@@ -24,8 +35,12 @@ public:
 		{
 			for(j=i-1;j>current;j--)
 			{
-				if(k+stones[current]==stones[j]&&canCross(stones,j,k))
-					return true;
+				if(k+stones[current]==stones[j])
+				{
+					if(canCross(stones,j,k,dp))
+						return true;
+					dp[j][k]=false;
+				}
 				if(stones[j]<k+stones[current])
 					break;
 			}
@@ -35,12 +50,17 @@ public:
 		{
 			for(;j>current;j--)
 			{
-				if(k+stones[current]==stones[j]&&canCross(stones,j,k))
-					return true;
+				if(k+stones[current]==stones[j])
+				{
+					if(canCross(stones,j,k,dp))
+						return true;
+					dp[j][k]=false;
+				}
 				if(stones[j]<k+stones[current])
 					break;
 			}
 		}
+		dp[current][k]=false;
 		return false;
 	}
 };
