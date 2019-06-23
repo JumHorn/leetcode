63fa10353ed56c1d63df6d8c088d5f0735bf8164
JumHorn <JumHorn@gmail.com>
2019-06-23 20:12:08 +0800

    Find K-th Smallest Pair Distance beats 64.95%

--- Cplus/FindK-thSmallestPairDistance.cpp
+++ Cplus/FindK-thSmallestPairDistance.cpp
@@ -1,63 +1,26 @@
 #include<vector>
 #include<algorithm>
-#include<queue>
 using namespace std;
 
 class Solution {
 public:
     int smallestDistancePair(vector<int>& nums, int k) {
-        vector<int> tmp(nums.size());
-		priority_queue<int> q;
-		divideAndMerge(nums,0,nums.size(),tmp,k,q);
-		return q.top();
-    }
-
-	int divideAndMerge(vector<int>& nums,int start,int end,vector<int>& tmp,int k,priority_queue<int>& q)
-	{
-		if(end-start<=1)
-			return 0;
-		int mid=(end-start)/2+start;
-		int res=divideAndMerge(nums,start,mid,tmp,k,q)+divideAndMerge(nums,mid,end,tmp,k,q);
-		//todo
-		int i=start,j=mid;	
-		for(;i<mid;i++)
+		sort(nums.begin(),nums.end());
+		int high=nums.back()-nums.front(),low=0;
+		while(low<high)
 		{
-			while(j<end&&nums[i]>nums[j])
-				j++;
-			int tmp=j-1;
-			while(tmp>=mid)
+			int mid=(high-low)/2+low,count=0,i=0,j=0;
+			for(;i<(int)nums.size();i++)
 			{
-				int d=nums[i]-nums[tmp];
-				if((int)q.size()<k)
-					q.push(d);
-				else if(d<q.top())
-				{
-					q.pop();
-					q.push(d);
-				}
-				else
-					break;
-				tmp--;
+				while(j<(int)nums.size()&&nums[j]-nums[i]<=mid)
+					j++;
+				count+=j-i-1;
 			}
-			tmp=j;
-			while(tmp<end)
-			{
-				int d=nums[tmp]-nums[i];
-				if((int)q.size()<k)
-					q.push(d);
-				else if(d<q.top())
-				{
-					q.pop();
-					q.push(d);
-				}
-				else
-					break;
-				tmp++;
-			}
+            if(count<k)
+                low=mid+1;
+            else
+                high=mid;
 		}
-
-		merge(nums.begin()+start,nums.begin()+mid,nums.begin()+mid,nums.begin()+end,tmp.begin());
-		copy(tmp.begin(),tmp.begin()+end-start,nums.begin()+start);
-		return res;
-	}
+		return low;
+    }
 };
