a838a89301874bf1cc31feef8eb525618350da63
JumHorn <JumHorn@gmail.com>
2019-06-27 21:58:22 +0800

    Reaching Points beats 78.79%

--- Cplus/ReachingPoints.cpp
+++ Cplus/ReachingPoints.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     bool reachingPoints(int sx, int sy, int tx, int ty) {
- 		if(sx==tx&&sy==ty)
-	 		return true;		
-		if(sx>tx||sy>ty)
-			return false;
-		return reachingPoints(sx,sx+sy,tx,ty)||reachingPoints(sx+sy,sy,tx,ty);
+		while(sx<tx&&sy<ty)
+		{
+			if(tx>ty)
+				tx%=ty;
+			else
+				ty%=tx;
+		}
+		return (sx==tx&&sy==ty)||(sx==tx&&ty>=sy&&0==(ty-sy)%tx)||(sy==ty&&tx>=sx&&0==(tx-sx)%ty);
     }
 };
