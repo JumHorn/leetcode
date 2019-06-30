187a03f05f7cc3be707c622c57fd383379d7c262
JumHorn <JumHorn@gmail.com>
2019-06-30 19:37:23 +0800

    Cherry Pickup beats 67.55%

--- Cplus/CherryPickup.cpp
+++ Cplus/CherryPickup.cpp
@@ -1,63 +1,32 @@
 #include<vector>
 #include<algorithm>
+#include<climits>
 using namespace std;
 
 class Solution {
 public:
     int cherryPickup(vector<vector<int>>& grid) {
-		int m=grid.size(),n=grid[0].size();
-		vector<vector<int>> tmp(m+1,vector<int>(n+1));
-		for(int i=2;i<=m;i++)
-			tmp[i][0]=-1;
-		for(int i=2;i<=n;i++)
-			tmp[0][i]=-1;
-		for(int i=0;i<m;i++)
-			copy(grid[i].begin(),grid[i].end(),tmp[i+1].begin()+1);
-		for(int i=1;i<=m;i++)
-			for(int j=1;j<=n;j++)
-			{
-				if(tmp[i][j]!=-1)
-				{
-					if(tmp[i-1][j]==-1&&tmp[i][j-1]==-1)
-						tmp[i][j]=-1;
-					else if(tmp[i-1][j]==-1)
-						tmp[i][j]+=tmp[i][j-1];
-					else if(tmp[i][j-1]==-1)
-						tmp[i][j]+=tmp[i-1][j];
-					else
-						tmp[i][j]+=max(tmp[i-1][j],tmp[i][j-1]);
-				}
-			}		
-		int res=tmp.back().back();
-		if(res==-1)
-			return 0;
-		int i=m,j=n;
-        grid[0][0]=0;
-		while(i>1||j>1)
-		{
-			grid[i-1][j-1]=0;
-			if(tmp[i][j]-tmp[i][j-1]>tmp[i][j]-tmp[i-1][j])
-				i--;
-			else
-				j--;
-		}
-		for(int i=0;i<m;i++)
-			copy(grid[i].rbegin(),grid[i].rend(),tmp[m-i].begin()+1);
-		for(int i=1;i<=m;i++)
-			for(int j=1;j<=n;j++)
-			{
-				if(tmp[i][j]!=-1)
-				{
-					if(tmp[i-1][j]==-1&&tmp[i][j-1]==-1)
-						tmp[i][j]=-1;
-					else if(tmp[i-1][j]==-1)
-						tmp[i][j]+=tmp[i][j-1];
-					else if(tmp[i][j-1]==-1)
-						tmp[i][j]+=tmp[i-1][j];
-					else
-						tmp[i][j]+=max(tmp[i-1][j],tmp[i][j-1]);
-				}
-			}		
-		return res+tmp.back().back();
+		int N=grid.size();
+		//r1 c1 r2
+		vector<vector<vector<int>>> dp(N,vector<vector<int>>(N,vector<int>(N,INT_MIN)));
+		return max(0,dfs(grid,dp,0,0,0));
     }
+
+	int dfs(vector<vector<int>>& grid,vector<vector<vector<int>>>& dp,int r1,int c1,int r2)
+	{
+		int N=grid.size(),c2=r1+c1-r2;
+		if(r1>=N||c1>=N||r2>=N||c2>=N||grid[r1][c1]==-1||grid[r2][c2]==-1)
+			return -5000;//max N*N make all results minus
+		if(r1==N-1&&c1==N-1)
+			return grid[r1][c1];
+		if(dp[r1][c1][r2]!=INT_MIN)
+			return dp[r1][c1][r2];
+        int res=grid[r1][c1];
+        if(r1!=r2)
+            res+=grid[r2][c2];
+		res+=max(max(dfs(grid,dp,r1,c1+1,r2),dfs(grid,dp,r1,c1+1,r2+1)),
+                max(dfs(grid,dp,r1+1,c1,r2),dfs(grid,dp,r1+1,c1,r2+1)));
+        dp[r1][c1][r2]=res;
+		return res;
+	}
 };
