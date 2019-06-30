b67be597607dffddd19b8d810ae380e3e530c8f5
JumHorn <JumHorn@gmail.com>
2019-06-30 18:59:35 +0800

    Shortest Subarray with Sum at Least K beats 92.75%

--- Cplus/ShortestSubarraywithSumatLeastK.cpp
+++ Cplus/ShortestSubarraywithSumatLeastK.cpp
@@ -1,23 +1,31 @@
 #include<algorithm>
 #include<numeric>
 #include<vector>
+#include<deque>
+#include<algorithm>
+#include<climits>
 using namespace std;
 
 class Solution {
 public:
     int shortestSubarray(vector<int>& A, int K) {
+		vector<int> sums(A.size()+1);
+		deque<int> monoqueue;
+		int res=INT_MAX;
+		for(int i=1;i<=(int)A.size();i++)
+			sums[i]=sums[i-1]+A[i-1];
+		monoqueue.push_back(0);
 		for(int i=1;i<=(int)A.size();i++)
 		{
-			int sum=accumulate(A.begin(),A.begin()+i,0);
-			if(sum>=K)
-				return i;
-			for(int j=0;j<(int)A.size()-i;j++)
+			while(!monoqueue.empty()&&sums[i]-sums[monoqueue.front()]>=K)
 			{
-				sum=sum-A[j]+A[i+j];
-				if(sum>=K)
-					return i;
+				res=min(res,i-monoqueue.front());
+				monoqueue.pop_front();
 			}
+			while(!monoqueue.empty()&&sums[i]<=sums[monoqueue.back()])
+				monoqueue.pop_back();
+			monoqueue.push_back(i);
 		}
-		return -1;
+		return res==INT_MAX?-1:res;
     }
 };
