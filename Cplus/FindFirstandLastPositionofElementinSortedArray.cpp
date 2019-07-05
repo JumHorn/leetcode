601fc98baac460adea8f8e3fd88a63943dcfa8a4
JumHorn <JumHorn@gmail.com>
2019-07-05 22:31:27 +0800

    Find First and Last Position of Element in Sorted Array beats 91.09%

--- Cplus/FindFirstandLastPositionofElementinSortedArray.cpp
+++ Cplus/FindFirstandLastPositionofElementinSortedArray.cpp
@@ -10,42 +10,27 @@ public:
 		if(target<nums.front()||target>nums.back())
 			return res;	
 		int low=0,high=nums.size()-1,mid;
-		while(low<=high)
-		{
-			mid=(high-low)/2+low;
-			if(target==nums[mid])
-				break;
-			if(target<nums[mid])
-				high=mid-1;
-			else
-				low=mid+1;
-		}
-		if(low>high)
-			return res;
-		res[1]=mid;
-		low=0,high=mid;
 		while(low<high)
 		{
 			mid=(high-low)/2+low;
-			if(nums[mid]==target)
+			if(target<=nums[mid])
 				high=mid;
 			else
 				low=mid+1;
 		}
-		res[0]=high;
-		low=res[1]+1;
+		if(nums[low]!=target)
+			return res;
+        res[0]=low;
 		high=nums.size()-1;
-		while(low<=high)
+		while(low<high)
 		{
-			mid=(high-low)/2+low;
-			if(nums[mid]==target)
-            {
-                res[1]=mid;
-				low=mid+1;
-            }
+			mid=(high-low)/2+low+1;
+			if(nums[mid]<=target)
+				low=mid;
 			else
 				high=mid-1;
 		}
+        res[1]=high;
 		return res;
     }
 };
