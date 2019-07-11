97d362a5a3dbb44a5cee7814f52bf6b3570f7f1b
JumHorn <JumHorn@gmail.com>
2019-07-11 22:50:52 +0800

    Course Schedule II beats 97.42%

--- Cplus/CourseScheduleII.cpp
+++ Cplus/CourseScheduleII.cpp
@@ -1,13 +1,13 @@
 #include<vector>
-#include<set>
+#include<unordered_set>
 using namespace std;
 
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
 		vector<int> res(numCourses),parents(numCourses);
-		set<int> noParents;
-		vector<set<int>> children(numCourses);		
+		unordered_set<int> noParents;
+		vector<unordered_set<int>> children(numCourses);		
 		for(int i=0;i<(int)prerequisites.size();i++)
 		{
 			++parents[prerequisites[i][0]];
@@ -23,7 +23,7 @@ public:
 			int orphan=*noParents.begin();
 			noParents.erase(orphan);
 			res[i]=orphan;
-			for(set<int>::iterator iter=children[orphan].begin();iter!=children[orphan].end();++iter)
+			for(unordered_set<int>::iterator iter=children[orphan].begin();iter!=children[orphan].end();++iter)
 				if(--parents[*iter]==0)
 					noParents.insert(*iter);
 		}
