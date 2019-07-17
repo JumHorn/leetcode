3e61679dcc291d81e6fb15b1f59c5fd16ee03ef5
JumHorn <JumHorn@gmail.com>
2019-07-18 06:53:53 +0800

    Minimum Swaps To Make Sequences Increasing beats 26.96%

--- Cplus/MinimumSwapsToMakeSequencesIncreasing.cpp
+++ Cplus/MinimumSwapsToMakeSequencesIncreasing.cpp
@@ -6,37 +6,56 @@ using namespace std;
 class Solution {
 public:
     int minSwap(vector<int>& A, vector<int>& B) {
-		int res=minSwap(A,B,1);
+		vector<vector<int>> dp(A.size(),vector<int>(2,-1));
+		int res=minSwap(A,B,1,dp);
 		if(A[0]!=B[0])
 		{
 			swap(A[0],B[0]);
-			res=min(res,minSwap(A,B,1)+1);	
+			res=min(res,minSwap(A,B,1,dp)+1);	
 		}
 		return res;        
     }
 
-	int minSwap(vector<int>& A,vector<int>& B,int index)
+	int minSwap(vector<int>& A,vector<int>& B,int index,vector<vector<int>>& dp)
 	{
 		if(index>=(int)A.size())
 			return 0;
 		int res=INT_MAX;
 		if(A[index]==B[index])
-			return minSwap(A,B,index+1);
+        {
+            if(dp[index][0]!=-1)
+                return dp[index][0];
+			res=minSwap(A,B,index+1,dp);
+            dp[index][0]=res;
+            return res;
+        }
 		bool flag=true;
 		if(A[index]>A[index-1]&&B[index]>B[index-1])
 		{
 			flag=false;
-			res=min(res,minSwap(A,B,index+1));
+            if(dp[index][0]!=-1)
+                res=dp[index][0];
+            else
+            {
+			    res=min(res,minSwap(A,B,index+1,dp));
+                dp[index][0]=res;
+            }
 		}
 		if(A[index]>B[index-1]&&B[index]>A[index-1])
 		{
 			flag=false;
 			swap(A[index],B[index]);
-			res=min(res,minSwap(A,B,index+1)+1);
+            if(dp[index][1]!=-1)
+                res=dp[index][1];
+            else
+            {
+			    res=min(res,minSwap(A,B,index+1,dp)+1);
+                dp[index][1]=res;
+            }
             swap(A[index],B[index]);
 		}
 		if(flag)
-			return 10000;
+			res=10000;
 		return res;
 	}
 };
