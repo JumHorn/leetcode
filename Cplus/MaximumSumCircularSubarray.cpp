a4cc9d59186b69e00f6eb31f621872873a8891f2
JumHorn <JumHorn@gmail.com>
2019-07-24 19:59:22 +0800

    Maximum Sum Circular Subarray beats 19.07%

--- Cplus/MaximumSumCircularSubarray.cpp
+++ Cplus/MaximumSumCircularSubarray.cpp
@@ -1,34 +1,34 @@
 #include<vector>
 #include<algorithm>
 #include<numeric>
+#include<deque>
 using namespace std;
 
 class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& A) {
-		int N=A.size(),res=INT_MIN,minimum=0;
-        bool flag=true;
+		int N=A.size(),res=INT_MIN;
 		vector<int> sum(2*N+1);
 		for(int i=1;i<(int)sum.size();i++)
 		{
 			if(i<=N)
-            {
 				sum[i]=sum[i-1]+A[i-1];
-                if(A[i-1]<0)
-                    flag=false;
-            }
 			else
 				sum[i]=sum[i-1]+A[i-N-1];
 		}
-        if(flag)
-            return accumulate(A.begin(),A.end(),0);
+		deque<int> monoqueue;
+		monoqueue.push_back(0);
 		for(int i=1;i<(int)sum.size();i++)
 		{
-			res=max(res,sum[i]-sum[minimum]);
-			if(sum[i]<=sum[minimum])
-				minimum=i;
-			if(i>=N&&i-N==minimum)
-				minimum=distance(sum.begin(),min_element(sum.begin()+i-N+1,sum.begin()+i+1));
+			if(!monoqueue.empty()&&i-monoqueue.front()>N)
+				monoqueue.pop_front();
+			if(!monoqueue.empty())
+				res=max(res,sum[i]-sum[monoqueue.front()]);
+			else
+				res=max(res,sum[i]-sum[i-1]);
+			while(!monoqueue.empty()&&sum[i]<=sum[monoqueue.back()])
+				monoqueue.pop_back();
+			monoqueue.push_back(i);
 		}
 		return res;
     }
