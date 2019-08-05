65564aad12bcbcd2087377327b4c06bafcf85f9f
JumHorn <JumHorn@gmail.com>
2019-08-05 12:46:45 +0800

    3 Sum beats 99.60%

--- Cplus/3Sum.cpp
+++ Cplus/3Sum.cpp
@@ -5,28 +5,42 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-		vector<vector<int>> res;        
-		vector<int> tmp;
+		vector<vector<int>> res;
+		vector<int> tmp(3);
 		sort(nums.begin(),nums.end());
-		threeSum(res,tmp,nums,0,0,3,0);
+		threeSum(res,tmp,nums,0,3,0);
 		return res;
     }
 
-	void threeSum(vector<vector<int>>& res,vector<int>& tmp,vector<int>& nums,int start,int sum,int n,int target)
+	void threeSum(vector<vector<int>>& res,vector<int>& tmp,vector<int>& nums,int start,int n,int target)
 	{
-		if((int)tmp.size()==n)
+		if(n==2)
 		{
-			if(sum==target)
-				res.push_back(tmp);
+			int low=start,high=nums.size()-1;
+			while(low<high)
+			{
+				int sum=nums[low]+nums[high]-target;
+				if(sum==0)
+				{
+					tmp[0]=nums[low];
+					tmp[1]=nums[high];
+					res.push_back(tmp);
+					while(++low<high&&nums[low]==nums[low-1]);
+					while(low<--high&&nums[high]==nums[high+1]);
+				}
+				else if(sum<0)
+					low++;
+				else
+					high--;
+			}
 			return;
 		}
 		for(int i=start;i<(int)nums.size();)
 		{
-			tmp.push_back(nums[i]);
-			threeSum(res,tmp,nums,i+1,sum+nums[i],n,target);
-			tmp.pop_back();
+			tmp[n-1]=nums[i];
+			threeSum(res,tmp,nums,i+1,n-1,target-nums[i]);
 			i++;
-			while(i>0&&i<(int)nums.size()&&nums[i]==nums[i-1])
+			while(i<(int)nums.size()&&nums[i]==nums[i-1])
 				i++;
 		}
 	}
