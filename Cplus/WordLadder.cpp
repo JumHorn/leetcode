b818e4fa03624ab17e51438352bec85f51965af6
JumHorn <JumHorn@gmail.com>
2019-08-18 07:05:09 +0800

    Word Ladder beats 62.07%

---
+++ Cplus/WordLadder.cpp
@@ -0,0 +1,44 @@
+#include<string>
+#include<unordered_set>
+#include<queue>
+using namespace std;
+
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+		unordered_set<string> words(wordList.begin(),wordList.end()),visited;
+	    queue<string> q;
+		visited.insert(beginWord);
+		q.push(beginWord);	
+		if(words.find(endWord)==words.end())
+			return 0;
+		int res=1;
+		while(!q.empty())
+		{
+			int size=q.size();
+			++res;
+			while(--size>=0)
+			{
+				string tmp=q.front();
+				q.pop();
+				for(int i=0;i<(int)tmp.length();i++)
+				{
+					char origin=tmp[i];
+					for(char c='a';c<='z';c++)
+					{
+						tmp[i]=c;
+						if(words.find(tmp)!=words.end()&&visited.find(tmp)==visited.end())
+						{
+							if(tmp==endWord)
+								return res;
+							q.push(tmp);
+							visited.insert(tmp);
+						}
+					}
+					tmp[i]=origin;
+				}
+			}
+		}
+		return 0;
+    }
+};
