0712cfdc5f5e6c9c8228943b0035f10e2cc4a123
JumHorn <JumHorn@gmail.com>
2019-08-18 08:08:34 +0800

    Word Ladder beats 99.24% Two end BFS

--- Cplus/WordLadder.cpp
+++ Cplus/WordLadder.cpp
@@ -1,42 +1,47 @@
 #include<string>
 #include<unordered_set>
 #include<queue>
+#include<algorithm>
 using namespace std;
 
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
 		unordered_set<string> words(wordList.begin(),wordList.end());
-	    queue<string> q;
-		q.push(beginWord);	
+		unordered_set<string> *beginset=new unordered_set<string>();
+		unordered_set<string> *endset=new unordered_set<string>();
 		if(words.find(endWord)==words.end())
 			return 0;
+		beginset->insert(beginWord);
+		endset->insert(endWord);
 		int res=1;
-		while(!q.empty())
+		while(!beginset->empty())
 		{
-			int size=q.size();
+			if(beginset->size()>endset->size())
+				swap(beginset,endset);
 			++res;
-			while(--size>=0)
+			unordered_set<string> *newset=new unordered_set<string>();
+			for(unordered_set<string>::iterator iter=beginset->begin();iter!=beginset->end();++iter)
 			{
-				string tmp=q.front();
-				q.pop();
+				string tmp=*iter;
 				for(int i=0;i<(int)tmp.length();i++)
 				{
-					char origin=tmp[i];
+                    char origin=tmp[i];
 					for(char c='a';c<='z';c++)
 					{
 						tmp[i]=c;
+                        if(endset->find(tmp)!=endset->end())
+							return res;
 						if(words.find(tmp)!=words.end())
 						{
-							if(tmp==endWord)
-								return res;
-							q.push(tmp);
 							words.erase(tmp);
+							newset->insert(tmp);
 						}
 					}
-					tmp[i]=origin;
+                    tmp[i]=origin;
 				}
 			}
+			beginset=newset;
 		}
 		return 0;
     }
