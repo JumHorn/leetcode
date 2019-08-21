22917377dcafeacad978896b3a00448e45645d11
JumHorn <JumHorn@gmail.com>
2019-08-21 22:22:25 +0800

    Minimum Height Trees beats 14.44%

--- Cplus/MinimumHeightTrees.cpp
+++ Cplus/MinimumHeightTrees.cpp
@@ -1,42 +1,43 @@
 #include<vector>
 #include<algorithm>
+#include<unordered_set>
+#include<unordered_map>
 #include<climits>
 using namespace std;
 
 class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-		vector<int> mhts(n);
-		vector<vector<int>> graph(n);
-        vector<int> visited(n);
+        if(edges.empty())
+            return {0};
+		unordered_map<int,unordered_set<int>> graph;
 		for(int i=0;i<(int)edges.size();i++)
 		{
-			graph[edges[i][0]].push_back(edges[i][1]);
-			graph[edges[i][1]].push_back(edges[i][0]);
+			graph[edges[i][0]].insert(edges[i][1]);
+			graph[edges[i][1]].insert(edges[i][0]);
 		}
-		for(int i=0;i<n;i++)
-			mhts[i]=getMinHeight(graph,INT_MAX,i);
-		vector<int> res;
-		res.push_back(0);
-		for(int i=1;i<n;i++)
-		{
-			if(mhts[i]<mhts[res.front()])
-			{
-				res.clear();
-				res.push_back(i);
-			}
-			else if(mhts[i]==mhts[res.front()])
-				res.push_back(i);
-		}
+        vector<int> movenode(n);
+        while(n>2)
+        {
+            int leafnode=0;
+            for(unordered_map<int,unordered_set<int>>::iterator iter=graph.begin();iter!=graph.end();++iter)
+            {
+                if(iter->second.size()==1)
+                {
+                    movenode[leafnode]=iter->first;
+                    leafnode++;
+                }
+            }
+            for(int i=0;i<leafnode;i++)
+            {
+                graph[*graph[movenode[i]].begin()].erase(movenode[i]);
+                graph.erase(movenode[i]);
+            }
+            n-=leafnode;
+        }
+        vector<int> res;
+        for(auto& node:graph)
+            res.push_back(node.first);
 		return res;
     }
-
-	int getMinHeight(vector<vector<int>>& graph,int parent,int current)
-	{
-		int height=0;
-		for(int i=0;i<(int)graph[current].size();i++)
-            if(graph[current][i]!=parent)
-			    height=max(height,1+getMinHeight(graph,current,graph[current][i]));
-		return height;
-	}
 };
