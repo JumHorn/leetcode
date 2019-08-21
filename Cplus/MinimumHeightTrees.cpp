d64fd48b8760d33e3309461fbe9240ed4d876e3b
JumHorn <JumHorn@gmail.com>
2019-08-21 22:43:26 +0800

    Minimum Height Trees beats 19.74%

--- Cplus/MinimumHeightTrees.cpp
+++ Cplus/MinimumHeightTrees.cpp
@@ -1,7 +1,6 @@
 #include<vector>
 #include<algorithm>
 #include<unordered_set>
-#include<unordered_map>
 #include<climits>
 using namespace std;
 
@@ -10,34 +9,37 @@ public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
         if(edges.empty())
             return {0};
-		unordered_map<int,unordered_set<int>> graph;
+		vector<unordered_set<int>> graph(n);
 		for(int i=0;i<(int)edges.size();i++)
 		{
 			graph[edges[i][0]].insert(edges[i][1]);
 			graph[edges[i][1]].insert(edges[i][0]);
 		}
         vector<int> movenode(n);
+        vector<int> leftnode(n);
         while(n>2)
         {
             int leafnode=0;
-            for(unordered_map<int,unordered_set<int>>::iterator iter=graph.begin();iter!=graph.end();++iter)
+            for(int i=0;i<(int)graph.size();i++)
             {
-                if(iter->second.size()==1)
+                if(leftnode[i]!=-1&&graph[i].size()==1)
                 {
-                    movenode[leafnode]=iter->first;
+                    movenode[leafnode]=i;
                     leafnode++;
                 }
             }
             for(int i=0;i<leafnode;i++)
             {
                 graph[*graph[movenode[i]].begin()].erase(movenode[i]);
-                graph.erase(movenode[i]);
+                graph[movenode[i]].clear();
+                leftnode[movenode[i]]=-1;
             }
             n-=leafnode;
         }
         vector<int> res;
-        for(auto& node:graph)
-            res.push_back(node.first);
+        for(int i=0;i<(int)leftnode.size();i++)
+            if(leftnode[i]!=-1)
+                res.push_back(i);
 		return res;
     }
 };
