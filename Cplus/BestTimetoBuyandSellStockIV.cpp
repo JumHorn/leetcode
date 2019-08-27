c24ad01f1c86c727d676997e62e5c749767f4f47
JumHorn <JumHorn@gmail.com>
2019-08-28 06:48:26 +0800

    Best Time to Buy and Sell Stock IV beats 98.27%

---
+++ Cplus/BestTimetoBuyandSellStockIV.cpp
@@ -0,0 +1,34 @@
+#include<vector>
+#include<algorithm>
+#include<cstring>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        if(prices.empty()||k==0)
+            return 0;
+        if(k>(int)prices.size()/2)
+        {
+            int res=0;
+            for(int i=1;i<(int)prices.size();i++)
+                res+=max(prices[i]-prices[i-1],0);
+            return res;
+        }
+		int hold[k],sold[k];
+        memset(sold,0,sizeof(sold));
+        for(int i=0;i<k;i++)
+            hold[i]=-prices[0];
+		for(int i=0;i<(int)prices.size();i++)
+		{
+            hold[0]=max(-prices[i],hold[0]);
+			sold[0]=max(hold[0]+prices[i],sold[0]);
+			for(int j=1;j<k;j++)
+			{
+				hold[j]=max(sold[j-1]-prices[i],hold[j]);
+				sold[j]=max(hold[j]+prices[i],sold[j]);
+			}
+		}
+		return sold[k-1];
+    }
+};
