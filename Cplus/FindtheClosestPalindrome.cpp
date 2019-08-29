32dfde508b07ece9453b2ed095e219645995ba28
JumHorn <JumHorn@gmail.com>
2019-08-29 21:49:58 +0800

    Find the Closest Palindrome beats 100%

--- Cplus/FindtheClosestPalindrome.cpp
+++ Cplus/FindtheClosestPalindrome.cpp
@@ -18,81 +18,49 @@ public:
 
         i=n.size()/2-1+(n.size()&1),j=n.size()/2;
         string tmp(res);
-        if(tmp[i]=='9')
+        while (i>=0&&tmp[i]=='9')
         {
-            while (i>=0&&tmp[i]=='9')
-            {
-                tmp[i]='0';
-                tmp[j]=tmp[i];
-                i--;
-                j++;
-            }
-            if(i<0)
-                res1=pow(10,tmp.length())+1;
-            else
-            {
-                tmp[i]=((tmp[i]-'0')+1)%10;
-                res1=stol(tmp);
-            }
+            tmp[i]='0';
+            tmp[j]=tmp[i];
+            i--;
+            j++;
         }
+        if(i<0)
+            res1=pow(10,tmp.length())+1;
         else
         {
-            if(i==j)
-                tmp[i]+=1;
-            else
-            {
-                tmp[i]+=1;
-                tmp[j]+=1;
-            }
+            tmp[i]=((tmp[i]-'0')+1)%10+'0';
+            tmp[j]=tmp[i];
             res1=stol(tmp);
         }
         abs1=abs(res1-nn);
         
         i=n.size()/2-1+(n.size()&1),j=n.size()/2;
         tmp=res;
-        if(tmp[i]=='0')
+        while (i>=0&&tmp[i]=='0')
         {
-            while (i>=0&&tmp[i]=='0')
-            {
-                tmp[i]='9';
-                tmp[j]=tmp[i];
-                i--;
-                j++;
-            }
-            if(i<0)
-                res2=pow(10,tmp.length())-1;
-            else
-            {
-                tmp[i]=((tmp[i]-'0')-1)%10;
-                res2=stol(tmp);
-            }
+            tmp[i]='9';
+            tmp[j]=tmp[i];
+            i--;
+            j++;
         }
+        if(i<0)
+            res2=pow(10,tmp.length())-1;
         else
         {
-            if(i==j)
-                tmp[i]-=1;
+            tmp[i]=((tmp[i]-'0')-1)%10+'0';
+            if(tmp[i]=='0'&&i==0&&i!=j)
+                tmp[j]='9';
             else
-            {
-                tmp[i]-=1;
-                tmp[j]-=1;
-            }
+                tmp[j]=tmp[i];
             res2=stol(tmp);
         }
         abs2=abs(res2-nn);
 
-        long diff=min(min(abs0,abs1),abs2);
-        if(diff==abs2)
+        if(abs2<=abs1&&abs2<=abs0)
             return to_string(res2);
-        if(diff==abs0)
+        if(abs0<=abs1&&abs0<=abs2)
             return to_string(res0);
 		return to_string(res1);
     }
 };
-
-
-int main()
-{
-    Solution sol;
-    sol.nearestPalindromic("11");
-    return 0;
-}
