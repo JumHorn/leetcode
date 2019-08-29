335b246af4c535552189595b733988d533d555d5
JumHorn <JumHorn@gmail.com>
2019-08-29 22:29:03 +0800

    Poor Pigs beats 50.56%

---
+++ Cplus/PoorPigs.cpp
@@ -0,0 +1,12 @@
+#include<cmath>
+using namespace std;
+
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+		int k=minutesToTest/minutesToDie;	        
+		if(k==0)
+			return -1;
+		return ceil(log(buckets)/log(k+1));
+    }
+};
