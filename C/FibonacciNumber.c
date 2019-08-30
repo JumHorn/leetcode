2bfa15ce9ba85f3a8c6f11f20973477e385fe0e8
JumHorn <JumHorn@gmail.com>
2019-08-30 21:32:02 +0800

    Fibonacci Number beats 100%

--- C/FibonacciNumber.c
+++ C/FibonacciNumber.c
@@ -6,11 +6,15 @@ int fib(int N){
 		return 0;
 	if(N==1)
 		return 1;
-	int *p=(int*)malloc((N+1)*sizeof(int));
-	p[0]=0;
-	p[1]=1;
-	for(int i=2;i<=N;i++)
-		p[i]=p[i-1]+p[i-2];
-	return p[N];
+	if(N==2)
+		return 1;
+	int p0=0,p1=1,p2=1,tmp;
+	for(int i=3;i<=N;i++)
+	{
+		tmp=p1+p2;
+		p0=p1;
+		p1=p2;
+		p2=tmp;
+	}
+	return p2;
 }
-
