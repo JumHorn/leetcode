12c76de02891cbac341ea7e9f9d9cd604ef52344
JumHorn <JumHorn@gmail.com>
2019-09-05 23:17:31 +0800

    Number of Equivalent Domino Pairs beats 97.10%

--- C/NumberofEquivalentDominoPairs.c
+++ C/NumberofEquivalentDominoPairs.c
@@ -1,18 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
 
 bool Equiv(int** dominoes,int i,int j)
 {
-	return (dominoes[i][0]==dominoes[j][0]&&dominoes[i][1]==dominoes[j][1])
-		|| (dominoes[i][0]==dominoes[j][1]&&dominoes[i][1]==dominoes[j][0]);
+	return dominoes[i][0]==dominoes[j][0]&&dominoes[i][1]==dominoes[j][1];
+}
+
+int comp(const void* lhs,const void* rhs)
+{
+	int* a=*(int**)lhs;
+	int* b=*(int**)rhs;
+	if(a[0]!=b[0])
+		return a[0]-b[0];
+	return a[1]-b[1];
 }
 
 int numEquivDominoPairs(int** dominoes, int dominoesSize, int* dominoesColSize){
 	int res=0;
-	for(int i=0;i<dominoesSize-1;i++)
-		for(int j=i+1;j<dominoesSize;j++)
-		{
-			if(Equiv(dominoes,i,j))
-				res++;
-		}
+    for(int i=0;i<dominoesSize;i++)
+    {
+        if(dominoes[i][0]>dominoes[i][1])
+        {
+            dominoes[i][0]^=dominoes[i][1];
+            dominoes[i][1]^=dominoes[i][0];
+            dominoes[i][0]^=dominoes[i][1];
+        }
+    }
+	qsort(dominoes,dominoesSize,sizeof(int*),comp);
+	for(int i=0;i<dominoesSize-1;)
+	{
+		int j=i+1;
+		while(j<dominoesSize&&Equiv(dominoes,i,j))
+			j++;
+		if(j-i>1)
+			res+=(j-i)*(j-i-1)/2;
+		i=j;
+	}
 	return res;
 }
-
