80a6d38966e5c39491a906c828efaaca9ca32423
JumHorn <JumHorn@gmail.com>
2019-09-10 07:01:13 +0800

    Remove All Adjacent Duplicates In String beats 100%

--- C/RemoveAllAdjacentDuplicatesInString.c
+++ C/RemoveAllAdjacentDuplicatesInString.c
@@ -1,42 +1,32 @@
 char * removeDuplicates(char * S){
 	char *p,*q;
-	bool flag=true;
-	while(flag)
-	{
-		p=S;
-		q=S;
-        flag=false;
-		while(*p&&*p=='0')
-			p++;
-        if(!*p)
-            break;
-		q=p+1;
-		while(*q)
-		{
-			while(*q&&*q=='0')
-				q++;
-			if(*q&&*p==*q)
-			{
-				*p='0';
-				*q='0';
-				flag=true;
-				break;
-			}
-			if(*q&&*p!=*q)
-			{
-				p=q;
-				q++;
-			}
-		}
-	}
 	p=S;
-	q=S;
+	q=S+1;
 	while(*q)
 	{
-		if(*q!='0')
-			*p++=*q;
+		if(*p=='0')
+		{
+			*p=*q;
+		}
+		else
+		{
+			if(*p==*q)
+			{
+				if(p==S)
+					*p='0';
+				else
+					p--;
+			}
+			else
+			{
+				p++;
+				*p=*q;
+			}
+		}
 		q++;
 	}
+	if(*p!='0')
+		p++;
 	*p='\0';
 	return S;
 }
