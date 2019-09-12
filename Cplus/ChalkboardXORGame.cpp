9da75929d3eb9b55f5467e1c04011d4aeda8b2cf
JumHorn <JumHorn@gmail.com>
2019-09-12 20:53:46 +0800

    Chalkboard XOR Game beats 98.51%

--- Cplus/ChalkboardXORGame.cpp
+++ Cplus/ChalkboardXORGame.cpp
@@ -6,36 +6,7 @@ public:
     bool xorGame(vector<int>& nums) {
 		int XOR=0;
 		for(int i=0;i<(int)nums.size();i++)
-			XOR^=nums[i];		
-		return xorGame(nums,XOR);
+			XOR^=nums[i];	
+		return XOR==0||nums.size()%2==0;
     }
-
-	bool xorGame(vector<int>& nums,int XOR)
-	{
-		if(XOR==0)
-			return true;
-        bool res=true;
-		for(int i=0;i<(int)nums.size();i++)
-		{
-			if(nums[i]!=-1)
-			{
-				XOR^=nums[i];
-				int tmp=nums[i];
-				nums[i]=-1;
-				if(XOR!=0&&xorGame(nums,XOR))
-					res=false;
-				nums[i]=tmp;
-				XOR^=nums[i];
-			}
-		}
-		return res;
-	}
 };
-
-int main()
-{
-    vector<int> v={1,1,2,3,4};
-    Solution sol;
-    sol.xorGame(v);
-    return 0;
-}
