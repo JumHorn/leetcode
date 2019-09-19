d27f843e5755a7169536688de03e1ff948064962
JumHorn <JumHorn@gmail.com>
2019-09-19 21:59:58 +0800

    Minimum Number of K Consecutive Bit Flips beats 94.74%

--- Cplus/MinimumNumberofKConsecutiveBitFlips.cpp
+++ Cplus/MinimumNumberofKConsecutiveBitFlips.cpp
@@ -4,19 +4,24 @@ using namespace std;
 class Solution {
 public:
     int minKBitFlips(vector<int>& A, int K) {
-		int res=0;
+		int res=0,flip=0;
+        vector<int> dp(A.size()+1);
 		for(int i=0;i<=(int)A.size()-K;i++)
 		{
-			if(A[i]==0)
+            flip+=dp[i];
+			if((A[i]+flip%2)%2==0)
 			{
-				res++;
-				for(int j=i;j<i+K;j++)
-					A[j]=1-A[j];
+                flip++;
+                dp[i+K]--;
+                res++;
 			}
 		}
-		for(int i=0;i<K;i++)
-			if(A[A.size()-i-1]==0)
+		for(int i=A.size()-K+1;i<(int)A.size();i++)
+        {
+            flip+=dp[i];
+			if((A[i]+flip%2)%2==0)
 				return -1;
+        }
 		return res;	
     }
 };
