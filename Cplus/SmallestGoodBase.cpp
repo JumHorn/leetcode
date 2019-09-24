77d15a50e05148b7bc2ffc677d197ebcdae3fc92
JumHorn <JumHorn@gmail.com>
2019-09-24 23:21:55 +0800

    Smallest Good Base beats 100%

---
+++ Cplus/SmallestGoodBase.cpp
@@ -0,0 +1,21 @@
+#include<string>
+#include<cmath>
+using namespace std;
+
+class Solution {
+public:
+    string smallestGoodBase(string n) {
+		unsigned long long S=stoll(n);
+		int max_m=log2(S);
+		for(int m=max_m;m>=2;m--)
+		{
+			int k=pow(S,1.0/m);
+			unsigned long long sum=1,prod=1;
+			for(int i=0;i<m;i++)
+				sum+=(prod*=k);
+			if(sum==S)
+				return to_string(k);
+		}
+		return to_string(S-1);
+    }
+};
