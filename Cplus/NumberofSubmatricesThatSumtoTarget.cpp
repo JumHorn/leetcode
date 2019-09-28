9652fda0db274b80fef28362bac4f6ca59c012b1
JumHorn <JumHorn@gmail.com>
2019-09-29 06:45:17 +0800

    in-place algorithm

--- Cplus/NumberofSubmatricesThatSumtoTarget.cpp
+++ Cplus/NumberofSubmatricesThatSumtoTarget.cpp
@@ -5,20 +5,19 @@ using namespace std;
 class Solution {
 public:
     int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
-		int res=0,M=matrix.size(),N=matrix[0].size();
-		vector<vector<int>> dp(M,vector<int>(N+1));        
+		int res=0,M=matrix.size(),N=matrix[0].size();      
 		for(int i=0;i<M;i++)
-			for(int j=0;j<N;j++)
-				dp[i][j+1]=matrix[i][j]+dp[i][j];
-		for(int i=1;i<=N;i++)
-			for(int j=i;j<=N;j++)
+			for(int j=1;j<N;j++)
+				matrix[i][j]+=matrix[i][j-1];
+		for(int i=0;i<N;i++)
+			for(int j=i;j<N;j++)
 			{
                 unordered_map<int,int> m;
                 m[0]=1;
                 int add=0;
 				for(int k=0;k<M;k++)
                 {
-                    add+=dp[k][j]-dp[k][i-1];
+                    add+=matrix[k][j]-(i>0?matrix[k][i-1]:0);
                     res+=m[add-target];
                     m[add]++;
                 }
