9688f3f3bd333bd18a4f4d5fea9400d58adba4da
JumHorn <JumHorn@gmail.com>
2019-10-05 19:25:32 +0800

    Encode and Decode TinyURL beats 88.75%

---
+++ Cplus/EncodeandDecodeTinyURL.cpp
@@ -0,0 +1,23 @@
+#include<string>
+#include<vector>
+using namespace std;
+
+class Solution {
+	vector<string> v;
+public:
+
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) {
+        v.push_back(longUrl);
+		return to_string(v.size()-1);
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        return v[stoi(shortUrl)];
+    }
+};
+
+// Your Solution object will be instantiated and called as such:
+// Solution solution;
+// solution.decode(solution.encode(url));
