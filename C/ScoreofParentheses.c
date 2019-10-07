9d927e3eaddcffd40587933f60f35600b165e068
JumHorn <JumHorn@gmail.com>
2019-10-08 06:33:25 +0800

    Score of Parentheses beats 100%

---
+++ C/ScoreofParentheses.c
@@ -0,0 +1,26 @@
+
+
+
+int scoreOfParentheses(char * S){
+	int left=0,base=1,res=0;
+	char *top=S;
+	while(*S)
+	{
+		if(*S=='(')
+		{
+			base<<=1;
+			left++;
+			top=S;
+		}
+		else if(*S==')')
+		{
+			base>>=1;
+			left--;
+			if(S-top==1)
+				res+=base;
+		}
+		S++;
+	}
+	return res;
+}
+
