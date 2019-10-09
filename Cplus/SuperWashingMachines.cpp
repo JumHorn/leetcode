e1bc0119f6c61ef88d743a31fba620f81a72229b
JumHorn <JumHorn@gmail.com>
2019-10-09 22:23:33 +0800

    Super Washing Machines beats 75.29%

---
+++ Cplus/SuperWashingMachines.cpp
@@ -0,0 +1,24 @@
+#include<vector>
+#include<numeric>
+#include<cmath>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int findMinMoves(vector<int>& machines) {
+		int N=machines.size();
+		int sum=accumulate(machines.begin(),machines.end(),0);   
+		if(sum%N!=0)
+			return -1;
+		int avg=sum/N,res=0,count=0;
+		for(int i=0;i<(int)machines.size();i++)
+			machines[i]-=avg;
+		for(int i=0;i<(int)machines.size();i++)
+		{
+            count+=machines[i];
+			res=max(res,max(machines[i],abs(count)));	
+		}
+		return res;
+	}
+};
