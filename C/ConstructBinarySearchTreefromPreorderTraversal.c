1850c747197ada7caa53fbdb2a8bd2b077505bd6
JumHorn <JumHorn@gmail.com>
2019-10-18 20:09:23 +0800

    Construct Binary Search Tree from Preorder Traversal beats 72.13

---
+++ C/ConstructBinarySearchTreefromPreorderTraversal.c
@@ -0,0 +1,30 @@
+#include<stdlib.h>
+
+//Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+struct TreeNode* createNode(int val)
+{
+	struct TreeNode* node=(struct TreeNode*)malloc(sizeof(struct TreeNode));
+	node->val=val;
+	node->left=NULL;
+	node->right=NULL;
+	return node;
+}
+
+struct TreeNode* bstFromPreorder(int* preorder, int preorderSize){
+    if(preorderSize==0)
+        return NULL;
+	struct TreeNode* root=createNode(*preorder);
+    int i;
+    for(i=1;i<preorderSize;i++)
+        if(preorder[0]<preorder[i])
+            break;
+    root->left=bstFromPreorder(preorder+1,i-1);
+    root->right=bstFromPreorder(preorder+i,preorderSize-i);
+    return root;
+}
