ae36d0796398df0c17abc6317c9fe053c26845f0
JumHorn <JumHorn@gmail.com>
2019-10-19 22:23:19 +0800

    Sum of Subsequence Widths beats 87.99%

--- Cplus/SumofSubsequenceWidths.cpp
+++ Cplus/SumofSubsequenceWidths.cpp
@@ -6,15 +6,15 @@ using namespace std;
 class Solution {
 public:
     int sumSubseqWidths(vector<int>& A) {
-		int res=0,MOD=1e9+7;
+		int res=0,MOD=1e9+7,N=A.size();
         sort(A.begin(),A.end());
 		long *pow2=new long[A.size()];
 		pow2[0]=1;
-		for(int i=1;i<(int)A.size();i++)
+		for(int i=1;i<N;i++)
 			pow2[i]=pow2[i-1]*2%MOD;
-		for(int i=0;i<(int)A.size()-1;i++)
-			for(int j=i+1;j<(int)A.size();j++)
-				res=(res+pow2[j-i-1]*(A[j]-A[i]))%MOD;
+		for(int i=0;i<N;i++)
+			res=(res+(pow2[i]-pow2[N-i-1])*A[i])%MOD;
 		return res;
     }
 };
+
