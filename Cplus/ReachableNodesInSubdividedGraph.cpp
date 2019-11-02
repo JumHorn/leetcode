09834f2f1cdce2906f70fa607a3233b4dbd9acc1
JumHorn <JumHorn@gmail.com>
2019-11-03 06:25:39 +0800

    dijkstra priority_queue

--- Cplus/ReachableNodesInSubdividedGraph.cpp
+++ Cplus/ReachableNodesInSubdividedGraph.cpp
@@ -1,12 +1,13 @@
 #include<vector>
 #include<algorithm>
 #include<unordered_map>
+#include<queue>
 using namespace std;
 
 class Solution {
 public:
     int reachableNodes(vector<vector<int>>& edges, int M, int N) {
-		vector<unordered_map<int,int>> graph(N,unordered_map<int,int>(N));       
+		vector<unordered_map<int,int>> graph(N);      
 		vector<vector<int>> visited(N,vector<int>(N));
 		vector<int> v(N,-1);
 		for(int i=0;i<(int)edges.size();i++)
@@ -14,33 +15,37 @@ public:
 			graph[edges[i][0]][edges[i][1]]=edges[i][2];
 			graph[edges[i][1]][edges[i][0]]=edges[i][2];
 		}
-		dfs(0,M,graph,visited,v);
+		
 		int res=0;
-		for(int i=0;i<N;i++)
-			if(v[i]>=0)
-				res+=1;
-		for(int i=0;i<N-1;i++)
-			for(int j=i+1;j<N;j++)
-				res+=min(graph[i][j],visited[i][j]+visited[j][i]);
+		//dijkstra
+		priority_queue<pair<int,int>> q;
+		q.push({M,0});
+		while(!q.empty())
+		{
+			int weight=q.top().first;
+			int node=q.top().second;
+			q.pop();
+			if(weight<=v[node])
+				continue;
+			v[node]=weight;
+			++res;
+			for(auto n : graph[node])
+			{
+				if(weight>n.second)
+				{
+					visited[node][n.first]=n.second;
+					q.push({weight-n.second-1,n.first});
+				}
+				else
+					visited[node][n.first]=max(visited[node][n.first],weight);
+			}
+		}
+
+		for(int k=0;k<(int)edges.size();k++)
+        {
+			int i=edges[k][0],j=edges[k][1],w=edges[k][2];
+				res+=min(w,visited[i][j]+visited[j][i]);
+        }
 		return res;
     }
-
-	void dfs(int n,int m,vector<unordered_map<int,int>>& graph,vector<vector<int>>& visited,vector<int>& v)
-	{
-		if(v[n]>=m)
-			return;
-		v[n]=m;
-		if(m==0)
-			return;
-		for(auto iter : graph[n])
-		{
-			if(m>iter.second)
-			{
-				visited[n][iter.first]=iter.second;
-				dfs(iter.first,m-iter.second-1,graph,visited,v);
-			}
-			else
-				visited[n][iter.first]=max(visited[n][iter.first],m);
-		}
-	}
 };
