11cb28b832ba1d5c3b70c7453a626a004c3ac37a
JumHorn <JumHorn@gmail.com>
2019-11-07 07:00:05 +0800

    K Closest Points to Origin beats 81.86%

---
+++ Cplus/KClosestPointstoOrigin.cpp
@@ -0,0 +1,19 @@
+#include<vector>
+#include<queue>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+		priority_queue<pair<int,int>> q;
+		for(int i=0;i<(int)points.size();i++)
+			q.push({-(points[i][0]*points[i][0]+points[i][1]*points[i][1]),i});
+		vector<vector<int>> res(K);
+		for(int i=0;i<K;i++)
+		{
+			res[i]=points[q.top().second];
+			q.pop();
+		}
+		return res;
+    }
+};
