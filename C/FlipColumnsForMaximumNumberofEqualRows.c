5b3a4ac9e7f4d154aed060548050d53c0cf70152
JumHorn <JumHorn@gmail.com>
2019-11-07 06:42:59 +0800

    Flip Columns For Maximum Number of Equal Rows beats 37.93%

--- C/FlipColumnsForMaximumNumberofEqualRows.c
+++ C/FlipColumnsForMaximumNumberofEqualRows.c
@@ -1,30 +1,38 @@
 #include<stdlib.h>
 
+int arrayEquals(int* arr1,int* arr2,int size)
+{
+	for(int i=0;i<size;i++)
+		if(arr1[i]!=arr2[i])
+			return 0;
+	return 1;
+}
 
 int maxEqualRowsAfterFlips(int** matrix, int matrixSize, int* matrixColSize){
-	int* p=(int*)malloc(matrixSize*sizeof(int));
-	for(int i=0;i<matrixSize;i++)
-		p[i]=matrix[i][0];
+	int* p=(int*)malloc(*matrixColSize*sizeof(int));
+	int* visited=(int*)calloc(matrixSize,sizeof(int));
 	int res=0;
-	for(int j=1;j<*matrixColSize;j++)
-	{
-		int count=0,reverse=0;
-		for(int i=0;i<matrixSize;i++)
-		{
-			if(matrix[i][j]==p[i])
-				count++;
-			if(1-matrix[i][j]==p[i])
-				reverse++;
-		}
-		if(reverse>count)
-			for(int i=0;i<matrixSize;i++)
-				matrix[i][j]=1-matrix[i][j];
-		for(int i=0;i<matrixSize;i++)
-			if(matrix[i][j]!=p[i])
-				p[i]=-1;
-	}
 	for(int i=0;i<matrixSize;i++)
-		if(p[i]>=0)
-			res++;
+	{
+		if(visited[i]!=0)
+			continue;
+		for(int j=0;j<*matrixColSize;j++)
+			p[j]=1-matrix[i][j];
+		int count=0;
+		for(int j=0;j<matrixSize;j++)
+		{
+			if(i==j)
+			{
+				count++;
+				visited[i]=1;
+			}
+			if(visited[i]!=0)
+				continue;
+			if(arrayEquals(matrix[i],matrix[j],*matrixColSize)>0||arrayEquals(matrix[j],p,*matrixColSize)>0)
+				count++;
+		}
+		if(count>res)
+			res=count;
+	}
 	return res;
 }
