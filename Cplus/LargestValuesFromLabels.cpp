43fd846ec751c219e37cc4fd15aafa3c97a822d5
JumHorn <JumHorn@gmail.com>
2019-11-08 06:59:29 +0800

    Largest Values From Labels beats 61.42%

---
+++ Cplus/LargestValuesFromLabels.cpp
@@ -0,0 +1,25 @@
+#include<vector>
+#include<queue>
+#include<unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int largestValsFromLabels(vector<int>& values, vector<int>& labels, int num_wanted, int use_limit) {
+		priority_queue<pair<int,int>> q;
+		unordered_map<int,int> m;
+        for(int i=0;i<(int)values.size();i++)
+			q.push({values[i],labels[i]});
+		int res=0;
+		while(!q.empty()&&num_wanted>0)
+		{
+			if(++m[q.top().second]<=use_limit)
+			{
+				res+=q.top().first;
+				num_wanted--;
+			}
+			q.pop();
+		}
+		return res;
+    }
+};
