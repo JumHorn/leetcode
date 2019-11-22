e50a2b5bfe4de38b21a22e62ba00375b01ea8112
JumHorn <JumHorn@gmail.com>
2019-11-22 14:33:10 +0800

    Evaluate Reverse Polish Notation beats 89.67%

---
+++ Cplus/EvaluateReversePolishNotation.cpp
@@ -0,0 +1,55 @@
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+	int evalRPN(vector<string> &tokens)
+	{
+		stack<int> s;
+		int first, second;
+		for (int i = 0; i < (int)tokens.size(); i++)
+		{
+			if (tokens[i] == "+")
+			{
+				first = s.top();
+				s.pop();
+				second = s.top();
+				s.pop();
+				s.push(first + second);
+			}
+			else if (tokens[i] == "-")
+			{
+				first = s.top();
+				s.pop();
+				second = s.top();
+				s.pop();
+				s.push(second - first);
+			}
+			else if (tokens[i] == "*")
+			{
+				first = s.top();
+				s.pop();
+				second = s.top();
+				s.pop();
+				s.push(first * second);
+			}
+			else if (tokens[i] == "/")
+			{
+				first = s.top();
+				s.pop();
+				second = s.top();
+				s.pop();
+				s.push(second / first);
+			}
+			else
+			{
+				s.push(stoi(tokens[i]));
+			}
+		}
+		return s.top();
+	}
+};
+
