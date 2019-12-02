70ce5219b6eb36c147ef5c877801cae5dad94e63
JumHorn <JumHorn@gmail.com>
2019-12-02 23:00:35 +0800

    Can Make Palindrome from Substring beats 40.74%

--- C/CanMakePalindromefromSubstring.c
+++ C/CanMakePalindromefromSubstring.c
@@ -8,23 +8,29 @@ bool* canMakePaliQueries(char* s, int** queries, int queriesSize, int* queriesCo
 {
 	bool* res = (bool*)malloc(queriesSize * sizeof(bool));
 	*returnSize = queriesSize;
+	int len = strlen(s);
+	int** dp = (int**)malloc((len + 1) * sizeof(int*));
+	for (int i = 0; i <= len; i++)
+		dp[i] = (int*)malloc(26 * sizeof(int));
+	memset(dp[0], 0, sizeof(int) * 26);
+	for (int i = 0; i < len; i++)
+	{
+		memcpy(dp[i + 1], dp[i], sizeof(int) * 26);
+		++dp[i + 1][s[i] - 'a'];
+	}
+
 	for (int i = 0; i < queriesSize; i++)
 	{
-		char map[26] = {0};
-		for (int j = queries[i][0]; j <= queries[i][1]; j++)
-			++map[s[j] - 'a'];
+		int map[26];
+		for (int j = 0; j < 26; j++)
+			map[j] = dp[queries[i][1] + 1][j] - dp[queries[i][0]][j];
 		int r = 0;
 		for (int j = 0; j < 26; j++)
 			r += (map[j] & 1);
 		if ((queries[i][1] - queries[i][0]) % 2 == 0)
 			r -= 1;
 		r /= 2;
-		if (r <= queries[i][2])
-			res[i] = true;
-		else
-			res[i] = false;
+		res[i] = (queries[i][2] >= r);
 	}
-
 	return res;
 }
-
