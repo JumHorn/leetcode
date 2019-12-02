022d1a859320854994d6885616cb02f132922e8b
JumHorn <JumHorn@gmail.com>
2019-12-02 23:29:46 +0800

    Can Make Palindrome from Substring beats 92.59%

--- C/CanMakePalindromefromSubstring.c
+++ C/CanMakePalindromefromSubstring.c
@@ -4,33 +4,36 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+
+int bitReverse(int num, int bit)
+{
+	return ((~num) & (1u << bit)) | (num & (~(1u << bit)));
+}
+
+int bitCount(int x)
+{
+	x -= ((x >> 1) & 0x55555555);
+	x = (x & 0x33333333) + ((x >> 2) & 0x33333333);
+	x = (x + (x >> 4)) & 0x0F0F0F0F;
+	x += x >> 8;
+	x += x >> 16;
+	return x & 0x3F;
+}
+
 bool* canMakePaliQueries(char* s, int** queries, int queriesSize, int* queriesColSize, int* returnSize)
 {
 	bool* res = (bool*)malloc(queriesSize * sizeof(bool));
 	*returnSize = queriesSize;
 	int len = strlen(s);
-	int** dp = (int**)malloc((len + 1) * sizeof(int*));
-	for (int i = 0; i <= len; i++)
-		dp[i] = (int*)malloc(26 * sizeof(int));
-	memset(dp[0], 0, sizeof(int) * 26);
+	int* dp = (int*)malloc((len + 1) * sizeof(int));
+	dp[0] = 0;
 	for (int i = 0; i < len; i++)
-	{
-		memcpy(dp[i + 1], dp[i], sizeof(int) * 26);
-		++dp[i + 1][s[i] - 'a'];
-	}
+		dp[i + 1] = (dp[i] ^ (1 << (s[i] - 'a')));
 
 	for (int i = 0; i < queriesSize; i++)
 	{
-		int map[26];
-		for (int j = 0; j < 26; j++)
-			map[j] = dp[queries[i][1] + 1][j] - dp[queries[i][0]][j];
-		int r = 0;
-		for (int j = 0; j < 26; j++)
-			r += (map[j] & 1);
-		if ((queries[i][1] - queries[i][0]) % 2 == 0)
-			r -= 1;
-		r /= 2;
-		res[i] = (queries[i][2] >= r);
+		int r = bitCount(dp[queries[i][1] + 1] ^ dp[queries[i][0]]);
+		res[i] = (queries[i][2] >= (r >> 1));
 	}
 	return res;
 }
