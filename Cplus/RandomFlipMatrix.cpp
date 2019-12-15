7ae90c315543d5d1977b049dcc89bf3fe0900f38
JumHorn <JumHorn@gmail.com>
2019-12-16 07:39:27 +0800

    Random Flip Matrix beats 62.76%

---
+++ Cplus/RandomFlipMatrix.cpp
@@ -0,0 +1,54 @@
+#include <cstdlib>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+private:
+	int rows;
+	int cols;
+	int total;
+	unordered_map<int, int> m;
+
+public:
+	Solution(int n_rows, int n_cols)
+	{
+		rows = n_rows;
+		cols = n_cols;
+		total = n_rows * n_cols;
+	}
+
+	vector<int> flip()
+	{
+		int i = rand() % total--, res = i;
+		if (m.find(i) != m.end())
+			res = m[i];
+		m[i] = total;
+		if (m.find(total) != m.end())
+			m[i] = m[total];
+		m[total] = res;
+		return {res / cols, res % cols};
+	}
+
+	void reset()
+	{
+		total = rows * cols;
+	}
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(n_rows, n_cols);
+ * vector<int> param_1 = obj->flip();
+ * obj->reset();
+ */
+
+int main()
+{
+	Solution sol(2, 2);
+	sol.flip();
+	sol.flip();
+	sol.flip();
+	sol.flip();
+}
