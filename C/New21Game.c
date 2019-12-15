5b2b661bf9e7c67015ee67098e4bfe2e12ba51df
JumHorn <JumHorn@gmail.com>
2019-12-15 13:21:32 +0800

    New 21 Game beats 25.00%

--- C/New21Game.c
+++ C/New21Game.c
@@ -6,20 +6,16 @@ double new21Game(int N, int K, int W)
 		return 1.0;
 	double* dp = (double*)malloc(sizeof(double) * (N + 1));
 	dp[0] = 1.0;
-	double res = 0.0;
+	double sum = 1.0, res = 0.0;
 	for (int i = 1; i <= N; i++)
 	{
-		dp[i] = 0.0;
-		int min = 0;
-		if (i - W > min)
-			min = i - W;
-		for (int j = i - 1; j > min; j--)
-			dp[i] += dp[j];
-		for (int j = i; j > K; j--)
-			dp[i] -= dp[i - (j - K)];
-		dp[i] /= W;
+		dp[i] = sum / W;
 		if (i >= K)
 			res += dp[i];
+		else
+			sum += dp[i];
+		if (i >= W)
+			sum -= dp[i - W];
 	}
 	return res;
 }
