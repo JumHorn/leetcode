37daddb2194eddf89c791549a13e52379a0f793a
JumHorn <JumHorn@gmail.com>
2019-12-19 22:45:23 +0800

    Smallest Range II beats 100%

--- C/SmallestRangeII.c
+++ C/SmallestRangeII.c
@@ -1,37 +1,24 @@
-#include <limits.h>
-#include <math.h>
+#include <stdlib.h>
 
-int dfs(int *A, int ASize, int min, int max, int K, int res)
+int cmp(const void* lhs, const void* rhs)
 {
-	if (max - min >= res)
-		return res;
-	if (ASize == 0)
-		return max - min;
-	int tmp = 0;
-	if (*A - K < min)
-		tmp = dfs(A + 1, ASize - 1, *A - K, max, K, res);
-	else if (*A - K > max)
-		tmp = dfs(A + 1, ASize - 1, min, *A - K, K, res);
-	else
-		tmp = dfs(A + 1, ASize - 1, min, max, K, res);
-	if (tmp < res)
-		res = tmp;
-
-	if (*A + K < min)
-		tmp = dfs(A + 1, ASize - 1, *A + K, max, K, res);
-	else if (*A + K > max)
-		tmp = dfs(A + 1, ASize - 1, min, *A + K, K, res);
-	else
-		tmp = dfs(A + 1, ASize - 1, min, max, K, res);
-	if (tmp < res)
-		res = tmp;
-	return res;
+	return *(int*)lhs - *(int*)rhs;
 }
 
-int smallestRangeII(int *A, int ASize, int K)
+int smallestRangeII(int* A, int ASize, int K)
 {
-	int res = INT_MAX, tmp;
-	res = dfs(A + 1, ASize - 1, A[0] - K, A[0] - K, K, res);
-	tmp = dfs(A + 1, ASize - 1, A[0] + K, A[0] + K, K, res);
-	return res < tmp ? res : tmp;
+	qsort(A, ASize, sizeof(int), cmp);
+	int res = A[ASize - 1] - A[0], max, min;
+	for (int i = 0; i < ASize - 1; i++)
+	{
+		max = A[ASize - 1] - K;
+		if (A[i] + K > max)
+			max = A[i] + K;
+		min = A[0] + K;
+		if (A[i + 1] - K < min)
+			min = A[i + 1] - K;
+		if (max - min < res)
+			res = max - min;
+	}
+	return res;
 }
