aaec65f820ad101621f1412a8f67d37cf34b65e7
JumHorn <JumHorn@gmail.com>
2019-12-20 20:32:50 +0800

    Longest Well-Performing Interval beats 12.50%

--- C/LongestWell-PerformingInterval.c
+++ C/LongestWell-PerformingInterval.c
@@ -1,76 +1,35 @@
-
+#define max(a, b) (((a) > (b)) ? (a) : (b))
 
 int longestWPI(int *hours, int hoursSize)
 {
-	int well = 0, i = 0, j = -1, res = 0;
-	for (i = 0; i < hoursSize; i++)
+	for (int i = 0; i < hoursSize; i++)
 	{
 		if (hours[i] > 8)
-			well += 1;
+			hours[i] = 1;
 		else
-			well -= 1;
-		if (well > 0)
-		{
-			if (res < i - j)
-				res = i - j;
-			if (i - j + well - 1 <= hoursSize)
-			{
-				if (res < i - j + well - 1)
-					res = i - j + well - 1;
-			}
-			else
-				return hoursSize;
-		}
-		else if (well < 0)
-		{
-			while (well < 0)
-			{
-				++j;
-				if (hours[j] > 8)
-					well -= 1;
-				else
-					well += 1;
-			}
-		}
+			hours[i] = -1;
 	}
-	j = hoursSize;
-	well = 0;
-	for (i = hoursSize - 1; i >= 0; i--)
+	for (int i = 1; i < hoursSize; i++)
+		hours[i] += hours[i - 1];
+	int res = 0;
+	for (int i = 0; i < hoursSize; i++)
 	{
-		if (hours[i] > 8)
-			well += 1;
-		else
-			well -= 1;
-		if (well > 0)
+		if (hours[i] > 0)
 		{
-			if (res < j - i)
-				res = j - i;
-			if (j - i + well - 1 < hoursSize)
-			{
-				if (res < j - i + well - 1)
-					res = j - i + well - 1;
-			}
-			else
-				return hoursSize;
+			res = max(res, i + 1);
+			continue;
 		}
-		else if (well < 0)
+		for (int j = 0; j < i; j++)
 		{
-			while (well < 0)
+			if (res > i - j)
+				break;
+			if (hours[i] - hours[j] > 0)
 			{
-				--j;
-				if (hours[j] > 8)
-					well -= 1;
-				else
-					well += 1;
+				res = i - j;
+				break;
 			}
 		}
 	}
 	return res;
 }
 
-int main()
-{
-	int hours[] = {8, 12, 7, 6, 10, 10, 9, 11, 12, 6};
-	longestWPI(hours, 10);
-	return 0;
-}
