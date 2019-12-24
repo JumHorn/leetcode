38cb2c9028431d7d05de4fcf8f780fd0803c6095
JumHorn <JumHorn@gmail.com>
2019-12-24 12:12:13 +0800

    Lowest Common Ancestor of Deepest Leaves beats 11.54%

--- C/LowestCommonAncestorofDeepestLeaves.c
+++ C/LowestCommonAncestorofDeepestLeaves.c
@@ -15,26 +15,19 @@ int height(struct TreeNode* root)
 	return 1 + max(height(root->left), height(root->right));
 }
 
-bool postorder(struct TreeNode* root, int layer, int maxheight)
+struct TreeNode* postorder(struct TreeNode* root, int layer, int maxheight)
 {
 	if (!root)
-		return false;
+		return root;
 	if (layer == maxheight)
-		return true;
-	postorder(root->left, layer + 1, maxheight);
-	postorder(root->right, layer + 1, maxheight);
-	return true;
-}
-
-struct TreeNode* deepest(struct TreeNode* root, int maxheight)
-{
-	int leftheight = height(root->left);
-	int rightheight = height(root->right);
-	if (leftheight < maxheight)
-		return deepest(root->right, maxheight - 1);
-	if (rightheight < maxheight)
-		return deepest(root->left, maxheight - 1);
-	return root;
+		return root;
+	struct TreeNode* leftnode = postorder(root->left, layer + 1, maxheight);
+	struct TreeNode* rightnode = postorder(root->right, layer + 1, maxheight);
+	if (!leftnode && !rightnode)
+		return root;
+	if (!leftnode)
+		return leftnode;
+	return rightnode;
 }
 
 struct TreeNode* lcaDeepestLeaves(struct TreeNode* root)
@@ -42,6 +35,5 @@ struct TreeNode* lcaDeepestLeaves(struct TreeNode* root)
 	if (!root)
 		return root;
 	int h = height(root);
-	return deepest(root, h - 1);
+	return postorder(root, 1, h);
 }
-
