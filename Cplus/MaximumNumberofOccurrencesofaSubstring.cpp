776df00d75e4c8ecd5cfd8768d220b77a704c325
JumHorn <JumHorn@gmail.com>
2020-01-02 08:05:57 +0800

    Maximum Number of Occurrences of a Substring beats 39.35%

--- Cplus/MaximumNumberofOccurrencesofaSubstring.cpp
+++ Cplus/MaximumNumberofOccurrencesofaSubstring.cpp
@@ -10,23 +10,29 @@ class Solution
 public:
 	int maxFreq(string s, int maxLetters, int minSize, int maxSize)
 	{
-		unordered_map<char, int> maxchar, minchar;
+		int minchar[26] = {0}, mincount = 0;
 		unordered_map<string, int> strmap;
 		int res = 0;
 		for (int i = 0; i < minSize - 1; i++)
-			++minchar[s[i]];
+			if (++minchar[s[i] - 'a'] == 1)
+				++mincount;
 		for (int i = minSize - 1; i < (int)s.length(); i++)
 		{
-			++minchar[s[i]];
-			if ((int)minchar.size() <= maxLetters)
+			if (++minchar[s[i] - 'a'] == 1)
+				++mincount;
+			if (mincount <= maxLetters)
 			{
 				res = max(res, ++strmap[s.substr(i - minSize + 1, minSize)]);
-				unordered_map<char, int> maxchar = minchar;
+				int maxchar[26] = {0};
+				int maxcount = mincount;
+				memcpy((char*)&maxchar, (char*)&minchar, 26 * sizeof(int));
 				for (int j = i + 1; j < (int)s.length(); j++)
 				{
 					if (j - i + minSize < maxSize)
 					{
-						if (++maxchar[s[j]] <= maxLetters)
+						if (++maxchar[s[j] - 'a'] == 1)
+							++maxcount;
+						if (maxcount <= maxLetters)
 							res = max(res, ++strmap[s.substr(i - minSize + 1, minSize + j - i)]);
 						else
 							break;
@@ -35,8 +41,8 @@ public:
 						break;
 				}
 			}
-			if (--minchar[s[i - minSize + 1]] == 0)
-				minchar.erase(s[i - minSize + 1]);
+			if (--minchar[s[i - minSize + 1] - 'a'] == 0)
+				--mincount;
 		}
 		return res;
 	}
