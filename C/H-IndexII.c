218663b448f1eb7a626ffd1891032a3764ea04f1
JumHorn <JumHorn@gmail.com>
2020-01-06 07:55:15 +0800

    H-IndexII beats 70.00%

---
+++ C/H-IndexII.c
@@ -0,0 +1,12 @@
+
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+
+int hIndex(int* citations, int citationsSize)
+{
+	int res = 0;
+	for (int i = citationsSize - 1; i >= 0; i--)
+		res = max(res, min(citations[i], citationsSize - i));
+	return res;
+}
+
