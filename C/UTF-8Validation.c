b83cdbe6362fce9db5df0e3f606b0caa7fd87621
JumHorn <JumHorn@gmail.com>
2020-01-07 08:44:47 +0800

    UTF-8 Validation beats 88.89%

---
+++ C/UTF-8Validation.c
@@ -0,0 +1,32 @@
+
+#define HEAD0 128  //(1<<7)
+#define HEAD1 192  //(1<<7)|(1<<6)
+#define HEAD2 224  //(1<<7)|(1<<6)|(1<<5)
+#define HEAD3 240  //(1<<7)|(1<<6)|(1<<5)|(1<<4)
+#define HEAD4 248  //(1<<7)|(1<<6)|(1<<5)|(1<<4)|(1<<3)
+
+bool validUtf8(int* data, int dataSize)
+{
+	int i = 0, len = 0;
+	while (i < dataSize)
+	{
+		if ((data[i] & HEAD0) == 0)
+			len = 1;
+		else if ((data[i] & HEAD2) == HEAD1)
+			len = 2;
+		else if ((data[i] & HEAD3) == HEAD2)
+			len = 3;
+		else if ((data[i] & HEAD4) == HEAD3)
+			len = 4;
+		else
+			return false;
+		if (len + i > dataSize)
+			return false;
+		for (int j = i + 1; j < len + i; j++)
+			if ((data[j] & HEAD1) != HEAD0)
+				return false;
+		i += len;
+	}
+	return true;
+}
+
