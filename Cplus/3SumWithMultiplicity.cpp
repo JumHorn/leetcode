7f85a549858a98a2f082b125eabd00462f5c54a4
JumHorn <JumHorn@gmail.com>
2020-01-18 21:45:52 +0800

    3Sum With Multiplicity beats 80.83%

--- Cplus/3SumWithMultiplicity.cpp
+++ Cplus/3SumWithMultiplicity.cpp
@@ -9,31 +9,27 @@ public:
 	{
 		if ((int)A.size() < 3)
 			return 0;
-		vector<unordered_map<int, int>> dp(A.size());
-		for (int i = 1; i < (int)A.size(); i++)
-			for (int j = 0; j < i; j++)
+		vector<long> v(101);
+		for (int i = 0; i < (int)A.size(); i++)
+			++v[A[i]];
+		long res = 0;
+		for (int i = 0; i < (int)v.size(); i++)
+			for (int j = 0; j < (int)v.size(); j++)
 			{
-				int sum = A[i] + A[j];
-				if (dp[i][sum] + 1 == MOD)
-					dp[i][sum] = 0;
-				else
-					++dp[i][sum];
+				int k = target - i - j;
+				if (k < 0 || k > 100 || v[k] == 0 || v[i] == 0 || v[j] == 0)
+					continue;
+				if (i == j && j == k)
+					res += v[i] * (v[i] - 1) * (v[i] - 2) / 6 % MOD;
+				else if (i == j && j != k)
+					res += v[i] * (v[i] - 1) / 2 * v[k] % MOD;
+				else if (i < j && j < k)
+					res += v[i] * v[j] * v[k] % MOD;
 			}
-		int res = 0;
-		for (int i = 2; i < (int)A.size(); i++)
-			for (int j = 1; j < i; j++)
-				res = (res + dp[j][target - A[i]]) % MOD;
-		return res;
+		return res % MOD;
 	}
 
 private:
 	static const int MOD = 1e9 + 7;
 };
 
-int main()
-{
-	vector<int> v = {1, 1, 2, 2, 2, 2};
-	Solution sol;
-	sol.threeSumMulti(v, 5);
-	return 0;
-}
