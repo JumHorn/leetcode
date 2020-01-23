3fa15f8d68d66dacaa8ef2cba8737788ba196bed
JumHorn <JumHorn@gmail.com>
2020-01-23 20:20:21 +0800

    Convert Binary Number in a Linked List to Integer beats 57.43%

---
+++ C/ConvertBinaryNumberinaLinkedListtoInteger.c
@@ -0,0 +1,20 @@
+
+
+//Definition for singly-linked list.
+struct ListNode
+{
+	int val;
+	struct ListNode* next;
+};
+
+int getDecimalValue(struct ListNode* head)
+{
+	int res = 0;
+	while (head)
+	{
+		res = ((res << 1) | head->val);
+		head = head->next;
+	}
+	return res;
+}
+
