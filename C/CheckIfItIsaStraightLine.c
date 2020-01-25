6d7a301cb2655006eee210007252beb5d9c4944f
JumHorn <JumHorn@gmail.com>
2020-01-25 20:44:31 +0800

    Check If It Is a Straight Line beats 92.31%

---
+++ C/CheckIfItIsaStraightLine.c
@@ -0,0 +1,13 @@
+
+
+bool checkStraightLine(int** coordinates, int coordinatesSize, int* coordinatesColSize)
+{
+	for (int i = 2; i < coordinatesSize; i++)
+	{
+		if ((coordinates[i][1] - coordinates[i - 2][1]) * (coordinates[i - 1][0] - coordinates[i - 2][0]) !=
+			(coordinates[i - 1][1] - coordinates[i - 2][1]) * (coordinates[i][0] - coordinates[i - 2][0]))
+			return false;
+	}
+	return true;
+}
+
