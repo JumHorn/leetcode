7047e44ef09b4f9b60f0381b71997f17d7354158
JumHorn <JumHorn@gmail.com>
2020-01-27 09:49:22 +0800

    Find the City With the Smallest Number of Neighbors at a Threshold Distance beats 66.67%

--- Cplus/FindtheCityWiththeSmallestNumberofNeighborsataThresholdDistance.cpp
+++ Cplus/FindtheCityWiththeSmallestNumberofNeighborsataThresholdDistance.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <unordered_set>
 #include <vector>
 using namespace std;
@@ -7,21 +8,25 @@ class Solution
 public:
 	int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold)
 	{
-		vector<vector<int>> graph(n, vector<int>(n));
+		vector<vector<int>> graph(n, vector<int>(n, 1e9));
 		for (int i = 0; i < (int)edges.size(); i++)
 		{
 			graph[edges[i][0]][edges[i][1]] = edges[i][2];
 			graph[edges[i][1]][edges[i][0]] = edges[i][2];
 		}
-		int res = n - 1, count = 0;
-		unordered_set<int> visited;
-		dfs(graph, n - 1, visited, distanceThreshold);
-		count = visited.size();
-		for (int i = n - 2; i >= 0; i--)
+		for (int i = 0; i < n; i++)
+			graph[i][i] = 0;
+		for (int k = 0; k < n; k++)
+			for (int i = 0; i < n; i++)
+				for (int j = 0; j < n; j++)
+					graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j]);
+		int res = -1, count = INT_MAX;
+		for (int i = n - 1; i >= 0; i--)
 		{
-			visited.clear();
-			dfs(graph, i, visited, distanceThreshold);
-			int tmp = visited.size();
+			int tmp = 0;
+			for (int j = 0; j < n; j++)
+				if (graph[i][j] <= distanceThreshold)
+					tmp++;
 			if (tmp < count)
 			{
 				count = tmp;
@@ -29,19 +34,5 @@ public:
 			}
 		}
 		return res;
-	}
-
-	void dfs(vector<vector<int>>& graph, int n, unordered_set<int>& visited, int d)
-	{
-		if (d < 0)
-			return;
-		if (visited.find(n) != visited.end())
-			return;
-		visited.insert(n);
-		for (int i = 0; i < (int)graph[n].size(); i++)
-		{
-			if (graph[n][i] != 0)
-				dfs(graph, i, visited, d - graph[n][i]);
-		}
 	}
 };
