731b3c67a4e95a65aaa146cb88140636b3708695
JumHorn <JumHorn@gmail.com>
2020-02-02 19:48:57 +0800

    Jump Game V beats 100%

--- Cplus/JumpGameV.cpp
+++ Cplus/JumpGameV.cpp
@@ -6,46 +6,51 @@ using namespace std;
 class Solution
 {
 public:
-	int maxJumps(vector<int>& arr, int d)
+	int maxJumps(vector<int> &arr, int d)
 	{
-		vector<unordered_set<int>> dp(arr.size());
+		vector<int> dp(arr.size());
 		for (int i = 0; i < (int)arr.size(); i++)
 			dfs(arr, d, i, dp);
 		int res = 1;
 		for (int i = 0; i < (int)dp.size(); i++)
-			res = max(res, (int)dp[i].size());
+			res = max(res, dp[i]);
 		return res;
 	}
 
-	void dfs(vector<int>& arr, int d, int index, vector<unordered_set<int>>& dp)
+	void dfs(vector<int> &arr, int d, int index, vector<int> &dp)
 	{
-		if (!dp[index].empty())
+		if (dp[index] != 0)
 			return;
-		int left = index, right = index;
-		while (--left >= max(0, index - d))
+		int left = index - 1, right = index + 1;
+		while (left >= max(0, index - d))
 		{
-			if (arr[left] > arr[index])
-			{
-				++left;
+			if (arr[left] >= arr[index])
 				break;
-			}
-			if (dp[left].empty())
+			if (dp[left] == 0)
 				dfs(arr, d, left, dp);
+			--left;
 		}
-		while (++right <= min((int)arr.size(), index + d))
+		left++;
+		while (right <= min((int)arr.size() - 1, index + d))
 		{
-			if (arr[right] > arr[index])
-			{
-				--right;
+			if (arr[right] >= arr[index])
 				break;
-			}
-			if (dp[right].empty())
+			if (dp[right] == 0)
 				dfs(arr, d, right, dp);
+			++right;
 		}
+		right--;
 		int i = left, left_n = 0, j = right, right_n = 0;
 		while (i < index)
 		{
-			if ((int)dp[i].size() > left_n)
+			left_n = max(left_n, dp[i]);
+			i++;
 		}
+		while (j > index)
+		{
+			right_n = max(right_n, dp[j]);
+			j--;
+		}
+		dp[index] = 1 + max(left_n, right_n);
 	}
 };
