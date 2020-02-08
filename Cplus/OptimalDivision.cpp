cb21c3061cd47bda12139fe9393d671279cabc10
JumHorn <JumHorn@gmail.com>
2020-02-08 11:06:27 +0800

    Optimal Division beats 100%

---
+++ Cplus/OptimalDivision.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+	string optimalDivision(vector<int>& nums)
+	{
+		string res = to_string(nums[0]);
+		if (nums.size() == 1)
+			return res;
+		if (nums.size() == 2)
+			return res + "/" + to_string(nums[1]);
+		res += "/(";
+		for (int i = 1; i < (int)nums.size() - 1; i++)
+		{
+			res += to_string(nums[i]);
+			res += "/";
+		}
+		res += to_string(nums.back()) + ")";
+		return res;
+	}
+};
