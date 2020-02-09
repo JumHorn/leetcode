112a6dd6f988e412d28a979548c7ffa7c19221e8
JumHorn <JumHorn@gmail.com>
2020-02-09 08:27:39 +0800

    Beautiful Arrangement II beats 67.82%

--- Cplus/BeautifulArrangementII.cpp
+++ Cplus/BeautifulArrangementII.cpp
@@ -8,45 +8,27 @@ class Solution
 public:
 	vector<int> constructArray(int n, int k)
 	{
-		unordered_set<int> s;
-		vector<int> seen(n + 1), res(n);
-		for (int i = 1; i <= n; i++)
+		vector<int> res(n);
+		int i = 2, j = n, index = 0;
+		res[0] = 1;
+		while (--k > 0)
 		{
-			seen[i] = 1;
-			res[0] = i;
-			s.clear();
-			if (backTrace(res, seen, s, 1, i, n, k))
-				break;
-			seen[i] = 0;
+			if (abs(res[index] - i) >= abs(res[index] - j))
+				res[++index] = i++;
+			else
+				res[++index] = j--;
+		}
+		if (index < n - 1)
+		{
+			int symbol = 1;
+			if (res[index] - j == 1)
+				symbol = -1;
+			while (index < n - 1)
+			{
+				res[index + 1] = res[index] + symbol;
+				++index;
+			}
 		}
 		return res;
 	}
-
-	bool backTrace(vector<int> &res, vector<int> &seen, unordered_set<int> &s, int index, int num, int n, int k)
-	{
-		if ((int)s.size() > k)
-			return false;
-		if (index == n)
-			return (int)s.size() == k;
-		for (int i = 1; i <= n; i++)
-		{
-			if (seen[i] == 1)
-				continue;
-			seen[i] = 1;
-			s.insert(abs(i - num));
-			res[index] = i;
-			if (backTrace(res, seen, s, index + 1, i, n, k))
-				return true;
-			s.erase(abs(i - num));
-			seen[i] = 0;
-		}
-		return false;
-	}
 };
-
-int main()
-{
-	Solution sol;
-	sol.constructArray(3, 2);
-	return 0;
-}
