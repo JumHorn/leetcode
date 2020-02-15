5e9a74a52f1b132ef734c3f4b686cee1cb0257c0
JumHorn <JumHorn@gmail.com>
2020-02-15 09:26:32 +0800

    Complete Binary Tree Inserter beats 97.41%

---
+++ Cplus/CompleteBinaryTreeInserter.cpp
@@ -0,0 +1,61 @@
+#include <algorithm>
+#include <queue>
+using namespace std;
+//Definition for a binary tree node.
+struct TreeNode
+{
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class CBTInserter
+{
+public:
+	CBTInserter(TreeNode* root) : m_root(root)
+	{
+		m_q.push(root);
+		while (true)
+		{
+			TreeNode* tmp = m_q.front();
+			if (tmp->left == NULL || tmp->right == NULL)
+				break;
+			m_q.pop();
+			m_q.push(tmp->left);
+			if (tmp->right != NULL)
+				m_q.push(tmp->right);
+		}
+	}
+
+	int insert(int v)
+	{
+		TreeNode* tmp = m_q.front();
+		if (tmp->left == NULL)
+		{
+			tmp->left = new TreeNode(v);
+			return tmp->val;
+		}
+		tmp->right = new TreeNode(v);
+		m_q.pop();
+		m_q.push(tmp->left);
+		m_q.push(tmp->right);
+		return tmp->val;
+	}
+
+	TreeNode* get_root()
+	{
+		return m_root;
+	}
+
+private:
+	TreeNode* m_root;
+	queue<TreeNode*> m_q;
+};
+
+/**
+ * Your CBTInserter object will be instantiated and called as such:
+ * CBTInserter* obj = new CBTInserter(root);
+ * int param_1 = obj->insert(v);
+ * TreeNode* param_2 = obj->get_root();
+ */
