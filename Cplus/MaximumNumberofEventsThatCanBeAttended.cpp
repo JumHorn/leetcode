e246c4d315e798ad5f4fe1d8654f25673cc75bfa
JumHorn <JumHorn@gmail.com>
2020-02-16 16:39:57 +0800

    Maximum Number of Events That Can Be Attended beats 45.45%

--- Cplus/MaximumNumberofEventsThatCanBeAttended.cpp
+++ Cplus/MaximumNumberofEventsThatCanBeAttended.cpp
@@ -1,11 +1,50 @@
+#include <algorithm>
 #include <vector>
+#include <queue>
 using namespace std;
 
 class Solution
 {
 public:
-	int maxEvents(vector<vector<int>>& events)
+	int maxEvents(vector<vector<int>> &events)
 	{
 		sort(events.begin(), events.end());
+		priority_queue<int> q;
+		int res = 0, d = events[0][0], n = events.size();
+		for (int i = 0; i < n;)
+		{
+			if (events[i][0] <= d && events[i][1] >= d)
+			{
+				q.push(-events[i][1]);
+				i++;
+			}
+			else
+			{
+				while (!q.empty())
+				{
+					int tmp = -q.top();
+					q.pop();
+					if (tmp >= d)
+					{
+						res++;
+						d++;
+						break;
+					}
+				}
+				if (q.empty())
+					d = events[i][0];
+			}
+		}
+		while (!q.empty())
+		{
+			int tmp = -q.top();
+			q.pop();
+			if (tmp >= d)
+			{
+				res++;
+				d++;
+			}
+		}
+		return res;
 	}
 };
