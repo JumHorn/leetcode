cb5dc1b7ba4986368b8aa10af679c6ecb459b191
JumHorn <JumHorn@gmail.com>
2020-02-16 08:34:46 +0800

    Subarray Sums Divisible by K beats 57.06%

---
+++ Cplus/SubarraySumsDivisiblebyK.cpp
@@ -0,0 +1,24 @@
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+	int subarraysDivByK(vector<int> &A, int K)
+	{
+		unordered_map<int, int> m;
+		int res = 0, sum = 0;
+		m[0] = 1;
+		for (int i = 0; i < (int)A.size(); i++)
+			A[i] = (A[i] % K + K) % K;
+		for (int i = 0; i < (int)A.size(); i++)
+		{
+			sum += A[i];
+			int tmp = sum % K;
+			res += m[tmp];
+			m[tmp] += 1;
+		}
+		return res;
+	}
+};
