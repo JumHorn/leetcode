bf6bfef81dc4bf6ab23eb6895d4f57b1719c6a8b
JumHorn <JumHorn@gmail.com>
2020-02-16 14:51:45 +0800

    Construct Target Array With Multiple Sums beats 100%

--- Cplus/ConstructTargetArrayWithMultipleSums.cpp
+++ Cplus/ConstructTargetArrayWithMultipleSums.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <queue>
 #include <unordered_set>
 #include <vector>
 using namespace std;
@@ -8,54 +9,30 @@ class Solution
 public:
 	bool isPossible(vector<int> &target)
 	{
-		if (target.size() == 1)
-			return target[0] == 1;
-		sort(target.begin(), target.end());
-		vector<int> A(target.size(), 1);
-		vector<unordered_set<int>> seen(target.size());
-		return backTracking(target, 0, A, target.size(), seen);
-	}
-
-	bool backTracking(vector<int> &target, int index, vector<int> &A, int sum, vector<unordered_set<int>> &seen)
-	{
-		int n = target.size();
-		if (n == index)
-			return true;
-		if (seen[index].find(sum) != seen[index].end())
-			return false;
-		if (target[index] < sum)
+		priority_queue<int> q;
+		long sum = 0;
+		for (auto n : target)
 		{
-			seen[index].insert(sum);
-			return false;
+			q.push(n);
+			sum += n;
 		}
-		for (int i = index; i < n; i++)
+		while (!q.empty())
 		{
-			for (int j = index; j < n; j++)
-			{
-				int tmp = A[index];
-				A[index] = sum;
-				if (A[index] == target[index])
-				{
-					if (backTracking(target, index + 1, A, sum + sum - tmp, seen))
-						return true;
-				}
-				else
-				{
-					if (backTracking(target, index, A, sum + sum - tmp, seen))
-						return true;
-				}
-				A[index] = tmp;
-			}
+			int tmp = q.top();
+			q.pop();
+			if (tmp == 1)
+				return true;
+			sum -= tmp;
+			if (sum == 1)
+				return true;
+			if (tmp < sum)
+				return false;
+			tmp = tmp - tmp / sum * sum;
+			sum += tmp;
+			if (tmp < 1)
+				return false;
+			q.push(tmp);
 		}
-		seen[index].insert(sum);
 		return false;
 	}
 };
-
-int main()
-{
-	vector<int> v = {8, 5};
-	Solution sol;
-	sol.isPossible(v);
-	return 0;
-}
