2b79d11f897ecef593911671395782e99085f743
JumHorn <JumHorn@gmail.com>
2020-02-18 19:45:38 +0800

    Max Consecutive Ones beats 85.30%

--- Cplus/MaxConsecutiveOnes.cpp
+++ Cplus/MaxConsecutiveOnes.cpp
@@ -1,48 +1,25 @@
-#include<iostream>
-#include<vector>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int findMaxConsecutiveOnes(vector<int>& nums) {
-        //多次对iter!=nums.end()判断，效率低下
-        // int sum=0;
-        // int temp=0;
-        // vector<int>::iterator iter=nums.begin();
-        // while(iter!=nums.end())
-        // {
-        //     while(iter!=nums.end()&&*iter==0)
-        //     {
-        //         iter++;
-        //     }
-        //     temp=0;
-        //     while(iter!=nums.end()&&*iter==1)
-        //     {
-        //         iter++;
-        //         temp++;
-        //     }
-        //     if(temp>sum)sum=temp;
-        // }
-        // return sum;
-
-        int sum=0;
-        int temp=0;
-        for(vector<int>::iterator iter=nums.begin();iter!=nums.end();iter++)
-        {
-            if(*iter==0)
-            {
-                if(temp>sum)sum=temp;
-                temp=0;
-                continue;  
-            }
-            else
-            {
-                temp++;
-            }
-        }
-        return sum>temp?sum:temp;
-    }
+	int findMaxConsecutiveOnes(vector<int>& nums)
+	{
+		int sum = 0, tmp = 0;
+		for (vector<int>::iterator iter = nums.begin(); iter != nums.end(); ++iter)
+		{
+			if (*iter == 0)
+			{
+				if (tmp > sum)
+					sum = tmp;
+				tmp = 0;
+			}
+			else
+			{
+				tmp++;
+			}
+		}
+		return sum > tmp ? sum : tmp;
+	}
 };
-
-int main()
-{}
