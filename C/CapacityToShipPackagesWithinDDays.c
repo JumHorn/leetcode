75ac3113f178f54d8a9667741007e22e70c6281e
JumHorn <JumHorn@gmail.com>
2020-02-19 10:45:47 +0800

    Capacity To Ship Packages Within D Days beats 100%

---
+++ C/CapacityToShipPackagesWithinDDays.c
@@ -0,0 +1,30 @@
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+
+int shipWithinDays(int *weights, int weightsSize, int D)
+{
+	int lo = 0, hi = 0;
+	for (int i = 0; i < weightsSize; i++)
+	{
+		lo = max(lo, weights[i]);
+		hi += weights[i];
+	}
+	while (lo < hi)
+	{
+		int mi = (hi - lo) / 2 + lo, cap = 0, day = 1;
+		for (int i = 0; i < weightsSize && day <= D; i++)
+		{
+			if (cap + weights[i] > mi)
+			{
+				cap = weights[i];
+				day++;
+			}
+			else
+				cap += weights[i];
+		}
+		if (day > D)
+			lo = mi + 1;
+		else
+			hi = mi;
+	}
+	return lo;
+}
