68a6aeac9d6a4c3f01978fc202773837ee34982d
JumHorn <JumHorn@gmail.com>
2020-02-20 22:02:12 +0800

    Longest Arithmetic Sequence beats 88.14%

--- Cplus/LongestArithmeticSequence.cpp
+++ Cplus/LongestArithmeticSequence.cpp
@@ -9,12 +9,12 @@ public:
 	int longestArithSeqLength(vector<int> &A)
 	{
 		int n = A.size(), res = 0;
-		vector<unordered_map<int, int>> dp(n);
+		vector<vector<int>> dp(n, vector<int>(20001));
 		for (int i = 1; i < n; i++)
 		{
 			for (int j = 0; j < i; j++)
 			{
-				int diff = A[i] - A[j];
+				int diff = A[i] - A[j] + 10000;
 				dp[i][diff] = max(dp[i][diff], dp[j][diff] + 1);
 				res = max(res, dp[i][diff]);
 			}
