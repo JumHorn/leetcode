a281f028acfe9e26f19eb0bca8d32ad753898fd2
JumHorn <JumHorn@gmail.com>
2020-02-23 08:20:36 +0800

    Corporate Flight Bookings beats 69.02%

--- Cplus/CorporateFlightBookings.cpp
+++ Cplus/CorporateFlightBookings.cpp
@@ -4,14 +4,17 @@ using namespace std;
 class Solution
 {
 public:
-	vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n)
+	vector<int> corpFlightBookings(vector<vector<int>> &bookings, int n)
 	{
 		vector<int> res(n);
 		for (int i = 0; i < (int)bookings.size(); i++)
 		{
-			for (int j = bookings[i][0]; j <= bookings[i][1]; j++)
-				res[j - 1] += bookings[i][2];
+			res[bookings[i][0] - 1] += bookings[i][2];
+			if (bookings[i][1] < n)
+				res[bookings[i][1]] -= bookings[i][2];
 		}
+		for (int i = 1; i < n; i++)
+			res[i] += res[i - 1];
 		return res;
 	}
 };
