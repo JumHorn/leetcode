42d839d1ecd913215695df9cb5db0f24f7e19f07
JumHorn <JumHorn@gmail.com>
2020-02-24 19:24:06 +0800

    Binary Tree Coloring Game beats 82.62%

---
+++ Cplus/BinaryTreeColoringGame.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+using namespace std;
+
+//Definition for a binary tree node.
+struct TreeNode
+{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution
+{
+public:
+	bool btreeGameWinningMove(TreeNode *root, int n, int x)
+	{
+		TreeNode *node = getXNode(root, x);
+		int left = nodeCount(node->left), right = nodeCount(node->right);
+		return max(max(left, right), n - left - right - 1) * 2 > n;
+	}
+
+	TreeNode *getXNode(TreeNode *root, int x)
+	{
+		if (root == NULL || root->val == x)
+			return root;
+		TreeNode *res = NULL;
+		res = getXNode(root->left, x);
+		if (res == NULL)
+			res = getXNode(root->right, x);
+		return res;
+	}
+
+	int nodeCount(TreeNode *root)
+	{
+		if (root == NULL)
+			return 0;
+		return nodeCount(root->left) + nodeCount(root->right) + 1;
+	}
+};
