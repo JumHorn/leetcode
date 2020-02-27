4c07ba82ec19fa9bc79093aee0167af7e1348a86
JumHorn <JumHorn@gmail.com>
2020-02-27 20:38:54 +0800

    Circular Permutation in Binary Representation beats 88.37%

--- Cplus/CircularPermutationinBinaryRepresentation.cpp
+++ Cplus/CircularPermutationinBinaryRepresentation.cpp
@@ -10,44 +10,19 @@ public:
 	{
 		int len = pow(2, n);
 		vector<int> res(len);
-		for (int i = 0; i < len; i++)
-			res[i] = i;
-		res[start] = res[0];
 		res[0] = start;
-		backTracking(res, 1);
-		return res;
-	}
-
-	bool backTracking(vector<int>& v, int index)
-	{
-		int n = v.size();
-		if (index >= n)
-			return bitDiffCount(v[0], v[n - 1]);
-		for (int i = index; i < n; i++)
+		int tmp = 0;
+		while (start)
 		{
-			if (bitDiffCount(v[index - 1], v[i]))
-			{
-				swap(v[index], v[i]);
-				if (backTracking(v, index + 1))
-					return true;
-				swap(v[index], v[i]);
-			}
+			tmp ^= start;
+			start >>= 1;
 		}
-		return false;
-	}
-
-	bool bitDiffCount(int a, int b)
-	{
-		return bitCount(a ^ b) == 1;
-	}
-
-	int bitCount(int n)
-	{
-		int res = 0;
-		while (n != 0)
+		for (int i = 1; i < len; i++)
 		{
-			n &= n - 1;
-			res++;
+			tmp++;
+			if (tmp >= len)
+				tmp -= len;
+			res[i] = tmp ^ (tmp >> 1);
 		}
 		return res;
 	}
