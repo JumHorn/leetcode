f344ac4ec823003d2d147011f3d0769caf808e33
JumHorn <JumHorn@gmail.com>
2020-02-29 19:34:07 +0800

    Minimum Swaps to Make Strings Equal beats 100%

---
+++ Cplus/MinimumSwapstoMakeStringsEqual.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <cmath>
+#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+	int minimumSwap(string s1, string s2)
+	{
+		int x1 = 0, y1 = 0;
+		for (int i = 0; i < (int)s1.length(); i++)
+		{
+			if (s1[i] != s2[i])
+				s1[i] == 'x' ? x1++ : y1++;
+		}
+		if ((x1 + y1) % 2 != 0)
+			return -1;
+		return (x1 + y1) / 2 + x1 % 2;
+	}
+};
