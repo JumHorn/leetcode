8462b66a2a61c8dc5cb21d052ff63ebf8dc554e1
JumHorn <JumHorn@gmail.com>
2020-03-01 08:56:13 +0800

    Airplane Seat Assignment Probability beats 100%

--- C/AirplaneSeatAssignmentProbability.c
+++ C/AirplaneSeatAssignmentProbability.c
@@ -3,15 +3,6 @@
 
 double nthPersonGetsNthSeat(int n)
 {
-	double *dp = (double *)malloc(sizeof(double) * n);
-	memset(dp, 0, sizeof(double) * n);
-	dp[0] = 1.0;
-	for (int i = 1; i < n; i++)
-	{
-		for (int j = 0; j < i; j++)
-			dp[i] += dp[j];
-		dp[i] /= i + 1;
-	}
-	return dp[n - 1];
+	return n == 1 ? 1 : 0.5;
 }
 
