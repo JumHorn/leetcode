8bf53f4f8e86c87406dab3105a644238962e34a4
JumHorn <JumHorn@gmail.com>
2020-03-05 06:35:57 +0800

    Peeking Iterator beats 82.76%

---
+++ Cplus/PeekingIterator.cpp
@@ -0,0 +1,68 @@
+#include <vector>
+using namespace std;
+
+// Below is the interface for Iterator, which is already defined for you.
+// **DO NOT** modify the interface for Iterator.
+
+class Iterator
+{
+	struct Data;
+	Data* data;
+
+public:
+	Iterator(const vector<int>& nums);
+	Iterator(const Iterator& iter);
+	virtual ~Iterator();
+	// Returns the next element in the iteration.
+	int next();
+	// Returns true if the iteration has more elements.
+	bool hasNext() const;
+};
+
+class PeekingIterator : public Iterator
+{
+public:
+	PeekingIterator(const vector<int>& nums) : Iterator(nums)
+	{
+		// Initialize any member here.
+		// **DO NOT** save a copy of nums and manipulate it directly.
+		// You should only use the Iterator interface methods.
+		ispeeking = false;
+	}
+
+	// Returns the next element in the iteration without advancing the iterator.
+	int peek()
+	{
+		if (ispeeking)
+			return val;
+		if (hasNext())
+		{
+			val = next();
+			ispeeking = true;
+		}
+		return val;
+	}
+
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	int next()
+	{
+		if (ispeeking)
+		{
+			ispeeking = false;
+			return val;
+		}
+		return Iterator::next();
+	}
+
+	bool hasNext() const
+	{
+		if (ispeeking)
+			return true;
+		return Iterator::hasNext();
+	}
+
+private:
+	bool ispeeking;
+	int val;
+};
