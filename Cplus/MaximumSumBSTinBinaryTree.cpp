8fd754a68d1c731f3122a0474eb7c34146f601f9
JumHorn <JumHorn@gmail.com>
2020-03-08 07:44:41 +0800

    Maximum Sum BST in Binary Tree beats 75.00%

--- Cplus/MaximumSumBSTinBinaryTree.cpp
+++ Cplus/MaximumSumBSTinBinaryTree.cpp
@@ -1,30 +1,48 @@
 #include <algorithm>
+#include <climits>
 using namespace std;
 
 //Definition for a binary tree node.
 struct TreeNode
 {
 	int val;
-	TreeNode* left;
-	TreeNode* right;
+	TreeNode *left;
+	TreeNode *right;
 	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
 class Solution
 {
+private:
+	struct BST
+	{
+		int max;
+		int min;
+		int sum;
+	};
+
 public:
-	int maxSumBST(TreeNode* root)
+	int maxSumBST(TreeNode *root)
 	{
+		int res = 0;
+		postOrder(root, res);
+		return res;
 	}
 
-	bool isBST(TreeNode* root)
-	{
-	}
-
-	int sum(TreeNode* root)
+	BST postOrder(TreeNode *root, int &res)
 	{
 		if (root == NULL)
-			return 0;
-		return root->val + sum(root->left) + sum(root->right);
+			return {INT_MIN, INT_MAX, 0};
+		BST left = postOrder(root->left, res);
+		BST right = postOrder(root->right, res);
+		if (left.sum != INT_MAX && right.sum != INT_MAX)
+		{
+			if (root->val > left.max && root->val < right.min)
+			{
+				res = max(res, root->val + left.sum + right.sum);
+				return {max(right.max, root->val), min(left.min, root->val), root->val + left.sum + right.sum};
+			}
+		}
+		return {0, 0, INT_MAX};
 	}
 };
