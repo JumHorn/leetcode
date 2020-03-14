5d82aad619e5e4334ca9060da99a69bf47e93c59
JumHorn <JumHorn@gmail.com>
2020-03-14 09:19:43 +0800

    Verbal Arithmetic Puzzle beats 80.49%

--- Cplus/VerbalArithmeticPuzzle.cpp
+++ Cplus/VerbalArithmeticPuzzle.cpp
@@ -8,50 +8,44 @@ class Solution
 public:
 	bool isSolvable(vector<string> &words, string result)
 	{
-		vector<int> digit(10);
-		for (int i = 0; i < 10; i++)
-			digit[i] = i;
-		charindex.clear();
-		int index = 0;
+		vector<int> digit(26, -1), used(10, -1);
 		for (auto &word : words)
 		{
-			for (auto &c : word)
-				if (charindex.find(c) == charindex.end())
-					charindex[c] = index++;
+			if (word.length() > result.length())
+				return false;
 		}
-		for (auto &c : result)
-			if (charindex.find(c) == charindex.end())
-				charindex[c] = index++;
-		return permutation(digit, charindex.size(), 0, words, result);
+		return backTracking(words, result, digit, used, 0, 0);
 	}
 
-	bool permutation(vector<int> &digit, int n, int index, vector<string> &words, string &result)
+	bool backTracking(vector<string> &words, string &result, vector<int> &digit, vector<int> &used, int index, int carry)
 	{
-		if (index >= n)
-			return checkValid(words, result, digit);
-		for (int i = index; i < (int)digit.size(); i++)
-		{
-			swap(digit[index], digit[i]);
-			if (permutation(digit, n, index + 1, words, result))
-				return true;
-			swap(digit[index], digit[i]);
-		}
-		return false;
-	}
-
-	bool checkValid(vector<string> &words, string &result, vector<int> &digit)
-	{
-		int carry = 0, len = result.size();
-		for (int i = 0; i <= 7; i++)
+		int len = result.size();
+		for (int i = index; i <= 7; i++)
 		{
 			int tmp = 0;
 			bool flag = false;
 			for (auto &word : words)
 			{
 				int n = word.size();
-				if (i >= (int)word.size())
+				if (i >= n)
 					continue;
-				tmp += digit[charindex[word[n - i - 1]]];
+				if (digit[word[n - i - 1] - 'A'] == -1)
+				{
+					for (int j = 0; j < 10; j++)
+					{
+						if (used[j] == -1)
+						{
+							digit[word[n - i - 1] - 'A'] = j;
+							used[j] = 0;
+							if (backTracking(words, result, digit, used, i, carry))
+								return true;
+							used[j] = -1;
+							digit[word[n - i - 1] - 'A'] = -1;
+						}
+					}
+					return false;
+				}
+				tmp += digit[word[n - i - 1] - 'A'];
 				flag = true;
 			}
 			if (i >= (int)result.size())
@@ -63,27 +57,27 @@ public:
 			tmp += carry;
 			carry = tmp / 10;
 			tmp %= 10;
-			if (digit[charindex[result[len - i - 1]]] != tmp)
+			if (digit[result[len - i - 1] - 'A'] == -1)
+			{
+				if (used[tmp] != -1)
+					return false;
+				digit[result[len - i - 1] - 'A'] = tmp;
+				used[tmp] = 0;
+				if (backTracking(words, result, digit, used, i + 1, carry))
+					return true;
+				used[tmp] = -1;
+				digit[result[len - i - 1] - 'A'] = -1;
+			}
+			if (digit[result[len - i - 1] - 'A'] != tmp)
 				return false;
 		}
 		for (auto &word : words)
 		{
-			if (digit[charindex[word[0]]] == 0)
+			if (digit[word[0] - 'A'] == 0)
 				return false;
 		}
-		if (digit[charindex[result[0]]] == 0)
+		if (digit[result[0] - 'A'] == 0)
 			return false;
 		return carry == 0;
 	}
-
-private:
-	unordered_map<char, int> charindex;
 };
-
-int main()
-{
-	Solution sol;
-	vector<string> v = {"THIS", "IS", "TOO"};
-	bool res = sol.isSolvable(v, "FUNNY");
-	return 0;
-}
