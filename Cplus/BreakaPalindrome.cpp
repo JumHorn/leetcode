6cbc4dbcb3846d197e1ed788d4f429e8f4c96202
JumHorn <JumHorn@gmail.com>
2020-03-17 06:14:35 +0800

    Break a Palindrome beats 100%

---
+++ Cplus/BreakaPalindrome.cpp
@@ -0,0 +1,27 @@
+#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+	string breakPalindrome(string palindrome)
+	{
+		int i = 0, j = palindrome.length() - 1;
+		while (i < j)
+		{
+			if (palindrome[i] != 'a')
+			{
+				palindrome[i] = 'a';
+				return palindrome;
+			}
+			i++;
+			j--;
+		}
+		if (palindrome.size() > 1)
+		{
+			palindrome.back() = 'b';
+			return palindrome;
+		}
+		return "";
+	}
+};
