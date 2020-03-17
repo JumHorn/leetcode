1bf372b9d2c4ec9064d381910659f2b542f5f1d2
JumHorn <JumHorn@gmail.com>
2020-03-18 07:20:12 +0800

    Smallest String With Swaps beats 91.39%

--- Cplus/SmallestStringWithSwaps.cpp
+++ Cplus/SmallestStringWithSwaps.cpp
@@ -15,9 +15,9 @@ public:
 
 	int Find(int x)
 	{
-		if (x == parent[x])
+		if (parent[x] == x)
 			return x;
-		return Find(parent[x]);
+		return parent[x] = Find(parent[x]);
 	}
 
 	bool Union(int x, int y)
@@ -42,19 +42,22 @@ public:
 		for (auto &p : pairs)
 			dsu.Union(p[0], p[1]);
 		vector<int> parent(s.length());
-		unordered_map<int, string> m;
+		unordered_map<int, vector<int>> m;
 		for (int i = 0; i < (int)s.length(); i++)
 		{
 			int p = dsu.Find(i);
 			parent[i] = p;
-			m[p].push_back(s[i]);
+			m[p].push_back(i);
 		}
 		for (auto &n : m)
-			sort(n.second.begin(), n.second.end(), greater<char>());
-		for (int i = 0; i < (int)s.length(); i++)
 		{
-			s[i] = m[parent[i]].back();
-			m[parent[i]].pop_back();
+			string str;
+			for (auto i : n.second)
+				str += s[i];
+			sort(str.begin(), str.end());
+			int j = 0;
+			for (auto i : n.second)
+				s[i] = str[j++];
 		}
 		return s;
 	}
