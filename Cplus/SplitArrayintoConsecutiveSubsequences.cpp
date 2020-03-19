c8bd3dd01a332a256d9ee8b6146f0f7a319b3a13
JumHorn <JumHorn@gmail.com>
2020-03-19 21:17:31 +0800

    Split Array into Consecutive Subsequences beats 92.85%

---
+++ Cplus/SplitArrayintoConsecutiveSubsequences.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+	bool isPossible(vector<int> &nums)
+	{
+		int a = 0, b = 0, c = 0;  //a >=3 b =1 c=2
+		for (int i = 0, j = 0; i < (int)nums.size(); i = j)
+		{
+			j = i + 1;
+			while (j < (int)nums.size() && nums[i] == nums[j])
+				j++;
+			if (i - 1 >= 0 && nums[i] != nums[i - 1] + 1)
+			{
+				if (b != 0 || c != 0)
+					return false;
+				a = b = c = 0;
+			}
+			int tmp = a, diff = j - i - b - c;
+			if (diff < 0)
+				return false;
+			a = min(a, diff);
+			a += c;
+			c = b;
+			if (diff > tmp)
+				b = diff - tmp;
+			else
+				b = 0;
+		}
+		return b == 0 && c == 0;
+	}
+};
