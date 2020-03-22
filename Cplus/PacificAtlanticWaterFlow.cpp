702d7c5bf8c09e1837d1bd130f8ae671beea8e7e
JumHorn <JumHorn@gmail.com>
2020-03-22 17:28:31 +0800

    Pacific Atlantic Water Flow beats 46.98%

--- Cplus/PacificAtlanticWaterFlow.cpp
+++ Cplus/PacificAtlanticWaterFlow.cpp
@@ -1,56 +1,47 @@
-#include<vector>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int> > pacificAtlantic(vector<vector<int> >& matrix) {
-        if(matrix.empty())
-            return vector<vector<int> >();
-		vector<vector<int> > dp(matrix.size(),vector<int>(matrix[0].size())),res;
-		bool pacific=false,atlantic=false;
-		for(int i=0;i<(int)matrix.size();i++)
-			for(int j=0;j<(int)matrix[0].size();j++)
-			{
-				pacific=atlantic=false;
-				if(dp[i][j]==0&&pacificAtlantic(matrix,dp,i,j,pacific,atlantic))
-				{
-					res.push_back(vector<int>({i,j}));
-					dp[i][j]=2;
-				}
-			}
-
-		return res;
-    }
-
-	bool pacificAtlantic(vector<vector<int> >& matrix,vector<vector<int> >& dp,int i,int j,bool& p,bool& a)
+	vector<vector<int>> pacificAtlantic(vector<vector<int>> &matrix)
 	{
-		static const int dir[4][2]={{1,0},{-1,0},{0,1},{0,-1}};
-		if(dp[i][j]==3||i==0||j==0)
-			p=true;
-		if(dp[i][j]==4||i==(int)matrix.size()-1||j==(int)matrix[0].size()-1)
-			a=true;
-		if(p&&a)
-			return true;
-		if(dp[i][j]!=0)
-			return dp[i][j]==2;
-		dp[i][j]=1;
-		bool res=false;
-		for(int d=0;d<4;d++)
+		if (matrix.empty() || matrix[0].empty())
+			return {};
+		int m = matrix.size(), n = matrix[0].size();
+		vector<vector<int>> dp1(m, vector<int>(n)), dp2(m, vector<int>(n)), res;
+		for (int i = 0; i < m; i++)
 		{
-			int x=i+dir[d][0],y=j+dir[d][1];
-            if(x>=0&&x<(int)matrix.size()&&y>=0&&y<(int)matrix[0].size()&&matrix[i][j]>=matrix[x][y])
-				res|=pacificAtlantic(matrix,dp,x,y,p,a);
+			dfs(matrix, dp1, i, 0);
+			dfs(matrix, dp2, i, n - 1);
 		}
-		dp[i][j]=0;
-		if(!res)
+		for (int j = 0; j < n; j++)
 		{
-			if(p)
-				dp[i][j]=3;
-			else if(a)
-				dp[i][j]=4;
-			else
-				dp[i][j]=5;
+			dfs(matrix, dp1, 0, j);
+			dfs(matrix, dp2, m - 1, j);
+		}
+		for (int i = 0; i < m; i++)
+		{
+			for (int j = 0; j < n; j++)
+				if (dp1[i][j] == 1 && dp2[i][j] == 1)
+					res.push_back({i, j});
 		}
 		return res;
+	}
+
+	void dfs(vector<vector<int>> &matrix, vector<vector<int>> &dp, int i, int j)
+	{
+		if (dp[i][j] != 0)
+			return;
+		dp[i][j] = 1;
+		int m = matrix.size(), n = matrix[0].size();
+		if (i - 1 >= 0 && matrix[i - 1][j] >= matrix[i][j])
+			dfs(matrix, dp, i - 1, j);
+		if (j - 1 >= 0 && matrix[i][j - 1] >= matrix[i][j])
+			dfs(matrix, dp, i, j - 1);
+		if (i + 1 < m && matrix[i + 1][j] >= matrix[i][j])
+			dfs(matrix, dp, i + 1, j);
+		if (j + 1 < n && matrix[i][j + 1] >= matrix[i][j])
+			dfs(matrix, dp, i, j + 1);
 	}
 };
