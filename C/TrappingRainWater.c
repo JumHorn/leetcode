e23a103c6b83cc6a5ddb1ced001459549876d0c0
JumHorn <JumHorn@gmail.com>
2020-03-24 07:44:06 +0800

    beautiful code

--- C/TrappingRainWater.c
+++ C/TrappingRainWater.c
@@ -5,22 +5,17 @@ int trap(int *height, int heightSize)
 {
 	if (heightSize == 0)
 		return 0;
-	int stack[heightSize], top = -1, res = 0; //simulate stack using array
+	int stack[heightSize], top = -1, res = 0;  //simulate stack using array
 	for (int i = 0; i < heightSize; i++)
 	{
-		if (top == -1 || height[i] < height[stack[top]]) //stack is empty
-			stack[++top] = i;
-		else
+		if (top != -1)	//stack is not empty
 		{
 			while (top != -1 && height[i] >= height[stack[top]])
-			{
-				res -= height[stack[top]];
-				--top;
-			}
+				res -= height[stack[top--]];
 			if (top == -1)
 				res += height[stack[0]] * (i - stack[0]);
-			stack[++top] = i;
 		}
+		stack[++top] = i;
 	}
 	while (--top >= 0)
 		res += height[stack[top + 1]] * (stack[top + 1] - stack[top] - 1);
