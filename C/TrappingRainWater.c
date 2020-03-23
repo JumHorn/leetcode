4abe459368bea4b16a306fc6e2b4788bbe05c21c
JumHorn <JumHorn@gmail.com>
2020-03-24 07:20:21 +0800

    Trapping Rain Water beats 84.96%

--- C/TrappingRainWater.c
+++ C/TrappingRainWater.c
@@ -3,6 +3,8 @@
 //keep the stack decreasing
 int trap(int *height, int heightSize)
 {
+	if (heightSize == 0)
+		return 0;
 	int stack[heightSize], top = -1, res = 0; //simulate stack using array
 	for (int i = 0; i < heightSize; i++)
 	{
@@ -11,13 +13,16 @@ int trap(int *height, int heightSize)
 		else
 		{
 			while (top != -1 && height[i] >= height[stack[top]])
+			{
+				res -= height[stack[top]];
 				--top;
+			}
 			if (top == -1)
-				res += height[stack[0]] * (i - stack[0] - 1);
+				res += height[stack[0]] * (i - stack[0]);
 			stack[++top] = i;
 		}
 	}
 	while (--top >= 0)
-		res += height[stack[top + 1]] * (stack[top + 1] - stack[top]);
+		res += height[stack[top + 1]] * (stack[top + 1] - stack[top] - 1);
 	return res;
 }
