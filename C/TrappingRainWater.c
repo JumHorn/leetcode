d6215d6ace9b43ffc1e4c6bfec91e5ef21f4b177
JumHorn <JumHorn@gmail.com>
2020-03-24 06:43:54 +0800

    stack based solution failed

---
+++ C/TrappingRainWater.c
@@ -0,0 +1,23 @@
+
+//using stack to store the index of the height
+//keep the stack decreasing
+int trap(int *height, int heightSize)
+{
+	int stack[heightSize], top = -1, res = 0; //simulate stack using array
+	for (int i = 0; i < heightSize; i++)
+	{
+		if (top == -1 || height[i] < height[stack[top]]) //stack is empty
+			stack[++top] = i;
+		else
+		{
+			while (top != -1 && height[i] >= height[stack[top]])
+				--top;
+			if (top == -1)
+				res += height[stack[0]] * (i - stack[0] - 1);
+			stack[++top] = i;
+		}
+	}
+	while (--top >= 0)
+		res += height[stack[top + 1]] * (stack[top + 1] - stack[top]);
+	return res;
+}
