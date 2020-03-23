5360de70d59443ce606c8470516524146a0078d9
JumHorn <JumHorn@gmail.com>
2020-03-23 21:00:34 +0800

    Sum of Distances in Tree beats 22.73%

--- Cplus/SumofDistancesinTree.cpp
+++ Cplus/SumofDistancesinTree.cpp
@@ -1,51 +1,54 @@
-#include<vector>
-#include<set>
+#include <unordered_set>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> sumOfDistancesInTree(int N, vector<vector<int> >& edges) {
-        vector<vector<int> > graph(N,vector<int>()),dp(N,vector<int>(N));
-		for(int i=0;i<(int)edges.size();i++)
-		{
-			graph[edges[i][0]].push_back(edges[i][1]);
-			graph[edges[i][1]].push_back(edges[i][0]);
-            dp[edges[i][0]][edges[i][1]]=1;
-            dp[edges[i][1]][edges[i][0]]=1;
-		}
-		for(int i=0;i<N;i++)
-			for(int j=i+1;j<N;j++)
-			{
-		 		set<pair<int,int> > visited;
-		 		if(dp[i][j]==0)
-		 			sumOfDistancesInTree(graph,dp,visited,i,j);
-		    }
-		vector<int> res(N);
-		for(int i=0;i<N;i++)
-			for(int j=0;j<N;j++)
-				res[i]+=dp[i][j];
-		return res;
-    }
-
-	int sumOfDistancesInTree(vector<vector<int> >& graph,vector<vector<int> >& dp,set<pair<int,int> >& visited,int i,int j)
+	vector<int> sumOfDistancesInTree(int N, vector<vector<int>>& edges)
 	{
-		if(i==j)
-			return 0;
-		if(dp[i][j]!=0)
-			return dp[i][j];
-		if(visited.find({i,j})!=visited.end())
-			return -1;
-        visited.insert({i,j});
-		int res=-1;
-		for(int m=0;m<(int)graph[i].size();m++)
+		vector<unordered_set<int>> graph(N);
+		for (auto& edge : edges)
 		{
-			int tmp=sumOfDistancesInTree(graph,dp,visited,graph[i][m],j);
-			if(tmp!=-1)
-			{
-				res=dp[j][i]=dp[i][j]=1+tmp;
-				break;
-			}
+			graph[edge[0]].insert(edge[1]);
+			graph[edge[1]].insert(edge[0]);
 		}
+		vector<int> res(N), nodecount(N);
+		dfs(graph, nodecount, 0);
+		res[0] = node0distance(graph, nodecount, 0);
+		preorder(graph, nodecount, res, 0);
+		return res;
+	}
+
+	void preorder(vector<unordered_set<int>>& graph, vector<int>& nodecount, vector<int>& dis, int node)
+	{
+		int N = graph.size();
+		for (auto n : graph[node])
+		{
+			dis[n] = dis[node] + N - 2 * nodecount[n];
+			preorder(graph, nodecount, dis, n);
+		}
+	}
+
+	int node0distance(vector<unordered_set<int>>& graph, vector<int>& nodecount, int node)
+	{
+		int res = 0;
+		for (auto n : graph[node])
+		{
+			res += node0distance(graph, nodecount, n) + nodecount[n];
+		}
+		return res;
+	}
+
+	int dfs(vector<unordered_set<int>>& graph, vector<int>& nodecount, int node)
+	{
+		int res = 1;
+		for (auto n : graph[node])
+		{
+			graph[n].erase(node);
+			res += dfs(graph, nodecount, n);
+		}
+		nodecount[node] = res;
 		return res;
 	}
 };
