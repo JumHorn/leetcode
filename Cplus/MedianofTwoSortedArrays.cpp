9833e2637fb7805b2d9c099180daab077ae1fb7d
JumHorn <JumHorn@gmail.com>
2020-03-25 21:06:17 +0800

    Median of Two Sorted Arrays beats 89.61%

--- Cplus/MedianofTwoSortedArrays.cpp
+++ Cplus/MedianofTwoSortedArrays.cpp
@@ -1,45 +1,31 @@
-#include<vector>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-		int i=0,j=0,len1=nums1.size(),len2=nums2.size(),tmp=(len1+len2)/2+1,t1,t2;
-		bool even=((len1+len2)%2==0);
-		while(i<len1&&j<len2)
+	double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
+	{
+		int i = 0, j = 0, len1 = nums1.size(), len2 = nums2.size();
+		int sum = len1 + len2, count = sum / 2, midleft, midright;
+		while (count >= 0 && i < len1 && j < len2)
 		{
-			if(nums1[i]<nums2[j])
-			{
-				t1=t2;
-				t2=nums1[i];
-				i++;
-			}
-			else
-			{
-				t1=t2;
-				t2=nums2[j];
-				j++;
-			}
-
-			if(i+j==tmp)
-			{
-				return even?(t1+t2)/2.0:t2;
-			}
+			midright = (nums1[i] < nums2[j] ? nums1[i++] : nums2[j++]);
+			if (--count == 0)
+				midleft = midright;
 		}
-		while(i<len1)
+		while (count >= 0 && i < len1)
 		{
-			t1=t2;
-			t2=nums1[i++];
-			if(i+j==tmp)
-				return even?(t1+t2)/2.0:t2;
+			midright = nums1[i++];
+			if (--count == 0)
+				midleft = midright;
 		}
-		while(j<len2)
+		while (count >= 0 && j < len2)
 		{
-			t1=t2;
-			t2=nums2[j++];
-			if(i+j==tmp)
-				return even?(t1+t2)/2.0:t2;
+			midright = nums2[j++];
+			if (--count == 0)
+				midleft = midright;
 		}
-		return 0;
-    }
+		return sum % 2 == 0 ? (midleft + midright) / 2.0 : midright;
+	}
 };
