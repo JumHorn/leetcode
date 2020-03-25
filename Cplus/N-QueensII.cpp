40318b691e50e2e51b002df3ebb983566c739c18
JumHorn <JumHorn@gmail.com>
2020-03-25 20:41:53 +0800

    N-Queens II beats 100%

--- Cplus/N-QueensII.cpp
+++ Cplus/N-QueensII.cpp
@@ -1,38 +1,41 @@
-#include<iostream>
-#include<vector>
-#include<cmath>
+#include <iostream>
+#include <vector>
+#include <cmath>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int totalNQueens(int n) {
-		if(n<2)
-			return 1;
-		vector<int> queen(n);
- 		return totalNQueens(queen,0,0);
-    }
-
-	int totalNQueens(vector<int>& queen,int row,int column)
+	int totalNQueens(int n)
 	{
-		if(row==0&&column==queen.size())
-			return 0;
-		queen[row]=column;
-		for(int i=column;i<queen.size();i++)
-			if(queenCheck(queen,row,i))
-			{
-				queen[row]=i;
-				if(row==queen.size()-1)
-					return 1+totalNQueens(queen,row-1,queen[row-1]+1);
-				else
-					return totalNQueens(queen,row+1,0);
-			}
-		return totalNQueens(queen,row-1,queen[row-1]+1);
+		if (n < 2)
+			return 1;
+		vector<int> queen(n); //record queens in each row
+		return doTotalNQueens(queen, 0);
 	}
 
-	bool queenCheck(vector<int>& queen,int row,int column)
+	int doTotalNQueens(vector<int> &queen, int row)
 	{
-		for(int i=0;i<row;i++)
-			if(column==queen[i]||(abs(row-i)==abs(column-queen[i])))
+		int n = queen.size();
+		if (row >= n)
+			return 1;
+		int res = 0;
+		for (int j = 0; j < n; j++)
+		{
+			if (queenCheck(queen, row, j))
+			{
+				queen[row] = j;
+				res += doTotalNQueens(queen, row + 1);
+				queen[row] = 0;
+			}
+		}
+		return res;
+	}
+
+	bool queenCheck(vector<int> &queen, int row, int column)
+	{
+		for (int i = 0; i < row; i++)
+			if (column == queen[i] || (abs(row - i) == abs(column - queen[i])))
 				return false;
 		return true;
 	}
