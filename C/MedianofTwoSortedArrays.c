76088fa9ac7706d083a278a2f24c4716b62b65be
JumHorn <JumHorn@gmail.com>
2020-03-25 20:59:43 +0800

    Median of Two Sorted Arrays beats 97.94%

--- C/MedianofTwoSortedArrays.c
+++ C/MedianofTwoSortedArrays.c
@@ -1,29 +1,26 @@
 
 //merge
 //count the number of smaller half part
-double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size)
+double findMedianSortedArrays(int *nums1, int nums1Size, int *nums2, int nums2Size)
 {
-	int i = 0, j = 0, sum = nums1Size + nums2Size, count = sum / 2, last0, last1;
+	int i = 0, j = 0, sum = nums1Size + nums2Size, count = sum / 2, midleft, midright;
 	while (count != -1 && i < nums1Size && j < nums2Size)
 	{
-		last1 = (nums1[i] <= nums2[j] ? nums1[i++] : nums2[j++]);
+		midright = (nums1[i] <= nums2[j] ? nums1[i++] : nums2[j++]);
 		if (--count == 0)
-			last0 = last1;
+			midleft = midright;
 	}
 	while (count != -1 && i < nums1Size)
 	{
-		last1 = nums1[i++];
+		midright = nums1[i++];
 		if (--count == 0)
-			last0 = last1;
+			midleft = midright;
 	}
 	while (count != -1 && j < nums2Size)
 	{
-		last1 = nums2[j++];
+		midright = nums2[j++];
 		if (--count == 0)
-			last0 = last1;
+			midleft = midright;
 	}
-	if (sum % 2 == 1)
-		return last1;
-	return (last0 + last1) / 2.0;
+	return sum % 2 == 1 ? midright : (midleft + midright) / 2.0;
 }
-
