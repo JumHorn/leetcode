1117205917e1eb64bb1541f9bda48ac905bb88d5
JumHorn <JumHorn@gmail.com>
2020-03-26 20:51:46 +0800

    two ways to solve this match problem

--- C/RegularExpressionMatching.c
+++ C/RegularExpressionMatching.c
@@ -5,9 +5,26 @@
 /*
 1. create dp[s.size+1][p.size+1] array and dp[i][j] standard for s[0~i] is match p[0-j]
 2. p[j]=='.' dp[i+1][j+1] dp[i+1][j+1]=dp[i][j]
-   p[j]=='*' dp[i+1][j+1] p[j-1] is match s[i] dp[i + 1][j + 1] = dp[i][j - 1] else dp[i + 1][j + 1] = dp[i + 1][j] || dp[i + 1][j - 1]
+   p[j]=='*'
+   p[j-1] is match s[i]
+   dp[i + 1][j + 1] = dp[i][j + 1] || dp[i + 1][j - 1] || dp[i + 1][j]
+   p[j-1] is not match s[i]
+   dp[i + 1][j + 1] = dp[i + 1][j - 1]
    p[j]==(a-z) if s[i]==s[j] dp[i+1][j+1]=dp[i][j] else dp[i][j]=false
-3. initialize dp[1~i][0]=false dp[0][1~j]=false dp[0][0]=true
+3. initialize dp[0][0]=true dp[0][j]=(p[j] == '*' && dp[0][j - 1])
+
+using example tabular to show the dp function
+Y stand for true N stand for false
+s=aab
+p=c*a*b
+
+	0	c	*	a	*	b
+0	Y	N	Y	N	Y	N
+a	N	N	N	Y	Y	N
+a	N	N	N	N	Y	N
+b	N	N	N	N	N	Y
+
+if p[j]=='*' we will choose the previous two dp in the same row and the above one
 */
 
 bool isMatch(char *s, char *p)
--- Cplus/RegularExpressionMatching.cpp
+++ Cplus/RegularExpressionMatching.cpp
@@ -1,67 +1,41 @@
-#include<string>
+#include <string>
 using namespace std;
+/*
+match pattern using recusion
+match from right to left
 
-class Solution {
+follow up:
+can use memdp to optimize time complexity
+*/
+class Solution
+{
 public:
-    bool isMatch(string s, string p) {
-		int i=0;
-		while(i<(int)p.length()&&p[i]=='*')//escape preceding *
-			i++;
-		return isMatch(s,0,p,i);
-    }
-
-	bool isMatch(const string& s,int i,const string& p,int j)
+	bool isMatch(string s, string p)
 	{
-		if(i>=(int)s.length()&&j>=(int)p.length())
+		return doIsMatch(s, s.length() - 1, p, p.length() - 1);
+	}
+
+	bool doIsMatch(const string &s, int i, const string &p, int j)
+	{
+		if (i < 0 && j < 0)
 			return true;
-		if(j>=(int)p.length())
+		if (j < 0)
 			return false;
-		if(i>=(int)s.length())
+		if (i < 0)
 		{
-			if(p[j]!='*')
-			{
-				if(j+1>=(int)p.length()||p[j+1]!='*')
-					return false;
-                if(isMatch(s,i,p,j+2))
-                    return true;
-				j++;
-			}
-			while(j<(int)p.length()&&p[j]=='*')
-				j++;
-            return isMatch(s,i,p,j);
+			while (j >= 0 && p[j] == '*')
+				j -= 2;
+			return j < 0;
 		}
-		if(p[j]=='.')
-        {
-            if(j+1<(int)p.length()&&p[j+1]=='*')
-                if(isMatch(s,i,p,j+2))
-                    return true;
-			return isMatch(s,i+1,p,j+1);
-        }
-		if(p[j]=='*')
+		if (p[j] == '.' || p[j] == s[i])
+			return doIsMatch(s, i - 1, p, j - 1);
+		if (p[j] == '*')
 		{
-			char preceding=p[j-1];
-			while(j<(int)p.length()&&p[j]=='*')//skip continous *
-				j++;
-            if(isMatch(s,i,p,j))
-				return true;
-			for(int k=i;k<(int)s.length();k++)
-			{
-                if(preceding!=s[k]&&preceding!='.')
-					break;
-				if(isMatch(s,k+1,p,j))
-					return true;
-			}
-			return false;
+			if (p[j - 1] == '.' || p[j - 1] == s[i])
+				return doIsMatch(s, i - 1, p, j) || doIsMatch(s, i, p, j - 2) || doIsMatch(s, i - 1, p, j - 2);
+			else
+				return doIsMatch(s, i, p, j - 2);
 		}
-		if(s[i]!=p[j])
-		{
-			if(j+1<(int)p.length()&&p[j+1]=='*')
-				return isMatch(s,i,p,j+2);
-			return false;
-		}
-        if(j+1<(int)p.length()&&p[j+1]=='*')
-            if(isMatch(s,i,p,j+2))
-                return true;
-		return isMatch(s,i+1,p,j+1);
+		return false;
 	}
 };
