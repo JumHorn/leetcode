ac899c1561b4516f087f1dc020fb4371230fe0ea
JumHorn <JumHorn@gmail.com>
2020-03-27 06:27:36 +0800

    Merge k Sorted Lists beats 31.56%

--- Cplus/MergekSortedLists.cpp
+++ Cplus/MergekSortedLists.cpp
@@ -1,41 +1,33 @@
-#include<vector>
-#include<queue>
+#include <vector>
+#include <queue>
 using namespace std;
 
 //Definition for singly-linked list.
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+struct ListNode
+{
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
 };
 
-class Solution {
+class Solution
+{
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        ListNode* res=NULL,*tmp=NULL;
-        if(lists.empty())
-            return res;
-        priority_queue<pair<int,ListNode*> > q;
-		for(int i=0;i<(int)lists.size();i++)
-            if(lists[i]!=NULL)
-			    q.push({-lists[i]->val,lists[i]});
-        if(q.empty())
-            return res;
-		res=q.top().second;
-		q.pop();
-		if(res->next!=NULL)
-			q.push({-res->next->val,res->next});
-		tmp=res;
-		while(!q.empty())
+	ListNode *mergeKLists(vector<ListNode *> &lists)
+	{
+		ListNode *dummy = new ListNode(0), *res = dummy;
+		priority_queue<pair<int, ListNode *>> q;
+		for (int i = 0; i < (int)lists.size(); i++)
+			if (lists[i] != NULL)
+				q.push({-lists[i]->val, lists[i]});
+		while (!q.empty())
 		{
-			tmp->next=q.top().second;
-			tmp=tmp->next;
+			dummy->next = q.top().second;
+			dummy = dummy->next;
 			q.pop();
-			if(tmp->next!=NULL)
-				q.push({-tmp->next->val,tmp->next});
+			if (dummy->next != NULL)
+				q.push({-dummy->next->val, dummy->next});
 		}
-		return res;
-    }
+		return res->next;
+	}
 };
-
-
