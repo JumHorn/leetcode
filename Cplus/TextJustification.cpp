1b7b6c1212a63a2626eeaf53a660969d9af16d8b
JumHorn <JumHorn@gmail.com>
2020-03-26 21:06:09 +0800

    Formatted

--- Cplus/TextJustification.cpp
+++ Cplus/TextJustification.cpp
@@ -1,55 +1,57 @@
-#include<vector>
-#include<string>
-#include<algorithm>
+#include <vector>
+#include <string>
+#include <algorithm>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<string> fullJustify(vector<string>& words, int maxWidth) {
+	vector<string> fullJustify(vector<string> &words, int maxWidth)
+	{
 		vector<string> res;
-		string tmp(maxWidth,' ');		
-		for(int i=0;i<(int)words.size();)
+		string tmp(maxWidth, ' ');
+		for (int i = 0; i < (int)words.size();)
 		{
-			fill(tmp.begin(),tmp.end(),' ');
-			int len=words[i].size();
-			int j=i;
-			while(++j<(int)words.size()&&(int)(len+words[j].size()+1)<=maxWidth)
-				len+=words[j].size()+1;
-			if(j==(int)words.size())
+			fill(tmp.begin(), tmp.end(), ' ');
+			int len = words[i].size();
+			int j = i;
+			while (++j < (int)words.size() && (int)(len + words[j].size() + 1) <= maxWidth)
+				len += words[j].size() + 1;
+			if (j == (int)words.size())
 			{
-				copy(words[i].begin(),words[i].end(),tmp.begin());
-				int start=words[i].size();
-				for(int k=i+1;k<j;k++)
+				copy(words[i].begin(), words[i].end(), tmp.begin());
+				int start = words[i].size();
+				for (int k = i + 1; k < j; k++)
 				{
-					start+=1;
-					copy(words[k].begin(),words[k].end(),tmp.begin()+start);
-					start+=words[k].size();
+					start += 1;
+					copy(words[k].begin(), words[k].end(), tmp.begin() + start);
+					start += words[k].size();
 				}
-                res.push_back(tmp);
+				res.push_back(tmp);
 				break;
 			}
-			int num=j-i;//the number of words in one line
-			if(num==1)
-				copy(words[i].begin(),words[i].end(),tmp.begin());
+			int num = j - i; //the number of words in one line
+			if (num == 1)
+				copy(words[i].begin(), words[i].end(), tmp.begin());
 			else
 			{
-				int space=(maxWidth-len)/(num-1)+1;
-				int bigspace=(maxWidth-len)%(num-1);
-				copy(words[i].begin(),words[i].end(),tmp.begin());
-				int start=words[i].size();
-				for(int k=i+1;k<j;k++)
+				int space = (maxWidth - len) / (num - 1) + 1;
+				int bigspace = (maxWidth - len) % (num - 1);
+				copy(words[i].begin(), words[i].end(), tmp.begin());
+				int start = words[i].size();
+				for (int k = i + 1; k < j; k++)
 				{
-					if(--bigspace>=0)
-						start+=space+1;
+					if (--bigspace >= 0)
+						start += space + 1;
 					else
-						start+=space;
-					copy(words[k].begin(),words[k].end(),tmp.begin()+start);
-					start+=words[k].size();
+						start += space;
+					copy(words[k].begin(), words[k].end(), tmp.begin() + start);
+					start += words[k].size();
 				}
 			}
 			res.push_back(tmp);
-			i=j;
+			i = j;
 		}
 		return res;
-    }
+	}
 };
