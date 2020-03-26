afe3852a373e471782a42807210c648322f16f2e
JumHorn <JumHorn@gmail.com>
2020-03-26 21:31:18 +0800

    Formatted

--- Cplus/IntegertoEnglishWords.cpp
+++ Cplus/IntegertoEnglishWords.cpp
@@ -1,89 +1,91 @@
-#include<string>
-#include<unordered_map>
+#include <string>
+#include <unordered_map>
 using namespace std;
 
-class Solution {
-	unordered_map<int,string> radix;
+class Solution
+{
+	unordered_map<int, string> radix;
+
 public:
 	Solution()
 	{
-        radix[0]="Zero";
-        radix[1]="One";
-        radix[2]="Two";
-        radix[3]="Three";
-        radix[4]="Four";
-        radix[5]="Five";
-        radix[6]="Six";
-        radix[7]="Seven";
-        radix[8]="Eight";
-        radix[9]="Nine";
-        radix[10]="Ten";
-        radix[11]="Eleven";
-        radix[12]="Twelve";
-        radix[13]="Thirteen";
-        radix[14]="Fourteen";
-        radix[15]="Fifteen";
-        radix[16]="Sixteen";
-        radix[17]="Seventeen";
-        radix[18]="Eighteen";
-        radix[19]="Nineteen";
+		radix[0] = "Zero";
+		radix[1] = "One";
+		radix[2] = "Two";
+		radix[3] = "Three";
+		radix[4] = "Four";
+		radix[5] = "Five";
+		radix[6] = "Six";
+		radix[7] = "Seven";
+		radix[8] = "Eight";
+		radix[9] = "Nine";
+		radix[10] = "Ten";
+		radix[11] = "Eleven";
+		radix[12] = "Twelve";
+		radix[13] = "Thirteen";
+		radix[14] = "Fourteen";
+		radix[15] = "Fifteen";
+		radix[16] = "Sixteen";
+		radix[17] = "Seventeen";
+		radix[18] = "Eighteen";
+		radix[19] = "Nineteen";
 
-        radix[20]="Twenty";
-        radix[30]="Thirty";
-        radix[40]="Forty";
-        radix[50]="Fifty";
-        radix[60]="Sixty";
-        radix[70]="Seventy";
-        radix[80]="Eighty";
-        radix[90]="Ninety";
-		radix[100]="Hundred";
+		radix[20] = "Twenty";
+		radix[30] = "Thirty";
+		radix[40] = "Forty";
+		radix[50] = "Fifty";
+		radix[60] = "Sixty";
+		radix[70] = "Seventy";
+		radix[80] = "Eighty";
+		radix[90] = "Ninety";
+		radix[100] = "Hundred";
 
-		radix[1000]="Thousand";
-		radix[1000000]="Million";
-		radix[1000000000]="Billion";
+		radix[1000] = "Thousand";
+		radix[1000000] = "Million";
+		radix[1000000000] = "Billion";
 	}
 
-    string numberToWords(int num) 
+	string numberToWords(int num)
 	{
-		if(num==0)
+		if (num == 0)
 			return radix[num];
 		string res;
-		int base=1000000000;
-		for(int i=0;i<4;i++)
+		int base = 1000000000;
+		for (int i = 0; i < 4; i++)
 		{
-			int tmp=num/base;
-			if(tmp!=0)
-            {
-                if(base!=1)
-				    res+=" "+numberToStr(tmp)+" "+radix[base];
-                else
-				    res+=" "+numberToStr(tmp);
-            }
-			num%=base;
-			base/=1000;
+			int tmp = num / base;
+			if (tmp != 0)
+			{
+				if (base != 1)
+					res += " " + numberToStr(tmp) + " " + radix[base];
+				else
+					res += " " + numberToStr(tmp);
+			}
+			num %= base;
+			base /= 1000;
 		}
 		return res.substr(1);
-    }
+	}
 
 	string numberToStr(int num)
 	{
 		string res;
-		int base=100;
-		int tmp=num/base;
-		if(tmp>0)
-			res+=" "+radix[tmp]+" "+radix[base];
-		tmp=num%base;
-		if(tmp>0&&tmp<=20)
+		int base = 100;
+		int tmp = num / base;
+		if (tmp > 0)
+			res += " " + radix[tmp] + " " + radix[base];
+		tmp = num % base;
+		if (tmp > 0 && tmp <= 20)
 		{
-			res+=" "+radix[tmp];
+			res += " " + radix[tmp];
 			return res.substr(1);
 		}
-		base/=10;
-		if(tmp/base>0)
-			res+=" "+radix[tmp/base*10];
-		tmp%=base;
-		if(tmp>0)
-			res+=" "+radix[tmp];
+		base /= 10;
+		if (tmp / base > 0)
+			res += " " + radix[tmp / base * 10];
+		tmp %= base;
+		if (tmp > 0)
+			res += " " + radix[tmp];
 		return res.substr(1);
 	}
 };
--- Cplus/TagValidator.cpp
+++ Cplus/TagValidator.cpp
@@ -1,35 +1,37 @@
-#include<string>
-#include<stack>
-#include<cctype>
+#include <string>
+#include <stack>
+#include <cctype>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool isValid(string code) {
-		if(code.empty()||code[0]!='<')
+	bool isValid(string code)
+	{
+		if (code.empty() || code[0] != '<')
 			return false;
-		int index=0,N=code.length();
-        stack<string> tag;
-		while(index<N)
+		int index = 0, N = code.length();
+		stack<string> tag;
+		while (index < N)
 		{
-			if(code[index]=='<')
+			if (code[index] == '<')
 			{
 				++index;
-				if(index<N&&code[index]=='!')
+				if (index < N && code[index] == '!')
 				{
-                    if(tag.empty())
-                        return false;
-					++index;
-					if(code.substr(index,7)!="[CDATA[")
+					if (tag.empty())
 						return false;
-					index+=7;
-					while(true)
+					++index;
+					if (code.substr(index, 7) != "[CDATA[")
+						return false;
+					index += 7;
+					while (true)
 					{
-						while(index<N&&code[index]!=']')
+						while (index < N && code[index] != ']')
 							index++;
-						if(index<N)
+						if (index < N)
 						{
-							if(code.substr(index,3)=="]]>")
+							if (code.substr(index, 3) == "]]>")
 								break;
 						}
 						else
@@ -37,49 +39,49 @@ public:
 						++index;
 					}
 				}
-				else if(index<N&&code[index]=='/')
+				else if (index < N && code[index] == '/')
 				{
-					if(tag.empty())
+					if (tag.empty())
 						return false;
 					++index;
-					int i=index,len;
-					while(i<N&&code[i]!='>')
+					int i = index, len;
+					while (i < N && code[i] != '>')
 					{
-						len=i-index+1;
-						if(len>(int)tag.top().length()||code[i]!=tag.top()[len-1])
+						len = i - index + 1;
+						if (len > (int)tag.top().length() || code[i] != tag.top()[len - 1])
 							return false;
 						i++;
 					}
-					len=i-index;
-					if(len!=(int)tag.top().length())
+					len = i - index;
+					if (len != (int)tag.top().length())
 						return false;
 					tag.pop();
-					index=++i;
-                    if(tag.empty()&&index!=N)
-                        return false;
+					index = ++i;
+					if (tag.empty() && index != N)
+						return false;
 				}
 				else
 				{
-					int i=index,len;
-					while(i<N&&code[i]!='>')
+					int i = index, len;
+					while (i < N && code[i] != '>')
 					{
-						len=i-index+1;
-						if(code[i]>'Z'||code[i]<'A')
+						len = i - index + 1;
+						if (code[i] > 'Z' || code[i] < 'A')
 							return false;
 						i++;
-						if(len>9)
+						if (len > 9)
 							return false;
 					}
-					len=i-index;
-					if(i==N||len<1||len>9)
+					len = i - index;
+					if (i == N || len < 1 || len > 9)
 						return false;
-					tag.push(code.substr(index,len));
-					index=++i;
+					tag.push(code.substr(index, len));
+					index = ++i;
 				}
 			}
 			else
 				++index;
 		}
 		return tag.empty();
-    }
+	}
 };
