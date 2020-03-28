f2d571e77597a4978fcb1146d6fc32172f0d4176
JumHorn <JumHorn@gmail.com>
2020-03-28 11:22:17 +0800

    Reverse Linked List II beats 100%

--- C/ReverseLinkedListII.c
+++ C/ReverseLinkedListII.c
@@ -1,47 +1,37 @@
 
-
 //Definition for singly-linked list.
 struct ListNode
 {
 	int val;
-	struct ListNode* next;
+	struct ListNode *next;
 };
 
-struct ListNode* reverseBetween(struct ListNode* head, int m, int n)
+struct ListNode *reverseBetween(struct ListNode *head, int m, int n)
 {
 	if (n == m)
 		return head;
-	struct ListNode *end = head, *data = head, *tail = head, *res = head;
+	struct ListNode *res = head, *tail = 0, *data = head;
+	for (int i = 2; i < m; i++)
+		head = head->next;
+	if (m != 1)
+		data = head->next;
+	tail = data->next;
+	for (int i = 0; i < n - m; i++)
+	{
+		struct ListNode *tmp = tail->next;
+		tail->next = data;
+		data = tail;
+		tail = tmp;
+	}
 	if (m == 1)
 	{
-		end = end->next;
-		for (int i = 0; i < n - m; i++)
-		{
-			struct ListNode* tmp = end;
-			end = end->next;
-			tmp->next = tail;
-			tail = tmp;
-		}
-		head->next = end;
-		res = tail;
+		head->next = tail;
+		res = data;
 	}
 	else
 	{
-		for (int i = 2; i < m; i++)
-			head = head->next;
-		data = head->next;
-		tail = data;
-		end = data->next;
-		for (int i = 0; i < n - m; i++)
-		{
-			struct ListNode* tmp = end;
-			end = end->next;
-			tmp->next = tail;
-			tail = tmp;
-		}
-		head->next = tail;
-		data->next = end;
+		head->next->next = tail;
+		head->next = data;
 	}
 	return res;
 }
-
