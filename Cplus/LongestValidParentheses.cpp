62bde7e560baa03908716dae4c4a1b553f937cfd
JumHorn <JumHorn@gmail.com>
2020-03-29 09:33:21 +0800

    Longest Valid Parentheses beats 95.84%

--- Cplus/LongestValidParentheses.cpp
+++ Cplus/LongestValidParentheses.cpp
@@ -1,25 +1,40 @@
-#include<string>
-#include<vector>
-#include<algorithm>
+#include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-class Solution {
+/*
+dp[i] ith index has length dp[i]
+if i+1 ) has matched ( dp[i+1]=dp[i]+2+dp[j-dp[i]]
+else dp[i+1]=0
+
+initialization
+dp[0]=0
+*/
+
+class Solution
+{
 public:
-    int longestValidParentheses(string s) {
-        if(s.empty())
-            return 0;
-		vector<int> dp(s.length());        
-		for(int i=1;i<(int)s.length();i++)
+	int longestValidParentheses(string s)
+	{
+		if (s.empty())
+			return 0;
+		int len = s.length(), res = 0;
+		vector<int> dp(len + 1);
+		for (int i = 1; i < len; i++)
 		{
-			if(s[i]==')')
+			if (s[i] == ')')
 			{
-				int j=i-1;
-				while(j>=0&&dp[j]!=0)
-					j-=dp[j];
-				if(j>=0&&s[j]=='(')
-					dp[i]=i-j+1+(j>=1?dp[j-1]:0);
+				int j = i - 1;
+				if (j >= 0 && dp[j + 1] != 0)
+					j -= dp[j + 1];
+				if (j >= 0 && s[j] == '(')
+				{
+					dp[i + 1] = i - j + 1 + dp[j];
+					res = max(res, dp[i + 1]);
+				}
 			}
 		}
-        return *max_element(dp.begin(),dp.end());
-    }
+		return res;
+	}
 };
