3f3d88a8041840f00806788a9014951b1be5d088
JumHorn <JumHorn@gmail.com>
2020-03-31 06:33:00 +0800

    Wildcard Matching beats 6.67%

--- C/WildcardMatching.c
+++ C/WildcardMatching.c
@@ -1,11 +1,15 @@
 #include <string.h>
 
-bool isMatch(char* s, char* p)
+#define MAXLEN 1501
+
+bool doIsMatch(char* s, int i, char* p, int j, int (*dp)[MAXLEN])
 {
+	if (dp[i][j] != -1)
+		return dp[i][j];
 	if (!*s && !*p)
-		return true;
+		return dp[i][j] = 1;
 	if (!*p)
-		return false;
+		return dp[i][j] = 0;
 	if (!*s)
 	{
 		while (*p && *p == '*')
@@ -13,9 +17,15 @@ bool isMatch(char* s, char* p)
 		return !*p;
 	}
 	if (*p == '?' || *p == *s)
-		return isMatch(s + 1, p + 1);
+		return dp[i][j] = doIsMatch(s + 1, i + 1, p + 1, j + 1, dp);
 	if (*p == '*')
-		return isMatch(s + 1, p) || isMatch(s, p + 1) || isMatch(s + 1, p + 1);
-	return false;
+		return dp[i][j] = doIsMatch(s + 1, i + 1, p, j, dp) || doIsMatch(s, i, p + 1, j + 1, dp) || doIsMatch(s + 1, i + 1, p + 1, j + 1, dp);
+	return dp[i][j] = 0;
 }
 
+bool isMatch(char* s, char* p)
+{
+	int dp[MAXLEN][MAXLEN];
+	memset(dp, -1, sizeof(dp));
+	return doIsMatch(s, 0, p, 0, dp);
+}
