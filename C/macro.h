3a5467d6475d067ef614d9008dcae77ed04f4868
JumHorn <JumHorn@gmail.com>
2020-04-03 07:03:05 +0800

    add triple min and triple max

--- C/macro.h
+++ C/macro.h
@@ -3,6 +3,9 @@
 #define swap(a, b) ((a) ^= (b) ^= (a) ^= (b))
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
 
+#define trimax(a, b, c) (max(max(a, b), c))	 //the maximum of a,b,c
+#define trimin(a, b, c) (min(min(a, b), c))	 //the minimum of a,b,c
+
 /*
 //cmp function don't consider overflow
 int cmp(const void* lhs,const void* rhs)
