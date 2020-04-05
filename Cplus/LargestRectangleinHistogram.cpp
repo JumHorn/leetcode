1109a4424c3e76b8846dd2106c5b135957dbc6bf
JumHorn <JumHorn@gmail.com>
2020-04-05 23:27:08 +0800

    formatted

--- Cplus/LargestRectangleinHistogram.cpp
+++ Cplus/LargestRectangleinHistogram.cpp
@@ -1,27 +1,29 @@
-#include<vector>
-#include<algorithm>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int largestRectangleArea(vector<int>& heights) {
-		return largestRectangleArea(heights,0,heights.size()-1);
-    }
-
-	int largestRectangleArea(vector<int>& heights,int i,int j)
+	int largestRectangleArea(vector<int> &heights)
 	{
-		if(i>j)
+		return divide(heights, 0, heights.size() - 1);
+	}
+
+	int divide(vector<int> &heights, int i, int j)
+	{
+		if (i > j)
 			return 0;
-		int m=i,tmp=INT_MAX,res=0;
-		for(int k=i;k<=j;k++)
+		int m = i, tmp = INT_MAX, res = 0;
+		for (int k = i; k <= j; k++) //find the minimum index
 		{
-			if(tmp>heights[k])
+			if (tmp > heights[k])
 			{
-				tmp=heights[k];
-				m=k;
+				tmp = heights[k];
+				m = k;
 			}
 		}
-		res=heights[m]*(j-i+1);
-		return max(res,max(largestRectangleArea(heights,i,m-1),largestRectangleArea(heights,m+1,j)));
+		res = heights[m] * (j - i + 1);
+		return max({res, divide(heights, i, m - 1), divide(heights, m + 1, j)});
 	}
 };
