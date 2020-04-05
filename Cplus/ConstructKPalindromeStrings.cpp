74a9c6ca5b90a5246d3b0bc7e7ff22dc4e6d40bd
JumHorn <JumHorn@gmail.com>
2020-04-06 06:16:19 +0800

    Construct K Palindrome Strings beats 42.86%

--- Cplus/ConstructKPalindromeStrings.cpp
+++ Cplus/ConstructKPalindromeStrings.cpp
@@ -6,5 +6,19 @@ class Solution
 public:
 	bool canConstruct(string s, int k)
 	{
+		int count[26] = {0}, single = 0, n = s.length();
+		if (k > n)
+			return false;
+		for (auto c : s)
+			++count[c - 'a'];
+		for (int i = 0; i < 26; i++)
+		{
+			if (count[i] % 2 == 1)
+			{
+				++single;
+				--count[i];
+			}
+		}
+		return k >= single;
 	}
 };
