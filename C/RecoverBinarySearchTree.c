48f3b03d978f252e7c6c798a54a4a261d6ace650
JumHorn <JumHorn@gmail.com>
2020-04-07 06:32:47 +0800

    Recover Binary Search Tree beats 76.32%

---
+++ C/RecoverBinarySearchTree.c
@@ -0,0 +1,43 @@
+//Definition for a binary tree node.
+struct TreeNode
+{
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+};
+
+bool inorder(struct TreeNode *root, struct TreeNode **pre)
+{
+	if (!root)
+		return false;
+	bool res = inorder(root->left, pre);
+	if (res)
+		return res;
+	if (*pre && root->val < (*pre)->val)
+		return true;
+	*pre = root;
+	return inorder(root->right, pre);
+}
+
+bool reverseInorder(struct TreeNode *root, struct TreeNode **pre)
+{
+	if (!root)
+		return false;
+	bool res = reverseInorder(root->right, pre);
+	if (res)
+		return res;
+	if (*pre && root->val > (*pre)->val)
+		return true;
+	*pre = root;
+	return reverseInorder(root->left, pre);
+}
+
+void recoverTree(struct TreeNode *root)
+{
+	struct TreeNode *left = 0, *right = 0;
+	inorder(root, &left);
+	reverseInorder(root, &right);
+	int tmp = left->val;
+	left->val = right->val;
+	right->val = tmp;
+}
