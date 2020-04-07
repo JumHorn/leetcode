ddfd4aa8a1f1fc53a57075b906e40d7bab526959
JumHorn <JumHorn@gmail.com>
2020-04-07 22:19:55 +0800

    DP space optimized

--- Cplus/InterleavingString.cpp
+++ Cplus/InterleavingString.cpp
@@ -14,15 +14,16 @@ public:
 		if (s2.empty())
 			return s1 == s3;
 		int M = s1.size(), N = s2.size();
-		vector<vector<bool>> dp(M + 1, vector<bool>(N + 1, false));
-		dp[0][0] = true;
-		for (int i = 0; i < M && s1[i] == s3[i]; i++)
-			dp[i + 1][0] = true;
+		vector<bool> dp(N + 1, false);
+		dp[0] = true;
 		for (int j = 0; j < N && s2[j] == s3[j]; j++)
-			dp[0][j + 1] = true;
+			dp[j + 1] = true;
 		for (int i = 0; i < M; i++)
+		{
+			dp[0] = (dp[0] && s1[i] == s3[i]);
 			for (int j = 0; j < N; j++)
-				dp[i + 1][j + 1] = (s1[i] == s3[i + j + 1] && dp[i][j + 1]) || (s2[j] == s3[i + j + 1] && dp[i + 1][j]);
-		return dp[M][N];
+				dp[j + 1] = (s1[i] == s3[i + j + 1] && dp[j + 1]) || (s2[j] == s3[i + j + 1] && dp[j]);
+		}
+		return dp[N];
 	}
 };
