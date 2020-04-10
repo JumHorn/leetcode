e74a4178829329901a464a245805e1b1f439d833
JumHorn <JumHorn@gmail.com>
2020-04-11 06:02:40 +0800

    time limited exceeded

--- Cplus/WordLadderII.cpp
+++ Cplus/WordLadderII.cpp
@@ -1,80 +1,92 @@
-#include<vector>
-#include<string>
-#include<set>
-#include<unordered_set>
-#include<map>
-#include<queue>
+#include <vector>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-	vector<vector<string> > findLadders(string beginWord, string endWord, vector<string>& wordList) {
-		map<string,unordered_set<string> > graph;
-		vector<vector<string> >  res;
-		unordered_set<string> visited;
-		unordered_set<string> words(wordList.begin(),wordList.end());
+	vector<vector<string>> findLadders(string beginWord, string endWord, vector<string> &wordList)
+	{
+		int n = wordList.size();
+		vector<vector<int>> graph(n + 1);
+		vector<int> seen(n + 1);
+		unordered_map<string, int> words; //{str,index}
+		for (int i = 0; i < n; i++)
+			words[wordList[i]] = i;
+		wordList.push_back(beginWord);
+		words[beginWord] = n;
+
+		//BFS
 		queue<string> q;
-		int depth=1;
+		seen[words[beginWord]] = 1;
+		int depth = 1;
 		q.push(beginWord);
-		bool flag=true;
-		while(!q.empty()&&flag)
+		bool flag = true;
+		unordered_set<int> seennew;
+		while (!q.empty() && flag)
 		{
-			int n=q.size();
-			depth+=1;
-			for(unordered_set<string>::iterator iter=visited.begin();iter!=visited.end();++iter)
-				words.erase(*iter);
-			visited.clear();
-			while(--n>=0)
+			++depth;
+			int size = q.size();
+			for (auto n : seennew)
+				seen[n] = 1;
+			seennew.clear();
+			while (--size >= 0)
 			{
-				string t=q.front();
-				for(int i=0;i<(int)t.length();i++)
+				string tmp = q.front();
+				for (int i = 0; i < (int)tmp.length(); i++)
 				{
-					for(char c='a';c<='z';c++)
+					for (char c = 'a'; c <= 'z'; c++)
 					{
-						t[i]=c;
-						if(words.find(t)!=words.end())
+						tmp[i] = c;
+						if (words.find(tmp) != words.end() && seen[words[tmp]] == 0)
 						{
-							graph[q.front()].insert(t);
-							q.push(t);
-							visited.insert(t);
-							if(t==endWord)
-								flag=false;
-						}	
+							seennew.insert(words[tmp]);
+							graph[words[q.front()]].push_back(words[tmp]);
+							q.push(tmp);
+							if (tmp == endWord)
+							{
+								flag = false;
+								break;
+							}
+						}
 					}
-					t=q.front();
+					tmp = q.front();
 				}
 				q.pop();
 			}
 		}
-		if(flag)
-			return vector<vector<string> >();
+		if (flag)
+			return {};
 
-		//BFS
-		visited.clear();
-		vector<string> tmp(depth);
-		tmp[0]=beginWord;
-		tmp[depth-1]=endWord;
-		findLadders(graph,res,tmp,visited,endWord,depth);
+		//DFS
+		vector<int> onepath = {words[beginWord]};
+		vector<vector<string>> res;
+		dfs(graph, wordList, res, onepath, words[endWord], depth);
 		return res;
 	}
+
 	//DFS
-	void findLadders(map<string,unordered_set<string> >& graph,vector<vector<string> >& res,vector<string>& tmp,unordered_set<string>& visited,const string& dst,int depth)
+	void dfs(vector<vector<int>> &graph, vector<string> &wordlist, vector<vector<string>> &res, vector<int> &onepath, int dst, int depth)
 	{
-		if(depth<=2)
+		if (depth <= 1)
 		{
-			if(graph[tmp[tmp.size()-depth]].find(dst)!=graph[tmp[tmp.size()-depth]].end())
-				res.push_back(tmp);
+			if (onepath.back() == dst)
+			{
+				res.push_back({});
+				for (auto n : onepath)
+					res.back().push_back(wordlist[n]);
+			}
 			return;
 		}
-		for(unordered_set<string>::iterator iter=graph[tmp[tmp.size()-depth]].begin();iter!=graph[tmp[tmp.size()-depth]].end();++iter)
+		int n = graph.size();
+		for (auto to : graph[onepath.back()])
 		{
-			if(visited.find(*iter)==visited.end())
-			{
-				visited.insert(*iter);
-				tmp[tmp.size()-depth+1]=*iter;
-				findLadders(graph,res,tmp,visited,dst,depth-1);
-				visited.erase(visited.find(*iter));
-			}
+			onepath.push_back(to);
+			dfs(graph, wordlist, res, onepath, dst, depth - 1);
+			onepath.pop_back();
 		}
 	}
 };
