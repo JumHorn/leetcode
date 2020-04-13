376bc36919809f7b6a04fcf9a65f88a6e08c93b1
JumHorn <JumHorn@gmail.com>
2020-04-14 07:07:41 +0800

    formatted

--- Cplus/Candy.cpp
+++ Cplus/Candy.cpp
@@ -1,21 +1,23 @@
-#include<vector>
-#include<algorithm>
-#include<numeric>
+#include <vector>
+#include <algorithm>
+#include <numeric>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int candy(vector<int>& ratings) {
-		int n=ratings.size();
-		vector<int> res(n,1);
-		for(int i=1;i<n;i++)
-			if(ratings[i-1]<ratings[i])
-				res[i]+=res[i-1];
+	int candy(vector<int> &ratings)
+	{
+		int n = ratings.size();
+		vector<int> res(n, 1);
+		for (int i = 1; i < n; i++)
+			if (ratings[i - 1] < ratings[i])
+				res[i] += res[i - 1];
 
-		for(int i=n-1;i>0;i--)
-			if(ratings[i-1]>ratings[i])
-				res[i-1]=max(res[i-1],res[i]+1);
+		for (int i = n - 2; i >= 0; i--)
+			if (ratings[i] > ratings[i + 1])
+				res[i] = max(res[i], res[i + 1] + 1);
 
-		return accumulate(res.begin(),res.end(),0);
+		return accumulate(res.begin(), res.end(), 0);
 	}
 };
