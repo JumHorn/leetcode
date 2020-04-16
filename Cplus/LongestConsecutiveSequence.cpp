d978e9f9a00c9a7d3f7bd070e2d4e88fc427fddc
JumHorn <JumHorn@gmail.com>
2020-04-17 06:33:27 +0800

    Longest Consecutive Sequence beats 36.69%

--- Cplus/LongestConsecutiveSequence.cpp
+++ Cplus/LongestConsecutiveSequence.cpp
@@ -1,46 +1,47 @@
-#include<vector>
-#include<unordered_map>
-#include<algorithm>
-#include<unordered_set>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <unordered_set>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int longestConsecutive(vector<int>& nums) {
-        if(nums.empty())
-            return 0;
-        unordered_map<int,int> head,tail;
-		unordered_set<int> visited;
-		int res=1;
-		for(int i=0;i<(int)nums.size();i++)
+	int longestConsecutive(vector<int> &nums)
+	{
+		unordered_map<int, int> head, tail;
+		unordered_set<int> seen;
+		int res = 0;
+		for (auto n : nums)
 		{
-			if(visited.find(nums[i])!=visited.end())
+			if (seen.find(n) != seen.end())
 				continue;
-			visited.insert(nums[i]);
-			if(head.find(nums[i])==head.end()&&tail.find(nums[i])==tail.end())
+			seen.insert(n);
+			if (head.find(n + 1) != head.end() && tail.find(n - 1) != tail.end())
 			{
-				head[nums[i]-1]=nums[i]+1;
-				tail[nums[i]+1]=nums[i]-1;
+				head[tail[n - 1]] = head[n + 1];
+				tail[head[n + 1]] = tail[n - 1];
+				res = max(res, head[tail[n - 1]] - tail[n - 1] + 1);
 			}
-			else if(head.find(nums[i])!=head.end()&&tail.find(nums[i])==tail.end())
+			else if (head.find(n + 1) != head.end())
 			{
-				head[nums[i]-1]=head[nums[i]];
-				tail[head[nums[i]]]=nums[i]-1;
-				res=max(res,head[nums[i]]-nums[i]);
+				head[n] = head[n + 1];
+				tail[head[n + 1]] = n;
+				res = max(res, head[n] - n + 1);
 			}
-			else if(head.find(nums[i])==head.end()&&tail.find(nums[i])!=tail.end())
+			else if (tail.find(n - 1) != tail.end())
 			{
-				tail[nums[i]+1]=tail[nums[i]];
-				head[tail[nums[i]]]=nums[i]+1;
-				res=max(res,nums[i]-tail[nums[i]]);
+				tail[n] = tail[n - 1];
+				head[tail[n - 1]] = n;
+				res = max(res, n - tail[n] + 1);
 			}
 			else
 			{
-				head[tail[nums[i]]]=head[nums[i]];
-				tail[head[nums[i]]]=tail[nums[i]];
-				res=max(res,head[nums[i]]-tail[nums[i]]-1);
+				head[n] = n;
+				tail[n] = n;
+				res = max(res, 1);
 			}
 		}
 		return res;
-    }
+	}
 };
