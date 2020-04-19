d6db269be9a900febf4255f16a057b2c0cd416da
JumHorn <JumHorn@gmail.com>
2020-04-19 16:49:25 +0800

    Number of Ways to Paint N × 3 Grid beats 20.99%

--- Cplus/NumberofWaystoPaintNx3Grid.cpp
+++ Cplus/NumberofWaystoPaintNx3Grid.cpp
@@ -6,26 +6,54 @@ class Solution
 public:
 	int numOfWays(int n)
 	{
-		vector<vector<int>> grid(n + 1, vector<int>(4));
-		return memdp(grid, 0);
-	}
-
-	int memdp(vector<vector<int>> &grid, int k)
-	{
-		int n = grid.size(), i = k / 3 + 1, j = k % 3 + 1;
-		if (k == (n - 1) * 3)
-			return 1;
-		int res = 0;
-		for (int color = 1; color <= 3; color++)
+		//get initial state
+		vector<vector<int>> init;
+		for (int i = 0; i < 3; i++)
 		{
-			if (color == grid[i - 1][j] || color == grid[i][j - 1])
-				continue;
-			grid[i][j] = color;
-			res = (res + memdp(grid, k + 1)) % MOD;
+			for (int j = 0; j < 3; j++)
+				for (int k = 0; k < 3; k++)
+					if (i != j && j != k)
+						init.push_back({i, j, k});
 		}
+		int size = init.size();
+		if (n == 1)
+			return size;
+		//get state transition
+		vector<vector<int>> state(size);
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				if (transState(init[i], init[j]))
+					state[i].push_back(j);
+			}
+		}
+		//dp
+		vector<int> dp(size, 1);
+		for (int i = 1; i < n; i++)
+		{
+			vector<int> tmp(size);
+			for (int j = 0; j < size; j++)
+			{
+				for (int k = 0; k < (int)state[j].size(); k++)
+					tmp[state[j][k]] = (tmp[state[j][k]] + dp[j]) % MOD;
+			}
+			dp = tmp;
+		}
+		int res = 0;
+		for (auto n : dp)
+			res = (res + n) % MOD;
 		return res;
 	}
 
 private:
+	bool transState(vector<int>& a, vector<int>& b)
+	{
+		for (int i = 0; i < (int)a.size(); i++)
+			if (a[i] == b[i])
+				return false;
+		return true;
+	}
+
 	static const int MOD = 1e9 + 7;
 };
