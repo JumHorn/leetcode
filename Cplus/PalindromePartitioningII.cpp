1214c7adf26d562b4e90eb10fad3028828c2ebf5
JumHorn <JumHorn@gmail.com>
2020-04-19 16:00:07 +0800

    formatted

--- Cplus/PalindromePartitioningII.cpp
+++ Cplus/PalindromePartitioningII.cpp
@@ -1,31 +1,31 @@
-#include<string>
-#include<vector>
+#include <string>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int minCut(string s) {
-        if(s.empty())
-            return 0;
-		int M=s.length();
-		vector<int> dp(M+1,INT_MAX);
-        dp[0]=0;
-		for(int i=0;i<M;i++)
-			for(int j=0;j<=i;j++)
-			{
-				if(isPalindrome(s,j,i))
-				{
-					dp[i+1]=min(dp[i+1],dp[j]+1);
-				}
-			}
-	    return dp.back()-1;
-    }
-
-	bool isPalindrome(const string& s,int j,int i)
+	int minCut(string s)
 	{
-		while(j<i)
+		if (s.empty())
+			return 0;
+		int n = s.length();
+		vector<int> dp(n + 1, INT_MAX);
+		dp[0] = 0;
+		for (int i = 0; i < n; i++)
 		{
-			if(s[j++]!=s[i--])
+			for (int j = 0; j <= i; j++)
+				if (isPalindrome(s, j, i))
+					dp[i + 1] = min(dp[i + 1], dp[j] + 1);
+		}
+		return dp.back() - 1;
+	}
+
+	bool isPalindrome(const string &s, int j, int i)
+	{
+		while (j < i)
+		{
+			if (s[j++] != s[i--])
 				return false;
 		}
 		return true;
