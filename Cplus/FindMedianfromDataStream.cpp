c92e201eeec107b418b6150938e6b2f9bb19e11f
JumHorn <JumHorn@gmail.com>
2020-04-23 06:40:00 +0800

    Find Median from Data Stream beats 32.92%

--- Cplus/FindMedianfromDataStream.cpp
+++ Cplus/FindMedianfromDataStream.cpp
@@ -1,204 +1,57 @@
-#include<vector>
-#include<map>
-#include<algorithm>
+#include <queue>
 using namespace std;
 
-class MedianFinder {
-	vector<int> v;
-	map<int,int> small;
-	map<int,int> large;
-	pair<int,int> lhs;
-	pair<int,int> rhs;
-	bool even;
+class MedianFinder
+{
 public:
-    /** initialize your data structure here. */
-    MedianFinder() :v(101){
-		even=true;
-    }
-    
-    void addNum(int num) {
-        if(num<0)
-			++small[num];
-		else if(num>100)
-			++large[num];
-		else
-			v[num]++;
-		
-		if(even)
+	/** initialize your data structure here. */
+	MedianFinder()
+	{
+		median = 0.0;
+		count = 0;
+	}
+
+	void addNum(int num)
+	{
+		int left, right = num;
+		++count;
+		if (count == 1)
 		{
-			even=!even;
-			if(lhs.second==0&&rhs.second==0)
-			{
-				lhs.first=rhs.first=num;
-				lhs.second=rhs.second=1;
-			}
-			else if(num<=lhs.first)
-			{
-				if(num<lhs.first)
-					rhs=lhs;
-				else
-				{
-					lhs.second++;
-					rhs=lhs;
-				}
-				
-			}
-			else if(num>=rhs.first)
-			{
-				lhs=rhs;
-			}
-			else
-			{
-				lhs.first=num;
-				lhs.second=1;
-				rhs=lhs;
-			}
+			median = num;
+			low.push(num);
+			return;
 		}
+		if (count % 2 == 0)
+			high.push(-num);
 		else
+			low.push(num);
+		left = low.top();
+		right = -high.top();
+		if (left > right)
 		{
-			even=!even;
-			if(num<lhs.first)
-			{
-				map<int,int>::iterator iter=large.find(lhs.first);
-				if(iter!=large.end())
-				{
-					if(lhs.second>1)
-					{
-						lhs.second--;
-						return;
-					}
-					else
-					{
-						if(iter!=large.begin())
-						{
-							--iter;
-							lhs.first=iter->first;
-							lhs.second=iter->second;
-							return;
-						}
-					}
-				}
-				for(int i=min(100,lhs.first);i>=0;i--)
-				{
-					if(v[i]!=0)
-					{
-						if(i==lhs.first)
-						{
-							if(lhs.second>1)
-							{
-								lhs.second--;
-								return;
-							}
-						}
-						else
-						{
-							lhs.first=i;
-							lhs.second=v[i];
-							return;
-						}
-					}
-				}
-				iter=small.find(lhs.first);
-				if(iter!=small.end())
-				{
-					if(lhs.second>1)
-					{
-						lhs.second--;
-						return;
-					}
-					else
-					{
-						if(iter!=small.begin())
-						{
-							--iter;
-							lhs.first=iter->first;
-							lhs.second=iter->second;
-							return;
-						}
-					}
-				}
-				else
-				{
-					iter=small.end();
-					--iter;
-					lhs.first=iter->first;
-					lhs.second=iter->second;
-				}
-				
-			}
-			else if(num>=rhs.first)
-			{
-				map<int,int>::iterator iter=small.find(rhs.first);
-				if(iter!=small.end())
-				{
-					if(rhs.second<iter->second)
-					{
-						rhs.second++;
-						return;
-					}
-					else
-					{
-						++iter;
-						if(iter!=small.end())
-						{
-							rhs.first=iter->first;
-							rhs.second=1;
-							return;
-						}
-					}
-				}
-				for(int i=max(0,rhs.first);i<=100;i++)
-				{
-					if(v[i]!=0)
-					{
-						if(rhs.first==i)
-						{
-							if(rhs.second<v[i])
-							{
-								rhs.second++;
-								return;
-							}
-						}
-						else
-						{
-							rhs.first=i;
-							rhs.second=1;
-							return;
-						}
-					}
-				}
-				iter=large.find(rhs.first);
-				if(iter!=large.end())
-				{
-					if(rhs.second<iter->second)
-					{
-						rhs.second++;
-						return;
-					}
-					else
-					{
-						++iter;
-						if(iter!=large.end())
-						{
-							rhs.first=iter->first;
-							rhs.second=1;
-							return;
-						}
-					}
-				}
-				else
-				{
-					iter=large.begin();
-					rhs.first=iter->first;
-					rhs.second=1;
-				}	
-			}
+			low.pop();
+			high.pop();
+			low.push(right);
+			high.push(-left);
 		}
-    }
-    
-    double findMedian() {
-        return (lhs.first+rhs.first)/2.0;
-    }
+		left = low.top();
+		right = -high.top();
+		if (count % 2 == 0)
+			median = (left + right) / 2.0;
+		else
+			median = left;
+	}
+
+	double findMedian()
+	{
+		return median;
+	}
+
+private:
+	double median;
+	priority_queue<int> low;
+	priority_queue<int> high;
+	int count;
 };
 
 /**
