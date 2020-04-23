2fcd7b75642941991056a639d7f9871b36330ec5
JumHorn <JumHorn@gmail.com>
2020-04-23 21:42:19 +0800

    Remove Invalid Parentheses beats 78.93%

--- Cplus/RemoveInvalidParentheses.cpp
+++ Cplus/RemoveInvalidParentheses.cpp
@@ -1,92 +1,85 @@
-#include<vector>
-#include<string>
-#include<unordered_set>
+#include <string>
+#include <unordered_set>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-	vector<string> removeInvalidParentheses(string s) {
-		vector<string> res;
-		int open=0,close=0,count=0;//open ( for remove close ) for remove
-		for(int i=0;i<(int)s.length();i++)
+	vector<string> removeInvalidParentheses(string s)
+	{
+		int invalid_open = 0, invalid_closed = 0, count = 0;
+		for (auto c : s)
 		{
-			if(s[i]=='(')
+			if (c == '(')
 				++count;
-			else if(s[i]==')')
-				--count;
-			if(count<0)
+			else if (c == ')')
 			{
-				close++;
-				count=0;
+				if (count > 0)
+					--count;
+				else
+					++invalid_closed;
 			}
-		}	
-		open=count;	
-		if(open==0&&close==0)
-		{
-			res.push_back(s);
-			return res;
 		}
-		if(open+close==(int)s.length())
-		{
-			res.push_back("");
-			return res;
-		}
-		unordered_set<string> result;
-		removeInvalidParentheses(result,s,open,close,0);
-		for(unordered_set<string>::iterator iter=result.begin();iter!=result.end();++iter)
-			res.push_back(*iter);
-		return res;
+		invalid_open = count;
+
+		unordered_set<string> res;
+		string possible;
+		dfs(res, possible, 0, invalid_open, invalid_closed, s, 0);
+		return vector<string>(res.begin(), res.end());
 	}
 
-	bool isValid(const string& s)
+	void dfs(unordered_set<string> &res, string &possible, int count, int open, int closed, const string &s, int index)
 	{
-		int count=0;
-		for(int i=0;i<(int)s.length();i++)
+		if (open < 0 || closed < 0 || count < 0)
+			return;
+		if (index > (int)s.length())
 		{
-			if(s[i]=='(')
-				++count;
-			else if(s[i]==')')
-				--count;
-			if(count<0||count>(int)s.length()/2)
-				return false;
-		}
-		return count==0;
-	}
-
-	void removeInvalidParentheses(unordered_set<string>& res,string s,int open,int close,int start)
-	{
-		if(open==0&&close==0)
-		{
-			if(isValid(s))
-				res.insert(s);
+			if (open == 0 && closed == 0 && count == 0)
+				res.insert(possible);
 			return;
 		}
-		for(int i=start;i<(int)s.length();i++)
+		if (open == 0 && closed == 0)
 		{
-			if(s[i]=='(')
-			{
-				if(open>0)
-				{
-					string tmp=s;
-					tmp.erase(tmp.begin()+i);
-					removeInvalidParentheses(res,tmp,open-1,close,i);
-				}
-			}
-			else if(s[i]==')')
-			{
-				if(close>0)
-				{
-					string tmp=s;
-					tmp.erase(tmp.begin()+i);
-					removeInvalidParentheses(res,tmp,open,close-1,i);
-				}
-			}
+			if (isValid(s, index, count))
+				res.insert(possible + s.substr(index));
+			return;
+		}
+		if (s[index] == '(')
+		{
+			dfs(res, possible, count, open - 1, closed, s, index + 1);
+			possible.push_back('(');
+			dfs(res, possible, count + 1, open, closed, s, index + 1);
+			possible.pop_back();
+		}
+		else if (s[index] == ')')
+		{
+			dfs(res, possible, count, open, closed - 1, s, index + 1);
+			possible.push_back(')');
+			dfs(res, possible, count - 1, open, closed, s, index + 1);
+			possible.pop_back();
+		}
+		else
+		{
+			possible.push_back(s[index]);
+			dfs(res, possible, count, open, closed, s, index + 1);
+			possible.pop_back();
 		}
 	}
-};
 
-int main()
-{
-	Solution sol;
-	sol.removeInvalidParentheses(")()(");
-}
+	bool isValid(const string &s, int index, int count)
+	{
+		for (int i = index; i < (int)s.length(); i++)
+		{
+			if (s[i] == '(')
+				++count;
+			else if (s[i] == ')')
+			{
+				--count;
+				if (count < 0)
+					return false;
+			}
+		}
+		return count == 0;
+	}
+};
