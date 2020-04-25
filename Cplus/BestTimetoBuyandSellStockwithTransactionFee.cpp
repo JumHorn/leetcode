2c23b4ccac2fd2668f40d95f7bd697fba6b14329
JumHorn <JumHorn@gmail.com>
2020-04-26 07:22:02 +0800

    formatted

--- Cplus/BestTimetoBuyandSellStockwithTransactionFee.cpp
+++ Cplus/BestTimetoBuyandSellStockwithTransactionFee.cpp
@@ -1,19 +1,23 @@
-#include<vector>
-#include<algorithm>
+#include <vector>
+#include <algorithm>
 using namespace std;
+
 /*
 buy[i]=sold[i-1]-price[i]
 sold[i]=buy[i-1]+price[i]-fee
 */
-class Solution {
+
+class Solution
+{
 public:
-    int maxProfit(vector<int>& prices, int fee) {
-		int buy=-prices.front(),sold=0;
-		for(int i=1;i<(int)prices.size();i++)
+	int maxProfit(vector<int> &prices, int fee)
+	{
+		int buy = -prices.front(), sold = 0;
+		for (int i = 1; i < (int)prices.size(); i++)
 		{
-			sold=max(buy+prices[i]-fee,sold);
-			buy=max(sold-prices[i],buy);
+			sold = max(sold, buy + prices[i] - fee);
+			buy = max(buy, sold - prices[i]);
 		}
 		return sold;
-    }
+	}
 };
