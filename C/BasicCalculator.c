8574f051f77c419460a62c3a74d65ac757fe98b5
JumHorn <JumHorn@gmail.com>
2020-04-25 23:39:57 +0800

    Basic Calculator beats 86.96%

---
+++ C/BasicCalculator.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+
+int doCalculate(char *s, int *index)
+{
+	int res = 0, flag = 1;
+	while (s[*index])
+	{
+		if (s[*index] == ')')
+			break;
+		if (s[*index] == '(')
+		{
+			++(*index);
+			res += flag * doCalculate(s, index);
+		}
+		else if (s[*index] == '+')
+			flag = 1;
+		else if (s[*index] == '-')
+			flag = -1;
+		else if (s[*index] != ' ')
+		{
+			int i = *index + 1;
+			while (s[i] <= '9' && s[i] >= '0')
+				i++;
+			char tmp = s[i];
+			s[i] = '\0';
+			res += flag * atoi(&s[*index]);
+			s[i] = tmp;
+			*index = i - 1;
+		}
+		++(*index);
+	}
+	return res;
+}
+
+int calculate(char *s)
+{
+	int index = 0;
+	return doCalculate(s, &index);
+}
