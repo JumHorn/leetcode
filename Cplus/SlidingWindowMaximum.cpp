d5cf3a5cd1877d53f19e507284b7376e5b2a4ebe
JumHorn <JumHorn@gmail.com>
2020-04-25 22:15:50 +0800

    Sliding Window Maximum beats 65.21%

--- Cplus/SlidingWindowMaximum.cpp
+++ Cplus/SlidingWindowMaximum.cpp
@@ -1,21 +1,29 @@
-#include<vector>
-#include<algorithm>
+#include <vector>
+#include <deque>
+#include <algorithm>
 using namespace std;
 
-class Solution {
+/*
+MonoQueue
+*/
+
+class Solution
+{
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        if(nums.empty())
-            return vector<int>();
-		vector<int> res(nums.size()-k+1);
-		res[0]=*max_element(nums.begin(),nums.begin()+k);
-		for(int i=1;i<(int)nums.size()-k+1;i++)
+	vector<int> maxSlidingWindow(vector<int> &nums, int k)
+	{
+		vector<int> res(nums.size() - k + 1);
+		deque<int> q;
+		for (int i = 0, j = 0; i < (int)nums.size(); i++)
 		{
-			if(nums[i-1]==res[i-1])
-				res[i]=*max_element(nums.begin()+i,nums.begin()+i+k);
-			else
-				res[i]=max(res[i-1],nums[i+k-1]);
+			while (!q.empty() && nums[q.back()] < nums[i])
+				q.pop_back();
+			while (!q.empty() && i - k >= q.front())
+				q.pop_front();
+			q.push_back(i);
+			if (i + 1 >= k)
+				res[j++] = nums[q.front()];
 		}
 		return res;
-    }
+	}
 };
