57487b29c4735b47cd6332b3a71096779e25bc5e
JumHorn <JumHorn@gmail.com>
2020-04-26 06:13:08 +0800

    Best Time to Buy and Sell Stock beats 92.76%

--- C/BestTimeToBuyandSellStock.c
+++ C/BestTimeToBuyandSellStock.c
@@ -1,20 +1,13 @@
-#include<stdio.h>
-int main()
-{
-	int maxProfit(int * prices,int pricesSize);
-}
 
-int maxProfit(int * prices,int pricesSize)
+int maxProfit(int* prices, int pricesSize)
 {
-	int i,min=2147483647,max=0;
-	for(i=0;i<pricesSize;i++)
+	int min = 2147483647, max = 0;
+	for (int i = 0; i < pricesSize; i++)
 	{
-                min=prices[i]>min?min:prices[i];
-		if(prices[i]-min>max)
-		{
-			max=prices[i]-min;
-		}
-
+		if (prices[i] < min)
+			min = prices[i];
+		if (prices[i] - min > max)
+			max = prices[i] - min;
 	}
 	return max;
 }
