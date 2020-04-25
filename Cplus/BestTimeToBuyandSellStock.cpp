538541fcb56b796d5acff4b9f9465fbb0d266b75
JumHorn <JumHorn@gmail.com>
2020-04-26 06:24:48 +0800

    Best Time to Buy and Sell Stock beats 97.14%

---
+++ Cplus/BestTimeToBuyandSellStock.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+using namespace std;
+
+//Kadane's Algorithm
+
+class Solution
+{
+public:
+	int maxProfit(vector<int>& prices)
+	{
+		int res = 0, maxval = 0;
+		for (int i = 1; i < (int)prices.size(); i++)
+		{
+			maxval += prices[i] - prices[i - 1];
+			if (maxval < 0)
+				maxval = 0;
+			if (maxval > res)
+				res = maxval;
+		}
+		return res;
+	}
+};
