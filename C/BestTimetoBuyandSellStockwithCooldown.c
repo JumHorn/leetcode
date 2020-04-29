700cf67f386422a7e1140066b6cc7ae815ad12a7
JumHorn <JumHorn@gmail.com>
2020-04-30 06:55:27 +0800

    add unified dp thinking process

--- C/BestTimetoBuyandSellStockwithCooldown.c
+++ C/BestTimetoBuyandSellStockwithCooldown.c
@@ -1,24 +1,28 @@
 
 /*
-dp[i][0] buy
-dp[i][1] sell
+the original dp function
+dp[i]=max{prices[i]-prices[j]-fee+dp[j-2]} (1<j<i)
+
+optimization
+dp[i]=max{prices[i]-fee+(dp[j-2]-prices[j])} (1<j<i)
+dp1[j]=max{dp[j-2]-prices[j]} record max dp[j-2]-prices[j]
+dp[i]=max(dp[i-1],dp1[i-1]+prices[i]-fee)
 */
 
 #define max(a, b) (((a) > (b)) ? (a) : (b))
 
-int maxProfit(int* prices, int pricesSize)
+int maxProfit(int *prices, int pricesSize)
 {
 	if (pricesSize < 2)
 		return 0;
-	int dp[pricesSize][2];
-	dp[0][0] = -prices[0];
-	dp[0][1] = 0;
-	dp[1][0] = max(dp[0][0], -prices[1]);
-	dp[1][1] = max(0, dp[0][0] + prices[1]);
+	int dp[pricesSize];
+	dp[0] = 0;
+	dp[1] = max(0, prices[1] - prices[0]);
+	int buy = max(dp[0] - prices[0], dp[1] - prices[1]);
 	for (int i = 2; i < pricesSize; i++)
 	{
-		dp[i][0] = max(dp[i - 1][0], dp[i - 2][1] - prices[i]);
-		dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+		dp[i] = max(dp[i - 1], prices[i] + buy);
+		buy = max(buy, dp[i - 2] - prices[i]);
 	}
-	return dp[pricesSize - 1][1];
+	return dp[pricesSize - 1];
 }
--- C/BestTimetoBuyandSellStockwithTransactionFee.c
+++ C/BestTimetoBuyandSellStockwithTransactionFee.c
@@ -1,20 +1,25 @@
 
 /*
-dp[i][0] buy
-dp[i][1] sell
+the original dp function
+dp[i]=max{prices[i]-prices[j]-fee+dp[j-1]} (0<j<i)
+
+optimization
+dp[i]=max{prices[i]-fee+(dp[j-1]-prices[j])} (0=<j<i)
+dp1[j]=max{dp[j-1]-prices[j]} record max dp[j-1]-prices[j]
+dp[i]=max(dp[i-1],dp1[i-1]+prices[i]-fee)
 */
 
 #define max(a, b) (((a) > (b)) ? (a) : (b))
 
-int maxProfit(int* prices, int pricesSize, int fee)
+int maxProfit(int *prices, int pricesSize, int fee)
 {
-	int dp[pricesSize][2];
-	dp[0][0] = -prices[0] - fee;
-	dp[0][1] = 0;
+	int dp[pricesSize];
+	dp[0] = 0;
+	int buy = -prices[0];
 	for (int i = 1; i < pricesSize; i++)
 	{
-		dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] - prices[i] - fee);
-		dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+		dp[i] = max(dp[i - 1], prices[i] - fee + buy);
+		buy = max(buy, dp[i - 1] - prices[i]);
 	}
-	return dp[pricesSize - 1][1];
+	return dp[pricesSize - 1];
 }
