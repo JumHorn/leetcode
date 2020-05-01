6ebc9d648f0f6f8e85be7b96427d8af4eeb0c9fb
JumHorn <JumHorn@gmail.com>
2020-05-02 06:23:01 +0800

    formatted

--- Cplus/CountofSmallerNumbersAfterSelf.cpp
+++ Cplus/CountofSmallerNumbersAfterSelf.cpp
@@ -1,48 +1,52 @@
-#include<vector>
+#include <vector>
 using namespace std;
 
-class Solution {
-    vector<pair<int,int> > tmp;
+class Solution
+{
 public:
-    vector<int> countSmaller(vector<int>& nums) {
+	vector<int> countSmaller(vector<int> &nums)
+	{
 		vector<int> res(nums.size());
-        tmp.resize(nums.size());
-		vector<pair<int,int> > pairs(nums.size());
-		for(int i=0;i<(int)nums.size();i++)
+		dup.resize(nums.size());
+		vector<pair<int, int>> pairs(nums.size());
+		for (int i = 0; i < (int)nums.size(); i++)
 		{
-			pairs[i].first=nums[i];
-			pairs[i].second=i;
+			pairs[i].first = nums[i];
+			pairs[i].second = i;
 		}
-		mergeSort(res,pairs,0,nums.size());
+		mergeSort(res, pairs, 0, nums.size());
 		return res;
-    }
+	}
 
-	void mergeSort(vector<int>& res,vector<pair<int,int> >& pairs,int start,int end)
+	void mergeSort(vector<int> &res, vector<pair<int, int>> &pairs, int start, int end)
 	{
-		if(end-start<=1)
+		if (end - start <= 1)
 			return;
-		int mid=(end-start)/2+start;
-		mergeSort(res,pairs,start,mid);
-		mergeSort(res,pairs,mid,end);
-        for(int i=start,j=mid;i<mid;i++)
-        {
-            while(j!=end&&pairs[i].first>pairs[j].first)
-                j++;
-            res[pairs[i].second]+=j-mid;
-        }
-		merge(pairs,start,mid,end);
+		int mid = (end - start) / 2 + start;
+		mergeSort(res, pairs, start, mid);
+		mergeSort(res, pairs, mid, end);
+		for (int i = start, j = mid; i < mid; i++)
+		{
+			while (j != end && pairs[i].first > pairs[j].first)
+				j++;
+			res[pairs[i].second] += j - mid;
+		}
+		merge(pairs, start, mid, end);
 	}
 
-	void merge(vector<pair<int,int> >& pairs,int start,int mid,int end)
+	void merge(vector<pair<int, int>> &pairs, int start, int mid, int end)
 	{
-		int s=start,m=mid,d=0,d1=0;
-		while(s!=mid&&m!=end)
-		    tmp[d++]=(pairs[s].first>pairs[m].first)?pairs[m++]:pairs[s++];
-		while(s!=mid)
-			tmp[d++]=pairs[s++];
-		while(m!=end)
-			tmp[d++]=pairs[m++];
-		while(start!=end)
-			pairs[start++]=tmp[d1++];
+		int s = start, m = mid, d = 0, d1 = 0;
+		while (s != mid && m != end)
+			dup[d++] = (pairs[s].first > pairs[m].first) ? pairs[m++] : pairs[s++];
+		while (s != mid)
+			dup[d++] = pairs[s++];
+		while (m != end)
+			dup[d++] = pairs[m++];
+		while (start != end)
+			pairs[start++] = dup[d1++];
 	}
+
+private:
+	vector<pair<int, int>> dup;
 };
