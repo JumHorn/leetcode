294c570ed8c367660b882e32ad8c161908e379f7
JumHorn <JumHorn@gmail.com>
2020-05-03 23:41:04 +0800

    Remove Duplicate Letters beats 100%

---
+++ C/RemoveDuplicateLetters.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+
+char *removeDuplicateLetters(char *s)
+{
+	char count[26] = {0}, *p = s;
+	while (*p)
+		++count[*p++ - 'a'];
+	char *stack = (char *)malloc(sizeof(char) * 27);
+	memset(stack, 0, sizeof(char) * 27);
+	int top = -1, seen[26] = {0};
+	p = s;
+	while (*p)
+	{
+		if (seen[*p - 'a'] == 0)
+		{
+			while (top != -1 && count[stack[top] - 'a'] != 0 && stack[top] > *p)
+			{
+				seen[stack[top] - 'a'] = 0;
+				--top;
+			}
+			stack[++top] = *p;
+			seen[*p - 'a'] = 1;
+		}
+		--count[*p - 'a'];
+		++p;
+	}
+	stack[++top] = '\0';
+	return stack;
+}
