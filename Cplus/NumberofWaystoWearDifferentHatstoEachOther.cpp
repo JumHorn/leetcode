809270c65bb2330b1c846a92ac53288b65e37fa7
JumHorn <JumHorn@gmail.com>
2020-05-07 07:00:39 +0800

    Number of Ways to Wear Different Hats to Each Other beats 73.76%

--- Cplus/NumberofWaystoWearDifferentHatstoEachOther.cpp
+++ Cplus/NumberofWaystoWearDifferentHatstoEachOther.cpp
@@ -5,30 +5,32 @@ using namespace std;
 class Solution
 {
 public:
-	int numberWays(vector<vector<int>>& hats)
+	int numberWays(vector<vector<int>> &hats)
 	{
-		vector<unordered_map<int, int>> dp(hats.size());
-		return memdp(hats, 0, 0, dp);
+		int n = hats.size();
+		vector<vector<int>> dp(41, vector<int>(1 << n, -1));
+		vector<vector<int>> h2p(41); //hats to people
+		for (int i = 0; i < n; i++)
+			for (auto h : hats[i])
+				h2p[h].push_back(i);
+		return memdp(h2p, 1, 0, (1 << n) - 1, dp);
 	}
 
-	int memdp(vector<vector<int>>& hats, long seen, int index, vector<unordered_map<int, int>>& dp)
+	int memdp(vector<vector<int>> &h2p, int index, int mask, int allmask, vector<vector<int>> &dp)
 	{
-		int n = hats.size(), res = 0;
-		if (index >= n)
+		if (mask == allmask)
 			return 1;
-		if (dp[index].find(seen) != dp[index].end())
-			return dp[index][seen];
-		for (auto h : hats[index])
+		if (index > 40)
+			return 0;
+		if (dp[index][mask] != -1)
+			return dp[index][mask];
+		int res = memdp(h2p, index + 1, mask, allmask, dp);
+		for (auto p : h2p[index])
 		{
-			if ((seen & (1 << h)) == 0)
-			{
-				seen += (1 << h);
-				res = (res + memdp(hats, seen, index + 1, dp)) % MOD;
-				seen -= (1 << h);
-			}
+			if ((mask & (1 << p)) == 0)
+				res = (res + memdp(h2p, index + 1, mask | (1 << p), allmask, dp)) % MOD;
 		}
-		dp[index][seen] = res;
-		return res;
+		return dp[index][mask] = res;
 	}
 
 private:
