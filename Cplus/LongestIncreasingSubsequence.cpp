d49c449f769080295c919b501d9806dc69301d14
JumHorn <JumHorn@gmail.com>
2020-05-11 07:23:19 +0800

    Longest Increasing Subsequence beats 87.30%

--- Cplus/LongestIncreasingSubsequence.cpp
+++ Cplus/LongestIncreasingSubsequence.cpp
@@ -7,11 +7,8 @@ class Solution
 public:
 	int lengthOfLIS(vector<int> &nums)
 	{
-		if (nums.empty())
-			return 0;
 		vector<int> LIS;
-		LIS.push_back(nums.front());
-		for (int i = 1; i < (int)nums.size(); i++)
+		for (int i = 0; i < (int)nums.size(); i++)
 		{
 			auto iter = lower_bound(LIS.begin(), LIS.end(), nums[i]);
 			if (iter == LIS.end())
