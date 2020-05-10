34ad24ff530b417dda47ea62e3b025ef5b19e09b
JumHorn <JumHorn@gmail.com>
2020-05-10 08:10:32 +0800

    formatted

--- Cplus/RussianDollEnvelopes.cpp
+++ Cplus/RussianDollEnvelopes.cpp
@@ -1,20 +1,24 @@
-#include<vector>
-#include<algorithm>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int maxEnvelopes(vector<vector<int> >& envelopes) {
-		if(envelopes.empty())
+	int maxEnvelopes(vector<vector<int>> &envelopes)
+	{
+		if (envelopes.empty())
 			return 0;
-		vector<int> dp(envelopes.size(),1);        
-		sort(envelopes.begin(),envelopes.end());
-		for(int i=1;i<(int)dp.size();i++)
-			for(int j=0;j<i;j++)
+		vector<int> dp(envelopes.size(), 1);
+		sort(envelopes.begin(), envelopes.end());
+		for (int i = 1; i < (int)dp.size(); i++)
+		{
+			for (int j = 0; j < i; j++)
 			{
-				if(envelopes[i][0]>envelopes[j][0]&&envelopes[i][1]>envelopes[j][1])
-					dp[i]=max(dp[i],dp[j]+1);
+				if (envelopes[i][0] > envelopes[j][0] && envelopes[i][1] > envelopes[j][1])
+					dp[i] = max(dp[i], dp[j] + 1);
 			}
-		return *max_element(dp.begin(),dp.end());
-    }
+		}
+		return *max_element(dp.begin(), dp.end());
+	}
 };
