96a55bb994484e67e0033b2f2089507ae0b915a2
JumHorn <JumHorn@gmail.com>
2020-05-21 06:50:09 +0800

    LFU Cache beats 6.09%

--- Cplus/LFUCache.cpp
+++ Cplus/LFUCache.cpp
@@ -1,59 +1,57 @@
-#include<list>
-#include<unordered_map>
+#include <list>
+#include <unordered_map>
 using namespace std;
 
-class LFUCache {
-	int c;//capacity
-	int s;//size
-	int minfreq;
-
-	unordered_map<int,list<int>::iterator> iter;//key list iter
-	unordered_map<int,list<int> > freq;//freq key list
-
-	unordered_map<int,pair<int,int> > dict;//key {value freq}
+class LFUCache
+{
 public:
-    LFUCache(int capacity) {
-        c=capacity;
-		s=0;
-		minfreq=0;
-    }
-    
-    int get(int key) {
-		if(dict.find(key)==dict.end())
+	LFUCache(int capacity)
+	{
+		c = capacity;
+		minfreq = 0;
+	}
+
+	int get(int key)
+	{
+		if (dict.find(key) == dict.end())
 			return -1;
-
 		freq[dict[key].second].erase(iter[key]);
-		dict[key].second++;
-		freq[dict[key].second].push_back(key);
-		iter[key]=--freq[dict[key].second].end();
-		if(freq[minfreq].size()==0)
+		freq[++dict[key].second].push_back(key);
+		iter[key] = --freq[dict[key].second].end();
+		if (freq[minfreq].empty())
 			minfreq++;
-
 		return dict[key].first;
-    }
-    
-    void put(int key, int value) {
-        if(c<=0)
-            return;
-		int tmp=get(key);
-		if(tmp!=-1)
+	}
+
+	void put(int key, int value)
+	{
+		if (c <= 0)
+			return;
+		if (get(key) != -1)
 		{
-			dict[key].first=value;
+			dict[key].first = value;
 			return;
 		}
-		if(s>=c)
+		if ((int)dict.size() >= c)
 		{
 			dict.erase(freq[minfreq].front());
 			iter.erase(freq[minfreq].front());
 			freq[minfreq].pop_front();
-			s--;
 		}
 		freq[1].push_back(key);
-		iter[key]=--freq[1].end();
-		dict[key]=pair<int,int>(value,1);
-		minfreq=1;
-		s++;
+		iter[key] = --freq[1].end();
+		dict[key] = {value, 1};
+		minfreq = 1;
 	}
+
+private:
+	int c; //capacity
+	int minfreq;
+
+	unordered_map<int, list<int>::iterator> iter; //key list iter
+	unordered_map<int, list<int>> freq;			  //freq key list
+
+	unordered_map<int, pair<int, int>> dict; //key {value freq}
 };
 
 /**
