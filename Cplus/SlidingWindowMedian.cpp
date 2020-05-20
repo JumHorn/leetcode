3751e14e0e23e023970acaddf64859b70083821d
JumHorn <JumHorn@gmail.com>
2020-05-21 07:59:05 +0800

    Sliding Window Median beats 8.44%

--- Cplus/SlidingWindowMedian.cpp
+++ Cplus/SlidingWindowMedian.cpp
@@ -1,89 +1,31 @@
-#include<vector>
-#include<set>
+#include <vector>
+#include <set>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<double> medianSlidingWindow(vector<int>& nums, int k) {
-		multiset<int> s;        
-		vector<double> res(nums.size()-k+1);
-		for(int i=0;i<k;i++)
+	vector<double> medianSlidingWindow(vector<int> &nums, int k)
+	{
+		multiset<int> s;
+		vector<double> res;
+		for (int i = 0; i < k; i++)
 			s.insert(nums[i]);
-		multiset<int>::iterator iter=s.begin();
-		advance(iter,k/2);
-		if(k%2==1)
+		multiset<int>::iterator right = s.begin(), left;
+		advance(right, k / 2);
+		left = right;
+		advance(left, k % 2 - 1);
+		res.push_back(((double)*left + *right) / 2.0);
+		for (int i = k; i < (int)nums.size(); i++)
 		{
-			res[0]=*iter;
-			for(int i=1;i<(int)res.size();i++)
-			{
-				int left=nums[i-1];
-				int right=nums[i+k-1];
-				if(left!=*iter)
-					s.erase(s.find(left));
-				else
-				{
-					s.erase(iter);
-					s.insert(right);
-					iter=s.begin();
-					advance(iter,k/2);
-					res[i]=*iter;
-					continue;
-				}
-				s.insert(right);
-				if((left<=res[i-1]&&right<=res[i-1])||(left>=res[i-1]&&right>=res[i-1]))
-					res[i]=*iter;
-				else if(left<res[i-1]&&right>res[i-1])
-					res[i]=*++iter;
-				else
-					res[i]=*--iter;
-			}
+			s.insert(nums[i]);
+			s.erase(s.find(nums[i - k]));
+			right = s.begin();
+			advance(right, k / 2);
+			left = right;
+			advance(left, k % 2 - 1);
+			res.push_back(((double)*left + *right) / 2.0);
 		}
-		else
-		{
-			multiset<int>::iterator iter1=iter;
-			--iter;
-			res[0]=*iter/2.0+*iter1/2.0;
-			for(int i=1;i<(int)res.size();i++)
-			{
-				int left=nums[i-1];
-				int right=nums[i+k-1];
-				if(left!=*iter&&left!=*iter1)
-					s.erase(s.find(left));
-				else
-				{
-					if(left==*iter)
-						s.erase(iter);
-					else
-						s.erase(iter1);
-					
-					s.insert(right);
-					iter=s.begin();
-					advance(iter,k/2);
-					iter1=iter;
-					--iter;
-					res[i]=*iter/2.0+*iter1/2.0;
-					continue;
-				}
-				s.insert(right);
-				if((left<=*iter&&right<=*iter)||(left>=*iter1&&right>=*iter1))
-					res[i]=*iter/2.0+*iter1/2.0;
-				else if(left<*iter)
-				{
-					if(right>=*iter1)
-						res[i]=*(++iter)/2.0+*(++iter1)/2.0;
-					else
-						res[i]=*(++iter)/2.0+*(iter1)/2.0;
-				}
-				else if(left>*iter1)
-				{
-					if(right<*iter)
-						res[i]=*(--iter)/2.0+*(--iter1)/2.0;
-					else
-						res[i]=*(iter)/2.0+*(--iter1)/2.0;
-				}
-			}
-		}
-		
 		return res;
-    }
+	}
 };
