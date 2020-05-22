8049eac29cef17aae73c3754ecec1bd781be1078
JumHorn <JumHorn@gmail.com>
2020-05-23 07:15:08 +0800

    K-th Smallest in Lexicographical Order beats 100%

--- C/K-thSmallestinLexicographicalOrder.c
+++ C/K-thSmallestinLexicographicalOrder.c
@@ -1,29 +1,34 @@
 
-int dfs(int n, int *k, long val)
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+
+int gap(int n, long lower, long upper)
 {
-	if (val > n)
-		return -1;
-	if (*k == 1)
-		return val;
-	--*k;
-	for (int i = 0; i < 10; i++)
+	int res = 0;
+	while (lower <= n)
 	{
-		int res = dfs(n, k, val * 10 + i);
-		if (res == -1)
-			break;
-		if (res > 0)
-			return res;
+		res += min(n + 1, upper) - lower;
+		upper *= 10;
+		lower *= 10;
 	}
-	return 0;
+	return res;
 }
 
 int findKthNumber(int n, int k)
 {
-	for (int i = 1; i < 10; i++)
+	int res = 1;
+	while (k > 1)
 	{
-		int res = dfs(n, &k, i);
-		if (res > 0)
-			return res;
+		int diff = gap(n, res, res + 1);
+		if (diff < k)
+		{
+			++res;
+			k -= diff;
+		}
+		else
+		{
+			res *= 10;
+			--k;
+		}
 	}
-	return -1;
+	return res;
 }
