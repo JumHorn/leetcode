dd8200e4322cbf0243a681717eea49c0c43285ea
JumHorn <JumHorn@gmail.com>
2020-05-24 23:50:40 +0800

    Super Washing Machines beats 100%

---
+++ C/SuperWashingMachines.c
@@ -0,0 +1,21 @@
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define abs(x) (((x) < 0) ? (-(x)) : (x))
+
+int findMinMoves(int *machines, int machinesSize)
+{
+	int sum = 0;
+	for (int i = 0; i < machinesSize; i++)
+		sum += machines[i];
+	if (sum % machinesSize != 0)
+		return -1;
+	int avg = sum / machinesSize;
+	for (int i = 0; i < machinesSize; i++)
+		machines[i] -= avg;
+	int res = 0, count = 0;
+	for (int i = 0; i < machinesSize; i++)
+	{
+		count += machines[i];
+		res = max(res, max(abs(count), machines[i]));
+	}
+	return res;
+}
