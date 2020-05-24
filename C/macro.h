4d5f3fac29a95fafbccf40bdf2962f5c237c60a8
JumHorn <JumHorn@gmail.com>
2020-05-24 23:51:27 +0800

    add abs

--- C/macro.h
+++ C/macro.h
@@ -1,5 +1,6 @@
 #define max(a, b) (((a) > (b)) ? (a) : (b))
 #define min(a, b) (((a) < (b)) ? (a) : (b))
+#define abs(x) (((x) < 0) ? (-(x)) : (x))
 #define swap(a, b) ((a) ^= (b) ^= (a) ^= (b))
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
 
