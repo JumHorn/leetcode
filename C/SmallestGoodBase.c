c441e92d70c88f2f8de0de547bd55a3a2e206cbb
JumHorn <JumHorn@gmail.com>
2020-05-27 23:52:52 +0800

    Smallest Good Base beats 100%

---
+++ C/SmallestGoodBase.c
@@ -0,0 +1,22 @@
+#include <math.h>
+#include <stdlib.h>
+
+char *smallestGoodBase(char *n)
+{
+	unsigned long long S = atol(n);
+	int max_m = log2(S);
+	for (int m = max_m; m >= 2; m--)
+	{
+		int k = pow(S, 1.0 / m);
+		unsigned long long sum = 1, prod = 1;
+		for (int i = 0; i < m; i++)
+			sum += (prod *= k);
+		if (sum == S)
+		{
+			sprintf(n, "%d", k);
+			return n;
+		}
+	}
+	sprintf(n, "%ld", S - 1);
+	return n;
+}
