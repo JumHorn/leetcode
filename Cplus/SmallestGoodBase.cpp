f9ff4990e5c4aaa634070358764ee71120cad63d
JumHorn <JumHorn@gmail.com>
2020-05-27 23:54:32 +0800

    formatted

--- Cplus/SmallestGoodBase.cpp
+++ Cplus/SmallestGoodBase.cpp
@@ -1,21 +1,23 @@
-#include<string>
-#include<cmath>
+#include <string>
+#include <cmath>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    string smallestGoodBase(string n) {
-		unsigned long long S=stoll(n);
-		int max_m=log2(S);
-		for(int m=max_m;m>=2;m--)
+	string smallestGoodBase(string n)
+	{
+		unsigned long long S = stoll(n);
+		int max_m = log2(S);
+		for (int m = max_m; m >= 2; m--)
 		{
-			int k=pow(S,1.0/m);
-			unsigned long long sum=1,prod=1;
-			for(int i=0;i<m;i++)
-				sum+=(prod*=k);
-			if(sum==S)
+			int k = pow(S, 1.0 / m);
+			unsigned long long sum = 1, prod = 1;
+			for (int i = 0; i < m; i++)
+				sum += (prod *= k);
+			if (sum == S)
 				return to_string(k);
 		}
-		return to_string(S-1);
-    }
+		return to_string(S - 1);
+	}
 };
