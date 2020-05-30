7ec105116b8bdeeeb8dadd03868bf21d96fdca49
JumHorn <JumHorn@gmail.com>
2020-05-31 00:02:40 +0800

    Reverse Pairs beats 32.52%

--- Cplus/ReversePairs.cpp
+++ Cplus/ReversePairs.cpp
@@ -1,42 +1,30 @@
-#include<vector>
+#include <vector>
 using namespace std;
 
-class Solution {
-	vector<int> tmp;
+class Solution
+{
 public:
-    int reversePairs(vector<int>& nums) {
-		tmp.resize(nums.size());
-		return divide(nums,0,nums.size());
-    }
-
-	int divide(vector<int>& nums,int start,int end)
+	int reversePairs(vector<int> &nums)
 	{
-		int res=0;
-		if(end-start<=1)
-			return 0;
-		int mid=start+(end-start)/2;
-		res=divide(nums,start,mid)+divide(nums,mid,end);
-        int j=mid;
-		for(int i=start;i<mid;i++)
-		{
-			while(j<end&&(long)nums[i]-(long)nums[j]>(long)nums[j])
-				j++;
-			res+=j-mid;
-		}
-		merge(nums,start,mid,end);
-		return res;
+		return divide(nums, 0, nums.size());
 	}
 
-	void merge(vector<int>& nums,int start,int mid,int end)
+	int divide(vector<int> &nums, int start, int end)
 	{
-		int s=start,m=mid,d=0,d1=0;
-		while(s!=mid&&m!=end)
-		    tmp[d++]=(nums[s]>nums[m])?nums[m++]:nums[s++];
-		while(s!=mid)
-			tmp[d++]=nums[s++];
-		while(m!=end)
-			tmp[d++]=nums[m++];
-		while(start!=end)
-			nums[start++]=tmp[d1++];
+		int res = 0;
+		if (end - start <= 1)
+			return 0;
+		int mid = start + (end - start) / 2;
+		res = divide(nums, start, mid) + divide(nums, mid, end);
+		for (int i = start, j = mid; i < mid; i++)
+		{
+			while (j < end && (long)nums[i] - (long)nums[j] > (long)nums[j])
+				j++;
+			res += j - mid;
+		}
+		vector<int> dup(end - start);
+		merge(nums.begin() + start, nums.begin() + mid, nums.begin() + mid, nums.begin() + end, dup.begin());
+		copy(dup.begin(), dup.end(), nums.begin() + start);
+		return res;
 	}
 };
