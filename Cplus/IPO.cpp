f3e51d65b79c8099c3cd9eca4022a4c75c7f6aea
JumHorn <JumHorn@gmail.com>
2020-05-31 08:13:00 +0800

    IPO beats 60.76%

--- Cplus/IPO.cpp
+++ Cplus/IPO.cpp
@@ -1,35 +1,30 @@
-#include<queue>
-#include<vector>
+#include <queue>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int findMaximizedCapital(int k, int W, vector<int>& Profits, vector<int>& Capital) {
-		priority_queue<pair<int,int>> ipo,noipo;
- 		for(int i=0;i<(int)Profits.size();i++)
+	int findMaximizedCapital(int k, int W, vector<int> &Profits, vector<int> &Capital)
+	{
+		priority_queue<pair<int, int>> ipo, noipo;
+		for (int i = 0; i < (int)Profits.size(); i++)
 		{
-			if(Capital[i]<=W)
-				ipo.push({Profits[i],Capital[i]});
+			if (Capital[i] <= W)
+				ipo.push({Profits[i], Capital[i]});
 			else
-				noipo.push({-Capital[i],Profits[i]});
+				noipo.push({-Capital[i], Profits[i]});
 		}
-		for(int i=0;i<k;i++)
+		for (int i = 0; i < k && !ipo.empty(); i++)
 		{
-			if(ipo.empty())
-				break;
-			W+=ipo.top().first;
+			W += ipo.top().first;
 			ipo.pop();
-			while(!noipo.empty())
+			while (!noipo.empty() && W >= -noipo.top().first)
 			{
-				if(W>=-noipo.top().first)
-				{
-					ipo.push({noipo.top().second,-noipo.top().first});
-					noipo.pop();
-				}
-				else
-					break;
+				ipo.push({noipo.top().second, -noipo.top().first});
+				noipo.pop();
 			}
 		}
 		return W;
-    }
+	}
 };
