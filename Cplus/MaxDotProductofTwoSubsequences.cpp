32acd7a48beba308b20ee33576ebf50df5f3bf28
JumHorn <JumHorn@gmail.com>
2020-05-31 23:54:19 +0800

    Max Dot Product of Two Subsequences beats 35.82%

--- Cplus/MaxDotProductofTwoSubsequences.cpp
+++ Cplus/MaxDotProductofTwoSubsequences.cpp
@@ -1,3 +1,5 @@
+#include <climits>
+#include <numeric>
 #include <vector>
 using namespace std;
 
@@ -6,34 +8,27 @@ class Solution
 public:
 	int maxDotProduct(vector<int>& nums1, vector<int>& nums2)
 	{
-		sort(nums1.begin(), nums1.end());
-		sort(nums2.begin(), nums2.end());
-		int n1 = nums1.size(), n2 = nums2.size(), n = min(n1, n2);
-		int i = 0, j = 0, res = 0;
-		while (n > 0)
-		{
-			int tmp = nums1[i] * nums2[j];
-			if (tmp < 0)
-				break;
-			res += tmp;
-			++i;
-			++j;
-			--n;
-		}
-		i = n1 - 1;
-		j = n2 - 1;
-		while (n > 0)
-		{
-			int tmp = nums1[i] * nums2[j];
-			if (tmp < 0)
-				break;
-			res += tmp;
-			--i;
-			--j;
-			--n;
-		}
-		if (min(n1, n2) != n)
-			return res;
-		return max(nums1[0] * nums2[0], nums1[n1 - 1] * nums2[n2 - 1]);
+		int max1 = *max_element(nums1.begin(), nums1.end());
+		int min2 = *min_element(nums2.begin(), nums2.end());
+		if (max1 < 0 && min2 > 0)
+			return max1 * min2;
+		int min1 = *min_element(nums1.begin(), nums1.end());
+		int max2 = *max_element(nums2.begin(), nums2.end());
+		if (max2 < 0 && min1 > 0)
+			return max2 * min1;
+		vector<vector<int>> dp(nums1.size(), vector<int>(nums2.size(), INT_MIN));
+		return memdp(nums1, 0, nums2, 0, dp);
+	}
+
+	int memdp(vector<int>& nums1, int index1, vector<int>& nums2, int index2, vector<vector<int>>& dp)
+	{
+		if (index1 >= (int)nums1.size() || index2 >= (int)nums2.size())
+			return 0;
+		if (dp[index1][index2] != INT_MIN)
+			return dp[index1][index2];
+		int res = nums1[index1] * nums2[index2] + memdp(nums1, index1 + 1, nums2, index2 + 1, dp);
+		res = max(res, memdp(nums1, index1 + 1, nums2, index2, dp));
+		res = max(res, memdp(nums1, index1, nums2, index2 + 1, dp));
+		return dp[index1][index2] = res;
 	}
 };
