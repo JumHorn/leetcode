7b95a033c74b1bd2c69b90dea597f781d56b061c
JumHorn <JumHorn@gmail.com>
2020-06-01 22:59:11 +0800

    Freedom Trail beats 12.62%

--- Cplus/FreedomTrail.cpp
+++ Cplus/FreedomTrail.cpp
@@ -1,42 +1,31 @@
-#include<climits>
-#include<string>
-#include<vector>
+#include <climits>
+#include <string>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int findRotateSteps(string ring, string key) {
-		vector<vector<int> > dp(key.length(),vector<int>(ring.length()));
-		//dp first row
-		int n=ring.length();
-		for(int i=0;i<n;i++)
-        {
-            dp[0][i]=INT_MAX;
-			if(ring[i]==key[0])
-				dp[0][i]=min(i,n-i);
-        }
+	int findRotateSteps(string ring, string key)
+	{
+		int m = key.length(), n = ring.length();
+		vector<vector<int>> dp(m + 1, vector<int>(n));
 
-		for(int i=1;i<(int)key.length();i++)
-			for(int j=0;j<(int)ring.length();j++)
+		for (int i = m - 1; i >= 0; i--)
+		{
+			for (int j = 0; j < n; j++)
 			{
-				dp[i][j]=INT_MAX;
-				if(ring[j]==key[i])
+				dp[i][j] = INT_MAX;
+				for (int k = 0; k < n; k++)
 				{
-					for(int k=0;k<(int)ring.length();k++)
+					if (ring[k] == key[i])
 					{
-						if(dp[i-1][k]!=INT_MAX)
-						{
-							int step=abs(j-k);
-							dp[i][j]=min(dp[i][j],dp[i-1][k]+min(step,n-step));
-						}
+						int step = abs(j - k);
+						dp[i][j] = min(dp[i][j], dp[i + 1][k] + min(step, n - step));
 					}
 				}
 			}
-
-		int m=key.length(),res=INT_MAX;
-		for(int i=0;i<(int)ring.length();i++)
-			res=min(res,dp[m-1][i]);
-
-		return res+key.length();
-    }
+		}
+		return dp[0][0] + m;
+	}
 };
