75a11635e620455556dd75a4db50ac5b063759b0
JumHorn <JumHorn@gmail.com>
2020-06-02 22:29:35 +0800

    Remove Boxes beats 100%

---
+++ C/RemoveBoxes.c
@@ -0,0 +1,32 @@
+#include <string.h>
+
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+
+int removeBoxes(int *boxes, int boxesSize)
+{
+	int dp[boxesSize][boxesSize][boxesSize];
+	memset(dp, 0, sizeof(dp));
+	for (int i = 0; i < boxesSize; i++)
+	{
+		for (int k = 0; k <= i; k++)
+			dp[i][i][k] = (k + 1) * (k + 1);
+	}
+	for (int l = 1; l < boxesSize; l++)
+	{
+		for (int j = l; j < boxesSize; j++)
+		{
+			int i = j - l;
+			for (int k = 0; k <= i; k++)
+			{
+				int res = (k + 1) * (k + 1) + dp[i + 1][j][0];
+				for (int m = i + 1; m <= j; m++)
+				{
+					if (boxes[m] == boxes[i])
+						res = max(res, dp[i + 1][m - 1][0] + dp[m][j][k + 1]);
+				}
+				dp[i][j][k] = res;
+			}
+		}
+	}
+	return dp[0][boxesSize - 1][0];
+}
