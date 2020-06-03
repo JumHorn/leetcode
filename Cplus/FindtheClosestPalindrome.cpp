70bb8e1104064e21c7bb841dbe6cd60aefb2986d
JumHorn <JumHorn@gmail.com>
2020-06-03 12:35:21 +0800

    Find the Closest Palindrome beats 100%

--- Cplus/FindtheClosestPalindrome.cpp
+++ Cplus/FindtheClosestPalindrome.cpp
@@ -1,66 +1,53 @@
-#include<string>
-#include<cmath>
-#include<climits>
+#include <string>
+#include <cmath>
+#include <climits>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    string nearestPalindromic(string n) {
-		int i=0,j=n.length()-1;
-        long nn=stol(n),res0,res1,res2,abs0,abs1,abs2;
-		string res(n);
-		while(i<j)
-			res[j--]=res[i++];
-		res0=stol(res);
-        abs0=abs(res0-nn);
-        if(abs0==0)
-            abs0=LONG_MAX;
+	string nearestPalindromic(string n)
+	{
+		long num = stoll(n);
+		int len = n.length();
+		n[len / 2 + len % 2] = '\0';
+		long half = stoll(n.substr(0, len / 2 + len % 2));
+		long res0 = makePalindrome(half, len % 2);
+		long abs0 = abs(num - res0);
+		if (abs0 == 0)
+			abs0 = LONG_MAX;
 
-        i=n.size()/2-1+(n.size()&1),j=n.size()/2;
-        string tmp(res);
-        while (i>=0&&tmp[i]=='9')
-        {
-            tmp[i]='0';
-            tmp[j]=tmp[i];
-            i--;
-            j++;
-        }
-        if(i<0)
-            res1=pow(10,tmp.length())+1;
-        else
-        {
-            tmp[i]=((tmp[i]-'0')+1)%10+'0';
-            tmp[j]=tmp[i];
-            res1=stol(tmp);
-        }
-        abs1=abs(res1-nn);
-        
-        i=n.size()/2-1+(n.size()&1),j=n.size()/2;
-        tmp=res;
-        while (i>=0&&tmp[i]=='0')
-        {
-            tmp[i]='9';
-            tmp[j]=tmp[i];
-            i--;
-            j++;
-        }
-        if(i<0)
-            res2=pow(10,tmp.length())-1;
-        else
-        {
-            tmp[i]=((tmp[i]-'0')-1)%10+'0';
-            if(tmp[i]=='0'&&i==0&&i!=j)
-                tmp[j]='9';
-            else
-                tmp[j]=tmp[i];
-            res2=stol(tmp);
-        }
-        abs2=abs(res2-nn);
+		long res1;
+		if (half != pow(10, len / 2 + len % 2) - 1)
+			res1 = makePalindrome(half + 1, len % 2);
+		else
+			res1 = pow(10, len) + 1;
+		long abs1 = abs(num - res1);
 
-        if(abs2<=abs1&&abs2<=abs0)
-            return to_string(res2);
-        if(abs0<=abs1&&abs0<=abs2)
-            return to_string(res0);
+		long res2;
+		if (half != pow(10, len / 2 + len % 2 - 1))
+			res2 = makePalindrome(half - 1, len % 2);
+		else
+			res2 = pow(10, len - 1) - 1;
+		long abs2 = abs(num - res2);
+
+		if (abs2 <= abs0 && abs2 <= abs1)
+			return to_string(res2);
+		if (abs0 <= abs1 && abs0 < abs2)
+			return to_string(res0);
 		return to_string(res1);
-    }
+	}
+
+	long makePalindrome(long half, int even)
+	{
+		long left = half, right = 0;
+		if (even != 0)
+			half /= 10;
+		for (; half > 0; half /= 10)
+		{
+			left *= 10;
+			right = right * 10 + half % 10;
+		}
+		return left + right;
+	}
 };
