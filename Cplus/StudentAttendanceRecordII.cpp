573c622bfef6033354c9803244756b4bb6a2dc80
JumHorn <JumHorn@gmail.com>
2020-06-09 07:02:12 +0800

    Student Attendance Record II beats 51.06%

--- Cplus/StudentAttendanceRecordII.cpp
+++ Cplus/StudentAttendanceRecordII.cpp
@@ -1,26 +1,35 @@
-#include<iostream>
-#include<cmath>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int checkRecord(int n) {
-        int MOD = 1e9+7;
-		int (*dp)[2][3]=new int[n+1][2][3];
-		for(int i=0;i<2;i++)
-			for(int j=0;j<3;j++)
-				dp[0][i][j]=1;
-		for(int i=1;i<=n;i++)
-			for(int j=0;j<2;j++)
-				for(int k=0;k<3;k++)
+	int checkRecord(int n)
+	{
+		int MOD = 1e9 + 7;
+		int dp[2][3];
+		for (int i = 0; i < 2; i++)
+		{
+			for (int j = 0; j < 3; j++)
+				dp[i][j] = 1;
+		}
+		for (int i = 0; i < n; i++)
+		{
+			int dp1[2][3] = {0};
+			for (int j = 0; j < 2; j++)
+			{
+				for (int k = 0; k < 3; k++)
 				{
-					int val=dp[i-1][j][2];
-					if(j>0)
-						val=(val+dp[i-1][j-1][2])%MOD;
-					if(k>0)
-						val=(val+dp[i-1][j][k-1])%MOD;
-					dp[i][j][k]=val;
+					int val = dp[j][2];
+					if (j > 0)
+						val = (val + dp[j - 1][2]) % MOD;
+					if (k > 0)
+						val = (val + dp[j][k - 1]) % MOD;
+					dp1[j][k] = val;
 				}
-        return dp[n][1][2];
-    }
+			}
+			memcpy(dp, dp1, sizeof(dp));
+		}
+		return dp[1][2];
+	}
 };
