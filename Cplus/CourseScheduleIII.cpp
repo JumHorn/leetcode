18e824eb541ceb9450e7edf344b9c675faffc329
JumHorn <JumHorn@gmail.com>
2020-06-11 07:41:03 +0800

    Course Schedule III beats 74.10%

--- Cplus/CourseScheduleIII.cpp
+++ Cplus/CourseScheduleIII.cpp
@@ -1,39 +1,41 @@
-#include<vector>
-#include<queue>
-#include<algorithm>
+#include <vector>
+#include <queue>
+#include <algorithm>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int scheduleCourse(vector<vector<int> >& courses) {
-		if(courses.empty())
+	int scheduleCourse(vector<vector<int>> &courses)
+	{
+		if (courses.empty())
 			return 0;
-		sort(courses.begin(),courses.end(),*this);
-		priority_queue<vector<int> > q;
-		q.push(courses.front());
-		int start=courses.front().front();
-		for(int i=1;i<(int)courses.size();i++)
+		sort(courses.begin(), courses.end(), *this);
+		priority_queue<int> q;
+		int time = courses[0][0];
+		q.push(time);
+		for (int i = 1; i < (int)courses.size(); i++)
 		{
-			if(start+courses[i][0]<=courses[i][1])
+			if (time + courses[i][0] <= courses[i][1])
 			{
-				start+=courses[i][0];
-				q.push(courses[i]);
+				time += courses[i][0];
+				q.push(courses[i][0]);
 			}
 			else
 			{
-				if(courses[i][0]<q.top()[0])
+				if (courses[i][0] < q.top())
 				{
-					start+=courses[i][0]-q.top()[0];
+					time += courses[i][0] - q.top();
 					q.pop();
-                    q.push(courses[i]);
+					q.push(courses[i][0]);
 				}
 			}
 		}
 		return q.size();
-    }
+	}
 
-	bool operator()(vector<int>& lhs,vector<int>& rhs)
+	bool operator()(vector<int> &lhs, vector<int> &rhs)
 	{
-		return lhs[1]<rhs[1];
+		return lhs[1] < rhs[1];
 	}
 };
