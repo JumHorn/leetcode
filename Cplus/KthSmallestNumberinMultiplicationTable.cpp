15a9614b54b5fe403b54fec892f9bd19ab923336
JumHorn <JumHorn@gmail.com>
2020-06-13 07:20:17 +0800

    Kth Smallest Number in Multiplication Table beats 37.03%

--- Cplus/KthSmallestNumberinMultiplicationTable.cpp
+++ Cplus/KthSmallestNumberinMultiplicationTable.cpp
@@ -1,25 +1,25 @@
-#include<algorithm>
+#include <algorithm>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int findKthNumber(int m, int n, int k) {
-		int high=m*n,low=1,mid,cnt,tmp;
-		while(low<high)
+	int findKthNumber(int m, int n, int k)
+	{
+		int hi = m * n, lo = 1;
+		while (lo < hi)
 		{
-			cnt=0;
-			mid=(high-low)/2+low;
-            tmp=n;
-			for(int i=0;i<m;i++)
-			{
-                while(tmp >=1 && (i+1)*tmp > mid) tmp--;
-				cnt+=tmp;
-			}
-			if(cnt>=k)
-				high=mid;
+			int mid = (hi - lo) / 2 + lo;
+
+			int count = 0;
+			for (int i = 1; i <= m; i++)
+				count += min(mid / i, n);
+
+			if (count >= k)
+				hi = mid;
 			else
-				low=mid+1;
+				lo = mid + 1;
 		}
-		return high;
-    }
+		return lo;
+	}
 };
