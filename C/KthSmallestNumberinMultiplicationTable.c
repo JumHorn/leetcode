13aa862984bb5346149157d1df67cd76cb77ca4f
JumHorn <JumHorn@gmail.com>
2020-06-13 07:26:08 +0800

    Kth Smallest Number in Multiplication Table beats 100%

---
+++ C/KthSmallestNumberinMultiplicationTable.c
@@ -0,0 +1,24 @@
+
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+
+int count(int m, int n, int x)
+{
+	int res = 0;
+	for (int i = 1; i <= m; i++)
+		res += min(x / i, n);
+	return res;
+}
+
+int findKthNumber(int m, int n, int k)
+{
+	int lo = 1, hi = m * n;
+	while (lo < hi)
+	{
+		int mid = (hi - lo) / 2 + lo;
+		if (count(m, n, mid) < k)
+			lo = mid + 1;
+		else
+			hi = mid;
+	}
+	return lo;
+}
