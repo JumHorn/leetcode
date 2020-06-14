8ea43791fbfbf2fbf8b9fe33b76b5ad3f4b716a6
JumHorn <JumHorn@gmail.com>
2020-06-14 12:18:54 +0800

    24 Game beats 37.50%

---
+++ C/24Game.c
@@ -0,0 +1,48 @@
+#include <stdbool.h>
+#include <math.h>
+
+bool backTracking(double *nums, int n)
+{
+	if (n == 1 && fabs(nums[0] - 24) < 0.000001)
+		return true;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (i == j)
+				continue;
+			double v[4];
+			int size = 0;
+			for (int k = 0; k < n; k++) // add leftover nums to v
+			{
+				if (k != i && k != j)
+					v[size++] = nums[k];
+			}
+			for (int k = 0; k < 4; k++)
+			{
+				double tmp;
+				if (k == 0)
+					tmp = nums[i] + nums[j];
+				else if (k == 1)
+					tmp = nums[i] - nums[j];
+				else if (k == 2)
+					tmp = nums[i] * nums[j];
+				else if (nums[j] != 0)
+					tmp = nums[i] / nums[j];
+
+				v[size] = tmp;
+				if (backTracking(v, size + 1))
+					return true;
+			}
+		}
+	}
+	return false;
+}
+
+bool judgePoint24(int *nums, int numsSize)
+{
+	double dup[numsSize];
+	for (int i = 0; i < numsSize; i++)
+		dup[i] = nums[i];
+	return backTracking(dup, numsSize);
+}
