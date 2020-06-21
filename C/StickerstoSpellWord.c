81cd0621b461ac93d7992278c6cfaf7af727f552
JumHorn <JumHorn@gmail.com>
2020-06-22 06:57:18 +0800

    Stickers to Spell Word beats 100%

---
+++ C/StickerstoSpellWord.c
@@ -0,0 +1,34 @@
+#include <string.h>
+
+int minStickers(char** stickers, int stickersSize, char* target)
+{
+	int N = strlen(target);
+	int dp[1 << N];
+	memset(dp, -1, sizeof(dp));
+	dp[0] = 0;
+	for (int state = 0; state < 1 << N; state++)
+	{
+		if (dp[state] == -1)
+			continue;
+		for (int i = 0; i < stickersSize; i++)
+		{
+			int now = state;
+			for (int j = 0; stickers[i][j]; j++)
+			{
+				for (int k = 0; k < N; k++)
+				{
+					if (((now >> k) & 1) == 1)
+						continue;
+					if (target[k] == stickers[i][j])
+					{
+						now |= 1 << k;
+						break;
+					}
+				}
+			}
+			if (dp[now] == -1 || dp[now] > dp[state] + 1)
+				dp[now] = dp[state] + 1;
+		}
+	}
+	return dp[(1 << N) - 1];
+}
