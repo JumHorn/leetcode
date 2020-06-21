c5d1861b5b903580cb89874e20d560cbf6b58dc1
JumHorn <JumHorn@gmail.com>
2020-06-21 17:51:18 +0800

    Avoid Flood in The City beats 12.50%

--- Cplus/AvoidFloodinTheCity.cpp
+++ Cplus/AvoidFloodinTheCity.cpp
@@ -9,39 +9,32 @@ public:
 	vector<int> avoidFlood(vector<int>& rains)
 	{
 		int n = rains.size();
-		vector<int> res(n);
-		for (int i = 0; i < n; i++)
-			res[i] = rains[i] > 0 ? -1 : 0;
+		vector<int> res;
 		unordered_map<int, int> m;
 		set<int> dry;
 		for (int i = 0; i < n; i++)
 		{
 			if (rains[i] > 0)
 			{
-				if (m.find(rains[i]) == m.end())
-				{
-					m[rains[i]] = i;
-				}
-				else
+				res.push_back(-1);
+				if (m.find(rains[i]) != m.end())
 				{
 					if (dry.empty())
 						return {};
-					auto iter = upper_bound(dry.begin(), dry.end(), m[rains[i]]);
+					auto iter = dry.upper_bound(m[rains[i]]);
 					if (iter == dry.end())
 						return {};
-
 					res[*iter] = rains[i];
 					dry.erase(iter);
-					m[rains[i]] = i;
 				}
+				m[rains[i]] = i;
 			}
 			else
 			{
+				res.push_back(1);
 				dry.insert(i);
 			}
 		}
-		for (auto n : dry)
-			res[n] = 1;
 		return res;
 	}
 };
