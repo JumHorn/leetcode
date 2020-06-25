2b7b864bcaf9ac0cb40d2f15504a7e6b37a815cb
JumHorn <JumHorn@gmail.com>
2020-06-26 06:46:37 +0800

    Count Different Palindromic Subsequences beats 15.66%

--- Cplus/CountDifferentPalindromicSubsequences.cpp
+++ Cplus/CountDifferentPalindromicSubsequences.cpp
@@ -1,40 +1,43 @@
-#include<string>
+#include <string>
 using namespace std;
 
-class Solution {
-	static const int mod = 1000000007;
+class Solution
+{
 public:
-    int countPalindromicSubsequences(string S) {
-        int res=0;
-        static int dp[1000][1000][4];
-		memset(dp,-1,sizeof(dp));
-		for(int i=0;i<4;i++)
-			res=(res+recursion(dp,S,0,S.size()-1,i))%mod;
-		return res;
-    }
-
-	int recursion(int (*dp)[1000][4],const string& s,int l,int r,int alpha)
+	int countPalindromicSubsequences(string S)
 	{
-		if(l>r)
+		int res = 0, n = S.length();
+		static int dp[1000][1000][4];
+		memset(dp, -1, sizeof(dp));
+		for (int i = 0; i < 4; i++)
+			res = (res + memdp(S, 0, S.size() - 1, i, dp)) % MOD;
+		return res;
+	}
+
+	int memdp(const string &s, int left, int right, int alpha, int (*dp)[1000][4])
+	{
+		if (left > right)
 			return 0;
-		if(l==r)
-        {
-            if(s[l]==(alpha+'a'))
-                return 1;
-			return 0;
-        }
-		if(dp[l][r][alpha]!=-1)
-			return dp[l][r][alpha];
-		int res=0;
-		if(s[l]==s[r]&&s[l]==(alpha+'a'))
+		if (left == right)
+			return (s[left] == (alpha + 'a')) ? 1 : 0;
+		if (dp[left][right][alpha] != -1)
+			return dp[left][right][alpha];
+		int res = 0;
+		if (s[left] == s[right] && s[left] == (alpha + 'a'))
 		{
-			for(int i=0;i<4;i++)
-				res=(res+recursion(dp,s,l+1,r-1,i))%mod;
-			res+=2;
+			for (int i = 0; i < 4; i++)
+				res = (res + memdp(s, left + 1, right - 1, i, dp)) % MOD;
+			res = (res + 2) % MOD;
 		}
 		else
-			res=(recursion(dp,s,l+1,r,alpha)%mod+recursion(dp,s,l,r-1,alpha)%mod-recursion(dp,s,l+1,r-1,alpha)%mod)%mod;
-		dp[l][r][alpha]=res;
-		return res%mod;
+		{
+			res = (res + memdp(s, left + 1, right, alpha, dp)) % MOD;
+			res = (res + memdp(s, left, right - 1, alpha, dp)) % MOD;
+			res = (res + MOD - memdp(s, left + 1, right - 1, alpha, dp)) % MOD;
+		}
+		return dp[left][right][alpha] = res;
 	}
+
+private:
+	static const int MOD = 1e9 + 7;
 };
