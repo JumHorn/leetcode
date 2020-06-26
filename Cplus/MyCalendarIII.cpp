f00e113d8c58602101992b85ff2cc99a93c91b89
JumHorn <JumHorn@gmail.com>
2020-06-26 15:25:04 +0800

    formatted

--- Cplus/MyCalendarIII.cpp
+++ Cplus/MyCalendarIII.cpp
@@ -1,28 +1,29 @@
-#include<map>
-#include<algorithm>
+#include <map>
+#include <algorithm>
 using namespace std;
 
-//求所有book事件的最大值
-
-class MyCalendarThree {
-private:
-	map<int,int> boundary;
+class MyCalendarThree
+{
 public:
-    MyCalendarThree() {
-        
-    }
-    
-    int book(int start, int end) {
-        boundary[start]++;
-		boundary[end]--;
-		int active=0,res=0;
-		for(auto& n:boundary)
+	MyCalendarThree()
+	{
+	}
+
+	int book(int start, int end)
+	{
+		++boundary[start];
+		--boundary[end];
+		int active = 0, res = 0;
+		for (auto &n : boundary)
 		{
-			active+=n.second;
-			res=max(res,active);
+			active += n.second;
+			res = max(res, active);
 		}
 		return res;
-    }
+	}
+
+private:
+	map<int, int> boundary;
 };
 
 /**
