b39bc6a18799062b73d03c17e26fb5f94e223d6b
JumHorn <JumHorn@gmail.com>
2020-06-26 20:08:49 +0800

    Cherry Pickup beats 100%

--- C/CherryPickup.c
+++ C/CherryPickup.c
@@ -1,42 +1,38 @@
 #include <string.h>
 
-int memdp(int** grid, int N, int first, int second, int (*dp)[N * N])
-{
-	if (dp[first][second] != -1)
-		return dp[first][second];
-	int x1 = first / N, y1 = first % N, x2 = second / N, y2 = second % N;
-	if (first == N * N - 1)
-		return grid[N - 1][N - 1];
-	int path[][2] = {{1, 0}, {0, 1}};
-	int res = -N * N, current = 0;
-	current += grid[x1][y1];
-	if (first != second)
-		current += grid[x2][y2];
-	for (int i = 0; i < 2; i++)
-	{
-		for (int j = 0; j < 2; j++)
-		{
-			int dx1 = x1 + path[i][0];
-			int dy1 = y1 + path[i][1];
-			int dx2 = x2 + path[j][0];
-			int dy2 = y2 + path[j][1];
-			if (dx1 >= N || dy1 >= N || dx2 >= N || dy2 >= N || grid[dx1][dy1] == -1 || grid[dx2][dy2] == -1)
-				continue;
-			int tmp = memdp(grid, N, dx1 * N + dy1, dx2 * N + dy2, dp) + current;
-			if (res < tmp)
-				res = tmp;
-		}
-	}
-	return dp[first][second] = res;
-}
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define min(a, b) (((a) < (b)) ? (a) : (b))
 
-int cherryPickup(int** grid, int gridSize, int* gridColSize)
+int cherryPickup(int **grid, int gridSize, int *gridColSize)
 {
-	if (gridSize == 1)
-		return grid[0][0];
-	int n = gridSize * gridSize;
+	int n = gridSize;
 	int dp[n][n];
-	memset(dp, -1, sizeof(dp));
-	memdp(grid, gridSize, 0, 0, dp);
-	return dp[0][0] < 0 ? 0 : dp[0][0];
+	memset(dp, 1u << 7, sizeof(dp)); //init to 0x80808080 a large minus number
+	dp[0][0] = grid[0][0];
+	for (int k = 1; k < 2 * n - 1; k++)
+	{
+		int dp1[n][n];
+		memset(dp1, 1u << 7, sizeof(dp1));
+		for (int i = max(0, k - (n - 1)); i <= min(n - 1, k); i++)
+		{
+			for (int j = max(0, k - (n - 1)); j <= min(n - 1, k); j++)
+			{
+				if (grid[i][k - i] == -1 || grid[j][k - j] == -1)
+					continue;
+				int val = grid[i][k - i];
+				if (i != j)
+					val += grid[j][k - j];
+				for (int pi = i - 1; pi <= i; pi++)
+				{
+					for (int pj = j - 1; pj <= j; pj++)
+					{
+						if (pi >= 0 && pj >= 0)
+							dp1[i][j] = max(dp1[i][j], dp[pi][pj] + val);
+					}
+				}
+			}
+		}
+		memcpy(dp, dp1, sizeof(dp));
+	}
+	return dp[n - 1][n - 1] < 0 ? 0 : dp[n - 1][n - 1];
 }
