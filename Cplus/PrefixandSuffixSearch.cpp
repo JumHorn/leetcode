8b1d284b6893bfc366839777cb41cddd401b60b0
JumHorn <JumHorn@gmail.com>
2020-06-27 08:33:13 +0800

    Prefix and Suffix Search beats 85.83%

--- Cplus/PrefixandSuffixSearch.cpp
+++ Cplus/PrefixandSuffixSearch.cpp
@@ -1,103 +1,70 @@
-#include<vector>
-#include<string>
-#include<unordered_map>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
 using namespace std;
 
-
-class WordFilter {
-	struct TreeNode
-	{
-		TreeNode *node[26];
-		int weight;
-		TreeNode(int w=-1):node({NULL}),weight(-1){}
-	};
-
-	TreeNode* prefixroot;
-	TreeNode* suffixroot;
-	vector<string>& m_words;
-
-	void prefixInsert(const string& word,int weight)
-	{
-		TreeNode* tmp=prefixroot;
-		for(int i=0;i<(int)word.length();i++)
-		{
-			if(tmp->node[word[i]-'a']==NULL)
-				tmp->node[word[i]-'a']=new TreeNode();
-			tmp=tmp->node[word[i]-'a'];
-		}
-		tmp->weight=weight;
-	}
-
-	void suffixInsert(const string& word,int weight)
-	{
-		TreeNode* tmp=suffixroot;
-		for(int i=word.length()-1;i>=0;i--)
-		{
-			if(tmp->node[word[i]-'a']==NULL)
-				tmp->node[word[i]-'a']=new TreeNode();
-			tmp=tmp->node[word[i]-'a'];
-		}
-		tmp->weight=weight;
-	}
-
-	TreeNode* prefixSearch(const string& word)
-	{
-		TreeNode* tmp=prefixroot;
-		for(int i=0;i<(int)word.length();i++)
-		{
-			if(tmp->node[word[i]-'a']==NULL)
-				return NULL;
-			tmp=tmp->node[word[i]-'a'];
-		}
-		return tmp;
-	}
-
-	TreeNode* suffixSearch(const string& word)
-	{
-		TreeNode* tmp=suffixroot;
-		for(int i=word.length()-1;i>=0;i--)
-		{
-			if(tmp->node[word[i]-'a']==NULL)
-				return NULL;
-			tmp=tmp->node[word[i]-'a'];
-		}
-		return tmp;
-	}
-
-	void dfs(TreeNode* root,unordered_map<int,int>& m)
-	{
-		if(root==NULL)
-			return;
-		if(root->weight>=0)
-			++m[root->weight];
-		for(int i=0;i<26;i++)
-			dfs(root->node[i],m);
-	}
-
+class WordFilter
+{
 public:
-    WordFilter(vector<string>& words):m_words(words) {
-        prefixroot=new TreeNode();
-        suffixroot=new TreeNode();
-		for(int i=0;i<(int)words.size();i++)
+	WordFilter(vector<string> &words)
+	{
+		int n = words.size();
+		for (int i = 0; i < n; i++)
+			m_prefix[words[i]] = i;
+	}
+
+	int f(string prefix, string suffix)
+	{
+		auto iter = m_prefix.lower_bound(prefix);
+		set<int> s;
+		while (iter != m_prefix.end())
 		{
-			prefixInsert(words[i],i);
-			suffixInsert(words[i],i);
+			if (!starts_with(iter->first, prefix))
+				break;
+			if (ends_with(iter->first, suffix))
+				s.insert(iter->second);
+			++iter;
 		}
-    }
-    
-    int f(string prefix, string suffix) {
-        unordered_map<int,int> m;
-		TreeNode *pfx=prefixSearch(prefix),*sfx=suffixSearch(suffix);
-		if(pfx==NULL||sfx==NULL)
+		if (s.empty())
 			return -1;
-		dfs(pfx,m);
-		dfs(sfx,m);
-		int res=-1;
-		for(auto& n : m)
-			if(n.second>=2&&n.first>res)
-				res=n.first;
-		return res;
-    }
+		return *s.rbegin();
+	}
+
+	bool starts_with(const string &s, const string &prefix)
+	{
+		if (prefix.empty())
+			return true;
+		int n = s.length(), m = prefix.length();
+		int i = 0, j = 0;
+		while (i < n && j < m)
+		{
+			if (s[i] != prefix[j])
+				return false;
+			++i;
+			++j;
+		}
+		return j == m;
+	}
+
+	bool ends_with(const string &s, const string &suffix)
+	{
+		if (suffix.empty())
+			return true;
+		int n = s.length(), m = suffix.length();
+		int i = n - 1, j = m - 1;
+		while (i >= 0 && j >= 0)
+		{
+			if (s[i] != suffix[j])
+				return false;
+			--i;
+			--j;
+		}
+		return j == -1;
+	}
+
+private:
+	map<string, int> m_prefix;
 };
 
 /**
