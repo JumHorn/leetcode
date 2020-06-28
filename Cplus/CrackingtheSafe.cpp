efbcd23f090ab112a9cbfcf89b4916bf79ddbde4
JumHorn <JumHorn@gmail.com>
2020-06-29 07:36:33 +0800

    Cracking the Safe beats 59.34%

--- Cplus/CrackingtheSafe.cpp
+++ Cplus/CrackingtheSafe.cpp
@@ -1,29 +1,32 @@
-#include<string>
-#include<vector>
-#include<unordered_set>
-#include<cmath>
+#include <string>
+#include <vector>
+#include <unordered_set>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    string crackSafe(int n, int k) {
-        unordered_set<string> visited;
-        string node(n-1,'0'),res;
-        dfs(node,n,k,visited,res);
-        return res+node;
-    }
+	string crackSafe(int n, int k)
+	{
+		unordered_set<string> seen;
+		string node(n - 1, '0'), res;
+		dfs(node, k, seen, res);
+		return res + node;
+	}
 
-    void dfs(string node,int n,int k,unordered_set<string>& visited,string& res)
-    {
-        for(int i=0;i<k;i++)
-        {
-            string tmp=node+to_string(i);
-            if(visited.find(tmp)==visited.end())
-            {
-                visited.insert(tmp);
-                dfs(tmp.substr(1),n,k,visited,res);
-                res+=to_string(i);
-            }
-        }
-    }
+	void dfs(string &node, int k, unordered_set<string> &seen, string &res)
+	{
+		for (int i = 0; i < k; i++)
+		{
+			node.push_back('0' + i);
+			if (seen.find(node) == seen.end())
+			{
+				seen.insert(node);
+				string next = node.substr(1);
+				dfs(next, k, seen, res);
+				res.push_back('0' + i);
+			}
+			node.pop_back();
+		}
+	}
 };
