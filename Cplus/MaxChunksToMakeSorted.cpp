698e0fd41e96581454d559b02bf538f18b85183d
JumHorn <JumHorn@gmail.com>
2020-07-02 06:52:11 +0800

    Max Chunks To Make Sorted beats 100%

--- Cplus/MaxChunksToMakeSorted.cpp
+++ Cplus/MaxChunksToMakeSorted.cpp
@@ -1,44 +1,26 @@
-#include<iostream>
-#include<vector>
+#include <algorithm>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int maxChunksToSorted(vector<int>& arr) {
-        int res=0;
-		for(int i=0;i<(int)arr.size();i++)
-		{
-			res++;
-			int right=getIndex(arr,i);
-            int rightvalue=arr[i];
-			if(right<=i)
-			{
-				continue;
-			}
-			for(int j=i+1;j<=right;j++)
-			{
-				int tmp=getIndex(arr,j);
-				if(tmp>right)
-                {
-					right=tmp;
-                    rightvalue=arr[j];
-                }
-                else if(arr[j]==rightvalue)
-                    right++;
-				if(right==(int)arr.size())
-					return res;
-			}
-			i=right;
-		}
-		return res;
-    }
-
-	int getIndex(vector<int>& arr,int start)
+	int maxChunksToSorted(vector<int> &arr)
 	{
-		int res=0;
-		for(int i=0;i<(int)arr.size();i++)
-			if(arr[start]>arr[i])
-				res++;
+		vector<int> dup = arr;
+		sort(dup.begin(), dup.end());
+		unordered_map<int, int> m;
+		const int N = arr.size();
+		for (int i = N - 1; i >= 0; --i)
+			m[dup[i]] = i;
+		int res = 0;
+		for (int i = 0, j = 0; i < N; i++)
+		{
+			j = max(j, m[arr[i]]);
+			if (i == j)
+				++res;
+		}
 		return res;
 	}
 };
