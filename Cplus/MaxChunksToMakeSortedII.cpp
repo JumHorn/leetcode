79e613d54c5b5ef0e779920ba9105757fb5f7a23
JumHorn <JumHorn@gmail.com>
2020-07-02 06:47:03 +0800

    Max Chunks To Make Sorted II beats 33.63%

---
+++ Cplus/MaxChunksToMakeSortedII.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+	int maxChunksToSorted(vector<int> &arr)
+	{
+		vector<int> dup = arr;
+		sort(dup.begin(), dup.end());
+		unordered_map<int, int> m;
+		const int N = arr.size();
+		for (int i = N - 1; i >= 0; --i)
+			m[dup[i]] = i - 1;
+		int res = 0;
+		for (int i = 0, j = 0; i < N; i++)
+		{
+			++m[arr[i]];
+			j = max(j, m[arr[i]]);
+			if (i == j)
+				++res;
+		}
+		return res;
+	}
+};
