445a3ed30d34f184aa57e7c5c89aef38f0bcda2e
JumHorn <JumHorn@gmail.com>
2020-07-01 08:18:51 +0800

    formatted

--- Cplus/CouplesHoldingHands.cpp
+++ Cplus/CouplesHoldingHands.cpp
@@ -1,21 +1,25 @@
-#include<vector>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int minSwapsCouples(vector<int>& row) {
-		int res=0;
-		for(int i=0;i<(int)row.size();i+=2)
+	int minSwapsCouples(vector<int> &row)
+	{
+		int res = 0, n = row.size();
+		for (int i = 0; i < n; i += 2)
 		{
-			int match=(row[i]&1)?row[i]-1:row[i]+1;
-			if(row[i+1]!=match)
+			int match = (row[i] & 1) ? row[i] - 1 : row[i] + 1;
+			if (row[i + 1] != match)
 			{
 				res++;
-				for(int j=i+2;j<(int)row.size();j++)
-					if(match==row[j])
-						row[j]=row[i+1];
+				for (int j = i + 2; j < n; j++)
+				{
+					if (match == row[j])
+						row[j] = row[i + 1];
+				}
 			}
-		}		
+		}
 		return res;
-    }
+	}
 };
