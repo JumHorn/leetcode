08f149d0f539eaefa0f5f7d3a1e79410192c515e
JumHorn <JumHorn@gmail.com>
2020-07-02 09:24:24 +0800

    Sliding Puzzle beats 99.69%

--- Cplus/SlidingPuzzle.cpp
+++ Cplus/SlidingPuzzle.cpp
@@ -1,89 +1,89 @@
-#include<vector>
-#include<string>
-#include<unordered_set>
-#include<queue>
-#include<algorithm>
+#include <queue>
+#include <unordered_set>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int slidingPuzzle(vector<vector<int>>& board) {
-		unordered_set<string> visited;
-		queue<pair<string,pair<int,int>>> q;
- 		q.push(boardToString(board));
-		visited.insert(q.front().first);
-		string finalstate="123450";
-		int res=0,M=board.size(),N=board[0].size();
-		while(!q.empty())
+	int slidingPuzzle(vector<vector<int>> &board)
+	{
+		unordered_set<int> seen;
+		queue<int> q;
+		int res = 0, M = board.size(), N = board[0].size();
+		int state = compress(board), finalstate = 123450;
+		if (state == finalstate)
+			return res;
+		q.push(state);
+		while (!q.empty())
 		{
-			int s=q.size();
-			while(--s>=0)
+			++res;
+			int size = q.size();
+			while (--size >= 0)
 			{
-				string top=q.front().first;
-				if(top==finalstate)
-					return res;
-				int i=q.front().second.first,j=q.front().second.second;
-                q.pop();
-				if(i-1>=0)
+				state = q.front();
+				q.pop();
+				decompress(board, state);
+				pair<int, int> zero = findZero(board);
+				//board dfs direction
+				int path[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+				for (int k = 0; k < 4; k++)
 				{
-					swap(top[(i-1)*N+j],top[i*N+j]);
-					if(visited.find(top)==visited.end())
+					int x = zero.first + path[k][0], y = zero.second + path[k][1];
+					if (x < 0 || x >= M || y < 0 || y >= N)
+						continue;
+					swap(board[zero.first][zero.second], board[x][y]);
+					state = compress(board);
+					if (seen.find(state) == seen.end())
 					{
-						visited.insert(top);
-						q.push({top,{i-1,j}});
+						if (state == finalstate)
+							return res;
+						seen.insert(state);
+						q.push(state);
 					}
-					swap(top[(i-1)*N+j],top[i*N+j]);
-				}
-				if(i+1<M)
-				{
-					swap(top[(i+1)*N+j],top[i*N+j]);
-					if(visited.find(top)==visited.end())
-					{
-						visited.insert(top);
-						q.push({top,{i+1,j}});
-					}
-					swap(top[(i+1)*N+j],top[i*N+j]);
-				}
-				if(j-1>=0)
-				{
-					swap(top[i*N+j-1],top[i*N+j]);
-					if(visited.find(top)==visited.end())
-					{
-						visited.insert(top);
-						q.push({top,{i,j-1}});
-					}
-					swap(top[i*N+j-1],top[i*N+j]);
-				}
-				if(j+1<N)
-				{
-					swap(top[i*N+j+1],top[i*N+j]);
-					if(visited.find(top)==visited.end())
-					{
-						visited.insert(top);
-						q.push({top,{i,j+1}});
-					}
-					swap(top[i*N+j+1],top[i*N+j]);
+					swap(board[zero.first][zero.second], board[x][y]);
 				}
 			}
-			res++;
 		}
 		return -1;
-    }
+	}
 
-	pair<string,pair<int,int>> boardToString(vector<vector<int>>& board)
+	int compress(vector<vector<int>> &board)
 	{
-		int x,y;
-		string res;
-		for(int i=0;i<(int)board.size();i++)
-			for(int j=0;j<(int)board[0].size();j++)
+		int M = board.size(), N = board[0].size();
+		int res = 0;
+		for (int i = 0; i < M; i++)
+		{
+			for (int j = 0; j < N; j++)
+				res = res * 10 + board[i][j];
+		}
+		return res;
+	}
+
+	void decompress(vector<vector<int>> &board, int state)
+	{
+		int M = board.size(), N = board[0].size();
+		for (int i = M - 1; i >= 0; --i)
+		{
+			for (int j = N - 1; j >= 0; --j)
 			{
-				if(board[i][j]==0)
-				{
-					x=i;
-					y=j;
-				}
-				res+=to_string(board[i][j]);
+				board[i][j] = state % 10;
+				state /= 10;
 			}
-		return {res,{x,y}};
+		}
+	}
+
+	pair<int, int> findZero(vector<vector<int>> &board)
+	{
+		int M = board.size(), N = board[0].size();
+		for (int i = 0; i < M; i++)
+		{
+			for (int j = 0; j < N; j++)
+			{
+				if (board[i][j] == 0)
+					return {i, j};
+			}
+		}
+		return {0, 0};
 	}
 };
