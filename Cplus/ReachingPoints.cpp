32eec4f97e5d44445b5ade48af5ca1cc3ee94c9c
JumHorn <JumHorn@gmail.com>
2020-07-04 06:48:01 +0800

    formatted

--- Cplus/ReachingPoints.cpp
+++ Cplus/ReachingPoints.cpp
@@ -1,13 +1,15 @@
-class Solution {
+class Solution
+{
 public:
-    bool reachingPoints(int sx, int sy, int tx, int ty) {
-		while(sx<tx&&sy<ty)
+	bool reachingPoints(int sx, int sy, int tx, int ty)
+	{
+		while (sx < tx && sy < ty)
 		{
-			if(tx>ty)
-				tx%=ty;
+			if (tx > ty)
+				tx %= ty;
 			else
-				ty%=tx;
+				ty %= tx;
 		}
-		return (sx==tx&&sy==ty)||(sx==tx&&ty>=sy&&0==(ty-sy)%tx)||(sy==ty&&tx>=sx&&0==(tx-sx)%ty);
-    }
+		return (sx == tx && sy == ty) || (sx == tx && ty >= sy && 0 == (ty - sy) % tx) || (sy == ty && tx >= sx && 0 == (tx - sx) % ty);
+	}
 };
