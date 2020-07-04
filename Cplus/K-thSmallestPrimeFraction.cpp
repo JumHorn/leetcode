44cf9b3b4a9fd7d75cb6bec66b9872d2ed449563
JumHorn <JumHorn@gmail.com>
2020-07-04 10:18:43 +0800

    time limited exceeded

--- Cplus/K-thSmallestPrimeFraction.cpp
+++ Cplus/K-thSmallestPrimeFraction.cpp
@@ -1,28 +1,28 @@
-#include<vector>
-#include<queue>
+#include <vector>
+#include <queue>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> kthSmallestPrimeFraction(vector<int>& A, int K) {
-		int n=A.size();
-		priority_queue<pair<double,pair<int,int> > > q;
-		if(K==1)
-			return vector<int>({A.front(),A.back()});
-		q.push({-1.0*A[0]/A[n-2],{0,n-2}});
-		for(int i=1;i<n;i++)
+	vector<int> kthSmallestPrimeFraction(vector<int> &A, int K)
+	{
+		int n = A.size();
+		auto f = [&](int *lhs, int *rhs) { return A[lhs[0]] * A[rhs[1]] > A[lhs[1]] * A[rhs[0]]; };
+		priority_queue<int *, vector<int *>, decltype(f)> q(f);
+		for (int i = 0; i < n - 1; i++)
+			q.push(new int[2]{i, n - 1});
+		int x, y;
+		while (--K > 0)
 		{
-			q.push({-1.0*A[i]/A.back(),{i,n-1}});
+			int *top = q.top();
+			q.pop();
+			if (top[0] < top[1] - 1)
+			{
+				--top[1];
+				q.push(top);
+			}
 		}
-        int x,y;
-		while(--K!=0)
-		{
-			x=q.top().second.first;
-			y=q.top().second.second;
-            q.pop();
-			if(x<=y-1)
-				q.push({-1.0*A[x]/A[y-1],{x,y-1}});
-		}
-		return vector<int>({A[x],A[y]});
-    }
+		return {A[q.top()[0]], A[q.top()[1]]};
+	}
 };
