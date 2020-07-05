0b90477ac18b00e4d3044d38fa389ac161faf18c
JumHorn <JumHorn@gmail.com>
2020-07-06 07:21:21 +0800

    Smallest Rotation with Highest Score beats 100%

---
+++ C/SmallestRotationwithHighestScore.c
@@ -0,0 +1,18 @@
+#include <string.h>
+
+int bestRotation(int* A, int ASize)
+{
+	int score[ASize];
+	memset(score, 0, sizeof(score));
+	for (int i = 0; i < ASize; i++)	 //for every A[i] where to lost point
+		--score[(i - A[i] + 1 + ASize) % ASize];
+	for (int i = 1; i < ASize; i++)
+		score[i] += score[i - 1] + 1;
+	int index = 0;
+	for (int i = 1; i < ASize; i++)
+	{
+		if (score[i] > score[index])
+			index = i;
+	}
+	return index;
+}
