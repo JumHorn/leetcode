2a3f83f94e1f2314a04b58b413b1d625087b422c
JumHorn <JumHorn@gmail.com>
2020-07-09 07:21:24 +0800

    formatted

--- Cplus/ChalkboardXORGame.cpp
+++ Cplus/ChalkboardXORGame.cpp
@@ -1,12 +1,14 @@
-#include<vector>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool xorGame(vector<int>& nums) {
-		int XOR=0;
-		for(int i=0;i<(int)nums.size();i++)
-			XOR^=nums[i];	
-		return XOR==0||nums.size()%2==0;
-    }
+	bool xorGame(vector<int> &nums)
+	{
+		int XOR = 0;
+		for (int i = 0; i < (int)nums.size(); i++)
+			XOR ^= nums[i];
+		return XOR == 0 || nums.size() % 2 == 0;
+	}
 };
