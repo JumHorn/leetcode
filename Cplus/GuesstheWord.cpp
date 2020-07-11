47d5a407af6c04976ee397bf7ada02f95467b83c
JumHorn <JumHorn@gmail.com>
2020-07-11 22:12:23 +0800

    formatted

--- Cplus/GuesstheWord.cpp
+++ Cplus/GuesstheWord.cpp
@@ -1,48 +1,54 @@
-#include<string>
-#include<vector>
-#include<set>
+#include <string>
+#include <vector>
+#include <set>
 using namespace std;
 /**
  * // This is the Master's API interface.
  * // You should not implement it, or speculate about its implementation
  */
 
-class Master {
-  public:
-    int guess(string word);
+class Master
+{
+public:
+	int guess(string word);
 };
 
-class Solution {
+class Solution
+{
 public:
-    void findSecretWord(vector<string>& wordlist, Master& master) {
-        set<string> possible(wordlist.begin(),wordlist.end());
-		int diff=master.guess(*possible.begin());
-		while(diff<(int)wordlist.front().length())
-		{
-			wordlistReduce(possible,diff);
-			diff=master.guess(*possible.begin());
-		}
-    }
-
-	void wordlistReduce(set<string>& possible,int diff)
+	void findSecretWord(vector<string> &wordlist, Master &master)
 	{
-		set<string>::iterator iter=possible.begin();
-		string guessed=*iter;
-		set<string> tmp;
-		for(++iter;iter!=possible.end();++iter)
+		set<string> possible(wordlist.begin(), wordlist.end());
+		int diff = master.guess(*possible.begin());
+		while (diff < (int)wordlist.front().length())
 		{
-			if(strdiff(guessed,*iter)==diff)
-				tmp.insert(*iter);
+			wordlistReduce(possible, diff);
+			diff = master.guess(*possible.begin());
 		}
-		possible=tmp;
 	}
 
-	int strdiff(const string& lhs,const string& rhs)
+	void wordlistReduce(set<string> &possible, int diff)
 	{
-		int res=0;
-		for(int i=0;i<(int)lhs.length();i++)
-			if(lhs[i]==rhs[i])
+		set<string>::iterator iter = possible.begin();
+		string guessed = *iter;
+		iter = possible.erase(iter);
+		while (iter != possible.end())
+		{
+			if (strdiff(guessed, *iter) != diff)
+				iter = possible.erase(iter);
+			else
+				++iter;
+		}
+	}
+
+	int strdiff(const string &lhs, const string &rhs)
+	{
+		int res = 0;
+		for (int i = 0; i < (int)lhs.length(); i++)
+		{
+			if (lhs[i] == rhs[i])
 				res++;
+		}
 		return res;
 	}
 };
