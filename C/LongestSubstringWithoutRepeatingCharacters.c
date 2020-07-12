76c5cb61b1ceac467f5970f086c82768c32f18da
JumHorn <JumHorn@gmail.com>
2020-07-12 14:08:35 +0800

    Longest Substring Without Repeating Characters beats 100%

--- C/LongestSubstringWithoutRepeatingCharacters.c
+++ C/LongestSubstringWithoutRepeatingCharacters.c
@@ -1,30 +1,18 @@
 #include <stdbool.h>
-#include <string.h>
 
-bool checkRepeat(int* map)
-{
-	for (int i = 0; i < 128; i++)
-	{
-		if (map[i] > 1)
-			return false;
-	}
-	return true;
-}
+#define max(a, b) (((a) > (b)) ? (a) : (b))
 
-int lengthOfLongestSubstring(char* s)
+int lengthOfLongestSubstring(char *s)
 {
 	if (!*s)
 		return 0;
-	int map[128] = {0};
-	int res = 2, len = strlen(s);
-	++map[(int)*s];
-	for (int i = 1; i < len; i++)
+	int map[128] = {0}, res = 0;
+	char *p = s;
+	for (int i = 0; *p; ++p)
 	{
-		++map[(int)s[i]];
-		if (checkRepeat(map))
-			++res;
-		else
-			--map[(int)s[i - res + 1]];
+		i = max(i, map[(int)*p]);
+		map[(int)*p] = p - s + 1;
+		res = max(res, p - s - i + 1);
 	}
-	return res - 1;
+	return res;
 }
