456816cd09a63abdb80e7da3dd49e03bd74936da
JumHorn <JumHorn@gmail.com>
2020-07-14 06:46:21 +0800

    Count Unique Characters of All Substrings of a Given String beats 75.00%

---
+++ C/CountUniqueCharactersofAllSubstringsofaGivenString.c
@@ -0,0 +1,27 @@
+#include <string.h>
+
+int uniqueLetterString(char* s)
+{
+	static const int MOD = 1e9 + 7;
+	if (!*s)
+		return 0;
+	int N = strlen(s);
+	int left[N], right[N];
+	int map[26] = {0};
+	for (int i = 0; i < N; i++)
+	{
+		left[i] = i - map[s[i] - 'A'] + 1;
+		map[s[i] - 'A'] = i + 1;
+	}
+	for (int i = 0; i < 26; i++)
+		map[i] = N - 1;
+	for (int i = N - 1; i >= 0; --i)
+	{
+		right[i] = map[s[i] - 'A'] - i + 1;
+		map[s[i] - 'A'] = i - 1;
+	}
+	int res = 0;
+	for (int i = 0; i < N; i++)
+		res = (res + left[i] * right[i]) % MOD;
+	return res;
+}
