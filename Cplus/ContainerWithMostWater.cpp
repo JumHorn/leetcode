33fa2ebbc5659a82ada611926a032652378656a3
JumHorn <JumHorn@gmail.com>
2020-07-15 06:50:47 +0800

    Container With Most Water beats 17.32%

--- Cplus/ContainerWithMostWater.cpp
+++ Cplus/ContainerWithMostWater.cpp
@@ -1,18 +1,18 @@
-#include<vector>
-#include<algorithm>
+#include <vector>
+#include <algorithm>
 using namespace std;
-/*
-dp[i][j]=max{ dp[i-1][j],dp[i][j-1],(j-i)*min{height[i],height[j]} }
-0<=i<n,i<j<=n
-*/
 
-class Solution {
+class Solution
+{
 public:
-    int maxArea(vector<int>& height) {
-		int res=0;
-		for(int i=0;i<height.size();i++)
-			for(int j=i+1;j<height.size();j++)
-                res=max(res,(j-i)*min(height[i],height[j]));
+	int maxArea(vector<int> &height)
+	{
+		int res = 0;
+		for (int i = 0, j = height.size() - 1; i < j;)
+		{
+			res = max(res, (j - i) * min(height[i], height[j]));
+			height[i] < height[j] ? ++i : --j;
+		}
 		return res;
-    }
+	}
 };
