42d71da4e07c07f8fb44dd16006dac3c69846cc3
JumHorn <JumHorn@gmail.com>
2020-07-15 20:15:50 +0800

    Integer to Roman beats 40.87%

--- Cplus/IntegertoRoman.cpp
+++ Cplus/IntegertoRoman.cpp
@@ -1,5 +1,4 @@
-#include<iostream>
-#include<string>
+#include <string>
 using namespace std;
 
 // Symbol       Value
@@ -11,56 +10,56 @@ using namespace std;
 // D             500
 // M             1000
 
-class Solution {
+class Solution
+{
 public:
-    string intToRoman(int num) {
-		string roman;
-		int t=num/1000;
-		for(int i=0;i<t;i++)
-		{
-			roman+="M";
-		}	
-		num=num%1000;	
-		roman+=makeRoman(num/100,"M","D","C");
-		num=num%100;	
-		roman+=makeRoman(num/10,"C","L","X");
-		num=num%10;	
-		roman+=makeRoman(num,"X","V","I");
-		return roman;
-    }
-
-	string makeRoman(int num,const string& big,const string& middle,const string& small)
+	string intToRoman(int num)
 	{
 		string roman;
-		switch(num)
+		int t = num / 1000;
+		for (int i = 0; i < t; i++)
+			roman += "M";
+		num = num % 1000;
+		roman += makeRoman(num / 100, "M", "D", "C");
+		num = num % 100;
+		roman += makeRoman(num / 10, "C", "L", "X");
+		num = num % 10;
+		roman += makeRoman(num, "X", "V", "I");
+		return roman;
+	}
+
+	string makeRoman(int num, const string &big, const string &middle, const string &small)
+	{
+		string roman;
+		switch (num)
 		{
 		case 1:
-			roman=small;
-		break;
+			roman = small;
+			break;
 		case 2:
-			roman=small+small;
-		break;
+			roman = small + small;
+			break;
 		case 3:
-			roman=small+small+small;
-		break;
+			roman = small + small + small;
+			break;
 		case 4:
-			roman=small+middle;
-		break;
+			roman = small + middle;
+			break;
 		case 5:
-			roman=middle;
-		break;
+			roman = middle;
+			break;
 		case 6:
-			roman=middle+small;
-		break;
+			roman = middle + small;
+			break;
 		case 7:
-			roman=middle+small+small;
-		break;
+			roman = middle + small + small;
+			break;
 		case 8:
-			roman=middle+small+small+small;
-		break;
+			roman = middle + small + small + small;
+			break;
 		case 9:
-			roman=small+big;
-		break;
+			roman = small + big;
+			break;
 		}
 		return roman;
 	}
