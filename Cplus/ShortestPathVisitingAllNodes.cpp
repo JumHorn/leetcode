42207f7c34471ee024cc7a53000bc6326bf4fbc7
JumHorn <JumHorn@gmail.com>
2020-07-20 07:06:34 +0800

    Shortest Path Visiting All Nodes beats 48.12%

--- Cplus/ShortestPathVisitingAllNodes.cpp
+++ Cplus/ShortestPathVisitingAllNodes.cpp
@@ -1,54 +1,37 @@
-#include<vector>
-#include<queue>
-#include<set>
+#include <queue>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int shortestPathLength(vector<vector<int> >& graph) {
-        if(graph.empty()||graph[0].empty())
-            return 0;
-		int n=(1<<graph.size())-1;
-		queue<pair<int,int> > *q=new queue<pair<int,int> >();
-		queue<pair<int,int> > *p=new queue<pair<int,int> >();
-		set<pair<int,int> > visited;
-        for(int i=0;i<(int)graph.size();i++)
-			if((graph[i].size()&1)==1)
-			{
-				visited.insert(pair<int,int>(i,1<<i));
-				q->push(pair<int,int>(i,1<<i));
-			}
-		if(q->empty())
-			for(int i=0;i<(int)graph.size();i++)
-			{
-				visited.insert(pair<int,int>(i,1<<i));
-				q->push(pair<int,int>(i,1<<i));
-			}
-		int res=0;
-		while(true)
+	int shortestPathLength(vector<vector<int>>& graph)
+	{
+		int N = graph.size(), res = 0;
+		vector<vector<int>> dp(1 << N, vector<int>(N, N * N));
+		queue<pair<int, int>> q;
+		for (int i = 0; i < N; i++)
 		{
-			while(!q->empty())
-			{
-				if(q->front().second==n)
-					return res;
-				for(int i=graph[q->front().first].size()-1;i>=0;i--)
-				{
-					pair<int,int> tmp(graph[q->front().first][i],q->front().second|(1<<graph[q->front().first][i]));
-					if(visited.find(tmp)==visited.end())
-					{
-						visited.insert(tmp);
-						p->push(tmp);
-					}
-				}
-				q->pop();
-			}
-			res++;
-			queue<pair<int,int> > *tmp=p;
-			p=q;
-			q=tmp;
+			q.push({1 << i, i});
+			dp[1 << i][i] = 0;
 		}
-		delete p;
-		delete q;
+		while (!q.empty())
+		{
+			auto node = q.front();
+			q.pop();
+			int dist = dp[node.first][node.second];
+			if (node.first == (1 << N) - 1)
+				return dist;
+			for (int child : graph[node.second])
+			{
+				int cover = (node.first | (1 << child));
+				if (dist + 1 < dp[cover][child])
+				{
+					dp[cover][child] = dist + 1;
+					q.push({cover, child});
+				}
+			}
+		}
 		return res;
 	}
 };
