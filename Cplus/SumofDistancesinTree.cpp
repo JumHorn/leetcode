0152bd5182a399d289ac26ce84a1bb71f2f44542
JumHorn <JumHorn@gmail.com>
2020-07-21 18:45:54 +0800

    Sum of Distances in Tree beats 41.67%

--- Cplus/SumofDistancesinTree.cpp
+++ Cplus/SumofDistancesinTree.cpp
@@ -5,22 +5,21 @@ using namespace std;
 class Solution
 {
 public:
-	vector<int> sumOfDistancesInTree(int N, vector<vector<int>>& edges)
+	vector<int> sumOfDistancesInTree(int N, vector<vector<int>> &edges)
 	{
 		vector<unordered_set<int>> graph(N);
-		for (auto& edge : edges)
+		for (auto &edge : edges)
 		{
 			graph[edge[0]].insert(edge[1]);
 			graph[edge[1]].insert(edge[0]);
 		}
-		vector<int> res(N), nodecount(N);
-		dfs(graph, nodecount, 0);
-		res[0] = node0distance(graph, nodecount, 0);
+		vector<int> res(N), nodecount(N, 1);
+		res[0] = postorder(graph, nodecount, 0);
 		preorder(graph, nodecount, res, 0);
 		return res;
 	}
 
-	void preorder(vector<unordered_set<int>>& graph, vector<int>& nodecount, vector<int>& dis, int node)
+	void preorder(vector<unordered_set<int>> &graph, vector<int> &nodecount, vector<int> &dis, int node)
 	{
 		int N = graph.size();
 		for (auto n : graph[node])
@@ -30,25 +29,16 @@ public:
 		}
 	}
 
-	int node0distance(vector<unordered_set<int>>& graph, vector<int>& nodecount, int node)
+	int postorder(vector<unordered_set<int>> &graph, vector<int> &nodecount, int node)
 	{
 		int res = 0;
 		for (auto n : graph[node])
 		{
-			res += node0distance(graph, nodecount, n) + nodecount[n];
-		}
-		return res;
-	}
-
-	int dfs(vector<unordered_set<int>>& graph, vector<int>& nodecount, int node)
-	{
-		int res = 1;
-		for (auto n : graph[node])
-		{
 			graph[n].erase(node);
-			res += dfs(graph, nodecount, n);
+			res += postorder(graph, nodecount, n);
+			nodecount[node] += nodecount[n];
+			res += nodecount[n];
 		}
-		nodecount[node] = res;
 		return res;
 	}
 };
