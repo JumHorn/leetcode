2d225611a3c66739f3078919cdf5376ebfe0d1b5
JumHorn <JumHorn@gmail.com>
2020-07-22 07:11:52 +0800

    Rectangle Area II beats 96.12%

--- Cplus/RectangleAreaII.cpp
+++ Cplus/RectangleAreaII.cpp
@@ -1,46 +1,48 @@
 #include <algorithm>
-#include <map>
-#include <set>
+#include <climits>
 #include <vector>
 using namespace std;
+
+/*
+Algorithm
+Y axis line scan
+*/
 
 class Solution
 {
 public:
 	int rectangleArea(vector<vector<int>> &rectangles)
 	{
-		map<int, vector<vector<int>>> m; //{x,{{ylower,yupper,in/out}}}
-		for (auto &rect : rectangles)
+		static const int MOD = 1e9 + 7;
+		unsigned long long res = 0, y = 0;
+		sort(rectangles.begin(), rectangles.end(), *this);
+		while (y != INT_MAX)
 		{
-			m[rect[0]].push_back({rect[1], rect[3], 1});
-			m[rect[2]].push_back({rect[1], rect[3], -1});
-		}
-		multiset<int> lower, upper;
-		unsigned long long pre = 0, res = 0;
-		for (auto &n : m)
-		{
-			int y = 0;
-			if (!lower.empty())
-				y = *upper.rbegin() - *lower.begin();
-			res += y * (n.first - pre);
-			pre = n.first;
-
-			//add or delete
-			for (auto &yaxis : n.second)
+			bool flag = false;
+			unsigned long long x = 0, xlen = 0, ystep = INT_MAX;
+			for (int i = 0; i < (int)rectangles.size(); i++)
 			{
-				if (yaxis[2] == 1)
+				if (rectangles[i][1] > (int)y)
+					ystep = min((int)ystep, rectangles[i][1]);
+				if ((int)y >= rectangles[i][1] && (int)y < rectangles[i][3] && rectangles[i][2] > (int)x)
 				{
-					lower.insert(yaxis[0]);
-					upper.insert(yaxis[1]);
-				}
-				else
-				{
-					lower.erase(lower.find(yaxis[0]));
-					upper.erase(upper.find(yaxis[1]));
+					flag = true;
+					ystep = min((int)ystep, rectangles[i][3]);
+					x = max((int)x, rectangles[i][0]);
+					xlen += rectangles[i][2] - x;
+					x = rectangles[i][2];
 				}
 			}
+			if (flag)
+				res += xlen * (ystep - y);
+			y = ystep;
 		}
 		return res % MOD;
+	}
+
+	bool operator()(vector<int> &left, vector<int> &right)
+	{
+		return left[0] < right[0];
 	}
 
 private:
