8879879aa23f3ca313018dec9ae0b369c3655e0e
JumHorn <JumHorn@gmail.com>
2020-07-25 22:26:51 +0800

    formatted

--- Cplus/ShortestSubarraywithSumatLeastK.cpp
+++ Cplus/ShortestSubarraywithSumatLeastK.cpp
@@ -1,31 +1,31 @@
-#include<algorithm>
-#include<numeric>
-#include<vector>
-#include<deque>
-#include<algorithm>
-#include<climits>
+#include <algorithm>
+#include <vector>
+#include <deque>
+#include <climits>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int shortestSubarray(vector<int>& A, int K) {
-		vector<int> sums(A.size()+1);
+	int shortestSubarray(vector<int> &A, int K)
+	{
+		int res = INT_MAX, N = A.size();
+		vector<int> sums(N + 1);
 		deque<int> monoqueue;
-		int res=INT_MAX;
-		for(int i=1;i<=(int)A.size();i++)
-			sums[i]=sums[i-1]+A[i-1];
+		for (int i = 0; i < N; i++)
+			sums[i + 1] = sums[i] + A[i];
 		monoqueue.push_back(0);
-		for(int i=1;i<=(int)A.size();i++)
+		for (int i = 1; i <= N; i++)
 		{
-			while(!monoqueue.empty()&&sums[i]-sums[monoqueue.front()]>=K)
+			while (!monoqueue.empty() && sums[i] - sums[monoqueue.front()] >= K)
 			{
-				res=min(res,i-monoqueue.front());
+				res = min(res, i - monoqueue.front());
 				monoqueue.pop_front();
 			}
-			while(!monoqueue.empty()&&sums[i]<=sums[monoqueue.back()])
+			while (!monoqueue.empty() && sums[i] <= sums[monoqueue.back()])
 				monoqueue.pop_back();
 			monoqueue.push_back(i);
 		}
-		return res==INT_MAX?-1:res;
-    }
+		return res == INT_MAX ? -1 : res;
+	}
 };
