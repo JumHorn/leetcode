520e6b0c3d079c264eeba18ce53f1875af07135e
JumHorn <JumHorn@gmail.com>
2020-07-26 06:36:50 +0800

    Minimum Number of Increments on Subarrays to Form a Target Array beats 14.29%

--- Cplus/MinimumNumberofIncrementsonSubarraystoFormaTargetArray.cpp
+++ Cplus/MinimumNumberofIncrementsonSubarraystoFormaTargetArray.cpp
@@ -7,29 +7,11 @@ class Solution
 public:
 	int minNumberOperations(vector<int> &target)
 	{
-		int N = target.size();
-		return recursion(target, 0, N - 1);
-	}
-
-	int recursion(vector<int> &target, int first, int last)
-	{
-		if (first > last)
-			return 0;
-		if (first == last)
-			return target[first];
-		int min = *min_element(target.begin() + first, target.begin() + last + 1);
-		int res = min;
-		for (int i = first; i <= last; ++i)
-			target[i] -= min;
-		for (int i = first, j = first; j <= last;)
+		int res = 0, pre = 0;
+		for (auto n : target)
 		{
-			while (i <= last && target[i] == 0)
-				++i;
-			j = i;
-			while (j <= last && target[j] != 0)
-				++j;
-			res += recursion(target, i, j - 1);
-			i = j;
+			res += max(n - pre, 0);
+			pre = n;
 		}
 		return res;
 	}
