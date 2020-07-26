b6f4d3110f5dfd75daa47026f40cb2a404c4c5d7
JumHorn <JumHorn@gmail.com>
2020-07-27 07:16:43 +0800

    formatted

--- Cplus/NthMagicalNumber.cpp
+++ Cplus/NthMagicalNumber.cpp
@@ -1,27 +1,31 @@
-#include<algorithm>
+#include <algorithm>
 using namespace std;
 
-class Solution {
-	static const int MOD = 1e9+7;
+class Solution
+{
 public:
-    int nthMagicalNumber(int N, int A, int B) {
-		long long lo=0,hi=1e15,mi;
-		int L=A*B/gcd(A,B);
-		while(lo<hi)
-		{
-			mi=(hi-lo)/2+lo;
-			if(mi/A+mi/B-mi/L<N)
-				lo=mi+1;
-			else
-				hi=mi;
-		}
-		return lo%MOD;
-    }
-
-	int gcd(int x,int y)
+	int nthMagicalNumber(int N, int A, int B)
 	{
-		if(x==0)
-			return y;
-		return gcd(y%x,x);
+		long long lo = 0, hi = 1e15;
+		int L = A * B / gcd(A, B);
+		while (lo < hi)
+		{
+			long long mi = (hi - lo) / 2 + lo;
+			if (mi / A + mi / B - mi / L < N)
+				lo = mi + 1;
+			else
+				hi = mi;
+		}
+		return lo % MOD;
 	}
+
+	int gcd(int x, int y)
+	{
+		if (x == 0)
+			return y;
+		return gcd(y % x, x);
+	}
+
+private:
+	static const int MOD = 1e9 + 7;
 };
