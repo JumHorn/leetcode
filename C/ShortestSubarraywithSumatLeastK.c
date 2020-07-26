ee8a8894552c0dc91c400cb33eafd5d537b76924
JumHorn <JumHorn@gmail.com>
2020-07-26 09:59:20 +0800

    Shortest Subarray with Sum at Least K beats 40.00%

---
+++ C/ShortestSubarraywithSumatLeastK.c
@@ -0,0 +1,27 @@
+#include <limits.h>
+
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+
+int shortestSubarray(int *A, int ASize, int K)
+{
+	int res = INT_MAX, prefixsum[ASize + 1];
+	int N = ASize + 1, deque[N], front = 0, rear = 0;
+	prefixsum[0] = 0;
+	for (int i = 0; i < ASize; i++)
+		prefixsum[i + 1] = prefixsum[i] + A[i];
+	deque[rear] = 0;
+	rear = (rear + N - 1) % N;
+	for (int i = 1; i <= ASize; i++)
+	{
+		while (front != rear && prefixsum[i] - K >= prefixsum[deque[front]])
+		{
+			res = min(res, i - deque[front]);
+			front = (front + N - 1) % N;
+		}
+		while (front != rear && prefixsum[deque[(rear + 1) % N]] >= prefixsum[i])
+			rear = (rear + 1) % N;
+		deque[rear] = i;
+		rear = (rear + N - 1) % N;
+	}
+	return res == INT_MAX ? -1 : res;
+}
