05cacf7c5046304acb38ca44ac7199ff44b6836b
JumHorn <JumHorn@gmail.com>
2020-07-29 07:17:35 +0800

    formatted

--- Cplus/SumofSubsequenceWidths.cpp
+++ Cplus/SumofSubsequenceWidths.cpp
@@ -1,20 +1,24 @@
-#include<vector>
-#include<algorithm>
-#include<cmath>
+#include <vector>
+#include <algorithm>
+#include <cmath>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int sumSubseqWidths(vector<int>& A) {
-		int res=0,MOD=1e9+7,N=A.size();
-        sort(A.begin(),A.end());
-		long *pow2=new long[A.size()];
-		pow2[0]=1;
-		for(int i=1;i<N;i++)
-			pow2[i]=pow2[i-1]*2%MOD;
-		for(int i=0;i<N;i++)
-			res=(res+(pow2[i]-pow2[N-i-1])*A[i])%MOD;
+	int sumSubseqWidths(vector<int> &A)
+	{
+		int res = 0, N = A.size();
+		sort(A.begin(), A.end());
+		vector<long> pow2(N);
+		pow2[0] = 1;
+		for (int i = 1; i < N; i++)
+			pow2[i] = pow2[i - 1] * 2 % MOD;
+		for (int i = 0; i < N; i++)
+			res = (res + (pow2[i] - pow2[N - i - 1]) * A[i]) % MOD;
 		return res;
-    }
-};
+	}
 
+private:
+	static const int MOD = 1e9 + 7;
+};
