e1b9315f5ce165940705c2a182f69fc043c298f4
JumHorn <JumHorn@gmail.com>
2020-07-31 07:33:33 +0800

    formatted

--- Cplus/NumbersAtMostNGivenDigitSet.cpp
+++ Cplus/NumbersAtMostNGivenDigitSet.cpp
@@ -1,48 +1,41 @@
-#include<vector>
-#include<string>
+#include <vector>
+#include <string>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int atMostNGivenDigitSet(vector<string>& D, int N) {
-		int res=0,n=D.size();
-    	vector<char> v(n);
-		for(int i=0;i<n;i++)
-			v[i]=D[i][0];
-		string tmp=to_string(N);
-		int len=tmp.length();
-		for(int i=1;i<len;i++)
-			res+=repeatA(i,n);
-		for(int i=0;i<len;i++)
-		{
-			int j=0;
-			while(j<n&&v[j]<tmp[i])
-				j++;
-			res+=j*repeatA(len-i-1,n);
-			if(j>=n||v[j]!=tmp[i])
-				break;
-            if(v[j]==tmp[i]&&i==len-1)
-                res++;
-		}
-		return res;
-    }
-	
-	int repeatA(int m,int n)
+	int atMostNGivenDigitSet(vector<string> &D, int N)
 	{
-		if(m==0)
-			return 1;
-        int res=1;
-		for(int i=0;i<m;i++)
-			res*=n;
+		int res = 0, n = D.size();
+		vector<char> v(n);
+		for (int i = 0; i < n; i++)
+			v[i] = D[i][0];
+		string str = to_string(N);
+		int len = str.length();
+		for (int i = 1; i < len; i++)
+			res += repeatA(i, n);
+		for (int i = 0; i < len; i++)
+		{
+			int j = 0;
+			while (j < n && v[j] < str[i])
+				++j;
+			res += j * repeatA(len - i - 1, n);
+			if (j >= n || v[j] != str[i])
+				break;
+			if (i == len - 1)
+				++res;
+		}
 		return res;
 	}
 
-	int A(int m,int n)
+	int repeatA(int m, int n) //repeated Arrangement
 	{
-		if(m==0)
+		if (m == 0)
 			return 1;
-		if(m==1)
-			return n;
-		return (n-m+1)*A(m-1,n);
+		int res = 1;
+		for (int i = 0; i < m; i++)
+			res *= n;
+		return res;
 	}
 };
