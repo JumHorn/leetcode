e955295f638eec552dcb43f1d58d70394d1714ee
JumHorn <JumHorn@gmail.com>
2020-08-04 06:41:53 +0800

    formatted

--- Cplus/NumberofMusicPlaylists.cpp
+++ Cplus/NumberofMusicPlaylists.cpp
@@ -1,20 +1,33 @@
-#include<cmath>
-#include<vector>
+#include <cmath>
+#include <vector>
 using namespace std;
 
-class Solution {
-	static const int mod=1e9+7;
+/*
+Let dp[i][j] be the number of playlists of length i that have exactly j unique songs
+Consider dp[i][j]. Last song, we either played a song for the first time or we didn't.
+If we did, then we had dp[i-1][j-1] * (N-j) ways to choose it.
+If we didn't, then we repeated a previous song in dp[i-1][j] * max(j-K, 0) ways
+*/
+
+class Solution
+{
 public:
-    int numMusicPlaylists(int N, int L, int K) {
-		vector<vector<long> > dp(L+1,vector<long>(N+1));
-		dp[0][0]=1;
-		for(int i=1;i<=L;i++)
-			for(int j=1;j<=N;j++)
+	int numMusicPlaylists(int N, int L, int K)
+	{
+		vector<vector<long>> dp(L + 1, vector<long>(N + 1));
+		dp[0][0] = 1;
+		for (int i = 0; i < L; i++)
+		{
+			for (int j = 0; j < N; j++)
 			{
-				dp[i][j]=dp[i-1][j-1]*(N-(j-1))%mod;
-				if(j>K)
-					dp[i][j]=(dp[i][j]+dp[i-1][j]*(j-K)%mod)%mod;
+				dp[i + 1][j + 1] = dp[i][j] * (N - j) % MOD;
+				if (j >= K)
+					dp[i + 1][j + 1] = (dp[i + 1][j + 1] + dp[i][j + 1] * (j + 1 - K) % MOD) % MOD;
 			}
-		return dp.back().back();
-    }
+		}
+		return dp[L][N];
+	}
+
+private:
+	static const int MOD = 1e9 + 7;
 };
