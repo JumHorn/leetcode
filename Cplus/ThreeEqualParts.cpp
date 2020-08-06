483ef39c5ba598e7db19754d28e27a8b570bf655
JumHorn <JumHorn@gmail.com>
2020-08-06 08:16:30 +0800

    formatted

--- Cplus/ThreeEqualParts.cpp
+++ Cplus/ThreeEqualParts.cpp
@@ -1,44 +1,44 @@
-#include<vector>
-#include<algorithm>
-#include<numeric>
+#include <algorithm>
+#include <numeric>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> threeEqualParts(vector<int>& A) {
-		int sum=accumulate(A.begin(),A.end(),0); 
-		if(sum%3!=0)
-			return {-1,-1};
-		sum/=3;
-		if(sum==0)
-			return {0,2};
-		int i=0,j=0,k=0,ones=0;
-		while(A[k]!=1)
-			k++;
-		i=k;
-		ones=0;
-		while(ones!=sum)
-			if(A[++k]==1)
-				ones++;
-		j=k;
-		ones=0;
-		while(ones!=sum)
-			if(A[++k]==1)
-				ones++;
-		while(k<(int)A.size())
+	vector<int> threeEqualParts(vector<int> &A)
+	{
+		int sum = accumulate(A.begin(), A.end(), 0);
+		if (sum % 3 != 0)
+			return {-1, -1};
+		sum /= 3;
+		if (sum == 0)
+			return {0, 2};
+		int i = 0, j = 0, k = 0, ones = 0;
+		while (A[k] != 1)
+			++k;
+		i = k;
+		ones = 0;
+		while (ones != sum)
 		{
-			if(A[i]==1)
-				sum--;
-			if(A[i]==A[j]&&A[i]==A[k])
-			{
-				i++;
-				j++;
-				k++;
-			}
-			else
-				return {-1,-1};
+			if (A[++k] == 1)
+				++ones;
 		}
-		return {i-1,j};
-    }
+		j = k;
+		ones = 0;
+		while (ones != sum)
+		{
+			if (A[++k] == 1)
+				++ones;
+		}
+		while (k < (int)A.size())
+		{
+			if (A[i] != A[j] || A[i] != A[k])
+				return {-1, -1};
+			++i;
+			++j;
+			++k;
+		}
+		return {i - 1, j};
+	}
 };
-
