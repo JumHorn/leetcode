334f71901afa27935695b54486f6a97b13bfcc2a
JumHorn <JumHorn@gmail.com>
2020-08-07 06:43:41 +0800

    formatted

--- Cplus/DistinctSubsequencesII.cpp
+++ Cplus/DistinctSubsequencesII.cpp
@@ -1,24 +1,31 @@
-#include<string>
-#include<cstring>
-#include<vector>
+#include <cstring>
+#include <string>
+#include <vector>
 using namespace std;
 
-class Solution {
-	static const int mod=1e9+7;
+class Solution
+{
 public:
-    int distinctSubseqII(string S) {
-		vector<int> dp(S.length()+1);
-		dp[0]=1;
-		for(int i=1;i<=(int)S.length();i++)
+	int distinctSubseqII(string S)
+	{
+		int N = S.length();
+		vector<int> dp(N + 1);
+		dp[0] = 1;
+		for (int i = 0; i < N; ++i)
 		{
-			dp[i]=(dp[i-1]*2)%mod;
-			for(int j=i-2;j>=0;j--)
-                if(S[j]==S[i-1])
-                {
-				    dp[i]=(mod+dp[i]-dp[j])%mod;
-                    break;
-                }
+			dp[i + 1] = (dp[i] * 2) % MOD;
+			for (int j = i - 1; j >= 0; --j)
+			{
+				if (S[j] == S[i])
+				{
+					dp[i + 1] = (MOD + dp[i + 1] - dp[j]) % MOD;
+					break;
+				}
+			}
 		}
-		return dp.back()-1;
-    }
+		return dp.back() - 1;
+	}
+
+private:
+	static const int MOD = 1e9 + 7;
 };
