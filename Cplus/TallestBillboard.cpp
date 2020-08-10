e8ce9e4231689b985b0a485e04bdba25b710cf3b
JumHorn <JumHorn@gmail.com>
2020-08-10 08:05:36 +0800

    formatted

--- Cplus/TallestBillboard.cpp
+++ Cplus/TallestBillboard.cpp
@@ -1,26 +1,26 @@
-#include<vector>
-#include<unordered_map>
-#include<cmath>
-#include<algorithm>
+#include <algorithm>
+#include <cmath>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int tallestBillboard(vector<int>& rods) {
-		unordered_map<int,int> dp;
-		dp[0]=0;
-		for(int i=0;i<(int)rods.size();i++)
+	int tallestBillboard(vector<int> &rods)
+	{
+		unordered_map<int, int> dp; //{diff,height}
+		dp[0] = 0;
+		for (auto r : rods)
 		{
-			unordered_map<int,int> tmp(dp);
-			for(auto& n:tmp)
+			unordered_map<int, int> tmp(dp);
+			for (auto &n : tmp)
 			{
-				dp[n.first+rods[i]]=max(dp[n.first+rods[i]],tmp[n.first]);
-				if(rods[i]>=n.first)
-					dp[abs(n.first-rods[i])]=max(dp[abs(n.first-rods[i])],tmp[n.first]+n.first);
-				else
-					dp[abs(n.first-rods[i])]=max(dp[abs(n.first-rods[i])],tmp[n.first]+rods[i]);
+				dp[n.first + r] = max(dp[n.first + r], tmp[n.first]); //add to higher side
+				int delta = r >= n.first ? n.first : r;
+				dp[abs(n.first - r)] = max(dp[abs(n.first - r)], tmp[n.first] + delta); //add to lower side
 			}
 		}
 		return dp[0];
-    }
+	}
 };
