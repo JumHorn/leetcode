2a888e2b827ed75a06dc8f933b6d24264abd7212
JumHorn <JumHorn@gmail.com>
2020-08-11 08:30:00 +0800

    Odd Even Jump beats 88.82%

--- Cplus/OddEvenJump.cpp
+++ Cplus/OddEvenJump.cpp
@@ -1,84 +1,33 @@
-#include<vector>
-#include<algorithm>
-#include<set>
+#include <algorithm>
+#include <map>
+#include <vector>
 using namespace std;
 
-class Comp
+/*
+题目意思是先oddjump再evenjump一直循环看是否能到达终点
+*/
+
+class Solution
 {
 public:
-    bool operator()(const pair<int,int>& lhs,const pair<int,int>& rhs) const
-    {
-        if(lhs.first!=rhs.first)
-            return lhs.first<rhs.first;
-        return lhs.second<rhs.second;
-    }
-};
-
-class Solution {
-    set<pair<int,int>,Comp> s;
-public:
-    int oddEvenJumps(vector<int>& A) {
-        s.clear();
-		vector<vector<int>> dp(A.size(),vector<int>(2));        
-		dp.back()[0]=1;
-		dp.back()[1]=1;
-        s.insert({A.back(),A.size()-1});
-		for(int i=A.size()-2;i>=0;i--)
-        {
-            dp[i][0]=jumps(A,i,0,dp);
-            dp[i][1]=jumps(A,i,1,dp);
-            s.insert({A[i],i});
-        }
-        int res=0;
-		for(int i=0;i<(int)dp.size();i++)
-            if(dp[i][1]==1)
-                res++;
-        return res;
-    }
-
-	int jumps(vector<int>& A,int i,int oddeven,vector<vector<int>>& dp)
+	int oddEvenJumps(vector<int> &A)
 	{
-		if(i==-1)
-			return -1;
-        if(dp[i][oddeven]!=0)
-            return dp[i][oddeven];
-        int res;
-		if(oddeven==0)//even
-			res=jumps(A,getEven(A,i),(oddeven+1)&1,dp);
-		else
-			res=jumps(A,getOdd(A,i),(oddeven+1)&1,dp);
+		int n = A.size(), res = 1;
+		vector<int> odd(n), even(n);
+		odd[n - 1] = even[n - 1] = 1;
+		map<int, int> m;
+		m[A[n - 1]] = n - 1;
+		for (int i = n - 2; i >= 0; --i)
+		{
+			auto hi = m.lower_bound(A[i]), lo = m.upper_bound(A[i]);
+			if (hi != m.end())
+				odd[i] = even[hi->second];
+			if (lo != m.begin())
+				even[i] = odd[(--lo)->second];
+			if (odd[i])
+				++res;
+			m[A[i]] = i;
+		}
 		return res;
-	}
-
-	int getOdd(vector<int>& A,int i)
-	{
-        auto iter=s.lower_bound({A[i],i});
-        if(iter==s.end())
-            return -1;
-		return iter->second;
-	}
-
-	int getEven(vector<int>& A,int i)
-	{
-        auto iter=s.lower_bound({A[i],i});
-        if(iter==s.end())
-            --iter;
-        while(iter!=s.begin())
-        {
-            if(iter->first<=A[i])
-            {
-                int val=iter->first;
-                --iter;
-                while(iter!=s.begin()&&val==iter->first)
-                    --iter;
-                if(iter->first!=val)
-                    return (++iter)->second;
-                return iter->second;
-            }
-            --iter;
-        }
-        if(iter->first<=A[i])
-            return iter->second;
-        return -1;
 	}
 };
