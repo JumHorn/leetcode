7e25e418a287bbbf43bb0cfa5e124836db7c9887
JumHorn <JumHorn@gmail.com>
2020-08-12 06:28:45 +0800

    formatted

--- Cplus/TripleswithBitwiseANDEqualToZero.cpp
+++ Cplus/TripleswithBitwiseANDEqualToZero.cpp
@@ -1,19 +1,28 @@
-#include<vector>
-#include<unordered_map>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int countTriplets(vector<int>& A) {
-		unordered_map<int,int> m;
- 		for(int i=0;i<(int)A.size();i++)
-			for(int j=0;j<(int)A.size();j++)
-				++m[A[i]&A[j]];	
-		int res=0;
-		for(int i=0;i<(int)A.size();i++)
-            for(auto n : m)
-                if((A[i]&n.first)==0)
-			        res+=n.second;
+	int countTriplets(vector<int> &A)
+	{
+		int N = A.size();
+		unordered_map<int, int> m;
+		for (int i = 0; i < N; i++)
+		{
+			for (int j = 0; j < N; j++)
+				++m[A[i] & A[j]];
+		}
+		int res = 0;
+		for (int i = 0; i < N; i++)
+		{
+			for (auto n : m)
+			{
+				if ((A[i] & n.first) == 0)
+					res += n.second;
+			}
+		}
 		return res;
-    }
+	}
 };
