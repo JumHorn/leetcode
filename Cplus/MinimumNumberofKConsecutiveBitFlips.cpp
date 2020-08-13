8286c8766d7ec098b625cc052ffb42457d895f20
JumHorn <JumHorn@gmail.com>
2020-08-13 16:01:44 +0800

    formatted

--- Cplus/MinimumNumberofKConsecutiveBitFlips.cpp
+++ Cplus/MinimumNumberofKConsecutiveBitFlips.cpp
@@ -1,27 +1,29 @@
-#include<vector>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int minKBitFlips(vector<int>& A, int K) {
-		int res=0,flip=0;
-        vector<int> dp(A.size()+1);
-		for(int i=0;i<=(int)A.size()-K;i++)
+	int minKBitFlips(vector<int> &A, int K)
+	{
+		int res = 0, flip = 0;
+		vector<int> dp(A.size() + 1);
+		for (int i = 0; i <= (int)A.size() - K; i++)
 		{
-            flip+=dp[i];
-			if((A[i]+flip%2)%2==0)
+			flip += dp[i];
+			if ((A[i] + flip) % 2 == 0)
 			{
-                flip++;
-                dp[i+K]--;
-                res++;
+				++flip;
+				++dp[i + K];
+				++res;
 			}
 		}
-		for(int i=A.size()-K+1;i<(int)A.size();i++)
-        {
-            flip+=dp[i];
-			if((A[i]+flip%2)%2==0)
+		for (int i = A.size() - K + 1; i < (int)A.size(); i++)
+		{
+			flip += dp[i];
+			if ((A[i] + flip) % 2 == 0)
 				return -1;
-        }
-		return res;	
-    }
+		}
+		return res;
+	}
 };
