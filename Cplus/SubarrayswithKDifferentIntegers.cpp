7444b2290952dad16907534e87ee44124a67d505
JumHorn <JumHorn@gmail.com>
2020-08-13 10:14:17 +0800

    formatted

--- Cplus/SubarrayswithKDifferentIntegers.cpp
+++ Cplus/SubarrayswithKDifferentIntegers.cpp
@@ -1,31 +1,33 @@
-#include<vector>
-#include<unordered_map>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int subarraysWithKDistinct(vector<int>& A, int K) {
-		int res=0;
-		unordered_map<int,int> wk,wk_1;
-		for(int i=0,j=0,j_1=0;i<(int)A.size();i++)
+	int subarraysWithKDistinct(vector<int> &A, int K)
+	{
+		int res = 0;
+		unordered_map<int, int> wk, wk_1; //window k and window k-1
+		for (int i = 0, j = 0, j_1 = 0; i < (int)A.size(); i++)
 		{
-            ++wk[A[i]];
-            ++wk_1[A[i]];
-            if(wk_1.size()==K)
-            {
-                while(--wk_1[A[j_1]]!=0)
-                    j_1++;
-                wk_1.erase(A[j_1++]);
-            }
-            if(wk.size()>K)
-            {
-                while(--wk[A[j]]!=0)
-                    j++;
-                wk.erase(A[j++]);
-            }
-            if(wk.size()==K)
-                res+=j_1-j;
+			++wk[A[i]];
+			++wk_1[A[i]];
+			if (wk_1.size() == K)
+			{
+				while (--wk_1[A[j_1]] != 0)
+					j_1++;
+				wk_1.erase(A[j_1++]);
+			}
+			if (wk.size() > K)
+			{
+				while (--wk[A[j]] != 0)
+					j++;
+				wk.erase(A[j++]);
+			}
+			if (wk.size() == K)
+				res += j_1 - j;
 		}
-		return res;		
-    }
+		return res;
+	}
 };
