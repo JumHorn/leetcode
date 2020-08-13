2eeab05b66fa2db457c13b19b2cd9fbcb5b4bac0
JumHorn <JumHorn@gmail.com>
2020-08-13 15:55:46 +0800

    Minimum Number of K Consecutive Bit Flips beats 100%

---
+++ C/MinimumNumberofKConsecutiveBitFlips.c
@@ -0,0 +1,21 @@
+#include <string.h>
+
+int minKBitFlips(int *A, int ASize, int K)
+{
+	int flip[ASize + 1], flag = 0, res = 0;
+	memset(flip, 0, sizeof(flip));
+	for (int i = 0; i < ASize; ++i)
+	{
+		if (flip[i] == 1)
+			flag = 1 - flag;
+		if ((flag == 1 && A[i] == 1) || (flag == 0 && A[i] == 0))
+		{
+			++res;
+			flag = 1 - flag;
+			if (i + K > ASize)
+				return -1;
+			flip[i + K] = 1 - flip[i + K];
+		}
+	}
+	return res;
+}
