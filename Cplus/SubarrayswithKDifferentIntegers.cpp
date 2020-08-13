d66387cb1ea1d8ecbff8ffc4e16433e7bbfe41eb
JumHorn <JumHorn@gmail.com>
2020-08-13 10:35:57 +0800

    Subarrays with K Different Integers beats 68.85%

--- Cplus/SubarrayswithKDifferentIntegers.cpp
+++ Cplus/SubarrayswithKDifferentIntegers.cpp
@@ -7,26 +7,25 @@ class Solution
 public:
 	int subarraysWithKDistinct(vector<int> &A, int K)
 	{
+		return atMostK(A, K) - atMostK(A, K - 1);
+	}
+
+	int atMostK(vector<int> &A, int K)
+	{
+		unordered_map<int, int> window;
 		int res = 0;
-		unordered_map<int, int> wk, wk_1; //window k and window k-1
-		for (int i = 0, j = 0, j_1 = 0; i < (int)A.size(); i++)
+		for (int i = 0, j = 0; i < (int)A.size(); ++i)
 		{
-			++wk[A[i]];
-			++wk_1[A[i]];
-			if (wk_1.size() == K)
+			if (window[A[i]] == 0)
+				--K;
+			++window[A[i]];
+			while (K < 0)
 			{
-				while (--wk_1[A[j_1]] != 0)
-					j_1++;
-				wk_1.erase(A[j_1++]);
+				if (--window[A[j]] == 0)
+					++K;
+				++j;
 			}
-			if (wk.size() > K)
-			{
-				while (--wk[A[j]] != 0)
-					j++;
-				wk.erase(A[j++]);
-			}
-			if (wk.size() == K)
-				res += j_1 - j;
+			res += i - j + 1;
 		}
 		return res;
 	}
