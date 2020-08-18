5f0ad17e26b1f500a6d52f35d5b55073d4284341
JumHorn <JumHorn@gmail.com>
2020-08-18 08:36:41 +0800

    formatted

--- Cplus/NumbersWithRepeatedDigits.cpp
+++ Cplus/NumbersWithRepeatedDigits.cpp
@@ -1,37 +1,38 @@
-#include<string>
-#include<cmath>
-#include<unordered_set>
+#include <cmath>
+#include <string>
+#include <unordered_set>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int numDupDigitsAtMostN(int N) {
-		string tmp=to_string(N+1);
-		int len=tmp.length(),res=0;
-		for(int i=1;i<len;i++)
-			res+=9*A(i-1,9);
-		unordered_set<char> s;
-        s.insert(tmp[0]);
-        res+=(tmp[0]-'1')*A(len-1,9);
-		for(int i=1;i<len;i++)
-		{
-			for(char c='0';c<tmp[i];c++)
-                if(s.find(c)==s.end())
-				    res+=A(len-i-1,10-i-1);
-			if(s.find(tmp[i])==s.end())
-				s.insert(tmp[i]);
-			else
-				break;
-		}
-		return N-res;
-    }
-
-	int A(int m,int n)
+	int numDupDigitsAtMostN(int N)
 	{
-		if(m==0)
+		string num = to_string(N + 1);
+		int len = num.length(), res = 0;
+		for (int i = 1; i < len; i++)
+			res += 9 * A(i - 1, 9);
+		unordered_set<char> s;
+		s.insert(num[0]);
+		res += (num[0] - '1') * A(len - 1, 9);
+		for (int i = 1; i < len; i++)
+		{
+			for (char c = '0'; c < num[i]; c++)
+				if (s.find(c) == s.end())
+					res += A(len - i - 1, 10 - i - 1);
+			if (s.find(num[i]) != s.end())
+				break;
+			s.insert(num[i]);
+		}
+		return N - res;
+	}
+
+	int A(int m, int n) //arrangment
+	{
+		if (m == 0)
 			return 1;
-		if(m==1)
+		if (m == 1)
 			return n;
-		return (n-m+1)*A(m-1,n);
+		return (n - m + 1) * A(m - 1, n);
 	}
 };
