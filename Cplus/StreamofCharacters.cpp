8bbbba99ce6e33638cb66dd4643762125562d570
JumHorn <JumHorn@gmail.com>
2020-08-18 09:55:45 +0800

    formatted

--- Cplus/StreamofCharacters.cpp
+++ Cplus/StreamofCharacters.cpp
@@ -1,56 +1,63 @@
-#include<vector>
-#include<string>
+#include <string>
+#include <vector>
 using namespace std;
 
-class StreamChecker {
+class StreamChecker
+{
+private:
 	struct TreeNode
 	{
 		int count;
-		TreeNode* node[26];
-		TreeNode():count(0),node({NULL}){}
+		TreeNode *node[26];
+		TreeNode() : count(0)
+		{
+			for (int i = 0; i < 26; ++i)
+				node[i] = NULL;
+		}
 	};
 
-	TreeNode* root;
+	TreeNode *root;
+	vector<char> letters;
 
-	void insert(const string& str)
+	void insert(const string &str)
 	{
-		TreeNode* tmp=root;
-		for(int i=str.length()-1;i>=0;i--)
+		TreeNode *tmp = root;
+		for (int i = str.length() - 1; i >= 0; i--)
 		{
-            if(tmp->node[str[i]-'a']==NULL)
-			    tmp->node[str[i]-'a']=new TreeNode();
-			tmp=tmp->node[str[i]-'a'];
+			if (tmp->node[str[i] - 'a'] == NULL)
+				tmp->node[str[i] - 'a'] = new TreeNode();
+			tmp = tmp->node[str[i] - 'a'];
 		}
 		++tmp->count;
 	}
 
 	bool search()
 	{
-		TreeNode* tmp=root;
-		for(int i=v.size()-1;i>=0;i--)
+		TreeNode *tmp = root;
+		for (int i = letters.size() - 1; i >= 0; i--)
 		{
-			tmp=tmp->node[v[i]-'a'];
-			if(tmp==NULL)
+			tmp = tmp->node[letters[i] - 'a'];
+			if (tmp == NULL)
 				return false;
-			if(tmp->count>0)
+			if (tmp->count > 0)
 				return true;
 		}
 		return false;
 	}
-	
-	vector<char> v;
+
 public:
-    StreamChecker(vector<string>& words) {
-		v.clear();
-        root=new TreeNode();
-		for(int i=0;i<(int)words.size();i++)
+	StreamChecker(vector<string> &words)
+	{
+		root = new TreeNode();
+		for (int i = 0; i < (int)words.size(); i++)
 			insert(words[i]);
-    }
-    
-    bool query(char letter) {
-        v.push_back(letter);
+	}
+
+	bool query(char letter)
+	{
+		letters.push_back(letter);
 		return search();
-    }
+	}
 };
 
 /**
