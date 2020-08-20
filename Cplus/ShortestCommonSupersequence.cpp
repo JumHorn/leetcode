17085f2463e13de6bf8916fe3df30ed4df896014
JumHorn <JumHorn@gmail.com>
2020-08-21 07:59:57 +0800

    Shortest Common Supersequence beats 45.70%

--- Cplus/ShortestCommonSupersequence.cpp
+++ Cplus/ShortestCommonSupersequence.cpp
@@ -1,37 +1,39 @@
-#include<string>
-#include<vector>
+#include <string>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    string shortestCommonSupersequence(string str1, string str2) {
-		int M=str1.length(),N=str2.length();
-		vector<vector<string>> dp(M+1,vector<string>(N+1));
-		for(int i=1;i<=M;i++)
-			for(int j=1;j<=N;j++)
-			{
-				if(str1[i-1]==str2[j-1])
-					dp[i][j]=dp[i-1][j-1]+str1[i-1];
-				else
-					dp[i][j]=dp[i-1][j].length()>dp[i][j-1].length()?dp[i-1][j]:dp[i][j-1];
-			}
-		string lcs(dp[M][N]);
-		string res(M+N-lcs.length(),' ');
-		int i=0,j=0,k=0,s=0;
-		while(s<(int)lcs.length())
+	string shortestCommonSupersequence(string str1, string str2)
+	{
+		int M = str1.length(), N = str2.length();
+		vector<vector<int>> dp(M + 1, vector<int>(N + 1));
+		for (int i = 0; i < M; i++)
 		{
-			while(str1[i]!=lcs[s])
-				res[k++]=str1[i++];
-			while(str2[j]!=lcs[s])
-				res[k++]=str2[j++];
-			res[k++]=lcs[s++];
-			i++;
-			j++;
+			for (int j = 0; j < N; j++)
+			{
+				if (str1[i] == str2[j])
+					dp[i + 1][j + 1] = dp[i][j] + 1;
+				else
+					dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j]);
+			}
 		}
-		while(i<M)
-			res[k++]=str1[i++];
-		while(j<N)
-			res[k++]=str2[j++];
+		string res(M + N - dp[M][N], ' ');
+		int i = M - 1, j = N - 1, k = res.length() - 1;
+		while (i >= 0 || j >= 0)
+		{
+			if (i < 0 || j < 0)
+				res[k--] = i < 0 ? str2[j--] : str1[i--];
+			else if (str1[i] == str2[j])
+			{
+				res[k--] = str1[i];
+				--i;
+				--j;
+			}
+			else
+				res[k--] = dp[i][j + 1] > dp[i + 1][j] ? str1[i--] : str2[j--];
+		}
 		return res;
-    }
+	}
 };
