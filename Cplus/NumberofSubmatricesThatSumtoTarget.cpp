954b19d4401959b916b88d00a9e7aae1f3f94ff8
JumHorn <JumHorn@gmail.com>
2020-08-20 22:36:01 +0800

    formatted

--- Cplus/NumberofSubmatricesThatSumtoTarget.cpp
+++ Cplus/NumberofSubmatricesThatSumtoTarget.cpp
@@ -1,38 +1,33 @@
-#include<vector>
-#include<unordered_map>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 
-class Solution {
-public:
-    int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
-		int res=0,M=matrix.size(),N=matrix[0].size();      
-		for(int i=0;i<M;i++)
-			for(int j=1;j<N;j++)
-				matrix[i][j]+=matrix[i][j-1];
-		for(int i=0;i<N;i++)
-			for(int j=i;j<N;j++)
-			{
-                unordered_map<int,int> m;
-                m[0]=1;
-                int add=0;
-				for(int k=0;k<M;k++)
-                {
-                    add+=matrix[k][j]-(i>0?matrix[k][i-1]:0);
-                    res+=m[add-target];
-                    m[add]++;
-                }
-			}
-		return res;
-    }
-};
-
-int main()
+class Solution
 {
-    vector<vector<int>> v=
-    {
-        {{0,1,0},{1,1,1},{0,1,0}}
-    };
-    Solution sol;
-    sol.numSubmatrixSumTarget(v,0);
-    return 0;
-}
+public:
+	int numSubmatrixSumTarget(vector<vector<int>> &matrix, int target)
+	{
+		int res = 0, M = matrix.size(), N = matrix[0].size();
+		for (int i = 0; i < M; i++)
+		{
+			for (int j = 1; j < N; j++)
+				matrix[i][j] += matrix[i][j - 1];
+		}
+		for (int i = 0; i < N; i++)
+		{
+			for (int j = i; j < N; j++)
+			{
+				unordered_map<int, int> m;
+				m[0] = 1;
+				int add = 0;
+				for (int k = 0; k < M; k++)
+				{
+					add += matrix[k][j] - (i > 0 ? matrix[k][i - 1] : 0);
+					res += m[add - target];
+					++m[add];
+				}
+			}
+		}
+		return res;
+	}
+};
