5bfcb1920882b2b6bf48eac384e71c3b1f7fb1f9
JumHorn <JumHorn@gmail.com>
2020-08-24 07:21:25 +0800

    formatted

--- Cplus/LastSubstringinLexicographicalOrder.cpp
+++ Cplus/LastSubstringinLexicographicalOrder.cpp
@@ -1,27 +1,33 @@
-#include<string>
-#include<algorithm>
+#include <algorithm>
+#include <string>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    string lastSubstring(string s) {
-	    auto iter=max_element(s.begin(),s.end()); 
-		int res=distance(s.begin(),iter);
-		for(int i=res+1;i<(int)s.length();i++)
-			if(s[i]==*iter)
+	string lastSubstring(string s)
+	{
+		int N = s.length();
+		auto iter = max_element(s.begin(), s.end());
+		int res = distance(s.begin(), iter);
+		for (int i = res + 1; i < N; ++i)
+		{
+			if (s[i] == *iter)
 			{
-				int j=res,k=i;
-				while(i<(int)s.length()&&s[++i]==s[++j]);
-				if(i==(int)s.length())
+				int j = res, k = i;
+				while (i < N && s[++i] == s[++j])
+					;
+				if (i == N)
 					break;
-				if(s[i]>s[j])
-                {
-					res=k;
-                    i=k;
-                }
-                else
-				    i--;
+				if (s[i] > s[j])
+				{
+					res = k;
+					i = k;
+				}
+				else
+					--i;
 			}
+		}
 		return s.substr(res);
-    }
+	}
 };
