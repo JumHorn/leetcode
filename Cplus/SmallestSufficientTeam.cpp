3e7c409f0f2f788413a341ee8ed4ef83db325305
JumHorn <JumHorn@gmail.com>
2020-08-23 09:38:44 +0800

    formatted

--- Cplus/SmallestSufficientTeam.cpp
+++ Cplus/SmallestSufficientTeam.cpp
@@ -1,64 +1,66 @@
-#include<vector>
-#include<string>
-#include<unordered_map>
-#include<unordered_set>
-#include<queue>
+#include <queue>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> smallestSufficientTeam(vector<string>& req_skills, vector<vector<string>>& people) {
-        unordered_map<string,int> skills;
-		int allskills=1;
-		for(int i=0;i<(int)req_skills.size();i++)
+	vector<int> smallestSufficientTeam(vector<string> &req_skills, vector<vector<string>> &people)
+	{
+		unordered_map<string, int> skills;
+		int allskills = 0;
+		for (int i = 0; i < (int)req_skills.size(); i++)
 		{
-			skills[req_skills[i]]=(1<<i);
-			allskills|=(1<<i);
+			skills[req_skills[i]] = (1 << i);
+			allskills |= (1 << i);
 		}
 		vector<int> peopleskill(people.size());
-		for(int i=0;i<(int)people.size();i++)
+		for (int i = 0; i < (int)people.size(); i++)
 		{
-			for(int j=0;j<(int)people[i].size();j++)
-				peopleskill[i]|=skills[people[i][j]];
-			if((peopleskill[i]^allskills)==0)
+			for (int j = 0; j < (int)people[i].size(); j++)
+				peopleskill[i] |= skills[people[i][j]];
+			if ((peopleskill[i] ^ allskills) == 0)
 				return {i};
 		}
-		
-		queue<pair<int,unordered_set<int>>> q;
-		unordered_set<int> visited;
-		for(int i=0;i<(int)peopleskill.size();i++)
+
+		queue<pair<int, unordered_set<int>>> q; //{skills, people's index}
+		unordered_set<int> seen;
+		for (int i = 0; i < (int)peopleskill.size(); i++)
 		{
-			if(visited.find(peopleskill[i])==visited.end())
+			if (seen.find(peopleskill[i]) == seen.end())
 			{
-				q.push({peopleskill[i],{i}});
-				visited.insert(peopleskill[i]);
+				q.push({peopleskill[i], {i}});
+				seen.insert(peopleskill[i]);
 			}
 		}
-		while(!q.empty())
+		while (!q.empty())
 		{
-			int size=q.size();
-			while(--size>=0)
+			int size = q.size();
+			while (--size >= 0)
 			{
-				for(int i=0;i<(int)peopleskill.size();i++)
+				for (int i = 0; i < (int)peopleskill.size(); i++)
 				{
-					int pskill=q.front().first;
-					pskill|=peopleskill[i];
-					if(visited.find(pskill)==visited.end())
+					int pskill = q.front().first;
+					pskill |= peopleskill[i];
+					if (seen.find(pskill) == seen.end())
 					{
-						if((pskill^allskills)==0)
+						if ((pskill ^ allskills) == 0)
 						{
 							q.front().second.insert(i);
-							return vector<int>(q.front().second.begin(),q.front().second.end());
+							return vector<int>(q.front().second.begin(), q.front().second.end());
 						}
-						visited.insert(pskill);
-						q.push(q.front());
-						q.back().first=pskill;
+						seen.insert(pskill);
+						q.push(q.front()); //append new result to the queue
+						q.back().first = pskill;
 						q.back().second.insert(i);
 					}
 				}
 				q.pop();
 			}
 		}
-		return vector<int>();
-    }
+		return {};
+	}
 };
