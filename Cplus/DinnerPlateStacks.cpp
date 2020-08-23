8bbaceb07cf8fbc5f7615fa2d82b3e5fbefc354e
JumHorn <JumHorn@gmail.com>
2020-08-24 07:55:39 +0800

    formatted

--- Cplus/DinnerPlateStacks.cpp
+++ Cplus/DinnerPlateStacks.cpp
@@ -1,62 +1,69 @@
-#include<vector>
-#include<stack>
+#include <stack>
+#include <vector>
 using namespace std;
 
-class DinnerPlates {
-	vector<stack<int>> data;
-	unsigned int s,c,p,v;
+class DinnerPlates
+{
 public:
-    DinnerPlates(int capacity) {
-		c=capacity;
-		s=0;
-		p=0;
-		v=0;
-    }
-    
-    void push(int val) {
+	DinnerPlates(int capacity)
+	{
+		c = capacity;
+		s = 0; //size
+		p = 0; //push index
+		v = 0; //pop index
+	}
+
+	void push(int val)
+	{
 		++s;
-      	while(p<data.size()&&data[p].size()==c)
-			p++;
-		if(p<data.size())
+		while (p < data.size() && data[p].size() == c)
+			++p;
+		if (p < data.size())
 			data[p].push(val);
 		else
 		{
 			data.emplace_back();
 			data.back().push(val);
 		}
-		if(p>v)
-			v=p;
-    }
-    
-    int pop() {
-		if(s==0)
+		if (p > v)
+			v = p;
+	}
+
+	int pop()
+	{
+		if (s == 0)
 			return -1;
 		--s;
-		int res=-1;
-		while(v>=0&&data[v].empty())
-			v--;
-		res=data[v].top();
+		int res = -1;
+		while (v >= 0 && data[v].empty())
+			--v;
+		res = data[v].top();
 		data[v].pop();
-		if(p>v)
-			p=v;
+		if (p > v)
+			p = v;
 		return res;
-    }
-    
-    int popAtStack(int index) {
-		int res=-1;
-		if(index<(int)data.size())
+	}
+
+	int popAtStack(int index)
+	{
+		int res = -1;
+		if (index < (int)data.size())
 		{
-			if(!data[index].empty())
+			if (!data[index].empty())
 			{
 				--s;
-				res=data[index].top();
+				res = data[index].top();
 				data[index].pop();
-				if((int)p>index)
-					p=index;
+				if (p > index)
+					p = index;
 			}
 		}
 		return res;
-    }
+	}
+
+private:
+	vector<stack<int>> data;
+	int s, c, p, v;
 };
 
 /**
