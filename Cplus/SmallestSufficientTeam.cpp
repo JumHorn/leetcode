a606a878903d1144f8e70aec9d352c8610b87028
JumHorn <JumHorn@gmail.com>
2020-08-23 10:00:36 +0800

    copy map data structure time consuming

--- Cplus/SmallestSufficientTeam.cpp
+++ Cplus/SmallestSufficientTeam.cpp
@@ -10,57 +10,28 @@ class Solution
 public:
 	vector<int> smallestSufficientTeam(vector<string> &req_skills, vector<vector<string>> &people)
 	{
-		unordered_map<string, int> skills;
-		int allskills = 0;
-		for (int i = 0; i < (int)req_skills.size(); i++)
+		int N = req_skills.size();
+		unordered_map<int, vector<int>> res; //{skillmask,people}
+		unordered_map<string, int> skills;   //{skill,bit}
+		for (int i = 0; i < N; ++i)
+			skills[req_skills[i]] = 1 << i;
+		res[0] = {};
+		for (int i = 0; i < (int)people.size(); ++i)
 		{
-			skills[req_skills[i]] = (1 << i);
-			allskills |= (1 << i);
-		}
-		vector<int> peopleskill(people.size());
-		for (int i = 0; i < (int)people.size(); i++)
-		{
-			for (int j = 0; j < (int)people[i].size(); j++)
-				peopleskill[i] |= skills[people[i][j]];
-			if ((peopleskill[i] ^ allskills) == 0)
-				return {i};
-		}
-
-		queue<pair<int, unordered_set<int>>> q; //{skills, people's index}
-		unordered_set<int> seen;
-		for (int i = 0; i < (int)peopleskill.size(); i++)
-		{
-			if (seen.find(peopleskill[i]) == seen.end())
+			int pskill = 0;
+			for (auto &str : people[i])
+				pskill |= skills[str];
+			auto old = res;
+			for (auto &iter : old)
 			{
-				q.push({peopleskill[i], {i}});
-				seen.insert(peopleskill[i]);
-			}
-		}
-		while (!q.empty())
-		{
-			int size = q.size();
-			while (--size >= 0)
-			{
-				for (int i = 0; i < (int)peopleskill.size(); i++)
+				int mask = iter.first | pskill;
+				if (res.find(mask) == res.end() || res[mask].size() > 1 + iter.second.size())
 				{
-					int pskill = q.front().first;
-					pskill |= peopleskill[i];
-					if (seen.find(pskill) == seen.end())
-					{
-						if ((pskill ^ allskills) == 0)
-						{
-							q.front().second.insert(i);
-							return vector<int>(q.front().second.begin(), q.front().second.end());
-						}
-						seen.insert(pskill);
-						q.push(q.front()); //append new result to the queue
-						q.back().first = pskill;
-						q.back().second.insert(i);
-					}
+					res[mask] = iter.second;
+					res[mask].push_back(i);
 				}
-				q.pop();
 			}
 		}
-		return {};
+		return res[(1 << N) - 1];
 	}
 };
