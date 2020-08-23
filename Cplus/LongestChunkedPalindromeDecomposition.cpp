b4927dce4970cb38167a1d3136c4f38390e6b60c
JumHorn <JumHorn@gmail.com>
2020-08-23 10:15:57 +0800

    formatted

--- Cplus/LongestChunkedPalindromeDecomposition.cpp
+++ Cplus/LongestChunkedPalindromeDecomposition.cpp
@@ -1,25 +1,23 @@
-#include<string>
+#include <string>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int longestDecomposition(string text) {
-		return recursive(text,0,text.length());		
-    }
-
-	int recursive(const string& text,int left,int right)
+	int longestDecomposition(string text)
 	{
-		if(left>=right)
+		return recursive(text, 0, text.length());
+	}
+
+	int recursive(const string &text, int left, int right)
+	{
+		if (left >= right)
 			return 0;
-		int res=1;
-		for(int i=right-1;i*2>=left+right;i--)
+		for (int i = right - 1; i * 2 >= left + right; --i)
 		{
-			if(text[left]==text[i]&&text.substr(left,right-i)==text.substr(i,right-i))
-			{
-				res=2+recursive(text,left+right-i,i);
-				break;
-			}
+			if (text[left] == text[i] && text.substr(left, right - i) == text.substr(i, right - i))
+				return 2 + recursive(text, left + right - i, i);
 		}
-		return res;
+		return 1;
 	}
 };
