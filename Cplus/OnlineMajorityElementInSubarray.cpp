7939515e28b8e6327e7b3ea35dca202361998bd6
JumHorn <JumHorn@gmail.com>
2020-08-24 06:35:51 +0800

    formatted

--- Cplus/OnlineMajorityElementInSubarray.cpp
+++ Cplus/OnlineMajorityElementInSubarray.cpp
@@ -1,32 +1,37 @@
-#include<cstdlib>
-#include<vector>
-#include<unordered_map>
-#include<algorithm>
+#include <algorithm>
+#include <cstdlib>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 
-class MajorityChecker {
-	unordered_map<int,vector<int>> hashmap;
-	vector<int> v;
+class MajorityChecker
+{
 public:
-    MajorityChecker(vector<int>& arr):v(arr) {
-        for(int i=0;i<(int)arr.size();i++)
+	MajorityChecker(vector<int> &arr) : v(arr)
+	{
+		for (int i = 0; i < (int)arr.size(); i++)
 			hashmap[arr[i]].push_back(i);
-    }
-    
-    int query(int left, int right, int threshold) {
-		for(int i=0;i<7;i++)
+	}
+
+	int query(int left, int right, int threshold)
+	{
+		for (int i = 0; i < 7; i++)
 		{
-			int tmp=v[left+rand()%(right-left+1)];
-			if(hashmap[tmp].size()>=threshold)
+			int val = v[left + rand() % (right - left + 1)];
+			if (hashmap[val].size() >= threshold)
 			{
-				auto it1=lower_bound(hashmap[tmp].begin(),hashmap[tmp].end(),left);
-				auto it2=upper_bound(hashmap[tmp].begin(),hashmap[tmp].end(),right);
-				if(it2-it1>=threshold)
-					return tmp;
+				auto it1 = lower_bound(hashmap[val].begin(), hashmap[val].end(), left);
+				auto it2 = upper_bound(hashmap[val].begin(), hashmap[val].end(), right);
+				if (it2 - it1 >= threshold)
+					return val;
 			}
 		}
 		return -1;
-    }
+	}
+
+private:
+	unordered_map<int, vector<int>> hashmap; //{value,index}
+	vector<int> v;							 //same as arr
 };
 
 /**
