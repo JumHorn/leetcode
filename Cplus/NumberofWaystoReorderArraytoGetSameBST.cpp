80b46ac69d3062a89dc95b10c82137a01e0e7d16
JumHorn <JumHorn@gmail.com>
2020-08-30 22:23:29 +0800

    Number of Ways to Reorder Array to Get Same BST beats 20.00%

--- Cplus/NumberofWaystoReorderArraytoGetSameBST.cpp
+++ Cplus/NumberofWaystoReorderArraytoGetSameBST.cpp
@@ -4,7 +4,38 @@ using namespace std;
 class Solution
 {
 public:
-	int numOfWays(vector<int>& nums)
+	int numOfWays(vector<int> &nums)
 	{
+		int N = nums.size();
+		cache = vector<vector<int>>(N, vector<int>(N));
+		return recursive(nums) - 1;
 	}
+
+	int recursive(vector<int> &nums)
+	{
+		int N = nums.size();
+		if (N <= 2)
+			return 1;
+		vector<int> left, right;
+		for (int i = 1; i < N; ++i)
+			nums[i] > nums[0] ? right.push_back(nums[i]) : left.push_back(nums[i]);
+		int l = recursive(left), r = recursive(right);
+		long res = combination(N - 1, left.size(), cache);
+		return res * l % MOD * r % MOD;
+	}
+
+	int combination(int n, int m, vector<vector<int>> &cache)
+	{
+		if (m >= n || m == 0)
+			return 1;
+		if (m == 1)
+			return n;
+		if (cache[n][m] != 0)
+			return cache[n][m];
+		return cache[n][m] = (combination(n - 1, m, cache) % MOD + combination(n - 1, m - 1, cache) % MOD) % MOD;
+	}
+
+private:
+	static const int MOD = 1e9 + 7;
+	vector<vector<int>> cache;
 };
