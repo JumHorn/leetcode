2bbe52407d9921dc7b792ef97710d9c166493ab4
JumHorn <JumHorn@gmail.com>
2020-09-03 23:47:09 +0800

    Number of Ways of Cutting a Pizza beats 81.49%

--- Cplus/NumberofWaysofCuttingaPizza.cpp
+++ Cplus/NumberofWaysofCuttingaPizza.cpp
@@ -9,56 +9,37 @@ public:
 	{
 		int m = pizza.size(), n = pizza[0].length();
 		vector<vector<vector<int>>> dp(m + 1, vector<vector<int>>(n + 1, vector<int>(k + 1, -1)));
-		return memdp(pizza, 0, 0, k, dp);
+		vector<vector<int>> count(m + 1, vector<int>(n + 1));
+		for (int i = 0; i < m; ++i)
+		{
+			for (int j = 0; j < n; ++j)
+				count[i + 1][j + 1] = (count[i][j + 1] + count[i + 1][j] - count[i][j] + (pizza[i][j] == 'A' ? 1 : 0));
+		}
+		return memdp(count, 0, 0, k, dp);
 	}
 
-	int memdp(vector<string> &pizza, int m, int n, int k, vector<vector<vector<int>>> &dp)
+	int memdp(vector<vector<int>> &count, int m, int n, int k, vector<vector<vector<int>>> &dp)
 	{
 		if (dp[m][n][k] != -1)
 			return dp[m][n][k];
-		int row = pizza.size(), col = pizza[0].length();
+		int row = count.size() - 1, col = count[0].size() - 1;
 		if (k == 1)
-		{
-			for (int i = m; i < row; i++)
-			{
-				for (int j = n; j < col; j++)
-				{
-					if (pizza[i][j] == 'A')
-						return 1;
-				}
-			}
-			return 0;
-		}
+			return existApple(count, m, n, row, col) ? 1 : 0;
 		int res = 0, i = m, j = n;
-		while (i < row && !rowHasApple(pizza, i, n))
+		while (i < row && !existApple(count, i, n, i + 1, col))
 			++i;
 		for (; i < row; i++)
-			res = (res + memdp(pizza, i + 1, n, k - 1, dp)) % MOD;
-		while (j < col && !colHasApple(pizza, m, j))
+			res = (res + memdp(count, i + 1, n, k - 1, dp)) % MOD;
+		while (j < col && !existApple(count, m, j, row, j + 1))
 			++j;
 		for (; j < col; j++)
-			res = (res + memdp(pizza, m, j + 1, k - 1, dp)) % MOD;
-		return dp[m][n][k] = res % MOD;
+			res = (res + memdp(count, m, j + 1, k - 1, dp)) % MOD;
+		return dp[m][n][k] = res;
 	}
 
-	bool rowHasApple(vector<string> &pizza, int row, int col)
+	bool existApple(vector<vector<int>> &count, int row0, int col0, int row1, int col1)
 	{
-		for (int j = col; j < (int)pizza[0].length(); j++)
-		{
-			if (pizza[row][j] == 'A')
-				return true;
-		}
-		return false;
-	}
-
-	bool colHasApple(vector<string> &pizza, int row, int col)
-	{
-		for (int i = row; i < (int)pizza.size(); i++)
-		{
-			if (pizza[i][col] == 'A')
-				return true;
-		}
-		return false;
+		return count[row1][col1] - count[row0][col1] - count[row1][col0] + count[row0][col0] != 0;
 	}
 
 private:
