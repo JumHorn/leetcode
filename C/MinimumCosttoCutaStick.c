2c5e2c804a4f63906b6be14b7f49e3fd6ec0496a
JumHorn <JumHorn@gmail.com>
2020-09-05 22:23:59 +0800

    Minimum Cost to Cut a Stick beats 33.33%

---
+++ C/MinimumCosttoCutaStick.c
@@ -0,0 +1,34 @@
+#include <limits.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+
+//cmp function don't consider overflow
+int cmp(const void *lhs, const void *rhs)
+{
+	return *(int *)lhs - *(int *)rhs;
+}
+
+int minCost(int n, int *cuts, int cutsSize)
+{
+	int dp[cutsSize + 2][cutsSize + 2];
+	memset(dp, 0, sizeof(dp));
+	int cut[cutsSize + 2];
+	memcpy(&cut[1], cuts, sizeof(int) * cutsSize);
+	cut[0] = 0;
+	cut[cutsSize + 1] = n;
+	qsort(cut, cutsSize + 2, sizeof(int), cmp);
+	for (int i = 0; i < cutsSize; ++i)
+		dp[i + 1][i + 1] = cut[i + 2] - cut[i];
+	for (int l = 1; l < cutsSize; ++l)
+	{
+		for (int i = 1, j = i + l; j <= cutsSize; ++i, ++j)
+		{
+			dp[i][j] = INT_MAX;
+			for (int k = i; k <= j; ++k)
+				dp[i][j] = min(dp[i][j], cut[j + 1] - cut[i - 1] + dp[i][k - 1] + dp[k + 1][j]);
+		}
+	}
+	return dp[1][cutsSize];
+}
