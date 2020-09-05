3d243de91a24c849700aad780b07f1d378008aed
JumHorn <JumHorn@gmail.com>
2020-09-05 20:45:02 +0800

    Get the Maximum Score beats 100%

--- C/GettheMaximumScore.c
+++ C/GettheMaximumScore.c
@@ -5,21 +5,20 @@
 int maxSum(int *nums1, int nums1Size, int *nums2, int nums2Size)
 {
 	static const int MOD = 1e9 + 7;
-	int size = max(nums1Size, nums2Size);
-	long dp[size + 1][2];
-	memset(dp, 0, sizeof(dp));
-	for (int i = nums1Size - 1; i >= 0; --i)
+	long sum1 = 0, sum2 = 0;
+	int i = 0, j = 0;
+	while (i < nums1Size || j < nums2Size)
 	{
-		for (int j = nums2Size - 1; j >= 0; --j)
+		if (i < nums1Size && (j == nums2Size || nums1[i] < nums2[j]))
+			sum1 += nums1[i++];
+		else if (j < nums2Size && (i == nums1Size || nums2[j] < nums1[i]))
+			sum2 += nums2[j++];
+		else
 		{
-			dp[i][0] = max(dp[i][0], dp[i + 1][0] + nums1[i]);
-			if (nums1[i] == nums2[j])
-				dp[i][0] = max(dp[i][0], dp[j + 1][1] + nums1[i]);
-
-			dp[j][1] = max(dp[j][1], dp[j + 1][1] + nums2[j]);
-			if (nums1[i] == nums2[j])
-				dp[j][1] = max(dp[j][1], dp[i + 1][0] + nums2[j]);
+			sum1 = sum2 = max(sum1, sum2) + nums1[i];
+			++i;
+			++j;
 		}
 	}
-	return max(dp[0][0], dp[0][1]) % MOD;
+	return max(sum1, sum2) % MOD;
 }
