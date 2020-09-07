3ab7cd213e13da44ee2106e168e0a503d53d0f48
JumHorn <JumHorn@gmail.com>
2020-09-07 13:17:04 +0800

    three dimension vector much slower than array

--- Cplus/NumberofPathswithMaxScore.cpp
+++ Cplus/NumberofPathswithMaxScore.cpp
@@ -1,3 +1,4 @@
+#include <climits>
 #include <string>
 #include <vector>
 using namespace std;
@@ -7,36 +8,34 @@ class Solution
 public:
 	vector<int> pathsWithMaxScore(vector<string> &board)
 	{
-		int m = board.size(), n = board[0].length();
-		vector<vector<vector<int>>> dp(m + 1, vector<vector<int>>(n + 1, vector<int>(2)));
-		for (int i = 1; i <= m; i++)
-			dp[i][0][0] = INT_MIN;
-		for (int j = 1; j <= n; j++)
-			dp[0][j][0] = INT_MIN;
-		dp[0][0][1] = 1;
-		for (int i = 0; i < m; i++)
-			for (int j = 0; j < n; j++)
+		int M = board.size(), N = board[0].length();
+		vector<vector<vector<int>>> dp(M, vector<vector<int>>(N, vector<int>(2)));
+		dp[M - 1][N - 1][1] = 1;
+		for (int i = M - 1; i >= 0; --i)
+		{
+			for (int j = N - 1; j >= 0; --j)
 			{
-				if (board[i][j] != 'X')
+				if (board[i][j] == 'X')
+					continue;
+				int path[][2] = {{1, 0}, {0, 1}, {1, 1}};
+				for (int k = 0; k < 3; ++k)
 				{
-					int tmp = 0;
-					if (board[i][j] >= '1' && board[i][j] <= '9')
-						tmp = board[i][j] - '0';
-					dp[i + 1][j + 1][0] = max(max(dp[i][j + 1][0], dp[i + 1][j][0]), dp[i][j][0]);
-					if (dp[i + 1][j + 1][0] == dp[i][j + 1][0])
-						dp[i + 1][j + 1][1] = (dp[i + 1][j + 1][1] + dp[i][j + 1][1]) % MOD;
-					if (dp[i + 1][j + 1][0] == dp[i + 1][j][0])
-						dp[i + 1][j + 1][1] = (dp[i + 1][j + 1][1] + dp[i + 1][j][1]) % MOD;
-					if (dp[i + 1][j + 1][0] == dp[i][j][0])
-						dp[i + 1][j + 1][1] = (dp[i + 1][j + 1][1] + dp[i][j][1]) % MOD;
-					dp[i + 1][j + 1][0] += tmp;
+					int dx = i + path[k][0], dy = j + path[k][1];
+					if (dx >= M || dy >= N || board[dx][dy] == 'X')
+						continue;
+					if (dp[dx][dy][0] > dp[i][j][0])
+					{
+						dp[i][j][0] = dp[dx][dy][0];
+						dp[i][j][1] = dp[dx][dy][1];
+					}
+					else if (dp[dx][dy][0] == dp[i][j][0])
+						dp[i][j][1] = (dp[i][j][1] + dp[dx][dy][1]) % MOD;
 				}
-				else
-					dp[i + 1][j + 1][0] = INT_MIN;
+				if (dp[i][j][1] != 0 && board[i][j] >= '0' && board[i][j] <= '9')
+					dp[i][j][0] += board[i][j] - '0';
 			}
-		if (dp[m][n][0] < 0)
-			return {0, 0};
-		return dp[m][n];
+		}
+		return dp[0][0];
 	}
 
 private:
