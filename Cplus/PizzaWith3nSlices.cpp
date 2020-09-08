2bc452d2c531e3be0d7647b14888eede6dfd2382
JumHorn <JumHorn@gmail.com>
2020-09-09 07:58:58 +0800

    Pizza With 3n Slices beats 57.08%

--- Cplus/PizzaWith3nSlices.cpp
+++ Cplus/PizzaWith3nSlices.cpp
@@ -4,21 +4,21 @@ using namespace std;
 class Solution
 {
 public:
-	int maxSizeSlices(vector<int>& slices)
+	int maxSizeSlices(vector<int> &slices)
 	{
-		int n = slices.size() / 3;
-		return max(maxSubsequenceSum(vector<int>(slices.begin(), slices.end() - 1), n), maxSubsequenceSum(vector<int>(slices.begin() + 1, slices.end()), n));
+		int N = slices.size(), n = N / 3;
+		return max(maxSubSlice(slices, n, 0, N - 1), maxSubSlice(slices, n, 1, N));
 	}
 
-	int maxSubsequenceSum(vector<int> v, int n)
+	int maxSubSlice(vector<int> &slice, int n, int first, int last)
 	{
-		int len = v.size();
-		vector<vector<int>> dp(len + 2, vector<int>(n + 1));
-		for (int i = 2; i <= len + 1; i++)
+		int N = last - first;
+		vector<vector<int>> dp(N + 2, vector<int>(n + 1));
+		for (int i = 0, k = first; k < last; ++i, ++k)
 		{
-			for (int j = 1; j <= n; j++)
-				dp[i][j] = max(dp[i - 1][j], v[i - 2] + dp[i - 2][j - 1]);
+			for (int j = 0; j < n; ++j)
+				dp[i + 2][j + 1] = max(dp[i + 1][j + 1], slice[k] + dp[i][j]);
 		}
-		return dp[len + 1][n];
+		return dp[N + 1][n];
 	}
 };
