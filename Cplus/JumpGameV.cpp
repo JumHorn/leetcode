213faba6b6d9e5f160c09584af90914249ebeedd
JumHorn <JumHorn@gmail.com>
2020-09-08 16:02:18 +0800

    Jump Game V beats 95.72%

--- Cplus/JumpGameV.cpp
+++ Cplus/JumpGameV.cpp
@@ -8,49 +8,35 @@ class Solution
 public:
 	int maxJumps(vector<int> &arr, int d)
 	{
-		vector<int> dp(arr.size());
-		for (int i = 0; i < (int)arr.size(); i++)
-			dfs(arr, d, i, dp);
+		int N = arr.size();
+		vector<int> dp(N);
+		for (int i = 0; i < N; ++i)
+			memdp(arr, d, i, dp);
 		int res = 1;
-		for (int i = 0; i < (int)dp.size(); i++)
+		for (int i = 0; i < N; ++i)
 			res = max(res, dp[i]);
 		return res;
 	}
 
-	void dfs(vector<int> &arr, int d, int index, vector<int> &dp)
+	void memdp(vector<int> &arr, int d, int index, vector<int> &dp)
 	{
+		int left = index - 1, right = index + 1, left_max = 0, right_max = 0, N = arr.size();
 		if (dp[index] != 0)
 			return;
-		int left = index - 1, right = index + 1;
-		while (left >= max(0, index - d))
+		while (left >= max(0, index - d) && arr[left] < arr[index])
 		{
-			if (arr[left] >= arr[index])
-				break;
 			if (dp[left] == 0)
-				dfs(arr, d, left, dp);
+				memdp(arr, d, left, dp);
+			left_max = max(left_max, dp[left]);
 			--left;
 		}
-		left++;
-		while (right <= min((int)arr.size() - 1, index + d))
+		while (right <= min(N - 1, index + d) && arr[right] < arr[index])
 		{
-			if (arr[right] >= arr[index])
-				break;
 			if (dp[right] == 0)
-				dfs(arr, d, right, dp);
+				memdp(arr, d, right, dp);
+			right_max = max(right_max, dp[right]);
 			++right;
 		}
-		right--;
-		int i = left, left_n = 0, j = right, right_n = 0;
-		while (i < index)
-		{
-			left_n = max(left_n, dp[i]);
-			i++;
-		}
-		while (j > index)
-		{
-			right_n = max(right_n, dp[j]);
-			j--;
-		}
-		dp[index] = 1 + max(left_n, right_n);
+		dp[index] = 1 + max(left_max, right_max);
 	}
 };
