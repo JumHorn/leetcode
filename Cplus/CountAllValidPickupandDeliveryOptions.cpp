4385527c35e99e41016309d00c8f7bdc486de22d
JumHorn <JumHorn@gmail.com>
2020-09-08 20:35:59 +0800

    add comment

--- Cplus/CountAllValidPickupandDeliveryOptions.cpp
+++ Cplus/CountAllValidPickupandDeliveryOptions.cpp
@@ -1,6 +1,16 @@
 #include <vector>
 using namespace std;
 
+/*
+dp[i]
+this is one of dp[i]'s state
+P...D
+dp[i+1] add two new position into the state
+P...D##
+choose 2 position from 2i positions
+Combination(2i,2)
+*/
+
 class Solution
 {
 public:
@@ -8,7 +18,6 @@ public:
 	{
 		if (n == 1)
 			return 1;
-		static const int MOD = 1e9 + 7;
 		vector<long> dp(n + 1);
 		dp[1] = 1;
 		dp[2] = 6;
@@ -16,4 +25,7 @@ public:
 			dp[i] = (dp[i - 1] * i * (2 * i - 1)) % MOD;
 		return dp[n];
 	}
+
+private:
+	static const int MOD = 1e9 + 7;
 };
