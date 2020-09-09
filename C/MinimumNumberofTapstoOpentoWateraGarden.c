45cdd63c23e1abb5c671ca86816daac75e4d533f
JumHorn <JumHorn@gmail.com>
2020-09-09 12:29:47 +0800

    O(N^2)==>O(N)

--- C/MinimumNumberofTapstoOpentoWateraGarden.c
+++ C/MinimumNumberofTapstoOpentoWateraGarden.c
@@ -1,18 +1,32 @@
 #include <stdlib.h>
+#include <string.h>
+
 #define max(a, b) ((a) > (b) ? (a) : (b))
 #define min(a, b) ((a) < (b) ? (a) : (b))
 
-int minTaps(int n, int* ranges, int rangesSize)
+int minTaps(int n, int *ranges, int rangesSize)
 {
-	int* dp = (int*)malloc((n + 1) * sizeof(int));
-	for (int i = 1; i <= n; i++)
-		dp[i] = n + 2;
-	dp[0] = 0;
-	for (int i = 0; i <= n; i++)
+	int interval[n + 1];
+	memset(interval, 0, sizeof(interval));
+	for (int i = 0; i <= n; ++i)
+		interval[max(0, i - ranges[i])] = max(interval[max(0, i - ranges[i])], i + ranges[i]);
+	int res = 0, maxright = interval[0], maxjump = 0;
+	if (maxright >= n)
+		return 1;
+	for (int i = 0; i <= n; ++i)
 	{
-		int max_left = max(0, i - ranges[i]), max_right = min(n, i + ranges[i]);
-		for (int j = max_left + 1; j <= max_right; j++)
-			dp[j] = min(dp[j], dp[max_left] + 1);
+		if (i > maxright)
+			return -1;
+		maxjump = max(maxjump, interval[i]);
+		if (i == maxright)
+		{
+			++res;
+			if (maxright >= n)
+				return res;
+			maxright = maxjump;
+			if (maxright >= n)
+				return res + 1;
+		}
 	}
-	return dp[n] < n + 2 ? dp[n] : -1;
+	return -1;
 }
--- Cplus/MinimumNumberofTapstoOpentoWateraGarden.cpp
+++ Cplus/MinimumNumberofTapstoOpentoWateraGarden.cpp
@@ -7,36 +7,27 @@ class Solution
 public:
 	int minTaps(int n, vector<int> &ranges)
 	{
-		vector<vector<int>> dp(n + 1, vector<int>(n + 1));
-		int res = memdp(ranges, 0, n, dp);
-		if (res >= 1000000)
-			return -1;
-		return res;
-	}
-
-	int memdp(vector<int> &ranges, int i, int j, vector<vector<int>> &dp)
-	{
-		if (i > j)
-			return 0;
-		if (dp[i][j] != 0)
-			return dp[i][j];
-		int res = 1000000;
-		for (int k = i; k <= j; k++)
+		vector<int> interval(n + 1);
+		for (int i = 0; i <= n; ++i)
+			interval[max(0, i - ranges[i])] = max(interval[max(0, i - ranges[i])], i + ranges[i]);
+		int res = 0, maxright = interval[0], maxjump = 0;
+		if (maxright >= n)
+			return 1;
+		for (int i = 0; i <= n; ++i)
 		{
-			if (ranges[k] != 0)
+			if (i > maxright)
+				return -1;
+			maxjump = max(maxjump, interval[i]);
+			if (i == maxright)
 			{
-				res = min(res, memdp(ranges, i, k - ranges[k] - 1, dp) + memdp(ranges, k + ranges[k] + 1, j, dp) + 1);
+				++res;
+				if (maxright >= n)
+					return res;
+				maxright = maxjump;
+				if (maxright >= n)
+					return res + 1;
 			}
 		}
-		dp[i][j] = res;
-		return res;
+		return -1;
 	}
 };
-
-int main()
-{
-	vector<int> range = {1, 0, 4, 0, 4, 1, 4, 3, 1, 1, 1, 2, 1, 4, 0, 3, 0, 3, 0, 3, 0, 5, 3, 0, 0, 1, 2, 1, 2, 4, 3, 0, 1, 0, 5, 2};
-	Solution sol;
-	sol.minTaps(35, range);
-	return 0;
-}
