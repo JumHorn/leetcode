d0466bb3f4a50a4bc13049a3b307cc4774e6a791
JumHorn <JumHorn@gmail.com>
2020-09-12 14:29:05 +0800

    overflow must be taken care

--- C/DivideTwoIntegers.c
+++ C/DivideTwoIntegers.c
@@ -1,61 +1,17 @@
 #include <limits.h>
 
-int doublingSearch(unsigned int* d, unsigned int di)
-{
-	unsigned dividend = *d;
-	unsigned long divisor = di;
-	if (divisor > dividend)
-		return 0;
-	int res = 1;
-	divisor <<= 1;
-	while (divisor <= dividend)
-	{
-		res <<= 1;
-		divisor <<= 1;
-	}
-	*d -= divisor >> 1;
-	return res;
-}
-
 int divide(int dividend, int divisor)
 {
-	if ((dividend == INT_MIN && divisor == -1) || divisor == 0)
+	if ((dividend == INT_MIN && divisor == -1) || (divisor == 0)) //overflow
 		return INT_MAX;
-	if (divisor == 1)
-		return dividend;
-	int sign = 1;
-	if ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0))
-		sign = -1;
-	unsigned int dv = dividend;
-	if (dividend < 0)
+	long res = 0, d = labs(dividend), dv = labs(divisor);
+	while (d >= dv)
 	{
-		if (dividend != INT_MIN)
-			dv = -dividend;
-		else
-			dv = dividend;
+		int i = 1;
+		while (d - (dv << i) >= 0)
+			++i;
+		res += 1u << (i - 1);
+		d -= dv << (i - 1);
 	}
-	unsigned int di = divisor;
-	if (divisor < 0)
-	{
-		if (divisor != INT_MIN)
-			di = -divisor;
-		else
-			di = divisor;
-	}
-	int res = 0;
-	while (dv >= di)
-	{
-		unsigned long d = di;
-		int tmp = 1;
-		d <<= 1;
-		while (d <= dv)
-		{
-			tmp <<= 1;
-			d <<= 1;
-		}
-		dv -= d >> 1;
-		res += tmp;
-	}
-	return res * sign;
+	return (dividend > 0) == (divisor > 0) ? res : -res;
 }
-
--- Cplus/DivideTwoIntegers.cpp
+++ Cplus/DivideTwoIntegers.cpp
@@ -7,7 +7,7 @@ class Solution
 public:
 	int divide(int dividend, int divisor)
 	{
-		if ((dividend == INT_MIN && divisor == -1) || (divisor == 0))  //overflow
+		if ((dividend == INT_MIN && divisor == -1) || (divisor == 0)) //overflow
 			return INT_MAX;
 		long res = 0, d = labs(dividend), dv = labs(divisor);
 		while (d >= dv)
@@ -15,7 +15,7 @@ public:
 			int i = 1;
 			while (d - (dv << i) >= 0)
 				++i;
-			res += 1 << (i - 1);
+			res += 1u << (i - 1);
 			d -= dv << (i - 1);
 		}
 		return (dividend > 0) == (divisor > 0) ? res : -res;
