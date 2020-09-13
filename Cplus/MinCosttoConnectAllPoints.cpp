81ef9115209b47f2c5981443617ed709f593ba86
JumHorn <JumHorn@gmail.com>
2020-09-13 21:02:06 +0800

    Prim algorithm of minimum spanning tree

--- Cplus/MinCosttoConnectAllPoints.cpp
+++ Cplus/MinCosttoConnectAllPoints.cpp
@@ -1,66 +1,41 @@
 #include <algorithm>
 #include <cmath>
+#include <queue>
 #include <vector>
 using namespace std;
-
-class DSU
-{
-public:
-	DSU(int size) : parent(size), rank(size, 1)
-	{
-		for (int i = 0; i < size; i++)
-			parent[i] = i;
-	}
-
-	int Find(int x)
-	{
-		if (x != parent[x])
-			parent[x] = Find(parent[x]);
-		return parent[x];
-	}
-
-	bool Union(int x, int y)
-	{
-		int xr = Find(x), yr = Find(y);
-		if (xr == yr)
-			return false;
-		parent[yr] = xr;
-		rank[xr] += rank[yr];
-		return true;
-	}
-	//all nodes united into a single component
-	bool United()
-	{
-		return rank[Find(0)] == parent.size();
-	}
-
-private:
-	vector<int> parent;
-	vector<int> rank;
-};
 
 class Solution
 {
 public:
 	int minCostConnectPoints(vector<vector<int>> &points)
 	{
-		vector<vector<int>> edges;
 		int N = points.size();
+		vector<vector<int>> graph(N, vector<int>(N));
 		for (int i = 0; i < N - 1; ++i)
 		{
 			for (int j = i + 1; j < N; ++j)
-			{
-				int weight = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
-				edges.push_back({weight, i, j});
-			}
+				graph[j][i] = graph[i][j] = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
 		}
-		sort(edges.begin(), edges.end());
-		DSU dsu(N);
-		int res = 0;
-		for (auto &edge : edges)
+		priority_queue<pair<int, int>> q; //{weith,node}
+		vector<int> seen(N);
+		seen[0] = 1;
+		for (int i = 1; i < N; ++i)
+			q.push({-graph[0][i], i});
+		int res = 0, count = 1;
+		while (!q.empty() && count < N)
 		{
-			if (dsu.Union(edge[1], edge[2]))
-				res += edge[0];
+			auto top = q.top();
+			q.pop();
+			if (seen[top.second] == 1)
+				continue;
+			seen[top.second] = 1;
+			res += -top.first;
+			++count;
+			for (int i = 0; i < N; ++i)
+			{
+				if (seen[i] == 0)
+					q.push({-graph[top.second][i], i});
+			}
 		}
 		return res;
 	}
