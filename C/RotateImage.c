f95eedb3f8b44d618fa4de543ad9f4f5671d5b58
JumHorn <JumHorn@gmail.com>
2020-09-14 07:34:31 +0800

    Rotate Image beats 100%

---
+++ C/RotateImage.c
@@ -0,0 +1,18 @@
+
+#define swap(a, b) ((a) ^= (b) ^= (a) ^= (b))
+
+void rotate(int** matrix, int matrixSize, int* matrixColSize)
+{
+	//vertival flip
+	for (int i = 0; i < matrixSize / 2; ++i)
+	{
+		for (int j = 0; j < matrixSize; ++j)
+			swap(matrix[i][j], matrix[matrixSize - i - 1][j]);
+	}
+	//diagonal flip
+	for (int i = 0; i < matrixSize - 1; ++i)
+	{
+		for (int j = i + 1; j < matrixSize; ++j)
+			swap(matrix[i][j], matrix[j][i]);
+	}
+}
