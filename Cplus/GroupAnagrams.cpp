bd668e1e79309fc9db4e68ee9b9385116b0b3dc7
JumHorn <JumHorn@gmail.com>
2020-09-14 07:53:22 +0800

    a little improve using map,the previous solution got TLE

--- Cplus/GroupAnagrams.cpp
+++ Cplus/GroupAnagrams.cpp
@@ -1,38 +1,24 @@
-#include<vector>
-#include<string>
+#include <map>
+#include <string>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<vector<string> > groupAnagrams(vector<string>& strs) {
-		vector<vector<string> > res;
-        vector<vector<int> > hasharray;
-		for(int i=0;i<strs.size();i++)
-		{
-			vector<int> hash(26,0);
-			for(int j=0;j<strs[i].length();j++)
-				hash[strs[i][j]-'a']++;
-			int k;
-			for(k=0;k<hasharray.size();k++)
-				if(compareHashArray(hasharray[k],hash))
-				{
-					res[k].push_back(strs[i]);
-					break;
-				}
-			if(k==hasharray.size())
-			{
-				res.push_back(vector<string>(1,strs[i]));
-				hasharray.push_back(hash);
-			}
-		}
-		return res;
-    }
-
-	bool compareHashArray(vector<int>& v1,vector<int>& v2)
+	vector<vector<string>> groupAnagrams(vector<string> &strs)
 	{
-		for(int i=0;i<v1.size();i++)
-			if(v1[i]!=v2[i])
-				return false;
-		return true;
+		vector<vector<string>> res;
+		map<vector<int>, vector<string>> m;
+		for (auto &str : strs)
+		{
+			vector<int> hash(26);
+			for (auto c : str)
+				++hash[c - 'a'];
+			m[hash].push_back(str);
+		}
+		for (auto &n : m)
+			res.push_back(n.second);
+		return res;
 	}
 };
