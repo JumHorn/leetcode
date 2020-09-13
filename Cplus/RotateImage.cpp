34d99f71350870746aee02e5957e7163c07bf5ab
JumHorn <JumHorn@gmail.com>
2020-09-14 07:38:10 +0800

    formatted

--- Cplus/RotateImage.cpp
+++ Cplus/RotateImage.cpp
@@ -1,28 +1,24 @@
-#include<vector>
+#include <vector>
 using namespace std;
 
 //for 45 and horizantal symmetry
-class Solution {
+class Solution
+{
 public:
-    void rotate(vector<vector<int> >& matrix) {
-		for(int i=0;i<matrix.size();i++)
+	void rotate(vector<vector<int>> &matrix)
+	{
+		int N = matrix.size();
+		//vertival flip
+		for (int i = 0; i < N / 2; ++i)
 		{
-			for(int j=0;j<i;j++)
-			{
-				matrix[i][j]^=matrix[j][i];
-				matrix[j][i]^=matrix[i][j];
-				matrix[i][j]^=matrix[j][i];
-			}
+			for (int j = 0; j < N; ++j)
+				swap(matrix[i][j], matrix[N - i - 1][j]);
 		}
-		for(int j=0;j<matrix.size()/2;j++)
+		//diagonal flip
+		for (int i = 0; i < N - 1; ++i)
 		{
-			int k=matrix.size()-j-1;
-			for(int i=0;i<matrix.size();i++)
-			{
-				matrix[i][j]^=matrix[i][k];
-				matrix[i][k]^=matrix[i][j];
-				matrix[i][j]^=matrix[i][k];
-			}
+			for (int j = i + 1; j < N; ++j)
+				swap(matrix[i][j], matrix[j][i]);
 		}
-    }
+	}
 };
