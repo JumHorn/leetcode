104c0aef3e294cc779e7b430ad045642d175ed24
JumHorn <JumHorn@gmail.com>
2020-09-13 09:23:11 +0800

    bits operation one loop

--- C/ValidSudoku.c
+++ C/ValidSudoku.c
@@ -1,27 +1,29 @@
 #include <stdbool.h>
 
-bool isValidSudoku(char** board, int boardSize, int* boardColSize)
+/*
+bit operation
+we use low 12 bits of a int(at least 4 bit can store 1-9)
+row  col  val
+1111 1111 1111
+row val encodes 1111 0000 1111
+col val encodes 0000 1111 1111
+sub val encodes 1111 1111 1111(subboard encodes with fixed prefix 1111 0000 0000)
+*/
+
+bool isValidSudoku(char **board, int boardSize, int *boardColSize)
 {
-	for (int k = 0; k < 9; ++k)
+	int hash[1 << 12] = {0};
+	for (int i = 0; i < 9; ++i)
 	{
-		int row[9] = {0};
-		for (int j = 0; j < 9; ++j)  //check row
+		for (int j = 0; j < 9; ++j)
 		{
-			if (board[k][j] != '.' && ++row[board[k][j] - '1'] > 1)
-				return false;
-		}
-		int col[9] = {0};
-		for (int i = 0; i < 9; ++i)  //check col
-		{
-			if (board[i][k] != '.' && ++col[board[i][k] - '1'] > 1)
-				return false;
-		}
-		int sub[9] = {0};
-		for (int i = k / 3 * 3; i < k / 3 * 3 + 3; ++i)
-		{
-			for (int j = k % 3 * 3; j < k % 3 * 3 + 3; ++j)
+			if (board[i][j] != '.')
 			{
-				if (board[i][j] != '.' && ++sub[board[i][j] - '1'] > 1)
+				int val = board[i][j] - '1';
+				int row = ((i + 1) << 8) | val;
+				int col = ((j + 1) << 4) | val;
+				int sub = ((i / 3 * 3 + j / 3 % 3) << 4) | val | 0xF << 8;
+				if (++hash[row] > 1 || ++hash[col] > 1 || ++hash[sub] > 1)
 					return false;
 			}
 		}
