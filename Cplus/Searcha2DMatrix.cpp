13a36a4e8f124e07aedb2684eca2d738638f72e2
JumHorn <JumHorn@gmail.com>
2020-09-15 11:47:30 +0800

    formatted

--- Cplus/Searcha2DMatrix.cpp
+++ Cplus/Searcha2DMatrix.cpp
@@ -1,21 +1,23 @@
-#include<vector>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool searchMatrix(vector<vector<int> >& matrix, int target) {
-        if(matrix.empty())
+	bool searchMatrix(vector<vector<int>> &matrix, int target)
+	{
+		if (matrix.empty())
 			return false;
-		int m=matrix.size(),n=matrix[0].size(),i=0,j=n-1;
-		while(i>=0&&i<m&&j>=0&&j<n)
+		int m = matrix.size(), i = 0, j = matrix[0].size() - 1;
+		while (i < m && j >= 0)
 		{
-			if(matrix[i][j]==target)
+			if (matrix[i][j] == target)
 				return true;
-			if(matrix[i][j]>target)
-				j--;
+			if (matrix[i][j] < target)
+				++i;
 			else
-				i++;
+				--j;
 		}
 		return false;
-    }
+	}
 };
