8701c7181497135dcb8cfb36821f095b48940f9c
JumHorn <JumHorn@gmail.com>
2020-09-15 09:19:18 +0800

    simplified dp method

--- C/UniquePaths.c
+++ C/UniquePaths.c
@@ -7,10 +7,8 @@ int uniquePaths(int m, int n)
 	dp[0] = 1;
 	for (int i = 0; i < m; ++i)
 	{
-		int dp_copy[n + 1];
-		memcpy(dp_copy, dp, sizeof(dp));
 		for (int j = 0; j < n; ++j)
-			dp[j + 1] = dp_copy[j + 1] + dp[j];
+			dp[j + 1] += dp[j];
 		dp[0] = 0;
 	}
 	return dp[n];
