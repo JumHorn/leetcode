04f23089e4145a32382a0ffe2165ba758329351f
JumHorn <JumHorn@gmail.com>
2020-09-15 08:42:46 +0800

    formatted

--- C/RotateList.c
+++ C/RotateList.c
@@ -1,30 +1,28 @@
-#include<stdlib.h>
+#include <stdlib.h>
 
 //Definition for singly-linked list.
-struct ListNode {
-    int val;
-    struct ListNode *next;
+struct ListNode
+{
+	int val;
+	struct ListNode *next;
 };
 
-
-
-struct ListNode* rotateRight(struct ListNode* head, int k){
-	if(!head||k==0)
+struct ListNode *rotateRight(struct ListNode *head, int k)
+{
+	if (!head || k == 0)
 		return head;
-	int len=1;
-	struct ListNode* rear=head,*runner=head;
-	while(rear->next!=NULL)
+	int size = 1;
+	struct ListNode *rear = head, *runner = head;
+	while (rear->next != NULL)
 	{
-		len++;
-		rear=rear->next;
+		++size;
+		rear = rear->next;
 	}
-	rear->next=head;
-	len=len-k%len-1;
-	for(int i=0;i<len;i++)
-		runner=runner->next;
-	head=runner->next;
-	runner->next=NULL;
+	rear->next = head;
+	size = size - k % size - 1;
+	for (int i = 0; i < size; i++)
+		runner = runner->next;
+	head = runner->next;
+	runner->next = NULL;
 	return head;
 }
-
-
