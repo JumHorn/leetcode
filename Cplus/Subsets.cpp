5d6b17e2094c5b12ab5420a852cd17fc3a249214
JumHorn <JumHorn@gmail.com>
2020-09-15 13:14:24 +0800

    all sets of combination

--- Cplus/Subsets.cpp
+++ Cplus/Subsets.cpp
@@ -1,60 +1,25 @@
-#include<vector>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int> > subsets(vector<int>& nums) {
-        vector<vector<int> > res;
-		for(int i=0;i<=nums.size();i++)
-		{
-			vector<int> instance(i);
-			combine(res,nums,instance,0,0);
-		}
-		return res;
-    }
-
-	void combine(vector<vector<int> >& res,vector<int>& nums,vector<int>& instance,int l,int k)
+	vector<vector<int>> subsets(vector<int> &nums)
 	{
-		if(l==instance.size())
+		vector<vector<int>> res;
+		vector<int> instance;
+		dfs(nums, 0, instance, res);
+		return res;
+	}
+
+	void dfs(vector<int> &nums, int index, vector<int> &instance, vector<vector<int>> &res)
+	{
+		res.push_back(instance);
+		for (int i = index; i < (int)nums.size(); i++)
 		{
-			res.push_back(instance);
-			return;
-		}
-		for(int i=k;i<nums.size();i++)
-		{
-			instance[l]=nums[i];
-			combine(res,nums,instance,l+1,i+1);
+			instance.push_back(nums[i]);
+			dfs(nums, i + 1, instance, res);
+			instance.pop_back();
 		}
 	}
 };
-
-//a greater solution
-/*
-class Solution {
-public:
-    vector<vector<int>> subsets(vector<int>& nums)
-    {
-        vector< vector<int>>res;
-        if(nums.size()==0)
-            return res;
-
-        unsigned int pow_set_size = pow(2, nums.size());
-        int counter, j;
-
-
-        for(counter = 0; counter < pow_set_size; counter++)
-        {
-            vector<int>r;
-          for(j = 0; j < nums.size(); j++)
-           {
-
-              if(counter & (1<<j))
-                r.push_back(nums[j]);
-           }
-           res.push_back(r);
-        }
-        return res;
-    }
-};
-
-*/
