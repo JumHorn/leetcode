064f63a28b50bcbd802deaff3b05a02854fe1ab2
JumHorn <JumHorn@gmail.com>
2020-09-15 11:58:27 +0800

    Sort Colors beats 100%

---
+++ C/SortColors.c
@@ -0,0 +1,18 @@
+
+
+void sortColors(int* nums, int numsSize)
+{
+	int count[3] = {0};
+	for (int i = 0; i < numsSize; ++i)
+		++count[nums[i]];
+	for (int i = 0, j = 0; i < numsSize;)
+	{
+		if (count[j] > 0)
+		{
+			--count[j];
+			nums[i++] = j;
+		}
+		else
+			++j;
+	}
+}
