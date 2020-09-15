e824f729bbc9eb9475bc2d174c53ceadfd3e638a
JumHorn <JumHorn@gmail.com>
2020-09-15 14:23:31 +0800

    Subsets II beats 66.67%

---
+++ C/SubsetsII.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+
+//cmp function don't consider overflow
+int cmp(const void *lhs, const void *rhs)
+{
+	return *(int *)lhs - *(int *)rhs;
+}
+
+//malloc result
+int **mallocRes(int (*data)[30], int dataSize, int *dataColSize, int *returnSize, int **returnColumnSizes)
+{
+	*returnSize = dataSize;
+	*returnColumnSizes = (int *)malloc(sizeof(int) * (*returnSize));
+	memcpy(*returnColumnSizes, dataColSize, sizeof(int) * (*returnSize));
+	int **res = (int **)malloc(sizeof(int *) * (*returnSize));
+	for (int i = 0; i < *returnSize; ++i)
+	{
+		res[i] = (int *)malloc(sizeof(int) * ((*returnColumnSizes)[i]));
+		memcpy(res[i], data[i], sizeof(int) * ((*returnColumnSizes)[i]));
+	}
+	return res;
+}
+
+void addOneResult(int (*staticRes)[30], int *size, int *colSize, int *data, int dataSize)
+{
+	memcpy(staticRes[*size], data, sizeof(int) * dataSize);
+	colSize[*size] = dataSize;
+	++*size;
+}
+
+void dfs(int *nums, int numsSize, int index, int *data, int dataSize, int (*staticRes)[30], int *size, int *colSize)
+{
+	addOneResult(staticRes, size, colSize, data, dataSize);
+	for (int i = index; i < numsSize; ++i)
+	{
+		if (i != index && nums[i] == nums[i - 1]) //remove dumplicate
+			continue;
+		data[dataSize] = nums[i];
+		dfs(nums, numsSize, i + 1, data, dataSize + 1, staticRes, size, colSize);
+	}
+}
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int **subsetsWithDup(int *nums, int numsSize, int *returnSize, int **returnColumnSizes)
+{
+	static int staticRes[10000][30], staticResColSize[10000];
+	int data[30];
+	qsort(nums, numsSize, sizeof(int), cmp);
+	*returnSize = 0;
+	dfs(nums, numsSize, 0, data, 0, staticRes, returnSize, staticResColSize);
+	return mallocRes(staticRes, *returnSize, staticResColSize, returnSize, returnColumnSizes);
+}
