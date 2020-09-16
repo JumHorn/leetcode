bac31a1dd076045e90cb949c89cfb0f26601d802
JumHorn <JumHorn@gmail.com>
2020-09-17 07:45:59 +0800

    first time to use tuple

--- Cplus/ValidateBinarySearchTree.cpp
+++ Cplus/ValidateBinarySearchTree.cpp
@@ -1,28 +1,37 @@
-#include<iostream>
+#include <climits>
+#include <tuple>
 using namespace std;
-//Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-class Solution {
+class Solution
+{
 public:
-    bool isValidBST(TreeNode* root) {
-		return isValidBST(root,INT_MIN-1L,INT_MAX+1L);
+	bool isValidBST(TreeNode *root)
+	{
+		return get<0>(postorder(root));
 	}
 
-    bool isValidBST(TreeNode* root,long min,long max) {
-        if(root==NULL)
-			return true;
-		if(root->val>=max||root->val<=min)
-			return false;
-		if(root->left!=NULL&&(root->left->val>=root->val||root->left->val<=min))
-			return false;
-		if(root->right!=NULL&&(root->right->val<=root->val||root->right->val>=max))
-			return false;
-		return isValidBST(root->left,min,root->val)&&isValidBST(root->right,root->val,max);
-    }
+	tuple<bool, long, long> postorder(TreeNode *root) //return {isBst,minval,maxval}
+	{
+		if (root == nullptr)
+			return {true, INT_MAX + 1u, INT_MIN - 1u};
+		auto l = postorder(root->left);
+		auto r = postorder(root->right);
+		if (get<0>(l) && get<0>(r))
+		{
+			if (root->val > get<2>(l) && root->val < get<1>(r))
+				return {true, min(get<1>(l), (long)root->val), max(get<2>(r), (long)root->val)};
+		}
+		return {false, INT_MAX, INT_MIN};
+	}
 };
