ab664576429d187de5298cad92141e09cefc9c13
JumHorn <JumHorn@gmail.com>
2020-09-16 08:52:12 +0800

    linus classic

--- Cplus/RemoveDuplicatesfromSortedListII.cpp
+++ Cplus/RemoveDuplicatesfromSortedListII.cpp
@@ -1,27 +1,31 @@
-#include<iostream>
-using namespace std;
+
 //Definition for singly-linked list.
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+struct ListNode
+{
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-class Solution {
+class Solution
+{
 public:
-    ListNode* deleteDuplicates(ListNode* head) {
-        ListNode** indirect=&head;
-		while((*indirect)!=NULL&&(*indirect)->next!=NULL)
+	ListNode *deleteDuplicates(ListNode *head)
+	{
+		ListNode **indirect = &head;
+		while ((*indirect) != nullptr && (*indirect)->next != nullptr)
 		{
-			if((*indirect)->val==(*indirect)->next->val)
+			if ((*indirect)->val == (*indirect)->next->val)
 			{
-				while((*indirect)!=NULL&&(*indirect)->next!=NULL&&(*indirect)->val==(*indirect)->next->val)
-					*indirect=(*indirect)->next;
-				*indirect=(*indirect)->next;
+				while ((*indirect) != nullptr && (*indirect)->next != nullptr && (*indirect)->val == (*indirect)->next->val)
+					*indirect = (*indirect)->next;
+				*indirect = (*indirect)->next;
 			}
 			else
-				indirect=&((*indirect)->next);
+				indirect = &((*indirect)->next);
 		}
 		return head;
-    }
+	}
 };
