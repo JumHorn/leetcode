35b5c25cc61cbecb174f245553a321acddddf191
JumHorn <JumHorn@gmail.com>
2020-09-16 19:52:58 +0800

    easy dp solution

--- Cplus/UniqueBinarySearchTrees.cpp
+++ Cplus/UniqueBinarySearchTrees.cpp
@@ -1,20 +1,20 @@
-#include<vector>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int numTrees(int n) {
-		if(n<3)
+	int numTrees(int n)
+	{
+		if (n < 3)
 			return n;
-		vector<int> dp(n+1,0);
-		dp[0]=1;
-		dp[1]=1;
-		dp[2]=2;
-		for(int i=3;i<n+1;i++)
+		vector<int> dp(n + 1);
+		dp[0] = dp[1] = 1;
+		for (int i = 1; i < n; ++i)
 		{
-			for(int j=0;j<i;j++)
-				dp[i]+=dp[j]*dp[i-j-1];
+			for (int j = 0; j <= i; ++j)
+				dp[i + 1] += dp[j] * dp[i - j];
 		}
 		return dp[n];
-    }
+	}
 };
