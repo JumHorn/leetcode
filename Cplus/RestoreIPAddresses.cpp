2c01bbf3d494db65a0f8bf99bbafe1c204769f08
JumHorn <JumHorn@gmail.com>
2020-09-16 14:32:21 +0800

    normal dp way

--- Cplus/RestoreIPAddresses.cpp
+++ Cplus/RestoreIPAddresses.cpp
@@ -1,59 +1,48 @@
-#include<vector>
-#include<string>
+#include <string>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<string> restoreIpAddresses(string s) {
-		vector<string> res;
-		if(s.empty())
-			return res;
-		if(s[0]=='0')
-		{
-			restoreIpAddresses(res,s,1,"0",2);
-			return res;
-		}
-		for(int i=0;i<(int)s.length();i++)
-		{
-			string tmp=s.substr(0,i+1);
-			if(stoi(tmp)>255)
-				break;
-			restoreIpAddresses(res,s,i+1,tmp,2);
-		}
-		return res;
-    }
-
-	void restoreIpAddresses(vector<string>& res,const string& s,int index,string ip,int num)
+	vector<string> restoreIpAddresses(string s)
 	{
-		if(index>=(int)s.length())
-			return;
-		if(num<=0)
+		int N = s.length();
+		if (N < 4 || N > 12)
+			return {};
+		vector<vector<vector<string>>> dp(N + 1, vector<vector<string>>(5));
+		dp[0][0].push_back("");
+		for (int i = 0; i < N; ++i)
 		{
-			string tmp=s.substr(index);
-			if(tmp.size()>0&&tmp.size()<4)
+			for (int j = 0; j <= i && j < 3; ++j)
 			{
-				int val=stoi(tmp);
-				if(tmp[0]=='0')
+				if (canRestore(s, i, j + 1))
 				{
-					if(tmp.length()==1)
-						res.push_back(ip+"."+tmp);
+					for (int k = 0; k < 4; ++k)
+					{
+						for (auto &str : dp[i - j][k])
+						{
+							if (str.empty())
+								dp[i + 1][k + 1].push_back(s.substr(i - j, j + 1));
+							else
+								dp[i + 1][k + 1].push_back(str + '.' + s.substr(i - j, j + 1));
+						}
+					}
 				}
-				else if(val<=255)
-					res.push_back(ip+"."+tmp);
 			}
-			return;
 		}
-		for(int i=index;i<(int)s.length();i++)
-		{
-			string tmp=s.substr(index,i-index+1);
-			if(s[index]=='0')
-			{
-				restoreIpAddresses(res,s,i+1,ip+".0",num-1);
-				break;
-			}
-			if(stoi(tmp)>255)
-				break;
-			restoreIpAddresses(res,s,i+1,ip+"."+tmp,num-1);
-		}
+		return dp[N][4];
+	}
+
+	bool canRestore(const string &s, int index, int n)
+	{
+		if (n <= 1)
+			return true;
+		int val = (s[index - 1] - '0') * 10 + (s[index] - '0');
+		if (n == 2)
+			return val >= 10;
+		//n==3
+		val += (s[index - 2] - '0') * 100;
+		return val >= 100 && val <= 255;
 	}
 };
