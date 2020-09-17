faf84a1fe1da81d714f3245f524095baad5a002c
JumHorn <JumHorn@gmail.com>
2020-09-17 09:46:34 +0800

    Binary Tree Zigzag Level Order Traversal beats 100%

--- Cplus/BinaryTreeZigzagLevelOrderTraversal.cpp
+++ Cplus/BinaryTreeZigzagLevelOrderTraversal.cpp
@@ -1,32 +1,44 @@
-#include<vector>
-#include<algorithm>
+#include <queue>
+#include <vector>
 using namespace std;
 
 //Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+struct TreeNode
+{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int> > zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int> > res;
-		zigzagLevelOrder(root,res,0);
-		for(int i=1;i<(int)res.size();i+=2)
-			reverse(res[i].begin(),res[i].end());
-		return res;
-    }
-	void zigzagLevelOrder(TreeNode* root,vector<vector<int> >& res,unsigned int layer)
+	vector<vector<int>> zigzagLevelOrder(TreeNode *root)
 	{
-		if(root==NULL)
-			return;
-		if(layer==res.size())
-			res.emplace_back();
-		res[layer].push_back(root->val);
-		zigzagLevelOrder(root->left,res,layer+1);
-		zigzagLevelOrder(root->right,res,layer+1);
+		vector<vector<int>> res;
+		queue<TreeNode *> q;
+		if (root == nullptr)
+			return res;
+		q.push(root);
+		bool left_to_right = false;
+		while (!q.empty())
+		{
+			int size = q.size();
+			res.emplace_back(vector<int>(size));
+			left_to_right = !left_to_right;
+			for (int i = 0; i < size; ++i)
+			{
+				auto node = q.front();
+				q.pop();
+				int index = left_to_right ? i : size - i - 1;
+				res.back()[index] = node->val;
+				if (node->left)
+					q.push(node->left);
+				if (node->right)
+					q.push(node->right);
+			}
+		}
+		return res;
 	}
 };
