7a90268786fcff970a97646dd19040381a8e2d6a
JumHorn <JumHorn@gmail.com>
2020-09-18 07:52:33 +0800

    Populating Next Right Pointers in Each Node beats 97.25%

---
+++ C/PopulatingNextRightPointersinEachNode.c
@@ -0,0 +1,22 @@
+
+//Definition for a Node.
+struct Node
+{
+	int val;
+	struct Node *left;
+	struct Node *right;
+	struct Node *next;
+};
+
+struct Node *connect(struct Node *root)
+{
+	if (!root)
+		return root;
+	if (root->left)
+		root->left->next = root->right;
+	if (root->right && root->next)
+		root->right->next = root->next->left;
+	connect(root->left);
+	connect(root->right);
+	return root;
+}
