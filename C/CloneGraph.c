76b0d25637f2efab335887c0b176c004f3e4742c
JumHorn <JumHorn@gmail.com>
2020-09-18 14:06:36 +0800

    Clone Graph beats 100%

---
+++ C/CloneGraph.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <string.h>
+
+//Definition for a Node.
+struct Node
+{
+	int val;
+	int numNeighbors;
+	struct Node** neighbors;
+};
+
+struct Node* createNode(int val)
+{
+	struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+	node->neighbors = (struct Node**)malloc(sizeof(struct Node*) * 101);
+	memset(node->neighbors, 0, sizeof(struct Node*) * 101);
+	node->numNeighbors = 0;
+	node->val = val;
+	return node;
+}
+
+void dfs(struct Node* s, struct Node* copy, struct Node** seen)
+{
+	if (!s)
+		return;
+	copy->numNeighbors = s->numNeighbors;
+	for (int i = 0; i < s->numNeighbors; ++i)
+	{
+		int index = s->neighbors[i]->val;
+		if (seen[index] == 0)
+		{
+			seen[index] = createNode(index);
+			copy->neighbors[i] = seen[index];
+			dfs(s->neighbors[i], copy->neighbors[i], seen);
+		}
+		else
+			copy->neighbors[i] = seen[index];
+	}
+}
+
+struct Node* cloneGraph(struct Node* s)
+{
+	if (!s)
+		return NULL;
+	struct Node* nodes[101];
+	memset(nodes, 0, sizeof(nodes));
+	struct Node* root = createNode(s->val);
+	dfs(s, root, nodes);
+	return root;
+}
