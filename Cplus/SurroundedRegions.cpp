0ad58002f8f9d9fa9ad1fafccc0ec4b3f2e46b61
JumHorn <JumHorn@gmail.com>
2020-09-18 09:15:23 +0800

    formatted

--- Cplus/SurroundedRegions.cpp
+++ Cplus/SurroundedRegions.cpp
@@ -1,39 +1,41 @@
-#include<vector>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    void solve(vector<vector<char>>& board) {
-		if(board.empty()||board[0].empty())
-			return;
-		for(int i=0;i<(int)board.size();i++)
-		{
-			dfs(board,i,0);
-			dfs(board,i,board[0].size()-1);
-		}
-		for(int j=0;j<(int)board[0].size();j++)
-		{
-			dfs(board,0,j);
-			dfs(board,board.size()-1,j);
-		}	
-		for(int i=0;i<(int)board.size();i++)
-			for(int j=0;j<(int)board[0].size();j++)
-			{
-				if(board[i][j]=='#')
-					board[i][j]='O';
-				else
-					board[i][j]='X';
-			}
-    }
-
-	void dfs(vector<vector<char>>& board,int i,int j)
+	void solve(vector<vector<char>> &board)
 	{
-		if(i<0||i>=(int)board.size()||j<0||j>=(int)board[0].size()||board[i][j]=='#'||board[i][j]=='X')
+		if (board.empty() || board[0].empty())
 			return;
-		board[i][j]='#';
-		dfs(board,i+1,j);
-		dfs(board,i-1,j);
-		dfs(board,i,j+1);
-		dfs(board,i,j-1);
+		int M = board.size(), N = board[0].size();
+		for (int i = 0; i < M; ++i)
+		{
+			dfs(board, i, 0);
+			dfs(board, i, N - 1);
+		}
+		for (int j = 1; j < N - 1; ++j)
+		{
+			dfs(board, 0, j);
+			dfs(board, M - 1, j);
+		}
+		for (int i = 0; i < M; ++i)
+		{
+			for (int j = 0; j < N; ++j)
+				board[i][j] = (board[i][j] == '#' ? 'O' : 'X');
+		}
+	}
+
+	void dfs(vector<vector<char>> &board, int row, int col)
+	{
+		int M = board.size(), N = board[0].size();
+		if (row < 0 || row >= M || col < 0 || col >= N)
+			return;
+		if (board[row][col] == '#' || board[row][col] == 'X')
+			return;
+		board[row][col] = '#';
+		int path[] = {-1, 0, 1, 0, -1};
+		for (int k = 0; k < 4; ++k)
+			dfs(board, row + path[k], col + path[k + 1]);
 	}
 };
