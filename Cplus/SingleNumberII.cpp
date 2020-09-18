8f13240b27fea61a6a75ec97575f7c646541855c
JumHorn <JumHorn@gmail.com>
2020-09-18 09:51:23 +0800

    Single Number II beats 53.28%

--- Cplus/SingleNumberII.cpp
+++ Cplus/SingleNumberII.cpp
@@ -1,9 +1,26 @@
-#include<vector>
+#include <vector>
 using namespace std;
 
-class Solution {
+/*
+count every bit int the array
+if the sum is not a multiple of 3
+the bit in the result should be 1
+*/
+
+class Solution
+{
 public:
-    int singleNumber(vector<int>& nums) {
-        
-    }
+	int singleNumber(vector<int> &nums)
+	{
+		int res = 0;
+		for (int i = 0; i < 32; ++i)
+		{
+			int count = 0;
+			for (auto n : nums)
+				count += (n >> i) & 0x01;
+			if (count % 3 != 0)
+				res |= 1u << i;
+		}
+		return res;
+	}
 };
