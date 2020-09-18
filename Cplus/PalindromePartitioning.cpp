f023fe2fa1e2989367f9a0389de949532b69c3ce
JumHorn <JumHorn@gmail.com>
2020-09-18 19:46:55 +0800

    bottom-up dp solution

--- Cplus/PalindromePartitioning.cpp
+++ Cplus/PalindromePartitioning.cpp
@@ -11,29 +11,28 @@ public:
 		if (s.empty())
 			return {};
 		int N = s.length();
-		vector<vector<bool>> dp(N, vector<bool>(N));
+		vector<vector<bool>> palindrome(N, vector<bool>(N));
 		for (int l = 0; l < N; ++l)
 		{
 			for (int i = 0, j = l; j < N; ++i, ++j)
-				dp[i][j] = s[i] == s[j] && (j - i <= 1 || dp[i + 1][j - 1]);
+				palindrome[i][j] = s[i] == s[j] && (j - i <= 1 || palindrome[i + 1][j - 1]);
 		}
-		vector<vector<string>> res;
-		vector<string> instance;
-		dfs(s, 0, dp, instance, res);
-		return res;
-	}
-
-	void dfs(const string &s, int index, vector<vector<bool>> &dp, vector<string> &instance, vector<vector<string>> &res)
-	{
-		if (index >= s.length())
-			res.push_back(instance);
-		for (int i = index + 1; i <= (int)s.length(); ++i)
+		vector<vector<vector<string>>> dp(N + 1);
+		dp[0] = {{}};
+		for (int i = 0; i < N; ++i)
 		{
-			if (!dp[index][i - 1])
-				continue;
-			instance.push_back(s.substr(index, i - index));
-			dfs(s, i, dp, instance, res);
-			instance.pop_back();
+			for (int j = 0; j <= i; ++j)
+			{
+				if (palindrome[j][i])
+				{
+					for (auto &v : dp[j])
+					{
+						dp[i + 1].push_back(v);
+						dp[i + 1].back().push_back(s.substr(j, i - j + 1));
+					}
+				}
+			}
 		}
+		return dp.back();
 	}
 };
