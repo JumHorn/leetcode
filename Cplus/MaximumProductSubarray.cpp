7b73e6e64f10998397503411258f9dec4e5b8b82
JumHorn <JumHorn@gmail.com>
2020-09-19 17:54:38 +0800

    Maximum Product Subarray beats 98.21%

--- Cplus/MaximumProductSubarray.cpp
+++ Cplus/MaximumProductSubarray.cpp
@@ -1,23 +1,30 @@
-#include<vector>
-#include<algorithm>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-class Solution {
+/*
+First, if there's no zero in the array,
+then the subarray with maximum product must start with the first element or end with the last element
+
+Second if there are zeros in the array,
+the zero bounded subarray meets the first requirement
+
+Algorithm:
+the result must exists in the prefix product or suffix product of the array
+*/
+
+class Solution
+{
 public:
-    int maxProduct(vector<int>& nums) {
-		if(nums.empty())
-			return 0;
-		vector<int> dp(nums.size());        
-		dp[0]=nums[0];
-		for(int i=1;i<(int)nums.size();i++)
+	int maxProduct(vector<int> &nums)
+	{
+		int N = nums.size(), res = nums[0], prefixproduct = 0, suffixproduct = 0;
+		for (int i = 0; i < N; i++)
 		{
-			int tmp=1;
-			for(int j=i;j>=1;j--)
-			{
-				tmp*=nums[j];
-				dp[i]=max(dp[i],tmp*nums[j]*dp[j-1]);
-			}
+			prefixproduct = (prefixproduct != 0 ? prefixproduct : 1) * nums[i];
+			suffixproduct = (suffixproduct != 0 ? suffixproduct : 1) * nums[N - 1 - i];
+			res = max(res, max(prefixproduct, suffixproduct));
 		}
-		return *max_element(dp.begin(),dp.end());
-    }
+		return res;
+	}
 };
