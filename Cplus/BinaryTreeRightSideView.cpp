022f92bb8e050713847f767bd8c691aa9e7768f7
JumHorn <JumHorn@gmail.com>
2020-09-20 09:42:51 +0800

    formatted

--- Cplus/BinaryTreeRightSideView.cpp
+++ Cplus/BinaryTreeRightSideView.cpp
@@ -1,32 +1,35 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-//Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+// Definition for a binary tree node.
+struct TreeNode
+{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> rightSideView(TreeNode* root) {
-		vector<int> res;
-		findMostRight(res,root,0);
-		return res;		
-    }
-
-	void findMostRight(vector<int>& res,TreeNode* root,int depth)
+	vector<int> rightSideView(TreeNode *root)
 	{
-		if(root==NULL)
+		vector<int> res;
+		flipPreorder(root, 0, res);
+		return res;
+	}
+
+	void flipPreorder(TreeNode *root, int depth, vector<int> &res)
+	{
+		if (root == nullptr)
 			return;
-		if(depth==res.size())
-		{
+		if (depth == (int)res.size())
 			res.push_back(root->val);
-		}
-		findMostRight(res,root->right,depth+1);
-		findMostRight(res,root->left,depth+1);
+		flipPreorder(root->right, depth + 1, res);
+		flipPreorder(root->left, depth + 1, res);
 	}
 };
