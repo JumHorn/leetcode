c5a58880a2b4aa9e91991519f3f9af1839bc4fd7
JumHorn <JumHorn@gmail.com>
2020-09-20 17:56:44 +0800

    Bitwise AND of Numbers Range beats 65.43%

---
+++ Cplus/BitwiseANDofNumbersRange.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+	int rangeBitwiseAnd(int m, int n)
+	{
+		int res = 0, bit = 0;
+		while (m != 0 && n != 0)
+		{
+			if (m == n && (m & 1) != 0)
+				res |= 1 << bit;
+			m >>= 1;
+			n >>= 1;
+			++bit;
+		}
+		return res;
+	}
+};
