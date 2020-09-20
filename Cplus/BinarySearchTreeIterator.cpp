5a1e4334473ed9889b05540a2776d50781a99f2c
JumHorn <JumHorn@gmail.com>
2020-09-20 09:27:48 +0800

    greatly simplified

--- Cplus/BinarySearchTreeIterator.cpp
+++ Cplus/BinarySearchTreeIterator.cpp
@@ -1,55 +1,57 @@
-#include<stack>
+#include <stack>
 using namespace std;
-//Definition for binary tree
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-class BSTIterator {
+class BSTIterator
+{
 public:
-    BSTIterator(TreeNode *root) {
-		node=end=root;
-		s.push(node);
-        while(node!=NULL&&node->left!=NULL)
+	BSTIterator(TreeNode *root)
+	{
+		while (root != nullptr)
 		{
-			node=node->left;
-			s.push(node);
-		} 
-		while(end!=NULL)end=end->right;
-    }
-
-    /** @return whether we have a next smallest number */
-    bool hasNext() {
-        return node!=end; 
-    }
-
-    /** @return the next smallest number */
-    int next() {
-        int res=node->val;
-		s.pop();
-		node=node->right;
-		while(node!=NULL)
-		{
-			s.push(node);
-			node=node->left;
+			s.push(root);
+			root = root->left;
 		}
-		if(s.empty())
-			node=NULL;
-		else
-			node=s.top();
-	   return res;	
-    }
+	}
+
+	/** @return the next smallest number */
+	int next()
+	{
+		TreeNode *root = s.top();
+		s.pop();
+		int res = root->val;
+		root = root->right;
+		while (root != nullptr)
+		{
+			s.push(root);
+			root = root->left;
+		}
+		return res;
+	}
+
+	/** @return whether we have a next smallest number */
+	bool hasNext()
+	{
+		return !s.empty();
+	}
+
 private:
-	stack<TreeNode*> s;
-	TreeNode* node;
-	TreeNode* end;
+	stack<TreeNode *> s;
 };
 
 /**
- * Your BSTIterator will be called like this:
- * BSTIterator i = BSTIterator(root);
- * while (i.hasNext()) cout << i.next();
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
  */
