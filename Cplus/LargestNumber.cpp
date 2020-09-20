99208fbda0d0489f0ff5ae45df6877a6ca3f2281
JumHorn <JumHorn@gmail.com>
2020-09-20 17:13:23 +0800

    formatted

--- Cplus/LargestNumber.cpp
+++ Cplus/LargestNumber.cpp
@@ -1,28 +1,26 @@
-#include<string>
-#include<vector>
-#include<algorithm>
+#include <algorithm>
+#include <string>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    string largestNumber(vector<int>& nums) {
-		vector<string> v(nums.size());
-		for(int i=0;i<(int)nums.size();i++)
-			v[i]=to_string(nums[i]);
-		sort(v.begin(),v.end(),*this);
-	   	string res;
-		int i=0;
-		while(i<(int)v.size()&&v[i]=="0")
-			i++;
-		for(;i<(int)v.size();i++)
-			res+=v[i];
-		if(res.empty())
-			return "0";
-		return res;	
-    }
-
-	bool operator()(const string& lhs,const string& rhs)
+	string largestNumber(vector<int> &nums)
 	{
-		return lhs+rhs>rhs+lhs;
+		int N = nums.size();
+		vector<string> v(nums.size());
+		for (int i = 0; i < N; i++)
+			v[i] = to_string(nums[i]);
+		sort(v.begin(), v.end(), *this);
+		string res;
+		for (auto &str : v)
+			res += str;
+		return res[0] == '0' ? "0" : res;
+	}
+
+	bool operator()(const string &lhs, const string &rhs)
+	{
+		return lhs + rhs > rhs + lhs;
 	}
 };
