8682a9a8282e0d484152a80e4f951f0646ea8939
JumHorn <JumHorn@gmail.com>
2020-09-20 17:19:44 +0800

    formatted

--- Cplus/RepeatedDNASequences.cpp
+++ Cplus/RepeatedDNASequences.cpp
@@ -1,21 +1,23 @@
-#include<vector>
-#include<string>
-#include<unordered_set>
+#include <string>
+#include <unordered_set>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<string> findRepeatedDnaSequences(string s) {
-        unordered_set<string> s1,s2;
-		for(int i=0;i<(int)s.length()-9;i++)
+	vector<string> findRepeatedDnaSequences(string s)
+	{
+		unordered_set<string> s1, s2;
+		for (int i = 0; i < (int)s.length() - 9; ++i)
 		{
-			string tmp=s.substr(i,10);
-			if(s1.find(tmp)==s1.end())
-				s1.insert(tmp);
+			string str = s.substr(i, 10);
+			if (s1.find(str) == s1.end())
+				s1.insert(str);
 			else
-				s2.insert(tmp);
+				s2.insert(str);
 		}
 
-		return vector<string>(s2.begin(),s2.end());
-    }
+		return vector<string>(s2.begin(), s2.end());
+	}
 };
