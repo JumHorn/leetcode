1c725fbe2d08457a9ffdbb1c79e0730980c55249
JumHorn <JumHorn@gmail.com>
2020-09-21 19:34:22 +0800

    Design Add and Search Words Data Structure beats 86.80%

--- Cplus/DesignAddandSearchWordsDataStructure.cpp
+++ Cplus/DesignAddandSearchWordsDataStructure.cpp
@@ -1,66 +1,68 @@
-#include<vector>
-#include<string>
+#include <string>
+#include <vector>
 using namespace std;
 
-class WordDictionary {
+class WordDictionary
+{
 	struct TreeNode
 	{
-		vector<TreeNode*> v;
-		bool exist;
-		TreeNode():v(26),exist(false)
+		int count; //number of words ends with this node
+		vector<TreeNode *> nodes;
+
+		TreeNode() : count(0), nodes(26, nullptr)
 		{
-			for(int i=0;i<26;i++)
-				v[i]=NULL;
 		}
 	};
 
-	TreeNode* root;
 public:
-    /** Initialize your data structure here. */
-    WordDictionary() {
-        root=new TreeNode();
-    }
-    
-    /** Adds a word into the data structure. */
-    void addWord(string word) {
-		TreeNode* tmp=root;
-        for(int i=0;i<(int)word.length();i++)
-		{
-			int index=word[i]-'a';
-			if(tmp->v[index]==NULL)
-				tmp->v[index]=new TreeNode();
-			tmp=tmp->v[index];
-		}
-		tmp->exist=true;
-    }
-    
-    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
-    bool search(string word) {
-		return search(word,0,root);
-    }
-
-	bool search(const string& word,int index,TreeNode* root)
+	/** Initialize your data structure here. */
+	WordDictionary()
 	{
-        for(int i=index;i<(int)word.length();i++)
+		root = new TreeNode();
+	}
+
+	/** Adds a word into the data structure. */
+	void addWord(string word)
+	{
+		TreeNode *node = root;
+		for (auto c : word)
 		{
-			if(word[i]=='.')
+			int index = c - 'a';
+			if (node->nodes[index] == nullptr)
+				node->nodes[index] = new TreeNode();
+			node = node->nodes[index];
+		}
+		++node->count;
+	}
+
+	/** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+	bool search(string word)
+	{
+		return doSearch(word, 0, root);
+	}
+
+private:
+	TreeNode *root;
+
+	bool doSearch(const string &str, int index, TreeNode *node)
+	{
+		for (int i = index; i < (int)str.length(); ++i)
+		{
+			if (str[i] == '.')
 			{
-				for(int j=0;j<26;j++)
+				for (int j = 0; j < 26; ++j)
 				{
-					if(root->v[j]!=NULL)
-					{
-						if(search(word,i+1,root->v[j]))
-							return true;
-					}
+					if (node->nodes[j] != nullptr && doSearch(str, i + 1, node->nodes[j]))
+						return true;
 				}
 				return false;
 			}
-			int index=word[i]-'a';
-			if(root->v[index]==NULL)
+			int index = str[i] - 'a';
+			if (node->nodes[index] == nullptr)
 				return false;
-			root=root->v[index];
+			node = node->nodes[index];
 		}
-		return root->exist;
+		return node->count > 0;
 	}
 };
 
