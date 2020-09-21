d71e163faa2d47597443ad02d82528016e322505
JumHorn <JumHorn@gmail.com>
2020-09-21 12:12:08 +0800

    slide window

--- Cplus/MinimumSizeSubarraySum.cpp
+++ Cplus/MinimumSizeSubarraySum.cpp
@@ -1,37 +1,25 @@
-#include<vector>
-#include<algorithm>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int minSubArrayLen(int s, vector<int>& nums) {
-		int res=0,l=0,r=nums.size();
-		while(l<=r)
+	int minSubArrayLen(int s, vector<int> &nums)
+	{
+		int N = nums.size(), res = N;
+		for (int i = 1; i < N; ++i) //prefix sum
+			nums[i] += nums[i - 1];
+		if (nums.empty() || nums.back() < s)
+			return 0;
+		for (int i = 0, j = 0; i < N; ++i)
 		{
-			int tmp=(r-l)/2+l;
-			int all=accumulate(nums.begin(),nums.begin()+tmp,0);
-			if(all>=s)
-			{
-				res=tmp;
-				r=tmp-1;
+			if (nums[i] < s)
 				continue;
-			}
-			int i=0;
-			for(i=1;i<nums.size()-tmp+1;i++)
-			{
-				all=all-nums[i-1]+nums[i+tmp-1];
-				if(all>=s)
-				{
-					res=tmp;
-					r=tmp-1;
-					break;
-				}
-			}
-			if(i==nums.size()-tmp+1)
-			{
-				l=tmp+1;
-			}
+			while (nums[i] - nums[j] >= s)
+				++j;
+			res = min(res, i - j + 1);
 		}
 		return res;
-    }
+	}
 };
