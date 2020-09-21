cbfcc89a2b0f7ceb9c5e33c5c1f4a985b8968553
JumHorn <JumHorn@gmail.com>
2020-09-21 21:17:13 +0800

    simplified code

--- Cplus/HouseRobberII.cpp
+++ Cplus/HouseRobberII.cpp
@@ -1,35 +1,33 @@
-/*
-A(m)=max{A(m-1),A(m-2)+x}
-*/
-#include<vector>
-#include<algorithm>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-class Solution {
+/*
+dp[i]=max{dp[i-1],dp[i-2]+A}
+*/
+
+class Solution
+{
 public:
-    int rob(vector<int>& nums) {
-		if(nums.empty())
+	int rob(vector<int> &nums)
+	{
+		int N = nums.size();
+		if (N == 0)
 			return 0;
-		if(nums.size()==1)
+		if (N == 1)
 			return nums[0];
-		if(nums.size()==2)
-			return max(nums[0],nums[1]);
-		if(nums.size()==3)
-			return max(nums[0],max(nums[1],nums[2]));
-		vector<int> dp0_n_1(nums.size()-1);
-		dp0_n_1[0]=nums[0];
-		dp0_n_1[1]=max(nums[0],nums[1]);
-		for(int i=2;i<dp0_n_1.size();i++)
-		{
-			dp0_n_1[i]=max(dp0_n_1[i-1],dp0_n_1[i-2]+nums[i]);
-		}
-		vector<int> dp1_n(nums.size()-1);
-		dp1_n[0]=nums[1];
-		dp1_n[1]=max(nums[1],nums[2]);
-		for(int i=2;i<dp1_n.size();i++)
-		{
-			dp1_n[i]=max(dp1_n[i-1],dp1_n[i-2]+nums[i+1]);
-		}
-		return max(dp1_n[dp1_n.size()-1],dp0_n_1[dp0_n_1.size()-1]);
+		int r0 = doRob(nums, 0, N - 1);
+		int r1 = doRob(nums, 1, N);
+		return max(r0, r1);
+	}
+
+	int doRob(vector<int> &nums, int first, int last)
+	{
+		vector<int> dp(last - first + 1);
+		dp[0] = 0;
+		dp[1] = nums[first];
+		for (int i = first + 1, j = 1; i < last; ++i, ++j)
+			dp[j + 1] = max(dp[j], dp[j - 1] + nums[i]);
+		return dp.back();
 	}
 };
