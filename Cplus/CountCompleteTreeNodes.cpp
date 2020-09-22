154ac039f0a2de2c2d4350f5c02a4ffd508e6826
JumHorn <JumHorn@gmail.com>
2020-09-22 10:46:56 +0800

    Count Complete Tree Nodes beats 90.38%

--- Cplus/CountCompleteTreeNodes.cpp
+++ Cplus/CountCompleteTreeNodes.cpp
@@ -1,44 +1,35 @@
-#include<algorithm>
-using namespace std;
-//Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-class Solution {
+class Solution
+{
 public:
-	int countNodes(TreeNode* root)
+	int countNodes(TreeNode *root)
 	{
-		int res=0;
-		countNodes(root,res);
-		return res;
+		if (root == nullptr)
+			return 0;
+		int l = heght(root->left);
+		int r = heght(root->right);
+		return (r < l) ? (1 << r) + countNodes(root->left) : (1 << l) + countNodes(root->right);
 	}
 
-    bool countNodes(TreeNode* root,int& res) {
-        if(root==NULL)
-			return true;
-		bool l=countNodes(root->left,res);
-		bool r=countNodes(root->right,res);
-		if(l&&r)
-		{
-			int lh=height(root->left);
-			int rh=height(root->right);
-			if(lh-rh<=1)
-			{
-				++res;
-				return true;
-            }
-		}
-		return false;
-    }
-
-	int height(TreeNode* root)
+	int heght(TreeNode *root)
 	{
-		if(root==NULL)
-			return 0;
-		return height(root->left)+1;
+		int res = 0;
+		while (root != nullptr)
+		{
+			++res;
+			root = root->left;
+		}
+		return res;
 	}
 };
