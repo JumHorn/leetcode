4769d3a747a90586f6d9d92d6e3e65bf65185d98
JumHorn <JumHorn@gmail.com>
2020-09-22 13:10:32 +0800

    Search a 2D Matrix II beats 38.91%

--- Cplus/Searcha2DMatrixII.cpp
+++ Cplus/Searcha2DMatrixII.cpp
@@ -1,25 +1,27 @@
-#include<vector>
+#include <vector>
 using namespace std;
 
 /*
 search from top right
 */
 
-class Solution {
+class Solution
+{
 public:
-    bool searchMatrix(vector<vector<int> >& matrix, int target) {
-		if(matrix.empty())
+	bool searchMatrix(vector<vector<int>> &matrix, int target)
+	{
+		if (matrix.empty())
 			return false;
-		int row=0,column=matrix[0].size()-1;
-		while(row<(int)matrix.size()&&column>=0)
+		int m = matrix.size(), i = 0, j = matrix[0].size() - 1;
+		while (i < m && j >= 0)
 		{
-			if(matrix[row][column]==target)
+			if (matrix[i][j] == target)
 				return true;
-			else if(target>matrix[row][column])
-				row++;
+			if (matrix[i][j] < target)
+				++i;
 			else
-				column--;
+				--j;
 		}
 		return false;
-    }
+	}
 };
