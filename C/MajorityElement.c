0f2aab0446a6988d5686890eec5771b2619c3850
JumHorn <JumHorn@gmail.com>
2020-09-22 16:41:00 +0800

    Majority Element beats 95.42%

--- C/MajorityElement.c
+++ C/MajorityElement.c
@@ -1,39 +1,18 @@
-#include<stdio.h>
-int main()
-{
-	int majorityElement(int *nums,int numsSize);
-}
 
-int majorityElement(int *nums,int numsSize)
+int majorityElement(int *nums, int numsSize)
 {
-	int a[200][2]={0},i,j,k=0;
-	for(i=0;i<numsSize;i++)
+	int val = 0, count = 0;
+	for (int i = 0; i < numsSize; ++i)
 	{
-		for(j=0;j<k;j++)
+		if (val == nums[i])
+			++count;
+		else if (count == 0)
 		{
-			if(a[j][0]==nums[i])
-			{
-				a[j][1]++;
-				if(a[j][1]>numsSize/2)
-				{
-					return a[j][0];
-				}
-				else
-				{
-					break;
-				}
-			}
+			val = nums[i];
+			count = 1;
 		}
-		if(j==k)
-		{
-			a[k][0]=nums[i];
-			a[k][1]=1;
-			if(a[k][1]>numsSize/2)
-			{
-				return a[k][0];
-			}
-			k++;
-		}
+		else
+			--count;
 	}
-	return 0;
+	return val;
 }
