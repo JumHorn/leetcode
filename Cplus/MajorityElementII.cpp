37ecca004179b49162db81e48c03b4626efaba37
JumHorn <JumHorn@gmail.com>
2020-09-22 18:45:09 +0800

    formatted

--- Cplus/MajorityElementII.cpp
+++ Cplus/MajorityElementII.cpp
@@ -1,25 +1,27 @@
-#include<vector>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> majorityElement(vector<int>& nums) {
-		int count1=0,count2=0,value1=0,value2=1;
-		for(int i=0;i<(int)nums.size();i++)     
+	vector<int> majorityElement(vector<int> &nums)
+	{
+		int count1 = 0, count2 = 0, value1, value2, N = nums.size();
+		for (int i = 0; i < N; ++i)
 		{
-			if(nums[i]==value1)
+			if (nums[i] == value1)
 				++count1;
-			else if(nums[i]==value2)
+			else if (nums[i] == value2)
 				++count2;
-			else if(count1==0)
+			else if (count1 == 0)
 			{
-				value1=nums[i];
-				++count1;
+				value1 = nums[i];
+				count1 = 1;
 			}
-			else if(count2==0)
+			else if (count2 == 0)
 			{
-				++count2;
-				value2=nums[i];
+				value2 = nums[i];
+				count2 = 1;
 			}
 			else
 			{
@@ -27,21 +29,20 @@ public:
 				--count2;
 			}
 		}
-		vector<int> res;
 
-		count1=0;
-		for(int i=0;i<(int)nums.size();i++)
-			if(value1==nums[i])
+		count1 = count2 = 0;
+		for (int i = 0; i < N; ++i)
+		{
+			if (value1 == nums[i])
 				++count1;
-		if(count1>(int)nums.size()/3)
-			res.push_back(value1);
-
-		count2=0;
-		for(int i=0;i<(int)nums.size();i++)
-			if(value2==nums[i])
+			else if (value2 == nums[i])
 				++count2;
-		if(count2>(int)nums.size()/3)
+		}
+		vector<int> res;
+		if (count1 > N / 3)
+			res.push_back(value1);
+		if (count2 > N / 3)
 			res.push_back(value2);
 		return res;
-    }
+	}
 };
