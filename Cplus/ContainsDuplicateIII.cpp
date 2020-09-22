64e5cc60cf3e8d1c7bf6e885a86c890f5e2f9ec7
JumHorn <JumHorn@gmail.com>
2020-09-22 08:19:20 +0800

    using bucket sort to solve this problem

--- Cplus/ContainsDuplicateIII.cpp
+++ Cplus/ContainsDuplicateIII.cpp
@@ -1,22 +1,30 @@
 #include <algorithm>
-#include <set>
+#include <climits>
+#include <unordered_map>
 #include <vector>
 using namespace std;
+
+/*
+bucket sort
+*/
 
 class Solution
 {
 public:
-	bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t)
+	bool containsNearbyAlmostDuplicate(vector<int> &nums, int k, int t)
 	{
-		set<long> window;
-		for (int i = 0; i < (int)nums.size(); i++)
+		int N = nums.size();
+		if (k < 1 || N < 2)
+			return false;
+		unordered_map<long, long> m;
+		for (int i = 0; i < N; ++i)
 		{
-			if ((long)window.size() > k)
-				window.erase(nums[i - k - 1]);
-			set<long>::iterator iter = window.lower_bound((long)nums[i] - t);
-			if (iter != window.end() && *iter - nums[i] <= t)
+			long bucket = ((long)nums[i] - INT_MIN) / ((long)t + 1); //map nums[i] to bucket
+			if (m.find(bucket) != m.end() || (m.find(bucket - 1) != m.end() && nums[i] - m[bucket - 1] <= t) || (m.find(bucket + 1) != m.end() && m[bucket + 1] - nums[i] <= t))
 				return true;
-			window.insert(nums[i]);
+			if (m.size() >= k)
+				m.erase(((long)nums[i - k] - INT_MIN) / ((long)t + 1));
+			m[bucket] = nums[i];
 		}
 		return false;
 	}
