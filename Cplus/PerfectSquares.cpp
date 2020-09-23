93cf463a6a42694f0ba2c5ab4d82348409b30c44
JumHorn <JumHorn@gmail.com>
2020-09-23 12:41:35 +0800

    copy a solution based on math

--- Cplus/PerfectSquares.cpp
+++ Cplus/PerfectSquares.cpp
@@ -1,27 +1,45 @@
-#include<algorithm>
-#include<iostream>
-#include<vector>
+#include <algorithm>
+#include <cmath>
+#include <vector>
 using namespace std;
 
-class Solution {
-public:
-    int numSquares(int n) {
-		static vector<int> dp({0});
-        while(n>=(int)dp.size())
-        {
-            int m=dp.size(),tmp=INT_MAX;
-            for(int j=1;j*j<=m;j++)
-            {
-                tmp=min(tmp,1+dp[m-j*j]);
-            }
-            dp.push_back(tmp);
-        }
-		return dp[n];
-    }
-};
+/*
+Lagrange's four-square theorem
+*/
 
-int main()
+class Solution
 {
-	Solution sol;
-	sol.numSquares(11);
-}
+public:
+	// Based on Lagrange's Four Square theorem, there
+	// are only 4 possible results: 1, 2, 3, 4.
+	int numSquares(int n)
+	{
+		// If n is a perfect square, return 1.
+		if (isSquare(n))
+			return 1;
+
+		// The result is 4 if and only if n can be written in the
+		// form of 4^k*(8*m + 7). Please refer to
+		// Legendre's three-square theorem.
+		while ((n & 3) == 0) // n%4 == 0
+			n >>= 2;
+		if ((n & 7) == 7) // n%8 == 7
+			return 4;
+
+		// Check whether 2 is the result.
+		int sqrt_n = (int)(sqrt(n));
+		for (int i = 1; i <= sqrt_n; i++)
+		{
+			if (isSquare(n - i * i))
+				return 2;
+		}
+
+		return 3;
+	}
+
+	int isSquare(int n)
+	{
+		int sqrt_n = (int)(sqrt(n));
+		return (sqrt_n * sqrt_n == n);
+	}
+};
