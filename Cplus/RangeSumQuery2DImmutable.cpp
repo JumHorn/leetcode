4b994b7c7b4f3590d98acae7c8053c6f5ccf38a2
JumHorn <JumHorn@gmail.com>
2020-09-24 07:12:07 +0800

    formatted

--- Cplus/RangeSumQuery2DImmutable.cpp
+++ Cplus/RangeSumQuery2DImmutable.cpp
@@ -1,22 +1,29 @@
-#include<vector>
+#include <vector>
 using namespace std;
 
-class NumMatrix {
-	vector<vector<int>> *m_matrix;
+class NumMatrix
+{
 public:
-    NumMatrix(vector<vector<int>>& matrix) {
-		if(matrix.empty()||matrix[0].empty())
+	NumMatrix(vector<vector<int>> &matrix)
+	{
+		if (matrix.empty() || matrix[0].empty())
 			return;
-		int M=matrix.size(),N=matrix[0].size();
-		m_matrix=new vector<vector<int>>(M+1,vector<int>(N+1));        
-		for(int i=1;i<=M;i++)
-			for(int j=1;j<=N;j++)
-				(*m_matrix)[i][j]=(*m_matrix)[i][j-1]+(*m_matrix)[i-1][j]-(*m_matrix)[i-1][j-1]+matrix[i-1][j-1];
-    }
-    
-    int sumRegion(int row1, int col1, int row2, int col2) {
-    	return (*m_matrix)[row2+1][col2+1]-(*m_matrix)[row2+1][col1]-(*m_matrix)[row1][col2+1]+(*m_matrix)[row1][col1];
-    }
+		int M = matrix.size(), N = matrix[0].size();
+		m_matrix = vector<vector<int>>(M + 1, vector<int>(N + 1));
+		for (int i = 0; i < M; ++i)
+		{
+			for (int j = 0; j < N; ++j)
+				m_matrix[i + 1][j + 1] = m_matrix[i + 1][j] + m_matrix[i][j + 1] - m_matrix[i][j] + matrix[i][j];
+		}
+	}
+
+	int sumRegion(int row1, int col1, int row2, int col2)
+	{
+		return m_matrix[row2 + 1][col2 + 1] - m_matrix[row2 + 1][col1] - m_matrix[row1][col2 + 1] + m_matrix[row1][col1];
+	}
+
+private:
+	vector<vector<int>> m_matrix;
 };
 
 /**
