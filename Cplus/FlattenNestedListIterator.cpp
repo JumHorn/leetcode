6798aa62aeb8fff04f50bf8bea98120c976a0eb5
JumHorn <JumHorn@gmail.com>
2020-09-24 14:50:16 +0800

    a noncopy iterator,copy will cost a lot

--- Cplus/FlattenNestedListIterator.cpp
+++ Cplus/FlattenNestedListIterator.cpp
@@ -1,57 +1,58 @@
-#include<vector>
+#include <stack>
+#include <vector>
 using namespace std;
 
 // This is the interface that allows for creating nested lists.
 // You should not implement it, or speculate about its implementation
-class NestedInteger {
-  public:
-    // Return true if this NestedInteger holds a single integer, rather than a nested list.
-    bool isInteger() const;
-
-    // Return the single integer that this NestedInteger holds, if it holds a single integer
-    // The result is undefined if this NestedInteger holds a nested list
-    int getInteger() const;
-
-    // Return the nested list that this NestedInteger holds, if it holds a nested list
-    // The result is undefined if this NestedInteger holds a single integer
-    const vector<NestedInteger> &getList() const;
+class NestedInteger
+{
+public:
+	// Return true if this NestedInteger holds a single integer, rather than a nested list.
+	bool isInteger() const;
+	// Return the single integer that this NestedInteger holds, if it holds a single integer
+	// The result is undefined if this NestedInteger holds a nested list
+	int getInteger() const;
+	// Return the nested list that this NestedInteger holds, if it holds a nested list
+	// The result is undefined if this NestedInteger holds a single integer
+	vector<NestedInteger> &getList() const;
 };
 
-class NestedIterator {
+class NestedIterator
+{
 public:
-    NestedIterator(vector<NestedInteger> &nestedList){
-		nindex=0;
-        nestedToVector(nestedList); 
-    }
-
-    int next() {
-		return nested[nindex++];
-    }
-
-    bool hasNext() {
-		if(nindex!=nested.size())
-			return true;
-		return false;
-    }
-private:
-	void nestedToVector(const vector<NestedInteger>& n)
+	NestedIterator(vector<NestedInteger> &nestedList)
 	{
-		for(vector<NestedInteger>::const_iterator iter=n.begin();iter!=n.end();iter++)
+		s.push({nestedList.begin(), nestedList.end()});
+	}
+
+	int next()
+	{
+		hasNext();
+		int res = s.top().first->getInteger();
+		++s.top().first;
+		return res;
+	}
+
+	bool hasNext()
+	{
+		while (!s.empty())
 		{
-			if(iter->isInteger())
-			{
-				nested.push_back(iter->getInteger());
-			}
+			if (s.top().first == s.top().second)
+				s.pop();
 			else
 			{
-				nestedToVector(iter->getList());
+				auto iter = s.top().first;
+				if (iter->isInteger())
+					return true;
+				++s.top().first;
+				s.push({iter->getList().begin(), iter->getList().end()});
 			}
 		}
+		return false;
 	}
 
 private:
-	vector<int> nested;
-	int nindex;
+	stack<pair<vector<NestedInteger>::iterator, vector<NestedInteger>::iterator>> s;
 };
 
 /**
