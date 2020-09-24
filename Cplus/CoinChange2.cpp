37785e1335565a0e93f52c62d4e704a74dc743bd
JumHorn <JumHorn@gmail.com>
2020-09-24 08:47:12 +0800

    formatted

--- Cplus/CoinChange2.cpp
+++ Cplus/CoinChange2.cpp
@@ -1,24 +1,29 @@
-#include<vector>
-#include<algorithm>
-#include<unordered_map>
+#include <algorithm>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int change(int amount, vector<int>& coins) {
-        if(amount==0)
-            return 1;
-		vector<vector<int> > dp(coins.size()+1,vector<int>(amount+1));
-		for(int j=1;j<=amount;j++)
-			for(int i=1;i<=(int)coins.size();i++)
+	int change(int amount, vector<int> &coins)
+	{
+		int N = coins.size();
+		if (amount == 0)
+			return 1;
+		vector<vector<int>> dp(N + 1, vector<int>(amount + 1));
+		for (int i = 0; i < N; ++i)
+		{
+			for (int j = 1; j <= amount; ++j)
 			{
-				if(j==coins[i-1])
-					dp[i][j]=1+dp[i-1][j];
-				else if(j<coins[i-1])
-					dp[i][j]=dp[i-1][j];
+				if (j == coins[i])
+					dp[i + 1][j] = 1 + dp[i][j];
+				else if (j < coins[i])
+					dp[i + 1][j] = dp[i][j];
 				else
-					dp[i][j]=dp[i][j-coins[i-1]]+dp[i-1][j];
+					dp[i + 1][j] = dp[i + 1][j - coins[i]] + dp[i][j];
 			}
+		}
 		return dp.back().back();
-    }
+	}
 };
