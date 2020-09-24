623efef7e55d30d23f4b86cc5f14aab39b6599b7
JumHorn <JumHorn@gmail.com>
2020-09-24 09:03:25 +0800

    Coin Change 2 beats 100%

--- C/CoinChange2.c
+++ C/CoinChange2.c
@@ -1,27 +1,16 @@
 #include <string.h>
 
-int memdp(int* coins, int coinsSize, int index, int amount, int (*dp)[coinsSize])
-{
-	if (amount < 0)
-		return 0;
-	if (amount == 0)
-		return 1;
-	if (dp[amount][index] != -1)
-		return dp[amount][index];
-	int res = 0;
-	for (int i = index; i < coinsSize; ++i)
-		res += memdp(coins, coinsSize, i, amount - coins[i], dp);
-	return dp[amount][index] = res;
-}
-
 int change(int amount, int* coins, int coinsSize)
 {
-	if (coinsSize == 0)
-		return amount == 0 ? 1 : 0;
 	if (amount == 0)
 		return 1;
-	int dp[amount + 1][coinsSize];
-	memset(dp, -1, sizeof(dp));
-	memdp(coins, coinsSize, 0, amount, dp);
-	return dp[amount][0];
+	int dp[amount + 1];
+	memset(dp, 0, sizeof(dp));
+	dp[0] = 1;
+	for (int i = 0; i < coinsSize; ++i)
+	{
+		for (int j = coins[i]; j <= amount; ++j)
+			dp[j] += dp[j - coins[i]];
+	}
+	return dp[amount];
 }
