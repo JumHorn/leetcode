85779725bb1f83a45d1d27619ae3a1a4333a1c56
JumHorn <JumHorn@gmail.com>
2020-09-24 16:50:22 +0800

    Largest Divisible Subset beats 33.33%

---
+++ C/LargestDivisibleSubset.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <string.h>
+
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+
+//cmp function don't consider overflow
+int cmp(const void *lhs, const void *rhs)
+{
+	return *(int *)lhs - *(int *)rhs;
+}
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int *largestDivisibleSubset(int *nums, int numsSize, int *returnSize)
+{
+	if (numsSize == 0)
+	{
+		*returnSize = 0;
+		return NULL;
+	}
+	qsort(nums, numsSize, sizeof(int), cmp);
+	int dp[numsSize];
+	memset(dp, 0, sizeof(dp));
+	int len = 1, index = 0;
+	for (int i = 0; i < numsSize; ++i)
+	{
+		for (int j = 0; j < i; ++j)
+		{
+			if (nums[i] % nums[j] == 0)
+				dp[i] = max(dp[i], dp[j] + 1);
+		}
+		dp[i] = max(dp[i], 1);
+		if (len < dp[i])
+		{
+			len = dp[i];
+			index = i;
+		}
+	}
+	*returnSize = len;
+	int *res = (int *)malloc(sizeof(int) * (*returnSize));
+	for (int i = index; i >= 0; --i)
+	{
+		if (len == dp[i] && nums[index] % nums[i] == 0)
+		{
+			res[--len] = nums[i];
+			index = i;
+		}
+	}
+	return res;
+}
