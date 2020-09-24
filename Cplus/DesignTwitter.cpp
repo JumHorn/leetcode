d71fd1ee8a952e5ae52422e5404b817aa5668898
JumHorn <JumHorn@gmail.com>
2020-09-24 17:58:58 +0800

    Design Twitter beats 63.54%

--- Cplus/DesignTwitter.cpp
+++ Cplus/DesignTwitter.cpp
@@ -1,79 +1,70 @@
-#include<unordered_map>
-#include<list>
-#include<vector>
-#include<unordered_set>
+#include <list>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
 using namespace std;
 
-class Twitter {
-private:
-	vector<int> twitters;
-	unordered_map<int,list<int>> newsfeed;
-	unordered_map<int,unordered_set<int>> followermanager;
-	unordered_map<int,unordered_set<int>> followeemanager;
-	unordered_map<int,int> twittertouser;
+typedef pair<list<int>::iterator, list<int>::iterator> TwiterContent;
 
-    void updateNewsFeed(int userId)
-    {
-        newsfeed[userId].clear();
-		for(int i=twitters.size()-1;i>=0;i--)
-		{
-			if(followeemanager[userId].find(twittertouser[twitters[i]])!=followeemanager[userId].end())
-				newsfeed[userId].push_back(twitters[i]);
-			if(newsfeed[userId].size()>=10)
-				break;
-		}
-    }
-
+class Twitter
+{
 public:
-    /** Initialize your data structure here. */
-    Twitter() {
-        
-    }
-    
-    /** Compose a new tweet. */
-    void postTweet(int userId, int tweetId) {
+	/** Initialize your data structure here. */
+	Twitter()
+	{
+	}
+
+	/** Compose a new tweet. */
+	void postTweet(int userId, int tweetId)
+	{
 		twitters.push_back(tweetId);
-		twittertouser[tweetId]=userId;
-        followermanager[userId].insert(userId);
-        followeemanager[userId].insert(userId);
-		for(unordered_set<int>::iterator iter=followermanager[userId].begin();iter!=followermanager[userId].end();++iter)
+		newsfeed[userId].push_front(twitters.size() - 1);
+	}
+
+	/** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
+	vector<int> getNewsFeed(int userId)
+	{
+		auto f = [=](TwiterContent &lhs, TwiterContent &rhs) { return *lhs.first < *rhs.first; };
+		priority_queue<TwiterContent, vector<TwiterContent>, decltype(f)> q(f);
+		followermanager[userId].insert(userId);
+		for (auto &user : followermanager[userId])
 		{
-			newsfeed[*iter].push_front(tweetId);
-			if(newsfeed[*iter].size()>10)
-				newsfeed[*iter].pop_back();
+			if (!newsfeed[user].empty())
+				q.push({newsfeed[user].begin(), newsfeed[user].end()});
 		}
-    }
-    
-    /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
-    vector<int> getNewsFeed(int userId) {
-        return vector<int>(newsfeed[userId].begin(),newsfeed[userId].end());
-    }
-    
-    /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
-    void follow(int followerId, int followeeId) {
-        followermanager[followerId].insert(followerId);
-        followeemanager[followerId].insert(followerId);
-		if(followerId==followeeId)
+		vector<int> res;
+		while (res.size() < 10 && !q.empty())
+		{
+			auto content = q.top();
+			q.pop();
+			res.push_back(twitters[*content.first++]);
+			if (content.first != content.second)
+				q.push(content);
+		}
+		return res;
+	}
+
+	/** Follower follows a followee. If the operation is invalid, it should be a no-op. */
+	void follow(int followerId, int followeeId)
+	{
+		if (followerId == followeeId)
 			return;
-		if(followermanager[followeeId].find(followerId)!=followermanager[followeeId].end())
+		followermanager[followerId].insert(followeeId);
+	}
+
+	/** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
+	void unfollow(int followerId, int followeeId)
+	{
+		if (followerId == followeeId)
 			return;
-        followermanager[followeeId].insert(followerId);
-        followeemanager[followerId].insert(followeeId);
-        updateNewsFeed(followerId);
-    }
-    
-    /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
-    void unfollow(int followerId, int followeeId) {
-        followermanager[followerId].insert(followerId);
-        followeemanager[followerId].insert(followerId);
-		if(followeeId==followerId)
-			return;
-		if(followermanager[followeeId].find(followerId)==followermanager[followeeId].end())
-			return;
-        followermanager[followeeId].erase(followerId);
-        followeemanager[followerId].erase(followeeId);
-        updateNewsFeed(followerId);
-    }
+		followermanager[followerId].erase(followeeId);
+	}
+
+private:
+	vector<int> twitters;									//twitters ordered by post time
+	unordered_map<int, list<int>> newsfeed;					//{userid,twiters index}
+	unordered_map<int, unordered_set<int>> followermanager; //{userid,following userid}
 };
 
 /**
