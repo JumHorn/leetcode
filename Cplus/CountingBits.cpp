829f80dc229fdbaaa0239f2383205faa5631571f
JumHorn <JumHorn@gmail.com>
2020-09-24 10:54:47 +0800

    Counting Bits beats 98.49%

---
+++ Cplus/CountingBits.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+	vector<int> countBits(int num)
+	{
+		vector<int> dp(num + 1);
+		for (int i = 1; i <= num; ++i)
+			dp[i] = dp[i >> 1] + (i & 1);
+		return dp;
+	}
+};
