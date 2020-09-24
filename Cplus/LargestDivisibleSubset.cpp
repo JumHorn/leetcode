a6610f3e2e8374d1d29ebea856d01e5c041e5667
JumHorn <JumHorn@gmail.com>
2020-09-24 17:15:42 +0800

    Largest Divisible Subset beats 80.74%

--- Cplus/LargestDivisibleSubset.cpp
+++ Cplus/LargestDivisibleSubset.cpp
@@ -1,41 +1,41 @@
-#include<vector>
-#include<algorithm>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> largestDivisibleSubset(vector<int>& nums) {
+	vector<int> largestDivisibleSubset(vector<int> &nums)
+	{
 		vector<int> res;
-		if(nums.empty())
+		if (nums.empty())
 			return res;
-		sort(nums.begin(),nums.end());
-        int (*dp)[2] = new int[nums.size()][2];
-        for(int i=0;i<(int)nums.size();i++)
-            dp[i][0]=1;
-		int maxnum=1,maxindex=0;
-		for(int i=1;i<(int)nums.size();i++)
+		sort(nums.begin(), nums.end());
+		int N = nums.size();
+		vector<vector<int>> dp(N, vector<int>(2)); // dp[][0] max size dp[][1] previous index
+		int maxsize = 1, maxindex = 0;
+		for (int i = 0; i < N; ++i)
 		{
-			for(int j=0;j<i;j++)
-				if(nums[i]%nums[j]==0&&dp[i][0]<dp[j][0]+1)
+			for (int j = 0; j < i; ++j)
+			{
+				if (nums[i] % nums[j] == 0 && dp[i][0] < dp[j][0] + 1)
 				{
-					dp[i][1]=j;
-				 	dp[i][0]=dp[j][0]+1;
-					if(dp[i][0]>maxnum)
+					dp[i][0] = dp[j][0] + 1;
+					dp[i][1] = j;
+					if (dp[i][0] > maxsize)
 					{
-						maxindex=i;
-						maxnum=dp[i][0];
+						maxsize = dp[i][0];
+						maxindex = i;
 					}
 				}
+			}
+			dp[i][0] = max(dp[i][0], 1);
 		}
-        if(maxnum==1)
-            res.push_back(nums[maxindex]);
-		while(maxnum!=1)
+		while (--maxsize >= 0)
 		{
 			res.push_back(nums[maxindex]);
-			maxnum=dp[maxindex][0];
-			maxindex=dp[maxindex][1];
+			maxindex = dp[maxindex][1];
 		}
-
 		return res;
-    }
+	}
 };
