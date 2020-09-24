9e374301263e9109b5d854797b3ce20c4edd4894
JumHorn <JumHorn@gmail.com>
2020-09-24 10:34:38 +0800

    add a elegant three way partioning solution

--- Cplus/WiggleSortII.cpp
+++ Cplus/WiggleSortII.cpp
@@ -1,19 +1,47 @@
-#include<vector>
-#include<algorithm>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-class Solution {
+// Index-rewiring.  "virtual indexing"
+#define A(i) nums[(1 + 2 * (i)) % (N | 1)]
+/*
+Accessing A(0) actually accesses nums[1].
+Accessing A(1) actually accesses nums[3].
+Accessing A(2) actually accesses nums[5].
+Accessing A(3) actually accesses nums[7].
+Accessing A(4) actually accesses nums[9].
+Accessing A(5) actually accesses nums[0].
+Accessing A(6) actually accesses nums[2].
+Accessing A(7) actually accesses nums[4].
+Accessing A(8) actually accesses nums[6].
+Accessing A(9) actually accesses nums[8].
+*/
+
+/*
+three-way partitioning
+*/
+
+class Solution
+{
 public:
-    void wiggleSort(vector<int>& nums) {
-		int N=nums.size();
-		vector<int> res(nums);
-		sort(res.begin(),res.end());
-		int i=N/2-((N^1)&1),m=i,j=N-1,k=0;
-		while(i>=0)
+	void wiggleSort(vector<int> &nums)
+	{
+		int N = nums.size();
+		// Find a median.
+		auto midptr = nums.begin() + N / 2;
+		nth_element(nums.begin(), midptr, nums.end());
+		int mid = *midptr;
+
+		// 3-way-partition-to-wiggly in O(n) time with O(1) space.
+		int i = 0, j = 0, k = N - 1;
+		while (j <= k)
 		{
-			nums[k++]=res[i--];
-            if(j>m)
-			    nums[k++]=res[j--];
+			if (A(j) > mid)
+				swap(A(i++), A(j++));
+			else if (A(j) < mid)
+				swap(A(j), A(k--));
+			else
+				++j;
 		}
-    }
+	}
 };
