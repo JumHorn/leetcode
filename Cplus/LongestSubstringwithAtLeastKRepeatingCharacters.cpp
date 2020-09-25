d0fd0f57adfc8629a37060caf4442ea6ac3461cf
JumHorn <JumHorn@gmail.com>
2020-09-25 17:28:09 +0800

    formatted

--- Cplus/LongestSubstringwithAtLeastKRepeatingCharacters.cpp
+++ Cplus/LongestSubstringwithAtLeastKRepeatingCharacters.cpp
@@ -1,26 +1,26 @@
-#include<string>
+#include <string>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int longestSubstring(string s, int k) {
-		return longestSubstring(s,k,0,s.length());
-    }
-
-	int longestSubstring(const string& s,int k,int left,int right)
+	int longestSubstring(string s, int k)
 	{
-		if(left>=right||right-left<k)
+		return divide(s, k, 0, s.length());
+	}
+
+	int divide(const string &s, int k, int left, int right)
+	{
+		if (right - left < k)
 			return 0;
-		int hash[26]={0};
-		for(int i=left;i<right;i++)
-			hash[s[i]-'a']++;
-		for(int i=left;i<right;i++)
+		int hash[26] = {0};
+		for (int i = left; i < right; ++i)
+			++hash[s[i] - 'a'];
+		for (int i = left; i < right; ++i)
 		{
-			if(hash[s[i]-'a']<k)
-			{
-				return max(longestSubstring(s,k,left,i),longestSubstring(s,k,i+1,right));
-			}
+			if (hash[s[i] - 'a'] < k)
+				return max(divide(s, k, left, i), divide(s, k, i + 1, right));
 		}
-		return right-left;		
+		return right - left;
 	}
 };
