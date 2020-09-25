6a034c6b3b6c82327cf7b0a9a1f9ebf9f71346f7
JumHorn <JumHorn@gmail.com>
2020-09-25 09:30:12 +0800

    formatted

--- Cplus/LexicographicalNumbers.cpp
+++ Cplus/LexicographicalNumbers.cpp
@@ -1,27 +1,26 @@
-#include<vector>
-#include<string>
-#include<algorithm>
+#include <algorithm>
+#include <string>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> lexicalOrder(int n) {
-		vector<int> res(n);
-        int tmp=0;
-		lexicalOrder(res,1,n,tmp);
-		return res;
-    }
-
-	void lexicalOrder(vector<int>& res,int start,int n,int& index)
+	vector<int> lexicalOrder(int n)
 	{
-		if(start>n)
-			return;
-		for(int i=0;i<=9;i++)
+		vector<int> res;
+		dfs(n, 1, res);
+		return res;
+	}
+
+	void dfs(int n, int value, vector<int> &res)
+	{
+		for (int i = 0; i <= 9; ++i)
 		{
-            if(start+i>n||start%10+i==10)
-                break;
-            res[index++]=start+i;
-			lexicalOrder(res,(start+i)*10,n,index);
+			if (value + i > n || value % 10 + i >= 10)
+				break;
+			res.push_back(value + i);
+			dfs(n, (value + i) * 10, res);
 		}
 	}
 };
