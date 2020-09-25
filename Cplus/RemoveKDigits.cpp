1781d565fb1ca35b45175f55a6297d3eade5c841
JumHorn <JumHorn@gmail.com>
2020-09-25 23:10:18 +0800

    Remove K Digits beats 99.93%

--- Cplus/RemoveKDigits.cpp
+++ Cplus/RemoveKDigits.cpp
@@ -1,52 +1,27 @@
-#include<string>
-#include<stack>
+#include <string>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    string removeKdigits(string num, int k) {
-		if(k>=(int)num.size())
-			return "0";
-		int len=num.size()-k;
-		string res(len,' ');
-		stack<char> monoqueue;
-		int i=0;
-		monoqueue.push(num[i++]);
-		while(i<(int)num.size())
+	string removeKdigits(string num, int k)
+	{
+		string res;
+		for (auto c : num)
 		{
-			while(!monoqueue.empty()&&monoqueue.top()>num[i])
+			while (k > 0 && !res.empty() && res.back() > c)
 			{
-				monoqueue.pop();
-				k--;
-				if(k==0)
-				{
-					int s=monoqueue.size();
-					int t=s;
-					while(--s>=0)
-					{
-						res[s]=monoqueue.top();
-						monoqueue.pop();
-					}
-					while(i<(int)num.size())
-						res[t++]=num[i++];
-                    i=0;
-                    while(i<(int)res.size()-1&&res[i]=='0')
-                        ++i;
-					return res.substr(i);
-				}
+				res.pop_back();
+				--k;
 			}
-			monoqueue.push(num[i++]);
+			res.push_back(c);
 		}
-		while(--k>=0)
-			monoqueue.pop();
-		while(--len>=0)
-		{
-			res[len]=monoqueue.top();
-			monoqueue.pop();
-		}
-        i=0;
-        while(i<(int)res.size()-1&&res[i]=='0')
-            ++i;
-		return res.substr(i);
-    }
+		while (--k >= 0)
+			res.pop_back();
+		//trim leading zero
+		int i = 0;
+		while (i < (int)res.length() && res[i] == '0')
+			++i;
+		return i < (int)res.length() ? res.substr(i) : "0";
+	}
 };
