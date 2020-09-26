88289d44dbf179f9135a4283f71fe19b78d89465
JumHorn <JumHorn@gmail.com>
2020-09-26 23:22:39 +0800

    Sort Characters By Frequency beats 98.71%

--- Cplus/SortCharactersByFrequency.cpp
+++ Cplus/SortCharactersByFrequency.cpp
@@ -1,94 +1,22 @@
-#include<iostream>
-#include<string>
-#include<map>
-#include<vector>
+#include <algorithm>
+#include <string>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    string frequencySort(string s) {
-        map<char,int> hash;
-        vector<string> bucket(s.size()+1,"");
-        string res;
-		for(int i=0;i<s.length();i++)
-		{
-			hash[s[i]]++;
-		}
-
-		for(map<char,int>::iterator iter=hash.begin();iter!=hash.end();iter++)
-		{
-			bucket[iter->second].append(iter->second,iter->first);
-		}
-
-		for(int i=bucket.size()-1;i>=0;i--)
-		{
-			if(!bucket[i].empty())
-			{
-				res.append(bucket[i]);
-			}
-		}
-
+	string frequencySort(string s)
+	{
+		vector<pair<int, char>> hash(256); //{count,character}
+		for (int i = 0; i < 256; ++i)
+			hash[i].second = i;
+		for (auto c : s)
+			++hash[(int)c].first;
+		sort(hash.begin(), hash.end(), greater<pair<int, char>>());
+		string res;
+		for (int i = 0; i < 256; ++i)
+			res += string(hash[i].first, hash[i].second);
 		return res;
-    }
+	}
 };
-
-// time limit exceeded
-// class Solution0 {
-// public:
-//     string frequencySort(string s) {
-//         map<char,int> hash;
-// 		for(int i=0;i<s.length();i++)
-// 		{
-// 			if(hash.find(s[i])!=hash.end())
-// 			{
-// 				hash[s[i]]++;
-// 			}
-// 			else
-// 			{
-// 				hash[s[i]]=1;
-// 			}
-// 		}
-//		
-// 		bool swap=false;
-// 		int len=s.length();
-// 		while(true)
-// 		{
-// 			len--;
-// 			swap=false;
-// 			for(int i=0;i<len;i++)
-// 			{
-// 				if(hash[s[i]]<hash[s[i+1]])
-// 				{
-// 					char temp=s[i];
-// 					s[i]=s[i+1];
-// 					s[i+1]=temp;
-// 					swap=true;
-// 				}
-// 			}
-// 			if(swap==false)
-// 			{
-// 				break;
-// 			}
-// 		}
-// 		for(int i=0;i<s.length();i++)
-// 		{
-// 			for(int j=i-1;j>=0;j--)
-// 			{
-// 				if(hash[s[i]]!=hash[s[j]])
-// 				{
-// 					break;
-// 				}
-// 				if(s[i]==s[j])
-// 				{
-// 					if(i-j>1)
-// 					{
-// 						s.insert(j,1,s[i]);
-// 						s.erase(i+1,1);
-// 					}
-// 					break;
-// 				}
-// 			}
-// 		}
-// 		return s;
-//     }
-// };
