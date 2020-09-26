805ad7ed3b62cb9274d0910d72e713deb5e0d210
JumHorn <JumHorn@gmail.com>
2020-09-26 23:35:14 +0800

    Sort Characters By Frequency beats 69.23%

---
+++ C/SortCharactersByFrequency.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+
+//cmp function don't consider overflow
+int cmp(const void *lhs, const void *rhs)
+{
+	return *(int *)rhs - *(int *)lhs;
+}
+
+char *frequencySort(char *s)
+{
+	int hash[256][2]; //{count,character}
+	for (int i = 0; i < 256; ++i)
+	{
+		hash[i][0] = 0;
+		hash[i][1] = i;
+	}
+	for (int i = 0; s[i]; ++i)
+		++hash[(int)s[i]][0];
+	qsort(hash, 256, sizeof(hash[0]), cmp);
+	int size = 0;
+	for (int i = 0; i < 256; ++i)
+	{
+		memset(s + size, hash[i][1], sizeof(char) * hash[i][0]);
+		size += hash[i][0];
+	}
+	return s;
+}
