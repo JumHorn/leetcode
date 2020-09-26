ac5936e9233fea8fcf5e4dd8fbadf0d3854a6ad9
JumHorn <JumHorn@gmail.com>
2020-09-26 21:36:12 +0800

    Flatten a Multilevel Doubly Linked List beats 56.62%

--- Cplus/FlattenaMultilevelDoublyLinkedList.cpp
+++ Cplus/FlattenaMultilevelDoublyLinkedList.cpp
@@ -1,57 +1,59 @@
-#include<iostream>
-using namespace std;
+
 // Definition for a Node.
-class Node {
+class Node
+{
 public:
-    int val;
-    Node* prev;
-    Node* next;
-    Node* child;
+	int val;
+	Node *prev;
+	Node *next;
+	Node *child;
 
-    Node() {}
+	Node() {}
 
-    Node(int _val, Node* _prev, Node* _next, Node* _child) {
-        val = _val;
-        prev = _prev;
-        next = _next;
-        child = _child;
-    }
+	Node(int _val, Node *_prev, Node *_next, Node *_child)
+	{
+		val = _val;
+		prev = _prev;
+		next = _next;
+		child = _child;
+	}
 };
 
-class Solution {
+class Solution
+{
 public:
-    Node* flatten(Node* head) {
-        Node* node=head;
-		if(node==NULL)
-			return node;
-		while(node!=NULL)
+	Node *flatten(Node *head)
+	{
+		if (head == nullptr)
+			return head;
+		Node *node = head;
+		while (node != nullptr)
 		{
-			Node* tmp=node;
-			node=node->next;
-			flatten(tmp,node);
+			Node *tmp = node;
+			node = node->next;
+			flatten(tmp, node);
 		}
 		return head;
-    }
+	}
 
-	Node* flatten(Node* head,Node* tail)
+	void flatten(Node *head, Node *tail)
 	{
-		if(head->child==NULL)
-			return head;
-		head->next=head->child;
-		head->next->prev=head;
-		Node* tmp2,*tmp1=head->child;
-		head->child=NULL;
-		while(tmp1->next!=NULL)
+		if (head->child == nullptr)
+			return;
+		head->next = head->child;
+		head->child->prev = head;
+		Node *childhead, *childtail = head->child;
+		head->child = nullptr;
+		while (childtail != nullptr)
 		{
-			tmp2=tmp1;
-			tmp1=tmp1->next;
-			flatten(tmp2,tmp1);
+			childhead = childtail;
+			childtail = childtail->next;
+			flatten(childhead, childtail);
 		}
-        tmp2=flatten(tmp1,tmp1->next);
-		tmp2->next=tail;
-		if(tail==NULL)
-			return tmp2;
-		tail->prev=tmp2;
-		return tail;
+		while (childhead->next != nullptr)
+			childhead = childhead->next;
+		childhead->next = tail;
+		if (tail != nullptr)
+			tail->prev = childhead;
 	}
 };
