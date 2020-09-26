2c57aaac08775f3027e9f27b207232f08ef248e3
JumHorn <JumHorn@gmail.com>
2020-09-26 18:03:06 +0800

    memory optimized dp,however time limited exceeded

--- C/LongestRepeatingCharacterReplacement.c
+++ C/LongestRepeatingCharacterReplacement.c
@@ -5,26 +5,28 @@
 int characterReplacement(char *s, int k)
 {
 	int N = strlen(s), res = 0;
-	int dp[N + 1][26][k + 1];
-	memset(dp, 0, sizeof(dp));
+	int dp[26][k + 1], next_dp[26][k + 1];
+	memset(next_dp, 0, sizeof(next_dp));
 	for (int i = 0; i < N; ++i)
 	{
+		memcpy(dp, next_dp, sizeof(dp));
+		memset(next_dp, 0, sizeof(next_dp));
 		for (int j = 0; j < 26; ++j)
 		{
 			if (s[i] - 'A' != j)
 			{
 				for (int l = 0; l < k; ++l)
 				{
-					dp[i + 1][j][l + 1] = max(dp[i + 1][j][l + 1], dp[i][j][l] + 1);
-					res = max(res, dp[i + 1][j][l + 1]);
+					next_dp[j][l + 1] = max(next_dp[j][l + 1], dp[j][l] + 1);
+					res = max(res, next_dp[j][l + 1]);
 				}
 			}
 			else
 			{
 				for (int l = 0; l <= k; ++l)
 				{
-					dp[i + 1][j][l] = max(dp[i + 1][j][l], dp[i][j][l] + 1);
-					res = max(res, dp[i + 1][j][l]);
+					next_dp[j][l] = max(next_dp[j][l], dp[j][l] + 1);
+					res = max(res, next_dp[j][l]);
 				}
 			}
 		}
