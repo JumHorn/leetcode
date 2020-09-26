e2e505e3c474bb10ce809c98e12d18502cfbc65a
JumHorn <JumHorn@gmail.com>
2020-09-27 07:53:08 +0800

    Find Right Interval beats 40.05%

--- Cplus/FindRightInterval.cpp
+++ Cplus/FindRightInterval.cpp
@@ -1,5 +1,5 @@
 #include <algorithm>
-#include <queue>
+#include <map>
 #include <vector>
 using namespace std;
 
@@ -8,28 +8,18 @@ class Solution
 public:
 	vector<int> findRightInterval(vector<vector<int>> &intervals)
 	{
+		map<int, int> hash;
+		vector<int> res;
 		int n = intervals.size();
 		for (int i = 0; i < n; ++i)
-			intervals[i].push_back(i);
-		sort(intervals.begin(), intervals.end());
-		vector<int> res(n);
-		auto f = [&](int lhs, int rhs) { return intervals[lhs][1] > intervals[rhs][1]; };
-		priority_queue<int, vector<int>, decltype(f)> q(f);
-		if (!intervals.empty())
-			q.push(0);
-		for (int i = 1; i < n; ++i)
+			hash[intervals[i][0]] = i;
+		for (auto interval : intervals)
 		{
-			while (!q.empty() && intervals[q.top()][1] <= intervals[i][0])
-			{
-				res[intervals[q.top()][2]] = intervals[i][2];
-				q.pop();
-			}
-			q.push(i);
-		}
-		while (!q.empty())
-		{
-			res[intervals[q.top()][2]] = -1;
-			q.pop();
+			auto iter = hash.lower_bound(interval[1]);
+			if (iter == hash.end())
+				res.push_back(-1);
+			else
+				res.push_back(iter->second);
 		}
 		return res;
 	}
