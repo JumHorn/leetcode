f0cf523fd0af8fe91f2d1c0a3b7e8d925d4825bb
JumHorn <JumHorn@gmail.com>
2020-09-26 19:05:04 +0800

    formatted

--- Cplus/ConstructQuadTree.cpp
+++ Cplus/ConstructQuadTree.cpp
@@ -1,58 +1,80 @@
-#include<vector>
+#include <vector>
 using namespace std;
 
 // Definition for a QuadTree node.
-class Node {
+class Node
+{
 public:
-    bool val;
-    bool isLeaf;
-    Node* topLeft;
-    Node* topRight;
-    Node* bottomLeft;
-    Node* bottomRight;
+	bool val;
+	bool isLeaf;
+	Node *topLeft;
+	Node *topRight;
+	Node *bottomLeft;
+	Node *bottomRight;
 
-    Node() {}
+	Node()
+	{
+		val = false;
+		isLeaf = false;
+		topLeft = NULL;
+		topRight = NULL;
+		bottomLeft = NULL;
+		bottomRight = NULL;
+	}
 
-    Node(bool _val, bool _isLeaf, Node* _topLeft, Node* _topRight, Node* _bottomLeft, Node* _bottomRight) {
-        val = _val;
-        isLeaf = _isLeaf;
-        topLeft = _topLeft;
-        topRight = _topRight;
-        bottomLeft = _bottomLeft;
-        bottomRight = _bottomRight;
-    }
+	Node(bool _val, bool _isLeaf)
+	{
+		val = _val;
+		isLeaf = _isLeaf;
+		topLeft = NULL;
+		topRight = NULL;
+		bottomLeft = NULL;
+		bottomRight = NULL;
+	}
+
+	Node(bool _val, bool _isLeaf, Node *_topLeft, Node *_topRight, Node *_bottomLeft, Node *_bottomRight)
+	{
+		val = _val;
+		isLeaf = _isLeaf;
+		topLeft = _topLeft;
+		topRight = _topRight;
+		bottomLeft = _bottomLeft;
+		bottomRight = _bottomRight;
+	}
 };
 
-class Solution {
+class Solution
+{
 public:
-    Node* construct(vector<vector<int>>& grid) {
-		return construct(grid,0,0,grid.size(),grid.size());        
-    }
-
-	Node* construct(vector<vector<int>>& grid,int top,int left,int bottom,int right)
+	Node *construct(vector<vector<int>> &grid)
 	{
-		Node* root=new Node(true,false,NULL,NULL,NULL,NULL);
-		if(right-left<=1&&bottom-top<=1)
+		return construct(grid, 0, 0, grid.size(), grid[0].size());
+	}
+
+	Node *construct(vector<vector<int>> &grid, int top, int left, int bottom, int right)
+	{
+		Node *root = new Node();
+		if (right - left <= 1 && bottom - top <= 1)
 		{
-			root->isLeaf=true;
-			root->val=grid[top][left]==1;
+			root->isLeaf = true;
+			root->val = grid[top][left] == 1;
 			return root;
 		}
-		root->topLeft=construct(grid,top,left,top+(bottom-top)/2,left+(right-left)/2);
-		root->topRight=construct(grid,top,left+(right-left)/2,top+(bottom-top)/2,right);
-		root->bottomLeft=construct(grid,top+(bottom-top)/2,left,bottom,left+(right-left)/2);
-		root->bottomRight=construct(grid,top+(bottom-top)/2,left+(right-left)/2,bottom,right);
-		if(root->topLeft->isLeaf&&root->topRight->isLeaf&&root->bottomLeft->isLeaf&&root->bottomRight->isLeaf)
+		root->topLeft = construct(grid, top, left, top + (bottom - top) / 2, left + (right - left) / 2);
+		root->topRight = construct(grid, top, left + (right - left) / 2, top + (bottom - top) / 2, right);
+		root->bottomLeft = construct(grid, top + (bottom - top) / 2, left, bottom, left + (right - left) / 2);
+		root->bottomRight = construct(grid, top + (bottom - top) / 2, left + (right - left) / 2, bottom, right);
+		if (root->topLeft->isLeaf && root->topRight->isLeaf && root->bottomLeft->isLeaf && root->bottomRight->isLeaf)
 		{
-			if(root->topLeft->val==root->topRight->val&&root->topLeft->val==root->bottomLeft->val&&root->topLeft->val==root->bottomRight->val)
+			if (root->topLeft->val == root->topRight->val && root->topLeft->val == root->bottomLeft->val && root->topLeft->val == root->bottomRight->val)
 			{
-				root->isLeaf=true;
-				root->val=root->topLeft->val;
-				root->topLeft=root->topRight=root->bottomLeft=root->bottomRight=NULL;
+				root->isLeaf = true;
+				root->val = root->topLeft->val;
+				root->topLeft = root->topRight = root->bottomLeft = root->bottomRight = NULL;
 				return root;
 			}
 		}
-		root->isLeaf=false;
+		root->isLeaf = false;
 		return root;
 	}
 };
