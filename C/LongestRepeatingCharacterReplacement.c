ff755f72fba103c90f4af6d9a379404a26d9a3a8
JumHorn <JumHorn@gmail.com>
2020-09-26 18:16:27 +0800

    Longest Repeating Character Replacement beats 27.27%

--- C/LongestRepeatingCharacterReplacement.c
+++ C/LongestRepeatingCharacterReplacement.c
@@ -1,35 +1,43 @@
+#include <stdbool.h>
 #include <string.h>
 
 #define max(a, b) (((a) > (b)) ? (a) : (b))
 
+bool check(char *s, int k, int window)
+{
+	int hash[26] = {0}, maxcount = 0;
+	for (int i = 0; i < window; ++i)
+		++hash[s[i] - 'A'];
+	for (int i = 0; i < 26; ++i)
+		maxcount = max(maxcount, hash[i]);
+	if (window - maxcount <= k)
+		return true;
+	for (int i = window; s[i]; ++i)
+	{
+		++hash[s[i] - 'A'];
+		--hash[s[i - window] - 'A'];
+		maxcount = 0;
+		for (int i = 0; i < 26; ++i)
+			maxcount = max(maxcount, hash[i]);
+		if (window - maxcount <= k)
+			return true;
+	}
+	return false;
+}
+
 int characterReplacement(char *s, int k)
 {
-	int N = strlen(s), res = 0;
-	int dp[26][k + 1], next_dp[26][k + 1];
-	memset(next_dp, 0, sizeof(next_dp));
-	for (int i = 0; i < N; ++i)
+	int N = strlen(s);
+	int lo = 0, hi = N;
+	while (lo < hi)
 	{
-		memcpy(dp, next_dp, sizeof(dp));
-		memset(next_dp, 0, sizeof(next_dp));
-		for (int j = 0; j < 26; ++j)
-		{
-			if (s[i] - 'A' != j)
-			{
-				for (int l = 0; l < k; ++l)
-				{
-					next_dp[j][l + 1] = max(next_dp[j][l + 1], dp[j][l] + 1);
-					res = max(res, next_dp[j][l + 1]);
-				}
-			}
-			else
-			{
-				for (int l = 0; l <= k; ++l)
-				{
-					next_dp[j][l] = max(next_dp[j][l], dp[j][l] + 1);
-					res = max(res, next_dp[j][l]);
-				}
-			}
-		}
+		int mi = (hi - lo) / 2 + lo;
+		if (check(s, k, mi))
+			lo = mi + 1;
+		else
+			hi = mi;
 	}
-	return res;
+	if (!check(s, k, lo))
+		--lo;
+	return lo;
 }
