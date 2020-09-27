0e701b2deeb38de28f9737069395511f25aefd4b
JumHorn <JumHorn@gmail.com>
2020-09-27 16:35:19 +0800

    Add Two Numbers II beats 86.20%

--- Cplus/AddTwoNumbersII.cpp
+++ Cplus/AddTwoNumbersII.cpp
@@ -1,63 +1,59 @@
-#include<iostream>
-#include<vector>
-using namespace std;
+
 // Definition for singly-linked list.
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+struct ListNode
+{
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-class Solution {
+class Solution
+{
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        vector<int> n1,n2;
-	    while(l1!=NULL)
+	ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+	{
+		int size1 = getListSize(l1), size2 = getListSize(l2);
+		if (size1 >= size2)
+			l1 = recursive(l1, size1, l2, size2);
+		else
+			l1 = recursive(l2, size2, l1, size1);
+		if (l1->val < 10)
+			return l1;
+		ListNode *res = new ListNode(1);
+		l1->val %= 10;
+		res->next = l1;
+		return res;
+	}
+
+	ListNode *recursive(ListNode *l1, int size1, ListNode *l2, int size2) //size1 must be bigger than size2
+	{
+		if (size1 == 0)
+			return nullptr;
+		if (size1 > size2)
+			l1->next = recursive(l1->next, size1 - 1, l2, size2);
+		else
 		{
-			n1.push_back(l1->val);
-			l1=l1->next;
+			l1->next = recursive(l1->next, size1 - 1, l2->next, size2 - 1);
+			l1->val += l2->val;
 		}
-	    while(l2!=NULL)
+		if (l1->next)
 		{
-			n2.push_back(l2->val);
-			l2=l2->next;
+			l1->val += l1->next->val / 10;
+			l1->next->val %= 10;
 		}
-		ListNode* head=NULL;
-		ListNode* tmp=NULL;
-		int bit=0;
-		int n,n3,n4;
-		while(!n1.empty()||!n2.empty())
+		return l1;
+	}
+
+	int getListSize(ListNode *head)
+	{
+		int res = 0;
+		while (head)
 		{
-			if(!n1.empty())
-			{
-				n3=n1.back();
-				n1.pop_back();
-			}
-			else
-			{
-				n3=0;
-			}
-			if(!n2.empty())
-			{
-				n4=n2.back();
-				n2.pop_back();
-			}
-			else
-			{
-				n4=0;
-			}
-			n=n3+n4+bit;
-			tmp=new ListNode(n%10);
-			tmp->next=head;
-			head=tmp;	
-			bit=n/10;
+			++res;
+			head = head->next;
 		}
-		if(bit==1)
-		{
-			tmp=new ListNode(bit);
-			tmp->next=head;
-			head=tmp;
-		}
-		return head;
-    }
+		return res;
+	}
 };
