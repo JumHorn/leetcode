a57e28caf49c4d14ed80883f0ac258fec4a5e348
JumHorn <JumHorn@gmail.com>
2020-09-27 22:30:01 +0800

    Implement Rand10() Using Rand7() beats 100%

--- Cplus/ImplementRand10UsingRand7.cpp
+++ Cplus/ImplementRand10UsingRand7.cpp
@@ -1,5 +1,6 @@
-#include<vector>
+#include <vector>
 using namespace std;
+
 // The rand7() API is already defined for you.
 int rand7();
 // @return a random integer in the range 1 to 7
@@ -16,23 +17,26 @@ int rand7();
 7  *  *  *  *  *  *  *
 */
 
-class Solution {
-	vector<vector<int> > quard;
+class Solution
+{
+
 public:
-	Solution():quard(7,vector<int>(7))
+	Solution() : quard(7, vector<int>(7))
 	{
-		for(int i=0;i<4;i++)
-		{
-			for(int j=0;j<10;j++)
-			{
-				int tmp=i*10+j;
-				quard[tmp/7][tmp%7]=j+1;
-			}
-		}
+		for (int i = 0; i < 40; i++)
+			quard[i / 7][i % 7] = i % 10 + 1;
 	}
-    int rand10() {
+
+	int rand10()
+	{
 		int res;
-		while((res=quard[rand7()-1][rand7()-1])==0);
+		do
+		{
+			res = quard[rand7() - 1][rand7() - 1];
+		} while (res == 0);
 		return res;
-    }
+	}
+
+private:
+	vector<vector<int>> quard;
 };
