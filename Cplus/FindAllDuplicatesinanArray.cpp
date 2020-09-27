68cf25a77451187b5c61ffc3175737d4f15b255b
JumHorn <JumHorn@gmail.com>
2020-09-27 09:52:48 +0800

    Find All Duplicates in an Array beats 30.00%

--- Cplus/FindAllDuplicatesinanArray.cpp
+++ Cplus/FindAllDuplicatesinanArray.cpp
@@ -1,30 +1,30 @@
-#include<iostream>
-#include<vector>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> findDuplicates(vector<int>& nums) {
-        vector<int> result;
-        int temp;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]>0)
-            {
-                temp=nums[i]-1;
-                if(nums[temp]==0)
-                {
-                    result.push_back(nums[i]);
-                    nums[temp]=-1;
-                }
-                else if(nums[temp]>0)
-                {
-                    nums[i]=nums[temp];
-                    nums[temp]=0;
-                    i--;
-                }
-            }
-        }
-        return result;
-    }
+	vector<int> findDuplicates(vector<int> &nums)
+	{
+		vector<int> res;
+		for (int i = 0; i < (int)nums.size(); ++i)
+		{
+			int index = nums[i] - 1;
+			if (nums[i] > 0)
+			{
+				if (nums[index] == 0)
+				{
+					res.push_back(nums[i]);
+					nums[i] = nums[index] = -1;
+				}
+				else
+				{
+					nums[i] = nums[index];
+					nums[index] = 0;
+					--i; //still process from i
+				}
+			}
+		}
+		return res;
+	}
 };
