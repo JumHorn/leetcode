de63a921f9ce26557d07c3b08ac97159c908b075
JumHorn <JumHorn@gmail.com>
2020-09-27 19:14:24 +0800

    Delete Node in a BST beats 16.14%

--- Cplus/DeleteNodeinaBST.cpp
+++ Cplus/DeleteNodeinaBST.cpp
@@ -1,87 +1,39 @@
-#include<iostream>
-using namespace std;
-//Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-class Solution {
+class Solution
+{
 public:
-    TreeNode* deleteNode(TreeNode* root, int key) {
-        if(root==NULL)
-		   return root;
-		TreeNode* tmp=root;
-		if(root->val==key)
+	TreeNode *deleteNode(TreeNode *root, int key)
+	{
+		if (root == nullptr)
+			return nullptr;
+		if (root->val == key)
 		{
-			insertNode(root->left,root->right);
-			root=(root->left!=NULL?root->left:root->right);
-			delete tmp;
-			return root;
+			if (root->right == nullptr)
+				root = root->left;
+			else
+			{
+				TreeNode **node = &root->right;
+				while ((*node)->left)
+					node = &(*node)->left;
+				root->val = (*node)->val;
+				*node = (*node)->right;
+			}
 		}
-		if(root->val>key)
-			deleteNode(root,root,root->left,key);
+		else if (root->val > key)
+			root->left = deleteNode(root->left, key);
 		else
-			deleteNode(root,root,root->right,key);
+			root->right = deleteNode(root->right, key);
 		return root;
-    }
-	
-	void deleteNode(TreeNode* root,TreeNode* p,TreeNode* node,int key)
-	{
-		if(node==NULL)
-			return;
-		if(node->val==key)
-		{
-			TreeNode* left=node->left;
-			TreeNode* right=node->right;
-			if(p->left==node)
-			{
-				p->left=NULL;
-				delete node;
-			}
-			else
-			{
-				p->right=NULL;
-				delete node;
-			}
-			insertNode(root,left);
-			insertNode(root,right);
-		}
-		else if(node->val>key)
-		{
-			if(node->left!=NULL)
-			{
-				deleteNode(root,node,node->left,key);
-			}
-		}
-		else
-		{
-			if(node->right!=NULL)
-			{
-				deleteNode(root,node,node->right,key);
-			}
-		}
-	}
-
-	void insertNode(TreeNode* root,TreeNode* node)
-	{
-		if(root==NULL||node==NULL)
-			return;
-		if(root->val>node->val)
-		{
-			if(root->left==NULL)
-				root->left=node;
-			else
-				insertNode(root->left,node);
-		}
-		else
-		{
-			if(root->right==NULL)
-				root->right=node;
-			else
-				insertNode(root->right,node);
-		}
 	}
 };
