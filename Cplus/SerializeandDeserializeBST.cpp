77dc68135d3a7ad3f125e9b77986847e4d348f52
JumHorn <JumHorn@gmail.com>
2020-09-27 16:53:22 +0800

    Serialize and Deserialize BST beats 96.20%

--- Cplus/SerializeandDeserializeBST.cpp
+++ Cplus/SerializeandDeserializeBST.cpp
@@ -1,77 +1,51 @@
-#include<iostream>
-#include<string>
-#include<sstream>
-#include<stack>
+#include <sstream>
+#include <stack>
+#include <string>
 using namespace std;
+
 //Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+struct TreeNode
+{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-/*
-		1
-	2		3
-	  4
-E1E2NE4NNE3NN
-*/
-class Codec {
+class Codec
+{
 public:
-
-    // Encodes a tree to a single string.
-    string serialize(TreeNode* root) {
-		if(root==NULL)
+	// Encodes a tree to a single string.
+	string serialize(TreeNode *root)
+	{
+		if (root == NULL)
 			return "N";
-		return "E"+to_string(root->val)+serialize(root->left)+serialize(root->right);	
-    }
+		return to_string(root->val) + "," + serialize(root->left) + "," + serialize(root->right);
+	}
 
-    // Decodes your encoded data to tree.
-    TreeNode* deserialize(string data) {
-       	stringstream ss(data); 
-		int value;
-		char exist;
-		ss>>exist;
-		if(exist=='N')
-			return NULL;
-		ss>>value;
-		TreeNode* node=new TreeNode(value);
-		stack<TreeNode*> s;
-		s.push(node);
-		TreeNode* tmp;
-		bool lr=true;
-		ss>>exist;
-		while(!ss.eof())
+	// Decodes your encoded data to tree.
+	TreeNode *deserialize(string data)
+	{
+		stringstream ss(data);
+		return deserialize(ss);
+	}
+
+	TreeNode *deserialize(stringstream &data)
+	{
+		int val;
+		char dot;
+		if (data.peek() == 'N')
 		{
-			tmp=s.top();
-			if(exist=='E')
-			{
-				ss>>value;
-				if(lr)
-				{
-					tmp->left=new TreeNode(value);
-					s.push(tmp->left);
-				}
-				else
-				{
-					tmp->right=new TreeNode(value);
-					s.pop();
-					s.push(tmp->right);
-				}
-				lr=true;
-			}
-			else
-			{
-				if(lr)
-					lr=false;
-				else
-					s.pop();
-			}
-			ss>>exist;
+			data >> dot >> dot;
+			return nullptr;
 		}
-		return node;
-    }
+		data >> val;
+		data >> dot; //ignore .
+		TreeNode *root = new TreeNode(val);
+		root->left = deserialize(data);
+		root->right = deserialize(data);
+		return root;
+	}
 };
 
 // Your Codec object will be instantiated and called as such:
