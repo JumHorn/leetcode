773db7976e48eadb9442c1a007e674a326be60b7
JumHorn <JumHorn@gmail.com>
2020-09-27 10:28:01 +0800

    String Compression beats 45.05%

--- Cplus/StringCompression.cpp
+++ Cplus/StringCompression.cpp
@@ -1,55 +1,33 @@
-#include<iostream>
-#include<vector>
+#include <string>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int compress(vector<char>& chars) {
-        if(chars.empty())
-        {
-            return 0;
-        }
-        int result=0,sum=1,temp=0;
-        for(int i=1;i<chars.size();i++)
-        {
-            if(chars[temp]==chars[i])
-            {
-                sum++;
-            }
-            else
-            {
-                if(sum==1)
-                {
-                    result++;
-                }
-                else 
-                {
-                    result = result+to_string(sum).size()+1;
-                    string s=to_string(sum);
-                    int j;
-                    for(j=0;j<s.size();j++)
-                    {
-                        chars[temp+1+j]=s[j];
-                    }
-                    chars.erase(chars.begin()+temp+j+1,chars.begin()+i);
-                    i=temp+j+1;
-                }
-                temp=i;
-                sum=1;
-            }
-        }
-        if(sum==1)
-        {
-            return result+1;
-        }
-        else 
-        {
-            string s=to_string(sum);
-            for(int j=0;j<s.size();j++)
-            {
-                chars[temp+1+j]=s[j];
-            }
-            return result+to_string(sum).size()+1;
-        }
-    }
+	int compress(vector<char> &chars)
+	{
+		if (chars.empty())
+			return 0;
+		char pre = chars[0];
+		int N = chars.size(), res = 0;
+		for (int i = 1, j = 0; i <= N; ++i)
+		{
+			if (i == N || chars[i] != pre)
+			{
+				int count = i - j;
+				chars[res++] = pre;
+				if (count > 1)
+				{
+					string str = to_string(count);
+					for (auto c : str)
+						chars[res++] = c;
+				}
+				j = i;
+			}
+			if (i < N)
+				pre = chars[i];
+		}
+		return res;
+	}
 };
