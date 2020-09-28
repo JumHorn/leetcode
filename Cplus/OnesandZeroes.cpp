e6a149e632a14cc23338b9c9c2784346bfddf262
JumHorn <JumHorn@gmail.com>
2020-09-28 17:31:43 +0800

    Ones and Zeroes beats 26.40%

--- Cplus/OnesandZeroes.cpp
+++ Cplus/OnesandZeroes.cpp
@@ -1,24 +1,34 @@
-#include<vector>
-#include<string>
-#include<algorithm>
-#include<iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int findMaxForm(vector<string>& strs, int m, int n) {
-		if(strs.empty())
+	int findMaxForm(vector<string> &strs, int m, int n)
+	{
+		if (strs.empty())
 			return 0;
-		vector<vector<int> > dp(m+1,vector<int>(n+1));
-		vector<vector<int> > binstr(strs.size(),vector<int>(2));
-		for(int i=0;i<(int)strs.size();i++)
-			for(int j=0;j<(int)strs[i].size();j++)
-				strs[i][j]=='1'? binstr[i][1]++:binstr[i][0]++;
-        for(int k=0;k<(int)binstr.size();k++)
-            for(int i=m;i>=0;i--)
-                for(int j=n;j>=0;j--)
-                    if(i>=binstr[k][0]&&j>=binstr[k][1])
-                        dp[i][j]=max(dp[i][j],1+dp[i-binstr[k][0]][j-binstr[k][1]]);
-		return dp.back().back();
-    }
+		int N = strs.size();
+		vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+		vector<vector<int>> binstr(N, vector<int>(2));
+		for (int i = 0; i < N; ++i)
+		{
+			for (int j = 0; j < (int)strs[i].size(); ++j)
+				++binstr[i][strs[i][j] - '0'];
+		}
+		for (int k = 0; k < N; ++k)
+		{
+			for (int i = m; i >= 0; --i)
+			{
+				for (int j = n; j >= 0; --j)
+				{
+					if (i >= binstr[k][0] && j >= binstr[k][1])
+						dp[i][j] = max(dp[i][j], 1 + dp[i - binstr[k][0]][j - binstr[k][1]]);
+				}
+			}
+		}
+		return dp[m][n];
+	}
 };
