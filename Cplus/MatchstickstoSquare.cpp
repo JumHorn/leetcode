c82ac62044ba013b59805ab33aaa97f51d912e66
JumHorn <JumHorn@gmail.com>
2020-09-28 15:21:51 +0800

    Matchsticks to Square beats 32.54%

--- Cplus/MatchstickstoSquare.cpp
+++ Cplus/MatchstickstoSquare.cpp
@@ -1,35 +1,44 @@
-#include<vector>
-#include<numeric>
+#include <numeric>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool makesquare(vector<int>& nums) {
-        if(nums.empty())
-            return false;
-        int sum=accumulate(nums.begin(),nums.end(),0);
-		if(sum%4!=0)
-			return false;
-		int matchlen=sum/4;
-		vector<int> square(4);
-		square[0]=nums.front();
-		return makesquare(nums,matchlen,square,1);
-    }
-
-	bool makesquare(vector<int>& nums,int matchlen,vector<int>& square,int start)
+	bool makesquare(vector<int> &nums)
 	{
-		if(start==(int)nums.size())
-			return square[0]==matchlen&&square[1]==matchlen&&square[2]==matchlen&&square[3]==matchlen;
-		for(int j=0;j<4;j++)
+		if (nums.empty())
+			return false;
+		int perimeter = accumulate(nums.begin(), nums.end(), 0);
+		if (perimeter % 4 != 0)
+			return false;
+		int sidelen = perimeter / 4;
+		vector<unordered_map<int, bool>> dp(1 << nums.size());
+		return backTracking(nums, sidelen, perimeter, 0, dp);
+	}
+
+	bool backTracking(vector<int> &nums, int sidelen, int perimeter, int mask, vector<unordered_map<int, bool>> &dp)
+	{
+		if (perimeter == 0)
+			return true;
+		//not finish side len
+		if (dp[mask].find(perimeter) != dp[mask].end())
+			return dp[mask][perimeter];
+		int len = perimeter - perimeter / sidelen * sidelen;
+		if (len == 0)
+			len = sidelen;
+		for (int i = 0; i < (int)nums.size(); ++i)
 		{
-			if(square[j]+nums[start]<=matchlen)
+			if ((mask & (1 << i)) == 0)
 			{
-				square[j]+=nums[start];
-				if(makesquare(nums,matchlen,square,start+1))
-					return true;
-				square[j]-=nums[start];
+				if (nums[i] <= len)
+				{
+					if (backTracking(nums, sidelen, perimeter - nums[i], mask ^ (1 << i), dp))
+						return dp[mask][perimeter] = true;
+				}
 			}
 		}
-		return false;
+		return dp[mask][perimeter] = false;
 	}
 };
