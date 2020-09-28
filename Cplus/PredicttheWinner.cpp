d029269d515b2b56c7f69f5a77adcac383ac6ad3
JumHorn <JumHorn@gmail.com>
2020-09-28 22:04:50 +0800

    formatted

--- Cplus/PredicttheWinner.cpp
+++ Cplus/PredicttheWinner.cpp
@@ -1,19 +1,21 @@
-#include<vector>
-#include<numeric>
-#include<algorithm>
+#include <algorithm>
+#include <numeric>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool PredictTheWinner(vector<int>& nums) {
-        vector<vector<int> > dp(nums.size(),vector<int>(nums.size()));
-		for(int i=0;i<nums.size();i++)
-			dp[i][i]=nums[i];
-		for(int i=1;i<nums.size();i++)
-			for(int j=0;j<int(nums.size()-i);j++)
-			{
-				dp[j][j+i]=max(nums[j]+accumulate(nums.begin()+j+1,nums.begin()+j+i+1,0)-dp[j+1][j+i],nums[j+i]+accumulate(nums.begin()+j,nums.begin()+j+i,0)-dp[j][j+i-1]);
-			}
-		return dp.front().back()>=accumulate(nums.begin(),nums.end(),0)/2.0;
-    }
+	bool PredictTheWinner(vector<int> &nums)
+	{
+		int N = nums.size();
+		vector<vector<int>> dp(N + 1, vector<int>(N));
+		for (int i = N - 1; i >= 0; --i)
+		{
+			dp[i][i] = nums[i];
+			for (int j = i + 1; j < N; ++j)
+				dp[i][j] = max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+		}
+		return dp[0][N - 1] >= 0;
+	}
 };
