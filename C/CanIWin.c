63d3cf3a2e580b695681d82a329fe8b5fdf0c47b
JumHorn <JumHorn@gmail.com>
2020-09-28 13:37:10 +0800

    Can I Win beats 100%

---
+++ C/CanIWin.c
@@ -0,0 +1,47 @@
+#include <stdbool.h>
+
+/*
+TSP DP problem
+*/
+
+bool canIWin(int maxChoosableInteger, int desiredTotal)
+{
+	int M = maxChoosableInteger, T = desiredTotal;
+	if (T <= 1)
+		return true;
+	if (M * (M + 1) / 2 < T)
+		return false;
+	if (M * (M + 1) / 2 == T) //who choose last one wins
+		return M & 1;
+	int dp[1 << M]; // 0 -- invalid; 1 -- lose; 2 -- win
+	dp[0] = 0;
+	//mask 0 bit already choosed 1 bit not choosed yet
+	for (int mask = 1; mask < 1 << M; ++mask)
+	{
+		int choosed = 0;
+		for (int bit = 0; bit < M; ++bit)
+		{
+			if (((1 << bit) & mask) == 0)
+				choosed += bit + 1;
+		}
+		if (choosed >= desiredTotal) //already game over
+		{
+			dp[mask] = 0; //invalid state
+			continue;
+		}
+		dp[mask] = 1;
+		for (int bit = 0; bit < M; ++bit)
+		{
+			if (((1 << bit) & mask) != 0)
+			{
+				int pre = (mask ^ (1 << bit));
+				if (choosed + bit + 1 >= desiredTotal || dp[pre] == 1)
+				{
+					dp[mask] = 2;
+					break;
+				}
+			}
+		}
+	}
+	return dp[(1 << M) - 1] == 2;
+}
