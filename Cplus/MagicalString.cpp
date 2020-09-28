90c862fa12417d4d97021f27d208bc066ed16189
JumHorn <JumHorn@gmail.com>
2020-09-28 18:06:41 +0800

    formatted

--- Cplus/MagicalString.cpp
+++ Cplus/MagicalString.cpp
@@ -11,32 +11,25 @@ public:
 		if (n <= 3)
 			return 1;
 		n -= 3;
-		queue<int> q;
+		queue<int> q; //number of count
 		q.push(2);
-		int top = 2, res = 1;
+		int elements = 2, res = 1;
 		while (--n >= 0)
 		{
-			int tmp = q.front();
+			int count = q.front();
 			q.pop();
-			top = 3 - top;
-			if (tmp == 2)
+			elements = 3 - elements;
+			if (count == 2)
 			{
-				q.push(top);
-				if (top == 1)
-					res++;
+				q.push(elements);
+				if (elements == 1)
+					++res;
 				--n;
 			}
-			q.push(top);
-			if (top == 1 && n >= 0)
-				res++;
+			q.push(elements);
+			if (elements == 1 && n >= 0)
+				++res;
 		}
 		return res;
 	}
 };
-
-int main()
-{
-	Solution sol;
-	sol.magicalString(10);
-	return 0;
-}
