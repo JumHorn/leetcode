ab4be621ea385ecfdb2f29b0428a17a4485aac43
JumHorn <JumHorn@gmail.com>
2020-09-28 19:47:46 +0800

    Most Frequent Subtree Sum beats 90.36%

--- Cplus/MostFrequentSubtreeSum.cpp
+++ Cplus/MostFrequentSubtreeSum.cpp
@@ -1,58 +1,44 @@
-#include<iostream>
-#include<vector>
-#include<map>
-#include<algorithm>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 
 // Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+struct TreeNode
+{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-class Solution {
+class Solution
+{
 public:
-    map<int,int> frequence;
-    vector<int> findFrequentTreeSum(TreeNode* root) {
-        vector<int> result;
-        int maxfreq=0;
-        nodesum(root);
-        for(map<int,int>::iterator iter=frequence.begin();iter!=frequence.end();iter++)
-        {
-            if(iter->second>maxfreq)
-            {
-                maxfreq=iter->second;
-            }
-        }
-        for(map<int,int>::iterator iter=frequence.begin();iter!=frequence.end();iter++)
-        {
-            if(iter->second==maxfreq)
-            {
-                result.push_back(iter->first);
-            }
-        }        
-        return result;
-    }
+	vector<int> findFrequentTreeSum(TreeNode *root)
+	{
+		unordered_map<int, int> m; //{value,freq}
+		postorder(root, m);
+		int freq = -1;
+		for (auto n : m)
+			freq = max(freq, n.second);
+		vector<int> res;
+		for (auto n : m)
+		{
+			if (n.second == freq)
+				res.push_back(n.first);
+		}
+		return res;
+	}
 
-    void nodesum(TreeNode* root)
-    {
-        if(!root)
-        {
-            return;
-        }
-        nodesum(root->left);
-        nodesum(root->right);
-
-        root->val+=(root->left?root->left->val:0)+(root->right?root->right->val:0);
-        if(frequence.count(root->val)==0)
-        {
-            frequence[root->val]=1;
-        }
-        else
-        {
-            frequence[root->val]++;
-        }
-    }
+	int postorder(TreeNode *root, unordered_map<int, int> &m)
+	{
+		if (root == nullptr)
+			return 0;
+		int l = postorder(root->left, m);
+		int r = postorder(root->right, m);
+		++m[l + r + root->val];
+		return l + r + root->val;
+	}
 };
