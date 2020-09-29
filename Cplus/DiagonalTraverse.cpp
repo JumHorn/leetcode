105a7877a4a7017519ab1d7c9759460b989f7966
JumHorn <JumHorn@gmail.com>
2020-09-29 11:12:49 +0800

    Diagonal Traverse beats 94.29%

--- Cplus/DiagonalTraverse.cpp
+++ Cplus/DiagonalTraverse.cpp
@@ -1,64 +1,36 @@
-#include<vector>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> findDiagonalOrder(vector<vector<int>>& matrix) {
-		if(matrix.size()==0)
-			return vector<int>();
-		vector<int> res(matrix.size()*matrix[0].size());
-		int i=0,j=0,index=0;
-		bool dir=true;
-		while(true)
+	vector<int> findDiagonalOrder(vector<vector<int>> &matrix)
+	{
+		if (matrix.empty() || matrix[0].empty())
+			return {};
+		int M = matrix.size(), N = matrix[0].size();
+		vector<int> res(M * N);
+		int i = 0, j = 0;
+		int direction = 1;
+		int path[][2][2] = {{{1, 0}, {0, 1}}, {}, {{0, 1}, {1, 0}}};
+		for (int k = 0; k < M * N; ++k)
 		{
-			res[index++]=matrix[i][j];
-			if(i==matrix.size()-1&&j==matrix[0].size()-1)
-				break;
-			if(dir)//up-right
+			res[k] = matrix[i][j];
+			int dx = i - direction, dy = j + direction;
+			if (dx < 0 || dx >= M || dy < 0 || dy >= N)
 			{
-				while(true)
-				if(j==matrix[0].size()-1)
-				{
-					dir=false;
-					i++;
-					break;
-				}
-				else if(i==0)
-				{
-					dir=false;
-					j++;
-					break;
-				}
-				else
-				{
-					i--;
-					j++;
-					res[index++]=matrix[i][j];
-				}
-			}	
-			else//down-left
-			{
-				while(true)
-				if(i==matrix.size()-1)
-				{
-					dir=true;
-					j++;
-					break;
-				}
-				else if(j==0)
-				{
-					dir=true;
-					i++;
-					break;
-				}
-				else
-				{
-					i++;
-					j--;
-					res[index++]=matrix[i][j];
-				}
+				dx = i + path[1 + direction][0][0];
+				dy = j + path[1 + direction][0][1];
+				direction = -direction;
 			}
+			if (dx < 0 || dx >= M || dy < 0 || dy >= N)
+			{
+				dx = i + path[1 - direction][1][0];
+				dy = j + path[1 - direction][1][1];
+			}
+			i = dx;
+			j = dy;
 		}
 		return res;
-    }
+	}
 };
