0d79e08c09ebe39249c8a52e2b8cd2784c775adb
JumHorn <JumHorn@gmail.com>
2020-09-29 22:21:28 +0800

    Complex Number Multiplication beats 100%

--- Cplus/ComplexNumberMultiplication.cpp
+++ Cplus/ComplexNumberMultiplication.cpp
@@ -1,27 +1,24 @@
-#include<iostream>
-#include<string>
+#include <string>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int m1,n1,m2,n2;
-    string complexNumberMultiply(string a, string b) {
-        int m,n;
-        string result;
-        decompose(a,m1,n1);
-        decompose(b,m2,n2);
-        m=m1*m2-n1*n2;
-        n=m1*n2+m2*n1;
-        result=to_string(m)+"+"+to_string(n)+"i";
-        return result;
-    }
+	string complexNumberMultiply(string a, string b)
+	{
+		int m1, n1, m2, n2;
+		decompose(a, m1, n1);
+		decompose(b, m2, n2);
+		int m = m1 * m2 - n1 * n2;
+		int n = m1 * n2 + m2 * n1;
+		return to_string(m) + "+" + to_string(n) + "i";
+	}
 
-    void decompose(string c,int &m,int &n)
-    {
-        string left;
-        size_t sz;
-        m=stoi(c,&sz);
-        left=c.substr(sz+1);
-        n=stoi(left,&sz);
-    }
+	void decompose(const string &c, int &m, int &n) const
+	{
+		size_t size;
+		m = stoi(c, &size);
+		string left = c.substr(size + 1);
+		n = stoi(left, &size);
+	}
 };
