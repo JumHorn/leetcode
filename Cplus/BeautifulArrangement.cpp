5143ea36cb70f03ca023afcfd72c0aa9b499dd50
JumHorn <JumHorn@gmail.com>
2020-09-30 07:20:19 +0800

    Beautiful Arrangement beats 87.86%

---
+++ Cplus/BeautifulArrangement.cpp
@@ -0,0 +1,29 @@
+#include <unordered_map>
+using namespace std;
+
+class Solution
+{
+public:
+	int countArrangement(int N)
+	{
+		unordered_map<int, int> hash, next_hash; //{mask,count}
+		hash[0] = 1;
+		for (int i = 1; i <= N; ++i)
+		{
+			next_hash.clear();
+			for (int j = 1; j <= N; ++j)
+			{
+				if (i % j == 0 || j % i == 0)
+				{
+					for (auto &iter : hash)
+					{
+						if ((iter.first & (1 << (j - 1))) == 0)
+							next_hash[iter.first | (1 << (j - 1))] += hash[iter.first];
+					}
+				}
+			}
+			hash.swap(next_hash);
+		}
+		return hash[(1 << N) - 1];
+	}
+};
