c967417809147ee6ff32c2ef37847b3a341a19a6
JumHorn <JumHorn@gmail.com>
2020-09-29 08:30:29 +0800

    Find Largest Value in Each Tree Row beats 97.22%

--- Cplus/FindLargestValueinEachTreeRow.cpp
+++ Cplus/FindLargestValueinEachTreeRow.cpp
@@ -1,41 +1,36 @@
-#include<iostream>
-#include<vector>
+#include <vector>
 using namespace std;
-//Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> result;
-    vector<int> largestValues(TreeNode* root) {
-        CompareValues(root,1);
-        return result;
-    }
+	vector<int> largestValues(TreeNode *root)
+	{
+		vector<int> res;
+		preorder(root, 0, res);
+		return res;
+	}
 
-    void CompareValues(TreeNode* node,int depth)
-    {
-        if(!node)
-        {
-            return;
-        }
-        //processing
-        if(result.size()>=depth)
-        {
-            if(result[depth-1]<node->val)
-            {
-                result[depth-1]=node->val;
-            }
-        }
-        else
-        {
-            result.push_back(node->val);
-        }
-        CompareValues(node->left,depth+1);
-        CompareValues(node->right,depth+1);
-    }
+	void preorder(TreeNode *root, int layer, vector<int> &res)
+	{
+		if (root == nullptr)
+			return;
+		if (layer >= (int)res.size())
+			res.push_back(root->val);
+		else if (root->val > res[layer])
+			res[layer] = root->val;
+		preorder(root->left, layer + 1, res);
+		preorder(root->right, layer + 1, res);
+	}
 };
