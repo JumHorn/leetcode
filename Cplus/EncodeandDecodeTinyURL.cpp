c20d1f01ba2cc8fb178d7ff438f930b6bc67ba12
JumHorn <JumHorn@gmail.com>
2020-09-29 15:59:22 +0800

    formatted

--- Cplus/EncodeandDecodeTinyURL.cpp
+++ Cplus/EncodeandDecodeTinyURL.cpp
@@ -1,21 +1,25 @@
-#include<string>
-#include<vector>
+#include <string>
+#include <vector>
 using namespace std;
 
-class Solution {
-	vector<string> v;
+class Solution
+{
 public:
+	// Encodes a URL to a shortened URL.
+	string encode(string longUrl)
+	{
+		data.push_back(longUrl);
+		return to_string(data.size() - 1);
+	}
 
-    // Encodes a URL to a shortened URL.
-    string encode(string longUrl) {
-        v.push_back(longUrl);
-		return to_string(v.size()-1);
-    }
+	// Decodes a shortened URL to its original URL.
+	string decode(string shortUrl)
+	{
+		return data[stoi(shortUrl)];
+	}
 
-    // Decodes a shortened URL to its original URL.
-    string decode(string shortUrl) {
-        return v[stoi(shortUrl)];
-    }
+private:
+	vector<string> data;
 };
 
 // Your Solution object will be instantiated and called as such:
