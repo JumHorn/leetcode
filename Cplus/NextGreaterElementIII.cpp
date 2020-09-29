a176094e7795e42f6f65d7df7ff6b9095301b166
JumHorn <JumHorn@gmail.com>
2020-09-29 15:48:33 +0800

    formatted

--- Cplus/NextGreaterElementIII.cpp
+++ Cplus/NextGreaterElementIII.cpp
@@ -1,30 +1,29 @@
-#include<string>
-#include<algorithm>
-#include<climits>
+#include <algorithm>
+#include <climits>
+#include <string>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int nextGreaterElement(int n) {
-		string s(to_string(n));
-		int i=s.size();
-		while(--i>0)
+	int nextGreaterElement(int n)
+	{
+		string s = to_string(n);
+		int N = s.length();
+		int i = N;
+		while (--i > 0)
 		{
-			if(s[i]>s[i-1])
+			if (s[i] > s[i - 1])
 			{
-				int j=s.size();
-				while(--j>=i)
-				{
-					if(s[j]>s[i-1])
-					{
-						swap(s[j],s[i-1]);
-						reverse(s.begin()+i,s.end());
-						long res=stol(s);
-						return res>INT_MAX?-1:res;
-					}
-				}
+				int j = N - 1;
+				while (s[j] <= s[i - 1])
+					--j;
+				swap(s[i - 1], s[j]);
+				reverse(s.begin() + i, s.end());
+				long res = stol(s);
+				return res < INT_MAX ? res : -1;
 			}
 		}
 		return -1;
-    }
+	}
 };
