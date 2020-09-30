cf8a18d76967528f2dfe9e40c3911f3f6bc7d069
JumHorn <JumHorn@gmail.com>
2020-09-30 08:05:11 +0800

    Single Element in a Sorted Array beats 88.19%

--- Cplus/SingleElementinaSortedArray.cpp
+++ Cplus/SingleElementinaSortedArray.cpp
@@ -1,15 +1,30 @@
-#include<iostream>
-#include<vector>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int singleNonDuplicate(vector<int>& nums) {
-        int result=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            result^=nums[i];
-        }
-        return result;
-    }
+	int singleNonDuplicate(vector<int> &nums)
+	{
+		int N = nums.size(), lo = 0, hi = N - 1;
+		while (lo < hi)
+		{
+			int mi = (hi - lo) / 2 + lo;
+			if (mi % 2 == 0)
+			{
+				if (mi != N - 1 && nums[mi] == nums[mi + 1])
+					lo = mi + 2;
+				else
+					hi = mi;
+			}
+			else
+			{
+				if (mi != 0 && nums[mi] == nums[mi - 1])
+					lo = mi + 1;
+				else
+					hi = mi;
+			}
+		}
+		return nums[lo];
+	}
 };
