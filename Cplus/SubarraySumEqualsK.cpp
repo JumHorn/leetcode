7de7084c6aa4a53cfeeff68e4d28b2036c5b49c6
JumHorn <JumHorn@gmail.com>
2020-09-30 16:01:56 +0800

    Subarray Sum Equals K beats 89.80%

--- Cplus/SubarraySumEqualsK.cpp
+++ Cplus/SubarraySumEqualsK.cpp
@@ -1,52 +1,22 @@
-#include<vector>
+#include <unordered_map>
+#include <vector>
 using namespace std;
-/*
-------------------------->
-|    Sum(i,j)=Sum(i-1,j)-A[i];
-|
-|
-|
-|
-|
-|
-*/
-class Solution {
+
+class Solution
+{
 public:
-    int subarraySum(vector<int>& nums, int k) {
-		vector<int> dp(nums.size());
-		int res=0,sum=0;
-		for(int i=0;i<nums.size();i++)
+	int subarraySum(vector<int> &nums, int k)
+	{
+		int res = 0, prefixsum = 0;
+		unordered_map<int, int> m; //{value,count}
+		m[0] = 1;
+		for (auto n : nums)
 		{
-			sum+=nums[i];
-			dp[i]=sum;
-			if(sum==k)
-				res++;
+			prefixsum += n;
+			if (m.find(prefixsum - k) != m.end())
+				res += m[prefixsum - k];
+			++m[prefixsum];
 		}
-		for(int i=1;i<nums.size();i++)
-        {
-			for(int j=0;j<i;j++)
-			{
-				dp[i]=dp[i]-nums[j];
-				if(dp[i]==k)
-					res++;
-			}
-        }
 		return res;
 	}
 };
-
-
-//public class Solution {
-//    public int subarraySum(int[] nums, int k) {
-//        int count = 0, sum = 0;
-//        HashMap < Integer, Integer > map = new HashMap < > ();
-//        map.put(0, 1);
-//        for (int i = 0; i < nums.length; i++) {
-//            sum += nums[i];
-//            if (map.containsKey(sum - k))
-//                count += map.get(sum - k);
-//            map.put(sum, map.getOrDefault(sum, 0) + 1);
-//        }
-//        return count;
-//    }
-//}
