aee0de502cd726567d665adb646e348c8bd3931a
JumHorn <JumHorn@gmail.com>
2020-09-30 22:41:32 +0800

    Array Nesting beats 75.00%

---
+++ C/ArrayNesting.c
@@ -0,0 +1,22 @@
+
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+
+int arrayNesting(int* nums, int numsSize)
+{
+	int res = 0;
+	for (int i = 0; i < numsSize; ++i)
+	{
+		if (nums[i] == -1)
+			continue;
+		int index = i, count = 0;
+		while (nums[index] != -1)
+		{
+			++count;
+			int next = nums[index];
+			nums[index] = -1;
+			index = next;
+		}
+		res = max(res, count);
+	}
+	return res;
+}
