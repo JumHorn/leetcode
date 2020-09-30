88057b131440392a86a6633dee58256182c39a3a
JumHorn <JumHorn@gmail.com>
2020-09-30 22:39:51 +0800

    Array Nesting beats 97.53%

--- Cplus/ArrayNesting.cpp
+++ Cplus/ArrayNesting.cpp
@@ -1,55 +1,31 @@
-#include<iostream>
-#include<vector>
-#include<map>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-class Solution {
+/*
+there is always a circle in this array
+*/
+
+class Solution
+{
 public:
-    int arrayNesting(vector<int>& nums) {
-		int unmarked=nums.size();
-		if(unmarked==0)
+	int arrayNesting(vector<int> &nums)
+	{
+		int res = 0;
+		for (int i = 0; i < (int)nums.size(); ++i)
 		{
-			return 0;
+			if (nums[i] == -1)
+				continue;
+			int index = i, count = 0;
+			while (nums[index] != -1)
+			{
+				++count;
+				int next = nums[index];
+				nums[index] = -1;
+				index = next;
+			}
+			res = max(res, count);
 		}
-		int *mark = new int[unmarked];
-		memset(mark,0,sizeof(int)*unmarked);
-		int marknum=1,maxmarknum=0,markflag=1;
-		map<int,int> hash;
-		//initialize
-		mark[0]=markflag;
-		unmarked--;
-		int tmp=nums[0];
-		while(unmarked!=0)
-		{
-			if(mark[tmp]==markflag)
-			{
-				hash[markflag]=marknum;
-				if(maxmarknum<marknum)
-					maxmarknum=marknum;
-				//remark
-				markflag++;
-				if(unmarked==0)
-					return maxmarknum;
-				marknum=1;
-				unmarked--;
-				for(int i=0;i<nums.size();i++)
-				{
-					if(mark[i]==0)
-					{
-						mark[i]=markflag;
-						tmp=nums[i];
-						break;
-					}
-				}
-			}
-			else if(mark[tmp]==0)
-			{
-				mark[tmp]=markflag;
-				marknum++;
-				unmarked--;
-				tmp=nums[tmp];
-			}
-		}	
-		return maxmarknum>marknum?maxmarknum:marknum;		
-    }
+		return res;
+	}
 };
