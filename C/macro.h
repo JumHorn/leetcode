6d9d5f56db369a3ed5ba2c0c8414b0d787a9c237
JumHorn <JumHorn@gmail.com>
2020-09-30 15:06:22 +0800

    hash channing simple implementation

--- C/macro.h
+++ C/macro.h
@@ -184,6 +184,77 @@ bool dsu_union(DSU *dsu, int x, int y)
 }
 /********end of DSU********/
 
+// HASH
+#define HASH_TABLE_SIZE (1 << 10)
+typedef struct HashNode
+{
+	int key;
+	int value;
+	struct HashNode *next;
+} HashNode;
+
+HashNode *HashTable[HASH_TABLE_SIZE];
+
+void hash_init()
+{
+	memset(HashTable, 0, sizeof(HashTable));
+}
+
+void hash_release()
+{
+	for (int i = 0; i < HASH_TABLE_SIZE; ++i)
+	{
+		HashNode *node = HashTable[i];
+		while (node)
+		{
+			HashNode *deletenode = node;
+			node = node->next;
+			free(deletenode);
+		}
+	}
+}
+
+HashNode *hash_createNode(int key, int value)
+{
+	HashNode *node = (HashNode *)malloc(sizeof(HashNode));
+	node->key = key;
+	node->value = value;
+	node->next = NULL;
+	return node;
+}
+
+void hash_set(int key, int value)
+{
+	int bucket = (key & (HASH_TABLE_SIZE - 1));
+	HashNode *head = HashTable[bucket];
+	while (head)
+	{
+		if (head->key == key)
+		{
+			head->value = value;
+			return;
+		}
+		head = head->next;
+	}
+	head = hash_createNode(key, value);
+	head->next = HashTable[bucket];
+	HashTable[bucket] = head;
+}
+
+HashNode *hash_get(int key)
+{
+	int bucket = (key & (HASH_TABLE_SIZE - 1));
+	HashNode *head = HashTable[bucket];
+	while (head)
+	{
+		if (head->key == key)
+			break;
+		head = head->next;
+	}
+	return head;
+}
+/********end of HASH********/
+
 //Fenwick tree(BIT)
 int sum(int *tree, int size, int index)
 {
