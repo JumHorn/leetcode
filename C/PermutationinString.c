c33d1ad7a30c9daa14c8dc9b20946d529dad44d4
JumHorn <JumHorn@gmail.com>
2020-09-30 17:52:18 +0800

    formatted

--- C/PermutationinString.c
+++ C/PermutationinString.c
@@ -1,33 +1,35 @@
+#include <stdbool.h>
 #include <string.h>
 
-bool arrEqual(int* arr1, int* arr2, int size)
+bool arrEqual(int *arr1, int *arr2, int size)
 {
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < size; ++i)
+	{
 		if (arr1[i] != arr2[i])
 			return false;
+	}
 	return true;
 }
 
-bool checkInclusion(char* s1, char* s2)
+bool checkInclusion(char *s1, char *s2)
 {
-	int map[26] = {0};
+	int map1[26] = {0}, map2[26] = {0};
 	int len1 = strlen(s1), len2 = strlen(s2);
 	if (len1 > len2)
 		return false;
-	while (*s1)
-		++map[*s1++ - 'a'];
-	int window[26] = {0};
-	for (int i = 0; i < len1; i++)
-		++window[s2[i] - 'a'];
-	if (arrEqual(map, window, 26))
-		return true;
-	for (int i = len1; i < len2; i++)
+	for (int i = 0; i < len1; ++i)
 	{
-		++window[s2[i] - 'a'];
-		--window[s2[i - len1] - 'a'];
-		if (arrEqual(map, window, 26))
+		++map1[s1[i] - 'a'];
+		++map2[s2[i] - 'a'];
+	}
+	if (arrEqual(map1, map2, 26))
+		return true;
+	for (int i = len1; i < len2; ++i)
+	{
+		++map2[s2[i] - 'a'];
+		--map2[s2[i - len1] - 'a'];
+		if (arrEqual(map1, map2, 26))
 			return true;
 	}
 	return false;
 }
-
