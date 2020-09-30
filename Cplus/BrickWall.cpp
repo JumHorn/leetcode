decfb72d62bdd5affa84fe1328ce58e255726ed3
JumHorn <JumHorn@gmail.com>
2020-09-30 11:33:25 +0800

    Brick Wall beats 97.94%

--- Cplus/BrickWall.cpp
+++ Cplus/BrickWall.cpp
@@ -8,24 +8,17 @@ class Solution
 public:
 	int leastBricks(vector<vector<int>> &wall)
 	{
-		unordered_map<int, int> m; //{edge,crossed walls}
-		for (int i = 0; i < (int)wall.size(); ++i)
+		unordered_map<int, int> m; //{edge,edge count}
+		int res = wall.size(), N = wall.size();
+		for (auto &row : wall)
 		{
 			int prefixsum = 0;
-			for (int j = 0; j < (int)wall[i].size() - 1; ++j)
+			for (int i = 0; i < (int)row.size() - 1; ++i) // skip last brick
 			{
-				prefixsum += wall[i][j];
-				if (m.find(prefixsum) == m.end())
-					m[prefixsum] = i - 1;
-				else
-					--m[prefixsum];
+				prefixsum += row[i];
+				res = min(res, N - (++m[prefixsum]));
 			}
-			for (auto &n : m)
-				++n.second;
 		}
-		int res = wall.size();
-		for (auto &n : m)
-			res = min(res, n.second);
 		return res;
 	}
 };
