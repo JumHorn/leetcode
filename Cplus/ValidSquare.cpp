e1df3d9585d204cbb4a81ebcfddf5eeab701239c
JumHorn <JumHorn@gmail.com>
2020-10-01 08:39:08 +0800

    Valid Square beats 68.24%

--- Cplus/ValidSquare.cpp
+++ Cplus/ValidSquare.cpp
@@ -4,26 +4,16 @@ using namespace std;
 class Solution
 {
 public:
-	bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4)
+	bool validSquare(vector<int> &p1, vector<int> &p2, vector<int> &p3, vector<int> &p4)
 	{
-		vector<int> tmp;
-		if (isSquare(p1, p2, p3, tmp) && tmp == p4)
-			return true;
-		if (isSquare(p1, p2, p4, tmp) && tmp == p3)
-			return true;
-		if (isSquare(p1, p3, p4, tmp) && tmp == p2)
-			return true;
-		return false;
+		return isSquare(p1, p2, p3, p4) || isSquare(p1, p2, p4, p3) || isSquare(p1, p3, p4, p2);
 	}
 
-	bool isSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& out)
+	bool isSquare(vector<int> &p1, vector<int> &p2, vector<int> &p3, vector<int> &p4)
 	{
 		int a = p2[0] - p1[0], b = p2[1] - p1[1], c = p3[0] - p1[0], d = p3[1] - p1[1];
-		if (a * c + b * d != 0)
+		if (a * a + b * b == 0 || a * c + b * d != 0 || a * a + b * b != c * c + d * d)
 			return false;
-		if (a * a + b * b == 0 || a * a + b * b != c * c + d * d)
-			return false;
-		out = {a + c + p1[0], b + d + p1[1]};
-		return true;
+		return p4[0] == a + c + p1[0] && p4[1] == b + d + p1[1];
 	}
 };
