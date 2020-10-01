2dc65ac78285350f64afff1d37c2c870cd10c9a5
JumHorn <JumHorn@gmail.com>
2020-10-02 07:26:49 +0800

    use dp to accelerate

--- Cplus/2KeysKeyboard.cpp
+++ Cplus/2KeysKeyboard.cpp
@@ -1,19 +1,31 @@
-class Solution {
-public:
-    int minSteps(int n) {
-    	if(n==1)
-			return 0;
-		int m=n;
-		for(int i=2;i*i<=n;i++)
-		{
-			if(n%i==0)
-			{
-				int tmp=i+minSteps(n/i);
-				if(m>tmp)
-					m=tmp;
-			}
+#include <unordered_map>
+using namespace std;
 
+class Solution
+{
+public:
+	int minSteps(int n)
+	{
+		unordered_map<int, int> dp; //{n,minstep}
+		return memdp(n, dp);
+	}
+
+	int memdp(int n, unordered_map<int, int> &dp)
+	{
+		if (n == 1)
+			return 0;
+		if (dp.find(n) != dp.end())
+			return dp[n];
+		int res = n;
+		for (int i = 2; i * i <= n; i++)
+		{
+			if (n % i == 0)
+			{
+				int step = i + memdp(n / i, dp);
+				if (res > step)
+					res = step;
+			}
 		}
-		return m;
-    }
+		return dp[n] = res;
+	}
 };
