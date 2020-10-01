6e670edb8fdf6b18657ed3387e68fea8ab73a4c1
JumHorn <JumHorn@gmail.com>
2020-10-01 12:20:23 +0800

    Sum of Square Numbers beats 16.25%

--- Cplus/SumofSquareNumbers.cpp
+++ Cplus/SumofSquareNumbers.cpp
@@ -1,44 +1,30 @@
-#include<iostream>
-#include<cmath>
+#include <cmath>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool judgeSquareSum(int c) {
-        double temp;
-        for(long i=0;i*i<=c;i++)
-        {
-            temp=sqrt(c-i*i);
-            if(temp==(int)temp)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-};
+	bool judgeSquareSum(int c)
+	{
+		for (long i = 0; i * i * 2 <= c; ++i)
+		{
+			if (isSquare(c - i * i))
+				return true;
+		}
+		return false;
+	}
 
-//solution0 只要把int i 改为long i不会越界即可
-// class Solution0 {
-// public:
-//     bool judgeSquareSum(int c) {
-//         int temp;
-//         double real;
-//         real = sqrt(c);
-//         temp=(int)real;
-//         if(real==temp)
-//         {
-//             return true;
-//         }
-//         for(int i=0;i*i<c;i++)
-//         {
-//             real=sqrt(c-i*i);
-//             temp=(int)real;
-//             if(real==temp)
-//             {
-//                 return true;
-//             }
-//         }
-//         return false;
-//     }
-// };
+	bool isSquare(long n)
+	{
+		long lo = 0, hi = n;
+		while (lo < hi)
+		{
+			long mi = (hi - lo) / 2 + lo;
+			if (mi * mi < n)
+				lo = mi + 1;
+			else
+				hi = mi;
+		}
+		return lo * lo == n;
+	}
+};
