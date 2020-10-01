f6c5afd9437ae250328f03bdc25ed765d714a813
JumHorn <JumHorn@gmail.com>
2020-10-01 08:10:34 +0800

    Fraction Addition and Subtraction beats 100%

---
+++ C/FractionAdditionandSubtraction.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int gcd(int x, int y)
+{
+	if (x == 0)
+		return y;
+	return gcd(y % x, x);
+}
+
+char *fractionAddition(char *expression)
+{
+	char *res = (char *)malloc(sizeof(char) * 25);
+	char *p = expression;
+	int A = 0, B = 1, a, b;
+	while (sscanf(p, "%d/%d", &a, &b) != EOF)
+	{
+		while (*p != '/')
+			++p;
+		while (*p && *p != '+' && *p != '-')
+			++p;
+		A = A * b + a * B;
+		B *= b;
+		int g = abs(gcd(A, B));
+		A /= g;
+		B /= g;
+	}
+	sprintf(res, "%d/%d", A, B);
+	return res;
+}
