0ba456ae373771c37de6f3ab58f8fc04df9c5b13
JumHorn <JumHorn@gmail.com>
2020-10-01 20:40:57 +0800

    beats 99.25%

--- Cplus/ReplaceWords.cpp
+++ Cplus/ReplaceWords.cpp
@@ -1,62 +1,80 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include<algorithm>
+#include <algorithm>
+#include <sstream>
+#include <string>
+#include <vector>
 using namespace std;
 
-class Solution {
-public:
-    string replaceWords(vector<string>& dict, string sentence) {
-		sortdict(dict);
-		int i=0,j,k;
-		while(i<sentence.length()&&sentence[i]==' ')i++;
-		while(i<sentence.length())
-		{
-			k=i;
-			while(k<sentence.length()&&sentence[k]!=' ')k++;
-			int l = k-i;
-			for(j=0;j<dict.size()&&dict[j].length()<=l;j++)
-			{
-				int n;
-				for(n=0;n<dict[j].length();n++)
-				{
-					if(dict[j][n]!=sentence[i+n])
-					{
-						break;
-					}
-				}
-				if(n==dict[j].length())
-				{
-					sentence.erase(i+dict[j].length(),l-dict[j].length());
-                    k=i+dict[j].length();
-					break;
-				}
-			}
-		    i=k+1;
-		}
-		return sentence;
-    }
-
-	void sortdict(vector<string>& dict)
+class Trie
+{
+	struct TreeNode
 	{
-		int len = dict.size();
-		bool sorted = false;
-		while(len>1)
+		int count; //number of words ends with this node
+		vector<TreeNode *> nodes;
+
+		TreeNode() : count(0), nodes(26, nullptr)
 		{
-			sorted=true;
-			for(int i=0;i<len-1;i++)
-			{
-				if(dict[i].length()>dict[i+1].length())
-				{
-					swap(dict[i],dict[i+1]);
-					sorted=false;
-				}
-			}
-			if(sorted)
-			{
-				break;
-			}
-			len--;
 		}
+	};
+
+public:
+	Trie()
+	{
+		root = new TreeNode();
+	}
+
+	void insert(const string &word)
+	{
+		TreeNode *node = root;
+		for (auto c : word)
+		{
+			int index = c - 'a';
+			if (node->nodes[index] == NULL)
+				node->nodes[index] = new TreeNode();
+			node = node->nodes[index];
+		}
+		++node->count;
+	}
+
+	bool search(const string &word, string &res)
+	{
+		TreeNode *node = root;
+		for (auto c : word)
+		{
+			int index = c - 'a';
+			res.push_back(c);
+			if (node->nodes[index] == nullptr)
+				return false;
+			node = node->nodes[index];
+			if (node->count > 0)
+				return true;
+		}
+		return false;
+	}
+
+private:
+	TreeNode *root;
+};
+
+class Solution
+{
+public:
+	string replaceWords(vector<string> &dictionary, string sentence)
+	{
+		Trie trie;
+		for (auto d : dictionary)
+			trie.insert(d);
+		string token, res;
+		stringstream ss(sentence);
+		while (ss >> token)
+		{
+			string prefix;
+			if (trie.search(token, prefix))
+				res += prefix + " ";
+			else
+				res += token + " ";
+		}
+		if (!res.empty())
+			res.pop_back();
+		return res;
 	}
 };
