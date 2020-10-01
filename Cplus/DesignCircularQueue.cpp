8c37ec01ebc3db9bdc1d32ddc6dcd7cbecbe4f6f
JumHorn <JumHorn@gmail.com>
2020-10-01 11:02:17 +0800

    formatted

--- Cplus/DesignCircularQueue.cpp
+++ Cplus/DesignCircularQueue.cpp
@@ -1,57 +1,62 @@
-class MyCircularQueue {
-	int *p;
+class MyCircularQueue
+{
+public:
+	/** Initialize your data structure here. Set the size of the queue to be k. */
+	MyCircularQueue(int k)
+	{
+		data = new int[k + 1];
+		head = tail = 0;
+		size = k + 1;
+	}
+
+	/** Insert an element into the circular queue. Return true if the operation is successful. */
+	bool enQueue(int value)
+	{
+		if (isFull())
+			return false;
+		data[tail] = value;
+		tail = (tail - 1 + size) % size;
+		return true;
+	}
+
+	/** Delete an element from the circular queue. Return true if the operation is successful. */
+	bool deQueue()
+	{
+		if (isEmpty())
+			return false;
+		head = (head - 1 + size) % size;
+		return true;
+	}
+
+	/** Get the front item from the queue. */
+	int Front()
+	{
+		return isEmpty() ? -1 : data[head];
+	}
+
+	/** Get the last item from the queue. */
+	int Rear()
+	{
+		return isEmpty() ? -1 : data[(tail + 1) % size];
+	}
+
+	/** Checks whether the circular queue is empty or not. */
+	bool isEmpty()
+	{
+		return head == tail;
+	}
+
+	/** Checks whether the circular queue is full or not. */
+	bool isFull()
+	{
+		return (head + 1) % size == tail;
+	}
+
+private:
+	int *data;
 	int size;
 	int head;
 	int tail;
-public:
-    /** Initialize your data structure here. Set the size of the queue to be k. */
-    MyCircularQueue(int k) {
-		p=new int[k+1];      
-		head=0;
-		tail=0;
-		size=k+1;
-    }
-    
-    /** Insert an element into the circular queue. Return true if the operation is successful. */
-    bool enQueue(int value) {
-        if(isFull())
-			return false;
-		p[tail]=value;
-		tail=(tail-1+size)%size;
-		return true;
-    }
-    
-    /** Delete an element from the circular queue. Return true if the operation is successful. */
-    bool deQueue() {
-        if(isEmpty())
-			return false;
-		head=(head-1+size)%size;
-		return true;
-    }
-    
-    /** Get the front item from the queue. */
-    int Front() {
-		if(isEmpty())
-			return -1;
-        return p[head];
-    }
-    
-    /** Get the last item from the queue. */
-    int Rear() {
-		if(isEmpty())
-			return -1;
-        return p[(tail+1)%size];
-    }
-    
-    /** Checks whether the circular queue is empty or not. */
-    bool isEmpty() {
- 		return head==tail;       
-    }
-    
-    /** Checks whether the circular queue is full or not. */
-    bool isFull() {
-        return (head+1)%size==tail; 
-    }
 };
 
 /**
