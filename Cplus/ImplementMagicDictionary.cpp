05062a18b1089c1fc431c8e7a5d67167589cc468
JumHorn <JumHorn@gmail.com>
2020-10-02 22:06:01 +0800

    Implement Magic Dictionary beats 32.16%

--- Cplus/ImplementMagicDictionary.cpp
+++ Cplus/ImplementMagicDictionary.cpp
@@ -1,50 +1,48 @@
-#include<iostream>
-#include<vector>
-#include<string>
+#include <string>
+#include <unordered_set>
+#include <vector>
 using namespace std;
 
-class MagicDictionary {
+class MagicDictionary
+{
 public:
-	vector<string> dict;
-    /** Initialize your data structure here. */
-    MagicDictionary() {
-        
-    }
-    
-    /** Build a dictionary through a list of words */
-    void buildDict(vector<string> dict) {
-        this->dict=dict;
-    }
-    
-    /** Returns if there is any word in the trie that equals to the given word after modifying exactly one character */
-    bool search(string word) {
-       for(vector<string>::iterator iter=dict.begin();iter!=dict.end();iter++)
-	   {
-		   if(iter->length()==word.length())
-		   {
-			   int diff=0;
-			   for(int i=0;i<iter->length();i++)
-			   {
-				   if((*iter)[i]!=word[i])
-				   {
-					   diff++;
-					   if(diff>=2)
-					   	   break;
-				   }
-			   }
-			   if(diff==1)
-			   {
-				   return true;
-			   }
-		   }
-	   }
-	   return false;
-    }
+	/** Initialize your data structure here. */
+	MagicDictionary()
+	{
+	}
+
+	void buildDict(vector<string> dictionary)
+	{
+		for (auto &str : dictionary)
+			s.insert(str);
+	}
+
+	bool search(string searchWord)
+	{
+		for (auto &ch : searchWord)
+		{
+			char old = ch;
+			for (char c = 'a'; c <= 'z'; ++c)
+			{
+				if (c != old)
+				{
+					ch = c;
+					if (s.find(searchWord) != s.end())
+						return true;
+				}
+			}
+			ch = old;
+		}
+		return false;
+	}
+
+private:
+	unordered_set<string> s;
 };
 
 /**
  * Your MagicDictionary object will be instantiated and called as such:
- * MagicDictionary obj = new MagicDictionary();
- * obj.buildDict(dict);
- * bool param_2 = obj.search(word);
+ * MagicDictionary* obj = new MagicDictionary();
+ * obj->buildDict(dictionary);
+ * bool param_2 = obj->search(searchWord);
  */
