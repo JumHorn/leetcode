835682c470912358608dda550c281dd479dab7b8
JumHorn <JumHorn@gmail.com>
2020-10-02 15:12:52 +0800

    Print Binary Tree beats 69.82%

--- Cplus/PrintBinaryTree.cpp
+++ Cplus/PrintBinaryTree.cpp
@@ -1,40 +1,46 @@
-#include<vector>
-#include<string>
-#include<algorithm>
-#include<cmath>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
 using namespace std;
-//Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-class Solution {
+class Solution
+{
 public:
-    vector<vector<string> > printTree(TreeNode* root) {
-		int layer=getTreeHeight(root);
-		int len=pow(2,layer)-1;
-		vector<vector<string> > res(layer,vector<string>(len,""));
-		printTree(res,root,0,len,0);
-		return res;
-    }
-
-	void printTree(vector<vector<string> >& res,TreeNode* root,int l,int r,int h)
+	vector<vector<string>> printTree(TreeNode *root)
 	{
-		if(root==NULL)
-			return;
-		int mid=(r-l)/2+l;
-		res[h][mid]=to_string(root->val);
-		printTree(res,root->left,l,mid,h+1);
-		printTree(res,root->right,mid,r,h+1);
+		int layer = getTreeHeight(root);
+		int len = pow(2, layer) - 1;
+		vector<vector<string>> res(layer, vector<string>(len));
+		preorder(root, 0, len, 0, res);
+		return res;
 	}
 
-	int getTreeHeight(TreeNode* root)
+	void preorder(TreeNode *root, int left, int right, int height, vector<vector<string>> &res)
 	{
-		if(root==NULL)
+		if (root == nullptr)
+			return;
+		int mid = (right - left) / 2 + left;
+		res[height][mid] = to_string(root->val);
+		preorder(root->left, left, mid, height + 1, res);
+		preorder(root->right, mid, right, height + 1, res);
+	}
+
+	int getTreeHeight(TreeNode *root)
+	{
+		if (root == nullptr)
 			return 0;
-		return 1+max(getTreeHeight(root->left),getTreeHeight(root->right));
+		return 1 + max(getTreeHeight(root->left), getTreeHeight(root->right));
 	}
 };
