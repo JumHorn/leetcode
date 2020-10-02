cce033b981757f8a29eb8efec3c53ec61a06b1ef
JumHorn <JumHorn@gmail.com>
2020-10-02 08:41:45 +0800

    Find Duplicate Subtrees beats 98.36%

--- Cplus/FindDuplicateSubtrees.cpp
+++ Cplus/FindDuplicateSubtrees.cpp
@@ -1,27 +1,37 @@
-#include<vector>
-#include<list>
+#include <string>
+#include <unordered_map>
+#include <vector>
 using namespace std;
-//Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-class Solution {
+class Solution
+{
 public:
-    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
-        unordered_map<string, int>m;
-        vector<TreeNode*>res;
-        DFS(root, m, res);
-        return res;
-    }
+	vector<TreeNode *> findDuplicateSubtrees(TreeNode *root)
+	{
+		unordered_map<string, int> dp; //{tree,count}
+		vector<TreeNode *> res;
+		preorder(root, dp, res);
+		return res;
+	}
 
-    string DFS(TreeNode* root, unordered_map<string, int>& m, vector<TreeNode*>& res){
-        if(!root) return "";
-        string s = to_string(root->val) +','+ DFS(root->left, m, res) +','+ DFS(root->right, m, res);
-        if(++m[s] == 2) res.push_back(root);
-        return s;
-    }
+	string preorder(TreeNode *root, unordered_map<string, int> &dp, vector<TreeNode *> &res)
+	{
+		if (root == nullptr)
+			return "";
+		string compressedtree = to_string(root->val) + ',' + preorder(root->left, dp, res) + ',' + preorder(root->right, dp, res);
+		if (++dp[compressedtree] == 2)
+			res.push_back(root);
+		return compressedtree;
+	}
 };
