fd413b6d7ecf19f8e28cd85167efd364ca41cbb7
JumHorn <JumHorn@gmail.com>
2020-10-03 15:11:22 +0800

    Top K Frequent Words beats 84.16%

--- Cplus/TopKFrequentWords.cpp
+++ Cplus/TopKFrequentWords.cpp
@@ -1,36 +1,31 @@
-#include<vector>
-#include<string>
-#include<map>
+#include <queue>
+#include <string>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-	map<string,int> freq;
-    vector<string> topKFrequent(vector<string>& words, int k) {
-		freq.clear();
-		for(vector<string>::iterator iter=words.begin();iter!=words.end();iter++)
-			freq[*iter]++;
-		vector<string> tmp;
-		for(map<string,int>::iterator iter=freq.begin();iter!=freq.end();iter++)
-			tmp.push_back(iter->first);
-		make_heap(tmp.begin(),tmp.end(),*this);
+	vector<string> topKFrequent(vector<string> &words, int k)
+	{
+		unordered_map<string, int> freq;
+		for (auto &word : words)
+			++freq[word];
+		auto f = [&](pair<int, string> &lhs, pair<int, string> &rhs) {
+			if (lhs.first != rhs.first)
+				return lhs.first < rhs.first;
+			return lhs.second > rhs.second;
+		};
+		priority_queue<pair<int, string>, vector<pair<int, string>>, decltype(f)> q(f); //{freq,str}
+		for (auto &iter : freq)
+			q.push({iter.second, iter.first});
 		vector<string> res;
-		for(int i=0;i<k;i++)
+		for (int i = 0; i < k; ++i)
 		{
-			pop_heap(tmp.begin(),tmp.end()-i,*this);
-			res.push_back(*(tmp.end()-i-1));
+			res.push_back(q.top().second);
+			q.pop();
 		}
 		return res;
-    }
-
-	bool operator()(const string& s1,const string& s2)
-	{
-		if(freq[s1]<freq[s2])
-			return true;
-		else if(freq[s1]>freq[s2])
-			return false;
-		if(s1>s2)
-			return true;
-		return false;
 	}
 };
