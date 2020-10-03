83eecc5a76e18ae53ef7148c021e556ab0487728
JumHorn <JumHorn@gmail.com>
2020-10-03 15:21:52 +0800

    Max Area of Island beats 73.90%

--- Cplus/MaxAreaofIsland.cpp
+++ Cplus/MaxAreaofIsland.cpp
@@ -1,67 +1,34 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <algorithm>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> *grid;
-    int maxAreaOfIsland(vector<vector<int>>& grid) {
-        this->grid = &grid;
-        int ans=0;
-        for(int i=0;i<grid.size();i++)
-        {
-            for(int j=0;j<grid[0].size();j++)
-            {
-                int temp = areasum(i,j);
-                ans = temp>ans?temp:ans;
-            }
-        }
-        return ans;
-    }
+	int maxAreaOfIsland(vector<vector<int>> &grid)
+	{
+		if (grid.empty() || grid[0].empty())
+			return 0;
+		int M = grid.size(), N = grid[0].size(), res = 0;
+		for (int i = 0; i < M; i++)
+		{
+			for (int j = 0; j < N; j++)
+				res = max(res, dfs(grid, i, j));
+		}
+		return res;
+	}
 
-    int areasum(int r,int c)
-    {
-        if(r<0||r>=(*grid).size()||c<0||c>=(*grid)[0].size()||(*grid)[r][c]==0)
-        {
-            return 0;
-        }
-        (*grid)[r][c]=0;
-        return (1+areasum(r+1,c)+areasum(r-1,c)+areasum(r,c-1)+areasum(r,c+1));
-    }
+	int dfs(vector<vector<int>> &grid, int r, int c)
+	{
+		int M = grid.size(), N = grid[0].size();
+		if (r < 0 || r >= M || c < 0 || c >= N || grid[r][c] == 0)
+			return 0;
+		grid[r][c] = 0;
+		int res = 1;
+		int path[] = {-1, 0, 1, 0, -1};
+		for (int i = 0; i < 4; ++i)
+			res += dfs(grid, r + path[i], c + path[i + 1]);
+		return res;
+	}
 };
-
-//memory limit exceeded
-// class Solution0 {
-// public:
-//     vector<vector<int> > groups;
-//     vector<int> num;
-//     int AreasumOfIsland(vector<vector<int>>& grid) {
-//         vector<int> temp;
-//         for(int i=0,j=0;i<grid[0].size();i++)
-//         {
-//             if(grid[0][i]==1)
-//             {
-//                 temp.clear();
-//                 temp.push_back(i);
-//                 num.push_back(1);
-//                 i++;
-//                 while(i<grid[0].size()&&grid[0][i]==1)
-//                 {
-//                     temp.push_back(i);
-//                     num[j]++;
-//                 }
-//                 groups.push_back(temp);
-//                 j++;
-//             }
-//         }
-//         if(grid.size()==1)
-//         {
-//             return *max_element(num.begin(),num.end());
-//         }
-//         for(int i=1;i<grid.size();i++)
-//         {
-
-//         }
-//     }
-// };
