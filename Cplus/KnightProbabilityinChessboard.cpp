7f9ede274902c4cd21f22edd813dc556c22696cb
JumHorn <JumHorn@gmail.com>
2020-10-03 14:50:01 +0800

    Knight Probability in Chessboard beats 67.41%

--- Cplus/KnightProbabilityinChessboard.cpp
+++ Cplus/KnightProbabilityinChessboard.cpp
@@ -1,40 +1,28 @@
-#include<cmath>
-#include<vector>
-#include<map>
+#include <vector>
 using namespace std;
 
-class Solution {
-	int dr[8];
-    int dc[8];
+class Solution
+{
 public:
-	Solution():dr({2, 2, 1, 1, -1, -1, -2, -2}),dc({1, -1, 2, -2, 2, -2, 1, -1}){}
-    double knightProbability(int N, int K, int r, int c) {
-        map<vector<int>,double> m;
-		return probability(N,K,r,c,m);
-    }
-
-	double probability(int N,int K,int r,int c,map<vector<int>,double>& m)
+	double knightProbability(int N, int K, int r, int c)
 	{
-		if(r<0||r>=N||c<0||c>=N)
+		vector<vector<vector<double>>> dp(K + 1, vector<vector<double>>(N, vector<double>(N, -1)));
+		return memdp(N, K, r, c, dp);
+	}
+
+	double memdp(int N, int K, int r, int c, vector<vector<vector<double>>> &dp)
+	{
+		if (r < 0 || r >= N || c < 0 || c >= N)
 			return 0;
-        if(K==0)
-            return 1;
-		double sum=0;
-		for(int i=0;i<8;i++)
-		{
-			vector<int> tmp(3);
-			tmp[0]=r+dr[i];
-			tmp[1]=c+dc[i];
-			tmp[2]=K;
-			if(m.find(tmp)==m.end())
-			{
-				double res=probability(N,K-1,r+dr[i],c+dc[i],m)/8.0;
-				m[tmp]=res;
-                sum+=res;
-			}
-			else
-				sum+=m[tmp];
-		}
-		return sum;
+		if (K == 0)
+			return 1;
+		if (dp[K][r][c] != -1)
+			return dp[K][r][c];
+		int dr[] = {2, 2, 1, 1, -1, -1, -2, -2};
+		int dc[] = {1, -1, 2, -2, 2, -2, 1, -1};
+		double res = 0;
+		for (int i = 0; i < 8; ++i)
+			res += memdp(N, K - 1, r + dr[i], c + dc[i], dp) / 8.0;
+		return dp[K][r][c] = res;
 	}
 };
