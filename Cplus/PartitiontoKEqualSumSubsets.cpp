89efc15481b6f0d11858d9ac564827eaef0f7f25
JumHorn <JumHorn@gmail.com>
2020-10-04 13:28:04 +0800

    Partition to K Equal Sum Subsets beats 94.92%

--- Cplus/PartitiontoKEqualSumSubsets.cpp
+++ Cplus/PartitiontoKEqualSumSubsets.cpp
@@ -1,38 +1,34 @@
-#include<vector>
-#include<numeric>
-#include<algorithm>
+#include <algorithm>
+#include <numeric>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool canPartitionKSubsets(vector<int>& nums, int k) {
-        int sum=accumulate(nums.begin(),nums.end(),0);
-		if(sum%k!=0)
-			return false;
-		sum/=k;
-		sort(nums.begin(),nums.end());
-		if(nums.back()>sum)
-			return false;
-		return canPartitionKSubsets(nums,sum,sum,nums.size(),0);
-    }
-
-	bool canPartitionKSubsets(vector<int>& nums,int k,int sum,int left,int start)
+	bool canPartitionKSubsets(vector<int> &nums, int k)
 	{
-		if(left==0&&k==0)
-			return true;
-		if(left==0||k<0)
+		int sum = accumulate(nums.begin(), nums.end(), 0);
+		if (sum % k != 0)
 			return false;
-		if(k==0)
-			return canPartitionKSubsets(nums,sum,sum,left,0);
-		for(int i=start;i<(int)nums.size();i++)
+		sum /= k;
+		return backTracking(nums, 0, k, sum, sum);
+	}
+
+	bool backTracking(vector<int> &nums, int index, int k, int sub, int sum)
+	{
+		if (k == 1)
+			return true;
+		if (sub == 0)
+			return backTracking(nums, 0, k - 1, sum, sum);
+		for (int i = index; i < (int)nums.size(); ++i)
 		{
-			if(nums[i]==-1)
+			if (nums[i] < 0)
 				continue;
-			int tmp=nums[i];
-			nums[i]=-1;
-			if(canPartitionKSubsets(nums,k-tmp,sum,left-1,i+1))
+			nums[i] = -nums[i];
+			if (sub + nums[i] >= 0 && backTracking(nums, i + 1, k, sub + nums[i], sum))
 				return true;
-			nums[i]=tmp;
+			nums[i] = -nums[i];
 		}
 		return false;
 	}
