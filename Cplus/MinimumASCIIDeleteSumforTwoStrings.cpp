eb9eaebe1513eb21a5e457d8df5f003566e40f80
JumHorn <JumHorn@gmail.com>
2020-10-04 13:47:47 +0800

    Minimum ASCII Delete Sum for Two Strings beats 72.84%

--- Cplus/MinimumASCIIDeleteSumforTwoStrings.cpp
+++ Cplus/MinimumASCIIDeleteSumforTwoStrings.cpp
@@ -1,41 +1,28 @@
-#include<iostream>
-#include<string>
+#include <string>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int minimumDeleteSum(string s1, string s2) {
-		int m = s1.length();
-		int n = s2.length();
-        //int **dp = new (int*)[m+1];//no parenthesis 
-        int **dp = new int *[m+1]; 
-		for(int i=0;i<m+1;i++)
+	int minimumDeleteSum(string s1, string s2)
+	{
+		int M = s1.length(), N = s2.length();
+		vector<vector<int>> dp(M + 1, vector<int>(N + 1));
+		for (int i = 0; i < M; ++i)
+			dp[i + 1][0] = dp[i][0] + s1[i];
+		for (int j = 0; j < N; ++j)
+			dp[0][j + 1] = dp[0][j] + s2[j];
+		for (int i = 0; i < M; ++i)
 		{
-			dp[i] = new int[n+1];
-		}
-		dp[0][0] = 0;
-		for(int i=0;i<m;i++)
-		{
-			dp[i+1][0] = dp[i][0] + s1[i];
-		}
-		for(int i=0;i<n;i++)
-		{
-			dp[0][i+1] = dp[0][i] + s2[i];
-		}
-		for(int j=1;j<n+1;j++)
-		{
-			for(int i=1;i<m+1;i++)
+			for (int j = 0; j < N; ++j)
 			{
-				if(s1[i-1]==s2[j-1])
-				{
-					dp[i][j] = dp[i-1][j-1];
-				}
+				if (s1[i] == s2[j])
+					dp[i + 1][j + 1] = dp[i][j];
 				else
-				{
-					dp[i][j]=(dp[i-1][j]+s1[i-1]<dp[i][j-1]+s2[j-1])?dp[i-1][j]+s1[i-1]:dp[i][j-1]+s2[j-1];
-				}
+					dp[i + 1][j + 1] = min(dp[i + 1][j] + s2[j], dp[i][j + 1] + s1[i]);
 			}
 		}
-		return dp[m][n];
-    }
+		return dp[M][N];
+	}
 };
