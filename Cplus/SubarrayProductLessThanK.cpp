bed8db635f17cbece1c2e12a69ed07787231c24b
JumHorn <JumHorn@gmail.com>
2020-10-04 22:26:39 +0800

    Subarray Product Less Than K beats 52.81%

--- Cplus/SubarrayProductLessThanK.cpp
+++ Cplus/SubarrayProductLessThanK.cpp
@@ -1,17 +1,19 @@
-#include<vector>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-		int res=0,product=1,i=-1,j=0;
-		while(++i<(int)nums.size())
+	int numSubarrayProductLessThanK(vector<int> &nums, int k)
+	{
+		int res = 0, product = 1, N = nums.size();
+		for (int i = 0, j = 0; i < N; ++i)
 		{
-			product*=nums[i];
-            while(j<=i&&product>=k)
-                product/=nums[j++];
-            res+=i-j+1;
+			product *= nums[i];
+			while (j <= i && product >= k)
+				product /= nums[j++];
+			res += i - j + 1;
 		}
 		return res;
-    }
+	}
 };
