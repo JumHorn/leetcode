4cdaf1b963d6c64be0814cb488a9639b9c479a40
JumHorn <JumHorn@gmail.com>
2020-10-05 22:35:45 +0800

    Asteroid Collision beats 89.82%

--- Cplus/AsteroidCollision.cpp
+++ Cplus/AsteroidCollision.cpp
@@ -5,42 +5,30 @@ using namespace std;
 class Solution
 {
 public:
-	vector<int> asteroidCollision(vector<int>& asteroids)
+	vector<int> asteroidCollision(vector<int> &asteroids)
 	{
 		vector<int> res;
 		stack<int> s;
-		for (int i = 0; i < (int)asteroids.size(); i++)
+		for (auto asteroid : asteroids)
 		{
-			if (asteroids[i] > 0)
-				s.push(asteroids[i]);
+			if (asteroid > 0)
+				s.push(asteroid);
 			else
 			{
-				bool flag = true;
-				while (!s.empty())
-				{
-					if (-asteroids[i] > s.top())
-						s.pop();
-					else if (-asteroids[i] == s.top())
-					{
-						flag = false;
-						s.pop();
-						break;
-					}
-					else
-						break;
-				}
-				if (flag && s.empty())
-					res.push_back(asteroids[i]);
+				while (!s.empty() && s.top() < -asteroid)
+					s.pop();
+				if (!s.empty() && s.top() == -asteroid)
+					s.pop();
+				else if (s.empty())
+					res.push_back(asteroid);
 			}
 		}
-		vector<int> right;
-		while (!s.empty())
+		res.resize(res.size() + s.size());
+		for (int i = res.size() - 1; !s.empty(); --i)
 		{
-			right.push_back(s.top());
+			res[i] = s.top();
 			s.pop();
 		}
-		for (int i = right.size() - 1; i >= 0; i--)
-			res.push_back(right[i]);
 		return res;
 	}
 };
