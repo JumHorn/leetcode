ef45a6eee8a530b8cbe8c48e5b623858e3ea326c
JumHorn <JumHorn@gmail.com>
2020-10-05 23:41:31 +0800

    Network Delay Time beats 53.33%

--- C/NetworkDelayTime.c
+++ C/NetworkDelayTime.c
@@ -1,30 +1,25 @@
+#include <limits.h>
 #include <string.h>
 
 #define max(a, b) (((a) > (b)) ? (a) : (b))
 
-void dfs(int N, int (*graph)[N + 1], int at, int time, int* delay)
-{
-	if (delay[at] < time)
-		return;
-	delay[at] = time;
-	for (int i = 1; i <= N; ++i)
-	{
-		if (graph[at][i] != -1)
-			dfs(N, graph, i, time + graph[at][i], delay);
-	}
-}
-
 int networkDelayTime(int** times, int timesSize, int* timesColSize, int N, int K)
 {
-	int graph[N + 1][N + 1];
-	memset(graph, -1, sizeof(graph));
-	for (int i = 0; i < timesSize; ++i)
-		graph[times[i][0]][times[i][1]] = times[i][2];
-	int delay[N + 1];
-	memset(delay, 1 << 6, sizeof(delay));
-	dfs(N, graph, K, 0, delay);
+	int dist[N + 1];
+	memset(dist, 1 << 6, sizeof(dist));
+	dist[K] = 0;
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < timesSize; ++j)
+		{
+			int u = times[j][0], v = times[j][1], w = times[j][2];
+			if (dist[u] != INT_MAX && dist[v] > dist[u] + w)
+				dist[v] = dist[u] + w;
+		}
+	}
+
 	int res = 0;
 	for (int i = 1; i <= N; ++i)
-		res = max(res, delay[i]);
+		res = max(res, dist[i]);
 	return res >= 1e5 ? -1 : res;
 }
