f4de485fa1cac1a8200e5cdf11c0414c5ffd6773
JumHorn <JumHorn@gmail.com>
2020-10-06 15:20:39 +0800

    formatted

--- C/StoneGame.c
+++ C/StoneGame.c
@@ -1,25 +1,23 @@
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define max(a, b) (((a) > (b)) ? (a) : (b))
 
-bool stoneGame(int* piles, int pilesSize)
+bool stoneGame(int *piles, int pilesSize)
 {
-	int* sum = (int*)malloc((pilesSize + 1) * sizeof(int));
-	memset(sum, 0, sizeof(int) * (pilesSize + 1));
-	for (int i = 0; i < pilesSize; i++)
+	int sum[pilesSize + 1];
+	sum[0] = 0;
+	for (int i = 0; i < pilesSize; ++i)
 		sum[i + 1] = sum[i] + piles[i];
-	int** dp = (int**)malloc(pilesSize * sizeof(int*));
-	for (int i = 0; i < pilesSize; i++)
-	{
-		dp[i] = (int*)malloc(pilesSize * sizeof(int));
-		memset(dp[i], 0, sizeof(int) * pilesSize);
-	}
-	for (int i = 0; i < pilesSize; i++)
+	int dp[pilesSize][pilesSize];
+	memset(dp, 0, sizeof(dp));
+	for (int i = 0; i < pilesSize; ++i)
 		dp[i][i] = piles[i];
-	for (int j = 0; j < pilesSize; j++)
-		for (int i = j - 1; i >= 0; i--)
+	for (int j = 0; j < pilesSize; ++j)
+	{
+		for (int i = j - 1; i >= 0; --i)
 			dp[i][j] = max(sum[j + 1] - sum[i + 1] - dp[i + 1][j] + piles[i], sum[j] - sum[i] - dp[i][j - 1] + piles[j]);
+	}
 	return dp[0][pilesSize - 1] > sum[pilesSize] / 2;
 }
-
