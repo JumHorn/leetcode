432f8daf39451fc4a8c3188af47bc834f99ba876
JumHorn <JumHorn@gmail.com>
2020-10-06 15:16:33 +0800

    Stone Game beats 81.42%

---
+++ Cplus/StoneGame.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+using namespace std;
+
+/*
+the first people can either take odd squence or even squence which is larger
+*/
+
+class Solution
+{
+public:
+	bool stoneGame(vector<int> &piles)
+	{
+		return true;
+	}
+};
