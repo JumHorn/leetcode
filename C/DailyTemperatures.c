614f0e00c3c024ee12a784f62a1d7668a10445ef
JumHorn <JumHorn@gmail.com>
2020-10-07 11:04:34 +0800

    Daily Temperatures beats 69.09%

---
+++ C/DailyTemperatures.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* dailyTemperatures(int* T, int TSize, int* returnSize)
+{
+	*returnSize = TSize;
+	int* res = (int*)malloc(sizeof(int) * (*returnSize));
+	memset(res, 0, sizeof(int) * (*returnSize));
+	int stack[TSize], top = -1;
+	for (int i = 0; i < TSize; ++i)
+	{
+		while (top != -1 && T[stack[top]] < T[i])
+		{
+			res[stack[top]] = i - stack[top];
+			--top;
+		}
+		stack[++top] = i;
+	}
+	return res;
+}
