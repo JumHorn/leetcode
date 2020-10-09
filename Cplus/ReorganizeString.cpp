7bbfb385d0d491624e9eb91c8e169a66360bd79f
JumHorn <JumHorn@gmail.com>
2020-10-09 19:37:42 +0800

    Reorganize String beats 100%

--- Cplus/ReorganizeString.cpp
+++ Cplus/ReorganizeString.cpp
@@ -1,53 +1,42 @@
-#include<string>
-#include<algorithm>
-#include<map>
+#include <algorithm>
+#include <queue>
+#include <string>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    string reorganizeString(string S) {
-        map<char,int> m;
-		for(string::size_type i=0;i<S.length();i++)
-			m[S[i]]++;
-		int max=-1;
-		map<char,int>::iterator it;
-		for(map<char,int>::iterator iter=m.begin();iter!=m.end();iter++)
-			if(iter->second>max)
-			{
-				max=iter->second;
-				it=iter;
-			}
-		if(S.length()-max<max-1)
+	string reorganizeString(string S)
+	{
+		vector<int> hash(26);
+		for (auto c : S)
+			++hash[c - 'a'];
+		int N = S.length();
+		int mostletter = 0;
+		for (int i = 0; i < 26; ++i)
+		{
+			if (hash[mostletter] < hash[i])
+				mostletter = i;
+		}
+		if (hash[mostletter] > (N + 1) / 2)
 			return "";
-		char tmp=it->first;
-		for(int i=0;i<S.length()-1;i++)
+		string res(N, '\0');
+		int index = 0;
+		while (--hash[mostletter] >= 0)
 		{
-			if(S[i]==S[i+1])
+			res[index] = 'a' + mostletter;
+			index += 2;
+		}
+		for (int i = 0; i < 26; ++i)
+		{
+			while (--hash[i] >= 0)
 			{
-				for(int j=i+2;j<S.length();j++)
-				{
-					if(S[i]!=S[j])
-					{
-						swap(S[i+1],S[j]);
-						break;
-					}
-				}
+				if (index >= N)
+					index = 1;
+				res[index] = 'a' + i;
+				index += 2;
 			}
 		}
-        for(int i=S.length()-1;i>=1;i--)
-		{
-			if(S[i]==S[i-1])
-			{
-				for(int j=i-2;j>=0;j--)
-				{
-					if(S[i]!=S[j])
-					{
-						swap(S[i-1],S[j]);
-						break;
-					}
-				}
-			}
-		}
-		return S;
-    }
+		return res;
+	}
 };
