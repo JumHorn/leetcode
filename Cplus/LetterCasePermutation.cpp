89a0cd73a41f5b7deb148a00cf3ceca82d8d732d
JumHorn <JumHorn@gmail.com>
2020-10-09 22:29:30 +0800

    formatted

--- Cplus/LetterCasePermutation.cpp
+++ Cplus/LetterCasePermutation.cpp
@@ -1,29 +1,32 @@
-#include<vector>
-#include<string>
-#include<locale>
-#include<cctype>
+#include <cctype>
+#include <string>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<string> letterCasePermutation(string S) {
-		vector<string> res;
-		letterCasePermutation(res,S,0);
-		return res;		
-    }
-
-	void letterCasePermutation(vector<string>& v,string s,int start)
+	vector<string> letterCasePermutation(string S)
 	{
-		if(start==(int)s.length())
+		vector<string> res;
+		dfs(S, 0, res);
+		return res;
+	}
+
+	void dfs(string &s, int index, vector<string> &res)
+	{
+		if (index >= (int)s.length())
 		{
-			v.push_back(s);
+			res.push_back(s);
 			return;
 		}
-		if(!isalpha(s[start]))
-			return letterCasePermutation(v,s,start+1);
-		s[start]=tolower(s[start]);
-		letterCasePermutation(v,s,start+1);
-		s[start]=toupper(s[start]);
-		letterCasePermutation(v,s,start+1);
+		if (!isalpha(s[index]))
+			return dfs(s, index + 1, res);
+		char old = s[index];
+		s[index] = tolower(s[index]);
+		dfs(s, index + 1, res);
+		s[index] = toupper(s[index]);
+		dfs(s, index + 1, res);
+		s[index] = old;
 	}
 };
