464244ec60048fe6057d9d2c01ec64d4cbcd7274
JumHorn <JumHorn@gmail.com>
2020-10-10 10:18:39 +0800

    Custom Sort String beats 61.83%

--- Cplus/CustomSortString.cpp
+++ Cplus/CustomSortString.cpp
@@ -1,37 +1,17 @@
-#include<iostream>
-#include<string>
+#include <algorithm>
+#include <string>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    string customSortString(string S, string T) {
-    	char hash[26];
-		for(int i=0;i<26;i++)
-		{
-			hash[i] = 26;
-		}
-
-		for(int i=0;i<S.length();i++)
-		{
-			hash[S[i]-'a'] = i;
-		}
-		
-		for(int i=0;i<T.length()-1;i++)
-		{
-			if(hash[T[i]-'a'] == 26)
-			{
-				continue;
-			}
-			for(int j=i+1;j<T.length();j++)
-			{
-				if(hash[T[i]-'a']>hash[T[j]-'a'])
-				{
-					char temp = T[i];
-					T[i] = T[j];
-					T[j] = temp;
-				}
-			}
-		}	
+	string customSortString(string S, string T)
+	{
+		vector<int> hash(26, 26);
+		for (int i = 0; i < (int)S.length(); ++i)
+			hash[S[i] - 'a'] = i;
+		sort(T.begin(), T.end(), [&](char lhs, char rhs) { return hash[lhs - 'a'] < hash[rhs - 'a']; });
 		return T;
-    }
+	}
 };
