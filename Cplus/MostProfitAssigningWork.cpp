e628bfcd8cd4086ce75e23cfe7f35ce12299d4cf
JumHorn <JumHorn@gmail.com>
2020-10-12 20:58:37 +0800

    Most Profit Assigning Work beats 89.50%

--- Cplus/MostProfitAssigningWork.cpp
+++ Cplus/MostProfitAssigningWork.cpp
@@ -1,36 +1,28 @@
-#include<vector>
-#include<algorithm>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-		sort(worker.begin(),worker.end());       
-		for(int i=0;i<(int)difficulty.size();i++)
+	int maxProfitAssignment(vector<int> &difficulty, vector<int> &profit, vector<int> &worker)
+	{
+		int N = difficulty.size();
+		vector<pair<int, int>> job; //{difficulty,profit}
+		for (int i = 0; i < N; ++i)
+			job.push_back({difficulty[i], profit[i]});
+		sort(job.begin(), job.end());
+		sort(worker.begin(), worker.end());
+		int res = 0, d = 0, p = 0;
+		for (int i = 0; i < (int)worker.size(); ++i)
 		{
-			for(int j=(int)difficulty.size()-1;j>i;j--)
+			while (d < N && worker[i] >= job[d].first)
 			{
-				if(difficulty[j-1]>difficulty[j])
-				{
-					swap(difficulty[j-1],difficulty[j]);
-					swap(profit[j-1],profit[j]);
-				}
+				p = max(p, job[d].second);
+				++d;
 			}
-		}
-		for(int i=1;i<(int)profit.size();i++)
-			profit[i]=max(profit[i],profit[i-1]);
-
-		int res=0,d=0,i=0;
-		//worker who's profit is 0
-		while(i<(int)worker.size()&&difficulty[0]>worker[i])
-			i++;
-		//worker who's profit >0
-		for(;i<(int)worker.size();i++)
-		{
-			while(d<(int)difficulty.size()&&worker[i]>=difficulty[d])
-				d++;
-			res+=profit[d-1];
+			res += p;
 		}
 		return res;
-    }
+	}
 };
