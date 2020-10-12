2d24dbc6cf2f341f56e2959a22598547b504c1af
JumHorn <JumHorn@gmail.com>
2020-10-12 16:41:51 +0800

    formatted

--- Cplus/KeysandRooms.cpp
+++ Cplus/KeysandRooms.cpp
@@ -1,24 +1,27 @@
-#include<vector>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool canVisitAllRooms(vector<vector<int>>& rooms) {
-		int N=rooms.size();        
-		vector<bool> visited(N,false);
-		dfs(rooms,0,visited);
-		for(int i=0;i<N;i++)
-			if(!visited[i])
-				return false;
-		return true;
-    }
-
-	void dfs(vector<vector<int>>& rooms,int start,vector<bool>& visited)
+	bool canVisitAllRooms(vector<vector<int>> &rooms)
 	{
-		if(visited[start])
+		vector<bool> seen(rooms.size());
+		dfs(rooms, 0, seen);
+		for (auto b : seen)
+		{
+			if (!b)
+				return false;
+		}
+		return true;
+	}
+
+	void dfs(vector<vector<int>> &rooms, int index, vector<bool> &seen)
+	{
+		if (seen[index])
 			return;
-		visited[start]=true;
-		for(int i=0;i<(int)rooms[start].size();i++)
-			dfs(rooms,rooms[start][i],visited);
+		seen[index] = true;
+		for (auto &room : rooms[index])
+			dfs(rooms, room, seen);
 	}
 };
