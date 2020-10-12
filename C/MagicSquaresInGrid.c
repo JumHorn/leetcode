4ffcde13b1f3eea138ccc2c8f82fe4ddd6f8b79c
JumHorn <JumHorn@gmail.com>
2020-10-12 16:57:56 +0800

    Magic Squares In Grid beats 100%

--- C/MagicSquaresInGrid.c
+++ C/MagicSquaresInGrid.c
@@ -1,26 +1,36 @@
 
-
-
-int numMagicSquaresInside(int** grid, int gridSize, int* gridColSize){
-	int colSize=gridColSize[0],res=0;
-	for(int i=1;i<gridSize-1;i++)
-		for(int j=1;j<colSize-1;j++)
+int numMagicSquaresInside(int **grid, int gridSize, int *gridColSize)
+{
+	int colSize = gridColSize[0], res = 0;
+	for (int i = 1; i < gridSize - 1; i++)
+	{
+		for (int j = 1; j < colSize - 1; j++)
 		{
-			if(grid[i][j]==5&&grid[i-1][j-1]!=5&&grid[i-1][j+1]!=5&&grid[i+1][j-1]!=5&&grid[i+1][j+1]!=5
-            &&grid[i-1][j-1]&&grid[i-1][j]&&grid[i-1][j+1]&&grid[i][j-1]&&grid[i][j+1]
-            &&grid[i+1][j-1]&&grid[i+1][j]&&grid[i+1][j+1])
+			if (grid[i][j] == 5 &&
+				grid[i - 1][j - 1] != 5 &&
+				grid[i - 1][j + 1] != 5 &&
+				grid[i + 1][j - 1] != 5 &&
+				grid[i + 1][j + 1] != 5 &&
+				grid[i - 1][j - 1] &&
+				grid[i - 1][j] &&
+				grid[i - 1][j + 1] &&
+				grid[i][j - 1] &&
+				grid[i][j + 1] &&
+				grid[i + 1][j - 1] &&
+				grid[i + 1][j] &&
+				grid[i + 1][j + 1])
 			{
-				if(grid[i-1][j]+grid[i+1][j]==10
-					&&grid[i][j-1]+grid[i][j+1]==10
-					&&grid[i-1][j-1]+grid[i+1][j+1]==10
-					&&grid[i-1][j+1]+grid[i+1][j-1]==10
-					&&grid[i-1][j-1]+grid[i-1][j]+grid[i-1][j+1]==15
-					&&grid[i+1][j-1]+grid[i+1][j]+grid[i+1][j+1]==15
-					&&grid[i-1][j-1]+grid[i][j-1]+grid[i+1][j-1]==15
-					&&grid[i-1][j+1]+grid[i][j+1]+grid[i+1][j+1]==15)
-						res++;
+				if (grid[i - 1][j] + grid[i + 1][j] == 10 &&
+					grid[i][j - 1] + grid[i][j + 1] == 10 &&
+					grid[i - 1][j - 1] + grid[i + 1][j + 1] == 10 &&
+					grid[i - 1][j + 1] + grid[i + 1][j - 1] == 10 &&
+					grid[i - 1][j - 1] + grid[i - 1][j] + grid[i - 1][j + 1] == 15 &&
+					grid[i + 1][j - 1] + grid[i + 1][j] + grid[i + 1][j + 1] == 15 &&
+					grid[i - 1][j - 1] + grid[i][j - 1] + grid[i + 1][j - 1] == 15 &&
+					grid[i - 1][j + 1] + grid[i][j + 1] + grid[i + 1][j + 1] == 15)
+					++res;
 			}
 		}
+	}
 	return res;
 }
-
---
+++ Cplus/MagicSquaresInGrid.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+	int numMagicSquaresInside(vector<vector<int>> &grid)
+	{
+		int M = grid.size(), N = grid[0].size(), res = 0;
+		for (int i = 1; i < M - 1; i++)
+		{
+			for (int j = 1; j < N - 1; j++)
+			{
+				if (grid[i][j] == 5 &&
+					grid[i - 1][j - 1] != 5 &&
+					grid[i - 1][j + 1] != 5 &&
+					grid[i + 1][j - 1] != 5 &&
+					grid[i + 1][j + 1] != 5 &&
+					grid[i - 1][j - 1] &&
+					grid[i - 1][j] &&
+					grid[i - 1][j + 1] &&
+					grid[i][j - 1] &&
+					grid[i][j + 1] &&
+					grid[i + 1][j - 1] &&
+					grid[i + 1][j] &&
+					grid[i + 1][j + 1])
+				{
+					if (grid[i - 1][j] + grid[i + 1][j] == 10 &&
+						grid[i][j - 1] + grid[i][j + 1] == 10 &&
+						grid[i - 1][j - 1] + grid[i + 1][j + 1] == 10 &&
+						grid[i - 1][j + 1] + grid[i + 1][j - 1] == 10 &&
+						grid[i - 1][j - 1] + grid[i - 1][j] + grid[i - 1][j + 1] == 15 &&
+						grid[i + 1][j - 1] + grid[i + 1][j] + grid[i + 1][j + 1] == 15 &&
+						grid[i - 1][j - 1] + grid[i][j - 1] + grid[i + 1][j - 1] == 15 &&
+						grid[i - 1][j + 1] + grid[i][j + 1] + grid[i + 1][j + 1] == 15)
+						++res;
+				}
+			}
+		}
+		return res;
+	}
+};
