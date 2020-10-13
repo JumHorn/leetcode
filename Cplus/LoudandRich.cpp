cb5e54e95275253768f37219c9197667ac146c60
JumHorn <JumHorn@gmail.com>
2020-10-13 21:08:07 +0800

    Loud and Rich beats 98.98%

--- Cplus/LoudandRich.cpp
+++ Cplus/LoudandRich.cpp
@@ -7,31 +7,27 @@ class Solution
 public:
 	vector<int> loudAndRich(vector<vector<int>> &richer, vector<int> &quiet)
 	{
-		int n = quiet.size();
-		vector<vector<int>> graph(n, vector<int>(n));
-		for (int i = 0; i < (int)richer.size(); i++)
-			graph[richer[i][1]][richer[i][0]] = 1;
-		vector<int> res(n, n);
-		for (int i = 0; i < n; i++)
+		int N = quiet.size();
+		vector<vector<int>> graph(N);
+		for (auto &rich : richer)
+			graph[rich[1]].push_back(rich[0]);
+		vector<int> res(N, N);
+		for (int i = 0; i < N; ++i)
 			dfs(graph, quiet, res, i);
 		return res;
 	}
 
 	int dfs(vector<vector<int>> &graph, vector<int> &quiet, vector<int> &res, int index)
 	{
-		int n = graph.size();
-		if (res[index] < n)
+		int N = graph.size();
+		if (res[index] < N)
 			return res[index];
-		if (res[index] >= n)
-			res[index] = index;
-		for (int i = 0; i < n; i++)
+		res[index] = index;
+		for (auto n : graph[index])
 		{
-			if (graph[index][i] == 1)
-			{
-				int tmp = dfs(graph, quiet, res, i);
-				if (quiet[tmp] < quiet[res[index]])
-					res[index] = tmp;
-			}
+			int person = dfs(graph, quiet, res, n);
+			if (quiet[person] < quiet[res[index]])
+				res[index] = person;
 		}
 		return res[index];
 	}
