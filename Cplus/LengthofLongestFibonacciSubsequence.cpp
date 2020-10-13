3d572ac7336ee6803eda49bdd431c569734575b2
JumHorn <JumHorn@gmail.com>
2020-10-13 11:13:01 +0800

    Length of Longest Fibonacci Subsequence beats 24.26%

--- Cplus/LengthofLongestFibonacciSubsequence.cpp
+++ Cplus/LengthofLongestFibonacciSubsequence.cpp
@@ -1,11 +1,10 @@
-#include<vector>
-#include<list>
-#include<unordered_map>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 /*
 dp[i][j]
 	1	2	3	4	5	6	7	8
-1	X	X	X	X	X	X	X	X			
+1	X	X	X	X	X	X	X	X
 2	2	X	X	X	X	X	X	X
 3	2	3	X	X	X	X	X	X
 4	2			X	X	X	X	X
@@ -15,31 +14,30 @@ dp[i][j]
 8	2							X
 */
 
-class Solution {
+class Solution
+{
 public:
-    int lenLongestFibSubseq(vector<int>& A) {
-		unordered_map<int,int> index;
-		for(int i=0;i<A.size();i++)
-			index[A[i]]=i;
-		int res=0;
-		vector<vector<int> > dp(A.size(),vector<int>(A.size(),2));
-		for(int i=1;i<A.size();i++)
+	int lenLongestFibSubseq(vector<int> &A)
+	{
+		int res = 0, N = A.size();
+		unordered_map<int, int> index;
+		for (int i = 0; i < N; ++i)
+			index[A[i]] = i;
+		vector<vector<int>> dp(N, vector<int>(N));
+		for (int i = 1; i < N; ++i)
 		{
-			for(int j=0;j<i;j++)
+			for (int j = 0; j < i; ++j)
 			{
-				if(A[i]-A[j]<A[j]&&index.count(A[i]-A[j]))
+				dp[i][j] = 2;
+				if (index.find(A[i] - A[j]) != index.end())
 				{
-					int k=index[A[i]-A[j]];
-					dp[i][j]=dp[j][k]+1;
-					if(dp[i][j]>res)
-						res=dp[i][j];
-				}
-				else
-				{
-					dp[i][j]=2;
+					int k = index[A[i] - A[j]];
+					if (k < j)
+						dp[i][j] = dp[j][k] + 1;
+					res = max(res, dp[i][j]);
 				}
 			}
 		}
-		return res==2?0:res;
-    }
+		return res <= 2 ? 0 : res;
+	}
 };
