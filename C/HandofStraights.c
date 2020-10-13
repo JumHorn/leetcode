defc6285ccb014792b07528862871df8faa80d77
JumHorn <JumHorn@gmail.com>
2020-10-13 20:40:40 +0800

    Hand of Straights beats 100%

---
+++ C/HandofStraights.c
@@ -0,0 +1,43 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+//cmp function don't consider overflow
+int cmp(const void *lhs, const void *rhs)
+{
+	return *(int *)lhs - *(int *)rhs;
+}
+
+bool isNStraightHand(int *hand, int handSize, int W)
+{
+	if (handSize % W != 0)
+		return false;
+	qsort(hand, handSize, sizeof(int), cmp);
+	int arr[handSize][2], arrSize = 0;  //{val,count}
+	for (int i = 1, j = 0; i <= handSize; ++i)
+	{
+		if (i == handSize || hand[i] != hand[j])
+		{
+			arr[arrSize][0] = hand[j];
+			arr[arrSize][1] = i - j;
+			j = i;
+			++arrSize;
+		}
+	}
+	for (int i = 0; i < arrSize; ++i)
+	{
+		if (arr[i][1] == 0)
+			continue;
+		int count = arr[i][1];
+		if (i + W - 1 >= arrSize)
+			return false;
+		for (int j = 0; j < W; ++j)
+		{
+			if (arr[i + j][0] - arr[i][0] != j)  //consecutive
+				return false;
+			arr[i + j][1] -= count;
+			if (arr[i + j][1] < 0)
+				return false;
+		}
+	}
+	return true;
+}
