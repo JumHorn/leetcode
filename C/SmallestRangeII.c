50f50cb9a5615c3662d615bd9e7062c814781e68
JumHorn <JumHorn@gmail.com>
2020-10-14 20:17:54 +0800

    formatted

--- C/SmallestRangeII.c
+++ C/SmallestRangeII.c
@@ -1,24 +1,24 @@
 #include <stdlib.h>
 
-int cmp(const void* lhs, const void* rhs)
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+
+//cmp function don't consider overflow
+int cmp(const void *lhs, const void *rhs)
 {
-	return *(int*)lhs - *(int*)rhs;
+	return *(int *)lhs - *(int *)rhs;
 }
 
-int smallestRangeII(int* A, int ASize, int K)
+int smallestRangeII(int *A, int ASize, int K)
 {
 	qsort(A, ASize, sizeof(int), cmp);
-	int res = A[ASize - 1] - A[0], max, min;
-	for (int i = 0; i < ASize - 1; i++)
+	int res = A[ASize - 1] - A[0];
+	for (int i = 0; i < ASize - 1; ++i)
 	{
-		max = A[ASize - 1] - K;
-		if (A[i] + K > max)
-			max = A[i] + K;
-		min = A[0] + K;
-		if (A[i + 1] - K < min)
-			min = A[i + 1] - K;
-		if (max - min < res)
-			res = max - min;
+		int a = A[i], b = A[i + 1];
+		int high = max(A[ASize - 1] - K, a + K);
+		int low = min(A[0] + K, b - K);
+		res = min(res, high - low);
 	}
 	return res;
 }
