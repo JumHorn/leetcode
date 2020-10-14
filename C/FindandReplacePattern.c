90f2541b5e018228e4b98771deeded8ec890fc41
JumHorn <JumHorn@gmail.com>
2020-10-14 10:43:07 +0800

    formatted

--- C/FindandReplacePattern.c
+++ C/FindandReplacePattern.c
@@ -4,13 +4,13 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-char** findAndReplacePattern(char** words, int wordsSize, char* pattern, int* returnSize)
+char **findAndReplacePattern(char **words, int wordsSize, char *pattern, int *returnSize)
 {
-	int* index = (int*)malloc(sizeof(int) * wordsSize);
+	int index[wordsSize];
 	memset(index, -1, sizeof(sizeof(int) * wordsSize));
 	*returnSize = 0;
 	int len = strlen(pattern);
-	for (int i = 0, j = 0; i < wordsSize; i++)
+	for (int i = 0; i < wordsSize; ++i)
 	{
 		char mapw[26] = {0}, mapp[26] = {0}, *w = words[i], *p = pattern;
 		while (*w && *p)
@@ -22,29 +22,20 @@ char** findAndReplacePattern(char** words, int wordsSize, char* pattern, int* re
 				mapw[*w - 'a'] = *p;
 				mapp[*p - 'a'] = *w;
 			}
-			else
-			{
-				if (mapw[*w - 'a'] != *p)
-					break;
-			}
-			p++;
-			w++;
+			else if (mapw[*w - 'a'] != *p)
+				break;
+			++p;
+			++w;
 		}
 		if (!*p && !*w)
-		{
-			++(*returnSize);
-			index[j++] = i;
-		}
+			index[(*returnSize)++] = i;
 	}
 
-	if (*returnSize == 0)
-		return NULL;
-	char** res = (char**)malloc(sizeof(char*) * (*returnSize));
-	for (int i = 0; i < *returnSize; i++)
+	char **res = (char **)malloc(sizeof(char *) * (*returnSize));
+	for (int i = 0; i < *returnSize; ++i)
 	{
-		res[i] = (char*)malloc(sizeof(char) * (len + 1));
-		memcpy(res[i], words[index[i]], len + 1);
+		res[i] = (char *)malloc(sizeof(char) * (len + 1));
+		strcpy(res[i], words[index[i]]);
 	}
 	return res;
 }
-
