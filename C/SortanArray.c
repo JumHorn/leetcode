232ef9d5cdf72005ca6d446a9cb5e7afe4554537
JumHorn <JumHorn@gmail.com>
2020-10-14 10:15:10 +0800

    use library function

--- C/SortanArray.c
+++ C/SortanArray.c
@@ -1,15 +1,17 @@
-#include<stdlib.h>
+#include <stdlib.h>
 
-int comp(const void* lhs,const void* rhs)
+//cmp function don't consider overflow
+int cmp(const void *lhs, const void *rhs)
 {
-	return *(int*)lhs-*(int*)rhs;
+	return *(int *)lhs - *(int *)rhs;
 }
+
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* sortArray(int* nums, int numsSize, int* returnSize){
-	*returnSize=numsSize;
-	qsort(nums,numsSize,sizeof(int),comp);
+int *sortArray(int *nums, int numsSize, int *returnSize)
+{
+	*returnSize = numsSize;
+	qsort(nums, numsSize, sizeof(int), cmp);
 	return nums;
 }
-
