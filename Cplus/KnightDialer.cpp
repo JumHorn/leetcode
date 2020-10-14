1e8a3ce531f513bfa708bc6d5cc4c267520578f7
JumHorn <JumHorn@gmail.com>
2020-10-14 21:17:58 +0800

    formatted

--- Cplus/KnightDialer.cpp
+++ Cplus/KnightDialer.cpp
@@ -6,23 +6,23 @@ using namespace std;
 class Solution
 {
 public:
-	int knightDialer(int N)
+	int knightDialer(int n)
 	{
 		vector<vector<int>> graph = {{4, 6}, {6, 8}, {7, 9}, {4, 8}, {0, 3, 9}, {}, {0, 1, 7}, {2, 6}, {1, 3}, {2, 4}};
 		vector<int> dp(10, 1);
-		for (int i = 1; i < N; i++)
+		for (int i = 1; i < n; ++i)
 		{
-			vector<int> tmp(10, 0);
-			for (int j = 0; j < 10; j++)
+			vector<int> nextdp(10);
+			for (int j = 0; j < 10; ++j)
 			{
 				for (int k = 0; k < (int)graph[j].size(); k++)
-					tmp[graph[j][k]] = (tmp[graph[j][k]] + dp[j]) % MOD;
+					nextdp[graph[j][k]] = (nextdp[graph[j][k]] + dp[j]) % MOD;
 			}
-			dp = tmp;
+			dp = nextdp;
 		}
 		int res = 0;
-		for (int i = 0; i < 10; i++)
-			res = (res + dp[i]) % MOD;
+		for (auto n : dp)
+			res = (res + n) % MOD;
 		return res;
 	}
 
