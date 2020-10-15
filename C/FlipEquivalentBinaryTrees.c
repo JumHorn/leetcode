9c371d852c5d409608b4a49ae97a25a36f944ac8
JumHorn <JumHorn@gmail.com>
2020-10-15 22:49:29 +0800

    Flip Equivalent Binary Trees beats 81.20%

--- C/FlipEquivalentBinaryTrees.c
+++ C/FlipEquivalentBinaryTrees.c
@@ -1,23 +1,21 @@
-#include<stdlib.h>
+#include <stdbool.h>
 
 //Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    struct TreeNode *left;
-    struct TreeNode *right;
+struct TreeNode
+{
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
 };
 
-
-
-bool flipEquiv(struct TreeNode* root1, struct TreeNode* root2){
-	if(root1==NULL&&root2==NULL)
+bool flipEquiv(struct TreeNode *root1, struct TreeNode *root2)
+{
+	if (!root1 && !root2)
 		return true;
-	if(root1==NULL||root2==NULL)
+	if (!root1 || !root2)
 		return false;
-	if(root1->val!=root2->val)
+	if (root1->val != root2->val)
 		return false;
-	return (flipEquiv(root1->left,root2->left)&&flipEquiv(root1->right,root2->right))
-		||(flipEquiv(root1->right,root2->left)&&flipEquiv(root1->left,root2->right));
+	return (flipEquiv(root1->left, root2->left) && flipEquiv(root1->right, root2->right)) ||
+		   (flipEquiv(root1->right, root2->left) && flipEquiv(root1->left, root2->right));
 }
-
-
