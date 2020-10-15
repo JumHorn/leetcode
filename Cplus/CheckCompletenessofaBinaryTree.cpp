0ffab58f0e852310b9c5bb7685e958c9a4c24e2d
JumHorn <JumHorn@gmail.com>
2020-10-15 16:20:36 +0800

    Check Completeness of a Binary Tree beats 59.53%

---
+++ Cplus/CheckCompletenessofaBinaryTree.cpp
@@ -0,0 +1,33 @@
+#include <queue>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+	bool isCompleteTree(TreeNode *root)
+	{
+		queue<TreeNode *> q;
+		q.push(root);
+		while (!q.empty() && q.front() != nullptr)
+		{
+			TreeNode *node = q.front();
+			q.pop();
+			q.push(node->left);
+			q.push(node->right);
+		}
+		while (!q.empty() && q.front() == nullptr)
+			q.pop();
+		return q.empty();
+	}
+};
