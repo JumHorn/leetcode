7dbeb77f96e08b6fbad195a40f260a6f5ecf5326
JumHorn <JumHorn@gmail.com>
2020-10-15 14:23:12 +0800

    formatted

--- Cplus/MaximumSumCircularSubarray.cpp
+++ Cplus/MaximumSumCircularSubarray.cpp
@@ -1,32 +1,35 @@
-#include<vector>
-#include<algorithm>
-#include<numeric>
-#include<deque>
+#include <algorithm>
+#include <climits>
+#include <deque>
+#include <numeric>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int maxSubarraySumCircular(vector<int>& A) {
-		int N=A.size(),res=INT_MIN;
-		vector<int> sum(2*N+1);
-		for(int i=1;i<(int)sum.size();i++)
+	int maxSubarraySumCircular(vector<int> &A)
+	{
+		int N = A.size(), res = INT_MIN;
+		vector<int> sum(2 * N + 1);
+		for (int i = 1; i <= 2 * N; ++i)
 		{
-			if(i<=N)
-				sum[i]=sum[i-1]+A[i-1];
+			if (i <= N)
+				sum[i] = sum[i - 1] + A[i - 1];
 			else
-				sum[i]=sum[i-1]+A[i-N-1];
+				sum[i] = sum[i - 1] + A[i - N - 1];
 		}
 		deque<int> monoqueue;
 		monoqueue.push_back(0);
-		for(int i=1;i<(int)sum.size();i++)
+		for (int i = 1; i <= 2 * N; ++i)
 		{
-			if(!monoqueue.empty()&&i-monoqueue.front()>N)
+			if (!monoqueue.empty() && i - monoqueue.front() > N)
 				monoqueue.pop_front();
-			res=max(res,sum[i]-sum[monoqueue.front()]);
-			while(!monoqueue.empty()&&sum[i]<=sum[monoqueue.back()])
+			res = max(res, sum[i] - sum[monoqueue.front()]);
+			while (!monoqueue.empty() && sum[i] <= sum[monoqueue.back()])
 				monoqueue.pop_back();
 			monoqueue.push_back(i);
 		}
 		return res;
-    }
+	}
 };
