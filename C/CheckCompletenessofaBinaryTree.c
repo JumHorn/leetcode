f9c40f5661f6dc236658bcd7b8c7bad8fc7cd4c0
JumHorn <JumHorn@gmail.com>
2020-10-15 16:09:36 +0800

    formatted

--- C/CheckCompletenessofaBinaryTree.c
+++ C/CheckCompletenessofaBinaryTree.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 
 //Definition for a binary tree node.
 struct TreeNode
@@ -14,34 +15,30 @@ int getHeight(struct TreeNode *root)
 	return getHeight(root->left) + 1;
 }
 
-bool dfs(struct TreeNode *root, int layer, int *height, bool *flag)
+bool preorder(struct TreeNode *root, int layer, int *height, bool *flag)
 {
 	if (!root)
 	{
 		if (layer == *height)
 			return true;
-		if (layer + 1 == *height)
+		if (layer + 1 == *height && *flag)
 		{
-			if (*flag)
-			{
-				*flag = false;
-				--*height;
-				return true;
-			}
+			*flag = false;
+			--*height;
+			return true;
 		}
 		return false;
 	}
 	if (layer > *height)
 		return false;
-	if (!dfs(root->left, layer + 1, height, flag))
+	if (!preorder(root->left, layer + 1, height, flag))
 		return false;
-	return dfs(root->right, layer + 1, height, flag);
+	return preorder(root->right, layer + 1, height, flag);
 }
 
 bool isCompleteTree(struct TreeNode *root)
 {
 	int height = getHeight(root);
 	bool flag = true;
-	return dfs(root, 0, &height, &flag);
+	return preorder(root, 0, &height, &flag);
 }
-
