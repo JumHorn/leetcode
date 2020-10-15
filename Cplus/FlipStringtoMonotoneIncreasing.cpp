4d68d5e5b6c606c608eab2d50c469a5aa4ecccad
JumHorn <JumHorn@gmail.com>
2020-10-15 09:39:51 +0800

    Flip String to Monotone Increasing beats 89.04%

--- Cplus/FlipStringtoMonotoneIncreasing.cpp
+++ Cplus/FlipStringtoMonotoneIncreasing.cpp
@@ -1,32 +1,22 @@
-#include<string>
-#include<algorithm>
+#include <algorithm>
+#include <string>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int minFlipsMonoIncr(string S) {
-		int res=getZero(S);        
-		if(res==0)
+	int minFlipsMonoIncr(string S)
+	{
+		int N = S.length(), res;
+		vector<int> prefixsum(N + 1); //sum of '1'
+		for (int i = 0; i < N; ++i)
+			prefixsum[i + 1] += prefixsum[i] + S[i] - '0';
+		if (prefixsum[N] == 0 || prefixsum[N] == N) //all '0' or all '1'
 			return 0;
-		return minFlips(S,0,res);
-    }
-
-	int minFlips(const string& s,int start,int ones)
-	{
-		for(int i=start;i<s.length();i++)
-			if(s[i]=='0')
-				ones--;
-			else
-				return min(ones,minFlips(s,i+1,ones)+1);
-		return ones;
-	}
-
-	int getZero(const string& s)
-	{
-		int total=0;
-		for(int i=0;i<s.length();i++)
-			if(s[i]=='0')
-				total++;
-		return total;
+		res = prefixsum[N];
+		for (int i = 0; i < N; ++i)
+			res = min(res, prefixsum[i] + (N - i) - (prefixsum[N] - prefixsum[i]));
+		return res;
 	}
 };
