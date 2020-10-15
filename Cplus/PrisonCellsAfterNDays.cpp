06feaa345bf18d5f390fa38a06936ac84822c7b1
JumHorn <JumHorn@gmail.com>
2020-10-15 16:57:31 +0800

    formatted

--- Cplus/PrisonCellsAfterNDays.cpp
+++ Cplus/PrisonCellsAfterNDays.cpp
@@ -7,50 +7,55 @@ class Solution
 public:
 	vector<int> prisonAfterNDays(vector<int> &cells, int N)
 	{
-		vector<int> update = cells;
-		unordered_map<int, int> m;
-		vector<int> v;
+		unordered_map<int, int> m; //{compressed data,index}
+		vector<int> state;
 		int index = 0;
-		for (int i = 1; i < 7; i++)
-			update[i] = 1 - (cells[i - 1] ^ cells[i + 1]);
-		update[0] = update[7] = 0;
-		for (int i = 1; i < N; i++)
+		cells = update(cells);
+		for (int i = 1; i < N; ++i)
 		{
-			int tmp = compress(update);
-			if (m.find(tmp) != m.end())
+			int data = compress(cells);
+			if (m.find(data) != m.end())
 			{
 				N = N - i;
-				int c = v.size() - m[tmp];
-				int ii = m[tmp] + N % c;
-				return uncompress(v[ii]);
+				int T = state.size() - m[data];
+				int finalindex = m[data] + N % T;
+				return uncompress(state[finalindex]);
 			}
 			else
 			{
-				m[tmp] = index++;
-				v.push_back(tmp);
+				m[data] = index++;
+				state.push_back(data);
 			}
 
-			cells = update;
-			for (int i = 1; i < 7; i++)
-				update[i] = 1 - (cells[i - 1] ^ cells[i + 1]);
+			cells = update(cells);
 		}
-		return update;
+		return cells;
+	}
+
+	vector<int> update(vector<int> &cells)
+	{
+		vector<int> nextcells(8);
+		for (int i = 1; i < 7; ++i)
+			nextcells[i] = (1 ^ cells[i - 1] ^ cells[i + 1]);
+		return nextcells;
 	}
 
 	int compress(vector<int> &cells)
 	{
 		int res = 0;
-		for (int i = 0; i < (int)cells.size(); i++)
+		for (int i = 0; i < (int)cells.size(); ++i)
+		{
 			if (cells[i] == 1)
 				res |= (1 << i);
+		}
 		return res;
 	}
 
 	vector<int> uncompress(int n)
 	{
 		vector<int> cells(8);
-		for (int i = 0; i < 8; i++)
-			cells[i] = ((n & (1 << i)) >> i);
+		for (int i = 0; i < 8; ++i)
+			cells[i] = ((n >> i) & 1);
 		return cells;
 	}
 };
