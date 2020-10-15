2173f651700c73a1a2c175a6847b465896eca6d5
JumHorn <JumHorn@gmail.com>
2020-10-15 23:20:28 +0800

    Largest Time for Given Digits beats 100%

--- C/LargestTimeforGivenDigits.c
+++ C/LargestTimeforGivenDigits.c
@@ -1,64 +1,49 @@
-#include<stdlib.h>
-#define swap(a,b)if((a)!=(b))(a)^=(b)^=(a)^=(b)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-char * largestTimeFromDigits(int* A, int ASize){
-	char* res=(char*)malloc(sizeof(char)*(ASize+2));
-	for(int i=0;i<ASize-1;i++)
-		for(int j=0;j<ASize-1-i;j++)
-			if(A[j]>A[j+1])
-				swap(A[j],A[j+1]);
-	res[0]='\0';
-	if(A[0]>2)
-		return res;
-	if(A[0]==2)
-	{
-		if(A[1]>3||A[2]>5)
-			return res;
-	}
-	else if(A[0]==1||A[0]==0)
-	{
-		if(A[1]>5)
-			return res;
-	}
-	int B[4]={0};
-	int i=3;
-	while(A[i]>2)
-		i--;
-	res[0]=A[i]+'0';
-	B[i]=-1;
-label:
-	i=3;
-	while(true)
-	{
-		if(B[i]==-1||(res[0]=='2'&&A[i]>3))
-			i--;
-		else
-		{
-			res[1]=A[i]+'0';
-			B[i]=-1;
-			break;
-		}
-	}
-	res[2]=':';
-	i=3;
-	while(i>=0&&(A[i]>5||B[i]==-1))
-		i--;
-	if(i<0)
-	{
-		B[1]=B[2]=B[3]=0;
-		B[0]=-1;
-		res[0]=A[0]+'0';
-		goto label;
-	}
-
-	res[3]=A[i]+'0';
-	B[i]=-1;
-
-	i=3;
-	while(B[i]==-1)
-		i--;
-	res[4]=A[i]+'0';
-	res[5]='\0';
-	return res;
+void swap(int *lhs, int *rhs)
+{
+	int tmp = *lhs;
+	*lhs = *rhs;
+	*rhs = tmp;
 }
 
+char *largestTimeFromDigits(int *arr, int arrSize)
+{
+	int maxhour = -1, maxminute = -1;
+	for (int i = 0; i < 4; ++i)
+	{
+		swap(&arr[0], &arr[i]);
+		for (int j = 1; j < 4; ++j)
+		{
+			swap(&arr[1], &arr[j]);
+			int hour = arr[0] * 10 + arr[1];
+			if (hour < 24)
+			{
+				for (int k = 2; k < 4; ++k)
+				{
+					swap(&arr[2], &arr[k]);
+					int minute = arr[2] * 10 + arr[3];
+					if (minute < 59)
+					{
+						if ((hour > maxhour) || (hour == maxhour && minute > maxminute))
+						{
+							maxhour = hour;
+							maxminute = minute;
+						}
+					}
+					swap(&arr[2], &arr[k]);
+				}
+			}
+			swap(&arr[1], &arr[j]);
+		}
+		swap(&arr[0], &arr[i]);
+	}
+	if (maxhour < 0)
+		return "";
+	char *res = (char *)malloc(sizeof(char) * 6);
+	res[5] = '\0';
+	sprintf(res, "%02d:%02d", maxhour, maxminute);
+	return res;
+}
