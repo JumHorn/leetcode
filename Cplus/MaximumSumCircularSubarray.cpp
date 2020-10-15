2993f113ca2ab9dc6cced4d291e096b2e62bdf60
JumHorn <JumHorn@gmail.com>
2020-10-15 15:06:50 +0800

    Maximum Sum Circular Subarray beats 98.82%

--- Cplus/MaximumSumCircularSubarray.cpp
+++ Cplus/MaximumSumCircularSubarray.cpp
@@ -1,35 +1,28 @@
 #include <algorithm>
 #include <climits>
-#include <deque>
-#include <numeric>
 #include <vector>
 using namespace std;
+
+/*
+Algorithm
+Kadane's (Max Variant)
+Kadane's (Min Variant)
+*/
 
 class Solution
 {
 public:
 	int maxSubarraySumCircular(vector<int> &A)
 	{
-		int N = A.size(), res = INT_MIN;
-		vector<int> sum(2 * N + 1);
-		for (int i = 1; i <= 2 * N; ++i)
+		int minSum = INT_MAX, curMin = 0, maxSum = INT_MIN, curMax = 0, sum = 0;
+		for (auto n : A)
 		{
-			if (i <= N)
-				sum[i] = sum[i - 1] + A[i - 1];
-			else
-				sum[i] = sum[i - 1] + A[i - N - 1];
+			sum += n;
+			curMax = max(curMax, 0) + n;
+			maxSum = max(maxSum, curMax);
+			curMin = min(curMin, 0) + n;
+			minSum = min(minSum, curMin);
 		}
-		deque<int> monoqueue;
-		monoqueue.push_back(0);
-		for (int i = 1; i <= 2 * N; ++i)
-		{
-			if (!monoqueue.empty() && i - monoqueue.front() > N)
-				monoqueue.pop_front();
-			res = max(res, sum[i] - sum[monoqueue.front()]);
-			while (!monoqueue.empty() && sum[i] <= sum[monoqueue.back()])
-				monoqueue.pop_back();
-			monoqueue.push_back(i);
-		}
-		return res;
+		return maxSum > 0 ? max(maxSum, sum - minSum) : maxSum;
 	}
 };
