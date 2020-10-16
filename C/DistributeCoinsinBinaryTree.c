cd437dc45155e221301d1025cc9e190885315119
JumHorn <JumHorn@gmail.com>
2020-10-16 17:04:44 +0800

    formatted

--- C/DistributeCoinsinBinaryTree.c
+++ C/DistributeCoinsinBinaryTree.c
@@ -1,22 +1,23 @@
-#include<stdlib.h>
+#include <stdlib.h>
+
 //Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    struct TreeNode *left;
-    struct TreeNode *right;
+struct TreeNode
+{
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
 };
 
-
-
-int distributeCoins(struct TreeNode* root){
-	if(!root)
+int distributeCoins(struct TreeNode *root)
+{
+	if (!root)
 		return 0;
-	int left=distributeCoins(root->left);
-	int right=distributeCoins(root->right);
-	root->val-=1;
-	if(root->left)
-		root->val+=root->left->val;
-	if(root->right)
-		root->val+=root->right->val;
-	return left+right+abs(root->val);
+	int left = distributeCoins(root->left);
+	int right = distributeCoins(root->right);
+	--root->val;
+	if (root->left)
+		root->val += root->left->val;
+	if (root->right)
+		root->val += root->right->val;
+	return left + right + abs(root->val);
 }
