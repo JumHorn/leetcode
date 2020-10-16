7d1ef5cab9b729252ce0c6356d957a550af00f6a
JumHorn <JumHorn@gmail.com>
2020-10-16 17:23:59 +0800

    Subarray Sums Divisible by K beats 80.00%

---
+++ C/SubarraySumsDivisiblebyK.c
@@ -0,0 +1,18 @@
+
+
+int subarraysDivByK(int* A, int ASize, int K)
+{
+	int hash[10001] = {0};
+	int res = 0, sum = 0;
+	hash[0] = 1;
+	for (int i = 0; i < ASize; ++i)
+		A[i] = (A[i] % K + K) % K;
+	for (int i = 0; i < ASize; ++i)
+	{
+		sum += A[i];
+		int val = sum % K;
+		res += hash[val];
+		++hash[val];
+	}
+	return res;
+}
