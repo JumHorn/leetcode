a7fc628627f4a33b6da84e79e744d1c32178c347
JumHorn <JumHorn@gmail.com>
2020-10-16 17:33:11 +0800

    formatted

--- Cplus/KClosestPointstoOrigin.cpp
+++ Cplus/KClosestPointstoOrigin.cpp
@@ -1,19 +1,21 @@
-#include<vector>
-#include<queue>
+#include <queue>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
-		priority_queue<pair<int,int>> q;
-		for(int i=0;i<(int)points.size();i++)
-			q.push({-(points[i][0]*points[i][0]+points[i][1]*points[i][1]),i});
+	vector<vector<int>> kClosest(vector<vector<int>> &points, int K)
+	{
+		priority_queue<pair<int, int>> q;
+		for (int i = 0; i < (int)points.size(); ++i)
+			q.push({-(points[i][0] * points[i][0] + points[i][1] * points[i][1]), i});
 		vector<vector<int>> res(K);
-		for(int i=0;i<K;i++)
+		for (int i = 0; i < K; ++i)
 		{
-			res[i]=points[q.top().second];
+			res[i] = points[q.top().second];
 			q.pop();
 		}
 		return res;
-    }
+	}
 };
