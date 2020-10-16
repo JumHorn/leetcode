d8c3f5c776be8933b233fd610ad69578e4b4e4f0
JumHorn <JumHorn@gmail.com>
2020-10-16 23:48:07 +0800

    Vowel Spellchecker beats 90.20%

--- Cplus/VowelSpellchecker.cpp
+++ Cplus/VowelSpellchecker.cpp
@@ -5,142 +5,50 @@
 #include <vector>
 using namespace std;
 
-class Trie
-{
-private:
-	struct TreeNode
-	{
-		TreeNode(int val = -1) : index(val) {}
-		int index;
-		unordered_map<char, TreeNode *> node;
-	};
-
-public:
-	Trie()
-	{
-		root = new TreeNode();
-		m_vowel = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
-	}
-
-	void insert(const string &str, int index)
-	{
-		TreeNode *tmp = root;
-		for (auto c : str)
-		{
-			if (tmp->node.find(c) == tmp->node.end())
-				tmp->node[c] = new TreeNode();
-			tmp = tmp->node[c];
-		}
-		if (tmp->index == -1)
-			tmp->index = index;
-	}
-
-	int search(const string &str)
-	{
-		TreeNode *tmp = root;
-		for (auto c : str)
-		{
-			if (tmp->node.find(c) == tmp->node.end())
-				return -1;
-			tmp = tmp->node[c];
-		}
-		return tmp->index;
-	}
-
-	int caseInsensitiveSearch(const string &str)
-	{
-		int res = INT_MAX;
-		caseInsensitiveSearch(str, 0, root, res);
-		if (res == INT_MAX)
-			return -1;
-		return res;
-	}
-
-	int vowelSearch(const string &str)
-	{
-		int res = INT_MAX;
-		vowelSearch(str, 0, root, res);
-		if (res == INT_MAX)
-			return -1;
-		return res;
-	}
-
-private:
-	void caseInsensitiveSearch(const string &str, int index, TreeNode *root, int &res)
-	{
-		if (index >= (int)str.length())
-		{
-			if (root->index != -1)
-				res = min(res, root->index);
-			return;
-		}
-		char c = tolower(str[index]);
-		if (root->node.find(c) != root->node.end())
-			caseInsensitiveSearch(str, index + 1, root->node[c], res);
-		c = toupper(c);
-		if (root->node.find(c) != root->node.end())
-			caseInsensitiveSearch(str, index + 1, root->node[c], res);
-	}
-
-	void vowelSearch(const string &str, int index, TreeNode *root, int &res)
-	{
-		if (index >= (int)str.length())
-		{
-			if (root->index != -1)
-				res = min(res, root->index);
-			return;
-		}
-		char c = str[index];
-		if (m_vowel.find(c) == m_vowel.end())
-		{
-			c = tolower(c);
-			if (root->node.find(c) != root->node.end())
-				vowelSearch(str, index + 1, root->node[c], res);
-			c = toupper(c);
-			if (root->node.find(c) != root->node.end())
-				vowelSearch(str, index + 1, root->node[c], res);
-			return;
-		}
-		for (auto c : m_vowel)
-		{
-			if (root->node.find(c) != root->node.end())
-				vowelSearch(str, index + 1, root->node[c], res);
-		}
-	}
-
-	TreeNode *root;
-	unordered_set<char> m_vowel;
-};
-
 class Solution
 {
 public:
 	vector<string> spellchecker(vector<string> &wordlist, vector<string> &queries)
 	{
-		vector<string> res;
-		Trie trie;
-		for (int i = 0; i < (int)wordlist.size(); i++)
-			trie.insert(wordlist[i], i);
+		unordered_set<string> words(wordlist.begin(), wordlist.end());
+		unordered_map<string, string> cap, vowel;
+		for (auto &w : wordlist)
+		{
+			string lower = strToLower(w), devowel = devowelPattern(w);
+			cap.insert({lower, w});
+			vowel.insert({devowel, w});
+		}
+
 		for (auto &query : queries)
 		{
-			int index = trie.search(query);
-			if (index != -1)
-			{
-				res.push_back(wordlist[index]);
+			if (words.find(query) != words.end())
 				continue;
-			}
-			index = trie.caseInsensitiveSearch(query);
-			if (index != -1)
-			{
-				res.push_back(wordlist[index]);
-				continue;
-			}
-			index = trie.vowelSearch(query);
-			if (index != -1)
-				res.push_back(wordlist[index]);
+			string lower = strToLower(query), devowel = devowelPattern(query);
+			if (cap.find(lower) != cap.end())
+				query = cap[lower];
+			else if (vowel.find(devowel) != vowel.end())
+				query = vowel[devowel];
 			else
-				res.push_back("");
+				query = "";
 		}
-		return res;
+		return queries;
+	}
+
+	string strToLower(string word)
+	{
+		for (auto &c : word)
+			c = tolower(c);
+		return word;
+	}
+
+	string devowelPattern(string word)
+	{
+		for (auto &c : word)
+		{
+			c = tolower(c);
+			if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+				c = '#';
+		}
+		return word;
 	}
 };
