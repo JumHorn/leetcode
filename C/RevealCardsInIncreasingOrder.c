8288448db238ddc45b103bc1ab1458690cf27aaf
JumHorn <JumHorn@gmail.com>
2020-10-16 13:15:45 +0800

    formatted

--- C/RevealCardsInIncreasingOrder.c
+++ C/RevealCardsInIncreasingOrder.c
@@ -1,42 +1,33 @@
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+
+//cmp function don't consider overflow
+int cmp(const void *lhs, const void *rhs)
+{
+	return *(int *)lhs - *(int *)rhs;
+}
 
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-
-int cmp(const void* lhs, const void* rhs)
-{
-	return *(int*)lhs - *(int*)rhs;
-}
-
-int* deckRevealedIncreasing(int* deck, int deckSize, int* returnSize)
+int *deckRevealedIncreasing(int *deck, int deckSize, int *returnSize)
 {
 	*returnSize = deckSize;
-	int* res = (int*)malloc(sizeof(int) * deckSize);
+	int *res = (int *)malloc(sizeof(int) * deckSize);
 	memset(res, 0, sizeof(int) * deckSize);
 	qsort(deck, deckSize, sizeof(int), cmp);
-	int count = 0, index = -1;
-	while (count < deckSize)
+	int count = 0, index = 0;
+	while (true)
 	{
-		while (true)
-		{
-			index = (index + 1) % deckSize;
-			if (res[index] == 0)
-			{
-				res[index] = deck[count++];
-				break;
-			}
-		}
+		res[index] = deck[count++];
 		if (count >= deckSize)
 			break;
-		while (true)
-		{
+		while (res[index] != 0)
 			index = (index + 1) % deckSize;
-			if (res[index] == 0)
-				break;
-		}
+		index = (index + 1) % deckSize;
+		while (res[index] != 0)
+			index = (index + 1) % deckSize;
 	}
 	return res;
 }
-
