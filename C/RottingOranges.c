f102f828bf39c9e5eaf0a5bd0683de8ae53be0f9
JumHorn <JumHorn@gmail.com>
2020-10-17 09:08:15 +0800

    Rotting Oranges beats 100%

--- C/RottingOranges.c
+++ C/RottingOranges.c
@@ -1,54 +1,50 @@
+#include <stdbool.h>
 
-
-int rotting(int** grid,int gridSize,int colSize,int rotnum)
+bool rotting(int **grid, int gridSize, int colSize, int rotnum)
 {
-	int res=0;
-	for(int i=0;i<gridSize;i++)
-		for(int j=0;j<colSize;j++)
+	bool res = false;
+	for (int i = 0; i < gridSize; ++i)
+	{
+		for (int j = 0; j < colSize; ++j)
 		{
-			if(grid[i][j]==rotnum)
+			if (grid[i][j] == rotnum)
 			{
-				if(i>0&&grid[i-1][j]==1)
+				//board dfs direction
+				int path[5] = {-1, 0, 1, 0, -1};
+				for (int k = 0; k < 4; ++k)
 				{
-					res++;
-					grid[i-1][j]=rotnum+1;
-				}
-				if(i<gridSize-1&&grid[i+1][j]==1)
-				{
-					res++;
-					grid[i+1][j]=rotnum+1;
-				}
-				if(j>0&&grid[i][j-1]==1)
-				{
-					res++;
-					grid[i][j-1]=rotnum+1;
-				}
-				if(j<colSize-1&&grid[i][j+1]==1)
-				{
-					res++;
-					grid[i][j+1]=rotnum+1;
+					int dx = i + path[k], dy = j + path[k + 1];
+					if (dx >= 0 && dx < gridSize && dy >= 0 && dy < colSize && grid[dx][dy] == 1)
+					{
+						res = true;
+						grid[dx][dy] = rotnum + 1;
+					}
 				}
 			}
 		}
+	}
 	return res;
 }
 
-int checkGrid(int** grid,int gridSize,int colSize)
+bool checkGrid(int **grid, int gridSize, int colSize)
 {
-	int res=0;
-	for(int i=0;i<gridSize;i++)
-		for(int j=0;j<colSize;j++)
-			if(grid[i][j]==1)
-				res++;
-	return res;
+	for (int i = 0; i < gridSize; ++i)
+	{
+		for (int j = 0; j < colSize; ++j)
+		{
+			if (grid[i][j] == 1)
+				return false;
+		}
+	}
+	return true;
 }
 
-int orangesRotting(int** grid, int gridSize, int* gridColSize){
-	int step=0,rotnum=2;
-	while(rotting(grid,gridSize,gridColSize[0],rotnum++)>0)
-		step++;
-	if(checkGrid(grid,gridSize,gridColSize[0])>0)
+int orangesRotting(int **grid, int gridSize, int *gridColSize)
+{
+	int step = 0, rotnum = 1;
+	while (rotting(grid, gridSize, gridColSize[0], ++rotnum))
+		++step;
+	if (!checkGrid(grid, gridSize, gridColSize[0]))
 		return -1;
 	return step;
 }
-
