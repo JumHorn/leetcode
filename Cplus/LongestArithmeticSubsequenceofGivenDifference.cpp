c1a2784f6e8efde9db14abcaa94ed5e9bb8a9aa9
JumHorn <JumHorn@gmail.com>
2020-10-18 22:35:45 +0800

    Longest Arithmetic Subsequence of Given Difference beats 79.31%

--- Cplus/LongestArithmeticSubsequenceofGivenDifference.cpp
+++ Cplus/LongestArithmeticSubsequenceofGivenDifference.cpp
@@ -6,26 +6,15 @@ using namespace std;
 class Solution
 {
 public:
-	int longestSubsequence(vector<int>& arr, int difference)
+	int longestSubsequence(vector<int> &arr, int difference)
 	{
-		vector<int> dp(arr.size(), 1);
-		unordered_map<int, int> m;
-		m[arr[0] + difference] = 0;
-		for (int i = 1; i < (int)arr.size(); i++)
+		int res = 0;
+		unordered_map<int, int> m; //{value,length}
+		for (auto n : arr)
 		{
-			if (m.find(arr[i]) != m.end())
-			{
-				dp[i] += dp[m[arr[i]]];
-			}
-			m[arr[i] + difference] = i;
+			m[n] = m[n - difference] + 1;
+			res = max(res, m[n]);
 		}
-		//for (int i = 1; i < (int)arr.size(); i++)
-		//for (int j = i - 1; j >= 0; j--)
-		//	if (arr[i] - arr[j] == difference)
-		//	{
-		//		dp[i] += dp[j];
-		//		break;
-		//	}
-		return *max_element(dp.begin(), dp.end());
+		return res;
 	}
 };
