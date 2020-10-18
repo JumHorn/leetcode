64b8315e54f16e0f52c1478fd227a9d2555387b7
JumHorn <JumHorn@gmail.com>
2020-10-18 21:39:44 +0800

    Maximum Sum of Two Non-Overlapping Subarrays beats 77.74%

--- Cplus/MaximumSumofTwoNon-OverlappingSubarrays.cpp
+++ Cplus/MaximumSumofTwoNon-OverlappingSubarrays.cpp
@@ -8,29 +8,22 @@ class Solution
 public:
 	int maxSumTwoNoOverlap(vector<int> &A, int L, int M)
 	{
-		int res = 0, n = A.size();
-		if (L > M)
-			swap(L, M);
-		vector<int> window(n - L + 1);
-		window[0] = accumulate(A.begin(), A.begin() + L, 0);
-		for (int i = 1; i <= n - L; i++)
-			window[i] = window[i - 1] - A[i - 1] + A[i + L - 1];
-		int left = 0, right = *max_element(window.begin() + M, window.end());
-		int m = accumulate(A.begin(), A.begin() + M, 0);
-		res = right + m;
-		for (int i = M; i < n; i++)
+		return max(sumTwoWindow(A, L, M), sumTwoWindow(A, M, L));
+	}
+
+	int sumTwoWindow(vector<int> &A, int L, int M)
+	{
+		int N = A.size();
+		int windowL = accumulate(A.begin(), A.begin() + L, 0);
+		int windowM = accumulate(A.begin() + L, A.begin() + L + M, 0);
+		int maxL = windowL;
+		int res = windowL + windowM;
+		for (int i = L + M, j = L; i < N; ++i, ++j)
 		{
-			if (i - M - L >= 0)
-				left = max(left, window[i - M - L]);
-			if (i + L < n)
-			{
-				if (right == window[i])
-					right = *max_element(window.begin() + i + 1, window.end());
-			}
-			else
-				right = 0;
-			m = m - A[i - M] + A[i];
-			res = max(res, m + max(left, right));
+			windowL = windowL - A[j - L] + A[j];
+			windowM = windowM - A[i - M] + A[i];
+			maxL = max(maxL, windowL);
+			res = max(res, maxL + windowM);
 		}
 		return res;
 	}
