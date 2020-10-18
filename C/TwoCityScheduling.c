67d6d65cc3e7bcc1bc54b049efdf3b14394f61bb
JumHorn <JumHorn@gmail.com>
2020-10-18 22:14:45 +0800

    Two City Scheduling beats 98.95%

--- C/TwoCityScheduling.c
+++ C/TwoCityScheduling.c
@@ -1,30 +1,24 @@
-#include<stdlib.h>
+#include <stdlib.h>
 
-typedef struct _Weight
+//cmp function don't consider overflow
+int cmp(const void *lhs, const void *rhs)
 {
-	int weight;
-	int index;
-}Weight;
-
-int comp(const void *lhs,const void *rhs)
-{
-	Weight* l=(Weight*)lhs;
-	Weight* r=(Weight*)rhs;
-	return l->weight-r->weight;
+	return *(int *)lhs - *(int *)rhs;
 }
 
-int twoCitySchedCost(int** costs, int costsSize, int* costsColSize){
-	Weight *w=(Weight*)malloc(costsSize*sizeof(Weight));
-	for(int i=0;i<costsSize;i++)
+int twoCitySchedCost(int **costs, int costsSize, int *costsColSize)
+{
+	int arr[costsSize][2]; //{diff,index}
+	for (int i = 0; i < costsSize; ++i)
 	{
-		w[i].index=i;
-		w[i].weight=costs[i][1]-costs[i][0];
+		arr[i][0] = costs[i][1] - costs[i][0];
+		arr[i][1] = i;
 	}
-	qsort(w,costsSize,sizeof(Weight),comp);
-	int res=0;
-	for(int i=0;i<costsSize/2;i++)
-		res+=costs[w[i].index][1];
-	for(int i=costsSize/2;i<costsSize;i++)
-		res+=costs[w[i].index][0];
+	qsort(arr, costsSize, sizeof(arr[0]), cmp);
+	int res = 0;
+	for (int i = 0; i < costsSize / 2; ++i)
+		res += costs[arr[i][1]][1];
+	for (int i = costsSize / 2; i < costsSize; ++i)
+		res += costs[arr[i][1]][0];
 	return res;
 }
---
+++ Cplus/TwoCityScheduling.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+	int twoCitySchedCost(vector<vector<int>> &costs)
+	{
+		int N = costs.size();
+		vector<pair<int, int>> arr;
+		for (int i = 0; i < N; ++i)
+			arr.push_back({costs[i][1] - costs[i][0], i});
+		sort(arr.begin(), arr.end());
+		int res = 0;
+		for (int i = 0; i < N / 2; ++i)
+			res += costs[arr[i].second][1] + costs[arr[N / 2 + i].second][0];
+		return res;
+	}
+};
