4b6f1be8df25cc4bc42663c89cf3c63d90eb67cf
JumHorn <JumHorn@gmail.com>
2020-10-18 21:19:03 +0800

    Coloring A Border beats 77.01%

--- Cplus/ColoringABorder.cpp
+++ Cplus/ColoringABorder.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <set>
 #include <vector>
 using namespace std;
 
@@ -7,33 +8,26 @@ class Solution
 public:
 	vector<vector<int>> colorBorder(vector<vector<int>> &grid, int r0, int c0, int color)
 	{
-		dfs(grid, r0, c0, grid[r0][c0]);
-		for (auto &v : grid)
-		{
-			for (auto &n : v)
-			{
-				if (n < 0)
-					n = color;
-			}
-		}
+		set<pair<int, int>> seen;
+		dfs(grid, r0, c0, grid[r0][c0], color, seen);
 		return grid;
 	}
 
-	void dfs(vector<vector<int>> &grid, int row, int col, int color)
+	bool dfs(vector<vector<int>> &grid, int row, int col, int oldcolor, int color, set<pair<int, int>> &seen)
 	{
 		int M = grid.size(), N = grid[0].size();
-		if (row < 0 || row >= M || col < 0 || col >= N || grid[row][col] != color)
-			return;
-		grid[row][col] = -color;
+		if (seen.find({row, col}) != seen.end())
+			return true;
+		if (row < 0 || row >= M || col < 0 || col >= N || grid[row][col] != oldcolor)
+			return false;
+		seen.insert({row, col});
 		//board dfs direction
 		int path[5] = {-1, 0, 1, 0, -1};
+		int res = 0;
 		for (int i = 0; i < 4; ++i)
-			dfs(grid, row + path[i], col + path[i + 1], color);
-		if (row > 0 && row < M - 1 && col > 0 && col < N - 1 &&
-			color == abs(grid[row - 1][col]) &&
-			color == abs(grid[row + 1][col]) &&
-			color == abs(grid[row][col - 1]) &&
-			color == abs(grid[row][col + 1]))
+			res += dfs(grid, row + path[i], col + path[i + 1], oldcolor, color, seen);
+		if (res < 4)
 			grid[row][col] = color;
+		return true;
 	}
 };
