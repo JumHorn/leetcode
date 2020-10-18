c6ba2218c34ce00fc3abd2465225c5185ab3a043
JumHorn <JumHorn@gmail.com>
2020-10-18 23:23:44 +0800

    Longest Arithmetic Subsequence beats 46.78%

--- Cplus/LongestArithmeticSubsequence.cpp
+++ Cplus/LongestArithmeticSubsequence.cpp
@@ -9,12 +9,12 @@ public:
 	int longestArithSeqLength(vector<int> &A)
 	{
 		int N = A.size(), res = 0;
-		vector<unordered_map<int, int>> dp(N); //{diff,length}
+		vector<vector<int>> dp(N, vector<int>(1001)); //{diff,length}
 		for (int i = 0; i < N; ++i)
 		{
 			for (int j = 0; j < i; ++j)
 			{
-				int diff = A[i] - A[j];
+				int diff = A[i] - A[j] + 500;
 				dp[i][diff] = max(2, dp[j][diff] + 1);
 				res = max(res, dp[i][diff]);
 			}
