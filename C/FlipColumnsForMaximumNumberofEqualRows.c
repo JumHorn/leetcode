97038556a5f36f9a79b15d5a46716ab54489b694
JumHorn <JumHorn@gmail.com>
2020-10-19 13:40:20 +0800

    Flip Columns For Maximum Number of Equal Rows beats 100%

--- C/FlipColumnsForMaximumNumberofEqualRows.c
+++ C/FlipColumnsForMaximumNumberofEqualRows.c
@@ -1,38 +1,39 @@
-#include<stdlib.h>
+#include <stdlib.h>
 
-int arrayEquals(int* arr1,int* arr2,int size)
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+
+int arrayEquals(int *arr1, int *arr2, int size)
 {
-	for(int i=0;i<size;i++)
-		if(arr1[i]!=arr2[i])
+	for (int i = 0; i < size; ++i)
+	{
+		if (arr1[i] != arr2[i])
 			return 0;
+	}
 	return 1;
 }
 
-int maxEqualRowsAfterFlips(int** matrix, int matrixSize, int* matrixColSize){
-	int* p=(int*)malloc(*matrixColSize*sizeof(int));
-	int* visited=(int*)calloc(matrixSize,sizeof(int));
-	int res=0;
-	for(int i=0;i<matrixSize;i++)
+void arrayFlip(int *arr, int size)
+{
+	for (int i = 0; i < size; ++i)
+		arr[i] ^= 1;
+}
+
+int maxEqualRowsAfterFlips(int **matrix, int matrixSize, int *matrixColSize)
+{
+	//normalise for first ele in row is 1 then flip all row
+	int M = matrixSize, N = *matrixColSize;
+	for (int i = 0; i < M; ++i)
 	{
-		if(visited[i]!=0)
-			continue;
-		for(int j=0;j<*matrixColSize;j++)
-			p[j]=1-matrix[i][j];
-		int count=0;
-		for(int j=0;j<matrixSize;j++)
-		{
-			if(i==j)
-			{
-				count++;
-				visited[i]=1;
-			}
-			if(visited[i]!=0)
-				continue;
-			if(arrayEquals(matrix[i],matrix[j],*matrixColSize)>0||arrayEquals(matrix[j],p,*matrixColSize)>0)
-				count++;
-		}
-		if(count>res)
-			res=count;
+		if (matrix[i][0] == 1)
+			arrayFlip(matrix[i], N);
+	}
+	int res = 1;
+	for (int i = 0; i < M - 1; ++i)
+	{
+		int same = 1;
+		for (int j = i + 1; j < M; ++j)
+			same += arrayEquals(matrix[i], matrix[j], N);
+		res = max(res, same);
 	}
 	return res;
 }
