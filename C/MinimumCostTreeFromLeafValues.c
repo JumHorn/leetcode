b38547ee357e553e60c8fee7b38a581f58507b78
JumHorn <JumHorn@gmail.com>
2020-10-21 18:21:37 +0800

    formatted

--- C/MinimumCostTreeFromLeafValues.c
+++ C/MinimumCostTreeFromLeafValues.c
@@ -1,44 +1,20 @@
+#include <limits.h>
 
+#define min(a, b) (((a) < (b)) ? (a) : (b))
 
-int mctFromLeafValues(int* arr, int arrSize)
+int mctFromLeafValues(int *arr, int arrSize)
 {
 	int res = 0, top = -1;
-	for (int i = 0; i < arrSize; i++)
+	for (int i = 0; i < arrSize; ++i)
 	{
-		if (top == -1)
-			arr[++top] = arr[i];
-		else if (top == 0)
+		while (top != -1 && arr[top] <= arr[i])
 		{
-			if (arr[i] >= arr[top])
-			{
-				res += arr[i] * arr[top];
-				arr[top] = arr[i];
-			}
-			else
-				arr[++top] = arr[i];
+			int mid = arr[top--];
+			res += mid * (top == -1 ? arr[i] : min(arr[top], arr[i]));
 		}
-		else
-		{
-			if (arr[i] < arr[top])
-				arr[++top] = arr[i];
-			else
-			{
-				if (arr[i] >= arr[top - 1])
-				{
-					res += arr[top] * arr[top - 1];
-					--top;
-					--i;
-				}
-				else
-				{
-					res += arr[top] * arr[i];
-					arr[top] = arr[i];
-				}
-			}
-		}
+		arr[++top] = arr[i];
 	}
-	for (int i = top; i > 0; i--)
+	for (int i = 1; i <= top; ++i)
 		res += arr[i] * arr[i - 1];
 	return res;
 }
-
