5c73e6e2bb20875a85e54d56ff55db3c6cdb95c3
JumHorn <JumHorn@gmail.com>
2020-10-21 17:31:38 +0800

    Minimum Cost Tree From Leaf Values beats 9.16%

--- Cplus/MinimumCostTreeFromLeafValues.cpp
+++ Cplus/MinimumCostTreeFromLeafValues.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <climits>
 #include <vector>
 using namespace std;
 
@@ -7,26 +8,24 @@ class Solution
 public:
 	int mctFromLeafValues(vector<int> &arr)
 	{
-		int res = 0;
-		vector<pair<int, int>> v(arr.size());
-		for (int i = 0; i < (int)v.size(); i++)
+		int N = arr.size();
+		vector<vector<int>> dp(N, vector<int>(N, -1));
+		return memdp(arr, 0, N - 1, dp);
+	}
+
+	int memdp(vector<int> &arr, int first, int last, vector<vector<int>> &dp) //[first,last]
+	{
+		if (first >= last)
+			return 0;
+		if (dp[first][last] != -1)
+			return dp[first][last];
+		int res = INT_MAX;
+		for (int i = first; i < last; ++i)
 		{
-			v[i].first = -arr[i];
-			v[i].second = arr[i];
+			int val = (*max_element(arr.begin() + first, arr.begin() + i + 1)) *
+					  (*max_element(arr.begin() + i + 1, arr.begin() + last + 1));
+			res = min(res, val + memdp(arr, first, i, dp) + memdp(arr, i + 1, last, dp));
 		}
-		make_heap(v.begin(), v.end());
-		int index = v.size() - 1;
-		while (index > 0)
-		{
-			pop_heap(v.begin(), v.begin() + index + 1);
-			pair<int, int> node1 = v[index--];
-			pop_heap(v.begin(), v.begin() + index + 1);
-			pair<int, int> node2 = v[index];
-			res += node1.second * node2.second;
-			v[index] = {node1.first + node2.first, max(node1.second, node2.second)};
-			if (index > 0)
-				push_heap(v.begin(), v.begin() + index);
-		}
-		return res;
+		return dp[first][last] = res;
 	}
 };
