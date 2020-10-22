94090638baa660cd494fe3d53b05225c25773807
JumHorn <JumHorn@gmail.com>
2020-10-22 22:03:09 +0800

    Roman to Integer beats 88.04%

--- C/RomantoInteger.c
+++ C/RomantoInteger.c
@@ -1,44 +1,17 @@
-#include<stdio.h>
-#include<string.h>
-int main()
-{
-	int romanToInt(char *s);
-}
+#include <string.h>
 
 int romanToInt(char *s)
 {
-
-	int map[100];
-	map['I']=1;
-	map['V']=5;
-	map['X']=10;
-	map['L']=50;
-	map['C']=100;
-	map['D']=500;
-	map['M']=1000;
-	int len=strlen(s);
-	int i,j,result=0,flag;
-	for(i=0;i<len-1;i++)
-	{
-		j=i+1;
-		switch(s[j]-s[i])
-		{
-			case 13:;
-			case 15 :;
-			case -12:;
-			case -21:;
-			case 1:;
-			case 10:if(s[i]=='I'||s[i]=='C'||s[i]=='X'){result+=map[s[j]]-map[s[i]];i++;flag=0;break;}
-			default :result+=map[s[i]];flag=1;break;
-		}
-	}
-	if(i==len-1&&flag!=1)
-	{
-		result+=map[s[i]];
-	}
-	if(flag==1)
-	{
-		result+=map[s[j]];
-	}
-	return result;
+	int map[26];
+	map['I' - 'A'] = 1;
+	map['V' - 'A'] = 5;
+	map['X' - 'A'] = 10;
+	map['L' - 'A'] = 50;
+	map['C' - 'A'] = 100;
+	map['D' - 'A'] = 500;
+	map['M' - 'A'] = 1000;
+	int N = strlen(s), res = map[s[N - 1] - 'A'];
+	for (int i = N - 2; i >= 0; --i)
+		res += (map[s[i] - 'A'] < map[s[i + 1] - 'A'] ? -map[s[i] - 'A'] : map[s[i] - 'A']);
+	return res;
 }
