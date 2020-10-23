7dd0714ff83669eb529191132bdab270724a65e9
JumHorn <JumHorn@gmail.com>
2020-10-23 17:24:59 +0800

    Remove Linked List Elements beats 79.29%

--- C/RemoveLinkedListElements.c
+++ C/RemoveLinkedListElements.c
@@ -1,39 +1,20 @@
-#include<stdio.h>
-//definition for singly-linked list
+
+// Definition for singly-linked list.
 struct ListNode
 {
 	int val;
 	struct ListNode *next;
 };
-int main()
-{
-	struct ListNode *removeElements(struct ListNode *head,int val);
-}
 
-struct ListNode *removeElements(struct ListNode *head,int val)
+struct ListNode *removeElements(struct ListNode *head, int val)
 {
-	if(head==NULL)
-		return head;
-	struct ListNode *p;
-	p=head;
-	while(p!=NULL&&p->val==val)
+	struct ListNode **p = &head;
+	while (*p)
 	{
-		p=p->next;
-	}
-	if(p==NULL)
-		return p;
-	head=p;
-	while(p!=NULL&&p->next!=NULL)
-	{
-		if(p->next->val==val)
-		{
-			p->next=p->next->next;
-		}
+		if ((*p)->val == val)
+			*p = (*p)->next;
 		else
-			p=p->next;
+			p = &(*p)->next;
 	}
 	return head;
 }
-
-
-
