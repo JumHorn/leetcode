e8a4303e1cdeefff6e2dc8969bdb23ade4e059c5
JumHorn <JumHorn@gmail.com>
2020-10-23 09:37:55 +0800

    Replace the Substring for Balanced String beats 66.67%

--- C/ReplacetheSubstringforBalancedString.c
+++ C/ReplacetheSubstringforBalancedString.c
@@ -1,3 +1,5 @@
+#include <stdbool.h>
+
 /*
 Q 1
 R 2
@@ -6,45 +8,48 @@ W 0
 */
 #define HASH(x) (((x) % 7) - 3)
 
+bool lowerThanAvg(int *count, int avg)
+{
+	for (int i = 0; i < 4; ++i)
+	{
+		if (count[i] > avg)
+			return false;
+	}
+	return true;
+}
+
+bool checkBalance(char *s, int *count, int avg, int len)
+{
+	for (int i = 0; i < len; ++i)
+		--count[HASH(s[i])];
+	if (lowerThanAvg(count, avg))
+		return true;
+	for (int i = len; s[i]; ++i)
+	{
+		--count[HASH(s[i])];
+		++count[HASH(s[i - len])];
+		if (lowerThanAvg(count, avg))
+			return true;
+	}
+	return false;
+}
+
 int balancedString(char *s)
 {
-	char *p = s;
 	int map[4] = {0}, len = 0;
-	while (*p)
+	for (; s[len]; ++len)
+		++map[HASH(s[len])];
+	int avg = len / 4, lo = 0, hi = avg * 3;
+	for (int i = 0; i < 4; ++i)
 	{
-		map[HASH(*p)]++;
-		len++;
-		p++;
-	}
-	int lo = 0, hi = len / 4 * 3, mi, avg = len / 4;
-	for (int i = 0; i < 4; i++)
 		if (map[i] > avg)
 			lo += map[i] - avg;
+	}
 	while (lo < hi)
 	{
-		mi = (hi - lo) / 2 + lo;
-		int arr[4] = {map[0], map[1], map[2], map[3]};
-		for (int i = 0; i < mi; i++)
-			arr[HASH(s[i])]--;
-		bool flag = true;
-		int i = mi - 1;
-		while (true)
-		{
-			int balance = 0;
-			for (int j = 0; j < 4; j++)
-				if (arr[j] < avg)
-					balance += avg - arr[j];
-			if (balance == mi)
-			{
-				flag = false;
-				break;
-			}
-			if (++i >= len)
-				break;
-			arr[HASH(s[i])]--;
-			arr[HASH(s[i - mi])]++;
-		}
-		if (flag)
+		int mi = (hi - lo) / 2 + lo;
+		int count[4] = {map[0], map[1], map[2], map[3]};
+		if (!checkBalance(s, count, avg, mi))
 			lo = mi + 1;
 		else
 			hi = mi;
