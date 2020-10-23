3c1c3b9bf7b1546ed744f1b1b811c999e7e149e5
JumHorn <JumHorn@gmail.com>
2020-10-23 10:36:20 +0800

    Remove Sub-Folders from the Filesystem beats 95.30%

---
+++ C/RemoveSub-FoldersfromtheFilesystem.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+
+//cmp function don't consider overflow
+int cmp(const void *lhs, const void *rhs)
+{
+	return strcmp(*(char **)lhs, *(char **)rhs);
+}
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+char **removeSubfolders(char **folder, int folderSize, int *returnSize)
+{
+	*returnSize = 0;
+	qsort(folder, folderSize, sizeof(char *), cmp);
+	for (int i = 0; i < folderSize; ++i)
+	{
+		if (*returnSize == 0 ||
+			strncmp(folder[*returnSize - 1], folder[i], strlen(folder[*returnSize - 1])) != 0 ||
+			folder[i][strlen(folder[*returnSize - 1])] != '/')
+			folder[(*returnSize)++] = folder[i];
+	}
+	char **res = (char **)malloc(sizeof(char *) * (*returnSize));
+	for (int i = 0; i < *returnSize; ++i)
+	{
+		int len = strlen(folder[i]);
+		res[i] = (char *)malloc(sizeof(char) * (len + 1));
+		strcpy(res[i], folder[i]);
+	}
+	return res;
+}
--- Cplus/RemoveSub-FoldersfromtheFilesystem.cpp
+++ Cplus/RemoveSub-FoldersfromtheFilesystem.cpp
@@ -1,77 +1,22 @@
+#include <algorithm>
 #include <string>
 #include <vector>
 using namespace std;
-
-//Trie
-class Trie
-{
-	struct TreeNode
-	{
-		int count; //number of words ends with this node
-		vector<TreeNode *> nodes;
-
-		TreeNode() : count(0), nodes(128, nullptr) {}
-	};
-
-public:
-	Trie()
-	{
-		root = new TreeNode();
-	}
-
-	/** Inserts a word into the trie. */
-	void insert(const string &word)
-	{
-		TreeNode *node = root;
-		for (auto c : word)
-		{
-			int index = c;
-			if (node->nodes[index] == NULL)
-				node->nodes[index] = new TreeNode();
-			node = node->nodes[index];
-		}
-		++node->count;
-	}
-
-	void search(vector<string> &res)
-	{
-		string instance;
-		dfs(root, instance, res);
-	}
-
-private:
-	void dfs(TreeNode *node, string &instance, vector<string> &res)
-	{
-		if (node->count > 0)
-		{
-			res.push_back(instance);
-			node->nodes['/'] = nullptr;
-		}
-		for (int i = 0; i < 128; ++i)
-		{
-			if (node->nodes[i] != nullptr)
-			{
-				instance.push_back(i);
-				dfs(node->nodes[i], instance, res);
-				instance.pop_back();
-			}
-		}
-	}
-
-	TreeNode *root;
-};
-/********end of Trie********/
 
 class Solution
 {
 public:
 	vector<string> removeSubfolders(vector<string> &folder)
 	{
-		Trie trie;
-		for (auto &f : folder)
-			trie.insert(f);
 		vector<string> res;
-		trie.search(res);
+		sort(folder.begin(), folder.end());
+		for (string &s : folder)
+		{
+			if (res.empty() ||
+				res.back().compare(0, res.back().length(), s, 0, res.back().length()) != 0 ||
+				s[res.back().length()] != '/')
+				res.push_back(s);
+		}
 		return res;
 	}
 };
