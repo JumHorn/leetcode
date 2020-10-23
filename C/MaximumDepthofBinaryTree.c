d6f44d001a685a1565b67be66b52c4d8080208cd
JumHorn <JumHorn@gmail.com>
2020-10-23 14:24:52 +0800

    Maximum Depth of Binary Tree beats 99.57%

--- C/MaximumDepthofBinaryTree.c
+++ C/MaximumDepthofBinaryTree.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+
 //definition for a binary tree
 struct TreeNode
 {
@@ -6,33 +6,12 @@ struct TreeNode
 	struct TreeNode *left;
 	struct TreeNode *right;
 };
-int findDepth(struct TreeNode * root);
-int main()
-{
-	int maxDepth(struct TreeNode * root);
-}
 
-int maxDepth(struct TreeNode * root)
+int maxDepth(struct TreeNode *root)
 {
-	if(root!=NULL)root->val=1;
-	else return 0;
-	return findDepth(root);
-}
-
-int findDepth(struct TreeNode * root)
-{
-	static int depth=1;
-	if(root->left!=NULL)
-	{
-		root->left->val=root->val+1;
-		if(root->left->val>depth)depth=root->left->val;
-		findDepth(root->left);
-	}
-	if(root->right!=NULL)
-	{
-		root->right->val=root->val+1;
-		if(root->right->val>depth)depth=root->right->val;
-		findDepth(root->right);
-	}
-	return depth;
+	if (!root)
+		return 0;
+	int l = maxDepth(root->left);
+	int r = maxDepth(root->right);
+	return 1 + (l < r ? r : l);
 }
