da0ed3fb4b238f0db472544d4484913b164a15f7
JumHorn <JumHorn@gmail.com>
2020-10-23 14:02:02 +0800

    Same Tree beats 100%

--- C/SameTree.c
+++ C/SameTree.c
@@ -1,35 +1,18 @@
+#include <stdbool.h>
+
 //definition for a binary tree node
-#include<stdio.h>
-#include<stdbool.h>
 struct TreeNode
 {
 	int val;
-	struct TreeNode * left;
-	struct TreeNode * right;
+	struct TreeNode *left;
+	struct TreeNode *right;
 };
-int main()
+
+bool isSameTree(struct TreeNode *p, struct TreeNode *q)
 {
-	bool isSameTree(struct TreeNode *p,struct TreeNode *q);
-}
-bool isSameTree(struct TreeNode *p,struct TreeNode *q)
-{
-	if(p==NULL&&q==NULL)
-	{
+	if (p == q) //p==NULL && q== NULL
 		return true;
-	}
-	else if(p!=NULL&&q!=NULL)
-	{
-		if(p->val!=q->val)
-		{
-			return false;
-		}
-		else
-		{
-			return isSameTree(p->left,q->left)&&isSameTree(p->right,q->right);
-		}
-	}
-	else
-	{
+	if (!p || !q)
 		return false;
-	}
+	return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
 }
