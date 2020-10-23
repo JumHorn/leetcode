3e55c06ccf5f80b56d7099d3f274b8e966ec5f37
JumHorn <JumHorn@gmail.com>
2020-10-23 22:58:00 +0800

    new heap function series

--- C/ContainsDuplicate.c
+++ C/ContainsDuplicate.c
@@ -1,52 +1,60 @@
-#define swap(a, b) \
-	if ((a) != (b)) (a) ^= (b) ^= (a) ^= (b)
+#include <stdbool.h>
 
-void pushheap(int* data, int hole)
+//max heap function series
+void push_heap(int *ptr, int size)
 {
-	int val = data[hole];
-	for (; hole > 0 && data[(hole - 1) / 2] < val; hole = (hole - 1) / 2)
-		data[hole] = data[(hole - 1) / 2];
-	data[hole] = val;
-}
-
-void popheap(int* data, int last)
-{
-	int val = data[0], hole = 0, index = 2 * 0 + 2;
-	for (; index < last; index = index * 2 + 2)
+	if (size <= 1)
+		return;
+	int val = ptr[size - 1], hole = size - 1;
+	for (int i = (hole - 1) >> 1; hole > 0 && val > ptr[i]; i = (hole - 1) >> 1)
 	{
-		if (data[index - 1] > data[index])
-			index--;
-		data[(index - 1) / 2] = data[index];
-		hole = index;
+		ptr[hole] = ptr[i];
+		hole = i;
 	}
-	if (index == last)
-	{
-		data[(index - 1) / 2] = data[index - 1];
-		hole = index - 1;
-	}
-	data[hole] = data[last];
-	data[last] = val;
-	pushheap(data, hole);
+	ptr[hole] = val;
 }
 
-void makeheap(int* data, int size)
+void pop_heap(int *ptr, int size)
 {
-	for (int i = 1; i < size; i++)
-		pushheap(data, i);
+	if (size <= 0)
+		return;
+	int val = *ptr, non_leaf = (size - 1) >> 1, hole = 0, i = 0;
+	while (i < non_leaf)
+	{
+		i = 2 * i + 2;
+		if (ptr[i] < ptr[i - 1])
+			--i;
+		ptr[hole] = ptr[i];
+		hole = i;
+	}
+	if (i == non_leaf && size % 2 == 0)
+	{
+		ptr[hole] = ptr[size - 1];
+		hole = size - 1;
+	}
+	ptr[hole] = ptr[size - 1];
+	push_heap(ptr, hole + 1);
+	ptr[size - 1] = val;
 }
 
-bool containsDuplicate(int* nums, int numsSize)
+void make_heap(int *ptr, int size)
+{
+	for (int i = 1; i < size; ++i)
+		push_heap(ptr, i + 1);
+}
+/********end of max heap********/
+
+bool containsDuplicate(int *nums, int numsSize)
 {
 	if (numsSize == 0)
 		return false;
-	makeheap(nums, numsSize);
-	popheap(nums, numsSize - 1);
+	push_heap(nums, numsSize);
+	pop_heap(nums, numsSize - 1);
 	for (int i = numsSize - 2; i >= 0; i--)
 	{
-		popheap(nums, i);
+		pop_heap(nums, i);
 		if (nums[i] == nums[i + 1])
 			return true;
 	}
 	return false;
 }
-
