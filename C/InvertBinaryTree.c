dcdc209f4c69e65dbf1c66b51134bee101e89c33
JumHorn <JumHorn@gmail.com>
2020-10-23 22:49:33 +0800

    Invert Binary Tree beats 100%

--- C/InvertBinaryTree.c
+++ C/InvertBinaryTree.c
@@ -1,46 +1,19 @@
-//definition for a binary tree node
-#include<stdio.h>
+
+// Definition for a binary tree node.
 struct TreeNode
 {
 	int val;
-	struct TreeNode * left;
-	struct TreeNode * right;
+	struct TreeNode *left;
+	struct TreeNode *right;
 };
-void swap(struct TreeNode **p,struct TreeNode **q);
-int main()
+
+struct TreeNode *invertTree(struct TreeNode *root)
 {
-	struct TreeNode * invertTree(struct TreeNode * root);
-}
-struct TreeNode * invertTree(struct TreeNode * root)
-{
-	if(root==NULL)return root;
-	if(root->left!=NULL&&root->right!=NULL)
-	{
-		swap(&root->left,&root->right);
-		invertTree(root->left);
-		invertTree(root->right);
-	}
-	if(root->left==NULL&&root->right!=NULL)
-	{
-		root->left=root->right;
-		root->right=NULL;
-		invertTree(root->left);
-	}
-	else if(root->left!=NULL&&root->right==NULL)
-	{
-		root->right=root->left;
-		root->left=NULL;
-		invertTree(root->right);
-	}
+	if (!root)
+		return root;
+	struct TreeNode *l = invertTree(root->left);
+	struct TreeNode *r = invertTree(root->right);
+	root->left = r;
+	root->right = l;
 	return root;
 }
-
-void swap(struct TreeNode **p,struct TreeNode **q)
-{
-	struct TreeNode * temp;
-
-	temp=*p;
-	*p=*q;
-	*q=temp;
-}
-
