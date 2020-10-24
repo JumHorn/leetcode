6ec034a7953a04d47138214e20abdd749fc84fbd
JumHorn <JumHorn@gmail.com>
2020-10-24 21:24:30 +0800

    Binary Tree Paths beats 100%

---
+++ C/BinaryTreePaths.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+};
+
+//malloc result
+char **mallocRes(char (*data)[100], int dataSize, int *returnSize)
+{
+	*returnSize = dataSize;
+	char **res = (char **)malloc(sizeof(char *) * (*returnSize));
+	for (int i = 0; i < *returnSize; ++i)
+	{
+		int n = strlen(data[i]);
+		res[i] = (char *)malloc(sizeof(char) * (n + 1));
+		strcpy(res[i], data[i]);
+	}
+	return res;
+}
+
+void addOneResult(char (*staticRes)[100], int *size, char *instance)
+{
+	strcpy(staticRes[(*size)++], instance);
+}
+
+void preorder(struct TreeNode *root, char *instance, int instanceSize, char (*staticRes)[100], int *size)
+{
+	if (!root)
+		return;
+	char number[20];
+	if (instanceSize == 0)
+		sprintf(number, "%d", root->val);
+	else
+		sprintf(number, "->%d", root->val);
+	strcpy(&instance[instanceSize], number);
+	if (root->left == root->right)
+	{
+		addOneResult(staticRes, size, instance);
+		return;
+	}
+	instanceSize += strlen(number);
+	preorder(root->left, instance, instanceSize, staticRes, size);
+	preorder(root->right, instance, instanceSize, staticRes, size);
+}
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+char **binaryTreePaths(struct TreeNode *root, int *returnSize)
+{
+	static char staticRes[1000][100];
+	char instance[100] = {0};
+	*returnSize = 0;
+	preorder(root, instance, 0, staticRes, returnSize);
+	return mallocRes(staticRes, *returnSize, returnSize);
+}
--- Cplus/BinaryTreePaths.cpp
+++ Cplus/BinaryTreePaths.cpp
@@ -1,94 +1,40 @@
-#include<iostream>
-#include<sstream>
-#include<vector>
-#include<string>
+#include <string>
+#include <vector>
 using namespace std;
 
 // Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+struct TreeNode
+{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-class Solution {
+class Solution
+{
 public:
-    vector<string> binaryTreePaths(TreeNode* root)
-    {
-        vector<string> result;
-        vector<int> temp;
-        FindTreePaths(root,result,temp);
-        return result;
-    }
-    void FindTreePaths(TreeNode* root,vector<string>& result,vector<int>& temp) 
-    {
-        if(!root)
-        {
-            return ;
-        }
-        temp.push_back(root->val);
-        if(root->left==NULL&&root->right==NULL)
-        {
-            stringstream ss;
-            ss<<temp[0];
-            for(vector<int>::iterator iter = temp.begin()+1;iter!=temp.end();iter++)
-            {
-                ss<<"->"<<*iter;
-            }
-            result.push_back(ss.str());
-            // return result;
-        }
-        FindTreePaths(root->left,result,temp);
-        FindTreePaths(root->right,result,temp);
-        temp.pop_back();
-        return ;
-    }
+	vector<string> binaryTreePaths(TreeNode *root)
+	{
+		vector<string> res;
+		string instance;
+		preorder(root, instance, res);
+		return res;
+	}
+
+	void preorder(TreeNode *root, string instance, vector<string> &data)
+	{
+		if (root == nullptr)
+			return;
+		instance += (instance.empty() ? "" : "->") + to_string(root->val);
+		if (root->left == root->right)
+		{
+			data.push_back(instance);
+			return;
+		}
+		preorder(root->left, instance, data);
+		preorder(root->right, instance, data);
+	}
 };
-
-class Solution0 {
-public:
-    vector<string> result;
-    vector<int> temp;
-    vector<string> binaryTreePaths(TreeNode* root) {
-        if(!root)
-        {
-            return result;
-        }
-        temp.push_back(root->val);
-        if(root->left==NULL&&root->right==NULL)
-        {
-            stringstream ss;
-            ss<<temp[0];
-            for(vector<int>::iterator iter = temp.begin()+1;iter!=temp.end();iter++)
-            {
-                ss<<"->"<<*iter;
-            }
-            result.push_back(ss.str());
-            // return result;
-        }
-        binaryTreePaths(root->left);
-        binaryTreePaths(root->right);
-        temp.pop_back();
-        return result;
-    }
-};
-
-
-// void binaryTreePaths(vector<string>& result, TreeNode* root, string t) {
-//     if(!root->left && !root->right) {
-//         result.push_back(t);
-//         return;
-//     }
-
-//     if(root->left) binaryTreePaths(result, root->left, t + "->" + to_string(root->left->val));
-//     if(root->right) binaryTreePaths(result, root->right, t + "->" + to_string(root->right->val));
-// }
-
-// vector<string> binaryTreePaths(TreeNode* root) {
-//     vector<string> result;
-//     if(!root) return result;
-    
-//     binaryTreePaths(result, root, to_string(root->val));
-//     return result;
-// }
