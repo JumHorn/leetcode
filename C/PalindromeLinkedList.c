ff7a9543c1db9bc4ae86dac28242318ed23bfbac
JumHorn <JumHorn@gmail.com>
2020-10-24 20:01:23 +0800

    Palindrome Linked List beats 68.36%

---
+++ C/PalindromeLinkedList.c
@@ -0,0 +1,24 @@
+#include <stdbool.h>
+
+// Definition for singly-linked list.
+struct ListNode
+{
+	int val;
+	struct ListNode* next;
+};
+
+bool checkPalindrome(struct ListNode** head, struct ListNode* tail)
+{
+	if (!tail)
+		return true;
+	bool res = checkPalindrome(head, tail->next);
+	if ((*head)->val != tail->val)
+		return false;
+	*head = (*head)->next;
+	return res;
+}
+
+bool isPalindrome(struct ListNode* head)
+{
+	return checkPalindrome(&head, head);
+}
