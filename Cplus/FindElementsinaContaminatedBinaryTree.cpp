70f685848c19eb1ed48588e3cdf47fc108eac520
JumHorn <JumHorn@gmail.com>
2020-10-24 14:49:57 +0800

    formatted

--- Cplus/FindElementsinaContaminatedBinaryTree.cpp
+++ Cplus/FindElementsinaContaminatedBinaryTree.cpp
@@ -1,13 +1,15 @@
 #include <unordered_set>
 using namespace std;
 
-//Definition for a binary tree node.
+// Definition for a binary tree node.
 struct TreeNode
 {
 	int val;
 	TreeNode *left;
 	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 class FindElements
@@ -15,27 +17,27 @@ class FindElements
 public:
 	FindElements(TreeNode *root)
 	{
-		if (root != NULL)
+		if (root != nullptr)
 		{
 			root->val = 0;
-			dfs(root);
+			preorder(root);
 		}
 	}
 
-	void dfs(TreeNode *root)
+	void preorder(TreeNode *root)
 	{
-		if (root == NULL)
+		if (root == nullptr)
 			return;
 		s.insert(root->val);
-		if (root->left != NULL)
+		if (root->left != nullptr)
 		{
 			root->left->val = 2 * root->val + 1;
-			dfs(root->left);
+			preorder(root->left);
 		}
-		if (root->right != NULL)
+		if (root->right != nullptr)
 		{
 			root->right->val = 2 * root->val + 2;
-			dfs(root->right);
+			preorder(root->right);
 		}
 	}
 
