4fce6a8bb24d3248d75547ded9292783e18f3e17
JumHorn <JumHorn@gmail.com>
2020-10-24 10:14:12 +0800

    Search Suggestions System beats 49.84%

--- Cplus/SearchSuggestionsSystem.cpp
+++ Cplus/SearchSuggestionsSystem.cpp
@@ -3,30 +3,85 @@
 #include <vector>
 using namespace std;
 
+//Trie
+class Trie
+{
+	struct TreeNode
+	{
+		int count; //number of words ends with this node
+		vector<TreeNode *> nodes;
+
+		TreeNode() : count(0), nodes(26, nullptr) {}
+	};
+
+public:
+	Trie()
+	{
+		root = new TreeNode();
+	}
+
+	/** Inserts a word into the trie. */
+	void insert(const string &word)
+	{
+		TreeNode *node = root;
+		for (auto c : word)
+		{
+			int index = c - 'a';
+			if (node->nodes[index] == nullptr)
+				node->nodes[index] = new TreeNode();
+			node = node->nodes[index];
+		}
+		++node->count;
+	}
+
+	void search(TreeNode *node, string &prefix, vector<string> &suggest) const
+	{
+		if (node == nullptr || (int)suggest.size() >= 3)
+			return;
+		if (node->count > 0)
+			suggest.push_back(prefix);
+		for (int i = 0; i < 26; ++i)
+		{
+			if (node->nodes[i] != nullptr)
+			{
+				prefix.push_back('a' + i);
+				search(node->nodes[i], prefix, suggest);
+				prefix.pop_back();
+			}
+		}
+	}
+
+	void startsWith(string &searchWord, vector<vector<string>> &suggest)
+	{
+		TreeNode *node = root;
+		string prefix;
+		suggest.resize(searchWord.size());
+		for (int i = 0; i < (int)searchWord.size(); ++i)
+		{
+			int index = searchWord[i] - 'a';
+			if (node->nodes[index] == nullptr)
+				break;
+			prefix.push_back(searchWord[i]);
+			node = node->nodes[index];
+			search(node, prefix, suggest[i]);
+		}
+	}
+
+private:
+	TreeNode *root;
+};
+/********end of Trie********/
+
 class Solution
 {
 public:
 	vector<vector<string>> suggestedProducts(vector<string> &products, string searchWord)
 	{
-		vector<vector<string>> res(searchWord.size());
-		vector<string> suggest;
-		for (int k = 0; k < (int)searchWord.size(); k++)
-		{
-			suggest.clear();
-			for (int i = 0; i < (int)products.size(); i++)
-				if (k < (int)products[i].length() && products[i][k] == searchWord[k])
-				{
-					suggest.push_back(products[i]);
-					push_heap(suggest.begin(), suggest.end(), greater<string>());
-				}
-			int n = suggest.size();
-			for (int i = 0; i < 3 && n - i > 0; i++)
-			{
-				pop_heap(suggest.begin(), suggest.begin() + n - i, greater<string>());
-				res[k].push_back(*(suggest.begin() + n - i - 1));
-			}
-			swap(products, suggest);
-		}
+		Trie trie;
+		for (auto &product : products)
+			trie.insert(product);
+		vector<vector<string>> res;
+		trie.startsWith(searchWord, res);
 		return res;
 	}
 };
