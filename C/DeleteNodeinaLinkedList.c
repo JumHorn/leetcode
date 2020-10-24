eb771263b69d506dac6817b57762d80759333a69
JumHorn <JumHorn@gmail.com>
2020-10-24 20:23:11 +0800

    Delete Node in a Linked List beats 97.64%

--- C/DeleteNodeinaLinkedList.c
+++ C/DeleteNodeinaLinkedList.c
@@ -1,36 +1,13 @@
-/*
-   Definition for singly-linked list
-   struct ListNode
-   {
-   int val;
-   struct ListNode *next;
-   };
- */
-#include<stdio.h>
 
+// Definition for singly-linked list.
 struct ListNode
 {
 	int val;
 	struct ListNode *next;
 };
-int main()
-{
-	void deleteNode(struct ListNode * node);
-}
 
-void deleteNode(struct ListNode * node)
+void deleteNode(struct ListNode *node)
 {
-	if(node->next!=NULL)
-	{
-		if(node->next->next!=NULL)
-		{
-			node->val=node->next->val;
-			node->next=node->next->next;
-		}
-		else
-		{
-			node->val=node->next->val;
-			node->next=NULL;
-		}
-	}
+	node->val = node->next->val;
+	node->next = node->next->next;
 }
