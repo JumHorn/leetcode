2b5b589b3315e14c9e5cde226d4f969ab40f5d5d
JumHorn <JumHorn@gmail.com>
2020-10-24 09:48:54 +0800

    fix trie bugs

--- Cplus/template.h
+++ Cplus/template.h
@@ -112,7 +112,7 @@ public:
 		for (auto c : word)
 		{
 			int index = c - 'a';
-			if (node->nodes[index] == NULL)
+			if (node->nodes[index] == nullptr)
 				node->nodes[index] = new TreeNode();
 			node = node->nodes[index];
 		}
