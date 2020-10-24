be81296446e984e511df1b5f5f518633f77d672e
JumHorn <JumHorn@gmail.com>
2020-10-24 23:33:20 +0800

    Add Digits beats 100%

--- C/AddDigits.c
+++ C/AddDigits.c
@@ -1,24 +1,13 @@
-#include<stdio.h>
-void main()
-{
-	int addDigits(int num);
-	printf("%d\n",addDigits(19));
-}
+
 int addDigits(int num)
 {
-	int temp=0;
-	if(num/10==0)
+	if (num / 10 == 0)
+		return num;
+	int sum = 0;
+	while (num != 0)
 	{
-		num=num%10;
+		sum += num % 10;
+		num /= 10;
 	}
-	else
-	{
-		while(num!=0)
-		{
-			temp=temp+num%10;
-			num=num/10;
-		}
-		return addDigits(temp);
-	}
-        return num;
+	return addDigits(sum);
 }
