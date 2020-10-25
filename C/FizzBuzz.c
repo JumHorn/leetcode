6ef31f193c1d01efb9ecfbd44f78041720bc86ed
JumHorn <JumHorn@gmail.com>
2020-10-25 22:19:45 +0800

    Fizz Buzz beats 100%

--- C/FizzBuzz.c
+++ C/FizzBuzz.c
@@ -1,37 +1,24 @@
-#include<stdio.h>
-#include<malloc.h>
-int main()
-{
-	char ** fizzBuzz(int n,int *returnSize);
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-char ** fizzBuzz(int n,int *returnSize)
+char **fizzBuzz(int n, int *returnSize)
 {
-	char **p = (char**)malloc(sizeof("FizzBuzz")*n);//申请行数
-	int i;
-	for(i=0;i<n;i++)
+	*returnSize = n;
+	char **res = (char **)malloc(sizeof(char *) * (*returnSize));
+	for (int i = 0; i < *returnSize; ++i)
+		res[i] = (char *)malloc(sizeof("FizzBuzz")); //sizeof("FizzBuzz") == 9 bytes
+	for (int i = 0; i < n; ++i)
 	{
-		p[i]=(char *)malloc(sizeof("FizzBuzz"));//为每行申请空间
-	}
-	*returnSize=n;
-	for(i=1;i<=n;i++)
-	{
-		if(i%3==0&&i%5==0)
-		{
-			p[i-1]="FizzBuzz";
-		}
-		else if(i%3==0)
-		{
-			p[i-1]="Fizz";
-		}
-		else if(i%5==0)
-		{
-			p[i-1]="Buzz";
-		}
+		int j = i + 1;
+		if (j % 3 == 0 && j % 5 == 0)
+			strcpy(res[i], "FizzBuzz");
+		else if (j % 3 == 0)
+			strcpy(res[i], "Fizz");
+		else if (j % 5 == 0)
+			strcpy(res[i], "Buzz");
 		else
-		{
-			sprintf(p[i-1],"%d",i);
-		}
+			sprintf(res[i], "%d", j);
 	}
-	return p;
+	return res;
 }
