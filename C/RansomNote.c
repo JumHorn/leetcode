5ddd616438b925d48667b68a1f55464fc7d583ef
JumHorn <JumHorn@gmail.com>
2020-10-26 00:21:07 +0800

    Ransom Note beats 91.24%

--- C/RansomNote.c
+++ C/RansomNote.c
@@ -1,39 +1,14 @@
-#include<stdio.h>
-#include<stdbool.h>
-#include<string.h>
-void main()
-{
-	bool canConstruct(char * ransomNote,char * magazine);
-        char a[]="bg",b[]="efjbdfbdgfjhhaiigfhbaejahgfbbgbjagbddfgdiaigdadhcfcj";
-        printf("%d\n",canConstruct(a,b));
-}
+#include <stdbool.h>
 
-bool canConstruct(char * ransomNote,char * magazine)
+bool canConstruct(char *ransomNote, char *magazine)
 {
-	int ransomNote_len=strlen(ransomNote);
-	int magazine_len=strlen(magazine);printf("%d,%d\n",ransomNote_len,magazine_len);
-	int i,j;
-	for(i=0;i<ransomNote_len;i++)
+	int count[26] = {0};
+	for (int i = 0; magazine[i]; ++i)
+		++count[magazine[i] - 'a'];
+	for (int i = 0; ransomNote[i]; ++i)
 	{
-		for(j=0;j<magazine_len;j++)
-		{
-			if(ransomNote[i]==magazine[j])
-			{
-				magazine[j]=0;
-				break;
-			}
-		}
-		if(j==magazine_len)
-		{
-			break;
-		}
+		if (--count[ransomNote[i] - 'a'] < 0)
+			return false;
 	}
-	if(i==ransomNote_len)
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
+	return true;
 }
