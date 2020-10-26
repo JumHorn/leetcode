27ad5cc2c902bc75ec86dd9fa3c446b49116058a
JumHorn <JumHorn@gmail.com>
2020-10-26 20:06:44 +0800

    Find All Numbers Disappeared in an Array beats 78.97%

---
+++ C/FindAllNumbersDisappearedinanArray.c
@@ -0,0 +1,22 @@
+#include <math.h>
+#include <stdlib.h>
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* findDisappearedNumbers(int* nums, int numsSize, int* returnSize)
+{
+	for (int i = 0; i < numsSize; i++)
+	{
+		int index = abs(nums[i]) - 1;  // index start from 0
+		nums[index] = nums[index] > 0 ? -nums[index] : nums[index];
+	}
+	int* res = (int*)malloc(sizeof(int) * (numsSize));
+	*returnSize = 0;
+	for (int i = 0; i < numsSize; ++i)
+	{
+		if (nums[i] > 0)
+			res[(*returnSize)++] = i + 1;
+	}
+	return res;
+}
--- Cplus/FindAllNumbersDisappearedinanArray.cpp
+++ Cplus/FindAllNumbersDisappearedinanArray.cpp
@@ -1,75 +1,25 @@
-#include<iostream>
-#include<vector>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> findDisappearedNumbers(vector<int>& nums) {
-        //时间复杂度O(n^2)
-        // vector<int> res;
-        // for(int i=1;i<nums.size()+1;i++)
-        // {
-        //     int j;
-        //     for(j=0;j<nums.size();j++)
-        //     {
-        //         if(nums[j]==i)
-        //         {
-        //             break;
-        //         }
-        //     }
-        //     if(j==nums.size())
-        //     {
-        //         res.push_back(i);
-        //     }
-        // }
-        // return res;
-
-        //O(n)解法
-        vector<int> res;
-        int temp,t;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]==0)
-            {
-                continue;
-            }
-            else
-            {
-                temp = nums[nums[i]-1];
-                nums[nums[i]-1]=0;
-                while(temp!=0)
-                {
-                    t=temp-1;
-                    temp=nums[t];
-                    nums[t]=0;
-                }
-            }
-        }
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]!=0)
-            {
-                res.push_back(i+1);
-            }
-        }
-        return res;
-    }
+	vector<int> findDisappearedNumbers(vector<int> &nums)
+	{
+		int N = nums.size();
+		for (int i = 0; i < N;)
+		{
+			if (i + 1 == nums[i] || nums[nums[i] - 1] == nums[i])
+				++i;
+			else
+				swap(nums[nums[i] - 1], nums[i]);
+		}
+		vector<int> res;
+		for (int i = 0; i < N; ++i)
+		{
+			if (i + 1 != nums[i])
+				res.push_back(i + 1);
+		}
+		return res;
+	}
 };
-
-void print(vector<int> vec)
-{
-    for(vector<int>::iterator iter=vec.begin();iter!=vec.end();iter++)
-    {
-        cout<<*iter<<",";
-    }
-    cout<<endl;
-}
-
-int main()
-{
-    int array[] = {4,3,2,7,8,2,3,1};
-    vector<int> vec(array,array+8);
-    Solution sol;
-    print(sol.findDisappearedNumbers(vec));
-    return 0;
-}
