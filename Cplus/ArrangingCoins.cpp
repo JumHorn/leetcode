616a8b2a7892d6fff307d83706ba86808c3161c7
JumHorn <JumHorn@gmail.com>
2020-10-26 20:58:16 +0800

    Arranging Coins beats 100%

---
+++ Cplus/ArrangingCoins.cpp
@@ -0,0 +1,18 @@
+
+class Solution
+{
+public:
+	int arrangeCoins(int n)
+	{
+		long lo = 0, hi = n + 1u;
+		while (lo < hi)
+		{
+			long mi = (hi - lo) / 2 + lo;
+			if (mi * (mi + 1) / 2 <= n)
+				lo = mi + 1;
+			else
+				hi = mi;
+		}
+		return lo - 1;
+	}
+};
