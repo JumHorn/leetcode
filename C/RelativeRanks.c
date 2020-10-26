2996ec173ce0b804e17f5295a50b2446e29839ff
JumHorn <JumHorn@gmail.com>
2020-10-27 00:01:38 +0800

    Relative Ranks beats 99.75%

---
+++ C/RelativeRanks.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+
+//cmp function don't consider overflow
+int cmp(const void *lhs, const void *rhs)
+{
+	return *(int *)rhs - *(int *)lhs;
+}
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+char **findRelativeRanks(int *nums, int numsSize, int *returnSize)
+{
+	int arr[numsSize][2]; //{score,index}
+	for (int i = 0; i < numsSize; ++i)
+	{
+		arr[i][0] = nums[i];
+		arr[i][1] = i;
+	}
+	qsort(arr, numsSize, sizeof(arr[0]), cmp); //descending
+	char *winner[] = {"Gold Medal", "Silver Medal", "Bronze Medal"};
+	*returnSize = numsSize;
+	char **res = (char **)malloc(sizeof(char *) * (*returnSize));
+	for (int i = 0; i < *returnSize; ++i)
+		res[i] = (char *)malloc(sizeof(char) * sizeof("Bronze Medal"));
+	for (int i = 0; i < min(3, numsSize); ++i)
+		strcpy(res[arr[i][1]], winner[i]);
+	for (int i = 3; i < numsSize; ++i)
+		sprintf(res[arr[i][1]], "%d", i + 1);
+	return res;
+}
--- Cplus/RelativeRanks.cpp
+++ Cplus/RelativeRanks.cpp
@@ -1,57 +1,24 @@
-#include<iostream>
-#include<algorithm>
-#include<vector>
-#include<string>
-#include<map>
+#include <algorithm>
+#include <functional>
+#include <string>
+#include <vector>
 using namespace std;
 
-class SolutionSimply {
+class Solution
+{
 public:
-    vector<string> findRelativeRanks(vector<int>& nums) {
-            vector<string> result;
-            vector<int> numscopy(nums);
-            string strs[]={"Gold Medal","Silver Medal","Bronze Medal"};
-            sort(numscopy.begin(),numscopy.end());
-            for(int i=0;i<nums.size();i++)result.push_back(to_string(nums.size()-distance(numscopy.begin(),find(numscopy.begin(),numscopy.end(),nums[i]))));           
-            for(int i=0;i<min((int)nums.size(),3);i++)result[distance(nums.begin(),find(nums.begin(),nums.end(),numscopy[nums.size()-i-1]))]=strs[i];
-            return result;
-    }
+	vector<string> findRelativeRanks(vector<int> &nums)
+	{
+		vector<pair<int, int>> v; //{score,index}
+		int N = nums.size();
+		for (int i = 0; i < N; ++i)
+			v.push_back({nums[i], i});
+		sort(v.begin(), v.end(), greater<pair<int, int>>());
+		vector<string> winner = {"Gold Medal", "Silver Medal", "Bronze Medal"}, res(N);
+		for (int i = 0; i < min(3, N); ++i)
+			res[v[i].second] = winner[i];
+		for (int i = 3; i < N; ++i)
+			res[v[i].second] = to_string(i + 1);
+		return res;
+	}
 };
-
-class Solution {
-	public:
-		vector<string> findRelativeRanks(vector<int>& nums) 
-		{
-			vector<string> result;
-			vector<int> numscopy(nums);
-			string strs[]={"Gold Medal","Silver Medal","Bronze Medal"};
-			//这里可以使用冒泡排序，实现前三即可
-			sort(numscopy.begin(),numscopy.end());
-			reverse(numscopy.begin(),numscopy.end());
-			for(int i=0;i<nums.size();i++)
-			{
-				result.push_back(to_string(1+distance(numscopy.begin(),find(numscopy.begin(),numscopy.end(),nums[i]))));
-			}
-			//调换下面方法
-			int length=3;
-			if(nums.size()<length)length=nums.size();
-			for(int i=0;i<length;i++)
-			{
-				result[distance(nums.begin(),find(nums.begin(),nums.end(),numscopy[0]))]=strs[i];
-			}
-			//int i,j,k;
-			//i=find(nums.begin(),nums.end(),numscopy[0])-nums.begin();
-			//j=find(nums.begin(),nums.end(),numscopy[1])-nums.begin();
-			//k=find(nums.begin(),nums.end(),numscopy[2])-nums.begin();
-			//i=distance(nums.begin(),find(nums.begin(),nums.end(),numscopy[0]));
-			//j=distance(nums.begin(),find(nums.begin(),nums.end(),numscopy[1]));
-			//k=distance(nums.begin(),find(nums.begin(),nums.end(),numscopy[2]));
-			//result[i]="Gold Medal";
-			//result[j]="Silver Medal";
-			//result[k]="Bronze Medal";
-			return result;
-		}
-};
-
-int main()
-{}
