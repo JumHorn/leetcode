60ebd8ef9237436b7627a6b5615661a50b9d64b2
JumHorn <JumHorn@gmail.com>
2020-10-26 12:10:05 +0800

    Delete Leaves With a Given Value beats 96.55%

--- Cplus/DeleteLeavesWithaGivenValue.cpp
+++ Cplus/DeleteLeavesWithaGivenValue.cpp
@@ -1,28 +1,26 @@
-#include <iostream>
-using namespace std;
-//Definition for a binary tree node.
+
+// Definition for a binary tree node.
 struct TreeNode
 {
 	int val;
-	TreeNode* left;
-	TreeNode* right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 class Solution
 {
 public:
-	TreeNode* removeLeafNodes(TreeNode* root, int target)
+	TreeNode *removeLeafNodes(TreeNode *root, int target)
 	{
-		if (root == NULL)
-			return NULL;
+		if (root == nullptr)
+			return nullptr;
 		root->left = removeLeafNodes(root->left, target);
 		root->right = removeLeafNodes(root->right, target);
-		if (root->left == NULL && root->right == NULL)
-		{
-			if (root->val == target)
-				return NULL;
-		}
+		if (root->left == nullptr && root->right == nullptr && root->val == target)
+			return nullptr;
 		return root;
 	}
 };
