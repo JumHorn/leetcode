3a710f39733170ca2c4418e670bd28182022486f
JumHorn <JumHorn@gmail.com>
2020-10-27 21:07:51 +0800

    Merge Two Binary Trees beats 78.48%

--- Cplus/MergeTwoBinaryTrees.cpp
+++ Cplus/MergeTwoBinaryTrees.cpp
@@ -1,46 +1,27 @@
-#include<iostream>
-using namespace std;
 
-//Definition for a binary tree node.
-struct TreeNode 
+// Definition for a binary tree node.
+struct TreeNode
 {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}  //成员初始化列表 
- };
-
-class Solution {
-public:
-    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) 
-    {
-        if(t2==NULL)
-        {
-        }      
-        else if(t1==NULL)
-        {
-            t1=t2;
-        }
-        else
-        {
-            t1->val+=t2->val;
-            if(t1->left==NULL)
-            {
-                t1->left=t2->left;
-            }
-            else 
-            mergeTrees(t1->left,t2->left);
-            if(t1->right==NULL)
-            {
-                t1->right=t2->right;
-            }
-            else
-            mergeTrees(t1->right,t2->right);
-        }
-        return t1;
-    }
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-int main()
-{}
- 
+class Solution
+{
+public:
+	TreeNode *mergeTrees(TreeNode *t1, TreeNode *t2)
+	{
+		if (t1 == nullptr)
+			return t2;
+		if (t2 == nullptr)
+			return t1;
+		t1->val += t2->val;
+		t1->left = mergeTrees(t1->left, t2->left);
+		t1->right = mergeTrees(t1->right, t2->right);
+		return t1;
+	}
+};
