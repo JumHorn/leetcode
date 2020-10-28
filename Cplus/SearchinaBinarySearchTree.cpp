35b7cbece89819565a5d4a53e067723c8cd905a8
JumHorn <JumHorn@gmail.com>
2020-10-28 20:33:09 +0800

    Search in a Binary Search Tree beats 91.70%

--- Cplus/SearchinaBinarySearchTree.cpp
+++ Cplus/SearchinaBinarySearchTree.cpp
@@ -1,23 +1,24 @@
-#include<iostream>
-using namespace std;
 
-//Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+// Definition for a binary tree node.
+struct TreeNode
+{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-class Solution {
+class Solution
+{
 public:
-    TreeNode* searchBST(TreeNode* root, int val) {
- 		if(root==NULL)
-	 		return NULL;
-		if(root->val==val)
+	TreeNode *searchBST(TreeNode *root, int val)
+	{
+		if (root == nullptr || root->val == val)
 			return root;
-		if(root->val>val)
-			return searchBST(root->left,val);	
-		return searchBST(root->right,val);
-    }
+		if (root->val > val)
+			return searchBST(root->left, val);
+		return searchBST(root->right, val);
+	}
 };
