4931a2adf0dff65b508d14500fddbbd5aff1cbb0
JumHorn <JumHorn@gmail.com>
2020-10-28 16:53:33 +0800

    Employee Importance beats 89.00%

--- Cplus/EmployeeImportance.cpp
+++ Cplus/EmployeeImportance.cpp
@@ -1,35 +1,36 @@
-#include<iostream>
-#include<vector>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 
 // Employee info
-class Employee {
+class Employee
+{
 public:
-    // It's the unique ID of each node.
-    // unique id of this employee
-    int id;
-    // the importance value of this employee
-    int importance;
-    // the id of direct subordinates
-    vector<int> subordinates;
+	// It's the unique ID of each node.
+	// unique id of this employee
+	int id;
+	// the importance value of this employee
+	int importance;
+	// the id of direct subordinates
+	vector<int> subordinates;
 };
 
-//优化是添加hash键值对提速
-class Solution {
+class Solution
+{
 public:
-    int importance=0;
-    int getImportance(vector<Employee*> employees, int id) {
-        for(int i=0;i<employees.size();i++)
-        {
-            if(employees[i]->id==id)
-            {
-                importance+=employees[i]->importance;
-                for(int j=0;j<(employees[i]->subordinates).size();j++)
-                {
-                    getImportance(employees,(employees[i]->subordinates)[j]);
-                }
-            }
-        }
-        return importance;
-    }
+	int getImportance(vector<Employee *> employees, int id)
+	{
+		unordered_map<int, Employee *> m; //{id,Employee}
+		for (int i = 0; i < (int)employees.size(); ++i)
+			m[employees[i]->id] = employees[i];
+		return dfs(id, m);
+	}
+
+	int dfs(int id, unordered_map<int, Employee *> &m)
+	{
+		int res = m[id]->importance;
+		for (auto employeeid : m[id]->subordinates)
+			res += dfs(employeeid, m);
+		return res;
+	}
 };
