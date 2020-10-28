94b779ccac5a549c888579d73f90c75123c0b99f
JumHorn <JumHorn@gmail.com>
2020-10-28 16:31:30 +0800

    Binary Number with Alternating Bits beats 100%

---
+++ C/BinaryNumberwithAlternatingBits.c
@@ -0,0 +1,7 @@
+#include <stdbool.h>
+
+bool hasAlternatingBits(int n)
+{
+	n = (n >> 2) ^ n;  //only one bit left in n
+	return (n & (n - 1)) == 0;
+}
--- Cplus/BinaryNumberwithAlternatingBits.cpp
+++ Cplus/BinaryNumberwithAlternatingBits.cpp
@@ -1,26 +1,10 @@
-#include<iostream>
-using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool hasAlternatingBits(int n) {
-        int temp = ((n>>1)^n);
-        while(temp&1==1)
-        {
-            temp=temp>>1;
-        }
-        if(temp==0)
-        {
-            return true;
-        }
-        return false;
+	bool hasAlternatingBits(int n)
+	{
+		n = (n >> 2) ^ n; //only one bit left in n
+		return (n & (n - 1)) == 0;
+	}
 };
-
-// bool hasAlternatingBits(int n) {
-//     return !((n ^= n/4) & n-1);
-// }
-// Xor the number with itself shifted right twice and check whether everything after the leading 1-bit became/stayed 0. Xor is 0 iff the bits are equal, so we get 0-bits iff the pair of leading 1-bit and the 0-bit in front of it are repeated until the end.
-
-//     000101010
-//   ^ 000001010
-//   = 000100000
