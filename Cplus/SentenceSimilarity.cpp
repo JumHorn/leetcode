e07f6b8925f49dea4690389c2e0a96a2b0126df4
JumHorn <JumHorn@gmail.com>
2020-10-28 21:38:59 +0800

    premium content now

--- Cplus/SentenceSimilarity.cpp
+++ Cplus/SentenceSimilarity.cpp
@@ -1,43 +1,29 @@
-#include<iostream>
-#include<vector>
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool areSentencesSimilar(vector<string>& words1, vector<string>& words2, vector<pair<string, string>> pairs) {
-        if(words1.size()!=words2.size())
-        {
-            return false;
-        }
-        for(int i=0;i<words1.size();i++)
-        {
-            if(words1[i]==words2[i])
-            {
-                continue;
-            }
-            int j;
-            for(j=0;j<pairs.size();j++)
-            {
-                if(pairs[j].first==words1[i])
-                {
-                    if(pairs[j].second==words2[i])
-                    {
-                        break;
-                    }
-                }
-                else if(pairs[j].second==words1[i])
-                {
-                    if(pairs[j].first==words2[i])
-                    {
-                        break;
-                    }
-                }
-            }
-            if(j==pairs.size())
-            {
-                return false;
-            }
-        }
-        return true;
-    }
+	bool areSentencesSimilar(vector<string> &words1, vector<string> &words2, vector<pair<string, string>> pairs)
+	{
+		unordered_map<string, string> m;
+		for (auto &p : pairs)
+		{
+			m[p.first] = p.second;
+			m[p.second] = p.first;
+		}
+		if (words1.size() != words2.size())
+			return false;
+		for (int i = 0; i < (int)words1.size(); ++i)
+		{
+			if (words1[i] == words2[i])
+				continue;
+			if (m.find(words1[i]) == m.end() || m[words1[i]] != words2[i])
+				return false;
+		}
+		return true;
+	}
 };
