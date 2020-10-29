636d01e7bc10d7120163d848f76cd8d5a078f290
JumHorn <JumHorn@gmail.com>
2020-10-29 23:09:38 +0800

    Reverse Only Letters beats 100%

---
+++ C/ReverseOnlyLetters.c
@@ -0,0 +1,23 @@
+#include <ctype.h>
+#include <string.h>
+
+#define swap(a, b) ((a) ^= (b) ^= (a) ^= (b))
+
+char* reverseOnlyLetters(char* S)
+{
+	int i = 0, j = strlen(S) - 1;
+	while (i < j)
+	{
+		if (!isalpha(S[i]))
+			++i;
+		else if (!isalpha(S[j]))
+			--j;
+		else
+		{
+			swap(S[i], S[j]);
+			++i;
+			--j;
+		}
+	}
+	return S;
+}
--- Cplus/ReverseOnlyLetters.cpp
+++ Cplus/ReverseOnlyLetters.cpp
@@ -1,21 +1,22 @@
-#include<string>
-#include<cctype>
+#include <cctype>
+#include <string>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    string reverseOnlyLetters(string S) {
-		if(S.empty())
-			return S;
-		int start=0,end=S.length()-1;
-		while(start<end)
+	string reverseOnlyLetters(string S)
+	{
+		int i = 0, j = (int)S.length() - 1;
+		while (i < j)
 		{
-			while(start<end&&!isalpha(S[start]))
-				start++;
-			while(start<end&&!isalpha(S[end]))
-				end--;
-			swap(S[start++],S[end--]);
+			if (!isalpha(S[i]))
+				++i;
+			else if (!isalpha(S[j]))
+				--j;
+			else
+				swap(S[i++], S[j--]);
 		}
 		return S;
-    }
+	}
 };
