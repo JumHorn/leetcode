d1a3d4b9fc73f15eb15baaed055965982ab88fd3
JumHorn <JumHorn@gmail.com>
2020-10-30 00:02:20 +0800

    Range Sum of BST beats 97.24%

--- C/RangeSumofBST.c
+++ C/RangeSumofBST.c
@@ -1,21 +1,19 @@
-#include<stdio.h>
 
 //Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    struct TreeNode *left;
-    struct TreeNode *right;
+struct TreeNode
+{
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
 };
 
-
-
-int rangeSumBST(struct TreeNode* root, int L, int R){
-	if(!root)
+int rangeSumBST(struct TreeNode *root, int L, int R)
+{
+	if (!root)
 		return 0;
-	if(root->val<L)
-		return rangeSumBST(root->right,L,R);
-	if(root->val>R)
-		return rangeSumBST(root->left,L,R);
-	return root->val+rangeSumBST(root->left,L,R)+rangeSumBST(root->right,L,R);
+	if (root->val < L)
+		return rangeSumBST(root->right, L, R);
+	if (root->val > R)
+		return rangeSumBST(root->left, L, R);
+	return root->val + rangeSumBST(root->left, L, R) + rangeSumBST(root->right, L, R);
 }
-
