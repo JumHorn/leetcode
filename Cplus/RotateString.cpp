61941c9c915a512f15fc793571cedef5a807883a
JumHorn <JumHorn@gmail.com>
2020-10-29 16:20:21 +0800

    find use kmp which much faster

--- Cplus/RotateString.cpp
+++ Cplus/RotateString.cpp
@@ -6,16 +6,6 @@ class Solution
 public:
 	bool rotateString(string A, string B)
 	{
-		if (A.length() != B.length())
-			return false;
-		if (A == B)
-			return true;
-		int N = A.length();
-		for (int i = 0; i < N; ++i)
-		{
-			if (A.compare(i, N - i, B, 0, N - i) == 0 && A.compare(0, i, B, N - i, i) == 0)
-				return true;
-		}
-		return false;
+		return A.length() == B.length() && (A + A).find(B) != string::npos;
 	}
 };
