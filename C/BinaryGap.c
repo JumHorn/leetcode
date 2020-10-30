51f1e47193ca8f462f980d6e0bde933709fa42d1
JumHorn <JumHorn@gmail.com>
2020-10-30 16:34:28 +0800

    Binary Gap beats 100%

--- C/BinaryGap.c
+++ C/BinaryGap.c
@@ -1,17 +1,15 @@
 
-
-
-int binaryGap(int N){
-	int res=0;
-	for(int i=0,j=32;i<31;i++)
+int binaryGap(int n)
+{
+	int res = 0;
+	for (int i = 0, j = 32; i < 31; ++i)
 	{
-		if(N&(1<<i))
+		if (n & (1 << i))
 		{
-			if(i-j+1>res)
-				res=i-j;
-			j=i;
+			if (i - j > res)
+				res = i - j;
+			j = i;
 		}
 	}
 	return res;
 }
-
---
+++ Cplus/BinaryGap.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+	int binaryGap(int n)
+	{
+		int res = 0;
+		for (int i = 0, j = 32; i < 31; ++i)
+		{
+			if (n & (1 << i))
+			{
+				if (i - j > res)
+					res = i - j;
+				j = i;
+			}
+		}
+		return res;
+	}
+};
