c054042adae14f190fdc999b2a5abdabace051a1
JumHorn <JumHorn@gmail.com>
2020-10-30 12:31:40 +0800

    Insertion Selection Bubble shell quick merge heap radix bucket count

--- C/SortanArray.c
+++ C/SortanArray.c
@@ -6,12 +6,115 @@ int cmp(const void *lhs, const void *rhs)
 	return *(int *)lhs - *(int *)rhs;
 }
 
+//divide and conquer
+void merge(int *arr, int *dup, int first, int mid, int last)
+{
+	for (int i = first, j = mid, d = 0; i < mid || j < last;)
+	{
+		if (i == mid)
+			dup[d++] = arr[j++];
+		else if (j == last)
+			dup[d++] = arr[i++];
+		else
+			dup[d++] = (arr[i] > arr[j]) ? arr[j++] : arr[i++];
+	}
+	memcpy(arr + first, dup, sizeof(int) * (last - first));
+}
+
+void divide(int *sum, int *dup, int first, int last)
+{
+	if (last - first < 2)
+		return;
+	int mid = (last - first) / 2 + first;
+	divide(sum, dup, first, mid);
+	divide(sum, dup, mid, last);
+	//to do
+	merge(sum, dup, first, mid, last);
+}
+/********end of divide and conquer********/
+
+//quick sort
+void quickSort(int *arr, int first, int last)
+{
+	if (last - first <= 1)
+		return;
+	int left = first, right = last - 1;
+	int pivot = arr[first];
+	while (right - left > 0)
+	{
+		while (right - left > 0 && arr[right] >= pivot)
+			--right;
+		arr[left] = arr[right];
+		while (right - left > 0 && arr[left] <= pivot)
+			++left;
+		arr[right] = arr[left];
+	}
+	arr[left] = pivot;
+	quickSort(arr, first, left);
+	quickSort(arr, left + 1, last);
+}
+/********end of quick sort********/
+
+//max heap function series
+void push_heap(int *ptr, int size)
+{
+	if (size <= 1)
+		return;
+	int val = ptr[size - 1], hole = size - 1;
+	for (int i = (hole - 1) >> 1; hole > 0 && val > ptr[i]; i = (hole - 1) >> 1)
+	{
+		ptr[hole] = ptr[i];
+		hole = i;
+	}
+	ptr[hole] = val;
+}
+
+void pop_heap(int *ptr, int size)
+{
+	if (size <= 0)
+		return;
+	int val = *ptr, non_leaf = (size - 1) >> 1, hole = 0, i = 0;
+	while (i < non_leaf)
+	{
+		i = 2 * i + 2;
+		if (ptr[i - 1] > ptr[i])
+			--i;
+		ptr[hole] = ptr[i];
+		hole = i;
+	}
+	if (i == non_leaf && size % 2 == 0)
+	{
+		ptr[hole] = ptr[size - 1];
+		hole = size - 1;
+	}
+	ptr[hole] = ptr[size - 1];
+	push_heap(ptr, hole + 1);
+	ptr[size - 1] = val;
+}
+
+void make_heap(int *ptr, int size)
+{
+	for (int i = 1; i < size; ++i)
+		push_heap(ptr, i + 1);
+}
+/********end of max heap********/
+
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int *sortArray(int *nums, int numsSize, int *returnSize)
 {
 	*returnSize = numsSize;
-	qsort(nums, numsSize, sizeof(int), cmp);
+	//merge sort
+	// int dup[numsSize];
+	// divide(nums, dup, 0, numsSize);
+
+	//quick sort
+	// quickSort(nums, 0, numsSize);
+	//library quick sort
+	//qsort(nums, numsSize, sizeof(int), cmp);
+	make_heap(nums, numsSize);
+	for (int i = 0; i < numsSize; ++i)
+		pop_heap(nums, numsSize - i);
 	return nums;
 }
--- Cplus/SortanArray.cpp
+++ Cplus/SortanArray.cpp
@@ -7,7 +7,118 @@ class Solution
 public:
 	vector<int> sortArray(vector<int> &nums)
 	{
-		sort(nums.begin(), nums.end());
+		//merge sort
+		//vector<int> dup(nums.size());
+		//divide(nums, dup, 0, nums.size());
+
+		// quick sort
+		quickSort(nums.begin(), nums.end());
+		//library quick sort
+		//sort(nums.begin(), nums.end());
+
+		//radix sort(negative number fault)
+		//radixSort(nums);
 		return nums;
 	}
+
+	//quick sort
+	void quickSort(vector<int>::iterator first, vector<int>::iterator last)
+	{
+		if (distance(first, last) <= 1)
+			return;
+		vector<int>::iterator left = first, right = last - 1;
+		int pivot = *first;
+		while (distance(left, right) > 0)
+		{
+			while (distance(left, right) > 0 && *right >= pivot)
+				--right;
+			*left = *right;
+			while (distance(left, right) > 0 && *left <= pivot)
+				++left;
+			*right = *left;
+		}
+		*left = pivot;
+		quickSort(first, left);
+		quickSort(left + 1, last);
+	}
+	/********end of quick sort********/
+
+	//divide and conquer
+	void merge(vector<int> &arr, vector<int> &dup, int first, int mid, int last)
+	{
+		for (int i = first, j = mid, d = 0; i < mid || j < last;)
+		{
+			if (i == mid)
+				dup[d++] = arr[j++];
+			else if (j == last)
+				dup[d++] = arr[i++];
+			else
+				dup[d++] = (arr[i] > arr[j]) ? arr[j++] : arr[i++];
+		}
+		for (int i = first, j = 0; i < last; ++i, ++j)
+			arr[i] = dup[j];
+	}
+
+	void divide(vector<int> &arr, vector<int> &dup, int first, int last)
+	{
+		if (last - first <= 1)
+			return;
+		int mid = first + (last - first) / 2;
+		divide(arr, dup, first, mid);
+		divide(arr, dup, mid, last);
+		//to do
+		merge(arr, dup, first, mid, last);
+	}
+	/********end of divide and conquer********/
+
+	//radix sort 10 based system
+	void radixSort(vector<int> &data, int first, int last, bool ascending = true)
+	{
+		int maxval = *max_element(data.begin(), data.end());
+		int exp = 1;	//1, 10, 100 ...
+		int radix = 10; //10 based system
+		int size = last - first;
+		vector<int> aux(size);
+
+		/* LSD Radix Sort */
+		while (maxval / exp > 0) //go through all digits from LSD to MSD
+		{
+			vector<int> count(radix);
+			for (int i = first; i < last; ++i)
+				++count[data[i] / exp % radix];
+			if (ascending)
+			{
+				for (int i = 1; i < radix; ++i)
+					count[i] += count[i - 1];
+			}
+			else
+			{
+				for (int i = radix - 2; i >= 0; --i)
+					count[i] += count[i + 1];
+			}
+			for (int i = last - 1; i >= first; --i)
+				aux[--count[data[i] / exp % radix]] = data[i];
+			for (int i = first; i < last; ++i)
+				data[i] = aux[i - first];
+			exp *= 10;
+		}
+	}
+
+	void radixSort(vector<int> &data)
+	{
+		int N = data.size();
+		int i = 0;
+		for (int j = N - 1; i < j;)
+		{
+			if (data[i] < 0)
+				++i;
+			else if (data[j] >= 0)
+				--j;
+			else
+				swap(data[i++], data[--j]);
+		}
+		radixSort(data, 0, i, false);
+		radixSort(data, i, N);
+	}
+	/********end of radix sort 10 based system********/
 };
