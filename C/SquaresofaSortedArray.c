0af0bbd6b54776a9dd16a10ff7f7c7713b613df2
JumHorn <JumHorn@gmail.com>
2020-10-30 19:30:43 +0800

    real O(N)

--- C/SquaresofaSortedArray.c
+++ C/SquaresofaSortedArray.c
@@ -1,25 +1,5 @@
 #include <stdlib.h>
 
-void arraySquare(int *arr, int arrSize)
-{
-	for (int i = 0; i < arrSize; ++i)
-		arr[i] *= arr[i];
-}
-
-int binarySeach(int *arr, int arrSize)
-{
-	int lo = 0, hi = arrSize;
-	while (lo < hi)
-	{
-		int mi = (hi - lo) / 2 + lo;
-		if (arr[mi] < 0)
-			lo = mi + 1;
-		else
-			hi = mi;
-	}
-	return lo;
-}
-
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
@@ -27,17 +7,19 @@ int *sortedSquares(int *A, int ASize, int *returnSize)
 {
 	*returnSize = ASize;
 	int *res = (int *)malloc(sizeof(int) * (*returnSize));
-	int i = binarySeach(A, ASize), j = i - 1;
-	arraySquare(A, ASize);
-	//merge
-	for (int k = 0; i < ASize || j >= 0; ++k)
+	int i = 0, j = ASize - 1;
+	for (int k = ASize - 1; k >= 0; --k)
 	{
-		if (i == ASize)
-			res[k] = A[j--];
-		else if (j == -1)
-			res[k] = A[i++];
+		if (abs(A[i]) > abs(A[j]))
+		{
+			res[k] = A[i] * A[i];
+			++i;
+		}
 		else
-			res[k] = (A[i] > A[j]) ? A[j--] : A[i++];
+		{
+			res[k] = A[j] * A[j];
+			--j;
+		}
 	}
 	return res;
 }
