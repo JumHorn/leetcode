8b9f08ea876a89db5dcc6a322f818c2a15919a96
JumHorn <JumHorn@gmail.com>
2020-10-30 14:42:01 +0800

    Surface Area of 3D Shapes beats 76.42%

--- C/SurfaceAreaof3DShapes.c
+++ C/SurfaceAreaof3DShapes.c
@@ -1,50 +1,27 @@
 
+#define max(a, b) (((a) > (b)) ? (a) : (b))
 
-int surfaceArea(int** grid, int gridSize, int* gridColSize){
-	int res=0;
-	for(int i=0;i<gridSize;i++)
-		for(int j=0;j<gridSize;j++)
-			if(grid[i][j]>0)
-				res++;
-	res*=2;
-	for(int i=0;i<gridSize;i++)
-		for(int j=0;j<gridSize;j++)
+int surfaceArea(int **grid, int gridSize, int *gridColSize)
+{
+	int M = gridSize, N = *gridColSize, res = 0;
+	//board dfs direction
+	int path[5] = {-1, 0, 1, 0, -1};
+	for (int i = 0; i < M; ++i)
+	{
+		for (int j = 0; j < N; ++j)
 		{
-			if(i>0)
+			if (grid[i][j] == 0)
+				continue;
+			for (int k = 0; k < 4; ++k)
 			{
-				if(grid[i][j]>grid[i-1][j])
-					res+=grid[i][j]-grid[i-1][j];
+				int dx = i + path[k], dy = j + path[k + 1];
+				if (dx >= 0 && dx < M && dy >= 0 && dy < N)
+					res += max(0, grid[i][j] - grid[dx][dy]);
+				else
+					res += grid[i][j];
 			}
-			else
-				res+=grid[i][j];
-
-			if(i<gridSize-1)
-			{
-				if(grid[i][j]>grid[i+1][j])
-					res+=grid[i][j]-grid[i+1][j];
-			}
-			else
-				res+=grid[i][j];
-
-			if(j>0)
-			{
-				if(grid[i][j]>grid[i][j-1])
-					res+=grid[i][j]-grid[i][j-1];
-			}
-			else
-				res+=grid[i][j];
-
-			if(j<gridSize-1)
-			{
-				if(grid[i][j]>grid[i][j+1])
-					res+=grid[i][j]-grid[i][j+1];
-			}
-			else
-				res+=grid[i][j];
+			res += 2; //top bottom
 		}
-
+	}
 	return res;
 }
-
-
-
---
+++ Cplus/SurfaceAreaof3DShapes.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+	int surfaceArea(vector<vector<int>> &grid)
+	{
+		int M = grid.size(), N = grid[0].size(), res = 0;
+		//board dfs direction
+		int path[5] = {-1, 0, 1, 0, -1};
+		for (int i = 0; i < M; ++i)
+		{
+			for (int j = 0; j < N; ++j)
+			{
+				if (grid[i][j] == 0)
+					continue;
+				for (int k = 0; k < 4; ++k)
+				{
+					int dx = i + path[k], dy = j + path[k + 1];
+					if (dx >= 0 && dx < M && dy >= 0 && dy < N)
+						res += max(0, grid[i][j] - grid[dx][dy]);
+					else
+						res += grid[i][j];
+				}
+				res += 2; //top bottom
+			}
+		}
+		return res;
+	}
+};
