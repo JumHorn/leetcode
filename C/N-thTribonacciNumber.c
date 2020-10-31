6bac3064f28b0001393f9d5018e7cffe21ceecb1
JumHorn <JumHorn@gmail.com>
2020-10-31 11:46:02 +0800

    N-th Tribonacci Number beats 100%

--- C/N-thTribonacciNumber.c
+++ C/N-thTribonacciNumber.c
@@ -1,18 +1,17 @@
-#include<stdio.h>
-#include<stdlib.h>
 
-int tribonacci(int n){
-	if(n==0)
+int tribonacci(int n)
+{
+	if (n == 0)
 		return 0;
-	if(n==1)
+	if (n == 1 || n == 2)
 		return 1;
-	if(n==2)
-		return 1;
-	int *p=(int*)malloc((n+1)*sizeof(int));
-	p[0]=0;
-	p[1]=1;
-	p[2]=1;
-	for(int i=3;i<=n;i++)
-		p[i]=p[i-1]+p[i-2]+p[i-3];
-	return p[n];
+	int f1 = 0, f2 = 1, f3 = 1;
+	for (int i = 3; i <= n; ++i)
+	{
+		int f4 = f1 + f2 + f3;
+		f1 = f2;
+		f2 = f3;
+		f3 = f4;
+	}
+	return f3;
 }
