aac446d4a332eef28f2e797d44c9bd362a8de247
JumHorn <JumHorn@gmail.com>
2020-10-31 13:07:41 +0800

    more simplified counting sort version

--- C/RelativeSortArray.c
+++ C/RelativeSortArray.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <string.h>
 
 /*
 counting sort
@@ -10,27 +9,21 @@ counting sort
  */
 int* relativeSortArray(int* arr1, int arr1Size, int* arr2, int arr2Size, int* returnSize)
 {
-	int count1[1001] = {0}, count2[1001];
-	memset(count2, -1, sizeof(count2));
-	for (int i = 0; i < arr2Size; ++i)
-		count2[arr2[i]] = 0;
+	int count[1001] = {0};
 	for (int i = 0; i < arr1Size; ++i)
-		count2[arr1[i]] == -1 ? ++count1[arr1[i]] : ++count2[arr1[i]];
+		++count[arr1[i]];
 	*returnSize = arr1Size;
 	int* res = (int*)malloc(sizeof(int) * (*returnSize));
-	int resIndex = *returnSize;
-	for (int i = 1000; i >= 0; --i)
+	int index = 0;
+	for (int i = 0; i < arr2Size; ++i)
 	{
-		if (count2[i] == -1)  //move to end
-		{
-			while (--count1[i] >= 0)
-				res[--resIndex] = i;
-		}
+		while (--count[arr2[i]] >= 0)
+			res[index++] = arr2[i];
 	}
-	for (int i = arr2Size - 1; i >= 0; --i)
+	for (int i = 0; i <= 1000; ++i)
 	{
-		while (--count2[arr2[i]] >= 0)
-			res[--resIndex] = arr2[i];
+		while (--count[i] >= 0)
+			res[index++] = i;
 	}
 	return res;
 }
