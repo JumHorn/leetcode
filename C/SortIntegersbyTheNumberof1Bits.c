3a0f8e399dd28636461a2efbaf0f9fc88ae7ebf2
JumHorn <JumHorn@gmail.com>
2020-11-01 16:20:24 +0800

    Sort Integers by The Number of 1 Bits beats 100%

---
+++ C/SortIntegersbyTheNumberof1Bits.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+
+int bitCount(int n)
+{
+	int res = 0;
+	while (n != 0)
+	{
+		++res;
+		n &= n - 1;
+	}
+	return res;
+}
+
+//cmp function don't consider overflow
+int cmp(const void *lhs, const void *rhs)
+{
+	int a = bitCount(*(int *)lhs), b = bitCount(*(int *)rhs);
+	if (a != b)
+		return a - b;
+	return *(int *)lhs - *(int *)rhs;
+}
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int *sortByBits(int *arr, int arrSize, int *returnSize)
+{
+	*returnSize = arrSize;
+	qsort(arr, arrSize, sizeof(int), cmp);
+	return arr;
+}
--- Cplus/SortIntegersbyTheNumberof1Bits.cpp
+++ Cplus/SortIntegersbyTheNumberof1Bits.cpp
@@ -1,11 +1,12 @@
 #include <algorithm>
+#include <bitset>
 #include <vector>
 using namespace std;
 
 class Solution
 {
 public:
-	vector<int> sortByBits(vector<int>& arr)
+	vector<int> sortByBits(vector<int> &arr)
 	{
 		sort(arr.begin(), arr.end(), *this);
 		return arr;
@@ -13,20 +14,9 @@ public:
 
 	bool operator()(int lhs, int rhs)
 	{
-		int a = bitCount(lhs), b = bitCount(rhs);
-		if (a == b)
-			return lhs < rhs;
-		return a < b;
-	}
-
-	int bitCount(int n)
-	{
-		int res = 0;
-		while (n != 0)
-		{
-			n &= n - 1;
-			res++;
-		}
-		return res;
+		int a = bitset<32>(lhs).count(), b = bitset<32>(rhs).count();
+		if (a != b)
+			return a < b;
+		return lhs < rhs;
 	}
 };
