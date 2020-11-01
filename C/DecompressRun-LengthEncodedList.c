450e67df37c6b8e234f86b8e4665a8627e9e73db
JumHorn <JumHorn@gmail.com>
2020-11-01 18:08:55 +0800

    Decompress Run-Length Encoded List beats 96.24%

---
+++ C/DecompressRun-LengthEncodedList.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* decompressRLElist(int* nums, int numsSize, int* returnSize)
+{
+	int* res = (int*)malloc(sizeof(int) * 10000);
+	*returnSize = 0;
+	for (int i = 0; i < numsSize; i += 2)
+	{
+		for (int j = 0; j < nums[i]; ++j)
+			res[(*returnSize)++] = nums[i + 1];
+	}
+	return res;
+}
--- Cplus/DecompressRun-LengthEncodedList.cpp
+++ Cplus/DecompressRun-LengthEncodedList.cpp
@@ -4,12 +4,12 @@ using namespace std;
 class Solution
 {
 public:
-	vector<int> decompressRLElist(vector<int>& nums)
+	vector<int> decompressRLElist(vector<int> &nums)
 	{
 		vector<int> res;
 		for (int i = 0; i < (int)nums.size(); i += 2)
 		{
-			for (int j = 0; j < nums[i]; j++)
+			for (int j = 0; j < nums[i]; ++j)
 				res.push_back(nums[i + 1]);
 		}
 		return res;
