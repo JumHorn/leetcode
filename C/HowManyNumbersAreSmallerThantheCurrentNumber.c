6936b9390fc6ad7f22f18684ab03d62ca791043a
JumHorn <JumHorn@gmail.com>
2020-11-01 15:52:52 +0800

    How Many Numbers Are Smaller Than the Current Number beats 99.51%

---
+++ C/HowManyNumbersAreSmallerThantheCurrentNumber.c
@@ -0,0 +1,19 @@
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* smallerNumbersThanCurrent(int* nums, int numsSize, int* returnSize)
+{
+	*returnSize = numsSize;
+	int count[101] = {0};
+	for (int i = 0; i < numsSize; ++i)
+		++count[nums[i]];
+	for (int i = 1; i < 101; ++i)
+		count[i] += count[i - 1];
+	for (int i = 0; i < numsSize; ++i)
+	{
+		if (nums[i] != 0)
+			nums[i] = count[nums[i] - 1];
+	}
+	return nums;
+}
--- Cplus/HowManyNumbersAreSmallerThantheCurrentNumber.cpp
+++ Cplus/HowManyNumbersAreSmallerThantheCurrentNumber.cpp
@@ -4,14 +4,14 @@ using namespace std;
 class Solution
 {
 public:
-	vector<int> smallerNumbersThanCurrent(vector<int>& nums)
+	vector<int> smallerNumbersThanCurrent(vector<int> &nums)
 	{
 		vector<int> count(101);
 		for (auto n : nums)
 			++count[n];
-		for (int i = 1; i < 101; i++)
+		for (int i = 1; i < 101; ++i)
 			count[i] += count[i - 1];
-		for (int i = 0; i < (int)nums.size(); i++)
+		for (int i = 0; i < (int)nums.size(); ++i)
 		{
 			if (nums[i] != 0)
 				nums[i] = count[nums[i] - 1];
