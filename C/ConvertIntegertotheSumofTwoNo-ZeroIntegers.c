555b3d5f60fc74686a222e7a56e5da4deea3a0d3
JumHorn <JumHorn@gmail.com>
2020-11-01 18:03:45 +0800

    Convert Integer to the Sum of Two No-Zero Integers beats 100%

---
+++ C/ConvertIntegertotheSumofTwoNo-ZeroIntegers.c
@@ -0,0 +1,32 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool noZero(int n)
+{
+	while (n != 0)
+	{
+		if (n % 10 == 0)
+			return false;
+		n /= 10;
+	}
+	return true;
+}
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* getNoZeroIntegers(int n, int* returnSize)
+{
+	*returnSize = 2;
+	int* res = (int*)malloc(sizeof(int) * (*returnSize));
+	for (int i = 1; i <= n / 2; i++)
+	{
+		if (noZero(i) && noZero(n - i))
+		{
+			res[0] = i;
+			res[1] = n - i;
+			break;
+		}
+	}
+	return res;
+}
