3b3f5182d5983c97d467625294d14ca2898fa85d
JumHorn <JumHorn@gmail.com>
2020-11-01 21:11:42 +0800

    Minimum Subsequence in Non-Increasing Order beats 93.02%

--- Cplus/MinimumSubsequenceinNon-IncreasingOrder.cpp
+++ Cplus/MinimumSubsequenceinNon-IncreasingOrder.cpp
@@ -6,15 +6,15 @@ using namespace std;
 class Solution
 {
 public:
-	vector<int> minSubsequence(vector<int>& nums)
+	vector<int> minSubsequence(vector<int> &nums)
 	{
 		make_heap(nums.begin(), nums.end());
-		int sum = accumulate(nums.begin(), nums.end(), 0), tmp = 0;
+		int sum = accumulate(nums.begin(), nums.end(), 0), prefixsum = 0;
 		vector<int> res;
-		for (int i = nums.size() - 1; i >= 0 && tmp <= sum - tmp; --i)
+		for (int i = (int)nums.size() - 1; i >= 0 && prefixsum <= sum - prefixsum; --i)
 		{
 			pop_heap(nums.begin(), nums.begin() + i + 1);
-			tmp += nums[i];
+			prefixsum += nums[i];
 			res.push_back(nums[i]);
 		}
 		return res;
