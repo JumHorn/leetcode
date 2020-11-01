fe78fff85b8f5d4c8c7c69235244e97037761708
JumHorn <JumHorn@gmail.com>
2020-11-01 19:44:16 +0800

    Number of Students Doing Homework at a Given Time beats 100%

--- Cplus/NumberofStudentsDoingHomeworkataGivenTime.cpp
+++ Cplus/NumberofStudentsDoingHomeworkataGivenTime.cpp
@@ -4,13 +4,13 @@ using namespace std;
 class Solution
 {
 public:
-	int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime)
+	int busyStudent(vector<int> &startTime, vector<int> &endTime, int queryTime)
 	{
-		int res = 0, n = startTime.size();
-		for (int i = 0; i < n; i++)
+		int res = 0, N = startTime.size();
+		for (int i = 0; i < N; ++i)
 		{
 			if (queryTime >= startTime[i] && queryTime <= endTime[i])
-				res++;
+				++res;
 		}
 		return res;
 	}
