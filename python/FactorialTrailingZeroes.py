1eeafa522721bd6047a0e1cb9af741592ea5264e
JumHorn <JumHorn@gmail.com>
2020-11-02 18:27:56 +0800

    Factorial Trailing Zeroes beats 62.90%

--- python/FactorialTrailingZeroes.py
+++ python/FactorialTrailingZeroes.py
@@ -1,12 +1,7 @@
-#coding=UTF-8
-class FactorialTrailingZeroes(object):
-    def trailingZeroes(self,n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        sum=0    #统计这些数中5的个数'
-        while n!=0:
-            n=n//5
-            sum+=n
-        return sum
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        res = 0  # 统计这些数中5的个数'
+        while n != 0:
+            n = n // 5
+            res += n
+        return res
