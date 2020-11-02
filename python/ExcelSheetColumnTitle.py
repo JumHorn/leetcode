8abd8d7c79140f7d19b6b7dba819d7e50f0519d8
JumHorn <JumHorn@gmail.com>
2020-11-02 17:58:56 +0800

    Excel Sheet Column Title beats 70.29%

--- python/ExcelSheetColumnTitle.py
+++ python/ExcelSheetColumnTitle.py
@@ -1,18 +1,8 @@
-class ExcelSheetColumnTitle(object):
-    def convertToTitle(self,n):
-        """
-        :type n: int
-        :rtype: str
-        """
-        l=[]
-        while n!=0:
-            if n%26==0:
-                l.append('Z')
-            else:
-                l.append(chr(64+n%26))
-            if n%26==0:
-                n=n//26-1
-                continue
-            n=n//26
-        l.reverse()
-        return ''.join(l)
+class Solution:
+    def convertToTitle(self, n: int) -> str:
+        res = []
+        while n != 0:
+            res.append(chr((n - 1) % 26 + ord('A')))
+            n = (n - 1) // 26
+        res.reverse()
+        return "".join(res)
