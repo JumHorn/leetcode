aaf0d80b90023526bf15f3c57c1bd414686df8de
JumHorn <JumHorn@gmail.com>
2020-11-02 20:02:55 +0800

    Intersection of Two Linked Lists beats 41.74%

--- python/IntersectionoftwoLinkedList.py
+++ python/IntersectionoftwoLinkedList.py
@@ -1,9 +1,16 @@
-#coding UTF-8
-#definition for singly-linked list
-class ListNode(object):
-    def __init__(self,x):
-        self.val=x
-        self.next=None
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
-class IntersectionoftwoLinkedLists(object):
-    def getIntersectionNode(self,headA,headB):
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        pA, pB = headA, headB
+        if pA is None or pB is None:
+            return None
+        while pA is not pB:
+            pA = headB if pA is None else pA.next
+            pB = headA if pB is None else pB.next
+        return pA
