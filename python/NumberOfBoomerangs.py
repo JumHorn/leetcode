0980b322fa4a287030844821dea3b574c72d40af
JumHorn <JumHorn@gmail.com>
2020-11-02 21:27:08 +0800

    Number of Boomerangs beats 85.29%

--- python/NumberOfBoomerangs.py
+++ python/NumberOfBoomerangs.py
@@ -1,24 +1,15 @@
-class NumberofBoomerangs(object):
-    def numberofBoomerangs(self,points):
-        """
-        :type points: List[List[int]]
-        :rtype: int
-        """
-        i=0
-        n=0
-        while i<len(points):
-            j=0
-            distance={}
-            while j<len(points):
-                if i!=j:
-                    d=(points[i][0]-points[j][0])*(points[i][0]-points[j][0])+(points[i][1]-points[j][1])*(points[i][1]-points[j][1])
-                    if d in distance:
-                        distance[d]+=1
-                    else:
-                        distance[d]=1
-                j+=1
-            for dis in distance.values():
-                if dis>=2:
-                    n+=dis*(dis-1)
-            i+=1
-        return n
+from typing import List
+
+
+class Solution:
+    def numberOfBoomerangs(self, points: List[List[int]]) -> int:
+        res = 0
+        for p in points:
+            cmap = {}
+            for q in points:
+                f = p[0]-q[0]
+                s = p[1]-q[1]
+                cmap[f*f + s*s] = 1 + cmap.get(f*f + s*s, 0)
+            for k in cmap:
+                res += cmap[k] * (cmap[k] - 1)
+        return res
