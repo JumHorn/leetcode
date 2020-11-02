61078801da1339360f5a1911c2a564b5173cd596
JumHorn <JumHorn@gmail.com>
2020-11-02 21:34:47 +0800

    Rectangle Area beats 44.76%

--- python/RectangleArea.py
+++ python/RectangleArea.py
@@ -1,7 +1,8 @@
-class RectangleArea(object):
-    def computerArea(self,A,B,C,D,E,F,G,H):
-        area=(C-A)*(D-B)+(G-E)*(H-F)
-        if B>=H or C<=E or F>=D or A>=G:
-            return area
-        extra=(min(C,G)-max(A,E))*(min(D,H)-max(B,F))
-        return area-extra
+class Solution:
+    def computeArea(self, A: int, B: int, C: int, D: int, E: int, F: int, G: int, H: int) -> int:
+        area1 = (C - A) * (D - B)
+        area2 = (G - E) * (H - F)
+        intersection = 0
+        if E < C and G > A and F < D and H > B:
+            intersection = (min(C, G) - max(A, E)) * (min(D, H) - max(B, F))
+        return area1 + (area2 - intersection)
