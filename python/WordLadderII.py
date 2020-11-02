129b4526a5b0b47de019e7c3f844090ba086680d
JumHorn <JumHorn@gmail.com>
2020-11-02 23:26:26 +0800

    Word Ladder II beats 61.49%

--- python/WordLadderII.py
+++ python/WordLadderII.py
@@ -1,9 +1,25 @@
-class WordLadder2(object):
-    def findLadders(self,beginWord,endWord,wordList):
-        """
-        :type beginWord: str
-        :type endWord: str
-        :type wordlist: Set[str]
-        :rtype: List[List[str]]
-        """
-        
+from typing import List
+
+
+class Solution:
+    def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
+        wordList = set(wordList)
+        res = []
+        layer = {}
+        layer[beginWord] = [[beginWord]]
+
+        while layer:
+            newlayer = collections.defaultdict(list)
+            for w in layer:
+                if w == endWord:
+                    res.extend(k for k in layer[w])
+                else:
+                    for i in range(len(w)):
+                        for c in 'abcdefghijklmnopqrstuvwxyz':
+                            neww = w[:i]+c+w[i+1:]
+                            if neww in wordList:
+                                newlayer[neww] += [j+[neww] for j in layer[w]]
+
+            wordList -= set(newlayer.keys())
+            layer = newlayer
+        return res
