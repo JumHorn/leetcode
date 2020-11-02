37dce41115563b5edecc4c1d2b7b49076ddc25a8
JumHorn <JumHorn@gmail.com>
2020-11-02 19:00:24 +0800

    Guess Number Higher or Lower beats 90.23%

--- python/GuessNumberHigherOrLower.py
+++ python/GuessNumberHigherOrLower.py
@@ -1,24 +1,19 @@
-#coding=UTF-8
 # The guess API is already defined for you.
 # @param num, your guess
 # @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
-# def guess(num):
+def guess(num: int) -> int:
 
-class GuessNumberHigherOrLower(object):
-    def guessNumber(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        r=n//2
-        a=1
-        b=n
-        while guess(r)!=0:
-            if guess(r)==-1:
-                r=r-1
-                b=r
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        lo, hi = 1, n
+        while lo < hi:
+            mi = (hi - lo) // 2 + lo
+            g = guess(mi)
+            if g == 0:
+                return mi
+            if g > 0:
+                lo = mi + 1
             else:
-                r=r+1
-                a=r
-            r=(a+b)//2
-        return r
+                hi = mi - 1
+        return lo
