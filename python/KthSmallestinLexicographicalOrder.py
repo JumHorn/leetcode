51aaa27df063151cbffa89a7983d1d6932be679e
JumHorn <JumHorn@gmail.com>
2020-11-02 20:37:53 +0800

    K-th Smallest in Lexicographical Order beats 87.25%

--- python/KthSmallestinLexicographicalOrder.py
+++ python/KthSmallestinLexicographicalOrder.py
@@ -1,33 +1,17 @@
-#coding=utf8
-class KthSmallestinLexicographicalOrder(object):
-    def findKthNumber(self,n,k):
-        """
-        :type n:int 
-        :type k:int
-        :rtype:int
-        """
-        #十叉树的先序遍历，判断1和2之间有多少个数，再对下一位做判断
-        k-=1
-        left=1
-        while k>0:
-            num=self.numBetween(n,left)
-            if num<=k:
-                k-=num
-                left+=1
+class Solution:
+    def findKthNumber(self, n: int, k: int) -> int:
+        res = 1
+        k -= 1
+        while k > 0:
+            count = 0
+            interval = [res, res + 1]
+            while interval[0] <= n:
+                count += (min(n + 1, interval[1]) - interval[0])
+                interval = [10 * interval[0], 10 * interval[1]]
+            if k >= count:
+                res += 1
+                k -= count
             else:
-                left*=10
-                k-=1
-        return left
-    def numBetween(self,n,left):
-        num=0
-        right=left+1
-        while left<=n:
-            num+=min(n+1,right)-left
-            left*=10
-            right*=10
-        return num
-
-n=10000
-k=9999
-K=KthSmallestinLexicographicalOrder()
-print K.findKthNumber(n,k)
+                res *= 10
+                k -= 1
+        return res
