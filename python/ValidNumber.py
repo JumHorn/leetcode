d2cd5230e9b5c739edc7b194d5b94239e7c7b7f6
JumHorn <JumHorn@gmail.com>
2020-11-02 22:41:24 +0800

    Valid Number beats 76.89%

--- python/ValidNumber.py
+++ python/ValidNumber.py
@@ -1,85 +1,53 @@
-class ValidNumber(object):
-    def isNumber(self,s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        s=s.strip()
-        if len(s)==0:
-            return False
-        d=['0','1','2','3','4','5','6','7','8','9','.']
-        c=['+','-','e']
-        for n in s:
-            if n not in d and n not in c:
-                return False
-        if s[0] == 'e' or s[len(s)-1] in c:
-            return False
-        digits=s.split('e')
-        if len(digits)>2:
-            return False
-        i=0
-        for n in digits:
-            if len(n)==0:
-                return False
-            if '.' in n and i==1:
-                return False
-            if '.' in n:
-                l=n.split('.')
-                # if len(l)!=2:
-                #     return False
-                # else:
-                # print l,len(l)
-                if len(l)==2:
-                # print n
-                    if not self.check1(n):
-                        return False
-                else:
-                    return False
-            else:
-                if not self.check0(n):
-                    return False
-            i+=1
-            # l=n.split('.')
-            # if len(l)>2:
-            #     return False
-            # if len(l)
-        return True
+"""
+state machine
+possible state
+0. invalid
+1. number
+2. number or exponent
+3. only number
+4. sign
+5. point
+6. exponent
+7. exponent with sign
+8. none start
 
-    def check0(self,l):
-        if not self.check_sign(l):
-            return False
-        return True
-    def check1(self,l):
-        # if len(l)==0:
-        #     return False
-        if not self.check_sign(l):
-            return False
-        if len(l)==1:
-            return False
-        return True
-    def check_sign(self,l):
-        if '-' in l:
-            if len(l)==1:
-                return False
-            if l[0]!='-':
-                return False
-            else:
-                if '.' in l and len(l)==2:
-                    return False
-            if len(l)>=3 and '-' in l[1:len(l)-1]:
-                return False 
-        if '+' in l:
-            if len(l)==1:
-                return False
-            if l[0]!='+':
-                return False
-            else:
-                if '.' in l and len(l)==2:
-                    return False
-            if len(l)>=3 and '+' in l[1:len(l)-1]:
-                return False   
-        return True     
+state machine
+                            +/-     .       e       number
+invalid				0		0		0		0		0
+none start			1		5		6		0		2
+number				2		0		3		7		2
+number and exponent	3		0		0		7		3
+only number			4		0		0		0		4
+sign				5		0		6		0		2
+point				6		0		0		0		3
+exponent			7		8		0		0		4
+exponent and sign	8		0		0		0		4
+"""
 
-s="+."
-V=ValidNumber()
-print V.isNumber(s)               
+
+class Solution:
+    def isNumber(self, s: str) -> bool:
+        state = [
+            [0, 0, 0, 0],
+            [5, 6, 0, 2],
+            [0, 3, 7, 2],
+            [0, 0, 7, 3],
+            [0, 0, 0, 4],
+            [0, 6, 0, 2],
+            [0, 0, 0, 3],
+            [8, 0, 0, 4],
+            [0, 0, 0, 4]]
+        s = s.strip()
+        n = 1  # state start
+        for c in s:
+            if c == '-' or c == '+':
+                n = state[n][0]
+            elif c == '.':
+                n = state[n][1]
+            elif c == 'e':
+                n = state[n][2]
+            elif c >= '0' and c <= '9':
+                n = state[n][3]
+            else:
+                return False
+        return n in (2, 3, 4)  # valid final state
