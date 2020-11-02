935d7455e2ee833c29d0208b1315e4a1daca85ab
JumHorn <JumHorn@gmail.com>
2020-11-02 13:56:23 +0800

    Add Binary beats 79.89%

--- python/AddBinary.py
+++ python/AddBinary.py
@@ -1,48 +1,19 @@
-class AddBinary(object):
-    def addBinary(self,a,b):
-        l=[]
-        carry=0
-        
-        if len(a)>=len(b):
-            i=len(a)-1
-            for m in b[::-1]:
-                t=int(a[i])+int(m)+carry
-                carry=self.add(t,l)
-                i=i-1
-        else:
-            i=len(b)-1
-            for n in a[::-1]:
-                t=int(n)+int(b[i])+carry
-                carry=self.add(t,l)
-                i=i-1
-        if len(a)>len(b):
-            for n in a[-len(b)-1::-1]:
-                t=int(n)+carry
-                carry=self.add(t,l)
-        if len(a)<len(b):
-            for m in b[-len(a)-1::-1]:
-                t=int(m)+carry
-                carry=self.add(t,l)
-        if carry==1:
-            l.insert(len(l),'1')
-            print l
-        l.reverse()
-        return ''.join(l)
-    def add(self,t,l):
-        if t==3:
-            l.append('1')
-            carry=1
-        elif t==2:
-            l.append('0')
-            carry=1
-        elif t==1:
-            l.append('1')
-            carry=0
-        else:
-            l.append('0')
-            carry=0
-        return carry
-
-
-addb=AddBinary()
-addb.addBinary("1","1")
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        res = ""
+        i, j, carry = len(a) - 1, len(b) - 1, 0
+        while i >= 0 or j >= 0 or carry > 0:
+            if i >= 0:
+                carry += int(a[i])
+                i -= 1
+            if j >= 0:
+                carry += int(b[j])
+                j -= 1
+            res += str(carry % 2)
+            carry //= 2
+        return res[::-1]
