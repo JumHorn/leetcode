595f5b4ae6ac733e8382b30048be906defe5041d
JumHorn <JumHorn@gmail.com>
2020-11-02 17:01:41 +0800

    Contains Duplicate beats 99.65%

--- python/ContainsDuplicate.py
+++ python/ContainsDuplicate.py
@@ -1,15 +1,6 @@
-#coding=UTF-8
-class ContainsDuplicate2(object):
-    def containsNearbyDuplicate(self,nums,k):
-        '''
-        :type nums:List[int]
-        :type k:int
-        :rtype:bool
-        '''
-        numDict = dict()#的构造函数,构造的dict是{“值”：“下标”}
-        for n in range(len(nums)):
-            i = numDict.get(nums[n])
-            if i >= 0 and n - i <= k:
-                return True
-            numDict[nums[n]] = n
-        return False       
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
