d2b135e2cf4953e8dcef4d8d5612b76c8d890e2a
JumHorn <JumHorn@gmail.com>
2020-11-02 18:55:40 +0800

    Find All Anagrams in a String beats 95.07%

--- python/FindAllAnagramsinaString.py
+++ python/FindAllAnagramsinaString.py
@@ -1,45 +1,21 @@
-class FindAllAnagramsinaString(object):
-    def findAllAnagramsinaString(self,s,p):
-        """
-        :type s: str
-        :type p: str
-        :rtype: List[int]
-        """
-        result=[]
-        if len(s)<len(p):
-            return result
-        data={}
-        for n in p:
-            if n not in data:
-                data[n]=1
-            else:
-                data[n]+=1
-        l=0
-        r=0
-        count=len(p)
-        while r<len(s):
-            if s[r] in data:
-                data[s[r]]-=1
-                count-=1
-            r+=1
-            print s[l:r],count
-            print data
-            if count==0:
-                t=0
-                for n in data.values():
-                    if n!=0:
-                        t=1
-                        break
-                if t==0:
-                    result.append(l)
-            if r-l==len(p):
-                if s[l] in data:
-                    data[s[l]]+=1
-                    count+=1
-                l+=1
-        return result
+from typing import List
 
-s="cbaebabacd"
-p="abc"
-F=FindAllAnagramsinaString()
-print F.findAllAnagramsinaString(s,p)
+
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        shash, phash, res = [0] * 26, [0] * 26, []
+        slen, plen = len(s), len(p)
+        if slen < plen:
+            return res
+        for c in p:
+            phash[ord(c) - ord('a')] += 1
+        for i in range(plen):
+            shash[ord(s[i]) - ord('a')] += 1
+        if shash == phash:
+            res.append(0)
+        for i in range(slen - plen):
+            shash[ord(s[i]) - ord('a')] -= 1
+            shash[ord(s[i+plen]) - ord('a')] += 1
+            if shash == phash:
+                res.append(i + 1)
+        return res
