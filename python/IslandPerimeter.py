758722126241941905b4075346a358938883d0a3
JumHorn <JumHorn@gmail.com>
2020-11-02 20:15:55 +0800

    Island Perimeter beats 87.84%

--- python/IslandPerimeter.py
+++ python/IslandPerimeter.py
@@ -1,3 +1,16 @@
-def islandPerimeter(self, grid):
-    return sum(sum(map(operator.ne, [0] + row, row + [0]))
-               for row in grid + map(list, zip(*grid)))
+from typing import List
+
+
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        M, N = len(grid), len(grid[0])
+        islands, neighbours = 0, 0
+        for i in range(M):
+            for j in range(N):
+                if grid[i][j] == 1:
+                    islands += 1
+                    if i < M - 1 and grid[i + 1][j] == 1:
+                        neighbours += 1
+                    if j < N - 1 and grid[i][j + 1] == 1:
+                        neighbours += 1
+        return islands * 4 - neighbours * 2
