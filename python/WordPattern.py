317881bf24e9bc33721be952eef71b2df40305d0
JumHorn <JumHorn@gmail.com>
2020-11-02 23:24:03 +0800

    Word Pattern beats 71.35%

--- python/WordPattern.py
+++ python/WordPattern.py
@@ -1,25 +1,5 @@
-class WordPattern(object):
-    def wordPattern(self,pattern,str):
-        """
-        :type pattern:str
-        :type str:str
-        :rtype:bool
-        """
-        l=str.split(" ")
-        if pattern=="" or l=="":
-            return False
-        if len(pattern)!=len(l):
-            return False
-        i=0
-        dic={}
-        while i<len(l):
-            if pattern[i] not in dic:
-                if l[i] in dic.values():
-                    return False
-                else:
-                    dic[pattern[i]]=l[i]
-            else:
-                if dic[pattern[i]]!=l[i]:
-                    return False
-            i=i+1
-        return True
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        p = pattern
+        t = s.split()
+        return len(set(zip(p, t))) == len(set(p)) == len(set(t)) and len(p) == len(t)
