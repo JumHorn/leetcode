1d0b28921ab83fb95c1ce8e6cf736dc5ae15548e
JumHorn <JumHorn@gmail.com>
2020-11-02 23:32:13 +0800

    formatted

--- Cplus/RotateFunction.cpp
+++ Cplus/RotateFunction.cpp
@@ -1,4 +1,3 @@
-#include <climits>
 #include <numeric>
 #include <vector>
 using namespace std;
@@ -9,13 +8,13 @@ public:
 	int maxRotateFunction(vector<int> &A)
 	{
 		long sum = accumulate(A.begin(), A.end(), 0L);
-		long res = INT_MIN, n = A.size(), value = 0;
-		for (int i = 0; i < n; i++)
+		long N = A.size(), value = 0;
+		for (int i = 0; i < N; ++i)
 			value += i * A[i];
-		res = value;
-		for (int i = 1; i < n; i++)
+		long res = value;
+		for (int i = 1; i < N; ++i)
 		{
-			value = value + sum - n * A[n - i];
+			value = value + sum - N * A[N - i];
 			res = max(res, value);
 		}
 		return res;
