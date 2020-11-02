36a297f15cde807cefaab94da0c0b0d2bcf56e8a
JumHorn <JumHorn@gmail.com>
2020-11-02 14:14:23 +0800

    Arithmetic Slices II - Subsequence beats 91.18%

--- python/ArithmeticSlicesIISubsequence.py
+++ python/ArithmeticSlicesIISubsequence.py
@@ -1,21 +1,16 @@
-#coding=utf8
-class ArithmeticSlicesIISubsequence(object):
-    def numberOfArithmeticSlices(self,A):
-        '''这里使用动态规划的思路dp
-           将每个数与前面所有的数的diff生成字典
-           每有新diff相同，就是一个等差数列
-        '''
-        sum=0
-        dic=[{} for _ in A] #生成A长度的list内容是字典
+class Solution(object):
+    def numberOfArithmeticSlices(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        sum = 0
+        dp = [{} for _ in A]  # 生成A长度的list内容是字典
         for i in xrange(len(A)):
             for j in xrange(i):
-                diff=A[i]-A[j]
-                dic[i][diff]=dic[i].get(diff,0)+1
-                if diff in dic[j]:
-                    sum+=dic[j][diff]
-                    dic[i][diff]+=dic[j][diff]
+                diff = A[i]-A[j]
+                dp[i][diff] = dp[i].get(diff, 0)+1
+                if diff in dp[j]:
+                    sum += dp[j][diff]
+                    dp[i][diff] += dp[j][diff]
         return sum
-
-A=[2,2,3,4,5]
-Ar=ArithmeticSlicesIISubsequence()
-print Ar.numberOfArithmeticSlices(A)
