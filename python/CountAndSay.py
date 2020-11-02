e1349f3d64f189a25742a55670547adf1fd57b18
JumHorn <JumHorn@gmail.com>
2020-11-02 17:40:02 +0800

    Count and Say beats 37.56%

--- python/CountAndSay.py
+++ python/CountAndSay.py
@@ -1,24 +1,17 @@
-class CountandSay(object):
-    def countAndSay(self,n):
-        '''
-        :type n: int
-        :rtype:  str
-        '''
-        if n==1:
-            return '1'
-        s=self.countAndSay(n-1)
-        i=0
-        r=''
-        while i<len(s):
-            j=i+1
-            while j<len(s):
-                if s[j]==s[i]:
-                    j+=1
-                else:
-                    break
-            r=r+'%d%s'%(j-i,s[i])
-            i=j
-        return r
-
-C=CountandSay()
-print C.countAndSay(6)            
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        if n == 1:
+            return "1"
+        data = self.countAndSay(n - 1)
+        res = ""
+        pre, count = '\0', 0
+        for c in data:
+            if pre != c:
+                if count != 0:
+                    res += str(count) + pre
+                pre = c
+                count = 1
+            else:
+                count += 1
+        res += str(count)+pre
+        return res
