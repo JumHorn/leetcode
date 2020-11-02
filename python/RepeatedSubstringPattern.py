846fa37682f37670f50779c2ca05a05c04e419de
JumHorn <JumHorn@gmail.com>
2020-11-02 21:44:41 +0800

    Repeated Substring Pattern beats 98.52%

--- python/RepeatedSubstringPattern.py
+++ python/RepeatedSubstringPattern.py
@@ -1,26 +1,3 @@
-class RepeatedSubstringPattern(object):
-    def repeatedSubstringPattern(self,str):
-        """
-        :type str: str
-        :rtype: bool
-        """
-        #ignore the length of str is 0 
-        if len(str)==1:
-            return False
-        j=1
-        while j<len(str)//2+1:
-            m=str[0:j]
-            print m ,j
-            i=j
-            while i<len(str):
-                if m!=str[i:i+j]:
-                    break
-                i+=j
-            if i==len(str):
-                return True
-            j+=1
-        return False
-
-str="abaababaab"
-R=RepeatedSubstringPattern()
-print R.repeatedSubstringPattern(str)         
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        return s in (s+s)[1:-1]
