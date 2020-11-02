fc14917b3e908c8391f1d662ba8ef07481c31659
JumHorn <JumHorn@gmail.com>
2020-11-02 21:59:07 +0800

    Rotate Function beats 68.25%

--- python/RotateFunction.py
+++ python/RotateFunction.py
@@ -1,37 +1,13 @@
-class RotateFunction(object):
-    def maxRotateFunction(self,A):
-        if not A:
-            return 0
-        i=0
-        max=-2147483649
-        while i<len(A):
-            sum=0
-            for j,n in enumerate(A):
-                sum=sum+j*n
-            if sum>max:
-                max=sum
-            A.insert(0,A[-1])
-            A.pop()
-            i=i+1
-        return max
+from typing import List
 
-R=RotateFunction()
-print R.maxRotateFunction([1,0,0,0,0,0,0,0,0,7])
 
-# simple Solution
-'''
-class Solution(object):
-    def maxRotateFunction(self, A):
-        """
-        :type A: List[int]
-        :rtype: int
-        """
-        sumA, l = sum(A), len(A)
-        temp_sum = sum([i * n for i, n in enumerate(A)])
-        max_sum = temp_sum
-        for v in A[:-1]:
-            temp_sum = temp_sum -sumA + l * v
-            if temp_sum > max_sum:
-                max_sum = temp_sum
-        return max_sum
-'''
+class Solution:
+    def maxRotateFunction(self, A: List[int]) -> int:
+        res, sumval, N, value = 0, sum(A), len(A), 0
+        for i in range(N):
+            value += i * A[i]
+        res = value
+        for i in range(1, N):
+            value += sumval - N * A[N - i]
+            res = max(res, value)
+        return res
