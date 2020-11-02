2d79e886c3e06d548953c0f65b7f1d1219ed9b5b
JumHorn <JumHorn@gmail.com>
2020-11-02 14:35:14 +0800

    Assign Cookies beats 90.69%

--- python/AssignCookies.py
+++ python/AssignCookies.py
@@ -1,24 +1,11 @@
-class AssignCookies(object):
-    def findContentChildren(self,g,s):
-        """
-        :type g: List[int]
-        :type s: List[int]
-        :rtype: int
-        """
-        if len(s)==0:
-            return 0
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
         g.sort()
         s.sort()
-        n=0
-        i=0
-        j=0
-        while i<len(g):
-            while j<len(s) and s[j]<g[i]:
-                j+=1
-            if j<len(s):
-                n+=1
-                i+=1
-                j+=1
-            else:
-                break
-        return n
+        res, i, j = 0, 0, 0
+        while i < len(g) and j < len(s):
+            if s[j] >= g[i]:
+                res += 1
+                i += 1
+            j += 1
+        return res
