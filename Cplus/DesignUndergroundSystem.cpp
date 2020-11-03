d80d1c72883a1a1f8c2886617120a7a21fe9e0d3
JumHorn <JumHorn@gmail.com>
2020-11-03 19:18:52 +0800

    Design Underground System beats 98.34%

--- Cplus/DesignUndergroundSystem.cpp
+++ Cplus/DesignUndergroundSystem.cpp
@@ -16,20 +16,19 @@ public:
 
 	void checkOut(int id, string stationName, int t)
 	{
-		auto tmp = avg[m[id].first][stationName];
-		avg[m[id].first][stationName] = {tmp.first + t - m[id].second, tmp.second + 1};
-		m.erase(id);
+		auto &time = avg[m[id].first][stationName];
+		avg[m[id].first][stationName] = {time.first + t - m[id].second, time.second + 1};
 	}
 
 	double getAverageTime(string startStation, string endStation)
 	{
-		auto tmp = avg[startStation][endStation];
-		return tmp.first * 1.0 / tmp.second;
+		auto &time = avg[startStation][endStation];
+		return time.first * 1.0 / time.second;
 	}
 
 private:
-	unordered_map<string, unordered_map<string, pair<int, int>>> avg;  //{startStation,{endStation,{all time,passage count}}}
-	unordered_map<int, pair<string, int>> m;						   //{id,{station,time}}
+	unordered_map<string, unordered_map<string, pair<int, int>>> avg; //{startStation,{endStation,{all time,passage count}}}
+	unordered_map<int, pair<string, int>> m;						  //{id,{station,time}}
 };
 
 /**
