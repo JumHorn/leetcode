39c729c7084744090727e3f9336d6b05888c0068
JumHorn <JumHorn@gmail.com>
2020-11-03 18:44:25 +0800

    Longest Happy String beats 100%

---
+++ C/LongestHappyString.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+
+typedef struct string
+{
+	char data[350];
+} string;
+
+string greedy(int a, int b, int c, char aa, char bb, char cc)
+{
+	string res;
+	if (a < b)
+		return greedy(b, a, c, bb, aa, cc);
+	if (b < c)
+		return greedy(a, c, b, aa, cc, bb);
+	if (b == 0)
+	{
+		memset(res.data, aa, min(2, a));
+		res.data[min(2, a)] = '\0';
+		return res;
+	}
+	int use_a = min(2, a), use_b = a - use_a >= b ? 1 : 0;
+	string data = greedy(a - use_a, b - use_b, c, aa, bb, cc);
+	memset(res.data, aa, use_a);
+	memset(&res.data[use_a], bb, use_b);
+	strcpy(&res.data[use_a + use_b], data.data);
+	return res;
+}
+
+char *longestDiverseString(int a, int b, int c)
+{
+	return greedy(a, b, c, 'a', 'b', 'c').data;
+}
--- Cplus/LongestHappyString.cpp
+++ Cplus/LongestHappyString.cpp
@@ -7,52 +7,19 @@ class Solution
 public:
 	string longestDiverseString(int a, int b, int c)
 	{
-		string res;
-		priority_queue<pair<int, char>> q;
-		if (a > 0)
-			q.push({a, 'a'});
-		if (b > 0)
-			q.push({b, 'b'});
-		if (c > 0)
-			q.push({c, 'c'});
-		//first one
-		auto tmp = q.top();
-		q.pop();
-		res.push_back(tmp.second);
-		if (tmp.first - 1 > 0)
-			q.push({tmp.first - 1, tmp.second});
-		//second one
-		if (q.empty())
-			return res;
-		tmp = q.top();
-		q.pop();
-		res.push_back(tmp.second);
-		if (tmp.first - 1 > 0)
-			q.push({tmp.first - 1, tmp.second});
-		//the other
-		while (!q.empty())
-		{
-			tmp = q.top();
-			q.pop();
-			int n = res.length();
-			if (tmp.second == res[n - 1] && tmp.second == res[n - 2])
-			{
-				if (q.empty())
-					break;
-				auto top = q.top();
-				q.pop();
-				res.push_back(top.second);
-				if (top.first - 1 > 0)
-					q.push({top.first - 1, top.second});
-			}
-			else
-			{
-				res.push_back(tmp.second);
-				--tmp.first;
-			}
-			if (tmp.first > 0)
-				q.push({tmp.first, tmp.second});
-		}
-		return res;
+		return greedy(a, b, c, 'a', 'b', 'c');
+	}
+
+	string greedy(int a, int b, int c, char aa, char bb, char cc)
+	{
+		if (a < b)
+			return greedy(b, a, c, bb, aa, cc);
+		if (b < c)
+			return greedy(a, c, b, aa, cc, bb);
+		if (b == 0)
+			return string(min(2, a), aa);
+		int use_a = min(2, a), use_b = a - use_a >= b ? 1 : 0;
+		return string(use_a, aa) + string(use_b, bb) +
+			   greedy(a - use_a, b - use_b, c, aa, bb, cc);
 	}
 };
