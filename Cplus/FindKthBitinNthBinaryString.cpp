1551a2bc6fdeaab4312e499609f1e3dc160d2d84
JumHorn <JumHorn@gmail.com>
2020-11-04 20:21:34 +0800

    recursive version

--- Cplus/FindKthBitinNthBinaryString.cpp
+++ Cplus/FindKthBitinNthBinaryString.cpp
@@ -4,12 +4,22 @@ class Solution
 public:
 	char findKthBit(int n, int k)
 	{
-		if (n == 1)
-			return '0';
-		if (k == (1 << (n - 1)))
-			return '1';
-		if (k < (1 << (n - 1)))
-			return findKthBit(n - 1, k);
-		return '1' - findKthBit(n, (1 << n) - k) + '0';
+		bool flip = false;
+		while (n > 1)
+		{
+			if (k == (1 << (n - 1)))
+			{
+				flip = !flip;
+				break;
+			}
+			if (k > (1 << (n - 1)))
+			{
+				flip = !flip;
+				k = (1 << n) - k;
+			}
+			else
+				--n;
+		}
+		return flip ? '1' : '0';
 	}
 };
