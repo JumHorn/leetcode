b054a392fe723d7ad39a426c2e506d183bc9ead6
JumHorn <JumHorn@gmail.com>
2020-11-05 16:26:18 +0800

    Minimum Operations to Make Array Equal beats 100%

---
+++ C/MinimumOperationstoMakeArrayEqual.c
@@ -0,0 +1,5 @@
+
+int minOperations(int n)
+{
+	return (n / 2) * ((n + 1) / 2);
+}
--- Cplus/MinimumOperationstoMakeArrayEqual.cpp
+++ Cplus/MinimumOperationstoMakeArrayEqual.cpp
@@ -3,9 +3,6 @@ class Solution
 public:
 	int minOperations(int n)
 	{
-		int res = 0;
-		for (int i = 1; i < n; i += 2)
-			res += n - i;
-		return res;
+		return (n / 2) * ((n + 1) / 2);
 	}
 };
