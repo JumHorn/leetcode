8593d4180af661c6fed68e91767cc8c1c6ecb278
JumHorn <JumHorn@gmail.com>
2020-11-09 13:42:18 +0800

    Dungeon Game beats 97.76%

--- C/DungeonGame.c
+++ C/DungeonGame.c
@@ -1,24 +1,18 @@
 #include <string.h>
 
 #define max(a, b) (((a) > (b)) ? (a) : (b))
+#define min(a, b) (((a) < (b)) ? (a) : (b))
 
 int calculateMinimumHP(int **dungeon, int dungeonSize, int *dungeonColSize)
 {
-	int m = dungeonSize, n = *dungeonColSize;
-	int dp[m + 1][n + 1];
-	memset(dp, 0, sizeof(dp));
-	for (int i = m - 2; i >= 0; --i)
-		dp[i][n] = -1000000;
-	for (int j = n - 2; j >= 0; --j)
-		dp[m][j] = -1000000;
-	for (int i = m - 1; i >= 0; --i)
+	int M = dungeonSize, N = *dungeonColSize;
+	int dp[M + 1][N + 1];
+	memset(dp, 1 << 6, sizeof(dp));
+	dp[M - 1][N] = dp[M][N - 1] = 0;
+	for (int i = M - 1; i >= 0; --i)
 	{
-		for (int j = n - 1; j >= 0; --j)
-		{
-			dp[i][j] = max(dp[i][j + 1], dp[i + 1][j]) + dungeon[i][j];
-			if (dp[i][j] > 0)
-				dp[i][j] = 0;
-		}
+		for (int j = N - 1; j >= 0; --j)
+			dp[i][j] = max(min(dp[i][j + 1], dp[i + 1][j]) - dungeon[i][j], 0);
 	}
-	return 1 - dp[0][0];
+	return dp[0][0] + 1;
 }
--- Cplus/DungeonGame.cpp
+++ Cplus/DungeonGame.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <climits>
 #include <vector>
 using namespace std;
 
@@ -8,20 +9,13 @@ public:
 	int calculateMinimumHP(vector<vector<int>> &dungeon)
 	{
 		int M = dungeon.size(), N = dungeon[0].size();
-		vector<vector<int>> dp(M + 1, vector<int>(N + 1));
-		for (int i = 0; i < M - 1; ++i)
-			dp[i][N] = INT_MAX;
-		for (int i = 0; i < N - 1; ++i)
-			dp[M][i] = INT_MAX;
-		dp[M - 1][N] = dp[M][N - 1] = 1;
+		vector<vector<int>> dp(M + 1, vector<int>(N + 1, INT_MAX));
+		dp[M - 1][N] = dp[M][N - 1] = 0;
 		for (int i = M - 1; i >= 0; --i)
 		{
 			for (int j = N - 1; j >= 0; --j)
-			{
-				int hp = min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j];
-				dp[i][j] = hp <= 0 ? 1 : hp;
-			}
+				dp[i][j] = max(min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j], 0);
 		}
-		return dp[0][0];
+		return dp[0][0] + 1;
 	}
 };
