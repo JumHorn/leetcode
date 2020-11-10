e7eb42ea64c8db3ceb89cf524f6d243f6c9fdd59
JumHorn <JumHorn@gmail.com>
2020-11-10 15:39:44 +0800

    Burst Balloons beats 88.24%

--- C/BurstBalloons.c
+++ C/BurstBalloons.c
@@ -9,20 +9,18 @@ dp[i][j] = max{nums[i]*nums[k]*nums[j]+dp[i][k]+dp[k][j]} (i<k<j)
 
 int maxCoins(int *nums, int numsSize)
 {
-	int n = numsSize + 2;
-	int dp[n][n], dup[n];
-	for (int i = 0; i < numsSize; ++i)
-		dup[i + 1] = nums[i];
-	dup[0] = dup[n - 1] = 1;
+	int N = numsSize + 2;
+	int dp[N][N], dup[N];
+	memcpy(&dup[1], nums, sizeof(int) * numsSize);
+	dup[0] = dup[N - 1] = 1;
 	memset(dp, 0, sizeof(dp));
-	for (int len = 2; len < n; len++)
+	for (int j = 2; j < N; ++j)
 	{
-		for (int i = 0; i < n - len; ++i)
+		for (int i = j - 1; i >= 0; --i)
 		{
-			int j = i + len;
 			for (int k = i + 1; k < j; k++)
 				dp[i][j] = max(dp[i][j], dup[i] * dup[k] * dup[j] + dp[i][k] + dp[k][j]);
 		}
 	}
-	return dp[0][n - 1];
+	return dp[0][N - 1];
 }
--- Cplus/BurstBalloons.cpp
+++ Cplus/BurstBalloons.cpp
@@ -9,17 +9,16 @@ public:
 	{
 		nums.insert(nums.begin(), 1);
 		nums.push_back(1);
-		int n = nums.size();
-		vector<vector<int>> dp(n, vector<int>(n));
-		for (int len = 2; len < n; len++)
+		int N = nums.size();
+		vector<vector<int>> dp(N, vector<int>(N));
+		for (int j = 2; j < N; ++j)
 		{
-			for (int i = 0; i < n - len; ++i)
+			for (int i = j - 1; i >= 0; --i)
 			{
-				int j = i + len;
-				for (int k = i + 1; k < j; k++)
+				for (int k = i + 1; k < j; ++k)
 					dp[i][j] = max(dp[i][j], nums[i] * nums[k] * nums[j] + dp[i][k] + dp[k][j]);
 			}
 		}
-		return dp[0][n - 1];
+		return dp[0][N - 1];
 	}
 };
