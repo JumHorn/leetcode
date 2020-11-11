548c55689c0e5cd80d05295a6f883a7750842455
JumHorn <JumHorn@gmail.com>
2020-11-11 08:37:10 +0800

    Arithmetic Slices II - Subsequence beats 84.62%

--- Cplus/ArithmeticSlicesII-Subsequence.cpp
+++ Cplus/ArithmeticSlicesII-Subsequence.cpp
@@ -1,3 +1,4 @@
+#include <climits>
 #include <unordered_map>
 #include <vector>
 using namespace std;
@@ -7,20 +8,19 @@ class Solution
 public:
 	int numberOfArithmeticSlices(vector<int> &A)
 	{
-		int n = A.size(), res = 0;
-		vector<unordered_map<long, int>> dp(n);
-		for (int i = 1; i < n; ++i)
+		int N = A.size(), res = 0;
+		vector<unordered_map<long, int>> dp(N);
+		for (int i = 1; i < N; ++i)
 		{
 			for (int j = 0; j < i; ++j)
 			{
-				long diff = (long)A[i] - A[j];
-				if (dp[j].find(diff) == dp[j].end())
-					++dp[i][diff];
-				else
-				{
-					dp[i][diff] += dp[j][diff] + 1;
-					res += dp[j][diff];
-				}
+				long diff = (long)A[i] - A[j], sum = 0;
+				if (diff < INT_MIN || diff > INT_MAX)
+					continue;
+				if (dp[j].find(diff) != dp[j].end())
+					sum = dp[j][diff];
+				dp[i][diff] += sum + 1;
+				res += sum;
 			}
 		}
 		return res;
