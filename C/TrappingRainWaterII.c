a218f754b3b82fcecf5ab14b2cb730f0a034d437
JumHorn <JumHorn@gmail.com>
2020-11-11 10:41:01 +0800

    Trapping Rain Water II beats 100%

--- C/TrappingRainWaterII.c
+++ C/TrappingRainWaterII.c
@@ -2,116 +2,108 @@
 
 #define max(a, b) (((a) > (b)) ? (a) : (b))
 
-//min heap function series
-void push_heap(int (*ptr)[2], int size)
+typedef struct pair
 {
-	if (size > 0)
+	int height;
+	int pos;
+} pair;
+
+//min heap function series
+void push_heap(pair *ptr, int size)
+{
+	if (size <= 1)
+		return;
+	pair val = ptr[size - 1];
+	int hole = size - 1;
+	for (int i = (hole - 1) >> 1; hole > 0 && val.height<ptr[i].height; i = (hole - 1)>> 1)
 	{
-		int val = ptr[size - 1][0], val1 = ptr[size - 1][1], hole = size - 1;
-		for (int i = (hole - 1) >> 1; hole > 0 && val<ptr[i][0]; i = (hole - 1)>> 1)
-		{
-			ptr[hole][0] = ptr[i][0];
-			ptr[hole][1] = ptr[i][1];
-			hole = i;
-		}
-		ptr[hole][0] = val;
-		ptr[hole][1] = val1;
+		ptr[hole] = ptr[i];
+		hole = i;
 	}
+	ptr[hole] = val;
 }
 
-//for internal usage
-void _adjust_heap(int (*ptr)[2], int size, int hole, int val, int val1)
+void pop_heap(pair *ptr, int size)
 {
-	int non_leaf = (size - 1) >> 1, i = hole;
+	if (size <= 0)
+		return;
+	pair val = *ptr;
+	int non_leaf = (size - 1) >> 1, hole = 0, i = 0;
 	while (i < non_leaf)
 	{
 		i = 2 * i + 2;
-		if (ptr[i][0] > ptr[i - 1][0])
+		if (ptr[i - 1].height < ptr[i].height)
 			--i;
-		ptr[hole][0] = ptr[i][0];
-		ptr[hole][1] = ptr[i][1];
+		ptr[hole] = ptr[i];
 		hole = i;
 	}
 	if (i == non_leaf && size % 2 == 0)
 	{
-		ptr[hole][0] = ptr[size - 1][0];
-		ptr[hole][1] = ptr[size - 1][1];
+		ptr[hole] = ptr[size - 1];
 		hole = size - 1;
 	}
-	ptr[hole][0] = val;
-	ptr[hole][1] = val1;
+	ptr[hole] = ptr[size - 1];
 	push_heap(ptr, hole + 1);
+	ptr[size - 1] = val;
 }
 
-void make_heap(int (*ptr)[2], int size)
+void make_heap(pair *ptr, int size)
 {
-	for (int hole = (size - 1) >> 1; hole >= 0; --hole)
-		_adjust_heap(ptr, size, hole, ptr[hole][0], ptr[hole][1]);
+	for (int i = 1; i < size; ++i)
+		push_heap(ptr, i + 1);
 }
-
-void pop_heap(int (*ptr)[2], int size)
-{
-	if (size > 0)
-	{
-		int val = ptr[0][0], val1 = ptr[0][1];
-		_adjust_heap(ptr, size, 0, ptr[size - 1][0], ptr[size - 1][1]);
-		ptr[size - 1][0] = val;
-		ptr[size - 1][1] = val1;
-	}
-}
+/********end of min heap********/
 
 int trapRainWater(int **heightMap, int heightMapSize, int *heightMapColSize)
 {
-	int m = heightMapSize, n = *heightMapColSize;
-	if (m <= 2 || n <= 2)
+	int M = heightMapSize, N = *heightMapColSize;
+	if (M <= 2 || N <= 2)
 		return 0;
-	int size = m * n, priorityqueue[size][2], top = -1;
-	int seen[m][n];
+	pair heap[M * N];
+	int seen[M][N], heapSize = 0;
 	memset(seen, 0, sizeof(seen));
-	for (int i = 0; i < m; ++i)
+	for (int i = 0; i < M; ++i)
 	{
-		priorityqueue[++top][0] = heightMap[i][0];
-		priorityqueue[top][1] = i * n + 0;
-		push_heap(priorityqueue, top + 1);
+		heap[heapSize].height = heightMap[i][0];
+		heap[heapSize].pos = i * N + 0;
+		push_heap(heap, ++heapSize);
 
-		priorityqueue[++top][0] = heightMap[i][n - 1];
-		priorityqueue[top][1] = i * n + n - 1;
-		push_heap(priorityqueue, top + 1);
+		heap[heapSize].height = heightMap[i][N - 1];
+		heap[heapSize].pos = i * N + N - 1;
+		push_heap(heap, ++heapSize);
 
-		seen[i][0] = seen[i][n - 1] = 1;
+		seen[i][0] = seen[i][N - 1] = 1;
 	}
-	for (int j = 1; j < n - 1; ++j)
+	for (int j = 1; j < N - 1; ++j)
 	{
-		priorityqueue[++top][0] = heightMap[0][j];
-		priorityqueue[top][1] = 0 * n + j;
-		push_heap(priorityqueue, top + 1);
+		heap[heapSize].height = heightMap[0][j];
+		heap[heapSize].pos = 0 * N + j;
+		push_heap(heap, ++heapSize);
 
-		priorityqueue[++top][0] = heightMap[m - 1][j];
-		priorityqueue[top][1] = (m - 1) * n + j;
-		push_heap(priorityqueue, top + 1);
+		heap[heapSize].height = heightMap[M - 1][j];
+		heap[heapSize].pos = (M - 1) * N + j;
+		push_heap(heap, ++heapSize);
 
-		seen[0][j] = seen[m - 1][j] = 1;
+		seen[0][j] = seen[M - 1][j] = 1;
 	}
-	int res = 0, maxheight = 0;
-	int path[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-	while (top != -1)
+	int res = 0, curMinHeight = 0;
+	//board dfs direction
+	int path[5] = {-1, 0, 1, 0, -1};
+	while (heapSize != 0)
 	{
-		pop_heap(priorityqueue, top + 1);
-		int height = priorityqueue[top][0];
-		int position = priorityqueue[top][1];
-		--top;
-		maxheight = max(maxheight, height);
+		pair top = heap[0];
+		pop_heap(heap, heapSize--);
+		curMinHeight = max(curMinHeight, top.height);
 		for (int i = 0; i < 4; ++i)
 		{
-			int x = position / n + path[i][0], y = position % n + path[i][1];
-			if (x < 0 || x >= m || y < 0 || y >= n || seen[x][y] == 1)
+			int x = top.pos / N + path[i], y = top.pos % N + path[i + 1];
+			if (x < 0 || x >= M || y < 0 || y >= N || seen[x][y] == 1)
 				continue;
 			seen[x][y] = 1;
-			if (heightMap[x][y] < maxheight)
-				res += maxheight - heightMap[x][y];
-			priorityqueue[++top][0] = heightMap[x][y];
-			priorityqueue[top][1] = x * n + y;
-			push_heap(priorityqueue, top + 1);
+			res += max(curMinHeight - heightMap[x][y], 0);
+			heap[heapSize].height = heightMap[x][y];
+			heap[heapSize].pos = x * N + y;
+			push_heap(heap, ++heapSize);
 		}
 	}
 	return res;
--- Cplus/TrappingRainWaterII.cpp
+++ Cplus/TrappingRainWaterII.cpp
@@ -7,39 +7,39 @@ class Solution
 public:
 	int trapRainWater(vector<vector<int>> &heightMap)
 	{
-		int m = heightMap.size(), n = heightMap[0].size();
-		if (m <= 2 || n <= 2)
+		int M = heightMap.size(), N = heightMap[0].size();
+		if (M <= 2 || N <= 2)
 			return 0;
 		priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
-		vector<vector<int>> seen(m, vector<int>(n));
-		for (int i = 0; i < m; ++i)
+		vector<vector<int>> seen(M, vector<int>(N));
+		for (int i = 0; i < M; ++i)
 		{
-			q.push({heightMap[i][0], i * n + 0});
-			q.push({heightMap[i][n - 1], i * n + n - 1});
-			seen[i][0] = seen[i][n - 1] = 1;
+			q.push({heightMap[i][0], i * N + 0});
+			q.push({heightMap[i][N - 1], i * N + N - 1});
+			seen[i][0] = seen[i][N - 1] = 1;
 		}
-		for (int j = 1; j < n - 1; ++j)
+		for (int j = 1; j < N - 1; ++j)
 		{
-			q.push({heightMap[0][j], 0 * n + j});
-			q.push({heightMap[m - 1][j], (m - 1) * n + j});
-			seen[0][j] = seen[m - 1][j] = 1;
+			q.push({heightMap[0][j], 0 * N + j});
+			q.push({heightMap[M - 1][j], (M - 1) * N + j});
+			seen[0][j] = seen[M - 1][j] = 1;
 		}
-		int res = 0, maxheight = 0;
-		int path[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+		int res = 0, curMinHeight = 0;
+		//board dfs direction
+		int path[5] = {-1, 0, 1, 0, -1};
 		while (!q.empty())
 		{
 			auto top = q.top();
 			q.pop();
-			maxheight = max(maxheight, top.first);
+			curMinHeight = max(curMinHeight, top.first);
 			for (int i = 0; i < 4; ++i)
 			{
-				int x = top.second / n + path[i][0], y = top.second % n + path[i][1];
-				if (x < 0 || x >= m || y < 0 || y >= n || seen[x][y] == 1)
+				int x = top.second / N + path[i], y = top.second % N + path[i + 1];
+				if (x < 0 || x >= M || y < 0 || y >= N || seen[x][y] == 1)
 					continue;
 				seen[x][y] = 1;
-				if (heightMap[x][y] < maxheight)
-					res += maxheight - heightMap[x][y];
-				q.push({heightMap[x][y], x * n + y});
+				res += max(curMinHeight - heightMap[x][y], 0);
+				q.push({heightMap[x][y], x * N + y});
 			}
 		}
 		return res;
