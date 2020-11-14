16a26f1b7fc04f8e93434c9a1bd34c5d24dbeee8
JumHorn <JumHorn@gmail.com>
2020-11-14 15:06:25 +0800

    Falling Squares beats 99.58%

--- Cplus/FallingSquares.cpp
+++ Cplus/FallingSquares.cpp
@@ -3,45 +3,62 @@
 #include <vector>
 using namespace std;
 
+class Range
+{
+public:
+	Range()
+	{
+	}
+
+	int addRange(int left, int right, int height)
+	{
+		auto iter = range.lower_bound({left, right});
+		auto prev = iter;
+		int res = 0;
+		if (prev != range.begin())
+		{
+			--prev;
+			if (prev->first.second > left)
+			{
+				res = max(res, prev->second);
+				range.insert({{prev->first.first, left}, prev->second});
+				if (prev->first.second > right)
+					range.insert({{right, prev->first.second}, prev->second});
+				range.erase(prev);
+			}
+		}
+		while (iter != range.end() && iter->first.second <= right)
+		{
+			res = max(res, iter->second);
+			iter = range.erase(iter);
+		}
+		if (iter != range.end() && iter->first.first < right)
+		{
+			res = max(res, iter->second);
+			range.insert({{right, iter->first.second}, iter->second});
+			range.erase(iter);
+		}
+		res += height;
+		range.insert({{left, right}, res});
+		return res;
+	}
+
+private:
+	map<pair<int, int>, int> range; //{{interval},height}
+};
+
 class Solution
 {
 public:
 	vector<int> fallingSquares(vector<vector<int>> &positions)
 	{
-		if (positions.empty())
-			return {};
-		int n = positions.size();
-		vector<int> res(n);
-		map<pair<int, int>, int> areamap;
-		res[0] = positions[0][1];
-		areamap[{positions[0][0], positions[0][0] + positions[0][1]}] = positions[0][1];
-		for (int i = 1; i < n; ++i)
+		Range range;
+		vector<int> res;
+		int maxheight = 0;
+		for (auto &pos : positions)
 		{
-			int maxheight = 0;
-			auto iter = areamap.lower_bound({positions[i][0], positions[i][0] + positions[i][1]});
-			if (iter != areamap.begin())
-			{
-				--iter;
-				if (iter->first.second > positions[i][0])
-				{
-					maxheight = max(maxheight, iter->second);
-					areamap[{iter->first.first, positions[i][0]}] = iter->second;
-					if (iter->first.second > positions[i][0] + positions[i][1])
-						areamap[{positions[i][0] + positions[i][1], iter->first.second}] = iter->second;
-					iter = areamap.erase(iter);
-				}
-				else
-					++iter;
-			}
-			while (iter != areamap.end() && positions[i][0] + positions[i][1] > iter->first.first)
-			{
-				maxheight = max(maxheight, iter->second);
-				if (positions[i][0] + positions[i][1] <= iter->first.second)
-					areamap[{positions[i][0] + positions[i][1], iter->first.second}] = iter->second;
-				iter = areamap.erase(iter);
-			}
-			res[i] = max(maxheight + positions[i][1], res[i - 1]);
-			areamap[{positions[i][0], positions[i][0] + positions[i][1]}] = maxheight + positions[i][1];
+			maxheight = max(maxheight, range.addRange(pos[0], pos[0] + pos[1], pos[1]));
+			res.push_back(maxheight);
 		}
 		return res;
 	}
