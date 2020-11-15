4508744784a204469aa13df1fbd7a0dd34d76056
JumHorn <JumHorn@gmail.com>
2020-11-15 15:48:20 +0800

    time limited exceeded

--- Cplus/MaximizeGridHappiness.cpp
+++ Cplus/MaximizeGridHappiness.cpp
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <vector>
 using namespace std;
 
@@ -6,63 +7,64 @@ class Solution
 public:
 	int getMaxGridHappiness(int m, int n, int introvertsCount, int extrovertsCount)
 	{
-		vector<vector<int>> grid(m, vector<int>(n));
-		return dfs(grid, 0, 0, introvertsCount, extrovertsCount, 0);
+		// row mask for every 2 bits as a group 00 for no people 01 for introverts 10 for extroverts
+		// int dp[1 << (2 * n)][m + 1][introvertsCount][extrovertsCount];
+		int dp[1 << 10][6][7][7];
+		memset(dp, -1, sizeof(dp));
+		return memdp(m, n, 0, 0, introvertsCount, extrovertsCount, dp);
 	}
 
-	int dfs(vector<vector<int>> &grid, int row, int col, int in, int ex, int happy)
+	//row mask for every 2 bits as a group 00 for no people 01 for introverts 10 for extroverts
+	int memdp(int M, int N, int mask, int row, int intro, int extro, int (*dp)[6][7][7])
 	{
-		int M = grid.size(), N = grid[0].size();
-		if (row == M - 1 && col == N)
-			return happy;
-		if (col == N)
+		if (row >= M)
+			return 0;
+		if (dp[mask][row][intro][extro] != -1)
+			return dp[mask][row][intro][extro];
+		int res = 0;
+		for (int i = 0; i < 1u << (2 * N); ++i)
 		{
-			col = 0;
-			++row;
+			int in = 0, ex = 0, pre = 0, happy = 0;
+			for (int j = 0; j < N; ++j)
+			{
+				if (((i >> (2 * j)) & 0b11) == 0b01) //introvert
+				{
+					++in;
+					happy += 120;
+					//inner row
+					if (pre == 0b01)
+						happy += -30 - 30;
+					else if (pre == 0b10)
+						happy += 20 - 30;
+					pre = 0b01;
+					// pre row
+					if (((mask >> (2 * j)) & 0b11) == 0b01)
+						happy += -30 - 30;
+					else if (((mask >> (2 * j)) & 0b11) == 0b10)
+						happy += 20 - 30;
+				}
+				else if (((i >> (2 * j)) & 0b11) == 0b10) //extrovert
+				{
+					++ex;
+					happy += 40;
+					//inner row
+					if (pre == 0b01)
+						happy += 20 - 30;
+					else if (pre == 0b10)
+						happy += 20 + 20;
+					pre = 0b10;
+					// pre row
+					if (((mask >> (2 * j)) & 0b11) == 0b01)
+						happy += 20 - 30;
+					else if (((mask >> (2 * j)) & 0b11) == 0b10)
+						happy += 20 + 20;
+				}
+				else //no people
+					pre = 0;
+			}
+			if (in <= intro && ex <= extro)
+				res = max(res, happy + memdp(M, N, i, row + 1, intro - in, extro - ex, dp));
 		}
-		int res = dfs(grid, row, col + 1, in, ex, happy);
-		if (in > 0)
-		{
-			grid[row][col] = 1;
-			int h = happy + 120;
-			if (row > 0 && grid[row - 1][col] != 0)
-			{
-				if (grid[row - 1][col] == 1)
-					h += -30 - 30;
-				else
-					h += 20 - 30;
-			}
-			if (col > 0 && grid[row][col - 1] != 0)
-			{
-				if (grid[row][col - 1] == 1)
-					h += -30 - 30;
-				else
-					h += 20 - 30;
-			}
-			res = max(res, dfs(grid, row, col + 1, in - 1, ex, h));
-			grid[row][col] = 0;
-		}
-		if (ex > 0)
-		{
-			grid[row][col] = 2;
-			int h = happy + 40;
-			if (row > 0 && grid[row - 1][col] != 0)
-			{
-				if (grid[row - 1][col] == 1)
-					h += 20 - 30;
-				else
-					h += 20 + 20;
-			}
-			if (col > 0 && grid[row][col - 1] != 0)
-			{
-				if (grid[row][col - 1] == 1)
-					h += 20 - 30;
-				else
-					h += 20 + 20;
-			}
-			res = max(res, dfs(grid, row, col + 1, in, ex - 1, h));
-			grid[row][col] = 0;
-		}
-		return res;
+		return dp[mask][row][intro][extro] = res;
 	}
 };
