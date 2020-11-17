7c3975e3355352abd53a82bdf633b0eae8457837
JumHorn <JumHorn@gmail.com>
2020-11-17 10:05:41 +0800

    Sum of Distances in Tree beats 25.00%

--- C/SumofDistancesinTree.c
+++ C/SumofDistancesinTree.c
@@ -1,28 +1,43 @@
+#include <stdlib.h>
 #include <string.h>
 
-void preorder(int **graph, int N, int *nodecount, int *dis, int node)
+// create graph
+typedef struct GraphNode
 {
-	for (int i = 0; i < N; ++i)
+	int node;
+	struct GraphNode *next;
+} GraphNode;
+
+GraphNode *createNode(int val)
+{
+	GraphNode *node = (GraphNode *)malloc(sizeof(GraphNode));
+	node->node = val;
+	node->next = NULL;
+	return node;
+}
+
+void preorder(GraphNode **graph, int N, int *nodecount, int from, int at, int *dis)
+{
+	for (GraphNode *to = graph[at]; to; to = to->next)
 	{
-		if (graph[node][i] == 1)
+		if (from != to->node) //don't go back to parent
 		{
-			dis[i] = dis[node] + N - 2 * nodecount[i];
-			preorder(graph, N, nodecount, dis, i);
+			dis[to->node] = dis[at] + N - 2 * nodecount[to->node];
+			preorder(graph, N, nodecount, at, to->node, dis);
 		}
 	}
 }
 
-int postorder(int **graph, int N, int *nodecount, int node)
+int postorder(GraphNode **graph, int from, int at, int *nodecount)
 {
 	int res = 0;
-	for (int i = 0; i < N; ++i)
+	for (GraphNode *to = graph[at]; to; to = to->next)
 	{
-		if (graph[node][i] == 1)
+		if (from != to->node) //don't go back to parent
 		{
-			graph[i][node] = 0;
-			res += postorder(graph, N, nodecount, i);
-			nodecount[node] += nodecount[i];
-			res += nodecount[i];
+			res += postorder(graph, at, to->node, nodecount);
+			nodecount[at] += nodecount[to->node];
+			res += nodecount[to->node];
 		}
 	}
 	return res;
@@ -33,24 +48,24 @@ int postorder(int **graph, int N, int *nodecount, int node)
  */
 int *sumOfDistancesInTree(int N, int **edges, int edgesSize, int *edgesColSize, int *returnSize)
 {
-	int **graph = (int **)malloc(sizeof(int *) * N);
-	for (int i = 0; i < N; ++i)
-	{
-		graph[i] = (int *)malloc(sizeof(int) * N);
-		memset(graph[i], 0, sizeof(int) * N);
-	}
-	*returnSize = N;
+	GraphNode *graph[N];
+	memset(graph, 0, sizeof(graph));
 	for (int i = 0; i < edgesSize; ++i)
 	{
-		graph[edges[i][0]][edges[i][1]] = 1;
-		graph[edges[i][1]][edges[i][0]] = 1;
+		GraphNode *node = createNode(edges[i][1]);
+		node->next = graph[edges[i][0]];
+		graph[edges[i][0]] = node;
+		node = createNode(edges[i][0]);
+		node->next = graph[edges[i][1]];
+		graph[edges[i][1]] = node;
 	}
 
 	int nodecount[N];
 	for (int i = 0; i < N; ++i)
 		nodecount[i] = 1;
-	int *res = (int *)malloc(sizeof(int) * N);
-	res[0] = postorder(graph, N, nodecount, 0);
-	preorder(graph, N, nodecount, res, 0);
+	*returnSize = N;
+	int *res = (int *)malloc(sizeof(int) * (*returnSize));
+	res[0] = postorder(graph, -1, 0, nodecount);
+	preorder(graph, N, nodecount, -1, 0, res);
 	return res;
 }
