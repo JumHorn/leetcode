b72643cd3f8aa9465d4f04730d83aafe98990279
JumHorn <JumHorn@gmail.com>
2020-11-28 14:25:51 +0800

    Paint House III beats 100%

--- C/PaintHouseIII.c
+++ C/PaintHouseIII.c
@@ -1,92 +1,45 @@
-#include <limits.h>
 #include <string.h>
 
 #define min(a, b) (((a) < (b)) ? (a) : (b))
 
 int minCost(int *houses, int housesSize, int **cost, int costSize, int *costColSize, int m, int n, int target)
 {
-	int dp[m][n][target + 1];
-	memset(dp, -1, sizeof(dp));
+	//INF == 1077952576
+	const int BASE = 1u << 6, INF = (BASE | (BASE << 8) | (BASE << 16) | (BASE << 24));
+	int dp[m][n][target + 1]; //house,color,neighboor
+	memset(dp, BASE, sizeof(dp));
 	//for the first house
-	if (houses[0] != 0)
-		dp[0][houses[0] - 1][1] = 0;
-	else
+	for (int c = 0; c < n; ++c)
 	{
-		for (int i = 0; i < n; ++i)
-			dp[0][i][1] = cost[0][i];
+		if (houses[0] == c + 1)
+			dp[0][c][1] = 0;
+		else if (houses[0] == 0)
+			dp[0][c][1] = cost[0][c];
 	}
 
 	for (int i = 1; i < m; ++i) //house
 	{
-		if (houses[i] != 0)
+		for (int c = 0; c < n; ++c) //color
 		{
-			int color = houses[i] - 1;
-			for (int c = 0; c < n; ++c) //color
+			for (int k = 0; k < min(target, i + 1); ++k) //neighborhood
 			{
-				for (int k = 0; k < target; ++k) //neighborhood
+				if (houses[i] != 0 && c != houses[i] - 1)
+					continue;
+				int same_neighbor = dp[i - 1][c][k + 1];
+				int diff_neighbor = INF;
+				for (int color = 0; color < n; ++color)
 				{
-					if (c != color)
-					{
-						if (dp[i - 1][c][k] != -1)
-						{
-							if (dp[i][color][k + 1] != -1)
-								dp[i][color][k + 1] = min(dp[i][color][k + 1], dp[i - 1][c][k]);
-							else
-								dp[i][color][k + 1] = dp[i - 1][c][k];
-						}
-					}
-					else
-					{
-						if (dp[i - 1][c][k + 1] != -1)
-						{
-							if (dp[i][color][k + 1] != -1)
-								dp[i][color][k + 1] = min(dp[i][color][k + 1], dp[i - 1][c][k + 1]);
-							else
-								dp[i][color][k + 1] = dp[i - 1][c][k + 1];
-						}
-					}
+					if (color != c)
+						diff_neighbor = min(diff_neighbor, dp[i - 1][color][k]);
 				}
-			}
-		}
-		else
-		{
-			for (int j = 0; j < n; ++j) //color
-			{
-				for (int c = 0; c < n; ++c)
-				{
-					for (int k = 0; k < target; ++k) //neighborhood
-					{
-						if (c != j)
-						{
-							if (dp[i - 1][c][k] != -1)
-							{
-								if (dp[i][j][k + 1] != -1)
-									dp[i][j][k + 1] = min(dp[i][j][k + 1], dp[i - 1][c][k] + cost[i][j]);
-								else
-									dp[i][j][k + 1] = dp[i - 1][c][k] + cost[i][j];
-							}
-						}
-						else
-						{
-							if (dp[i - 1][c][k + 1] != -1)
-							{
-								if (dp[i][j][k + 1] != -1)
-									dp[i][j][k + 1] = min(dp[i][j][k + 1], dp[i - 1][c][k + 1] + cost[i][j]);
-								else
-									dp[i][j][k + 1] = dp[i - 1][c][k + 1] + cost[i][j];
-							}
-						}
-					}
-				}
+				int paint_cost = cost[i][c] * (houses[i] == 0 ? 1 : 0);
+				dp[i][c][k + 1] = min(same_neighbor, diff_neighbor) + paint_cost;
 			}
 		}
 	}
 
-	int res = INT_MAX;
-	for (int i = 0; i < n; ++i)
-	{
-		if (dp[m - 1][i][target] != -1)
-			res = min(res, dp[m - 1][i][target]);
-	}
-	return res == INT_MAX ? -1 : res;
+	int res = INF;
+	for (int c = 0; c < n; ++c)
+		res = min(res, dp[m - 1][c][target]);
+	return res >= INF ? -1 : res;
 }
