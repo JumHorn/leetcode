fb88f501b20c10fa3a43db6be24517d7926c7664
JumHorn <JumHorn@gmail.com>
2020-12-03 12:22:38 +0800

    Group Anagrams beats 75.71%

--- C/GroupAnagrams.c
+++ C/GroupAnagrams.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-//integer cmp function
+//char cmp function
 int cmp(const void *lhs, const void *rhs)
 {
-	if (*(int *)lhs == *(int *)rhs)
+	if (*(char *)lhs == *(char *)rhs)
 		return 0;
-	return *(int *)lhs < *(int *)rhs ? -1 : 1;
+	return *(char *)lhs < *(char *)rhs ? -1 : 1;
 }
 
 typedef struct Anagrams
@@ -27,40 +27,29 @@ int anagramsCmp(const void *lhs, const void *rhs)
  */
 char ***groupAnagrams(char **strs, int strsSize, int *returnSize, int **returnColumnSizes)
 {
-	static char staticRes[10001][1001][101];
-	static int staticResColSize[10001];
-	memset(staticResColSize, 0, sizeof(staticResColSize));
 	Anagrams anagrams[strsSize];
 	for (int i = 0; i < strsSize; ++i)
 	{
 		int n = strlen(strs[i]);
 		anagrams[i].index = i;
-		anagrams[i].str = (char *)malloc(sizeof(char) * (n + 1));
-		strcpy(anagrams[i].str, strs[i]);
+		anagrams[i].str = strdup(strs[i]);
 		qsort(anagrams[i].str, n, sizeof(char), cmp);
 	}
 	qsort(anagrams, strsSize, sizeof(Anagrams), anagramsCmp);
-	int size = -1;
-	for (int i = 0; i < strsSize; ++i)
-	{
-		if (i == 0 || strcmp(anagrams[i].str, anagrams[i - 1].str) != 0)
-			strcpy(staticRes[++size][staticResColSize[size]++], strs[anagrams[i].index]);
-		else
-			strcpy(staticRes[size][staticResColSize[size]++], strs[anagrams[i].index]);
-	}
+	char ***res = (char ***)malloc(sizeof(char **) * strsSize);
+	*returnColumnSizes = (int *)malloc(sizeof(int) * strsSize);
 
-	*returnSize = size + 1;
-	*returnColumnSizes = (int *)malloc(sizeof(int) * (*returnSize));
-	memcpy(*returnColumnSizes, staticResColSize, sizeof(int) * (*returnSize));
-	char ***res = (char ***)malloc(sizeof(char **) * (*returnSize));
-	for (int i = 0; i < *returnSize; ++i)
+	*returnSize = 0;
+	for (int i = 1, j = 0; i <= strsSize; ++i)
 	{
-		res[i] = (char **)malloc(sizeof(char *) * (*returnColumnSizes)[i]);
-		for (int j = 0; j < (*returnColumnSizes)[i]; ++j)
+		if (i == strsSize || strcmp(anagrams[i].str, anagrams[j].str) != 0)
 		{
-			int n = strlen(staticRes[i][j]);
-			res[i][j] = (char *)malloc(sizeof(char) * (n + 1));
-			strcpy(res[i][j], staticRes[i][j]);
+			(*returnColumnSizes)[*returnSize] = i - j;
+			res[*returnSize] = (char **)malloc(sizeof(char *) * (*returnColumnSizes)[*returnSize]);
+			for (int k = 0; k < i - j; ++k)
+				res[*returnSize][k] = strdup(strs[anagrams[j + k].index]);
+			++*returnSize;
+			j = i;
 		}
 	}
 	return res;
