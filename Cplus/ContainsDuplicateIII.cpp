a40fcd0fad42d943c2a33d2f65b8e8a9c46d0cdb
JumHorn <JumHorn@gmail.com>
2020-12-11 14:21:06 +0800

    formatted

--- Cplus/ContainsDuplicateIII.cpp
+++ Cplus/ContainsDuplicateIII.cpp
@@ -20,7 +20,9 @@ public:
 		for (int i = 0; i < N; ++i)
 		{
 			long bucket = ((long)nums[i] - INT_MIN) / ((long)t + 1); //map nums[i] to bucket
-			if (m.find(bucket) != m.end() || (m.find(bucket - 1) != m.end() && nums[i] - m[bucket - 1] <= t) || (m.find(bucket + 1) != m.end() && m[bucket + 1] - nums[i] <= t))
+			if (m.find(bucket) != m.end() ||
+				(m.find(bucket - 1) != m.end() && nums[i] - m[bucket - 1] <= t) ||
+				(m.find(bucket + 1) != m.end() && m[bucket + 1] - nums[i] <= t))
 				return true;
 			if (m.size() >= k)
 				m.erase(((long)nums[i - k] - INT_MIN) / ((long)t + 1));
