2c93b247bacaee5994ab830061a2f278dc2c0909
JumHorn <JumHorn@gmail.com>
2020-12-11 14:03:09 +0800

    real slide window

--- C/MinimumSizeSubarraySum.c
+++ C/MinimumSizeSubarraySum.c
@@ -1,34 +1,19 @@
 
-#define min(a, b) (((a) < (b)) ? (a) : (b))
-
-int binarySeach(int* arr, int lo, int hi, int val)
+int minSubArrayLen(int s, int *nums, int numsSize)
 {
-	while (lo < hi)
-	{
-		int mi = (hi - lo) / 2 + lo;
-		if (arr[mi] <= val)
-			lo = mi + 1;
-		else
-			hi = mi;
-	}
-	return lo - 1;
-}
-
-int minSubArrayLen(int s, int* nums, int numsSize)
-{
-	int res = numsSize;
-	for (int i = 1; i < numsSize; ++i)
-		nums[i] += nums[i - 1];
-	if (numsSize == 0 || nums[numsSize - 1] < s)
+	int sum = 0, window = 0, i = 0, j = 0;
+	for (; i < numsSize && window < s; ++i)
+		window += nums[i];
+	if (window < s)
 		return 0;
-	for (int i = 0; i < numsSize; ++i)
+	while (window >= s)
+		window -= nums[j++];
+	for (; i < numsSize; ++i)
 	{
-		if (nums[i] < s)
-			continue;
-		int preindex = -1;
-		if (nums[i] - s >= nums[0])
-			preindex = binarySeach(nums, 0, i, nums[i] - s);
-		res = min(res, i - preindex);
+		window += nums[i];
+		window -= nums[j++];
+		while (window >= s)
+			window -= nums[j++];
 	}
-	return res;
+	return i - j + 1;
 }
