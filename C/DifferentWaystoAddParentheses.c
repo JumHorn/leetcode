75fe10418f87bd6baac839af95cb23750a5fcef6
JumHorn <JumHorn@gmail.com>
2020-12-12 09:05:21 +0800

    Different Ways to Add Parentheses beats 75.00%

--- C/DifferentWaystoAddParentheses.c
+++ C/DifferentWaystoAddParentheses.c
@@ -1,41 +1,55 @@
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct vector
+{
+	int data[2000];
+	int size;
+} vector;
+
+vector compute(char *input)
+{
+	vector res = {.size = 0};
+	for (int k = 0; input[k]; ++k)
+	{
+		if (input[k] == '+' || input[k] == '-' || input[k] == '*')
+		{
+			char old = input[k];
+			input[k] = '\0';
+			vector left = compute(input);
+			vector right = compute(input + k + 1);
+			input[k] = old;
+			for (int i = 0; i < left.size; ++i)
+			{
+				for (int j = 0; j < right.size; ++j)
+				{
+					if (input[k] == '+')
+						res.data[res.size] = left.data[i] + right.data[j];
+					else if (input[k] == '-')
+						res.data[res.size] = left.data[i] - right.data[j];
+					else
+						res.data[res.size] = left.data[i] * right.data[j];
+					++res.size;
+				}
+			}
+		}
+	}
+	if (res.size == 0)
+	{
+		res.size = 1;
+		res.data[0] = atoi(input);
+	}
+	return res;
+}
 
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int *diffWaysToCompute(char *input, int *returnSize)
 {
-	int *res = (int *)malloc(sizeof(int) * 10000);
-	*returnSize = 0;
-	for (int k = 0; input[k]; ++k)
-	{
-		if (input[k] == '+' || input[k] == '-' || input[k] == '*')
-		{
-			int leftsize = 0, rightsize = 0;
-			char old = input[k];
-			input[k] = '\0';
-			int *left = diffWaysToCompute(input, &leftsize);
-			int *right = diffWaysToCompute(input + k + 1, &rightsize);
-			input[k] = old;
-			for (int i = 0; i < leftsize; ++i)
-			{
-				for (int j = 0; j < rightsize; ++j)
-				{
-					if (input[k] == '+')
-						res[*returnSize] = left[i] + right[j];
-					else if (input[k] == '-')
-						res[*returnSize] = left[i] - right[j];
-					else
-						res[*returnSize] = left[i] * right[j];
-					++*returnSize;
-				}
-			}
-		}
-	}
-	if (*returnSize == 0)
-	{
-		*returnSize = 1;
-		res[0] = atoi(input);
-	}
+	vector val = compute(input);
+	*returnSize = val.size;
+	int *res = (int *)malloc(sizeof(int) * (*returnSize));
+	memcpy(res, val.data, sizeof(int) * (*returnSize));
 	return res;
 }
