1127c50368a89c3f148c16e6f18f8a68f0cbea41
JumHorn <JumHorn@gmail.com>
2020-12-15 10:08:07 +0800

    improved

--- C/IncreasingTripletSubsequence.c
+++ C/IncreasingTripletSubsequence.c
@@ -1,25 +1,19 @@
-#include <string.h>
+#include <limits.h>
+#include <stdbool.h>
 
-bool increasingTriplet(int* nums, int numsSize)
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+
+bool increasingTriplet(int *nums, int numsSize)
 {
-	int min, mid, i;
-	for (i = 1; i < numsSize; ++i)
+	int one = INT_MAX, two = INT_MAX;
+	for (int i = 0; i < numsSize; ++i)
 	{
-		if (nums[i - 1] < nums[i])
-			break;
-	}
-	if (i >= numsSize)
-		return false;
-	min = nums[i - 1];
-	mid = nums[i];
-	for (++i; i < numsSize; ++i)
-	{
-		if (nums[i] > mid)
+		if (nums[i] > two)
 			return true;
-		if (nums[i] < min)
-			min = nums[i];
-		else if (nums[i] > min && nums[i] < mid)
-			mid = nums[i];
+		if (nums[i] > one)
+			two = nums[i];
+		else
+			one = min(one, nums[i]);
 	}
 	return false;
 }
