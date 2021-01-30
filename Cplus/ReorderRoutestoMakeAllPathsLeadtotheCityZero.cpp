7e70eba05e040a966e28ba44c4509efc8b1c94b9
JumHorn <JumHorn@gmail.com>
2021-01-30 09:13:13 +0800

    Reorder Routes to Make All Paths Lead to the City Zero beats 92.59%

--- Cplus/ReorderRoutestoMakeAllPathsLeadtotheCityZero.cpp
+++ Cplus/ReorderRoutestoMakeAllPathsLeadtotheCityZero.cpp
@@ -7,24 +7,26 @@ class Solution
 public:
 	int minReorder(int n, vector<vector<int>> &connections)
 	{
-		vector<unordered_map<int, int>> graph(n);
+		vector<vector<pair<int, int>>> graph(n);
 		for (auto &conn : connections)
 		{
-			graph[conn[0]][conn[1]] = 1; //real edge
-			graph[conn[1]][conn[0]] = 0; //virtual edge
+			graph[conn[0]].push_back({conn[1], 1}); //real edge
+			graph[conn[1]].push_back({conn[0], 0}); //virtual edge
 		}
-		return dfs(graph, 0);
+		return dfs(graph, 0, 0);
 	}
 
-	int dfs(vector<unordered_map<int, int>> &graph, int at)
+	int dfs(vector<vector<pair<int, int>>> &graph, int from, int at)
 	{
 		int res = 0;
 		for (auto &to : graph[at])
 		{
-			if (to.second == 1)
-				++res;
-			graph[to.first].erase(at);
-			res += dfs(graph, to.first);
+			if (from != to.first)
+			{
+				if (to.second == 1)
+					++res;
+				res += dfs(graph, at, to.first);
+			}
 		}
 		return res;
 	}
