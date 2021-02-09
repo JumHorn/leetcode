363c209fdd6d6927f5901a58fe799ea9740c900c
JumHorn <JumHorn@gmail.com>
2021-02-09 20:19:58 +0800

    improved

--- Cplus/ChangeMinimumCharacterstoSatisfyOneofThreeConditions.cpp
+++ Cplus/ChangeMinimumCharacterstoSatisfyOneofThreeConditions.cpp
@@ -7,14 +7,14 @@ class Solution
 public:
 	int minCharacters(string a, string b)
 	{
-		int N1 = a.length(), N2 = b.length(), res;
+		int N1 = a.length(), N2 = b.length();
 		vector<int> acount(26), bcount(26);
 		int amost = 0, bmost = 0;
 		for (auto c : a)
 			amost = max(amost, ++acount[c - 'a']);
 		for (auto c : b)
 			bmost = max(bmost, ++bcount[c - 'a']);
-		res = N1 - amost + N2 - bmost;
+		int res = N1 - amost + N2 - bmost;
 		for (char d = 'a'; d < 'z'; ++d)
 		{
 			int sum1 = 0, sum2 = 0;
@@ -22,23 +22,17 @@ public:
 			{
 				if (c > d)
 					++sum1;
+				if (c <= d)
+					++sum2;
 			}
 			for (auto c : b)
 			{
 				if (c <= d)
 					++sum1;
-			}
-
-			for (auto c : b)
-			{
 				if (c > d)
 					++sum2;
 			}
-			for (auto c : a)
-			{
-				if (c <= d)
-					++sum2;
-			}
+
 			res = min(res, sum1);
 			res = min(res, sum2);
 		}
