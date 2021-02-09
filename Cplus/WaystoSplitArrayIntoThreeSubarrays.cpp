38c0ea7b9364d1674fc510a13fff9a08a377a19b
JumHorn <JumHorn@gmail.com>
2021-02-09 08:35:21 +0800

    Ways to Split Array Into Three Subarrays beats 99.00%

--- Cplus/WaystoSplitArrayIntoThreeSubarrays.cpp
+++ Cplus/WaystoSplitArrayIntoThreeSubarrays.cpp
@@ -1,3 +1,4 @@
+#include <numeric>
 #include <vector>
 using namespace std;
 
@@ -6,38 +7,15 @@ class Solution
 public:
 	int waysToSplit(vector<int> &nums)
 	{
-		int N = nums.size();
-		long res = 0;
-		vector<int> prefixsum(N + 1);
-		for (int i = 0; i < N; ++i)
-			prefixsum[i + 1] = prefixsum[i] + nums[i];
-		for (int i = 0; i < N - 2; ++i)
+		int N = nums.size(), res = 0;
+		partial_sum(nums.begin(), nums.end(), nums.begin());
+		for (int i = 0, j = 0, k = 0; i < N - 2; ++i)
 		{
-			if (prefixsum[i + 1] != 0 && prefixsum[N] <= 2 * prefixsum[i + 1])
-				break;
-			int lo = i + 2, hi = N;
-			while (lo < hi)
-			{
-				int mi = (hi - lo) / 2 + lo;
-				if (prefixsum[mi] < 2 * prefixsum[i + 1])
-					lo = mi + 1;
-				else
-					hi = mi;
-			}
-
-			if (prefixsum[lo] - prefixsum[i + 1] <= prefixsum[N] - prefixsum[lo])
-			{
-				int lo2 = lo + 1, hi2 = N;
-				while (lo2 < hi2)
-				{
-					int mi = (hi2 - lo2) / 2 + lo2;
-					if (prefixsum[mi] - prefixsum[i + 1] <= prefixsum[N] - prefixsum[mi])
-						lo2 = mi + 1;
-					else
-						hi2 = mi;
-				}
-				res = (res + lo2 - lo) % MOD;
-			}
+			while (j <= i || (j < N - 1 && nums[j] < nums[i] * 2))
+				++j;
+			while (k < j || (k < N - 1 && nums[k] - nums[i] <= nums[N - 1] - nums[k]))
+				++k;
+			res = (res + k - j) % MOD;
 		}
 		return res;
 	}
