a8bda0f5d9e9325ac03c0badc0c47bc8dad97964
JumHorn <JumHorn@gmail.com>
2021-02-22 09:23:51 +0800

    Maximum Score from Performing Multiplication Operations beats 33.33%

--- Cplus/MaximumScorefromPerformingMultiplicationOperations.cpp
+++ Cplus/MaximumScorefromPerformingMultiplicationOperations.cpp
@@ -7,19 +7,18 @@ public:
 	int maximumScore(vector<int> &nums, vector<int> &multipliers)
 	{
 		int N = nums.size(), M = multipliers.size();
-		vector<vector<int>> dp(N, vector<int>(N, -1));
-		return memdp(nums, multipliers, 0, N - 1, dp);
+		vector<vector<int>> dp(M, vector<int>(M, -1));
+		return memdp(nums, multipliers, 0, 0, dp);
 	}
 
-	int memdp(vector<int> &nums, vector<int> &multipliers, int first, int last, vector<vector<int>> &dp)
+	int memdp(vector<int> &nums, vector<int> &multipliers, int left, int index, vector<vector<int>> &dp)
 	{
 		int N = nums.size(), M = multipliers.size();
-		int index = first + N - 1 - last;
 		if (index >= M)
 			return 0;
-		if (dp[first][last] != -1)
-			return dp[first][last];
-		return dp[first][last] = max(memdp(nums, multipliers, first + 1, last, dp) + nums[first] * multipliers[index],
-									 memdp(nums, multipliers, first, last - 1, dp) + nums[last] * multipliers[index]);
+		if (dp[left][index] != -1)
+			return dp[left][index];
+		return dp[left][index] = max(memdp(nums, multipliers, left + 1, index + 1, dp) + nums[left] * multipliers[index],
+									 memdp(nums, multipliers, left, index + 1, dp) + nums[N - (index - left) - 1] * multipliers[index]);
 	}
 };
