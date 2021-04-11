a866516c8e700f1b68019c4f711e0a113b98b3d6
JumHorn <JumHorn@gmail.com>
2021-04-11 14:11:01 +0800

    Minimum Sideway Jumps beats 100%

--- Cplus/MinimumSidewayJumps.cpp
+++ Cplus/MinimumSidewayJumps.cpp
@@ -7,26 +7,17 @@ public:
 	int minSideJumps(vector<int> &obstacles)
 	{
 		int N = obstacles.size(), INF = 1e9;
-		vector<vector<int>> dp(N, vector<int>(3));
-		dp[0][1] = 0;
-		dp[0][0] = dp[0][2] = 1;
-		for (int i = 1; i < N; ++i)
+		vector<int> dp = {1, 0, 1};
+		for (auto n : obstacles)
 		{
-			dp[i][0] = dp[i - 1][0];
-			dp[i][1] = dp[i - 1][1];
-			dp[i][2] = dp[i - 1][2];
-			if (obstacles[i] != 0)
-				dp[i][obstacles[i] - 1] = INF;
-			//lane 0
-			if (obstacles[i] != 1)
-				dp[i][0] = min({dp[i - 1][0], dp[i][1] + 1, dp[i][2] + 1});
-			//lane 1
-			if (obstacles[i] != 2)
-				dp[i][1] = min({dp[i - 1][1], dp[i][0] + 1, dp[i][2] + 1});
-			//lane 2
-			if (obstacles[i] != 3)
-				dp[i][2] = min({dp[i - 1][2], dp[i][0] + 1, dp[i][1] + 1});
+			if (n > 0)
+				dp[n - 1] = INF;
+			for (int i = 0; i < 3; ++i)
+			{
+				if (n != i + 1)
+					dp[i] = min({dp[i], dp[(i + 1) % 3] + 1, dp[(i + 2) % 3] + 1});
+			}
 		}
-		return min({dp[N - 1][0], dp[N - 1][1], dp[N - 1][2]});
+		return min({dp[0], dp[1], dp[2]});
 	}
 };
