a0b6c52cef3988b287e0e556babb4ebe51bcad55
JumHorn <JumHorn@gmail.com>
2021-04-15 23:06:48 +0800

    Josephus problem

--- Cplus/FindtheWinneroftheCircularGame.cpp
+++ Cplus/FindtheWinneroftheCircularGame.cpp
@@ -1,27 +1,17 @@
-#include <vector>
-using namespace std;
+
+/*
+Josephus problem
+https://en.wikipedia.org/wiki/Josephus_problem
+*/
 
 class Solution
 {
 public:
 	int findTheWinner(int n, int k)
 	{
-		vector<int> v(n);
-		for (int index = -1, i = 0; i < n - 1; ++i)
-		{
-			for (int i = 0; i < k;)
-			{
-				index = (index + 1) % n;
-				if (v[index] == 0)
-					++i;
-			}
-			v[index] = 1;
-		}
-		for (int i = 0; i < n; ++i)
-		{
-			if (v[i] == 0)
-				return i + 1;
-		}
-		return 0;
+		int res = 0;
+		for (int i = 1; i <= n; ++i)
+			res = (res + k) % i;
+		return res + 1;
 	}
 };
