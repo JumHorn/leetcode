1fa294d5cd476c54d149aa2bb869e067b394ef9b
JumHorn <JumHorn@gmail.com>
2021-04-25 07:47:57 +0800

    Equal Sum Arrays With Minimum Number of Operations beats 97.05%

--- Cplus/EqualSumArraysWithMinimumNumberofOperations.cpp
+++ Cplus/EqualSumArraysWithMinimumNumberofOperations.cpp
@@ -7,10 +7,8 @@ class Solution
 public:
 	int minOperations(vector<int> &nums1, vector<int> &nums2)
 	{
+		int sum1 = 0, sum2 = 0;
 		vector<int> count1(7), count2(7);
-		int res = INT_MAX, sum1 = 0, sum2 = 0, N1 = nums1.size(), N2 = nums2.size();
-		if (N1 > N2 * 6 || N2 > N1 * 6)
-			return -1;
 		for (auto n : nums1)
 		{
 			sum1 += n;
@@ -21,47 +19,23 @@ public:
 			sum2 += n;
 			++count2[n];
 		}
-		if (sum1 == sum2)
-			return 0;
+		int N1 = nums1.size(), N2 = nums2.size();
 		if (sum1 > sum2)
 		{
 			swap(sum1, sum2);
 			swap(N1, N2);
 			count1.swap(count2);
 		}
-		for (int sum = max(sum1, N2); sum <= min(sum2, N1 * 6); ++sum)
-			res = min(res, add(count1, sum - sum1) + substract(count2, sum2 - sum));
-		return res;
-	}
-
-	int add(vector<int> &count, int sum)
-	{
-		int res = 0;
-		for (int i = 1; i < (int)count.size() && sum > 0; ++i)
+		int diff = sum2 - sum1, res = 0;
+		if (diff > 6 * N1 - sum1 + sum2 - N2)
+			return -1;
+		for (int i = 1; i < 7 && diff > 0; ++i)
 		{
-			if (sum <= count[i] * (6 - i))
-			{
-				res += (sum - 1) / (6 - i) + 1;
-				break;
-			}
-			res += count[i];
-			sum -= count[i] * (6 - i);
-		}
-		return res;
-	}
-
-	int substract(vector<int> &count, int sum)
-	{
-		int res = 0;
-		for (int i = (int)count.size() - 1; i >= 1 && sum > 0; --i)
-		{
-			if (sum <= count[i] * (i - 1))
-			{
-				res += (sum - 1) / (i - 1) + 1;
-				break;
-			}
-			res += count[i];
-			sum -= count[i] * (i - 1);
+			if (diff >= (6 - i) * (count1[i] + count2[7 - i]))
+				res += count1[i] + count2[7 - i];
+			else
+				res += (diff - 1) / (6 - i) + 1;
+			diff -= (6 - i) * (count1[i] + count2[7 - i]);
 		}
 		return res;
 	}
