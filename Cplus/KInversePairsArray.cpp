e5ee5c3102bfbdfb1737e98cd44aaf4c0c236a26
JumHorn <JumHorn@gmail.com>
2021-05-16 16:20:35 +0800

    K Inverse Pairs Array beats 65.19%

--- Cplus/KInversePairsArray.cpp
+++ Cplus/KInversePairsArray.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 
 /*
-dp[i][k] accumulate [0,k] inverse pairs until index i
+dp(i, j) = dp(i - 1, j) + dp(i - 1, j - 1) + ... + dp(i - 1, j - i + 1)
+dp(i, j - 1) = dp(i - 1, j - 1) + dp(i - 1, j - 2) + ... + dp(i - 1, j - i)
+
+dp(i, j) - dp(i - 1, j) = dp(i, j - 1) - dp(i - 1, j - i) ==>
+dp(i, j) = dp(i, j - 1) + dp(i - 1, j) - dp(i - 1, j - i)
 */
 
 class Solution
@@ -10,22 +14,18 @@ class Solution
 public:
 	int kInversePairs(int n, int k)
 	{
-		if (k == 0)
-			return 1;
 		vector<vector<int>> dp(n + 1, vector<int>(k + 1));
 		for (int i = 1; i <= n; ++i)
 			dp[i][0] = 1;
 		for (int i = 1; i <= n; ++i)
 		{
-			for (int j = 1; j <= k; ++j)
+			for (int j = 1; j <= min(k, i * (i - 1) / 2); ++j)
 			{
-				if (j >= i)
-					dp[i][j] = (dp[i][j - 1] + (dp[i - 1][j] + MOD - dp[i - 1][j - i]) % MOD) % MOD;
-				else
-					dp[i][j] = (dp[i][j - 1] + dp[i - 1][j]) % MOD;
+				long val = (j >= i ? dp[i - 1][j - i] : 0);
+				dp[i][j] = (dp[i][j - 1] + dp[i - 1][j] - val + MOD) % MOD;
 			}
 		}
-		return (dp[n][k] + MOD - dp[n][k - 1]) % MOD;
+		return dp[n][k];
 	}
 
 private:
