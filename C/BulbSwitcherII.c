f29ae2859e6c748c51a677246ca41d80da2c576b
JumHorn <JumHorn@gmail.com>
2021-05-17 08:49:48 +0800

    simplified

--- C/BulbSwitcherII.c
+++ C/BulbSwitcherII.c
@@ -7,7 +7,7 @@ int flipLights(int n, int m)
 	if (m == 0)
 		return 1;
 	if (m == 1)
-		return n == 1 ? 2 : n == 2 ? 3 : 4;
+		return n + 1;
 	if (m == 2)
 		return n == 1 ? 2 : n == 2 ? 4 : 7;
 	return n == 1 ? 2 : n == 2 ? 4 : 8;
