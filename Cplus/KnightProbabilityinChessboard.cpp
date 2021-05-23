02b66bcc26c5cd1f159c2a9bef75fda2d8898040
JumHorn <JumHorn@gmail.com>
2021-05-23 09:38:57 +0800

    Knight Probability in Chessboard beats 25.75%

--- Cplus/KnightProbabilityinChessboard.cpp
+++ Cplus/KnightProbabilityinChessboard.cpp
@@ -6,23 +6,32 @@ class Solution
 public:
 	double knightProbability(int N, int K, int r, int c)
 	{
-		vector<vector<vector<double>>> dp(K + 1, vector<vector<double>>(N, vector<double>(N, -1)));
-		return memdp(N, K, r, c, dp);
-	}
-
-	double memdp(int N, int K, int r, int c, vector<vector<vector<double>>> &dp)
-	{
-		if (r < 0 || r >= N || c < 0 || c >= N)
-			return 0;
-		if (K == 0)
-			return 1;
-		if (dp[K][r][c] != -1)
-			return dp[K][r][c];
+		vector<vector<vector<double>>> dp(K + 1, vector<vector<double>>(N, vector<double>(N)));
 		int dr[] = {2, 2, 1, 1, -1, -1, -2, -2};
 		int dc[] = {1, -1, 2, -2, 2, -2, 1, -1};
-		double res = 0;
-		for (int i = 0; i < 8; ++i)
-			res += memdp(N, K - 1, r + dr[i], c + dc[i], dp) / 8.0;
-		return dp[K][r][c] = res;
+		dp[0][r][c] = 1.0;
+		for (int k = 0; k < K; ++k)
+		{
+			for (int i = 0; i < N; ++i)
+			{
+				for (int j = 0; j < N; ++j)
+				{
+					for (int d = 0; d < 8; ++d)
+					{
+						int x = i + dr[d], y = j + dc[d];
+						if (x >= 0 && x < N && y >= 0 && y < N)
+							dp[k + 1][x][y] += dp[k][i][j] / 8.0;
+					}
+				}
+			}
+		}
+
+		double res = 0.0;
+		for (int i = 0; i < N; ++i)
+		{
+			for (int j = 0; j < N; ++j)
+				res += dp[K][i][j];
+		}
+		return res;
 	}
 };
