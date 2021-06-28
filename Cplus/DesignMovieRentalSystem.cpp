8733eab1dc9dbba0eae775c08c4dfda443c15946
JumHorn <JumHorn@gmail.com>
2021-06-28 22:25:21 +0800

    Design Movie Rental System beats 100%

--- Cplus/DesignMovieRentalSystem.cpp
+++ Cplus/DesignMovieRentalSystem.cpp
@@ -1,3 +1,4 @@
+#include <map>
 #include <set>
 #include <vector>
 using namespace std;
@@ -7,26 +8,56 @@ class MovieRentingSystem
 public:
 	MovieRentingSystem(int n, vector<vector<int>> &entries)
 	{
+		for (auto &e : entries)
+		{
+			data[{e[0], e[1]}] = e[2];
+			unrented[e[1]].insert({e[2], e[0]});
+		}
 	}
 
 	vector<int> search(int movie)
 	{
+		auto iter = unrented.find(movie);
+		if (iter == unrented.end())
+			return {};
+		vector<int> res;
+		auto iter1 = iter->second.begin();
+		for (int i = 0; i < 5 && iter1 != iter->second.end(); ++i, ++iter1)
+			res.push_back(iter1->second);
+		return res;
 	}
 
 	void rent(int shop, int movie)
 	{
+		int price = data[{shop, movie}];
+		rented.insert({price, movie, shop});
+		unrented[movie].erase({price, shop});
 	}
 
 	void drop(int shop, int movie)
 	{
+		int price = data[{shop, movie}];
+		rented.erase({price, movie, shop});
+		unrented[movie].insert({price, shop});
 	}
 
 	vector<vector<int>> report()
 	{
+		vector<vector<int>> res;
+		int i = 0;
+		for (auto [p, m, s] : rented)
+		{
+			if (++i > 5)
+				break;
+			res.push_back({s, m});
+		}
+		return res;
 	}
 
 private:
-	set<pair<int, int>> rented; //{shop,movie}
+	map<pair<int, int>, int> data;			//{{shop,movie},price}
+	set<tuple<int, int, int>> rented;		//{price,movie,shop}
+	map<int, set<pair<int, int>>> unrented; //{movie,{price,shop}}
 };
 
 /**
