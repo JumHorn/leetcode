87f400049420afd6c731b9408b12ff77d94e3e5e
JumHorn <JumHorn@gmail.com>
2021-07-07 09:02:49 +0800

    formatted

--- Cplus/FindK-thSmallestPairDistance.cpp
+++ Cplus/FindK-thSmallestPairDistance.cpp
@@ -8,13 +8,13 @@ public:
 	int smallestDistancePair(vector<int> &nums, int k)
 	{
 		sort(nums.begin(), nums.end());
-		int hi = nums.back() - nums.front(), lo = 0;
+		int hi = nums.back() - nums.front(), lo = 0, N = nums.size();
 		while (lo < hi)
 		{
 			int mi = (hi - lo) / 2 + lo;
 
 			int count = 0;
-			for (int i = 0, j = 0; i < (int)nums.size(); ++i)
+			for (int i = 0, j = 0; i < N; ++i)
 			{
 				while (nums[i] - nums[j] > mi)
 					++j;
