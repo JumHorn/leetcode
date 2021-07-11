a7f59b2b69dd285926d96033eac05a0d56901500
JumHorn <JumHorn@gmail.com>
2021-07-11 09:27:31 +0800

    Maximum Number of Removable Characters beats 64.28%

--- Cplus/MaximumNumberofRemovableCharacters.cpp
+++ Cplus/MaximumNumberofRemovableCharacters.cpp
@@ -8,11 +8,14 @@ class Solution
 public:
 	int maximumRemovals(string s, string p, vector<int> &removable)
 	{
-		int lo = 0, hi = removable.size() + 1;
+		int N = removable.size(), lo = 0, hi = N + 1;
+		vector<int> remove(s.size(), s.size());
+		for (int i = 0; i < N; ++i)
+			remove[removable[i]] = i;
 		while (lo < hi)
 		{
 			int mi = (hi - lo) / 2 + lo;
-			if (isSubsequence(s, p, removable, mi))
+			if (isSubsequence(s, p, remove, mi))
 				lo = mi + 1;
 			else
 				hi = mi;
@@ -20,20 +23,13 @@ public:
 		return lo - 1;
 	}
 
-	bool isSubsequence(string &s, string &p, vector<int> &v, int size)
+	bool isSubsequence(string &s, string &p, vector<int> &remove, int size)
 	{
 		int i = 0, j = 0, k = 0, M = s.length(), N = p.length();
-		vector<int> tmp(v.begin(), v.begin() + size);
-		sort(tmp.begin(), tmp.end());
 		for (; i < N && j < M; ++j)
 		{
-			if (k < size && j == tmp[k])
-				++k;
-			else
-			{
-				if (s[j] == p[i])
-					++i;
-			}
+			if (remove[j] >= size && s[j] == p[i])
+				++i;
 		}
 		return i == N;
 	}
