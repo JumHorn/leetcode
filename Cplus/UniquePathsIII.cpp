0584926381d1752e1bbd1171a7c0fde84d799047
JumHorn <JumHorn@gmail.com>
2021-08-21 22:22:39 +0800

    cache memory time consuming

--- Cplus/UniquePathsIII.cpp
+++ Cplus/UniquePathsIII.cpp
@@ -6,7 +6,7 @@ class Solution
 public:
 	int uniquePathsIII(vector<vector<int>> &grid)
 	{
-		int allzero = 0, M = 0, N = 0, startx = 0, starty = 0;
+		int M = grid.size(), N = grid[0].size(), mask = 0, startx = 0, starty = 0;
 		for (int i = 0; i < M; ++i)
 		{
 			for (int j = 0; j < N; ++j)
@@ -16,28 +16,37 @@ public:
 					startx = i;
 					starty = j;
 				}
-				else if (grid[i][j] == 0)
-					++allzero;
+				else if (grid[i][j] == 0 || grid[i][j] == 2)
+					mask |= encode(i, j, N);
 			}
 		}
-		return dfs(grid, allzero + 1, startx, starty); //for the start pos will consume count from allzero
+		vector<vector<vector<int>>> dp(M, vector<vector<int>>(N, vector<int>(1 << M * N, -1)));
+		return memdp(grid, startx, starty, mask, dp); //for the start pos will consume count from allzero
 	}
 
-	int dfs(vector<vector<int>> &grid, int allzero, int m, int n)
+	int memdp(vector<vector<int>> &grid, int r, int c, int mask, vector<vector<vector<int>>> &dp)
 	{
-		if (m >= (int)grid.size() || m < 0 || n >= (int)grid[0].size() || n < 0)
-			return 0;
-		if (grid[m][n] == -1 || allzero < 0)
-			return 0;
-		if (grid[m][n] == 2)
-			return allzero == 0 ? 1 : 0;
-		int res = 0;
-		grid[m][n] = -1;
+		int M = grid.size(), N = grid[0].size();
+		if (grid[r][c] == 2)
+			return mask == 0 ? 1 : 0;
+		if (dp[r][c][mask] != -1)
+			return dp[r][c][mask];
 		//board dfs direction
 		int path[5] = {-1, 0, 1, 0, -1};
+		int res = 0;
 		for (int i = 0; i < 4; ++i)
-			res += dfs(grid, allzero - 1, m + path[i], n + path[i + 1]);
-		grid[m][n] = 0;
-		return res;
+		{
+			int dx = r + path[i], dy = c + path[i + 1];
+			if (dx >= M || dx < 0 || dy >= N || dy < 0)
+				continue;
+			if ((mask & encode(dx, dy, N)) != 0)
+				res += memdp(grid, dx, dy, mask ^ encode(dx, dy, N), dp);
+		}
+		return dp[r][c][mask] = res;
+	}
+
+	int encode(int r, int c, int N)
+	{
+		return 1 << (r * N + c);
 	}
 };
