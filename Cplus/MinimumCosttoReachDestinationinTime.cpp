8310e251b93519e6c74ba4001cfbc1f1a968fcdb
JumHorn <JumHorn@gmail.com>
2021-08-22 10:25:22 +0800

    another bellman ford

--- Cplus/MinimumCosttoReachDestinationinTime.cpp
+++ Cplus/MinimumCosttoReachDestinationinTime.cpp
@@ -3,56 +3,38 @@
 #include <vector>
 using namespace std;
 
-struct info
-{
-	int cost;
-	int time;
-	int city;
-	info(int x, int y, int z) : cost(x), time(y), city(z)
-	{
-	}
-};
-
-bool operator<(const info &lhs, const info &rhs)
-{
-	return lhs.cost > rhs.cost;
-}
+/*
+Algorithm:
+Bellman Ford
+*/
 
 class Solution
 {
 public:
 	int minCost(int maxTime, vector<vector<int>> &edges, vector<int> &passingFees)
 	{
-		priority_queue<info> q; //{cost,time,city}
 		int N = passingFees.size();
-		vector<vector<pair<int, int>>> graph(N + 1); //{connected city,time}
-		for (auto &e : edges)
+		vector<vector<int>> dp(maxTime + 1, vector<int>(N, INF));
+		dp[0][0] = passingFees[0];
+		for (int t = 1; t <= maxTime; ++t)
 		{
-			graph[e[0]].push_back({e[1], e[2]});
-			graph[e[1]].push_back({e[0], e[2]});
-		}
-		q.push(info(passingFees[0], 0, 0));
-		vector<vector<int>> seen(N + 1, vector<int>(maxTime + 1, INT_MAX));
-		seen[0][0] = passingFees[0];
-		while (!q.empty())
-		{
-			int cost = q.top().cost, time = q.top().time, city = q.top().city;
-			q.pop();
-			for (auto [to, t] : graph[city])
+			for (const auto &edge : edges)
 			{
-				if (time + t > maxTime)
-					continue;
-				if (seen[to][time + t] < cost + passingFees[to])
-					continue;
-				if (to == N - 1)
-					return cost + passingFees[to];
-				if (cost + passingFees[to] < seen[to][time + t])
+				int i = edge[0], j = edge[1], cost = edge[2];
+				if (cost <= t)
 				{
-					seen[to][time + t] = cost + passingFees[to];
-					q.push(info(cost + passingFees[to], time + t, to));
+					dp[t][i] = min(dp[t][i], dp[t - cost][j] + passingFees[i]);
+					dp[t][j] = min(dp[t][j], dp[t - cost][i] + passingFees[j]);
 				}
 			}
 		}
-		return -1;
+
+		int res = INF;
+		for (int t = 1; t <= maxTime; ++t)
+			res = min(res, dp[t][N - 1]);
+		return res == INF ? -1 : res;
 	}
+
+private:
+	static constexpr int INF = INT_MAX / 2;
 };
