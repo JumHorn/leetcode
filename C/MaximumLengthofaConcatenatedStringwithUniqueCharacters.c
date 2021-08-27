86b063ec76b00d6a90ef38d987dd14880e453060
JumHorn <JumHorn@gmail.com>
2021-08-27 09:00:54 +0800

    greatly improved

--- C/MaximumLengthofaConcatenatedStringwithUniqueCharacters.c
+++ C/MaximumLengthofaConcatenatedStringwithUniqueCharacters.c
@@ -2,28 +2,38 @@
 
 #define max(a, b) (((a) > (b)) ? (a) : (b))
 
-int dfs(char **arr, int arrSize, int index, char *str, int strSize)
+int bitCount(int n)
 {
-	int mask = 0;
-	for (int i = 0; i < strSize; ++i)
-	{
-		if (mask & (1 << (str[i] - 'a')))
-			return 0;
-		mask |= 1 << (str[i] - 'a');
-	}
-	int res = strSize;
-	for (int i = index; i < arrSize; ++i)
-	{
-		int len = strlen(arr[i]);
-		memcpy(&str[strSize], arr[i], sizeof(char) * len);
-		int val = dfs(arr, arrSize, i + 1, str, strSize + len);
-		res = max(res, val);
-	}
+	int res = 0;
+	for (; n != 0; n &= n - 1)
+		++res;
 	return res;
 }
 
 int maxLength(char **arr, int arrSize)
 {
-	char str[1000] = {0};
-	return dfs(arr, arrSize, 0, str, 0);
+	int maskArr[1 << 16] = {0}, maskArrSize = 1, res = 0;
+	for (int i = 0; i < arrSize; ++i)
+	{
+		int mask = 0;
+		for (int j = 0; arr[i][j]; ++j)
+		{
+			if (mask & (1 << (arr[i][j] - 'a')))
+				break;
+			mask |= 1 << (arr[i][j] - 'a');
+		}
+		if (mask == 0)
+			continue;
+		int N = maskArrSize;
+		for (int j = 0; j < N; ++j)
+		{
+			if ((mask & maskArr[j]) == 0)
+			{
+				maskArr[maskArrSize++] = (mask | maskArr[j]);
+				int bit = bitCount(mask | maskArr[j]);
+				res = max(res, bit);
+			}
+		}
+	}
+	return res;
 }
