6646e932d794c113dd1d75e11168c0db667a3743
JumHorn <JumHorn@gmail.com>
2021-09-19 21:39:30 +0800

    copy code from official

--- Cplus/LongestCommonSubpath.cpp
+++ Cplus/LongestCommonSubpath.cpp
@@ -1,115 +1,100 @@
 #include <algorithm>
+#include <random>
+#include <unordered_set>
 #include <vector>
 using namespace std;
 
-/*
-sa[i] the ith rank's index
-rank[i] the ith index's rank
-
-1. sa[rank[i]]=i
-2 .rank[sa[i]]=i
-
-3. lcp[i] >= lcp[i-1]-1
-此处的i下标表示原始string的下标
-考虑到下标i与i+1有x长度的前缀
-那么下标i+1与i+2至少有x-1长度前缀，因为除去了i与i+1的首个字符
-这个问题是很简单的问题，只是概念比较复杂
-*/
-
-typedef struct
-{
-	int index;
-	int currank;
-	int nextrank;
-} SuffixArray;
-
 class Solution
 {
+private:
+	// 根据正文部分的分析，我们选择的 mod 需要远大于 10^10
+	// 所以 C++ 语言中我们需要用到 long long 类型
+	// 那么在进行哈希值的运算时，中间值会超出 long long 类型的上限，而没有更大的类型了
+	// 因此我们可以使用一种折中的办法：对 mod1 和 mod2 分别取模，得到一个二元组
+	// 这本质上与对 mod1 * mod2 取模是一致的，这里我们选择 mod1=10^9+7，mod2=10^9+9
+	// 它们都是质数，乘积约为 10^18，远大于 10^10
+	static constexpr int mod1 = 1000000007;
+	static constexpr int mod2 = 1000000009;
+
+	// 由于我们得到的哈希是二元组，C++ 默认不支持将 pair 放入哈希表，我们需要自行实现哈希函数
+	struct pairhash
+	{
+		size_t operator()(const pair<int, int> &p) const
+		{
+			auto fn = hash<int>();
+			return (fn(p.first) << 16) ^ fn(p.second);
+		}
+	};
+
+	using LL = long long;
+
 public:
 	int longestCommonSubpath(int n, vector<vector<int>> &paths)
 	{
-		int M = paths.size(), sentinel = 0;
-		vector<int> onepath, color;
-		for (auto &path : paths)
-		{
-			onepath.insert(onepath.end(), path.begin(), path.end());
-			onepath.push_back(--sentinel);
-		}
-		int N = onepath.size();
+		// 本题中数组元素的范围为 [0, 10^5]
+		// 因此我们在 [10^6, 10^7] 的范围内随机选取进制 base
+		// 为了更减少冲突，我们可以对 mod1 和 mod2 分别选取 base1 和 base2
+		mt19937 gen{random_device{}()};
+		auto dis = uniform_int_distribution<int>(1e6, 1e7);
+		int base1 = dis(gen);
+		int base2 = dis(gen);
 
-		vector<SuffixArray> sa(N);
-		//init
-		for (int i = 0; i < N - 1; ++i)
+		int N = paths.size();
+		// 确定二分查找的上下界
+		int lo = 1, hi = min_element(paths.begin(), paths.end(),
+									 [](const auto &p1, const auto &p2)
+									 { return p1.size() < p2.size(); })
+							 ->size();
+		while (lo <= hi)
 		{
-			sa[i].index = i;
-			sa[i].currank = onepath[i];
-			sa[i].nextrank = onepath[i + 1];
-		}
-		sa[N - 1].index = N - 1;
-		sa[N - 1].currank = onepath[N - 1];
-		sa[N - 1].nextrank = INT_MIN;
-		auto f = [](const SuffixArray &lhs, const SuffixArray &rhs) {
-			if (lhs.currank != rhs.currank)
-				return lhs.currank < rhs.currank;
-			return lhs.nextrank < rhs.nextrank;
-		};
-		sort(sa.begin(), sa.end(), f);
-
-		int rank[N];
-		for (int k = 4; k < 2 * N; k *= 2)
-		{
-			int rank_count = 0;
-			int pre_rank = sa[0].currank;
-			sa[0].currank = rank_count;
-			rank[sa[0].index] = 0;
-			//update rank
-			for (int i = 1; i < N; ++i)
+			int mi = (hi - lo) / 2 + lo;
+			// 预处理 mult1=base1^mi, mult2=base2^mi
+			int mult1 = 1, mult2 = 1;
+			for (int i = 1; i <= mi; ++i)
 			{
-				if (sa[i].currank == pre_rank && sa[i].nextrank == sa[i - 1].nextrank)
-					sa[i].currank = rank_count;
-				else
-				{
-					pre_rank = sa[i].currank;
-					sa[i].currank = ++rank_count;
-				}
-				rank[sa[i].index] = i;
+				mult1 = (LL)mult1 * base1 % mod1;
+				mult2 = (LL)mult2 * base2 % mod2;
 			}
+
+			unordered_set<pair<int, int>, pairhash> s;
+			bool check = true;
 			for (int i = 0; i < N; ++i)
 			{
-				int nextindex = sa[i].index + k / 2;
-				sa[i].nextrank = (nextindex < N) ? sa[rank[nextindex]].currank : -1;
-			}
-			sort(sa.begin(), sa.end(), f);
-		}
+				int hash1 = 0, hash2 = 0;
+				// 计算首个长度为 mi 的子数组的哈希值
+				for (int j = 0; j < mi; ++j)
+				{
+					hash1 = ((LL)hash1 * base1 + paths[i][j]) % mod1;
+					hash2 = ((LL)hash2 * base2 + paths[i][j]) % mod2;
+				}
 
-		//calc lcp array
-		int lcp[N];
-		for (int i = 0; i < N; ++i)
-			rank[sa[i].index] = i;
-		for (int i = 0, k = 0; i < N; ++i)
-		{
-			if (rank[i] == N - 1)
-				lcp[rank[i]] = k = 0;
+				unordered_set<pair<int, int>, pairhash> t;
+				// 如果我们遍历的是第 0 个数组，或者上一个数组的哈希表中包含该二元组
+				// 我们才会将二元组加入当前数组的哈希表中
+				if (i == 0 || s.count({hash1, hash2}))
+					t.emplace(hash1, hash2);
+
+				// 递推计算后续子数组的哈希值
+				for (int j = mi; j < paths[i].size(); ++j)
+				{
+					hash1 = (((LL)hash1 * base1 % mod1 - (LL)paths[i][j - mi] * mult1 % mod1 + paths[i][j]) % mod1 + mod1) % mod1;
+					hash2 = (((LL)hash2 * base2 % mod2 - (LL)paths[i][j - mi] * mult2 % mod2 + paths[i][j]) % mod2 + mod2) % mod2;
+					if (i == 0 || s.count({hash1, hash2}))
+						t.emplace(hash1, hash2);
+				}
+				if (t.empty())
+				{
+					check = false;
+					break;
+				}
+				s = move(t);
+			}
+
+			if (check)
+				lo = mi + 1;
 			else
-			{
-				int j = sa[rank[i] + 1].index;
-				while (i + k < N && j + k < N && onepath[i + k] == onepath[j + k])
-					++k;
-				lcp[rank[i]] = k;
-				if (k > 0)
-					--k;
-			}
+				hi = mi - 1;
 		}
-		//calc result
-		int len = lcp[0], index = 0;
-		for (int i = 1; i < N; ++i)
-		{
-			if (lcp[i] > len)
-			{
-				len = lcp[i];
-				index = sa[i].index;
-			}
-		}
-		return len;
+		return lo - 1;
 	}
 };
