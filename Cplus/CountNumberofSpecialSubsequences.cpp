5fffc3cf33cf17e9a8adcf406b0a9100aafadf97
JumHorn <JumHorn@gmail.com>
2021-09-20 09:33:32 +0800

    improved

--- Cplus/CountNumberofSpecialSubsequences.cpp
+++ Cplus/CountNumberofSpecialSubsequences.cpp
@@ -6,26 +6,10 @@ class Solution
 public:
 	int countSpecialSubsequences(vector<int> &nums)
 	{
-		int N = nums.size();
-		vector<vector<long>> dp(N + 1, vector<long>(3));
-		for (int i = 0; i < N; ++i)
-		{
-			if (nums[i] == 0)
-				dp[i + 1][0] = (dp[i][0] * 2 + 1) % MOD;
-			else
-				dp[i + 1][0] = dp[i][0];
-		}
-		for (int k = 1; k <= 2; ++k)
-		{
-			for (int i = 0; i < N; ++i)
-			{
-				if (nums[i] == k)
-					dp[i + 1][k] = (dp[i][k] * 2 + dp[i][k - 1]) % MOD;
-				else
-					dp[i + 1][k] = dp[i][k];
-			}
-		}
-		return dp[N][2];
+		vector<int> dp = {1, 0, 0, 0};
+		for (auto n : nums)
+			dp[n + 1] = (dp[n + 1] * 2 + dp[n]) % MOD;
+		return dp.back();
 	}
 
 private:
