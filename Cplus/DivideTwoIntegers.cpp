8ebb22dbedaed32c4985908bd5cf727da9b7106b
JumHorn <JumHorn@gmail.com>
2021-09-23 08:31:29 +0800

    greatly improved

--- Cplus/DivideTwoIntegers.cpp
+++ Cplus/DivideTwoIntegers.cpp
@@ -2,6 +2,10 @@
 #include <cmath>
 using namespace std;
 
+/*
+abs(INT_MIN) overflow depend on compilers
+*/
+
 class Solution
 {
 public:
@@ -9,14 +13,13 @@ public:
 	{
 		if ((dividend == INT_MIN && divisor == -1) || (divisor == 0)) //overflow
 			return INT_MAX;
-		long res = 0;
-		for (long d = labs(dividend), dv = labs(divisor); d >= dv;)
+		int res = 0, i = 31;
+		for (unsigned int d = abs(dividend), dv = abs(divisor); d >= dv;)
 		{
-			int i = 1;
-			while (d - (dv << i) >= 0)
-				++i;
-			res += 1u << (i - 1);
-			d -= dv << (i - 1);
+			while ((d >> i) < dv)
+				--i;
+			res |= 1u << i;
+			d -= dv << i;
 		}
 		return (dividend ^ divisor) >= 0 ? res : -res;
 	}
