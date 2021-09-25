a71eca05809706f99c1f8b78534939d6b1244c4f
JumHorn <JumHorn@gmail.com>
2021-09-25 08:19:25 +0800

    formatted

--- C/SetMatrixZeroes.c
+++ C/SetMatrixZeroes.c
@@ -1,11 +1,11 @@
 
 void setZeroes(int **matrix, int matrixSize, int *matrixColSize)
 {
-	int row = 0, M = matrixSize, N = *matrixColSize;
+	int first_column = 0, M = matrixSize, N = *matrixColSize;
 	for (int i = 0; i < M; ++i)
 	{
 		if (matrix[i][0] == 0)
-			row = 1;
+			first_column = 1;
 		for (int j = 1; j < N; ++j)
 		{
 			if (matrix[i][j] == 0)
@@ -20,7 +20,7 @@ void setZeroes(int **matrix, int matrixSize, int *matrixColSize)
 			if (matrix[i][0] == 0 || matrix[0][j] == 0)
 				matrix[i][j] = 0;
 		}
-		if (row == 1)
+		if (first_column == 1)
 			matrix[i][0] = 0;
 	}
 }
--- Cplus/SetMatrixZeroes.cpp
+++ Cplus/SetMatrixZeroes.cpp
@@ -7,11 +7,11 @@ public:
 	void setZeroes(vector<vector<int>> &matrix)
 	{
 		int M = matrix.size(), N = matrix[0].size();
-		bool row = false;
+		bool first_column = false;
 		for (int i = 0; i < M; ++i)
 		{
 			if (matrix[i][0] == 0)
-				row = true;
+				first_column = true;
 			for (int j = 1; j < N; ++j)
 			{
 				if (matrix[i][j] == 0)
@@ -26,7 +26,7 @@ public:
 				if (matrix[i][0] == 0 || matrix[0][j] == 0)
 					matrix[i][j] = 0;
 			}
-			if (row)
+			if (first_column)
 				matrix[i][0] = 0;
 		}
 	}
