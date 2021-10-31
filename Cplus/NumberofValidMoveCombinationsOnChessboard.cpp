3ca19c1abde22f593302a9006acf29d677ed1030
JumHorn <JumHorn@gmail.com>
2021-10-31 10:25:25 +0800

    wrong

--- Cplus/NumberofValidMoveCombinationsOnChessboard.cpp
+++ Cplus/NumberofValidMoveCombinationsOnChessboard.cpp
@@ -9,6 +9,7 @@ using namespace std;
 state
 xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx
 每个字节表示对应下标的压缩坐标位置x*8+y
+任意位置8个方向的所有可能集合，大于该问题的集合
 */
 
 class Solution
@@ -16,7 +17,6 @@ class Solution
 public:
 	int countCombinations(vector<string> &pieces, vector<vector<int>> &positions)
 	{
-		unordered_map<string, vector<vector<int>>> dir;
 		dir["rook"] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 		dir["queen"] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
 		dir["bishop"] = {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
@@ -29,15 +29,42 @@ public:
 			coord.push_back(x * 8 + y);
 		}
 		int state = compress(coord);
-		int res = 0, N = pieces.size();
+		int N = pieces.size();
 		q.push(state);
 		seen.insert(state);
 		while (!q.empty())
 		{
 			state = q.front();
 			q.pop();
+			vector<vector<int>> coordinate(N);
+			uncompress(state, N, pieces, coordinate);
+			dfs(coordinate, 0, 0, q, seen);
 		}
-		return res;
+		return seen.size();
+	}
+
+	void dfs(vector<vector<int>> &coord, int index, int state, queue<int> &q, unordered_set<int> &seen)
+	{
+		int N = coord.size();
+		if (index >= N)
+		{
+			if (valide(state, N) && seen.count(state) == 0)
+			{
+				seen.insert(state);
+				q.push(state);
+			}
+			return;
+		}
+		for (auto p : coord[index])
+			dfs(coord, index + 1, state | (p << (index * 8)), q, seen);
+	}
+
+	bool valide(int state, int N)
+	{
+		unordered_set<int> s;
+		for (int i = 0; i < N; ++i)
+			s.insert((state >> (i * 8)) & 0xFF);
+		return s.size() == N;
 	}
 
 	int compress(vector<int> &coordinate)
@@ -48,11 +75,22 @@ public:
 		return res;
 	}
 
-	vector<int> uncompress(int coord, int n)
+	void uncompress(int state, int N, vector<string> &pieces, vector<vector<int>> &coord)
 	{
-		vector<int> res;
-		for (int i = 0; i < n; ++i)
-			res.push_back(0xFF & (coord >> (i * 8)));
-		return res;
+		for (int i = 0; i < N; ++i)
+		{
+			int co = (0xFF & (state >> (i * 8)));
+			int x = co / SIZE, y = co % SIZE;
+			for (auto &v : dir[pieces[i]])
+			{
+				int dx = x + v[0], dy = y + v[1];
+				if (dx >= 0 && dx < SIZE && dy >= 0 && dy < SIZE)
+					coord[i].push_back(dx * SIZE + dy);
+			}
+		}
 	}
+
+private:
+	unordered_map<string, vector<vector<int>>> dir;
+	const static int SIZE = 8;
 };
