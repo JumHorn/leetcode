136405a1c3490322731f56cb94230e22e0a9f206
JumHorn <JumHorn@gmail.com>
2021-10-31 09:35:28 +0800

    formatted

--- Cplus/MovingStonesUntilConsecutiveII.cpp
+++ Cplus/MovingStonesUntilConsecutiveII.cpp
@@ -9,12 +9,12 @@ public:
 	{
 		sort(stones.begin(), stones.end());
 		int N = stones.size(), low = N;
-		int high = max(stones[N - 1] - N + 2 - stones[1], stones[N - 2] - stones[0] - N + 2);
+		int high = max(stones[N - 1] - stones[1] - N + 2, stones[N - 2] - stones[0] - N + 2);
 		for (int i = 0, j = 0; j < N; ++j)
 		{
 			while (stones[j] - stones[i] >= N)
 				++i;
-			if (j - i + 1 == N - 1 && stones[j] - stones[i] == N - 2)
+			if (j - i == N - 2 && stones[j] - stones[i] == N - 2)
 				low = min(low, 2);
 			else
 				low = min(low, N - (j - i + 1));
