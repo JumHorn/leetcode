de37df859ccc87cf640bbedf0e92c2932b3e62b3
JumHorn <JumHorn@gmail.com>
2021-11-06 11:12:21 +0800

    formatted

--- Cplus/MaximumEqualFrequency.cpp
+++ Cplus/MaximumEqualFrequency.cpp
@@ -8,27 +8,28 @@ public:
 	int maxEqualFreq(vector<int> &nums)
 	{
 		int res = 0;
-		unordered_map<int, int> freq, freqfreq;
+		unordered_map<int, int> count, freq;
 		for (int i = 0; i < (int)nums.size(); ++i)
 		{
-			if (freq.find(nums[i]) != freq.end())
+			auto it = count.find(nums[i]);
+			if (it != count.end())
 			{
-				int old = freq[nums[i]];
-				if (--freqfreq[old] == 0)
-					freqfreq.erase(old);
+				int old = it->second;
+				if (--freq[old] == 0)
+					freq.erase(old);
 			}
-			++freq[nums[i]];
-			++freqfreq[freq[nums[i]]];
-			if (freqfreq.size() == 1)
+			++count[nums[i]];
+			++freq[count[nums[i]]];
+			if (freq.size() == 1)
 			{
-				if (freq.size() == 1 || freq.begin()->second == 1)
+				if (count.size() == 1 || count.begin()->second == 1)
 					res = i + 1;
 			}
-			else if (freqfreq.size() == 2)
+			else if (freq.size() == 2)
 			{
-				int a = freqfreq.begin()->first, b = (++freqfreq.begin())->first;
-				if (((a == 1 || a - b == 1) && freqfreq[a] == 1) ||
-					((b == 1 || b - a == 1) && freqfreq[b] == 1))
+				int a = freq.begin()->first, b = (++freq.begin())->first;
+				if (((a == 1 || a - b == 1) && freq[a] == 1) ||
+					((b == 1 || b - a == 1) && freq[b] == 1))
 					res = i + 1;
 			}
 		}
