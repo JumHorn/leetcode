a4193ff0d281c2d478ef30656451ecfd6d983f4b
JumHorn <JumHorn@gmail.com>
2021-11-10 09:04:36 +0800

    formatted

--- Cplus/MaximumNumberofEventsThatCanBeAttended.cpp
+++ Cplus/MaximumNumberofEventsThatCanBeAttended.cpp
@@ -15,12 +15,12 @@ public:
 	int maxEvents(vector<vector<int>> &events)
 	{
 		priority_queue<int, vector<int>, greater<int>> q;
-		int res = 0, d = 0, N = events.size();
+		int res = 0, N = events.size();
 		vector<pair<int, int>> v;
 		for (auto &e : events)
 			v.push_back({e[0], e[1]});
 		sort(v.begin(), v.end());
-		for (int i = 0; !q.empty() || i < N;)
+		for (int i = 0, d = 0; !q.empty() || i < N;)
 		{
 			if (q.empty())
 				d = v[i].first;
