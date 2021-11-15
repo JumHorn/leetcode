fff774ff1a22b74ce4cea4a074aeb2ab6725a498
JumHorn <JumHorn@gmail.com>
2021-11-15 08:52:18 +0800

    improved

--- Cplus/FindTwoNon-overlappingSub-arraysEachWithTargetSum.cpp
+++ Cplus/FindTwoNon-overlappingSub-arraysEachWithTargetSum.cpp
@@ -9,16 +9,17 @@ public:
 	{
 		int N = arr.size(), sum = 0, res = N + 1, curMinLen = N + 1;
 		vector<int> dp(N + 1, N + 1);
-		unordered_map<int, int> m; //{value,index}
+		unordered_map<int, int> m; //{prefix sum,index}
 		m[0] = 0;
 		for (int i = 0; i < N; ++i)
 		{
 			sum += arr[i];
 			int diff = sum - target;
-			if (m.find(diff) != m.end())
+			auto it = m.find(diff);
+			if (it != m.end())
 			{
-				curMinLen = min(curMinLen, i - m[diff] + 1);
-				res = min(res, dp[m[diff]] + i - m[diff] + 1);
+				curMinLen = min(curMinLen, i - it->second + 1);
+				res = min(res, dp[it->second] + i - it->second + 1);
 			}
 			dp[i + 1] = curMinLen;
 			m[sum] = i + 1;
