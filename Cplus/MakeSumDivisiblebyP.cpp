86227a3c1daf85a45cf0d5c4e93fca7da4d6787f
JumHorn <JumHorn@gmail.com>
2021-11-19 10:29:46 +0800

    formatted

--- Cplus/MakeSumDivisiblebyP.cpp
+++ Cplus/MakeSumDivisiblebyP.cpp
@@ -9,21 +9,21 @@ public:
 	int minSubarray(vector<int> &nums, int p)
 	{
 		int N = nums.size();
-		long r = 0;
+		long r = 0, r1 = 0;
 		for (auto n : nums)
 			r = (n % p + r) % p;
 		if (r == 0)
 			return 0;
 		unordered_map<int, int> m; //{remainder,index}
-		long r1 = 0;
-		int res = INT_MAX;
 		m[0] = -1;
+		int res = INT_MAX;
 		for (int i = 0; i < N; ++i)
 		{
 			r1 = (r1 + nums[i] % p) % p;
-			int tmp = (r1 - r + p) % p;
-			if (m.find(tmp) != m.end())
-				res = min(res, i - m[tmp]);
+			int diff = (r1 - r + p) % p;
+			auto it = m.find(diff);
+			if (it != m.end())
+				res = min(res, i - it->second);
 			m[r1] = i;
 		}
 		return res >= N ? -1 : res;
