efebfc3b50cb79079a04523f0903b53aa7c03ff4
JumHorn <JumHorn@gmail.com>
2022-01-30 13:11:06 +0800

    Fermat's little theorem

--- C/NumberofSetsofKNon-OverlappingLineSegments.c
+++ C/NumberofSetsofKNon-OverlappingLineSegments.c
@@ -1,14 +1,33 @@
 /*
 combination number C(n + k - 1, 2k).
-still need inverse mod to get correct answer
+inverse mod
+费马小定理计算乘法逆元
+Fermat's little theorem
 */
+
+static const int MOD = 1e9 + 7;
+
+long long modPow(long long x, int n)
+{
+	long long res = 1;
+	for (int i = n; i > 0; i /= 2)
+	{
+		if (i % 2)
+			res = res * x % MOD;
+		x = x * x % MOD;
+	}
+	return res;
+}
 
 int combination(int n, int m)
 {
 	static const int MOD = 1e9 + 7;
-	long res = 1;
+	long long res = 1;
 	for (int i = 1; i <= m; ++i)
-		res = (res * (n - m + i) / i) % MOD;
+	{
+		// res = (res * (n - m + i) / i) % MOD;
+		res = (res * (n - m + i) % MOD * modPow(i, MOD - 2)) % MOD;
+	}
 	return res;
 }
 
