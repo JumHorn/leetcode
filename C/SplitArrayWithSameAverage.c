9a7794b8ec394189d412e1edd81a55fe94ba50a7
JumHorn <JumHorn@gmail.com>
2022-03-13 15:12:47 +0800

    add dp solution

--- C/SplitArrayWithSameAverage.c
+++ C/SplitArrayWithSameAverage.c
@@ -1,47 +1,27 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-//integer cmp function
-int cmp(const void *lhs, const void *rhs)
-{
-	if (*(int *)lhs == *(int *)rhs)
-		return 0;
-	return *(int *)lhs < *(int *)rhs ? -1 : 1;
-}
-
-bool backTracking(int *A, int ASize, int index, int count, int subsum, int sum)
-{
-	if (index >= ASize)
-		return false;
-	if (count != 0 && subsum * ASize == sum * count)
-		return true;
-	if (subsum * ASize > sum * count)
-		return false;
-	if (backTracking(A, ASize, index + 1, count, subsum, sum))
-		return true;
-	if (backTracking(A, ASize, index + 1, count + 1, subsum + A[index], sum))
-		return true;
-	return false;
-}
-
-bool isPossible(int ASize, int sum)
-{
-	for (int i = 1; i <= ASize / 2; ++i)
-	{
-		if (sum * i % ASize == 0)
-			return true;
-	}
-	return false;
-}
-
 bool splitArraySameAverage(int *A, int ASize)
 {
-	qsort(A, ASize, sizeof(int), cmp);
 	int sum = 0;
 	for (int i = 0; i < ASize; ++i)
 		sum += A[i];
-	// early pruning
-	if (!isPossible(ASize, sum))
-		return false;
-	return backTracking(A, ASize, 0, 0, 0, sum);
+	int dp[sum + 1];
+	memset(dp, 0, sizeof(dp));
+	dp[A[0]] = 0b10; //sum A[0] can be made up with lenth 1 in bit index
+	for (int i = 1; i < ASize; ++i)
+	{
+		for (int j = sum - A[i]; j >= 0; --j)
+		{
+			if (dp[j] != 0)
+				dp[j + A[i]] |= (dp[j] << 1);
+		}
+		dp[A[i]] |= 0b10;
+	}
+	for (int len = 1; len < ASize; ++len)
+	{
+		if (sum * len % ASize == 0 && ((1 << len) & dp[sum * len / ASize]))
+			return true;
+	}
+	return false;
 }
