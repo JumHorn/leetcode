1b97fed9dbc90be1a2c69ecc9b1756b4caad1fcf
JumHorn <JumHorn@gmail.com>
2022-03-30 11:18:00 +0800

    optimized with BIT

--- Cplus/CreateTargetArrayintheGivenOrder.cpp
+++ Cplus/CreateTargetArrayintheGivenOrder.cpp
@@ -1,14 +1,59 @@
+#include <set>
 #include <vector>
 using namespace std;
+
+//Fenwick tree(BIT)
+class Fenwick
+{
+public:
+	Fenwick(int size) : tree(size + 1) {}
+
+	int sum(int index) const
+	{
+		int res = 0;
+		for (++index; index > 0; index -= index & -index)
+			res += tree[index];
+		return res;
+	}
+
+	void update(int index, int delta)
+	{
+		int N = tree.size();
+		for (++index; index < N; index += index & -index)
+			tree[index] += delta;
+	}
+
+private:
+	vector<int> tree;
+};
+/********end of Fenwick tree(BIT)********/
 
 class Solution
 {
 public:
 	vector<int> createTargetArray(vector<int> &nums, vector<int> &index)
 	{
-		vector<int> res;
-		for (int i = 0; i < (int)nums.size(); ++i)
-			res.insert(res.begin() + index[i], nums[i]);
+		int N = index.size();
+		Fenwick bit(N + 1);
+		for (auto n : index)
+			bit.update(n, 1);
+		// this O(n^2) part can be optimized using BIT to O(nlogn)
+		// for (int i = 0; i < indexSize; ++i)
+		// {
+		// 	for (int j = i + 1; j < indexSize; ++j)
+		// 	{
+		// 		if (index[i] >= index[j])
+		// 			++index[i];
+		// 	}
+		// }
+		for (int i = 0; i < N; ++i)
+		{
+			bit.update(index[i], -1);
+			index[i] += bit.sum(index[i]);
+		}
+		vector<int> res(N);
+		for (int i = 0; i < N; ++i)
+			res[index[i]] = nums[i];
 		return res;
 	}
 };
