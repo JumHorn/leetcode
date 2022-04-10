c13e3b0bd72ccede6189b69c1b25481e42b51cd1
JumHorn <JumHorn@gmail.com>
2022-04-10 14:08:36 +0800

    optimized

--- Cplus/CountSubstringsThatDifferbyOneCharacter.cpp
+++ Cplus/CountSubstringsThatDifferbyOneCharacter.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <vector>
 using namespace std;
 
 class Solution
@@ -7,18 +8,26 @@ public:
 	int countSubstrings(string s, string t)
 	{
 		int N1 = s.length(), N2 = t.length(), res = 0;
+		vector<vector<int>> dp_pre(N1 + 1, vector<int>(N2 + 1));
+		for (int i = 0; i < N1; ++i)
+		{
+			for (int j = 0; j < N2; ++j)
+				dp_pre[i + 1][j + 1] = (s[i] == t[j] ? dp_pre[i][j] + 1 : 0);
+		}
+
+		vector<vector<int>> dp_next(N1 + 1, vector<int>(N2 + 1));
+		for (int i = N1 - 1; i >= 0; --i)
+		{
+			for (int j = N2 - 1; j >= 0; --j)
+				dp_next[i][j] = (s[i] == t[j] ? dp_next[i + 1][j + 1] + 1 : 0);
+		}
+
 		for (int i = 0; i < N1; ++i)
 		{
 			for (int j = 0; j < N2; ++j)
 			{
-				int diff = 0;
-				for (int k = 0; i + k < N1 && j + k < N2 && diff < 2; ++k)
-				{
-					if (s[i + k] != t[j + k])
-						++diff;
-					if (diff == 1)
-						++res;
-				}
+				if (s[i] != t[j])
+					res += (dp_pre[i][j] + 1) * (dp_next[i + 1][j + 1] + 1);
 			}
 		}
 		return res;
