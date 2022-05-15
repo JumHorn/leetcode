a03d9f732d20fa9d54934f11f1df5d7eac12adb7
JumHorn <JumHorn@gmail.com>
2022-05-15 13:21:54 +0800

    logical problem

--- Cplus/SubstringWithLargestVariance.cpp
+++ Cplus/SubstringWithLargestVariance.cpp
@@ -8,43 +8,37 @@ public:
 	int largestVariance(string s)
 	{
 		int res = 0, N = s.length();
-		vector<int> prefix(N + 1), suffix(N + 1);
 		for (char i = 'a'; i <= 'z'; ++i)
 		{
 			for (char j = 'a'; j <= 'z'; ++j)
 			{
 				if (i != j)
-					res = max(res, variance(s, i, j, prefix, suffix));
+					res = max(res, variance(s, i, j));
 			}
 		}
 		return res;
 	}
 
-	int variance(string s, char a, char b, vector<int> &prefix, vector<int> &suffix)
+	int variance(string s, char a, char b)
 	{
-		//choose b as the only b
-		int res = 0, N = s.length();
-		prefix[0] = suffix[N] = 0;
-		for (int i = 0; i < N; ++i)
+		int res = 0, var = 0;
+		bool has_b = false, first_b = false;
+		for (auto c : s)
 		{
-			if (s[i] == a)
-				prefix[i + 1] = max(prefix[i], 0) + 1;
-			else if (s[i] == b)
-				prefix[i + 1] = max(prefix[i] - 1, 0);
-			else
-				prefix[i + 1] = prefix[i];
-
-			if (s[N - 1 - i] == a)
-				suffix[N - 1 - i] = max(suffix[N - i], 0) + 1;
-			else if (s[N - 1 - i] == b)
-				suffix[N - 1 - i] = max(suffix[N - i] - 1, 0);
-			else
-				suffix[N - 1 - i] = suffix[N - i];
-		}
-		for (int i = 0; i < N; ++i)
-		{
-			if (s[i] == b)
-				res = max(res, prefix[i] + suffix[i + 1] - 1);
+			if (c == a)
+				++var;
+			else if (c == b)
+			{
+				has_b = true;
+				if (first_b && var >= 0)
+					first_b = false;
+				else if (--var < 0)
+				{
+					first_b = true;
+					var = -1;
+				}
+			}
+			res = max(res, has_b ? var : 0);
 		}
 		return res;
 	}
