14c15c022d5f43628668a1df3540ae328f40566a
JumHorn <JumHorn@gmail.com>
2022-07-31 09:42:07 +0800

    simplified

--- Cplus/QueensThatCanAttacktheKing.cpp
+++ Cplus/QueensThatCanAttacktheKing.cpp
@@ -7,29 +7,24 @@ class Solution
 public:
 	vector<vector<int>> queensAttacktheKing(vector<vector<int>> &queens, vector<int> &king)
 	{
-		int N = 8;
+		const int N = 8;
 		unordered_set<int> s;
 		for (auto &queen : queens)
 			s.insert(queen[0] * N + queen[1]);
 		vector<vector<int>> res;
-		vector<vector<int>> path = {{-1, -1}, {0, -1}, {1, -1}, {-1, 0}, {1, 0}, {-1, 1}, {0, 1}, {1, 1}};
-		vector<int> seen(N);
-		for (int i = 1; i < N; ++i)
+		for (int i = -1; i <= 1; ++i)
 		{
-			for (int j = 0; j < N; ++j)
+			for (int j = -1; j <= 1; ++j)
 			{
-				if (seen[j] == 0)
+				for (int k = 1; k < N; ++k)
 				{
-					int x = king[0] + i * path[j][0], y = king[1] + i * path[j][1];
-					if (x < 0 || x >= N || y < 0 || y >= N)
-					{
-						seen[j] = 1;
+					int x = king[0] + i * k, y = king[1] + j * k;
+					if (x < 0 || x >= N || y < 0 || y >= N) //越界后x*N+y编码就错误了
 						continue;
-					}
 					if (s.find(x * N + y) != s.end())
 					{
 						res.push_back({x, y});
-						seen[j] = 1;
+						break;
 					}
 				}
 			}
