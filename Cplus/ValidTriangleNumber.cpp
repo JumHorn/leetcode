3bde91480691aa56399808b1026d753c642aa08d
JumHorn <JumHorn@gmail.com>
2022-10-24 09:15:50 +0800

    simplified

--- Cplus/ValidTriangleNumber.cpp
+++ Cplus/ValidTriangleNumber.cpp
@@ -11,10 +11,7 @@ public:
 		int res = 0, N = nums.size();
 		for (int i = 0; i < N - 2; ++i)
 		{
-			if (nums[i] <= 0)
-				continue;
-			int k = i + 2;
-			for (int j = i + 1; j < N - 1; ++j)
+			for (int j = i + 1, k = i + 2; j < N - 1; ++j)
 			{
 				while (k < N && nums[i] + nums[j] > nums[k])
 					++k;
